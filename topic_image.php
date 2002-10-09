42ca432da9021e580abba36a9ed56ff2e11212d1
lrargerich <lrargerich@users.sourceforge.net>
2002-10-09 19:45:38 +0000

    new file, this file is used to show the topic image
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@7 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ topic_image.php
@@ -0,0 +1,16 @@
+<?
+// show_image.php
+// application to display an image from the database with 
+// option to resize the image dynamically creating a thumbnail on the fly.
+if(!isset($_REQUEST["id"])) {
+  die;
+}
+include_once('db/tiki-db.php');
+include_once('lib/tikilib.php');
+$tikilib = new Tikilib($dbTiki);
+$data = $tikilib->get_topic_image($_REQUEST["id"]);
+$type=$data["image_type"];
+$data = $data["image_data"];
+header("Content-type: $type");
+echo $data;
+?>
