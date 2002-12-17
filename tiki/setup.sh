1d435046640a9cc658242511d95f083a981a3061
rossta <rossta@users.sourceforge.net>
2002-12-17 07:52:06 +0000

    More setup related changes, feedback anyone?
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@67 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+if [ -z "$1" ];
+then
+	cat <<EOF
+Usage $0 user [group]
+
+For example, if apache is running as user nobody, type:
+
+  su -c '$0 nobody'
+
+Alternatively, you may wish to set both the user and group:
+  
+  su -c '$0 mylogin nobody'
+EOF
+exit 1
+fi
+
+if [ ! -d modules/cache ];
+then
+	mkdir -p modules/cache
+fi
+if [ ! -d templates_c ];
+then
+	mkdir -p templates_c
+fi
+
+chown -R $1.$1 modules/cache templates_c
+
+if [ -n "$2" ];
+then
+	chgrp -R $2 modules/cache templates_c
+	chmod g+w modules/cache templates_c
+fi
+
