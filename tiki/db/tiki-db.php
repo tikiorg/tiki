44b1dc04cc55cba93b5b0c1287a66cc3ae4284b5
rossta <rossta@users.sourceforge.net>
2002-12-19 16:14:35 +0000

    Removed security stuff, because it really is not secure
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@75 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/db/tiki-db.php
+++ tiki/db/tiki-db.php
@@ -7,21 +7,16 @@ $pass_tiki   = '';
 $dbs_tiki    = 'tiki14b1';
 
 /*
-To prevent others from discovering the above information, 
-do not enter your username/password above.
-Instead, create a php file containing the above four lines
-and name the file using a cryptographically secure name
-(do not use randomcharshere.php!).
-Then remove read/write rights to the db/ directory.
-
-Developers: This method allows you to change the username/password
-without having to change this file 
-(and potentially committing your username/password to CVS!).
+CVS Developers: Do not change any of the above.
+Instead, create a file, called local.php, containing any of
+the variables listed above that are different for your 
+development environment.  This will protect you from 
+accidentally committing your username/password to CVS!
 
 For example:
 
 cd <tiki_dir>/db
-cat >randomcharshere.php <<EOF
+cat >local.php <<EOF
 <?php
 \$host_tiki   = 'myhost';
 \$user_tiki   = 'myuser';
@@ -29,13 +24,9 @@ cat >randomcharshere.php <<EOF
 \$dbs_tiki    = 'mytiki';
 ?>
 EOF
-chmod go-rw+x .
-
-Finally, change the following name to match the random filename 
-you selected:
 */
 
-$file = dirname(__FILE__) . '/randomcharshere.php';
+$file = dirname(__FILE__) . '/local.php';
 if (file_exists($file))
 	require_once($file);
 
