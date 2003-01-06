700f721d1fc92597021d2fae753fc65386d673a2
rossta <rossta@users.sourceforge.net>
2003-01-06 03:07:53 +0000

    Changed session_unregister() to unset($_SESSION[]) per Damien
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@158 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-logout.php
+++ tiki/tiki-logout.php
@@ -3,8 +3,9 @@
 require_once('tiki-setup.php');
 $userlib->user_logout($user);
 session_unregister("user");
+unset($_SESSION['user']);
 session_destroy();
 unset($user);
 header("location: $tikiIndex");
-die;
+exit;
 ?>
