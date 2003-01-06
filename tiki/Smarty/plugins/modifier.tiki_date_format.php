6cf987fa9ae128de056145f21f9cbea2a37132d0
rossta <rossta@users.sourceforge.net>
2003-01-06 03:43:28 +0000

    Final (?!) changes to get date/times to display in the right timezone
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@159 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/Smarty/plugins/modifier.tiki_date_format.php
+++ tiki/Smarty/plugins/modifier.tiki_date_format.php
@@ -1,4 +1,4 @@
-<?php
+	<?php
 
 /*
  * Smarty plugin
@@ -16,23 +16,67 @@ function smarty_modifier_tiki_date_format($string, $format = "%b %e, %Y", $defau
 {
 	global $tikilib, $user;
 
-	require_once('lib/Date.php');
-	
 	if (!isset($string) || strlen($string) == 0) {
 		if (!isset($default_date) || strlen($default_date) == 0)
 			return;
 		$string = $default_date;
 	}
 	
-	if (!$tikilib)
-		return strftime($format, smarty_make_timestamp($string));
-
 	$timestamp = smarty_make_timestamp($string);
 
-	@setlocale(LC_TIME, $tikilib->get_locale($user));
+	if (!$tikilib)
+		return strftime($format, $timestamp);
+
+	static $localed = false;
+	if (!$localed) {
+		$localed = true;
+#		@setlocale(LC_TIME, $tikilib->get_locale($user));
+	}
+
+	require_once('lib/Date.php');
+
+	$original_tz = date('T', $timestamp);
+	
+$rv = "\n<pre>\n";
+$rv .= strftime($format, $timestamp);
+$rv .= " =timestamp\n";
+$rv .= strftime('%Z', $timestamp);
+$rv .= " =strftime('%Z')\n";
+$rv .= date('T', $timestamp);
+$rv .= " =date('T')\n";
 
 	$date = new Date($timestamp);
-	$date->convertTZbyID($tikilib->get_display_timezone($user));
+
+# Calling new Date() changes the timezone of the $timestamp var!
+# so we only change the timezone to UTC if the original TZ wasn't UTC
+# to begin with.
+# This seems really buggy, but I don't have time to delve into right now.
+
+$rv .= date('T', $timestamp);
+$rv .= " =date('T')\n";
+
+$rv .= $date->format($format);
+$rv .= " =new Date()\n";
+
+$rv .= date('T', $timestamp);
+$rv .= " =date('T')\n";
+	
+	if ($original_tz == 'UTC') {
+		$date->setTZbyID('UTC');
+$rv .= $date->format($format);
+$rv .= " =setTZbyID('UTC')\n";
+	}
+	
+	$tz_id = $tikilib->get_display_timezone($user);
+	if ($date->tz->getID() != $tz_id) {
+		# let's convert to the displayed timezone
+		$date->convertTZbyID($tz_id);
+$rv .= $date->format($format);
+$rv .= " =convertTZbyID($tz_id)\n";
+	}
+
+#return $rv;
+
 #	if ($format == "%b %e, %Y")
 #		$format = $tikilib->get_short_date_format();
    	return $date->format($format);
