e69c6f44d5f431a374032375c71e447ff3e38e3c
lrargerich <lrargerich@users.sourceforge.net>
2003-03-15 18:54:09 +0000

    Plugins dist directory added
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@281 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins-dist/wikiplugin_code.php
@@ -0,0 +1,21 @@
+<?php
+
+// Displays a snippet of code
+// Parameters: bgcolor (optional background color)
+// Example:
+// {CODE()}
+// print("foo");
+// {CODE}
+
+function wikiplugin_code($data,$params) {
+  global $tikilib;
+  extract($params);
+  $code=htmlspecialchars(trim($data));
+  if(!isset($bgcolor)) {$bgcolor='#EEEEEE';}
+  $data = '<div style="background-color:$bgcolor;">'.$code.'</div>';
+  return $data;
+}
+
+
+?>
+
