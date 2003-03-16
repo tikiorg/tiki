0c210ba8cab50bb8cef0681d92367ae0b285ac41
lrargerich <lrargerich@users.sourceforge.net>
2003-03-16 19:49:07 +0000

    File for running tests and missing file added
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@285 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki-tests.php
@@ -0,0 +1,14 @@
+<?php  
+include_once('tiki-setup.php');
+include_once('PHPUnit.php');
+
+$h = opendir('tests');
+while($file=readdir($h)) {
+  if(strstr($file,'.php') ) {
+    print("<b>$file</b>");
+    include_once("tests/$file");
+  }
+}
+closedir($h);
+
+?>
