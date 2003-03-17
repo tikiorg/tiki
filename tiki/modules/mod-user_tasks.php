213305ca94a153cdce810a5e5369638ce72fa7ad
lrargerich <lrargerich@users.sourceforge.net>
2003-03-17 19:07:38 +0000

    Improved module for user tasks
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@293 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/modules/mod-user_tasks.php
+++ tiki/modules/mod-user_tasks.php
@@ -14,13 +14,15 @@ if($user && $feature_tasks == 'y') {
 
   }
   if(isset($_REQUEST["modTasksCom"])) {
-
     foreach(array_keys($_REQUEST["modTasks"]) as $task) {      	
       $tikilib->complete_task($user, $task);
     }
-
-
   }
+  
+  if(isset($_REQUEST["modTasksSave"])) {
+    $tikilib->replace_task($user,0,$_REQUEST['modTasksTitle'],$_REQUEST['modTasksTitle'],date("U"),'o',3,0,0);
+  }
+  
   $ownurl = httpPrefix().$_SERVER["REQUEST_URI"];
   $smarty->assign('ownurl',$ownurl);
   $tasks_useDates = $tikilib->get_user_preference($user,'tasks_useDates');
--- tiki/tiki-custom_home.php
+++ tiki/tiki-custom_home.php
@@ -6,15 +6,15 @@ require_once('tiki-setup.php');
 require_once "lib/NNTP.php";
 $nntp = new Net_NNTP;
 $ret = $nntp->connect("news.php.net");
-$headers = $nntp->getHeaders($msg_id);
-if( PEAR::isError($headers)) {
- // handle error
-} else {
- // success - print the header
- echo $headers;
-}
+$groups = $nntp->getGroups();
+//print_r($groups);
+$z = $nntp->selectGroup('php.announce');
+print_r($z);
+$h = $nntp->splitHeaders(1);
+print_r($h);
+$b = $nntp->getBody(1);
+print_r($b);
 */
-
 
 
 if($feature_custom_home != 'y') {
