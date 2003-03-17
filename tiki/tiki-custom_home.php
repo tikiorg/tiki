eb2d2d1383930e9859b668128eb09bb5337829ce
lrargerich <lrargerich@users.sourceforge.net>
2003-03-17 14:19:46 +0000

    NNTP library added. This will be used by the news reader section (coming soon...)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@291 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-custom_home.php
+++ tiki/tiki-custom_home.php
@@ -1,7 +1,21 @@
 <?php
 // Initialization
-
 require_once('tiki-setup.php');
+
+/*
+require_once "lib/NNTP.php";
+$nntp = new Net_NNTP;
+$ret = $nntp->connect("news.php.net");
+$headers = $nntp->getHeaders($msg_id);
+if( PEAR::isError($headers)) {
+ // handle error
+} else {
+ // success - print the header
+ echo $headers;
+}
+*/
+
+
 
 if($feature_custom_home != 'y') {
     $smarty->assign('msg',tra("This feature has been disabled"));
