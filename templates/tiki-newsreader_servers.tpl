861d90ec5cb36894381aaa031c51b444599c05d7
lrargerich <lrargerich@users.sourceforge.net>
2003-03-17 21:22:25 +0000

    Newsreader additions
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@295 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ templates/tiki-newsreader_servers.tpl
@@ -0,0 +1,2 @@
+{*Smarty template*}
+<a class="pagetitle" href="tiki-newsreader_servers.php">{tr}Configure news servers{/tr}</a><br/><br/>
---
+++ tiki-newsreader_servers.php
@@ -0,0 +1,20 @@
+<?php
+require_once('tiki-setup.php');
+include_once('lib/newsreader/newslib.php');
+
+if(!$user) {
+   $smarty->assign('msg',tra("You are not logged in"));
+   $smarty->display("styles/$style_base/error.tpl");
+   die;
+}
+
+
+if($feature_newsreader != 'y') {
+  $smarty->assign('msg',tra("This feature is disabled"));
+  $smarty->display("styles/$style_base/error.tpl");
+  die;  
+}
+
+$smarty->assign('mid','tiki-newsreader_servers.tpl');
+$smarty->display('tiki.tpl');
+?>
