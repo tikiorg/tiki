b5acd6e2af4ca7587418d66ad7e2a487eb08a711
lrargerich <lrargerich@users.sourceforge.net>
2003-03-21 13:42:28 +0000

    New libraries added (tikilib factorization)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@335 b456876b-0849-0410-b77d-98878d47e9d5

--- categorize.php
+++ categorize.php
@@ -1,4 +1,5 @@
 <?php
+  include_once('lib/categories/categlib.php');
   if($feature_categories == 'y') {
     $smarty->assign('cat_categorize','n');
     if(isset($_REQUEST["cat_categorize"])&&$_REQUEST["cat_categorize"]=='on') {
@@ -7,20 +8,20 @@
   
     if(isset($_REQUEST["cat_categories"])) {
       if(isset($_REQUEST["cat_categorize"])&&$_REQUEST["cat_categorize"]=='on') {
-        $tikilib->uncategorize_object($cat_type,$cat_objid);
+        $categlib->uncategorize_object($cat_type,$cat_objid);
         foreach($_REQUEST["cat_categories"] as $cat_acat) {
           if($cat_acat) {
-            $catObjectId=$tikilib->is_categorized($cat_type,$cat_objid);
+            $catObjectId=$categlib->is_categorized($cat_type,$cat_objid);
             if(!$catObjectId) {
               // The object is not cateorized  
-              $catObjectId = $tikilib->add_categorized_object($cat_type,$cat_objid,$cat_desc,$cat_name,$cat_href);
+              $catObjectId = $categlib->add_categorized_object($cat_type,$cat_objid,$cat_desc,$cat_name,$cat_href);
             }
-            $tikilib->categorize($catObjectId,$cat_acat);
+            $categlib->categorize($catObjectId,$cat_acat);
           }
         }
       }
     }
-    $categories = $tikilib->list_all_categories(0,-1,'name_asc','',$cat_type,$cat_objid);
+    $categories = $categlib->list_all_categories(0,-1,'name_asc','',$cat_type,$cat_objid);
     $smarty->assign_by_ref('categories',$categories["data"]);
   }
 ?>
--- categorize_list.php
+++ categorize_list.php
@@ -1,11 +1,11 @@
-<?php # $Header: /cvsroot/tikiwiki/tiki/categorize_list.php,v 1.2 2003-01-04 19:34:15 rossta Exp $
-
+<?php # $Header: /cvsroot/tikiwiki/tiki/categorize_list.php,v 1.3 2003-03-21 13:42:26 lrargerich Exp $
+include_once('lib/categories/categlib.php');
 if($feature_categories == 'y') {
    $smarty->assign('cat_categorize','n');
    if(isset($_REQUEST["cat_categorize"])&&$_REQUEST["cat_categorize"]=='on') {
      $smarty->assign('cat_categorize','y');
    }
-   $categories = $tikilib->list_all_categories(0,-1,'name_asc','',$cat_type,$cat_objid);
+   $categories = $categlib->list_all_categories(0,-1,'name_asc','',$cat_type,$cat_objid);
    if(isset($_REQUEST["cat_categories"])&&isset($_REQUEST["cat_categorize"])&&$_REQUEST["cat_categorize"]=='on') {
      for($i=0;$i<count($categories["data"]);$i++) {
        if(in_array($categories["data"][$i]["categId"],$_REQUEST["cat_categories"])) {
