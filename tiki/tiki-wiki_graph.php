762c051b27565ce6ef4c038f147a72fce890cd92
lrargerich <lrargerich@users.sourceforge.net>
2003-04-02 18:59:03 +0000

    Plugin to generate graphs of wikis and subwikis indicating the source page name and number of levels to follow (soooo nice)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@403 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/tiki-wiki_graph.php
@@ -0,0 +1,14 @@
+<?php
+include_once("lib/graphviz/GraphViz.php");
+include_once('tiki-setup.php');
+include_once('lib/wiki/wikilib.php');
+
+
+
+
+if(!isset($_REQUEST['level'])) $_REQUEST['level']=0;
+$str = $wikilib->wiki_get_link_structure($_REQUEST['page'],$_REQUEST['level']);
+$graph = new Image_GraphViz();
+$wikilib->wiki_page_graph($str,$graph);
+$graph->image();
+?>
