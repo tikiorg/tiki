64c68337364adf07f5e0e57c347417aa298deb63
lrargerich <lrargerich@users.sourceforge.net>
2003-04-09 18:50:31 +0000

    Experimental install script added
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@447 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ templates/tiki-install.tpl
@@ -0,0 +1,138 @@
+<div style="margin-left:180px;margin-right:180px;">
+<h1>Tiki installer v1.6 -Tau Ceti-</h1>
+<a href="tiki-install.php?restart=1" class="link">reload</a><br/><br/>
+{if $can_write eq 'n'}
+  <b>We have problems!</b><br/>
+  The following directories must be writeable by Tiki, please
+  check the permissions on those directories and then re-run
+  the install script. <br/><br/>
+  db/<br/>
+  img/wiki<br/>
+  img/wiki_up<br/>
+  modules/cache<br/>
+  templates_c<br/>
+  backups<br/>
+  temp<br/><br/>
+  <a href="tiki-install.php" class="link">try again!</a>
+{else}
+	{if $dbcon eq 'n'}
+	  <b>Tiki cannot find a database connection</b><br/>
+	  Please enter your database connection info<br/><br/>
+	  <form action="tiki-install.php" method="post">
+	  <table class="normal">
+	  	<tr>
+	  		<td class="formcolor">Host:</td>
+	  		<td class="formcolor">
+	  			<table><tr><td>
+	  			<input type="text" name="host" value="localhost" />
+	  			</td><td>
+	  			<small>Hostname or IP for your MySQL database, example:
+	  			localhost if running in the same machine as tiki</small>
+	  			</td></tr></table>
+	  		
+	  		</td>
+	  	</tr>
+	  	<tr>
+	  		<td class="formcolor">User:</td>
+	  		<td class="formcolor">
+		  	  <table><tr><td>
+	  		  <input type="text" name="user" />
+	  		  </td><td>
+	  		  <small>Database user</small>
+	  		  </td></tr></table>
+	  		</td>
+	  	</tr>
+	  	<tr>
+	  		<td class="formcolor">Password:</td>
+	  		<td class="formcolor">
+		  	  <table><tr><td>
+	  		  <input type="text" name="pass" />
+	  		  </td><td>
+	  		  <small>Database password</small>
+	  		  </td></tr></table>
+	  		</td>
+	  	</tr>
+	  	<tr>
+	  		<td class="formcolor">Database name:</td>
+	  		<td class="formcolor">
+	  		<table><tr><td>
+	  		<input type="text" name="name" />
+	  		</td><td>
+	  		<small>
+	  		The name of the database where tiki will create tables. You can
+	  		create the database using mysqladmin, or PHPMyAdmin or ask your
+	  		hosting service to create a MySQL database.
+	  		Normally Tiki tables won't conflict with other product names. 
+	  		</small>
+	  		</td></tr></table>
+	  		</td>
+	  	</tr>
+	  	<tr>
+	  		<td class="formcolor">&nbsp;</td>
+	  		<td class="formcolor"><input type="submit" name="dbinfo" /></td>
+	  	</tr>
+	  	
+	  </table>
+	  </form>
+	{else}
+	  {* we do have a database connection *}
+	  {if $dbdone eq 'n'}
+		  {if $logged eq 'y'}
+		    {* we are logged if no admin account is found or if he user logged in*}
+		    <b>Welcome to the installation script!</b><br/>
+		    You can now create a new database or update your current database<br/><br/>
+		    <form method="post" action="tiki-install.php">
+		    <table>
+			<tr><td>
+			Create database from scratch <input type="submit" name="scratch" value="create" />	    
+		    </td></tr>
+		    <tr><td>
+		    Update database using script: 
+		    <select name="file">
+		    {section name=ix loop=$files}
+		    <option value="{$files[ix]}">{$files[ix]}</option>
+		    {/section}
+		    </select>
+		    <input type="submit" name="update" value="update" />
+		    </td></tr>
+		    </table>
+		    </form><br/>
+			<a href="tiki-index.php" class="link">Do nothing and enter Tiki!</a>		    
+		  {else}
+			{* we are not logged then no admin account found and user not logged*}
+			<b>This site has an admin account configured</b><br/>
+		    Please enter your admin password to continue<br/><br/>
+		  	<form action="tiki-install.php" method="post">
+		  	<table class="normal">
+		  		<tr>
+		  			<td class="formcolor">Admin password</td>
+		  			<td class="formcolor"><input type="password" name="pass" /></td>
+		  		</tr>
+		  		<tr>
+		  			<td class="formcolor">&nbsp;</td>
+		  			<td class="formcolor"><input type="submit" name="login" value="login" /></td>
+		  		</tr>
+		    </table>
+		    </form>  
+		  {/if}
+    	{else}
+    		<b>Print operations executed succesfully</b><br/>
+    		<textarea rows="5" cols="80">
+    		{section loop=$commands name=ix}
+    		{$commands[ix]}
+    		{/section}
+    		</textarea><br/><br/>
+    		Your database has been configured and Tiki is ready to run, if
+    		this is your first install your admin password is 'admin'. You can
+    		now log in into Tiki as 'admin' - 'admin' and start configuring
+    		the application.<br/>
+    		Note: This install script may be potentially harmful so we strongly
+    		recommend you to remove the script and then proceed into Tiki. If
+    		you decide to remove the script it will be renamed to tiki-install.done<br/><br/>
+    		<a href="tiki-install.php?kill=1" class="link">Click here to remove the install script and proceed into tiki</a><br/>
+    		<a href="tiki-index.php" class="link">Click here to proceed into tiki without removing the script</a>
+    	{/if}
+	{/if}
+
+{/if}
+</div>
---
+++ tiki-install.php
@@ -0,0 +1,266 @@
+<?
+session_start();
+// Define and load Smarty components
+define('SMARTY_DIR',"Smarty/");
+require_once(SMARTY_DIR.'Smarty.class.php');
+
+
+$commands=Array();
+function process_sql_file($file) {
+  global $dbTiki;
+  global $commands;
+  global $smarty;
+  $command = '';
+  $fp = fopen("db/$file","r");
+  while($line = fgets($fp)) {
+    if(substr($line,0,1)!='#') {
+      $command.=$line;
+    }
+    if(strstr($command,';')) {
+      $command = trim($command);
+      $commands[] = $command;
+      $result = $dbTiki->query($command);
+      if(DB::isError($result)) {
+      	trigger_error("MYSQL error:  ".$result->getMessage()." in query:<br/>".$command."<br/>",E_USER_WARNING);
+      	die;
+      }
+      $command = '';
+    }
+  }
+  $smarty->assign('commands',$commands);
+}
+
+
+
+class Smarty_Sterling extends Smarty {
+  function Smarty_Sterling() {
+    $this->template_dir = "templates/";
+    $this->compile_dir = "templates_c/";
+    $this->config_dir = "configs/";
+    $this->cache_dir = "cache/";
+    $this->caching = false;
+    $this->assign('app_name','Sterling');
+    //$this->debugging = true;
+    //$this->debug_tpl = 'debug.tpl';
+  }
+  
+  function _smarty_include($_smarty_include_tpl_file, $_smarty_include_vars)  
+  {
+    global $style;
+    global $style_base;
+
+    if(isset($style)&&isset($style_base)) {
+      if(file_exists("templates/styles/$style_base/$_smarty_include_tpl_file")) {
+        $_smarty_include_tpl_file="styles/$style_base/$_smarty_include_tpl_file";
+      } 
+      
+    }
+
+    return parent::_smarty_include($_smarty_include_tpl_file, $smarty_include_vars);
+  }
+
+  function fetch($_smarty_tpl_file, $_smarty_cache_id = null, $_smarty_compile_id = null, $_smarty_display = false) {
+    global $language;
+    global $style;
+    global $style_base;
+    
+    if(isset($style)&&isset($style_base)) {
+      if(file_exists("templates/styles/$style_base/$_smarty_tpl_file")) {
+        $_smarty_tpl_file="styles/$style_base/$_smarty_tpl_file";
+      }
+    }
+    
+    $_smarty_cache_id = $language.$_smarty_cache_id;
+    $_smarty_compile_id = $language.$_smarty_compile_id;
+    return parent::fetch($_smarty_tpl_file, $_smarty_cache_id, $_smarty_compile_id, $_smarty_display);
+  }
+ 
+}
+
+
+if(isset($_REQUEST['kill'])) {
+  rename('tiki-install.php','tiki-install.done');
+  header('location: tiki-index.php');
+  die;
+}
+
+$smarty = new Smarty_Sterling();
+$smarty->load_filter('pre','tr');
+$smarty->load_filter('output','trimwhitespace');
+$smarty->assign('style','default.css');
+$smarty->assign('mid','tiki-install.tpl');
+
+
+// First checking writeable directories
+$can_write = is_writable('db/') &&
+			 is_writable('templates_c/') &&
+			 is_writable('temp') &&
+			 is_writable('backups') &&
+			 is_writable('img/wiki') &&
+			 is_writable('img/wiki_up') &&
+			 is_writable('modules/cache');
+if($can_write) {
+  $smarty->assign('can_write','y');
+} else {
+  $smarty->assign('can_write','n');
+}		
+			 
+// Second check try to connect to the database
+// if no local.php => no con
+// if local then build dsn and try to connect
+//   then get con or nocon
+$separator='';
+$current_path=ini_get('include_path');
+if(strstr($current_path, ';')) {
+	$separator=';'; 
+} else {
+	$separator=':'; 
+}
+if($separator=='') $separator = ':'; // guess
+ini_set('include_path', $current_path.$separator.'lib/pear');
+include_once('DB.php');
+
+
+
+
+if(!file_exists('db/local.php')) {
+  $dbcon = false;
+  $smarty->assign('dbcon','n');
+} else {
+  // include the file to get the variables
+  include('db/local.php');
+  $dsn = "mysql://$user_tiki:$pass_tiki@$host_tiki/$dbs_tiki";    
+  $dbTiki = DB::connect($dsn);
+  if (DB::isError($dbTiki)) {        
+    $dbcon = false;
+    $smarty->assign('dbcon','n');
+  } else {
+    $dbcon = true;
+    $smarty->assign('dbcon','y');
+  }
+}
+// We won't update database info unless we can't connect to the
+// database.
+if(!$dbcon && isset($_REQUEST['dbinfo'])) {
+  $filetowrite='<'.'?'.'php'."\n";
+  $filetowrite.='$host_tiki="'.$_REQUEST['host'].'";'."\n";
+  $filetowrite.='$user_tiki="'.$_REQUEST['user'].'";'."\n";
+  $filetowrite.='$pass_tiki="'.$_REQUEST['pass'].'";'."\n";
+  $filetowrite.='$dbs_tiki="'.$_REQUEST['name'].'";'."\n";
+  $filetowrite.='?'.'>';
+  $fw = fopen('db/local.php','w');
+  fwrite($fw,$filetowrite);
+  fclose($fw);
+  include('db/local.php');
+  $dsn = "mysql://$user_tiki:$pass_tiki@$host_tiki/$dbs_tiki";    
+  $dbTiki = DB::connect($dsn);
+  if (DB::isError($dbTiki)) {        
+    $dbcon = false;
+    $smarty->assign('dbcon','n');
+  } else {
+    $dbcon = true;
+    $smarty->assign('dbcon','y');
+  } 
+}
+
+
+if(isset($_REQUEST['restart'])) {
+  $_SESSION['install-logged']='';
+}
+$noadmin = false;
+$admin_acc = 'n';
+if($dbcon) {
+  // Try to see if we have an admin account
+  $query = "select hash from users_users where login='admin'";
+  @$result =  $dbTiki->query($query);
+  if(DB::isError($result)) {
+    $admin_acc = 'n';
+    $noadmin = true;
+  } else {
+    if($result->numRows()) {
+
+	    $res = $result->fetchRow(DB_FETCHMODE_ASSOC);
+	    $hash = $res['hash'];
+	    if($hash == md5('admin')) {
+	    	$admin_acc = 'y';
+		} else {
+	    	$admin_acc = 'n';
+	    }
+    } else {
+
+    	$admin_acc = 'n';
+    	$noadmin =true;
+    }
+  }
+}
+
+if($noadmin) {
+  $smarty->assign('noadmin','y');
+} else {
+  $smarty->assign('noadmin','n');
+}
+
+//Load SQL scripts
+$files=Array();
+$h = opendir('db/');
+while($file = readdir($h)) {
+  if(strstr($file,'to')) {
+    $files[]=$file;
+  }
+}
+closedir($h);
+$smarty->assign('files',$files);
+
+// If no admin account then allow the creation of an admin account
+if(!$noadmin && $admin_acc == 'n' && isset($_REQUEST['createadmin'])) {
+  if($_REQUEST['pass1']==$_REQUEST['pass2']) {
+    $hash = md5($_REQUEST['pass1']);
+    $query = "delete from users_users where login='admin'";
+    $dbTiki->query($query);
+    $pass1 = $_REQUEST['pass1'];
+    $query = "insert into users_users(login,password,hash) 
+    values('admin','$pass1','$hash')";
+    $dbTiki->query($query);
+    $admin_acc = 'y';
+  } 
+}
+$smarty->assign('admin_acc',$admin_acc);
+
+// Since we do have an admin account the user must login to 
+// use the install script
+$logged = 'n';
+if($dbcon && $admin_acc=='y' && isset($_REQUEST['login'])){
+	$hash = md5($_REQUEST['pass']);
+	$cant = $dbTiki->getOne("select count(*) from users_users where login='admin' and hash='$hash'");
+	if($cant) {
+	  $logged = 'y';
+	  $_SESSION['install-logged']='y';
+	} else {
+	  $logged = 'n';
+	}
+}
+
+// If no admin account then we are logged
+if($noadmin) {
+	$logged = 'y';  
+	$_SESSION['install-logged']='y';
+}
+
+$smarty->assign('dbdone','n');
+$smarty->assign('logged',$logged);
+
+if(isset($_SESSION['install-logged']) && $_SESSION['install-logged']=='y') {
+	$smarty->assign('logged','y');
+	if(isset($_REQUEST['scratch'])) {
+	  process_sql_file('tiki.sql');
+	  $smarty->assign('dbdone','y');
+	}
+	if(isset($_REQUEST['update'])) {
+	  print("update");
+	}
+}
+
+
+
+$smarty->display("tiki.tpl");
+?>
