5c10b306a1ffba7d7a305e40afd4a959964a353c
lrargerich <lrargerich@users.sourceforge.net>
2003-04-21 14:16:52 +0000

    Updated docs for Workflow API
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@467 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/Galaxia/src/API/Instance.php
+++ tiki/lib/Galaxia/src/API/Instance.php
@@ -1,8 +1,11 @@
 <?php
-//!! 
-//! 
+//!! Instance
+//! A class representing a process instance.
 /*!
-
+This class represents a process instance, it is used when any activity is
+executed. The $instance object is created representing the instance of a
+process being executed in the activity or even a to-be-created instance
+if the activity is a start activity. 
 */
 class Instance extends Base {
   var $properties = Array();
@@ -25,8 +28,7 @@ class Instance extends Base {
   }
   
   /*!
-  Factory method used to return an instance
-  from the database.
+  Method used to load an instance data from the database.
   */
   function getInstance($instanceId)
   {
@@ -55,6 +57,12 @@ class Instance extends Base {
 	}    
   }
   
+  /*! 
+  Sets the next activity to be executed, if the current activity is
+  a switch activity the complete() method will use the activity setted
+  in this method as the next activity for the instance. 
+  Note that this method receives an activity name as argument. (Not an Id)
+  */
   function setNextActivity($actname)
   {
     $pId = $this->pId;
@@ -68,6 +76,11 @@ class Instance extends Base {
     $this->query($query);
   }
 
+  /*!
+  This method can be used to set the user that must perform the next 
+  activity of the process. this effectively "assigns" the instance to
+  some user.
+  */
   function setNextUser($user)
   {
     $pId = $this->pId;
@@ -76,19 +89,16 @@ class Instance extends Base {
     $query = "update galaxia_instances set nextUser='$user' where instanceId=".$this->instanceId;
     $this->query($query);
   }
-
-  
-  function setNextActivityByName($name)
-  {
-    $this->nextActivity=$this->getOne("select activityId from galaxia_activities where name='$name' and pId=".$this->pId);
-  }
-  
+ 
 
     
   /*!
-   Creates a new instance for a start activity
+   \private
+   Creates a new instance.
+   This method is called in start activities when the activity is completed
+   to create a new instance representing the started process.
   */
-  function createNewInstance($activityId,$user)
+  function _createNewInstance($activityId,$user)
   {
   	// Creates a new instance setting up started,ended,user
   	// and status
@@ -122,19 +132,21 @@ class Instance extends Base {
   }
   
   
-  
-  
-  /*! sets a property in this instance */
+  /*! 
+  Sets a property in this instance. This method is used in activities to
+  set instance properties. Instance properties are inemdiately serialized.
+  */
   function set($name,$value)
   {
     $this->properties[$name] = $value;
     $props = addslashes(serialize($this->properties));
     $query = "update galaxia_instances set properties='$props' where instanceId=".$this->instanceId;
     $this->query($query);
-    //Save to the database
   }
   
-  /*! gets a property in this instance */
+  /*! 
+  Gets the value of an instance property.
+  */
   function get($name)
   {
     if(isset($this->properties[$name])) {
@@ -144,19 +156,28 @@ class Instance extends Base {
     }
   }
   
-  /*! gets activities */
+  /*! 
+  Returns an array of asocs describing the activities where the instance
+  is present, can be more than one activity if the instance was "splitted"
+  */
   function getActivities()
   {
     return $this->activities;
   }
   
-  /*! gets status */
+  /*! 
+  Gets the instance status can be
+  'completed', 'active', 'aborted' or 'exception'
+  */
   function getStatus()
   {
     return $this->status;
   }
   
-  /*! set status */
+  /*! 
+  Sets the instance status , the value can be:
+  'completed', 'active', 'aborted' or 'exception'
+  */
   function setStatus($status)
   {
     $this->status = $status; 
@@ -165,24 +186,34 @@ class Instance extends Base {
     $this->query($query);  
   }
   
+  
+  /*!
+  Returns the instanceId
+  */
   function getInstanceId()
   {
     return $this->instanceId;
   }
   
-  /*! get process */
+  /*! 
+  Returns the processId for this instance
+  */
   function getProcessId()
   {
   	return $this->pId;
   }
   
-  /*! get the user */
+  /*! 
+  Returns the user that created the instance
+  */
   function getOwner()
   {
     return $this->owner;
   }
   
-  /*! set user */
+  /*! 
+  Sets the instance creator user 
+  */
   function setOwner($user)
   {
     $this->owner = $user;
@@ -191,6 +222,12 @@ class Instance extends Base {
     $this->query($query);  
   }
   
+  
+  /*!
+  Sets the user that must execute the activity indicated by the activityId.
+  Note that the instance MUST be present in the activity to set the user,
+  you can't program who will execute an activity.
+  */
   function setActivityUser($activityId,$theuser)
   {
     if(empty($theuser)) $theuser='*';
@@ -204,17 +241,10 @@ class Instance extends Base {
 	}  
   }
   
-  function isCandidate($activityId,$user)
-  {
-	for($i=0;$i<count($this->activities);$i++) {
-	  if($this->activities[$i]['activityId']==$activityId) {
-	  	$cans = unserialize($this->activities[$i]['candidates']);
-	    return in_array($user,$cans);
-	  }
-	}  
-	return false;  
-  }
-  
+  /*!
+  Returns the user that must execute or is already executing an activity
+  wherethis instance is present.
+  */  
   function getActivityUser($activityId)
   {
 	for($i=0;$i<count($this->activities);$i++) {
@@ -224,7 +254,11 @@ class Instance extends Base {
 	}  
 	return false;
   }
-  
+
+  /*!
+  Sets the status of the instance in some activity, can be
+  'running' or 'completed'
+  */  
   function setActivityStatus($activityId,$status)
   {
 	for($i=0;$i<count($this->activities);$i++) {
@@ -236,6 +270,11 @@ class Instance extends Base {
 	}  
   }
   
+  
+  /*!
+  Gets the status of the instance in some activity, can be
+  'running' or 'completed'
+  */
   function getActivityStatus($activityId)
   {
 	for($i=0;$i<count($this->activities);$i++) {
@@ -246,6 +285,9 @@ class Instance extends Base {
 	return false;
   }
   
+  /*!
+  Resets the start time of the activity indicated to the current time.
+  */
   function setActivityStarted($activityId)
   {
   	$now = date("U");
@@ -258,6 +300,9 @@ class Instance extends Base {
 	}  
   }
   
+  /*!
+  Gets the Unix timstamp of the starting time for the given activity.
+  */
   function getActivityStarted($activityId)
   {
 	for($i=0;$i<count($this->activities);$i++) {
@@ -268,6 +313,10 @@ class Instance extends Base {
 	return false;
   }
   
+  /*!
+  \private
+  Gets an activity from the list of activities of the instance
+  */
   function _get_instance_activity($activityId)
   {
 	for($i=0;$i<count($this->activities);$i++) {
@@ -278,8 +327,9 @@ class Instance extends Base {
 	return false;
   }
 
-
-  
+  /*!
+  Sets the time where the instance was started.	  
+  */
   function setStarted($time)
   {
     $this->started=$time;
@@ -287,11 +337,17 @@ class Instance extends Base {
     $this->query($query);    
   }
   
+  /*!
+  Gets the time where the instance was started (Unix timestamp)
+  */
   function getStarted()
   {
     return $this->started;
   }
   
+  /*!
+  Sets the end time of the instance (when the process was completed)
+  */
   function setEnded($time)
   {
   	$this->ended=$time;
@@ -299,6 +355,9 @@ class Instance extends Base {
     $this->query($query);    
   }
   
+  /*!
+  Gets the end time of the instance (when the process was completed)
+  */
   function getEnded()
   {
     return $this->ended;
@@ -307,9 +366,20 @@ class Instance extends Base {
 
   
   /*!
-  Determines that the current activity
-  for this instance is completed adding a
-  workitem to the instance
+  Completes an activity, normally from any activity you should call this
+  function without arguments.
+  The arguments are explained just in case.
+  $activityId is the activity that is being completed, when this is not
+  passed the engine takes it from the $_REQUEST array,all activities
+  are executed passing the activityId in the URI.
+  $force indicates that the instance must be routed no matter if the
+  activity is auto-routing or not. This is used when "sending" an
+  instance from a non-auto-routed activity to the next activity.
+  $addworkitem indicates if a workitem should be added for the completed
+  activity.
+  YOU MUST NOT CALL complete() for non-interactive activities since
+  the engine does automatically complete automatic activities after
+  executing them.
   */
   function complete($activityId=0,$force=false,$addworkitem=true)
   {
@@ -328,7 +398,7 @@ class Instance extends Base {
   	// be created first!
 	$type = $this->getOne("select type from galaxia_activities where activityId=$activityId");  	
   	if($type=='start') {
-  	  $this->createNewInstance($activityId,$theuser);
+  	  $this->_createNewInstance($activityId,$theuser);
   	}
 	  	
 	// Now set ended
@@ -408,8 +478,10 @@ class Instance extends Base {
   }
   
   /*!
-  Terminates the instance marking the instance
+  Terminates the instance marking the instance and the process
   as completed. This is the end of a process.
+  Normally you should not call this method since it is automatically
+  called when an end activity is completed.
   */
   function terminate()
   {
@@ -423,6 +495,12 @@ class Instance extends Base {
   	$this->activities = Array();
   }
   
+  
+  /*!
+  Sends the instance from some activity to another activity.
+  You should not call this method unless you know very very well what
+  you are doing.
+  */
   function sendTo($from,$activityId,$split=false)
   {
     //1: if we are in a join check
@@ -526,7 +604,9 @@ class Instance extends Base {
    
   }
   
-  /*! Gets a comment */
+  /*! 
+  Gets a comment for this instance 
+  */
   function get_instance_comment($cId)
   {
     $iid = $this->instanceId;
@@ -537,7 +617,9 @@ class Instance extends Base {
 	      
   }
   
-  /*! Inserts or updates an instance comment */
+  /*! 
+  Inserts or updates an instance comment 
+  */
   function replace_instance_comment($cId, $activityId, $activity, $user, $title, $comment)
   {
     if(!$user) $user='"Anonymous"';
