9c5eb3e0cf5c09f8370ed70772fbe756fb7231f5
sylvieg <sylvieg@users.sourceforge.net>
2003-06-06 21:56:09 +0000

    more directories are scanned add a comment at the end of the potential not used strings find strings introduced by "tra('" and "tra (""....
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@657 b456876b-0849-0410-b77d-98878d47e9d5

--- get_strings.php
+++ get_strings.php
@@ -12,60 +12,54 @@ $languages = Array('tw','dk','ru','de','en','sp','fr','dk','ru','sw','it','pl');
 //$languages = Array('it','pl');
 
 $files = Array();
-  
-$handle=opendir('templates');
-while ($file = readdir($handle))
-{
-  if($file=='.'||$file=='..')
-  continue;
-  if(substr($file,strlen($file)-3,3)=="tpl") {
-    print("File: $file<br/>");  
-    $files[] = 'templates/'.$file;
-  }
-}    	
-closedir($handle);
 
-$handle=opendir('templates/modules');
-while ($file = readdir($handle))
-{
-  if($file=='.'||$file=='..')
-  continue;
-  if(substr($file,strlen($file)-3,3)=="tpl") {
-    print("File: $file<br/>");  
-    $files[] = 'templates/modules/'.$file;
-  }
-}    	
-closedir($handle);
-
-
+// tpl directories - look for all the .tpl files
+$dirs = Array('templates/');
+chdir('templates/');// see bug on is_dir on php.net
 $handle=opendir('.');
-while ($file = readdir($handle))
-{
-  if($file=='.'||$file=='..')
-  continue;
-  if(substr($file,strlen($file)-3,3)=="php") {
-    print("File: $file<br/>");  
-    $files[] = $file;
+while ($file = readdir($handle)) {
+  if (is_dir($file) && $file != "." && $file != "..")
+     $dirs[] = "templates/$file/";
+}
+chdir("..");
+foreach ($dirs as $dir) { 
+  $handle=opendir($dir);
+  while ($file = readdir($handle)) {
+    if($file=='.'||$file=='..')
+      continue;
+    if(substr($file,strlen($file)-3,3)=="tpl") {
+      print("File: $dir$file<br/>");  
+      $files[] = $dir.$file;
+    }
+  }    	
+  closedir($handle);
+}
+
+//php directories - look for all the .php files
+$dirs=Array('./', 'modules/', 'lib/');
+chdir('lib/');
+$handle=opendir('.');
+while ($file = readdir($handle)) {
+  if (is_dir($file) && $file != "." && $file != "..")
+     $dirs[] = "lib/$file/";
+}
+chdir("..");
+foreach ($dirs as $dir) {
+  $handle=opendir($dir);
+  while ($file = readdir($handle)) {
+    if($file=='.'||$file=='..')
+      continue;
+    if(substr($file,strlen($file)-3,3)=="php") {
+      print("File: $dir$file<br/>");  
+      $files[] = $dir.$file;
+    }
+  }    	
+  closedir($handle);
   }
-}    	
-closedir($handle);
-
-$handle=opendir('modules/');
-while ($file = readdir($handle))
-{
-  if($file=='.'||$file=='..')
-  continue;
-  if(substr($file,strlen($file)-3,3)=="php") {
-    print("File: $file<br/>");  
-    $files[] = 'modules/'.$file;
-  }
-}    	
-closedir($handle);
-
-
 
 foreach($languages as $sel) {
 unset($lang);
+unset($used);
 require("lang/$sel/language.php");
 $fw = fopen("lang/$sel/new_language.php",'w+');
 print("&lt;?php\n<br/>\$lang=Array(\n<br/>");
@@ -76,8 +70,10 @@ foreach($files as $file) {
   fclose($fp);
   preg_match_all("/\{tr\}([^\{]+)\{\/tr\}/",$data,$words);
   foreach(array_unique($words[1]) as $word) {
+    if (!isset($used[$word]))
+       $used[$word] = 1;
     if(isset($lang[$word])) {
-      print('"'.$word.'" => "'.$lang[$word].'",'."\n<br/>");   
+      print('"'.$word.'" => "'.$lang[$word].'",'."\n<br/>");
       //fwrite($fw,'"'.$word.'" => "'.$lang[$word].'",'."\n");
     } else {
       print('+++"'.$word.'" => "'.$word.'",'."\n<br/>");  
@@ -85,8 +81,23 @@ foreach($files as $file) {
       //fwrite($fw,'"'.$word.'" => "'.$word.'",'."\n");
     }
   }
-  preg_match_all("/tra\(\"([^\"]+)\"\)/",$data,$words);
+  preg_match_all("/tra[ \t]*\( *\"([^\"]+)\"[ \t]*\)/",$data,$words);
   foreach(array_unique($words[1]) as $word) {
+    if (!isset($used[$word]))
+       $used[$word] = 1;
+    if(isset($lang[$word])) {
+      print('<b>"'.$word.'" => "'.$lang[$word].'",'."</b>\n<br/>");   
+      //fwrite($fw,'"'.$word.'" => "'.$lang[$word].'",'."\n");
+    } else {
+      print('<b>+++"'.$word.'" => "'.$word.'",'."</b>\n<br/>");  
+      $lang[$word]=$word;
+      //fwrite($fw,'"'.$word.'" => "'.$word.'",'."\n");
+    }
+  }
+  preg_match_all("/tra[ \t]*\( *\'([^\']+)\'[ \t]*\)/",$data,$words);
+  foreach(array_unique($words[1]) as $word) {
+	if (!isset($used[$word]))
+      $used[$word] = 1;
     if(isset($lang[$word])) {
       print('<b>"'.$word.'" => "'.$lang[$word].'",'."</b>\n<br/>");   
       //fwrite($fw,'"'.$word.'" => "'.$lang[$word].'",'."\n");
@@ -99,7 +110,12 @@ foreach($files as $file) {
 }
 print('"'.'##end###'.'" => "'.'###end###'.'"'.");?&gt;\n<br/>");  
 foreach($lang as $key=>$val) {
-  fwrite($fw,'"'.$key.'" => "'.$val.'",'."\n");
+  fwrite($fw,'"'.$key.'" => "'.$val.'",');
+  //fwrite($fw,'"'.$key.'" => "'.utf8_decode($val).'",');
+  if (isset($used[$key]))
+     fwrite($fw, "\n");
+  else
+     fwrite($fw, "//perhaps not used\n");
 }
 fwrite($fw,'"'.'##end###'.'" => "'.'###end###'.'");'."\n".'?>'."\n");  
 fclose($fw);
