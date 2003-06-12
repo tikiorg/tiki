c7201a4e15a46d0094b271f96de024f3214f9992
sylvieg <sylvieg@users.sourceforge.net>
2003-06-12 21:09:01 +0000

    A string between {tr} tags can contain Smarty variables The complete string is looked in the language.php ou bd A string between {tr} tags with paramters is translated if possible and returned with the {tr} tags + parameters - block.tr.php will be called to substitute the variables and/or translate A string between {tr} tags without parameter that can not be translated is returned with the {tr} tag -  block.tr.php will be called ... A string between{tr} tags with no paramater that can be translated is translated and the translation is returned without tags
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@690 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/Smarty/plugins/prefilter.tr.php
+++ tiki/Smarty/plugins/prefilter.tr.php
@@ -1,7 +1,9 @@
 <?php
 function smarty_prefilter_tr($source) {
   // Now replace the matched language strings with the entry in the file
-  $return = preg_replace_callback('/\{tr\}([^\{]+)\{\/tr\}/', '_translate_lang', $source);
+//  $return = preg_replace_callback('/\{tr[^\{]*\}([^\{]+)\{\/tr\}/', '_translate_lang', $source);
+// correction in order to match when a variable is inside {tr} tags. Example: {tr}The newsletter was sent to {$sent} email addresses{/tr}, and where there are parameters with {tr} 
+  $return = preg_replace_callback('/(\{tr[^\}]*\})(.+?)\{\/tr\}/', '_translate_lang', $source);
   return $return;
 }
 
@@ -9,16 +11,21 @@ function _translate_lang($key) {
   global $language;
   global $lang_use_db;
   if ($lang_use_db!='y') {
-
-  include("lang/$language/language.php");
-  if(isset($lang[$key[1]])) {
-    return $lang[$key[1]];
-  } else {
-    return $key[1];
+    include("lang/$language/language.php");
+    if(isset($lang[$key[2]]))
+      if ($key[1] == "{tr}") {
+        return $lang[$key[2]];// no more possible translation in block.tr.php
+      }
+      else {
+        return $key[1].$lang[$key[2]]."{/tr}";// perhaps variable substitution to do in block.tr.php
+      }
+    else {
+      return $key[1].$key[2]."{/tr}";// keep the tags to be perhaps translated in block.tr.php
+    }
   }
-  } else {
+   else {
     global $tikilib;
-    $content = $key[1];
+    $content = $key[2];
     $query="select tran from tiki_language where source='".addslashes($content)."' and lang='".$language."'";
     $result=$tikilib->db->query($query);
     $res=$result->fetchRow(DB_FETCHMODE_ASSOC);
@@ -30,9 +37,12 @@ function _translate_lang($key) {
         //No eror checking here
         $tikilib->db->query($query);
         }
-      return $content;
+      return $key[1].$content."{/tr}";
     }
-    return $res["tran"];
+    if ($key[1] == "{tr}")
+        return $res["tran"];// no more possible translation in block.tr.php
+     else
+        return $key[1].$res["tran"]."{/tr}";// perhaps variable substituion to do in block.tr.php
   }
 }
 ?>
