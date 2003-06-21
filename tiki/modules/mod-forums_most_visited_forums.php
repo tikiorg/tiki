3da0a7691e2b16c70b239e5af94d347e54612989
mose <mose@users.sourceforge.net>
2003-06-21 21:03:30 +0000

    fixed call to ranklib
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@740 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/modules/mod-forums_most_visited_forums.php
+++ tiki/modules/mod-forums_most_visited_forums.php
@@ -1,4 +1,5 @@
 <?php
-$ranking = $tikilib->forums_ranking_most_visited_forums($module_rows);
+include_once('lib/rankings/ranklib.php');
+$ranking = $ranklib->forums_ranking_most_visited_forums($module_rows);
 $smarty->assign('modForumsMostVisitedForums',$ranking["data"]);
 ?>
