3800a2a09336e5abdf8fb2d3dd1e2218a8d7aa96
mose <mose@users.sourceforge.net>
2003-06-21 20:46:34 +0000

    fixed change object to ranklib
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@739 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-forums_best_voted_topics.php
+++ modules/mod-forums_best_voted_topics.php
@@ -1,4 +1,5 @@
 <?php
-$ranking = $tikilib->forums_ranking_top_topics($module_rows);
+include_once('lib/rankings/ranklib.php');
+$ranking = $ranklib->forums_ranking_top_topics($module_rows);
 $smarty->assign('modForumsTopTopics',$ranking["data"]);
 ?>
