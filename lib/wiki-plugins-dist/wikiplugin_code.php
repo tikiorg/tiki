6739bb1f8798648ded4a0d143eed34eb80c88d4f
zaufi <zaufi@users.sourceforge.net>
2003-06-30 02:44:35 +0000

    Optional line numbering added, background parameter removed (CSS should be used insted)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@799 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins-dist/wikiplugin_code.php
+++ lib/wiki-plugins-dist/wikiplugin_code.php
@@ -1,21 +1,46 @@
 <?php
-
+// $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins-dist/wikiplugin_code.php,v 1.4 2003-06-30 02:44:35 zaufi Exp $
 // Displays a snippet of code
-// Parameters: bgcolor (optional background color)
+// Parameters: ln => line numbering (default false)
 // Example:
 // {CODE()}
-// print("foo");
+//  print("foo");
 // {CODE}
 
-function wikiplugin_code($data,$params) {
-  global $tikilib;
+
+function wikiplugin_code_help()
+{
+  return "Displays a snippet of code.\nSet optional paramater -+ln+- to 1 if you need line numbering feature.";
+}
+
+function wikiplugin_code($data,$params)
+{
   extract($params);
-  $code=htmlspecialchars(trim($data));
-  if(!isset($bgcolor)) {$bgcolor='#EEEEEE';}
-  //If you want line numbering use something like this:
-  //$lines = explode("\n",$code);
-  //print_r($lines);
-  $data = "<div style='background-color:$bgcolor;'>".$code.'</div>';
+  if(isset($ln)&&$ln==1) {
+    $lines = explode("\n",$data);
+    $i=1;	                                // current line number
+    $code='';
+    // Will skip leading and trailing empty lines
+    // to make snippet look better :)
+    $fl=0;	                                // 'first code line printed' flag
+    $ae='';                                 // 
+    foreach($lines as $line) {
+      $len=strlen($line);
+      if(!($len||$fl)) continue;            // skip leading empty lines
+      if($len) {
+        // OK len >0
+        $code.=$ae.($fl?"\n":'').sprintf("%3d",$i).':  '.$line;
+        $fl=1;                              // first line already printed
+        $ae='';
+      } else $ae.="\n".sprintf("%3d",$i).':  '.$line;
+      $i++;
+    } 
+    $code=rtrim($code);
+  } else $code=$data;
+
+  // Wrap listing into div
+  $data = "<div class='codelisting'><pre>".$code."</pre></div>";
+
   return $data;
 }
 ?>
--- lib/wiki-plugins/wikiplugin_code.php
+++ lib/wiki-plugins/wikiplugin_code.php
@@ -1,34 +1,45 @@
 <?php
-
+// $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_code.php,v 1.5 2003-06-30 02:44:35 zaufi Exp $
 // Displays a snippet of code
-// Parameters: bgcolor (optional background color)
+// Parameters: ln => line numbering (default false)
 // Example:
 // {CODE()}
-// print("foo");
+//  print("foo");
 // {CODE}
 
 
-function wikiplugin_code_help() {
-  return 'Displays a snippet of code';
+function wikiplugin_code_help()
+{
+  return "Displays a snippet of code.\nSet optional paramater -+ln+- to 1 if you need line numbering feature.";
 }
 
-function wikiplugin_code($data,$params) {
-  global $tikilib;
-  global $style;
-
+function wikiplugin_code($data,$params)
+{
   extract($params);
-  $code=htmlspecialchars(trim($data));
-  if(!isset($bgcolor)) {$bgcolor='#EEEEEE';}
-  //If you want line numbering use something like this:
-  //$lines = explode("\n",$code);
-  //print_r($lines);
-  
-  if (strstr($style, "zaufi"))
-    $codestyle="class='codelisting'";
-  else
-    $codestyle="style='border: 1px solid #CCCCCC;margin: 4px;padding-left: 10px;background-color:$bgcolor;'";
+  if(isset($ln)&&$ln==1) {
+    $lines = explode("\n",$data);
+    $i=1;	                                // current line number
+    $code='';
+    // Will skip leading and trailing empty lines
+    // to make snippet look better :)
+    $fl=0;	                                // 'first code line printed' flag
+    $ae='';                                 // 
+    foreach($lines as $line) {
+      $len=strlen($line);
+      if(!($len||$fl)) continue;            // skip leading empty lines
+      if($len) {
+        // OK len >0
+        $code.=$ae.($fl?"\n":'').sprintf("%3d",$i).':  '.$line;
+        $fl=1;                              // first line already printed
+        $ae='';
+      } else $ae.="\n".sprintf("%3d",$i).':  '.$line;
+      $i++;
+    } 
+    $code=rtrim($code);
+  } else $code=$data;
+
   // Wrap listing into div
-  $data = "<div $codestyle><pre>".$code."</pre></div>";
+  $data = "<div class='codelisting'><pre>".$code."</pre></div>";
 
   return $data;
 }
