1610352694f4045e42b4a8d52dd92f62f2abf64f
zaufi <zaufi@users.sourceforge.net>
2003-07-04 20:00:40 +0000

    Add 'maxlen' module parameter. Just set 'maxlen=20' in paramaters field in admin modules panel and assign the module :) Maybe add same to other 'last_changed_xxx' modules?
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@842 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-last_modif_pages.php
+++ modules/mod-last_modif_pages.php
@@ -1,4 +1,5 @@
 <?php
 $ranking = $tikilib->list_pages(0, $module_rows, 'lastModif_desc');
 $smarty->assign('modLastModif',$ranking["data"]);
+$smarty->assign('maxlen',isset($module_params["maxlen"])?$module_params["maxlen"]:0);
 ?>
