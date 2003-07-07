b562f5d3502a6d6b6379cd30bcb8ef121d8a322b
awcolley <awcolley@users.sourceforge.net>
2003-07-07 20:13:30 +0000

    minor cleanup
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@897 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-remind_password.php
+++ tiki-remind_password.php
@@ -8,8 +8,8 @@ if ($forgotPass != 'y') {
   die;
 }
 
-
-$smarty->assign('showmsg','n');
+$smarty->assign( 'showmsg', 'n' );
+$smarty->assign( 'showfrm', 'y' );
 if (isset( $_REQUEST["remind"] )) {
   if ($tikilib->user_exists( $_REQUEST["username"] )) {
     if ($feature_clear_passwords == 'y') {
@@ -20,12 +20,13 @@ if (isset( $_REQUEST["remind"] )) {
     $email = $tikilib->get_user_email( $_REQUEST["username"] );
     $smarty->assign( 'mail_site', $_SERVER["SERVER_NAME"] );
     $smarty->assign( 'mail_user', $_REQUEST["username"] );
-    $smarty->assign( 'mail_same', $feature_clear_passwords );  // AWC added
+    $smarty->assign( 'mail_same', $feature_clear_passwords );
     $smarty->assign( 'mail_pass', $pass );
     $mail_data = $smarty->fetch( 'mail/password_reminder.tpl' );
     $tmp = tra( "Your Tiki account information for" );
     $tmp .= " ".$_SERVER["SERVER_NAME"];
     @mail( $email, $tmp, $mail_data );
+    // Just show "success" message and no form
     $smarty->assign( 'showmsg', 'y' );
     $smarty->assign( 'showfrm', 'n' );
     if ($feature_clear_passwords == 'y') {
@@ -37,15 +38,12 @@ if (isset( $_REQUEST["remind"] )) {
     $tmp .= " ".$_REQUEST["username"].".";
     $smarty->assign( 'msg', $tmp );
   } else {
+    // Show error message (and leave form visible so user can fix problem)
     $smarty->assign( 'showmsg', 'e' );
-    $smarty->assign( 'showfrm', 'y' );
     $tmp = tra( "Invalid or unknown username" );
     $tmp .= ": ".$_REQUEST["username"];
     $smarty->assign( 'msg', $tmp );
   }
-} else {
-  $smarty->assign( 'showmsg', 'n' );
-  $smarty->assign( 'showfrm', 'y' );
 }
 
 // Display the template
