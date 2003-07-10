03ac0ea271eff81607cf740258015fe80382255a
sylvieg <sylvieg@users.sourceforge.net>
2003-07-10 06:33:04 +0000

    oops
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@924 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/rss/rsslib.php
+++ lib/rss/rsslib.php
@@ -126,6 +126,7 @@ class RSSLib extends TikiLib {
   
   function refresh_rss_module($rssId)
   {
+	$info=$this->get_rss_module($rssId);
     @$fp = fopen($info["url"],"r");
     if(!$fp) return false;
     $data = '';
