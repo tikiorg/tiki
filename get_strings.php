baf52654ce604d60710c7344e8ecf5c9bfdfcad0
sylvieg <sylvieg@users.sourceforge.net>
2003-07-15 17:03:00 +0000

    multi-line strings + \ n in strings + set of languages + don't look for {tr} in .php files know bug : when get_strings is calles 2 times, the string with '\ n' (and not thoses with line feed) are duplicated
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1003 b456876b-0849-0410-b77d-98878d47e9d5

--- get_strings.php
+++ get_strings.php
@@ -8,7 +8,7 @@ if($tiki_p_admin != 'y') {
 
 // Cambiar lo que busca segun sea .php o .tpl
 
-$languages = Array('tw','dk','ru','de','en','sp','fr','dk','ru','sw','it','pl');
+$languages = Array('cn','de','dk','en','fr','he','it','nl','no','pl','ru','sp','sw','tw');
 //$languages = Array('it','pl');
 
 $files = Array();
@@ -68,22 +68,24 @@ foreach($files as $file) {
   $fp = fopen($file,"r");
   $data = fread($fp,filesize($file));
   fclose($fp);
-  preg_match_all("/\{tr\}(.+?)\{\/tr\}/",$data,$words);
-  foreach(array_unique($words[1]) as $word) {
-    if (ereg("^\{[$][^\}]*\}$", $word))
-       continue;
-    if (!isset($used[$word]))
-       $used[$word] = 1;
-    if(isset($lang[$word])) {
-      print('"'.$word.'" => "'.$lang[$word].'",'."\n<br/>");
-      //fwrite($fw,'"'.$word.'" => "'.$lang[$word].'",'."\n");
-    } else {
-      print('+++"'.$word.'" => "'.$word.'",'."\n<br/>");  
-      $lang[$word]=$word;
-      //fwrite($fw,'"'.$word.'" => "'.$word.'",'."\n");
-    }
+  if(substr($file,strlen($file)-3,3)=="tpl") {
+     preg_match_all("/(?s)\{tr\}(.+?)\{\/tr\}/",$data,$words);
+     foreach(array_unique($words[1]) as $word) {
+       if (ereg("^\{[$][^\}]*\}$", $word))
+          continue;
+       if (!isset($used[$word]))
+          $used[$word] = 1;
+       if(isset($lang[$word])) {
+          print('"'.$word.'" => "'.$lang[$word].'",'."\n<br/>");
+          //fwrite($fw,'"'.$word.'" => "'.$lang[$word].'",'."\n");
+       } else {
+          print('+++"'.$word.'" => "'.$word.'",'."\n<br/>");  
+          $lang[$word]=$word;
+          //fwrite($fw,'"'.$word.'" => "'.$word.'",'."\n");
+       }
+     }
   }
-  preg_match_all("/tra[ \t]*\( *\"([^\"]+)\"[ \t]*\)/",$data,$words);
+  preg_match_all("/(?s)tra[ \t]*\( *\"([^\"]+)\"[ \t]*\)/",$data,$words);
   foreach(array_unique($words[1]) as $word) {
     if (!isset($used[$word]))
        $used[$word] = 1;
@@ -96,7 +98,7 @@ foreach($files as $file) {
       //fwrite($fw,'"'.$word.'" => "'.$word.'",'."\n");
     }
   }
-  preg_match_all("/tra[ \t]*\( *\'([^\']+)\'[ \t]*\)/",$data,$words);
+  preg_match_all("/(?s)tra[ \t]*\( *\'([^\']+)\'[ \t]*\)/",$data,$words);
   foreach(array_unique($words[1]) as $word) {
 	if (!isset($used[$word]))
       $used[$word] = 1;
@@ -112,8 +114,8 @@ foreach($files as $file) {
 }
 print('"'.'##end###'.'" => "'.'###end###'.'"'.");?&gt;\n<br/>");  
 foreach($lang as $key=>$val) {
-  // backslash $ and \
-  fwrite($fw,'"'.str_replace("\$", "\\\$",str_replace("\\", "\\\\", $key)).'" => "'.str_replace("\$", "\\\$", str_replace("\\", "\\\\", $val)).'",');
+  // backslash $ and \n
+  fwrite($fw,'"'.str_replace("\$", "\\\$", str_replace("\n", "\\n", $key)).'" => "'.str_replace("\$", "\\\$", str_replace("\n", "\\n", $val)).'",');
   if (isset($used[$key]))
      fwrite($fw, "\n");
   else
