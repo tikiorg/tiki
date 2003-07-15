5768a94d23737dfa5463f2a0b4c8bd73ccfbd1ca
rossta <rossta@users.sourceforge.net>
2003-07-15 20:22:43 +0000

    Add ADOdb as the database option
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1008 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/db/tiki-db.php
+++ tiki/db/tiki-db.php
@@ -7,10 +7,12 @@ if(strstr($current_path, ';')) {
 	$separator=':'; 
 }
 if($separator=='') $separator = ':'; // guess
-ini_set('include_path', $current_path.$separator.'lib/pear');
+ini_set('include_path', dirname(dirname(__FILE__)).'/lib/pear'.$separator.$current_path);
 
-// Database connection for the tiki system
-require_once('DB.php');
+$api_tiki	 = 'pear';
+#$api_tiki	 = 'adodb';
+$db_tiki	 = 'mysql';
+#$db_tiki	 = 'pgsql';
 $host_tiki   = 'localhost';
 $user_tiki   = 'root';
 $pass_tiki   = '';
@@ -29,6 +31,8 @@ For example:
 cd <tiki_dir>/db
 cat >local.php <<EOF
 <?php
+\$api_tiki	  = 'pear';
+\$db_tiki	  = 'mysql';
 \$host_tiki   = 'myhost';
 \$user_tiki   = 'myuser';
 \$pass_tiki   = 'mypass';
@@ -39,30 +43,48 @@ EOF
 Site admins: you can use multiple virtualhosts with the same documentroot
 just changing database and $tikidomain that is used to split cache directories
 (template_c and modules/cache). For example a local.php file :
+
 $host_tiki   = 'localhost';
 $user_tiki   = 'root';
 $pass_tiki   = '';
-if ($HTTP_HOST == "feu.org") {
-	$dbs_tiki    = 'tiki_cvs';
-	$tikidomain  = 'feu';
-} elseif ($HTTP_HOST == "localis.org") {
-	$dbs_tiki    = 'tiki_localis';
-	$tikidomain  = 'localis';
 
-} elseif ( ... ) {
+$host_map = array(
+	'feu.org' = array(
+		'dbs_tiki'    => 'tiki_cvs',
+		'tikidomain'  => 'feu',
+	),
+	
+	'localis.org' = array(
+		'dbs_tiki'    => 'tiki_localis',
+		'tikidomain'  => 'localis',
+	),
+);
 
-...
-
-} else {
-	// default case, your choice is to block or open to a default domain
+if (isset($host_map[$HTTP_HOST])) {
+	extract($host_map[$HTTP_HOST]);
 }
+
 */
 
 $file = dirname(__FILE__) . '/local.php';
 if (file_exists($file))
 	require_once($file);
 
-$dsn = "mysql://$user_tiki:$pass_tiki@$host_tiki/$dbs_tiki";    
+if (preg_match('/^adodb$/i', $api_tiki)) {
+	ini_set('include_path', $current_path.$separator.'lib/adodb');
+#	error_reporting(E_ALL); # show any error messages triggered
+	include_once('adodb-errorpear.inc.php');
+#	include_once('adodb-errorhandler.inc.php');
+	include_once('adodb-pear.inc.php');
+	if ($db_tiki == 'pgsql') {
+		$db_tiki = 'postgres7';
+	}
+} else {
+	// Database connection for the tiki system
+	include_once('DB.php');
+}
+
+$dsn = "$db_tiki://$user_tiki:$pass_tiki@$host_tiki/$dbs_tiki";    
 //$dsn = "mysql://$user_tiki@$pass_tiki(localhost)/$dbs_tiki";
 $dbTiki = DB::connect($dsn);
 if (DB::isError($dbTiki)) {        
@@ -80,6 +102,9 @@ $user_tiki   = NULL;
 $pass_tiki   = NULL;
 $dbs_tiki    = NULL;
 
+unset($host_map);
+unset($api_tiki);
+unset($db_tiki);
 unset($host_tiki);
 unset($user_tiki);
 unset($pass_tiki);
