811878244f221415e0383131f53f45ebf70533b9
rossta <rossta@users.sourceforge.net>
2003-07-16 18:32:22 +0000

    setup.sh cleanup
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1019 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/setup.sh
+++ tiki/setup.sh
@@ -14,14 +14,17 @@ VIRTUALS=""
 
 UNAME=`uname | cut -c 1-6`
 
-if [ "$UNAME" = "CYGWIN" ];
-then
+if [ -f /etc/debian_version ]; then
+	AUSER=www-data
+	AGROUP=www-data
+fi
+
+if [ "$UNAME" = "CYGWIN" ]; then
 	AUSER=SYSTEM
 	AGROUP=SYSTEM
 fi
 
-if [ -z "$1" ];
-then
+if [ -z "$1" ]; then
 	cat <<EOF
 This script assigns necessary permissions for the directories that the
 webserver writes files to. It also creates the (initially empty) cache 
@@ -29,7 +32,7 @@ directories.
 
 Usage $0 user [group] [rights] [list of virtual host domains]
 
-For example, if apache is running as user $AUSER, type:
+For example, if apache is running as user $AUSER and group $AGROUP, type:
 
   su -c '$0 $AUSER'
  
@@ -50,37 +53,51 @@ If you can't become root, and are not a member of the apache group, then type:
 
 Replace yourgroup with your default group.
 
-NOTE: If you do this, you will not be able to delete certain files created by
-apache, and will need to ask your system administrator to delete them for you
-if needed.
+NOTE: If you do execute this last command, you will not be able to delete 
+certain files created by apache, and will need to ask your system
+administrator to delete them for you if needed.
 
-To use Tiki's multi-site capability (virtual hosts from a single doc root) add a list of domains
-to the command to create all the needed directories, for example:
+To use Tiki's multi-site capability (virtual hosts from a single DocumentRoot)
+add a list of domains to the command to create all the needed directories.
+For example:
 
-  su -c '$0 $USER $GROUP 02777 domain1 domain2 domain3'
+  su -c '$0 $USER $AGROUP $RIGHTS domain1 domain2 domain3'
+
+or, if you can't become root:
+
+  $0 $USER $AGROUP 02777 domain1 domain2 domain3
 
 EOF
-exit 1
+	exit 1
 fi
 
-AUSER=$1;shift
-AGROUP=$1;shift
-RIGHTS=$1;shift
-VIRTUALS=$@
+if [ -n "$1" ]; then
+	AUSER=$1
+	shift
+fi
+if [ -n "$1" ]; then
+	AGROUP=$1
+	shift
+fi
+if [ -n "$1" ]; then
+	RIGHTS=$1
+fi
+
+if [ -n "$1" ]; then
+	VIRTUALS=$@
+fi
 
 # Create directories as needed
 for dir in $DIRS
 do
-	if [ ! -d $dir ]
-	then
+	if [ ! -d $dir ]; then
+		echo Creating directory "$dir"
 		mkdir -p $dir
 	fi
         for vdir in $VIRTUALS; do
                 if [ ! -d "$dir/$vdir" ]; then
+			echo Creating directory "$dir/$vdir"
                         mkdir -p "$dir/$vdir"
-                        echo "$dir/$vdir missing ... created."
-                else
-                        echo "$dir/$vdir ok"
                 fi
         done
 done
@@ -88,14 +105,8 @@ done
 # Set ownerships of the directories
 chown -R $AUSER $DIRS
 
-if [ -n "$AGROUP" ];
-then
+if [ -n "$AGROUP" ]; then
 	chgrp -R $AGROUP $DIRS
-fi
-
-if [ -z "$RIGHTS" ];
-then
-	RIGHTS=02775
 fi
 
 chmod -R $RIGHTS $DIRS
