7298d004ba2ef550a60dbb93972ac25b115c8841
redflo <redflo@users.sourceforge.net>
2003-07-22 23:24:43 +0000

    One reason why we branched :-) new functionality: admin can choose if he wants to configure a http proxy (avoids hanging tiki until tcp timeout). For now only works for rss feeds.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1023 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/db/tiki_1.7to1.8.sql
+++ tiki/db/tiki_1.7to1.8.sql
@@ -1,3 +1,6 @@
 # here will come sql modifs until 1.8 is released
 #
+INSERT INTO tiki_preferences(name,value) VALUES ('use_proxy','n');
+INSERT INTO tiki_preferences(name,value) VALUES ('proxy_host','');
+INSERT INTO tiki_preferences(name,value) VALUES ('proxy_port','');
 
