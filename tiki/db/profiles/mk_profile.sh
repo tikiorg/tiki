4830b2afde62f54ceb44e305728f81eb8e2c9ecd
dheltzel <dheltzel@users.sourceforge.net>
2003-08-05 15:13:55 +0000

    added more comments.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1066 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/db/profiles/mk_profile.sh
+++ tiki/db/profiles/mk_profile.sh
@@ -1,6 +1,10 @@
 # mk_profile.sh - Generates a profile of all tiki preferences that differ from the default values
 #
 # Written by Dennis Heltzel - 8/3/03
+# This script can generate SQL to make a default tiki install have the same preferences as an existing database
+# It uses dump_prefs.sql to generate a list of all prefs in the specified database, then compares it to the default values
+# in default-inserts.sql and filters the differences into a profile.sql file.
+# Depending on how your database is setup, you may need to alter the mysql command parameters.
 
 if [ -z "$1" ]
 then
@@ -13,5 +17,5 @@ DATABASE=$1
 DUMP_FILE=$DATABASE-dump.sql
 PROFILE_FILE=$DATABASE-profile.sql
 
-mysql -s -u root -p $DATABASE <dump_prefs.sql |sort >$DUMP_FILE
+mysql -s -p $DATABASE <dump_prefs.sql |sort >$DUMP_FILE
 diff default-inserts.sql $DUMP_FILE|grep ">"|sed -e "s/> INSERT IGNORE/REPLACE/" >$PROFILE_FILE
