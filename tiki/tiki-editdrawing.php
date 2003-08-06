d8d00fe2b3b82ea5e1c06645e5656c6a7667b466
rossta <rossta@users.sourceforge.net>
2003-08-06 19:15:26 +0000

    Trita fix
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1082 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-editdrawing.php
+++ tiki/tiki-editdrawing.php
@@ -1,34 +1,73 @@
-<?php # $Header: /cvsroot/tikiwiki/tiki/tiki-editdrawing.php,v 1.7 2003-08-01 10:30:45 redflo Exp $
+<?php # $Header: /cvsroot/tikiwiki/tiki/tiki-editdrawing.php,v 1.8 2003-08-06 19:15:26 rossta Exp $
 
-include_once("tiki-setup_base.php");
+include_once ("tiki-setup_base.php");
 
-if(($tiki_p_admin_drawings != 'y') && ($tiki_p_edit_drawings != 'y')) {
-  die;  
+if (($tiki_p_admin_drawings != 'y') && ($tiki_p_edit_drawings != 'y')) {
+	die;
 }
 
-if(isset($_REQUEST["close"])) {
-  print("<script language='Javascript' type='text/javascript'>window.opener.location.reload();</script>");
-  print("<script language='Javascript' type='text/javascript'>window.close();</script>");
-  die;
+if (isset($_REQUEST["close"])) {
+	print ("<script language='Javascript' type='text/javascript'>window.opener.location.reload();</script>");
+
+	print ("<script language='Javascript' type='text/javascript'>window.close();</script>");
+	die;
 }
+
 $tikilib->invalidate_cache($_REQUEST['page']);
-$name=$_REQUEST["drawing"];
-$path=$_REQUEST["path"];
+$name = $_REQUEST["drawing"];
+$path = $_REQUEST["path"];
+
 ?>
-<applet archive="lib/jgraphpad/jgraphpad.jar"
-code="com.jgraph.JGraphpad.class" width=100% height=40>
-	<param name="drawpath" value="<?php echo $path?>/img/wiki/<?php echo $tikidomain.$name?>.pad_xml">
-	<param name="gifpath"  value="<?php echo $path?>/img/wiki/<?php echo $tikidomain.$name?>.gif">
-	<param name="savepath" value="<?php echo $path?>/jhot.php">
-	<param name="viewpath" value="tiki-editdrawing.php?close=1">
-</applet>	
+<html>
+<head>
+</head>
+<body>
+<applet archive = "lib/jgraphpad/jgraphpad.jar" code = "com.jgraph.JGraphpad.class" width = 100% height = 40>
+	<param name = "drawpath" value = "<?php echo $path?>/img/wiki/<?php echo $tikidomain.$name?>.pad_xml">
+
+	<param name = "gifpath" value = "<?php echo $path?>/img/wiki/<?php echo $tikidomain.$name?>.gif">
+
+	<param name = "savepath" value = "<?php echo $path?>/jhot.php">
+
+	<param name = "viewpath" value = "tiki-editdrawing.php?close=1">
+</applet>
+</body>
+</html>
+<?php
+/*
+
 <!--
 <applet code="CH.ifa.draw.twiki.TWikiDraw.class" archive="lib/jHotDraw/twikidraw.jar" width=100% height=40>
-	<param name="drawpath" value="<?php echo $path?>/img/wiki/<?php echo $tikidomain.$name?>.draw">
-	<param name="gifpath"  value="<?php echo $path?>/img/wiki/<?php echo $tikidomain.$name?>.gif">
-        <param name="extracolors" value="Aquamarine=#70DB93,New Tan=EBC79E,Sea Green=238E68,Motorola Blue=#3ff">
-	<param name="savepath" value="<?php echo $path?>/jhot.php">
-	<param name="viewpath" value="tiki-editdrawing.php?close=1">
-	<param name="helppath" value=".">
+	<param name="drawpath" value="
+
+< ?php
+
+echo $path
+
+? >
+
+/img/wiki/
+
+< ?php
+
+echo $tikidomain . $name
+ 
+? >
+
+.draw">
+
+<param name = "gifpath" value = "<?php echo $path?>/img/wiki/<?php echo $tikidomain.$name?>.gif">
+
+<param name = "extracolors" value = "Aquamarine=#70DB93,New Tan=EBC79E,Sea Green=238E68,Motorola Blue=#3ff">
+
+<param name = "savepath" value = "<?php echo $path?>/jhot.php">
+
+<param name = "viewpath" value = "tiki-editdrawing.php?close=1">
+
+<param name = "helppath" value = ".">
+
 </applet>
+
 -->
+*/
+?>
