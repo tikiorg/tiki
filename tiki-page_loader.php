904b1c128193652e6d2cc31b378f88ccc7d09f91
rossta <rossta@users.sourceforge.net>
2003-08-06 20:53:06 +0000

    Trita fix
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1083 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-page_loader.php
+++ tiki-page_loader.php
@@ -1,25 +1,36 @@
 <?php
-include_once('tiki-setup.php');
-include_once('lib/htmlpages/htmlpageslib.php');  
 
-$refresh=1000*$_REQUEST["refresh"];
+include_once ('tiki-setup.php');
+
+include_once ('lib/htmlpages/htmlpageslib.php');
+
+$refresh = 1000 * $_REQUEST["refresh"];
+
 ?>
+
 <html>
-<head>
-<script language='Javascript' type='text/javascript'>
-<?php
-$zones = $htmlpageslib->list_html_page_content($_REQUEST["pageName"],0,-1,'zone_asc','');
-$cmds=Array();
-for($i=0;$i<count($zones["data"]);$i++) {
-  $cmd='top.document.getElementById("'.$zones["data"][$i]["zone"].'").innerHTML="'.$zones["data"][$i]["content"].'";';
-  print($cmd);
-}
-?>
-</script>
-</head>
-<?php
-print('<body onLoad="window.setInterval(\'location.reload()\','.$refresh.');">');
-//print_r($cmds);
-?>
-</body>
+	<head>
+		<script language = 'Javascript' type = 'text/javascript'>
+
+		<?php
+
+		$zones = $htmlpageslib->list_html_page_content($_REQUEST["pageName"], 0, -1, 'zone_asc', '');
+		$cmds = array();
+
+		for ($i = 0; $i < count($zones["data"]); $i++) {
+			$cmd = 'top.document.getElementById("' . $zones["data"][$i]["zone"] . '").innerHTML="' . $zones["data"][$i]["content"] . '";';
+
+			echo $cmd;
+		}
+
+		?>
+
+		</script>
+	</head>
+
+	<body onLoad="window.setInterval('location.reload()','<?php echo $refresh ?>');">
+	<?php
+		//print_r($cmds);
+	?>
+	</body>
 </html>
