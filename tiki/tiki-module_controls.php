2ad2195346d64291b8e81eda3508f33ddb42cb37
zaufi <zaufi@users.sourceforge.net>
2003-08-08 19:40:36 +0000

    Fix problem if user still not configure modules and press control icon
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1132 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-module_controls.php
+++ tiki/tiki-module_controls.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-module_controls.php,v 1.4 2003-08-07 20:56:09 zaufi Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-module_controls.php,v 1.5 2003-08-08 19:40:36 zaufi Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -10,34 +10,41 @@
 require_once ('tiki-setup.php');
 
 include_once ('lib/usermodules/usermoduleslib.php');
+//include_once ('lib/debug/debugger.php');
 
 global $smarty;
 
 $check_req = (isset($_REQUEST["unassign"])
-	|| isset($_REQUEST["up"]) || isset($_REQUEST["down"]) || isset($_REQUEST["left"]) || isset($_REQUEST["right"]));
+           || isset($_REQUEST["up"])
+           || isset($_REQUEST["down"])
+           || isset($_REQUEST["left"])
+           || isset($_REQUEST["right"]));
 
 if ($tiki_p_configure_modules != 'y' && $check_req) {
 	$smarty->assign('msg', tra("You dont have permission to use this feature"));
-
 	$smarty->display("styles/$style_base/error.tpl");
 	die;
 }
 
 if ($user_assigned_modules != 'y' && $check_req) {
 	$smarty->assign('msg', tra("This feature is disabled"));
-
 	$smarty->display("styles/$style_base/error.tpl");
 	die;
 }
 
 if (!$user && $check_req) {
 	$smarty->assign('msg', tra("You must log in to use this feature"));
-
 	$smarty->display("styles/$style_base/error.tpl");
 	die;
 }
 
 if ($check_req) {
+//    global $debugger;
+//    $debugger->msg('Module control clicked: '.$check_req);
+    // Make defaults if user still ot configure modules for himself
+    if (!$usermoduleslib->user_has_assigned_modules($user))
+        $usermoduleslib->create_user_assigned_modules($user);
+    // Handle control icon click
 	if (isset($_REQUEST["up"]))
 		$usermoduleslib->swap_up_user_module($_REQUEST["up"], $user);
 	elseif (isset($_REQUEST["down"]))
