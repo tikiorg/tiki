d0f04cfa3d970879a7b762e34775c3a316be5d03
redflo <redflo@users.sourceforge.net>
2003-08-15 22:42:36 +0000

    conflicts: reverted from head version. no change should be in there ...
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1272 b456876b-0849-0410-b77d-98878d47e9d5

--- get_strings.php
+++ get_strings.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/get_strings.php,v 1.24 2003-08-13 16:41:59 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/get_strings.php,v 1.25 2003-08-15 22:42:36 redflo Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
