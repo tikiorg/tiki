75829b6de822d633d4d52720f6872210e033c2d3
teedog <teedog@users.sourceforge.net>
2003-08-17 19:29:39 +0000

    fixed bug with ext link caching when URL contains parameters
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1290 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-view_cache.php
+++ tiki/tiki-view_cache.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-view_cache.php,v 1.6 2003-08-07 04:33:57 rossta Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-view_cache.php,v 1.7 2003-08-17 19:29:39 teedog Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -38,7 +38,7 @@ if (!isset($_REQUEST["cacheId"])) {
 
 // Get a list of last changes to the Wiki database
 $info = $tikilib->get_cache($_REQUEST["cacheId"]);
-$ggcacheurl = 'http://google.com/search?q=cache:' . strstr($info['url'], 'http://');
+$ggcacheurl = 'http://google.com/search?q=cache:'.urlencode(strstr($info['url'],'http://'));
 
 // test if url ends with .txt : formatting for text
 if (substr($info["url"], -4, 4) == ".txt") {
