2f3d1c3fb1cb1fcaaea2bcf6baa3a6ae96b8c633
jwigdahl <jwigdahl@users.sourceforge.net>
2003-08-20 20:44:56 +0000

    Added comments to auth code. Replaced each tab with 4 spaces.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1312 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup_base.php
+++ tiki-setup_base.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-setup_base.php,v 1.26 2003-08-17 22:22:27 teedog Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-setup_base.php,v 1.27 2003-08-20 20:44:56 jwigdahl Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -20,95 +20,101 @@ $userlib = new UsersLib($dbTiki);
 // set session lifetime
 $session_lifetime = $tikilib->get_preference('session_lifetime','0');
 if ($session_lifetime > 0) {
-	ini_set('session.gc_maxlifetime',$session_lifetime*60);
+    ini_set('session.gc_maxlifetime',$session_lifetime*60);
 }
 
 // is session data  stored in DB or in filesystem?
 $session_db = $tikilib->get_preference('session_db','n');
 if ($session_db == 'y') {
-	include('db/local.php');
-	$ADODB_SESSION_DRIVER=$db_tiki;
-	$ADODB_SESSION_CONNECT=$host_tiki;
-	$ADODB_SESSION_USER=$user_tiki;
-	$ADODB_SESSION_PWD=$pass_tiki;
-	$ADODB_SESSION_DB=$dbs_tiki;
-	unset($db_tiki);
-	unset($host_tiki);
-	unset($user_tiki);
-	unset($pass_tiki);
-	unset($dbs_tiki);
-	ini_set('session.save_handler','user');
-	include('adodb-session.php');
+    include('db/local.php');
+    $ADODB_SESSION_DRIVER=$db_tiki;
+    $ADODB_SESSION_CONNECT=$host_tiki;
+    $ADODB_SESSION_USER=$user_tiki;
+    $ADODB_SESSION_PWD=$pass_tiki;
+    $ADODB_SESSION_DB=$dbs_tiki;
+    unset($db_tiki);
+    unset($host_tiki);
+    unset($user_tiki);
+    unset($pass_tiki);
+    unset($dbs_tiki);
+    ini_set('session.save_handler','user');
+    include('adodb-session.php');
 }
 session_start();
 
+// check if the remember me feature is enabled
 $rememberme = $tikilib->get_preference('rememberme', 'disabled');
 
+// if remember me is enabled, check for cookie where auth hash is stored
+// user gets logged in as the first user in the db with a matching hash
 if ($rememberme != 'disabled') {
-	if (isset($_COOKIE['tiki-user'])) {
-		if (!isset($user)and !isset($_SESSION['user'])) {
-			$user = $userlib->get_user_by_hash($_COOKIE['tiki-user']);
+    if (isset($_COOKIE['tiki-user'])) {
+        if (!isset($user)and !isset($_SESSION['user'])) {
+            $user = $userlib->get_user_by_hash($_COOKIE['tiki-user']);
 
-			$_SESSION['user'] = $user;
-		}
-	}
+            $_SESSION['user'] = $user;
+        }
+    }
 }
 
+// check what auth metod is selected. default is for the 'tiki' to auth users
 $auth_method = $tikilib->get_preference('auth_method', 'tiki');
 
+// if the auth method is 'web site', look for the username in $_SERVER
 if ($auth_method == 'ws') {
-	if (isset($_SERVER['REMOTE_USER'])) {
-		if ($userlib->user_exists($_SERVER['REMOTE_USER'])) {
-			$_SESSION["user"] = $_SERVER['REMOTE_USER'];
-		}
-	}
+    if (isset($_SERVER['REMOTE_USER'])) {
+        if ($userlib->user_exists($_SERVER['REMOTE_USER'])) {
+            $_SESSION["user"] = $_SERVER['REMOTE_USER'];
+        }
+    }
 }
 
+// if the username is already saved in the session, pull it from there
 if (isset($_SESSION["user"])) {
-	$user = $_SESSION["user"];
+    $user = $_SESSION["user"];
 } else {
-	$user = NULL;
+    $user = NULL;
 }
 
 function tra($content) {
-	global $lang_use_db;
+    global $lang_use_db;
 
-	if ($lang_use_db != 'y') {
-		global $lang;
+    if ($lang_use_db != 'y') {
+        global $lang;
 
-		if ($content) {
-			if (isset($lang[$content])) {
-				return $lang[$content];
-			} else {
-				return $content;
-			}
-		}
-	} else {
-		global $tikilib;
+        if ($content) {
+            if (isset($lang[$content])) {
+                return $lang[$content];
+            } else {
+                return $content;
+            }
+        }
+    } else {
+        global $tikilib;
 
-		global $language;
-		$query = "select tran from tiki_language where source='" . addslashes($content). "' and lang='" . $language . "'";
-		$result = $tikilib->db->query($query);
-		$res = $result->fetchRow(DB_FETCHMODE_ASSOC);
+        global $language;
+        $query = "select tran from tiki_language where source='" . addslashes($content). "' and lang='" . $language . "'";
+        $result = $tikilib->db->query($query);
+        $res = $result->fetchRow(DB_FETCHMODE_ASSOC);
 
-		if (!$res)
-			return $content;
+        if (!$res)
+            return $content;
 
-		if (!isset($res["tran"])) {
-			global $record_untranslated;
+        if (!isset($res["tran"])) {
+            global $record_untranslated;
 
-			if ($record_untranslated == 'y') {
-				$query = "insert into tiki_untranslated (source,lang) values('" . addslashes($content). "','" . $language . "')";
+            if ($record_untranslated == 'y') {
+                $query = "insert into tiki_untranslated (source,lang) values('" . addslashes($content). "','" . $language . "')";
 
-				//No eror checking here
-				$tikilib->db->query($query);
-			}
+                //No eror checking here
+                $tikilib->db->query($query);
+            }
 
-			return $content;
-		}
+            return $content;
+        }
 
-		return $res["tran"];
-	}
+        return $res["tran"];
+    }
 }
 
 // function user_has_permission($user,$perm) 
@@ -116,17 +122,17 @@ $allperms = $userlib->get_permissions(0, -1, 'permName_desc', '', '');
 $allperms = $allperms["data"];
 
 foreach ($allperms as $vperm) {
-	$perm = $vperm["permName"];
+    $perm = $vperm["permName"];
 
-	if ($user != 'admin' && (!$user || !$userlib->user_has_permission($user, 'tiki_p_admin'))) {
-		$$perm = 'n';
+    if ($user != 'admin' && (!$user || !$userlib->user_has_permission($user, 'tiki_p_admin'))) {
+        $$perm = 'n';
 
-		$smarty->assign("$perm", 'n');
-	} else {
-		$$perm = 'y';
+        $smarty->assign("$perm", 'n');
+    } else {
+        $$perm = 'y';
 
-		$smarty->assign("$perm", 'y');
-	}
+        $smarty->assign("$perm", 'y');
+    }
 }
 
 // Permissions
@@ -134,163 +140,163 @@ foreach ($allperms as $vperm) {
 $perms = $userlib->get_user_permissions($user);
 
 foreach ($perms as $perm) {
-	//print("Asignando permiso global : $perm<br/>");
-	$smarty->assign("$perm", 'y');
+    //print("Asignando permiso global : $perm<br/>");
+    $smarty->assign("$perm", 'y');
 
-	$$perm = 'y';
+    $$perm = 'y';
 }
 
 // If the user can admin file galleries then assign all the file galleries permissions
 if ($tiki_p_admin_file_galleries == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'file galleries');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'file galleries');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_admin_workflow == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'workflow');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'workflow');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_admin_directory == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'directory');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'directory');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_admin_charts == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'charts');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'charts');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_blog_admin == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'blogs');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'blogs');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_admin_trackers == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'trackers');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'trackers');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_admin_galleries == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'image galleries');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'image galleries');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_admin_forum == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'forums');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'forums');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_admin_wiki == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'wiki');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'wiki');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_admin_faqs == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'faqs');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'faqs');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_admin_shoutbox == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'shoutbox');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'shoutbox');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_admin_quizzes == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'quizzes');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'quizzes');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 if ($tiki_p_admin_cms == 'y') {
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'cms');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'cms');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 
-	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'topics');
+    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'topics');
 
-	foreach ($perms["data"] as $perm) {
-		$perm = $perm["permName"];
+    foreach ($perms["data"] as $perm) {
+        $perm = $perm["permName"];
 
-		$smarty->assign("$perm", 'y');
-		$$perm = 'y';
-	}
+        $smarty->assign("$perm", 'y');
+        $$perm = 'y';
+    }
 }
 
 $tikiIndex = $tikilib->get_preference("tikiIndex", 'tiki-index.php');
@@ -309,29 +315,29 @@ $language = $tikilib->get_preference('language', 'en');
 
 /* This seems to be done in tiki-setup.php
 if ($feature_userPreferences == 'y') {
-	// Check for FEATURES for the user
-	$user_style = $tikilib->get_preference("style", 'moreneat.css');
+    // Check for FEATURES for the user
+    $user_style = $tikilib->get_preference("style", 'moreneat.css');
 
-	if ($user) {
-		if ($change_theme == 'y') {
-			$user_style = $tikilib->get_user_preference($user, 'theme', $style);
+    if ($user) {
+        if ($change_theme == 'y') {
+            $user_style = $tikilib->get_user_preference($user, 'theme', $style);
 
-			if ($user_style) {
-				$style = $user_style;
-			}
-		}
+            if ($user_style) {
+                $style = $user_style;
+            }
+        }
 
-		if ($change_language == 'y') {
-			$user_language = $tikilib->get_user_preference($user, 'language', $language);
+        if ($change_language == 'y') {
+            $user_language = $tikilib->get_user_preference($user, 'language', $language);
 
-			if ($user_language) {
-				$language = $user_language;
-			}
-		}
-	}
+            if ($user_language) {
+                $language = $user_language;
+            }
+        }
+    }
 
-	$smarty->assign('style', $style);
-	$smarty->assign('language', $language);
+    $smarty->assign('style', $style);
+    $smarty->assign('language', $language);
 }
 */
 
@@ -340,10 +346,10 @@ $style_base = $stlstl[0];
 
 // Fix IIS servers not setting what they should set (ay ay IIS, ay ay)
 if (!isset($_SERVER['QUERY_STRING']))
-	$_SERVER['QUERY_STRING'] = '';
+    $_SERVER['QUERY_STRING'] = '';
 
 if (!isset($_SERVER['REQUEST_URI']) || empty($_SERVER['REQUEST_URI'])) {
-	$_SERVER['REQUEST_URI'] = $_SERVER['PHP_SELF'] . '/' . $_SERVER['QUERY_STRING'];
+    $_SERVER['REQUEST_URI'] = $_SERVER['PHP_SELF'] . '/' . $_SERVER['QUERY_STRING'];
 }
 
 //really needed? (todo: check)
@@ -355,15 +361,15 @@ include_once('lang/' . $language . '/language.php');
 
 // added for wirtual hosting suport
 if (!isset($tikidomain)) {
-	$tikidomain = "";
+    $tikidomain = "";
 } else {
-	$tikidomain .= "/";
+    $tikidomain .= "/";
 }
 
 $smarty->assign("tikidomain", $tikidomain);
 
 // Debug console open/close
 $smarty->assign('debugconsole_style',
-	isset($_COOKIE["debugconsole"]) && ($_COOKIE["debugconsole"] == 'o') ? 'display:block;' : 'display:none;');
+    isset($_COOKIE["debugconsole"]) && ($_COOKIE["debugconsole"] == 'o') ? 'display:block;' : 'display:none;');
 
 ?>
