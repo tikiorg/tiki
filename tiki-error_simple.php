e5aa7ded3ec733bb9698ae4a2e64a8f7ef86c19d
teedog <teedog@users.sourceforge.net>
2003-08-22 00:26:32 +0000

    added a simple error page that does not need to include smarty or tikilib or any other tiki libraries/classes/functions useful for displaying error msgs when server load is high or site is down for heavy maintainance
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1340 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki-error_simple.php
@@ -0,0 +1,20 @@
+<?php
+
+// $Header:
+
+echo '<html><body><pre><p>';
+if (isset($_REQUEST['error']) and !is_null($_REQUEST['error'])) {
+	echo $_REQUEST['error'];
+} else {
+	echo 'There was an unsepecified error.  Please go back and try again.';
+}
+
+echo '</p>
+<form name="loginbox" action="tiki-login.php?url=tikiIndex" method="post">
+user: <input type="text" name="user"  size="20" /><br />
+pass: <input type="password" name="pass" size="20" /><br />
+<input type="submit" name="login" value="login" /></form>';
+
+echo '<p><a href="javascript:history.back()">Go back</a></p></pre></body></html>';
+
+?>
