3a623849c60fe97be4ffb1afbadbb3aa1ef4c305
teedog <teedog@users.sourceforge.net>
2003-08-22 00:44:36 +0000

    commented some redundant code
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1342 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup_base.php
+++ tiki-setup_base.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-setup_base.php,v 1.28 2003-08-21 23:11:48 teedog Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-setup_base.php,v 1.29 2003-08-22 00:44:36 teedog Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -339,10 +339,10 @@ if ($feature_userPreferences == 'y') {
     $smarty->assign('style', $style);
     $smarty->assign('language', $language);
 }
-*/
 
 $stlstl = explode('.', $style);
 $style_base = $stlstl[0];
+*/
 
 // Fix IIS servers not setting what they should set (ay ay IIS, ay ay)
 if (!isset($_SERVER['QUERY_STRING']))
