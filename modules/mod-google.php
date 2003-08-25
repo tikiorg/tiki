9a09f1216eea58f7104735c41999216e0c70c004
guideweb <guideweb@users.sourceforge.net>
2003-08-25 03:56:29 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1356 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-google.php
+++ modules/mod-google.php
@@ -3,15 +3,16 @@
 // Google menu item for e107
 // This menu item will display a search box for Google
 /*
-why a redirect ? ? ?
+//why a redirect ? ? ?
 
 if(isset($_POST['sgoogle'])){
-echo "<script language='Javascript' type='text/javascript'>
-<!--
-  window.open ('http://www.google.com/search?hl=en&ie=UTF-8&amp;oe=UTF-8&amp;q=".$_POST['sgoogle']."&amp;btnG=Google+Search','Google');
--->
-</SCRIPT>";
-$smarty->assign('ownurl',$_SERVER["REQUEST_URI"]);
+
+	echo "<script language='Javascript' type='text/javascript'>\n"
+	. "<!--\n"
+	. "  window.open ('http://www.google.com/search?hl=en&ie=UTF-8&amp;oe=UTF-8&amp;q=".$_POST['sgoogle']."&amp;btnG=Google+Search','Google');\n"
+	. "-->\n"
+	. "</SCRIPT>\n";
+	$smarty->assign('ownurl',$_SERVER["REQUEST_URI"]);
 }
 */
 
