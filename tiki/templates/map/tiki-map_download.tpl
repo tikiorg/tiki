aa0a332cf2475c2b49a5d53a65940ca5a4f6ce61
franck <franck@users.sourceforge.net>
2003-08-26 05:51:33 +0000

    layer manager for maps
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1364 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/templates/map/tiki-map_download.tpl
+++ tiki/templates/map/tiki-map_download.tpl
@@ -1,9 +1,21 @@
-<div align="center">
-<br>
-{$userwatch}, you have requested to download the layer:{$layer} from
-the mapfile:{$mapfile}<br>
-Here are the files to download, do not forget to rename them<br>
+<h3>{tr}Download Layer{/tr}</h3>
+{$userwatch}, {tr}you have requested to download the layer:{/tr}{$layer} {tr}from
+the mapfile:{/tr}{$mapfile}<br><br>
+{tr}Here are the files to download, do not forget to rename them:{/tr}<br>
+<table border="1" cellpadding="0" cellspacing="0" width="97%">
 {section name=j loop=$files}
-<a href="files/{$dfiles[j]}">{$files[j]}</a><br>
+{if $smarty.section.j.index % 2}
+<tr>
+<td class="odd">
+<a class="link" href="files/{$dfiles[j]}">{$files[j]}</a>
+</td>
+</tr>
+{else}
+<tr>
+<td class="even">
+<a class="link" href="files/{$dfiles[j]}">{$files[j]}</a>
+</td>
+</tr>
+{/if}
 {/section}
-</div>
+</table>
---
+++ tiki/tiki-map_upload.php
@@ -0,0 +1,137 @@
+<?php
+
+// $Header: /cvsroot/tikiwiki/tiki/tiki-map_upload.php,v 1.1 2003-08-26 05:51:32 franck Exp $
+
+// Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+// Initialization
+require_once ('tiki-setup.php');
+
+if(@$feature_maps != 'y') {
+  $smarty->assign('msg',tra("Feature disabled"));
+  $smarty->display("styles/$style_base/error.tpl");
+  die;
+}
+
+if (!$tiki_p_map_edit) {
+  $smarty->assign('msg',tra("You do not have permissions to view the layers"));
+  $smarty->display("styles/$style_base/error.tpl");
+  die;      
+}
+
+$max_file_size=ini_get("upload_max_filesize");
+$smarty->assign('max_file_size', $max_file_size);
+
+if (isset($_REQUEST["dir"])) {
+  $dir=realpath($map_path.$_REQUEST["dir"]);
+  $directory_path=$dir;
+  $dir="/".substr($dir,strlen($map_path));
+  if (substr($dir,0,5)!=="/data") {
+    $directory_path=$map_path."/data";
+    $dir="/data";
+  }
+} else {
+  $directory_path=$map_path."/data";
+  $dir="/data";
+}
+$smarty->assign('dir', $dir);	
+
+//Do we have a file to upload?
+if (isset($_REQUEST["upload"])) {
+  if(!move_uploaded_file($_FILES['userfile']['tmp_name'], $directory_path."/".$_FILES['userfile']['name'])) {
+    $smarty->assign('msg',tra("Could not upload the file"));
+    $smarty->display("styles/$style_base/error.tpl");
+    die;
+  }
+}
+
+//Do we have a file to delete?
+if (isset($_REQUEST["action"]) && isset($_REQUEST["file"])) {
+  if ($_REQUEST["action"]=="delete") {
+    if (is_file($directory_path."/".$_REQUEST["file"])) {
+      if (!$tiki_p_map_delete) {
+        $smarty->assign('msg',tra("You do not have permissions to delete a file"));
+        $smarty->display("styles/$style_base/error.tpl");
+        die;      
+      }
+      unlink($directory_path."/".$_REQUEST["file"]);
+    }
+  }
+}
+
+//Do we have a directory to create or delete?
+if (isset($_REQUEST["action"]) && isset($_REQUEST["directory"])) {
+  if ($_REQUEST["action"]=="createdir") {
+    if(!preg_match("/^\./", $_REQUEST["directory"])){
+      if (!$tiki_p_map_create) {
+        $smarty->assign('msg',tra("You do not have permissions to create a directory"));
+        $smarty->display("styles/$style_base/error.tpl");
+        die;      
+      }
+      if(!@mkdir($directory_path."/".$_REQUEST["directory"])) {
+        $smarty->assign('msg',tra("The Directory is not empty"));
+        $smarty->display("styles/$style_base/error.tpl");
+        die;      
+      }
+    }
+  }
+  if ($_REQUEST["action"]=="deldir") {
+    if(!preg_match("/^\./", $_REQUEST["directory"])){
+      if (!$tiki_p_map_delete) {
+        $smarty->assign('msg',tra("You do not have permissions to delete a directory"));
+        $smarty->display("styles/$style_base/error.tpl");
+        die;      
+      }
+      if(!@rmdir($directory_path."/".$_REQUEST["directory"])) {
+        $smarty->assign('msg',tra("The Directory is not empty"));
+        $smarty->display("styles/$style_base/error.tpl");
+        die;      
+      }
+    }
+  }  
+}
+
+// Get layers from the layers directory
+$files = array();
+$dirs = array();
+$h = opendir($directory_path);
+
+while (($file = readdir($h)) !== false) {
+  // Ignore hidden files
+  if(!preg_match("/^\./", $file)){
+    // Put dirs in $dirs[] and files in $files[]
+    if(is_dir($directory_path."/".$file)){
+      $dirs[] = $file;
+    }else{
+      $files[] = $file;
+    }
+  }
+}
+closedir ($h);
+
+if($dir!=="/data") {
+  $dirs[]="..";
+}
+
+// if $dirs[] exists, sort it and print all elements in it.
+if(is_array($dirs)){
+  sort($dirs);
+}
+
+// if $files[] exists, sort it and print all elements in it.
+if(is_array($files)){
+  sort($files);
+}  
+
+
+$smarty->assign('files', $files);	
+$smarty->assign('dirs', $dirs);
+
+
+// Get templates from the templates/modules directori
+$smarty->assign('mid', 'map/tiki-map_upload.tpl');
+$smarty->display("styles/$style_base/tiki.tpl");
+
+?>
