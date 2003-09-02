e4784408627cab2126da3d27987bc1407cd6a5ae
ohertel <ohertel@users.sourceforge.net>
2003-09-02 19:38:22 +0000

    Tiki's blog rss feed should produce valid RSS 1.0 and RSS 2.0 output now. RSS version and use of CSS are configurable now:
    	/tiki-blog_rss.php?blogId=1 or /tiki-wiki_rss.php?blogId=1&ver=1 for RSS 1.0
    	/tiki-blog_rss.php?blogId=1&ver=2 for RSS 2.0
    
    when using RSS 1.0, you can tell the feed to add a stylesheet:
    
    	/tiki-blog_rss.php?blogId=1&css=y or
    	/tiki-blog_rss.php?blogId=1&ver=1&css=y
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1421 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-blog_rss.php
+++ tiki-blog_rss.php
@@ -1,138 +1,146 @@
 <?php
-// $Header: /cvsroot/tikiwiki/tiki/tiki-blog_rss.php,v 1.13 2003-08-21 00:51:20 redflo Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-blog_rss.php,v 1.14 2003-09-02 19:38:22 ohertel Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-  require_once('tiki-setup.php');
-  require_once('lib/tikilib.php');
-  include_once('lib/blogs/bloglib.php');
+require_once ('tiki-setup.php');
+require_once ('lib/tikilib.php'); # httpScheme()
+include_once ('lib/blogs/bloglib.php');
 
-  header("content-type: text/xml");
-  $foo = parse_url($_SERVER["REQUEST_URI"]);
+// object specific things:
+if ($rss_blog != 'y') {
+	$smarty -> assign('msg', tra("This feature is disabled"));
+	$smarty -> display("styles/$style_base/error.tpl");
+	die; // TODO: output of rss file with message: rss disabled
+}
 
-  if($rss_blog != 'y') {
-   die;
-  }
+if ($tiki_p_read_blog != 'y') {
+	$smarty -> assign('msg', tra("Permission denied you can not view this section"));
+	$smarty -> display("styles/$style_base/error.tpl");
+	die; // TODO: output of rss file with message: permission denied
+}
 
-  if($tiki_p_read_blog != 'y') {
-    $smarty->assign('msg',tra("Permission denied you can not view this section"));
-    $smarty->display("styles/$style_base/error.tpl");
-    die;  
-  }
+if (!isset($_REQUEST["blogId"])) {
+	$smarty -> assign('msg', tra("No blogId specified"));
+	$smarty -> display("styles/$style_base/error.tpl");
+	die; // TODO: output of rss file with message: object not found
+}
 
-  if(!isset($_REQUEST["blogId"])) {
-    die;
-  }
+$title = $tikilib -> get_preference("title", "Tiki RSS feed for the weblog:");
+$now = date("U");
+$changes = $bloglib -> list_blog_posts($_REQUEST["blogId"], 0, $max_rss_blog, 'created_desc', '', $now);
+$info = $tikilib -> get_blog($_REQUEST["blogId"]);
+$blogtitle = $info["title"];
+$blogdesc = $info["description"];
 
-  $foo1=str_replace("tiki-blog_rss.php",$tikiIndex,$foo["path"]);
-  $foo2=str_replace("tiki-blog_rss.php","img/tiki.jpg",$foo["path"]);
-  $foo3=str_replace("tiki-blog_rss","tiki-view_blog",$foo["path"]);
-  $foo4=str_replace("tiki-blog_rss.php","lib/rss/rss-style.css",$foo["path"]);
-  $home = httpPrefix().$foo1;
-  $img = httpPrefix().$foo2;
-  $read = httpPrefix().$foo3;
-  $css = httpPrefix().$foo4;
-  $title = $tikilib->get_preference("title","Tiki RSS feed for the weblog:");
-  $now = date("U");
-  $changes = $bloglib->list_blog_posts($_REQUEST["blogId"], 0,$max_rss_blog,'created_desc', '', $now);
-  $info = $tikilib->get_blog($_REQUEST["blogId"]);
-  $blogtitle = $info["title"];
-  $blogdesc = $info["description"];
+// --- object independend things: (TODO: cleaning up not yet finished)
 
-print '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
-print '<?xml-stylesheet href="' . $css . '" type="text/css"?>' . "\n";
+$rss_use_css = false; // default is: do not use css
+$rss_version = 1; // default is: rss v1.0 - TODO: make this configurable
 
-?>
-
-<rdf:RDF xmlns:dc = "http://purl.org/dc/elements/1.1/"
-	xmlns:h = "http://www.w3.org/1999/xhtml" xmlns:rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-	xmlns = "http://purl.org/rss/1.0/">
-	<channel rdf:about = "<?php echo $home; ?>">
-		<title><?php
-
-		echo $title;
-
-		?>
-
-		<?php
-
-		echo htmlspecialchars($blogtitle);
-
-		?>
-
-		</title>
-
-		<link>
-			<?php
-
-			echo $home;
-
-			?>
-
-		</link>
-
-		<description>
-			<?php
-
-			echo htmlspecialchars($blogdesc);
-
-			?>
-
-		</description>
-
-		<image rdf:about = "<?php echo $img; ?>"> <title><?php
-
-		echo $title;
-
-		?>
-
-		<?php
-
-		echo htmlspecialchars($blogtitle);
-
-		?>
-
-		</title>
-
-		<link>
-			<?php
-
-			echo $home
-
-			?>
-
-		</link> </image>
-
-		<items>
-			<rdf:Seq>
-				<?php
-
-			// LOOP collecting last changes to blog (index)
-			foreach ($changes["data"] as $chg) {
-				print ('        <rdf:li resource="' . $read . '?blogId=' . $_REQUEST["blogId"] . '" />' . "\n");
-			}
-
-			?>
-
-			</rdf:Seq>
-		</items>
-	</channel>
-
-	<?php
-
-	// LOOP collecting last changes to blogs
-	foreach ($changes["data"] as $chg) {
-		print ('<item rdf:about="' . $read . '?blogId=' . $_REQUEST["blogId"] . '">' . "\n");
-
-		print ('<title>' . $tikilib->date_format($tikilib->get_long_datetime_format(), $chg["created"]). '</title>' . "\n");
-		print ('<link>' . $read . '?blogId=' . $_REQUEST["blogId"] . '</link>' . "\n");
-		$data = $tikilib->date_format($tikilib->get_short_datetime_format(), $chg["created"]);
-		print ('<description>' . htmlspecialchars($chg["data"]). '</description>' . "\n");
-		print ('</item>' . "\n\n");
+if (isset($_REQUEST["css"])) {
+	$rss_use_css = true;
+}
+if (isset($_REQUEST["ver"]))
+	if ($_REQUEST["ver"] == '2') {
+		$rss_version = 2;
 	}
 
-	?>
+$url = $_SERVER["REQUEST_URI"];
+$url = substr($url, 0, strpos($url."?", "?")); // strip all parameters from url
+$urlarray = parse_url($url);
 
-</rdf:RDF>
+$pagename = substr($urlarray["path"], strrpos($urlarray["path"], '/') + 1);
+
+$home = httpPrefix().str_replace($pagename, $tikiIndex, $urlarray["path"]);
+$img = httpPrefix().str_replace($pagename, "img/tiki.jpg", $urlarray["path"]);
+$read = httpPrefix().str_replace($pagename, "tiki-view_blog_post.php?blogId=", $urlarray["path"]);
+$url = httpPrefix().$url;
+
+$css = httpPrefix().str_replace($pagename, "lib/rss/rss-style.css", $urlarray["path"]);
+
+// --- output starts here 
+header("content-type: text/xml");
+print '<?xml version="1.0" ?>'."\n";
+print '<!--  RSS generated by TikiWiki CMS on '.date('r').' -->'."\n";
+
+if ($rss_version == 2) {
+	print '<rss version="2.0">'."\n";
+}
+
+if (($rss_use_css) && ($rss_version == 1)) {
+	print '<?xml-stylesheet href="'.htmlspecialchars($css).'" type="text/css"?>'."\n";
+}
+
+if ($rss_version == 2) {
+	print "<channel>\n";
+} else {
+	print '<rdf:RDF xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:h="http://www.w3.org/1999/xhtml" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://purl.org/rss/1.0/">'."\n";
+	print '<channel rdf:about="'.htmlspecialchars($url).'">'."\n";
+}
+
+print "<title>".htmlspecialchars($title." ".$blogtitle)."</title>\n";
+print "<link>".htmlspecialchars($home)."</link>\n";
+print "<description>".htmlspecialchars($blogdesc)."</description>\n";
+
+if ($rss_version == 2) {
+	print "<language>en-us</language>\n";
+} // TODO: make language configurable
+
+print "\n";
+
+if ($rss_version == 2) {
+	print '<image>'."\n";
+} else {
+	print '<image rdf:about="'.htmlspecialchars($url).'" rdf:url="'.htmlspecialchars($img).'">'."\n";
+}
+print "<title>".htmlspecialchars($title)."</title>\n";
+print "<link>".htmlspecialchars($home)."</link>\n";
+print "<url>".htmlspecialchars($url)."</url>\n";
+print "</image>\n\n";
+
+if ($rss_version == 1) {
+	print "<items>\n";
+	print "<rdf:Seq>\n";
+	// LOOP collecting last changes to the blog entries (index)
+	foreach ($changes["data"] as $chg) {
+		print ('        <rdf:li resource="'.htmlspecialchars($read.$chg["blogId"]."&postId=".$chg["postId"]).'" />'."\n");
+	}
+	print "</rdf:Seq>\n";
+	print "</items>\n";
+
+	print "</channel>\n";
+}
+
+// LOOP collecting last changes to blogs
+foreach ($changes["data"] as $chg) {
+	if ($rss_version == 2) {
+		print ("<item>\n");
+	} else {
+		print ('<item rdf:about="'.htmlspecialchars($read.$chg["blogId"]."&postId=".$chg["postId"]).'">'."\n");
+	}
+	print ('  <title>'.htmlspecialchars($blogtitle.': '.$tikilib -> date_format($tikilib -> get_short_datetime_format(), $chg["created"])).'</title>'."\n");
+	print ('  <link>'.htmlspecialchars($read.$chg["blogId"]."&postId=".$chg["postId"]).'</link>'."\n");
+
+	if ($rss_version == 2) {
+		$date = gmdate('D, d M Y H:i:s T', $chg["created"]);
+		print ('<description>'.htmlspecialchars($chg["data"]).'</description>'."\n");
+		// print("<author>".$chg["user"]."</author>\n"); // TODO: email address of author
+		print ('<guid isPermaLink="true">'.htmlspecialchars($read.$chg["blogId"]."&postId=".$chg["postId"]).'</guid>'."\n");
+		print ("<pubDate>$date</pubDate>\n");
+	} else {
+		$date = $tikilib -> date_format($tikilib -> get_short_datetime_format(), $chg["created"]);
+		print ('  <description>'.htmlspecialchars($chg["data"]).'</description>'."\n");
+	}
+	print ('</item>'."\n\n");
+}
+
+if ($rss_version == 2) {
+	print "</channel>\n";
+	print "</rss>\n";
+} else {
+	print "</rdf:RDF>\n";
+}
+?>
