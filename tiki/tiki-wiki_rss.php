c4e927e82f39138e6423422888be2844e9a3e1cb
ohertel <ohertel@users.sourceforge.net>
2003-09-02 14:42:35 +0000

    Tiki's wiki rss feed should produce valid RSS 1.0 and RSS 2.0 output now. RSS version and use of CSS are configurable now:
    	/tiki-wiki_rss.php or /tiki-wiki_rss.php?ver=1 for RSS 1.0
    	/tiki-wiki_rss.php?ver=2 for RSS 2.0
    
    when using RSS 1.0, you can tell the feed to add a stylesheet:
    
    	/tiki-wiki_rss.php?css=y or
    	/tiki-wiki_rss.php?ver=1&css=y
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1416 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-wiki_rss.php
+++ tiki/tiki-wiki_rss.php
@@ -1,134 +1,138 @@
 <?php
-// $Header: /cvsroot/tikiwiki/tiki/tiki-wiki_rss.php,v 1.14 2003-08-21 00:51:20 redflo Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-wiki_rss.php,v 1.15 2003-09-02 14:42:35 ohertel Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-  require_once('tiki-setup.php');
-  require_once('lib/tikilib.php'); # httpScheme()
-  include_once('lib/wiki/histlib.php');
+require_once ('tiki-setup.php');
+require_once ('lib/tikilib.php'); # httpScheme()
+include_once ('lib/wiki/histlib.php');
 
-  if($rss_wiki != 'y') {
-   die;
-  }
-  
-  if($tiki_p_view != 'y') {
-    $smarty->assign('msg',tra("Permission denied you cannot view this section"));
-    $smarty->display("styles/$style_base/error.tpl");
-    die;  
-  }
+// object specific things:
+if ($rss_wiki != 'y') {
+	die; // TODO: output of rss file with message: rss disabled
+}
 
-  header("content-type: text/xml");
-  $foo = parse_url($_SERVER["REQUEST_URI"]);
-  $foo1=str_replace("tiki-wiki_rss.php","tiki-index.php",$foo["path"]);
-  $foo2=str_replace("tiki-wiki_rss.php","img/tiki.jpg",$foo["path"]);
-  $foo3=str_replace("tiki-wiki_rss.php","lib/rss/rss-style.css",$foo["path"]);
-  $home = httpPrefix().$foo1;
-  $img = httpPrefix().$foo2;
-  $css = httpPrefix().$foo3;
-  $title = $tikilib->get_preference("title","Tiki RSS feed for the wiki pages");
-  $title = "Tiki RSS feed for the wiki pages";
-  $desc = "Last modifications to the Wiki.";
-  $changes =   $histlib->get_last_changes(999, 0, $max_rss_wiki, $sort_mode = 'lastModif_desc');
+if ($tiki_p_view != 'y') {
+	$smarty -> assign('msg', tra("Permission denied you cannot view this section"));
+	$smarty -> display("styles/$style_base/error.tpl");
+	die; // TODO: output of rss file with message: permission denied
+}
 
-print '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
-print '<?xml-stylesheet href="' . $css . '" type="text/css"?>' . "\n";
+$title = "Tiki RSS feed for the wiki pages";
+$desc = "Last modifications to the Wiki.";
+$changes = $histlib -> get_last_changes(999, 0, $max_rss_wiki, $sort_mode = 'lastModif_desc');
 
-?>
+// --- object independend things: (TODO: cleaning up not yet finished)
 
-<rdf:RDF xmlns:dc = "http://purl.org/dc/elements/1.1/"
-	xmlns:h = "http://www.w3.org/1999/xhtml" xmlns:rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-	xmlns = "http://purl.org/rss/1.0/">
-	<channel rdf:about = "<?php echo $home; ?>">
-		<title><?php
+$rss_use_css = false; // default is: do not use css
+$rss_version = 1; // default is: rss v1.0 - TODO: make this configurable
 
-		echo $title;
-
-		?>
-
-		</title>
-
-		<link>
-			<?php
-
-			echo $home;
-
-			?>
-
-		</link>
-
-		<description>
-			<?php
-
-			echo $desc;
-
-			?>
-
-		</description>
-
-		<image rdf:about = "<?php echo $img; ?>" rdf:url = "<?php echo $img; ?>"> <title><?php
-
-		echo $title;
-
-		?>
-
-		</title>
-
-		<link>
-			<?php
-
-			echo $home
-
-			?>
-
-		</link>
-
-		<url>
-			<?php
-
-			echo $home
-
-			?>
-
-		</url> </image>
-
-		<items>
-			<rdf:Seq>
-				<?php
-
-			// LOOP collecting last changes to the wiki pages (index)
-			foreach ($changes["data"] as $chg) {
-				print ('        <rdf:li resource="' . $home . '?page=' . $chg["pageName"] . '" />' . "\n");
-			}
-
-			?>
-
-			</rdf:Seq>
-		</items>
-	</channel>
-
-	<?php
-
-	// LOOP collecting last changes to the wiki pages
-	foreach ($changes["data"] as $chg) {
-		print ('<item rdf:about="' . $home . '?page=' . $chg["pageName"] . '">' . "\n");
-
-		print ('  <title>' . $chg["pageName"] . ' ' . $chg["action"] . '</title>' . "\n");
-		print ('  <link>' . $home . '?page=' . $chg["pageName"] . '</link>' . "\n");
-		$data = $tikilib->date_format($tikilib->get_short_datetime_format(), $chg["lastModif"]);
-
-		if (!empty($chg["comment"])) {
-			$comment = "(" . htmlspecialchars($chg["comment"]). ")";
-		} else {
-			$comment = '';
-		}
-
-		print ('  <description>' . "[$data] :" . $chg["action"] . " " . htmlspecialchars(
-			$chg["pageName"]). $comment . '</description>' . "\n");
-		print ('</item>' . "\n\n");
+if (isset($_REQUEST["css"])) {
+	$rss_use_css = true;
+}
+if (isset($_REQUEST["ver"]))
+	if ($_REQUEST["ver"] == '2') {
+		$rss_version = 2;
 	}
 
-	?>
+$url = $_SERVER["REQUEST_URI"];
+$url = substr($url, 0, strpos($url."?", "?")); // strip all parameters from url
+$urlarray = parse_url($url);
 
-</rdf:RDF>
+$pagename = substr($urlarray["path"], strrpos($urlarray["path"], '/') + 1);
+
+$home = httpPrefix().str_replace($pagename, $tikiIndex, $urlarray["path"]);
+$img = httpPrefix().str_replace($pagename, "img/tiki.jpg", $urlarray["path"]);
+$url = httpPrefix().$url;
+
+$css = httpPrefix().str_replace($pagename, "lib/rss/rss-style.css", $urlarray["path"]);
+
+// --- output starts here 
+header("content-type: text/xml");
+print '<?xml version="1.0" ?>'."\n";
+print '<!--  RSS generated by TikiWiki CMS on '.date('r').' -->'."\n";
+
+if ($rss_version == 2) {
+	print '<rss version="2.0">'."\n";
+}
+
+if (($rss_use_css) && ($rss_version == 1)) {
+	print '<?xml-stylesheet href="'.$css.'" type="text/css"?>'."\n";
+}
+
+if ($rss_version == 2) {
+	print "<channel>\n";
+} else {
+	print '<rdf:RDF xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:h="http://www.w3.org/1999/xhtml" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://purl.org/rss/1.0/">'."\n";
+	print '<channel rdf:about="'.$url.'">'."\n";
+}
+
+print "<title>$title</title>\n";
+print "<link>$home</link>\n";
+print "<description>$desc</description>\n";
+
+if ($rss_version == 2) {
+	print "<language>en-us</language>\n";
+} // TODO: make language configurable
+
+print "\n";
+
+if ($rss_version == 2) {
+	print '<image>'."\n";
+} else {
+	print '<image rdf:about="'.$url.'" rdf:url="'.$img.'">'."\n";
+}
+print "<title>$title</title>\n";
+print "<link>$home</link>\n";
+print "<url>$url</url>\n";
+print "</image>\n\n";
+
+if ($rss_version == 1) {
+	print "<items>\n";
+	print "<rdf:Seq>\n";
+	// LOOP collecting last changes to the wiki pages (index)
+	foreach ($changes["data"] as $chg) {
+		print ('        <rdf:li resource="'.rawurlencode($home.'?page='.$chg["pageName"]).'" />'."\n");
+	}
+	print "</rdf:Seq>\n";
+	print "</items>\n";
+
+	print "</channel>\n";
+}
+
+// LOOP collecting last changes to the wiki pages 
+foreach ($changes["data"] as $chg) {
+	if ($rss_version == 2) {
+		print ("<item>\n");
+	} else {
+		print ('<item rdf:about="'.rawurlencode($home.'?page='.$chg["pageName"]).'">'."\n");
+	}
+	print ('  <title>'.rawurlencode($chg["pageName"].' '.$chg["action"]).'</title>'."\n");
+	print ('  <link>'.rawurlencode($home.'?page='.$chg["pageName"]).'</link>'."\n");
+	if (!empty($chg["comment"])) {
+		$comment = "(".htmlspecialchars($chg["comment"]).")";
+	} else {
+		$comment = '';
+	}
+	if ($rss_version == 2) {
+		$date = gmdate('D, d M Y H:i:s T', $chg["lastModif"]);
+		print ('<description>'.$chg["action"].urlencode(" ".$chg["pageName"].$comment).'</description>'."\n");
+		// print("<author>".$chg["user"]."</author>\n"); // TODO: email address of author
+		print ('<guid isPermaLink="true">'.rawurlencode($home.'?page='.$chg["pageName"]).'</guid>'."\n");
+		print ("<pubDate>$date</pubDate>\n");
+	} else {
+		$date = $tikilib -> date_format($tikilib -> get_short_datetime_format(), $chg["lastModif"]);
+		print ('  <description>'."[$date] :".$chg["action"].urlencode(" ".$chg["pageName"].$comment).'</description>'."\n");
+	}
+	print ('</item>'."\n\n");
+}
+
+if ($rss_version == 2) {
+	print "</channel>\n";
+	print "</rss>\n";
+} else {
+	print "</rdf:RDF>\n";
+}
+?>
