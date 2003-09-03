d7ef9b7dea7095cb72c21bc250aac2b2ac1d9841
mose <mose@users.sourceforge.net>
2003-09-03 17:56:26 +0000

    added a new setup for smarty new version
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1425 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ setup_smarty.php
@@ -0,0 +1,73 @@
+<?php
+
+// $Header: /cvsroot/tikiwiki/tiki/setup_smarty.php,v 1.1 2003-09-03 17:56:26 mose Exp $
+
+// Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+require_once ("db/tiki-db.php");
+
+error_reporting (E_ALL);
+ini_set('register_globals','off');
+
+// Remove automatic quotes added to POST/COOKIE by PHP
+if (get_magic_quotes_gpc()) {
+	foreach ($_REQUEST as $k => $v) {
+		if (!is_array($_REQUEST[$k])) $_REQUEST[$k] = stripslashes($v);
+	}
+}
+
+// Define and load Smarty components
+define('SMARTY_DIR', 'lib/smarty/');
+require_once (SMARTY_DIR . 'Smarty.class.php');
+
+class Smarty_TikiWiki extends Smarty {
+	
+	function Smarty_TikiWiki($tikidomain = "") {
+		$this->template_dir = "templates/";
+		$this->compile_dir = "templates_c/$tikidomain";
+		$this->config_dir = "configs/";
+		$this->cache_dir = "cache/$tikidomain";
+		$this->caching = false;
+		$this->assign('app_name', 'TikiWiki');
+		$this->plugins_dir = array("plugins","plugins_tiki");
+
+	}
+
+	function _smarty_include($params) {
+		global $style, $style_base;
+
+		if (isset($style) && isset($style_base)) {
+			if (file_exists("templates/styles/$style_base/".$params['smarty_include_tpl_file'])) {
+				$params['smarty_include_tpl_file'] = "styles/$style_base/".$params['smarty_include_tpl_file'];
+			}
+		}
+		return parent::_smarty_include($params);
+	}
+
+	function fetch($_smarty_tpl_file, $_smarty_cache_id = null, $_smarty_compile_id = null, $_smarty_display = false) {
+		global $language, $style, $style_base;
+
+		if (isset($style) && isset($style_base)) {
+			if (file_exists("templates/styles/$style_base/$_smarty_tpl_file")) {
+				$_smarty_tpl_file = "styles/$style_base/$_smarty_tpl_file";
+			}
+		}
+		$_smarty_cache_id = $language . $_smarty_cache_id;
+		$_smarty_compile_id = $language . $_smarty_compile_id;
+		return parent::fetch($_smarty_tpl_file, $_smarty_cache_id, $_smarty_compile_id, $_smarty_display);
+	}
+}
+
+if (!isset($tikidomain))
+	$tikidomain = "";
+
+$smarty = new Smarty_TikiWiki($tikidomain);
+$smarty->load_filter('pre', 'tr');
+//$smarty->load_filter('output','trimwhitespace');
+
+// Count number of online users using:
+// print($GLOBALS["PHPSESSID"]);
+
+?>
