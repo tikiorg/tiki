d484f57e31de5b772d751da3bc702171156c1b5d
mose <mose@users.sourceforge.net>
2003-09-04 16:10:56 +0000

    added some 14 optionnal parameters for wikigraphs
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1437 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki/wikilib.php
+++ lib/wiki/wikilib.php
@@ -86,33 +86,33 @@ class WikiLib extends TikiLib {
 		return $this->getOne("select `creator` from `tiki_pages` where `pageName`=?", array($name));
 	}
 
-	function wiki_page_graph(&$str, &$graph) {
+	function wiki_page_graph(&$str, &$graph, $garg) {
 		$page = $str['name'];
 
 		$graph->addAttributes(array(
-			'nodesep' => '.3',
-			'rankdir' => 'LR',
-			'size' => '6',
-			'bgcolor' => 'transparent',
+			'nodesep' => (isset($garg['att']['nodesep']))?$garg['att']['nodesep']:".1",
+			'rankdir' => (isset($garg['att']['rankdir']))?$garg['att']['rankdir']:'LR',
+			'size' => (isset($garg['att']['size']))?$garg['att']['size']:'6',
+			'bgcolor' => (isset($garg['att']['bgcolor']))?$garg['att']['bgcolor']:'transparent',
 			'URL' => 'tiki-index.php'
 		));
 
 		$graph->addNode("$page", array(
 			'URL' => "tiki-index.php?page=" . addslashes($page),
 			'label' => "$page",
-			'fontname' => "Arial",
-			'fontsize' => '9',
-			'shape' => 'ellipse',
-			'color' => '#AAAAAA',
-			'style' => 'filled',
-			'fillcolor' => '#FFFFFF',
-			'width' => '.5',
-			'height' => '.25'
+			'fontname' => (isset($garg['node']['fontname']))?$garg['node']['fontname']:"Arial",
+			'fontsize' => (isset($garg['node']['fontsize']))?$garg['node']['fontsize']:'9',
+			'shape' => (isset($garg['node']['shape']))?$garg['node']['shape']:'ellipse',
+			'color' => (isset($garg['node']['color']))?$garg['node']['color']:'#AAAAAA',
+			'style' => (isset($garg['node']['style']))?$garg['node']['style']:'filled',
+			'fillcolor' => (isset($garg['node']['fillcolor']))?$garg['node']['fillcolor']:'#FFFFFF',
+			'width' => (isset($garg['node']['width']))?$garg['node']['width']:'.5',
+			'height' => (isset($garg['node']['height']))?$garg['node']['height']:'.25'
 		));
 
 		//print("add node $page<br/>");
 		foreach ($str['pages'] as $neig) {
-			$this->wiki_page_graph($neig, $graph);
+			$this->wiki_page_graph($neig, $graph, $garg);
 
 			$graph->addEdge(array($page => $neig['name']), array(
 				'color' => '#998877',
@@ -122,11 +122,11 @@ class WikiLib extends TikiLib {
 		}
 	}
 
-	function get_graph_map($page, $level) {
+	function get_graph_map($page, $level, $garg) {
 		$str = $this->wiki_get_link_structure($page, $level);
 
 		$graph = new Image_GraphViz();
-		$this->wiki_page_graph($str, $graph);
+		$this->wiki_page_graph($str, $graph, $garg);
 		return $graph->map();
 	}
 
