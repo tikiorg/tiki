4676661db2b0ae01cba09621a7a28e03a7ee0a39
mose <mose@users.sourceforge.net>
2003-09-07 20:49:52 +0000

    added php color highlighting in CODE plugin : (colors=>php)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1457 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/wiki-plugins/wikiplugin_code.php
+++ tiki/lib/wiki-plugins/wikiplugin_code.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_code.php,v 1.8 2003-08-07 04:34:16 rossta Exp $
+// $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_code.php,v 1.9 2003-09-07 20:49:52 mose Exp $
 // Displays a snippet of code
 // Parameters: ln => line numbering (default false)
 // Example:
@@ -7,48 +7,59 @@
 //  print("foo");
 // {CODE}
 function wikiplugin_code_help() {
-	return tra("Displays a snippet of code.\nSet optional paramater -+ln+- to 1 if you need line numbering feature.");
+	$help = "Displays a snippet of code.\n";
+	$help.= "Parameters\n";
+	$help.= "* ln=>1 provides line-numbering\n";
+	$help.= "* colors=>php highlights phpcode (other syntaxes to come)\n";
+	$help.= "( note : those parameters are exclusive for now )\n";
+	return tra($help);
+}
+
+function decodeHTML($string) {
+    $string = strtr($string, array_flip(get_html_translation_table(HTML_ENTITIES)));
+    $string = preg_replace("/&#([0-9]+);/me", "chr('\\1')", $string);
+    return $string;
 }
 
 function wikiplugin_code($data, $params) {
-	$code = htmlspecialchars($data);
+	$code = $data;
 
 	extract ($params);
 
-	if (isset($ln) && $ln == 1) {
-		$lines = explode("\n", $code);
+	if (isset($colors) and ($colors == 'php')) {
+		$data = "<div class='codelisting'>".highlight_string(decodeHTML($code),1)."</div>";
+	} else {
+		if (isset($ln) && $ln == 1) {
+			$lines = explode("\n", $code);
 
-		$i = 1; // current line number
-		$code = '';
-		// Will skip leading and trailing empty lines
-		// to make snippet look better :)
-		$fl = 0;          // 'first code line printed' flag
-		$ae = '';         // 
+			$i = 1; // current line number
+			$code = '';
+			// Will skip leading and trailing empty lines
+			// to make snippet look better :)
+			$fl = 0;          // 'first code line printed' flag
+			$ae = '';         // 
 
-		foreach ($lines as $line) {
-			$len = strlen($line);
+			foreach ($lines as $line) {
+				$len = strlen($line);
 
-			if (!($len || $fl))
-				continue; // skip leading empty lines
+				if (!($len || $fl))
+					continue; // skip leading empty lines
 
-			if ($len) {
-				// OK len >0
-				$code .= $ae . ($fl ? "\n" : '') . sprintf("%3d", $i). ':  ' . $line;
+				if ($len) {
+					// OK len >0
+					$code .= $ae . ($fl ? "\n" : '') . sprintf("%3d", $i). ':  ' . $line;
 
-				$fl = 1; // first line already printed
-				$ae = '';
-			} else
-				$ae .= "\n" . sprintf("%3d", $i). ':  ' . $line;
-
-			$i++;
+					$fl = 1; // first line already printed
+					$ae = '';
+				} else {
+					$ae .= "\n" . sprintf("%3d", $i). ':  ' . $line;
+				}
+				$i++;
+			}
+			$code = rtrim($code);
 		}
-
-		$code = rtrim($code);
-	}                    //else $code=$data;
-
-	// Wrap listing into div
-	$data = "<div class='codelisting'><pre>" . $code . "</pre></div>";
-
+		$data = "<div class='codelisting'><pre>" . $code . "</pre></div>";
+	}
 	return $data;
 }
 
