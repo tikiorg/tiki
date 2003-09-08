5bba329683b3dfa24d0de0b3707bc4762bf8946c
mose <mose@users.sourceforge.net>
2003-09-08 08:40:28 +0000

    added display of number of comments in polls
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1463 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.poll.php
+++ lib/smarty_tiki/function.poll.php
@@ -1,13 +1,15 @@
 <?php
 require_once('lib/tikilib.php'); # httpScheme()
 
-function smarty_function_poll($params, &$smarty)
-{
+function smarty_function_poll($params, &$smarty) {
     global $tikilib;
     global $dbTiki;
+		global $commentslib;
+		global $feature_poll_comments;
     extract($params);
     // Param = zone
 	include_once('lib/polls/polllib.php');
+	include_once('lib/commentslib.php');
 
     if (empty($id)) { 
       $id = $polllib->get_random_active_poll();
@@ -15,6 +17,11 @@ function smarty_function_poll($params, &$smarty)
     if($id) {
       $menu_info = $polllib->get_poll($id);
       $channels = $polllib->list_poll_options($id,0,-1,'optionId_asc','');
+			if ($feature_poll_comments == 'y') {
+				$commentslib = new Comments($dbTiki);
+				$comments_count = $commentslib->count_comments("poll".$menu_info["pollId"]);
+			}
+			$smarty->assign('comments', $comments_count);
       $smarty->assign('ownurl',/*httpPrefix().*/$_SERVER["REQUEST_URI"]);
       $smarty->assign('menu_info',$menu_info);
       $smarty->assign('channels',$channels["data"]);
