85f4aca2d753deb19d280d3c92f0021a1df33026
redflo <redflo@users.sourceforge.net>
2003-09-09 08:32:23 +0000

    avoid error reporting when unlinking temp files
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1469 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-setup.php
+++ tiki/tiki-setup.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-setup.php,v 1.139 2003-09-07 00:07:59 jenolan1701 Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-setup.php,v 1.140 2003-09-09 08:32:23 redflo Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -114,7 +114,7 @@ class TikiSetup {
 		if (!$tempdir) {
 			$tempfile = tempnam( false, '' );
 			$tempdir = dirname( $tempfile );
-			unlink( $tempfile );
+			@unlink( $tempfile );
 		}
 
 		return $tempdir;
