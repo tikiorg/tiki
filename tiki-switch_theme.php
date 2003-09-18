b90f6be51ca020aaef33c958ab5058089198ffba
mose <mose@users.sourceforge.net>
2003-09-18 18:14:19 +0000

    added a module for anyone can change his own theme even anonymous (patch #799103 from hanno stock)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1506 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki-switch_theme.php
@@ -0,0 +1,24 @@
+<?php 
+
+// Initialization
+require_once('tiki-setup.php');
+include_once('lib/tikilib.php');
+
+if (isset($_SERVER['HTTP_REFERER'])) {
+	$orig_url = $_SERVER['HTTP_REFERER'];
+} else {
+	$orig_url = $tikiIndex;
+}
+
+if (isset($_GET['theme'])){
+	$new_theme = $_GET['theme'];
+	if($feature_userPreferences == 'y' && $user && $change_theme == 'y') {  
+		$tikilib->set_user_preference($user,'theme',$new_theme);
+	} else {
+		setcookie('tiki-theme', $new_theme, time()+3600*24*30*12, '', $_SERVER['SERVER_NAME']);
+	}
+}
+
+header("location: $orig_url");
+exit;
+?>
