df328d400b1655ab46704a215e7b4b7257a2c7d2
rlpowell <rlpowell@users.sourceforge.net>
2003-09-21 20:04:48 +0000

    Fixed a variety of problem with the PHPWiki importing.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1530 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-import_phpwiki.php
+++ tiki-import_phpwiki.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-import_phpwiki.php,v 1.9 2003-08-07 04:33:57 rossta Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-import_phpwiki.php,v 1.10 2003-09-21 20:04:48 rlpowell Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -8,167 +8,182 @@
 require_once ('tiki-setup.php');
 
 if ($tiki_p_admin != 'y') {
-	$smarty->assign('msg', tra("You dont have permission to use this feature"));
+    $smarty->assign('msg', tra("You dont have permission to use this feature"));
 
-	$smarty->display("styles/$style_base/error.tpl");
-	die;
+    $smarty->display("styles/$style_base/error.tpl");
+    die;
 }
 
 require ("lib/webmail/mimeDecode.php");
 
 function parse_output(&$obj, &$parts, $i) {
-	if (!empty($obj->parts)) {
-		for ($i = 0; $i < count($obj->parts); $i++)
-			parse_output($obj->parts[$i], $parts, $i);
-	} else {
-		$ctype = $obj->ctype_primary . '/' . $obj->ctype_secondary;
+    if (!empty($obj->parts)) {
+	for ($i = 0; $i < count($obj->parts); $i++)
+	    parse_output($obj->parts[$i], $parts, $i);
+    } else {
+	$ctype = $obj->ctype_primary . '/' . $obj->ctype_secondary;
 
-		switch ($ctype) {
-		case 'application/x-phpwiki':
-			$aux["body"] = $obj->body;
+	switch ($ctype) {
+	    case 'application/x-phpwiki':
+		$aux["body"] = $obj->body;
 
-			$ccc = $obj->headers["content-type"];
-			$items = split(';', $ccc);
+		$ccc = $obj->headers["content-type"];
+		$items = split(';', $ccc);
 
-			foreach ($items as $item) {
-				$portions = split('=', $item);
+		foreach ($items as $item) {
+		    $portions = split('=', $item);
 
-				if (isset($portions[0]) && isset($portions[1])) {
-					$aux[trim($portions[0])] = trim($portions[1]);
-				}
-			}
-
-			$parts[] = $aux;
-			break;
-
-		case 'application/x-tikiwiki':
-			$aux["body"] = $obj->body;
-
-			$ccc = $obj->headers["content-type"];
-			$items = split(';', $ccc);
-
-			foreach ($items as $item) {
-				$portions = split('=', $item);
-
-				if (isset($portions[0]) && isset($portions[1])) {
-					$aux[trim($portions[0])] = trim($portions[1]);
-				}
-			}
-
-			$parts[] = $aux;
-			break;
+		    if (isset($portions[0]) && isset($portions[1])) {
+			$aux[trim($portions[0])] = trim($portions[1]);
+		    }
 		}
+
+		$parts[] = $aux;
+		break;
+
+	    case 'application/x-tikiwiki':
+		$aux["body"] = $obj->body;
+
+		$ccc = $obj->headers["content-type"];
+		$items = split(';', $ccc);
+
+		foreach ($items as $item) {
+		    $portions = split('=', $item);
+
+		    if (isset($portions[0]) && isset($portions[1])) {
+			$aux[trim($portions[0])] = trim($portions[1]);
+		    }
+		}
+
+		$parts[] = $aux;
+		break;
 	}
+    }
 }
 
 function compare_import_versions($a1, $a2) {
-	return $a1["version"] - $a2["version"];
+    return $a1["version"] - $a2["version"];
 }
 
 $smarty->assign('result', 'y');
 
 if (isset($_REQUEST["import"])) {
-	$path = $_REQUEST["path"];
+    $path = $_REQUEST["path"];
 
-	$h = opendir("$path/");
-	$lines = array();
+    $h = opendir("$path/");
+    $lines = array();
 
-	while (false !== $file = readdir($h)) {
-		if (is_file("$path/$file")) {
-			$fp = fopen("$path/$file", "r");
+    while (false !== $file = readdir($h)) {
+	if (is_file("$path/$file")) {
+	    $fp = fopen("$path/$file", "r");
 
-			$full = fread($fp, filesize("$path/$file"));
-			//$full=htmlspecialchars($full);
-			fclose ($fp);
-			$params = array(
-				'input' => $full,
-				'crlf' => "\r\n",
-				'include_bodies' => TRUE,
-				'decode_headers' => TRUE,
-				'decode_bodies' => TRUE
-			);
+	    $full = fread($fp, filesize("$path/$file"));
+	    //$full=htmlspecialchars($full);
+	    fclose ($fp);
+	    $params = array(
+		    'input' => $full,
+		    'crlf' => "\r\n",
+		    'include_bodies' => TRUE,
+		    'decode_headers' => TRUE,
+		    'decode_bodies' => TRUE
+		    );
 
-			$output = Mail_mimeDecode::decode($params);
-			parse_output($output, $parts, 0);
-			usort($parts, 'compare_import_versions');
-			$last_part = '';
-			$last_part_ver = 0;
+	    $output = Mail_mimeDecode::decode($params);
+	    parse_output($output, $parts, 0);
+	    usort($parts, 'compare_import_versions');
+	    $last_part = '';
+	    $last_part_ver = 0;
 
-			foreach ($parts as $part) {
-				if ($part["version"] > $last_part_ver) {
-					$last_part_ver = $part["version"];
+	    foreach ($parts as $part) {
+		if ($part["version"] > $last_part_ver) {
+		    $last_part_ver = $part["version"];
 
-					$last_part = $part["body"];
-				}
-
-				if (isset($part["pagename"])) {
-					// Parse the body replacing links to Tiki links
-					$part["body"] = preg_replace("/ (http:\/\/[^ ]+) /", " [$1] ", $part["body"]);
-
-					$part["body"] = preg_replace("/\[(http:\/\/[^\]]+)\]/", "{img src=$1}", $part["body"]);
-					$part["body"] = preg_replace("/\[([^\|\]]+)\]/", "(($1))", $part["body"]);
-					//$part["body"]=preg_replace("/\[([^\|]+)\|([^\]]+)\]/","(($1|$2))",$part["body"]);
-					$part["body"] = preg_replace("/\[([^\|]+)\|([^\]]+)\]/", "(($2|$1))", $part["body"]);
-
-					$pagename = urldecode($part["pagename"]);
-					$version = urldecode($part["version"]);
-					$author = urldecode($part["author"]);
-					$lastmodified = $part["lastmodified"];
-
-					if (isset($part["description"])) {
-						$description = $part["description"];
-					} else {
-						$description = '';
-					}
-
-					$authorid = urldecode($part["author_id"]);
-
-					if (isset($part["hits"])) {
-						$hits = urldecode($part["hits"]);
-					} else {
-						$hits = 0;
-					}
-
-					$ex = substr($part["body"], 0, 25);
-					//print(strlen($part["body"]));
-					$msg = '';
-
-					if ($_REQUEST["remo"] == 'y') {
-						$tikilib->remove_all_versions($pagename, '');
-					}
-
-					if ($tikilib->page_exists($pagename)) {
-						if ($_REQUEST["crunch"] == 'n') {
-							$msg = '<b>' . tra('page not added (Exists)'). '</b>';
-						} else {
-							$msg = '<b>' . tra('overwriting old page'). '</b>';
-
-							$tikilib->update_page($pagename, $part["body"], tra('updated by the phpwiki import process'), $author,
-								$authorid, $description);
-						}
-					} else {
-						$msg = tra('page created');
-
-						$tikilib->create_page($pagename, $hits, $part["body"], $lastmodified, tra('created from phpwiki import'),
-							$author, $authorid, $description);
-					}
-
-					$aux["page"] = $pagename;
-					$aux["version"] = $version;
-					$aux["part"] = $ex;
-					$aux["msg"] = $msg;
-
-					$lines[] = $aux;
-				}
-			}
-
-			unset ($parts);
+		    $last_part = $part["body"];
 		}
-	}
 
-	closedir ($h);
-	$smarty->assign('lines', $lines);
-	$smarty->assign('result', 'y');
+		if (isset($part["pagename"])) {
+		    // PHPWiki footnotes.  -rlpowell
+		    $part["body"] = preg_replace("/\[\[([^\]]+)\]/", "(Was Footnote: $1)", $part["body"]);
+
+		    // Parse the body replacing links to Tiki links
+		    $part["body"] = preg_replace("/ (http:\/\/[^ ]+) /", " [$1] ", $part["body"]);
+
+		    // I have great difficulty imagining why someone
+		    // thought this was right.  It's not. It turns
+		    // external links into image links that point
+		    // nowhere. -rlpowell
+		    //	$part["body"] = preg_replace("/\[(http:\/\/[^\]]+)\]/", "{img src=$1}", $part["body"]);
+
+		    // The (?!http:) below should really include other URL
+		    // forms, but that would get complicated very quickly.
+		    // -rlpowell
+		    $part["body"] = preg_replace("/\[((?!http:)[^\|\]]+)\]/", "(($1))", $part["body"]);
+		    //$part["body"]=preg_replace("/\[([^\|]+)\|([^\]]+)\]/","(($1|$2))",$part["body"]);
+		    $part["body"] = preg_replace("/\[((?!http:)[^\|]+)\|([^\]]+)\]/", "(($2|$1))", $part["body"]);
+
+		    // %%% makes a linebreak in PHPWiki. -rlpowell
+		    $part["body"] = str_replace("%%%", "", $part["body"]);
+
+		    $pagename = urldecode($part["pagename"]);
+		    $version = urldecode($part["version"]);
+		    $author = urldecode($part["author"]);
+		    $lastmodified = $part["lastmodified"];
+
+		    if (isset($part["description"])) {
+			$description = $part["description"];
+		    } else {
+			$description = '';
+		    }
+
+		    $authorid = urldecode($part["author_id"]);
+
+		    if (isset($part["hits"])) {
+			$hits = urldecode($part["hits"]);
+		    } else {
+			$hits = 0;
+		    }
+
+		    $ex = substr($part["body"], 0, 25);
+		    //print(strlen($part["body"]));
+		    $msg = '';
+
+		    if ($_REQUEST["remo"] == 'y') {
+			$tikilib->remove_all_versions($pagename, '');
+		    }
+
+		    if ($tikilib->page_exists($pagename)) {
+			if ($_REQUEST["crunch"] == 'n') {
+			    $msg = '<b>' . tra('page not added (Exists)'). '</b>';
+			} else {
+			    $msg = '<b>' . tra('overwriting old page'). '</b>';
+
+			    $tikilib->update_page($pagename, $part["body"],
+				    tra('updated by the phpwiki import process'), $author,
+				    $authorid, $description);
+			}
+		    } else {
+			$msg = tra('page created');
+
+			$tikilib->create_page($pagename, $hits, $part["body"], $lastmodified,
+				tra('created from phpwiki import'), $author, $authorid, $description);
+		    }
+
+		    $aux["page"] = $pagename;
+		    $aux["version"] = $version;
+		    $aux["part"] = $ex;
+		    $aux["msg"] = $msg;
+
+		    $lines[] = $aux;
+		}
+	    }
+
+	    unset ($parts);
+	}
+    }
+
+    closedir ($h);
+    $smarty->assign('lines', $lines);
+    $smarty->assign('result', 'y');
 }
 
 $smarty->assign('mid', 'tiki-import_phpwiki.tpl');
