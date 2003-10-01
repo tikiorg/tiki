6aa1f48e1c17a670a75127d2230966f1d128a974
sylvieg <sylvieg@users.sourceforge.net>
2003-10-01 15:53:19 +0000

    date & time formats are looked in the language.php file for a language different than the site language
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1608 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/modifier.tiki_short_time.php
+++ lib/smarty_tiki/modifier.tiki_short_time.php
@@ -4,7 +4,7 @@ require_once $smarty->_get_plugin_filepath('modifier','tiki_date_format');
 function smarty_modifier_tiki_short_time($string)
 {
 	global $tikilib;
-	return smarty_modifier_tiki_date_format($string, $tikilib->get_short_time_format());
+	return smarty_modifier_tiki_date_format($string, $tikilib->get_short_time_format(), null, tra("%H:%M %Z"));
 }
 
 /* vim: set expandtab: */
