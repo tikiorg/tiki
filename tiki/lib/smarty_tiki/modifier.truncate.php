c3eb41c7bc7e6c80e735f16fa6e48cf51f40a9bd
sylvieg <sylvieg@users.sourceforge.net>
2003-10-02 17:17:25 +0000

    overload the smarty plugin truncate to call mb_substr for utf8 string
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1619 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/lib/smarty_tiki/modifier.truncate.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+
+/**
+ * Smarty truncate modifier plugin
+ *
+ * Type:     modifier<br>
+ * Name:     truncate<br>
+ * Purpose:  Truncate a string to a certain length if necessary,
+ *           optionally splitting in the middle of a word, and 
+ *           appending the $etc string.
+ * @link http://smarty.php.net/manual/en/language.modifier.truncate.php
+ *          truncate (Smarty online manual)
+ * @param string
+ * @param integer
+ * @param string
+ * @param boolean
+ * @return string
+ */
+function smarty_modifier_truncate($string, $length = 80, $etc = '...',
+                                  $break_words = false)
+{
+    if ($length == 0)
+        return '';
+
+    if (strlen($string) > $length) {
+        $length -= strlen($etc);	
+       if ($break_words) // break the word
+        	$fragment = mb_substr($string, 0, $length);
+        else {
+            $fragment = mb_substr($string, 0, $length + 1);
+            $fragment = preg_replace('/\s+(\S+)?$/', '', $fragment);
+        }
+	return $fragment.$etc;
+    } else
+        return $string;
+}
+
+/* vim: set expandtab: */
+
+?>
