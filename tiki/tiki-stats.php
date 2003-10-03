925c745b39f8b06b80a93919beab57fe725fb9f7
mose <mose@users.sourceforge.net>
2003-10-03 17:52:02 +0000

    limited the display of stats only for activated features
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1635 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-stats.php
+++ tiki/tiki-stats.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-stats.php,v 1.6 2003-08-07 04:33:57 rossta Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-stats.php,v 1.7 2003-10-03 17:52:02 mose Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -42,29 +42,75 @@ if (isset($_REQUEST["chart"])) {
 	$smarty->assign($_REQUEST["chart"] . "_chart", 'y');
 }
 
-$wiki_stats = $statslib->wiki_stats();
+if ($feature_wiki == "y") {
+	$wiki_stats = $statslib->wiki_stats();
+} else {
+	$wiki_stats = false;
+}
 $smarty->assign_by_ref('wiki_stats', $wiki_stats);
-$igal_stats = $statslib->image_gal_stats();
+
+if ($feature_galleries == 'y') {
+	$igal_stats = $statslib->image_gal_stats();
+} else {
+	$igal_stats = false;
+}
 $smarty->assign_by_ref('igal_stats', $igal_stats);
-$fgal_stats = $statslib->file_gal_stats();
+
+if ($feature_file_galleries == 'y') {
+	$fgal_stats = $statslib->file_gal_stats();
+} else {
+  $fgal_stats =	false;
+}
 $smarty->assign_by_ref('fgal_stats', $fgal_stats);
-$cms_stats = $statslib->cms_stats();
+
+if ($feature_articles == 'y') {
+	$cms_stats = $statslib->cms_stats();
+} else {
+  $cms_stats = false;
+}
 $smarty->assign_by_ref('cms_stats', $cms_stats);
-$forum_stats = $statslib->forum_stats();
+
+if ($feature_forums == 'y') {
+	$forum_stats = $statslib->forum_stats();
+} else {
+	$forum_stats = false;
+}
 $smarty->assign_by_ref('forum_stats', $forum_stats);
-$blog_stats = $statslib->blog_stats();
+
+if ($feature_blogs == 'y') {
+	$blog_stats = $statslib->blog_stats();
+} else {
+  $blog_stats =	false;
+}
 $smarty->assign_by_ref('blog_stats', $blog_stats);
-$poll_stats = $statslib->poll_stats();
+
+if ($feature_polls == 'y') {
+	$poll_stats = $statslib->poll_stats();
+} else {
+	$poll_stats = false;
+}
 $smarty->assign_by_ref('poll_stats', $poll_stats);
-$faq_stats = $statslib->faq_stats();
+
+if ($feature_faqs == 'y') {
+	$faq_stats = $statslib->faq_stats();
+} else {
+	$faq_stats = false;
+}
 $smarty->assign_by_ref('faq_stats', $faq_stats);
-$user_stats = $statslib->user_stats();
-$smarty->assign_by_ref('user_stats', $user_stats);
-$site_stats = $statslib->site_stats();
-$smarty->assign_by_ref('site_stats', $site_stats);
-$quiz_stats = $statslib->quiz_stats();
+
+if ($feature_quizzes == 'y') {
+	$quiz_stats = $statslib->quiz_stats();
+} else {
+	$quiz_stats = false;
+}
 $smarty->assign_by_ref('quiz_stats', $quiz_stats);
 
+
+$user_stats = $statslib->user_stats();
+$smarty->assign_by_ref('user_stats', $user_stats);
+
+$site_stats = $statslib->site_stats();
+$smarty->assign_by_ref('site_stats', $site_stats);
 // Display the template
 $smarty->assign('mid', 'tiki-stats.tpl');
 $smarty->display("styles/$style_base/tiki.tpl");
