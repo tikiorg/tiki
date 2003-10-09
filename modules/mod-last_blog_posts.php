434e15467667f91d1a7d322234f9692816ca4989
cdx <cdx@users.sourceforge.net>
2003-10-09 07:35:01 +0000

    * New feature: you can select show the title or the date of creation of the post. Use title=title on the module's params.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1701 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-last_blog_posts.php
+++ modules/mod-last_blog_posts.php
@@ -1,7 +1,11 @@
 <?php
 
+/**
+* Params: 
+* - title : if is "title", show the title of the post, else show the date of creation
+*/
 $ranking = $tikilib->list_posts(0, $module_rows, 'created_desc', '');
 
 $smarty->assign('modLastBlogPosts', $ranking["data"]);
-
+$smarty->assign('modLastBlogPostsTitle',(isset($module_params["title"])?$module_params["title"]:""));
 ?>
