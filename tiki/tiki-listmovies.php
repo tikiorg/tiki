bb5ed88a92df213e1ce46fe9dce4896c20f1e0b6
mose <mose@users.sourceforge.net>
2003-10-12 22:41:52 +0000

    added the simple page that list movies on tw.o
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1767 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/tiki-listmovies.php
@@ -0,0 +1,28 @@
+<?php
+// Initialization
+require_once('tiki-setup.php');
+
+if ($handle = opendir('tikimovies')) {
+	while (false !== ($file = readdir($handle))) { 
+		if (substr($file,-4,4) == '.swf' and ($file != 'controller.swf')) {
+			$movies[] = $file;
+		}
+	}
+	sort($movies);
+	closedir($handle);
+}
+
+// Get the page from the request var or default it to HomePage
+if(isset($_GET["movie"])) {
+  $movie = $_GET["movie"]; 
+} else {
+  $movie = "";
+}
+
+$smarty->assign_by_ref('movie',$movie);    
+$smarty->assign_by_ref('movies',$movies);    
+
+// Display the template
+$smarty->assign('mid','tiki-listmovies.tpl');
+$smarty->display("styles/$style_base/tiki.tpl");
+?>
