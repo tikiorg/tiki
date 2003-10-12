148900b47f3cf910b15672f4e0dd124305342e97
ohertel <ohertel@users.sourceforge.net>
2003-10-12 12:55:42 +0000

    cleanup / moved most of the code to tiki-rss.php, additional rss output
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1749 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-forums_rss.php
+++ tiki-forums_rss.php
@@ -1,71 +1,35 @@
 <?php
-  require_once('tiki-setup.php');
-  require_once('lib/tikilib.php');
+// $Header: /cvsroot/tikiwiki/tiki/tiki-forums_rss.php,v 1.12 2003-10-12 12:55:42 ohertel Exp $
 
-  if($rss_forums != 'y') {
-   die;
-  }
-  
-  if($tiki_p_admin_forum != 'y' && $tiki_p_forum_read != 'y') {
-      $smarty->assign('msg',tra("You dont have permission to use this feature"));
-      $smarty->display("styles/$style_base/error.tpl");
-      die;
-  }
+// Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-  header("content-type: text/xml");
-  $foo = parse_url($_SERVER["REQUEST_URI"]);
-  $foo1=str_replace("tiki-forums_rss.php",$tikiIndex,$foo["path"]);
-  $foo2=str_replace("tiki-forums_rss.php","img/tiki.jpg",$foo["path"]);
-  $foo3=str_replace("tiki-forums_rss","tiki-view_forum_thread",$foo["path"]);
-  $foo4=str_replace("tiki-forums_rss.php","lib/rss/rss-style.css",$foo["path"]);
-  $home = httpPrefix().$foo1;
-  $img = httpPrefix().$foo2;
-  $read = httpPrefix().$foo3;
-  $css = httpPrefix().$foo4;
+require_once ('tiki-setup.php');
+require_once ('lib/tikilib.php');
 
-  $now = date("U");
-  $changes = $tikilib->list_all_forum_topics(0,$max_rss_forums,'commentDate_desc', '');
-  $title = "Tiki RSS feed for forums";
-  $desc = "Last topics in forums.";
+if ($rss_forums != 'y') {
+	$smarty -> assign('msg', tra("This feature is disabled"));
+	$smarty -> display("styles/$style_base/error.tpl");
+	die; // TODO: output of rss file with message: rss disabled
+}
 
-  print '<?xml version="1.0" encoding="UTF-8"?>'."\n";
-  print '<?xml-stylesheet href="'.$css.'" type="text/css"?>'."\n";
+if($tiki_p_admin_forum != 'y' && $tiki_p_forum_read != 'y') {
+	$smarty -> assign('msg', tra("Permission denied you can not view this section"));
+	$smarty -> display("styles/$style_base/error.tpl");
+	die; // TODO: output of rss file with message: permission denied
+}
+
+$title = "Tiki RSS feed for forums"; // TODO: make configurable
+$desc = "Last topics in forums."; // TODO: make configurable
+$now = date("U");
+$id = "forumId";
+$descId = "data";
+$dateId = "commentDate";
+$titleId = "title";
+$readrepl = "tiki-view_forum_thread.php";
+$changes = $tikilib -> list_all_forum_topics(0, $max_rss_forums, $dateId.'_desc', '');
+
+require ("tiki-rss.php");
+
 ?>
-<rdf:RDF xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:h="http://www.w3.org/1999/xhtml" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns="http://purl.org/rss/1.0/">
-<channel rdf:about="<?php echo $home; ?>">
-  <title><?php echo $title; ?></title>
-  <link><?php echo $home?></link>
-  <description>
-    <?php echo $desc; ?>    
-  </description>
-  <image rdf:about="<?php echo $img; ?>">
-    <title><?php echo $title; ?></title>
-    <link><?php echo $home?></link>
-  </image>
-  
-    <items>
-    <rdf:Seq>
-      <?php
-        // LOOP collecting last topics in forums (index)
-        foreach($changes["data"] as $chg) {
-          print('        <rdf:li resource="'.$read.'?forumId='.$chg["forumId"].'&amp;comments_parentId='.$chg["threadId"].'" />'."\n");
-        }        
-      ?>
-    </rdf:Seq>  
-  </items>
-</channel>
-  
-<?php
-  // LOOP collecting last topics in forums
-  foreach($changes["data"] as $chg) {
-    print('<item rdf:about="'.$read.'?forumId='.$chg["forumId"].'&amp;comments_parentId='.$chg["threadId"].'">'."\n");
-    print('<title>'.htmlspecialchars($chg["title"]).': '.
-    $tikilib->date_format($tikilib->get_short_datetime_format(),$chg["commentDate"]).'</title>'."\n");
-    print('<link>'.$read.'?forumId='.$chg["forumId"].'&amp;comments_parentId='.$chg["threadId"].'</link>'."\n");
-    $data = $tikilib->date_format($tikilib->get_short_datetime_format(),$chg["commentDate"]);
-    print('<description>'.htmlspecialchars($chg["data"]).'</description>'."\n");
-    print('</item>'."\n\n");
-  }        
-?>
-
-</rdf:RDF>       
