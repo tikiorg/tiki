f1435d2819c0030a17fe4a1714d8f99c5d674831
mose <mose@users.sourceforge.net>
2003-10-14 14:37:59 +0000

    db abstract
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1815 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/hotwords/hotwordlib.php
+++ lib/hotwords/hotwordlib.php
@@ -11,20 +11,20 @@ class HotwordsLib extends TikiLib {
 	}
 
 	function list_hotwords($offset = 0, $maxRecords = -1, $sort_mode = 'word_desc', $find = '') {
-		$sort_mode = str_replace("_", " ", $sort_mode);
 
 		if ($find) {
 			$findesc = $this->qstr('%' . $find . '%');
-
-			$mid = " where (word like $findesc) ";
+			$mid = " where `word` like ?";
+			$bindvars = array($findesc);
 		} else {
 			$mid = '';
+			$bindvars = array();
 		}
 
-		$query = "select * from tiki_hotwords $mid order by $sort_mode limit $offset,$maxRecords";
-		$query_cant = "select count(*) from tiki_hotwords $mid";
-		$result = $this->query($query);
-		$cant = $this->getOne($query_cant);
+		$query = "select * from `tiki_hotwords` $mid order by ".$this->convert_sortmode($sort_mode);
+		$query_cant = "select count(*) from `tiki_hotwords` $mid";
+		$result = $this->query($query,$bindvars,$maxRecords,$offset);
+		$cant = $this->getOne($query_cant,$bindvars);
 		$ret = array();
 
 		while ($res = $result->fetchRow(DB_FETCHMODE_ASSOC)) {
@@ -41,15 +41,14 @@ class HotwordsLib extends TikiLib {
 		$word = addslashes($word);
 
 		$url = addslashes($url);
-		$query = "replace into tiki_hotwords(word,url) values('$word','$url')";
-		$result = $this->query($query);
+		$query = "replace into `tiki_hotwords`(`word`,`url`) values(?,?)";
+		$result = $this->query($query,array($word,$url));
 		return true;
 	}
 
 	function remove_hotword($word) {
-		$query = "delete from tiki_hotwords where word='$word'";
-
-		$result = $this->query($query);
+		$query = "delete from `tiki_hotwords` where `word`=?";
+		$result = $this->query($query,array($word));
 	}
 }
 
