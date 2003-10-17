3551543b0b5b291d3ae2771e65dccca4fb3ce190
mose <mose@users.sourceforge.net>
2003-10-17 13:44:55 +0000

    added a simple cache system for articles topics images (in temp/)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1927 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/topic_image.php
+++ tiki/topic_image.php
@@ -1,12 +1,12 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.3 2003-08-07 04:33:57 rossta Exp $
+// $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.4 2003-10-17 13:44:55 mose Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-# $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.3 2003-08-07 04:33:57 rossta Exp $
+# $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.4 2003-10-17 13:44:55 mose Exp $
 
 // application to display an image from the database with 
 // option to resize the image dynamically creating a thumbnail on the fly.
@@ -14,13 +14,24 @@ if (!isset($_REQUEST["id"])) {
 	die;
 }
 
-include_once ('db/tiki-db.php');
-include_once ('lib/tikilib.php');
-$tikilib = new Tikilib($dbTiki);
-$data = $tikilib->get_topic_image($_REQUEST["id"]);
-$type = $data["image_type"];
-$data = $data["image_data"];
-header ("Content-type: $type");
-echo $data;
+if (is_file("temp/topic.".$_REQUEST["id"]) and (!isset($_REQUEST["reload"]))) {
+	list($width, $height, $type, $attr) = getimagesize("temp/topic.".$_REQUEST["id"]);
+	header ("Content-type: $type");
+	readfile("temp/topic.".$_REQUEST["id"]);
+} else {
+	include_once ('db/tiki-db.php');
+	include_once ('lib/tikilib.php');
+	$tikilib = new Tikilib($dbTiki);
+	$data = $tikilib->get_topic_image($_REQUEST["id"]);
+	$type = $data["image_type"];
+	$data = $data["image_data"];
+	if ($data["image_data"]) {
+		$fp = fopen("temp/topic.".$_REQUEST["id"],"wb");
+		fputs($fp,$data);
+		fclose($fp);
+	}
+}
 
+header ("Content-type: $type");
+readfile("temp/topic.".$_REQUEST["id"]);
 ?>
