b9e56eac735b5b075a903df1cfa3bae259f4be0b
mose <mose@users.sourceforge.net>
2003-10-17 12:03:18 +0000

    fixed a lacking html tag
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1921 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-chat_users.php
+++ tiki-chat_users.php
@@ -1,12 +1,12 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-chat_users.php,v 1.5 2003-08-07 04:33:57 rossta Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-chat_users.php,v 1.6 2003-10-17 12:03:18 mose Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-# $Header: /cvsroot/tikiwiki/tiki/tiki-chat_users.php,v 1.5 2003-08-07 04:33:57 rossta Exp $
+# $Header: /cvsroot/tikiwiki/tiki/tiki-chat_users.php,v 1.6 2003-10-17 12:03:18 mose Exp $
 require_once ("tiki-setup.php");
 
 include_once ('lib/chat/chatlib.php');
@@ -44,7 +44,7 @@ include_once ('lib/chat/chatlib.php');
 
 						</td>
 
-						/tr>
+						</tr>
 
 			<?
 
