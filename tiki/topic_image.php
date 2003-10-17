cdf67d2f8fe8be3eb1cd98792d3b9db6e51e29b5
mose <mose@users.sourceforge.net>
2003-10-17 14:13:02 +0000

    fixed image type in cache reading
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1929 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/topic_image.php
+++ tiki/topic_image.php
@@ -1,12 +1,12 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.4 2003-10-17 13:44:55 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.5 2003-10-17 14:13:02 mose Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-# $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.4 2003-10-17 13:44:55 mose Exp $
+# $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.5 2003-10-17 14:13:02 mose Exp $
 
 // application to display an image from the database with 
 // option to resize the image dynamically creating a thumbnail on the fly.
@@ -15,8 +15,8 @@ if (!isset($_REQUEST["id"])) {
 }
 
 if (is_file("temp/topic.".$_REQUEST["id"]) and (!isset($_REQUEST["reload"]))) {
-	list($width, $height, $type, $attr) = getimagesize("temp/topic.".$_REQUEST["id"]);
-	header ("Content-type: $type");
+	$size = getimagesize("temp/topic.".$_REQUEST["id"]);
+	header ("Content-type: ".$size['mime']);
 	readfile("temp/topic.".$_REQUEST["id"]);
 } else {
 	include_once ('db/tiki-db.php');
