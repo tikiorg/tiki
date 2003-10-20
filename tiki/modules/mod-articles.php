81896435a92e8e309331d6d1c40bef4b1d6bd94f
dheltzel <dheltzel@users.sourceforge.net>
2003-10-20 22:47:25 +0000

    new module for displaying parts of articles in wiki pages.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1993 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/modules/mod-articles.php
@@ -0,0 +1,27 @@
+<?php
+
+$smarty->assign('title', isset($module_params["title"]) ? $module_params["title"] : 'Articles');
+
+if (isset($module_params["type"])) {
+	$type = $module_params["type"];
+} else {
+	$type = '';
+}
+if (isset($module_params["topicId"])) {
+	$topicId = $module_params["topicId"];
+} else {
+	$topicId = '';
+}
+/*
+$smarty->assign('type', isset($module_params["type"]) ? $module_params["type"] : '');
+$smarty->assign('topicId', isset($module_params["topicId"]) ? $module_params["topicId"] : '');
+
+function list_articles($offset = 0, $maxRecords = -1, $sort_mode = 'publishDate_desc', $find = '', $date = '', $user, $type = '', $topicId = '') {
+*/
+
+$ranking = $tikilib->list_articles(0, $module_rows, 'publishDate_desc', '', '', $user, $type, $topicId);
+//$ranking = $tikilib->list_articles(0, $module_rows, 'reads_desc', '', '', $user);
+
+$smarty->assign('modArticles', $ranking["data"]);
+
+?>
