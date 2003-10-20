e77392db43af7da68e9d333d92b19b4a58967f7f
dheltzel <dheltzel@users.sourceforge.net>
2003-10-20 22:41:33 +0000

    fixed mk_profile.sh
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@1992 b456876b-0849-0410-b77d-98878d47e9d5

--- db/profiles/mk_profile.sh
+++ db/profiles/mk_profile.sh
@@ -15,7 +15,7 @@ fi
 DATABASE=$1
 
 DUMP_FILE=$DATABASE-dump.sql
-PROFILE_FILE=$DATABASE-profile.sql
+PROFILE_FILE=$DATABASE.prf
 
 mysql -s -p $DATABASE <dump_prefs.sql |sort >$DUMP_FILE
-diff default-inserts.sql $DUMP_FILE|grep ">"|sed -e "s/> INSERT IGNORE/REPLACE/" >$PROFILE_FILE
+diff default-inserts.sql $DUMP_FILE|grep ">"|sed -e "s/> INSERT \/\* IGNORE \*\//REPLACE/" >$PROFILE_FILE
