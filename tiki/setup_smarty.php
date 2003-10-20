3cfcde516942a8f56317325064ad09b07f808283
mose <mose@users.sourceforge.net>
2003-10-20 21:15:01 +0000

    finished to port the auth patch for multi-tiki
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@1989 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/setup_smarty.php
+++ tiki/setup_smarty.php
@@ -1,11 +1,15 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/setup_smarty.php,v 1.5 2003-10-02 17:15:35 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/setup_smarty.php,v 1.6 2003-10-20 21:15:01 mose Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
+if (isset($_SERVER["REQUEST_URI"])) {
+  ini_set('session.cookie_path',dirname($_SERVER["REQUEST_URI"]));
+}
+	
 require_once ("db/tiki-db.php");
 
 error_reporting (E_ALL);
