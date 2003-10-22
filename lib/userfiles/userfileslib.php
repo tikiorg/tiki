ecff97d5ba9a29c040802f2d006cce7e67cfa7e3
redflo <redflo@users.sourceforge.net>
2003-10-22 00:31:04 +0000

    db abstraction
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2005 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/userfiles/userfileslib.php
+++ lib/userfiles/userfileslib.php
@@ -15,42 +15,38 @@ class UserFilesLib extends TikiLib {
 			return 0;
 		}
 
-		$part1 = $this->getOne("select sum(filesize) from tiki_userfiles where user='$user'");
-		$part2 = $this->getOne("select sum(size) from tiki_user_notes where user='$user'");
+		$part1 = $this->getOne("select sum(`filesize`) from `tiki_userfiles` where `user`=?",array($user));
+		$part2 = $this->getOne("select sum(`size`) from `tiki_user_notes` where `user`=?",array($user));
 		return $part1 + $part2;
 	}
 
 	function upload_userfile($user, $name, $filename, $filetype, $filesize, $data, $path) {
-		$name = addslashes($name);
 
-		$filename = addslashes($filename);
-		$data = addslashes($data);
 		$now = date("U");
-		$query = "insert into tiki_userfiles(user,name,filename,filetype,filesize,data,created,hits,path)
-    values('$user','$name','$filename','$filetype','$filesize','$data',$now,0,'$path')";
-		$this->query($query);
+		$query = "insert into `tiki_userfiles`(`user`,`name`,`filename`,`filetype`,`filesize`,`data`,`created`,`hits`,`path`)
+    values(?,?,?,?,?,?,?,?,?)";
+		$this->query($query,array($user,$name,$filename,$filetype,(int) $filesize,$data,(int) $now,0,$path));
 	}
 
 	function list_userfiles($user, $offset, $maxRecords, $sort_mode, $find) {
-		$sort_mode = str_replace("_desc", " desc", $sort_mode);
-
-		$sort_mode = str_replace("_asc", " asc", $sort_mode);
 
 		if ($find) {
-			$findesc = $this->qstr('%' . $find . '%');
+			$findesc = '%' . $find . '%';
 
-			$mid = " and (filename like $findesc)";
+			$mid = " and (`filename` like ?)";
+			$bindvars=array($user,$findesc);
 		} else {
 			$mid = " ";
+			$bindvars=array($user);
 		}
 
-		$query = "select fileId,user,name,filename,filetype,filesize,created,hits from tiki_userfiles where user='$user' $mid order by $sort_mode limit $offset,$maxRecords";
-		$query_cant = "select count(*) from tiki_userfiles where user='$user' $mid";
-		$result = $this->query($query);
-		$cant = $this->getOne($query_cant);
+		$query = "select `fileId`,`user`,`name`,`filename`,`filetype`,`filesize`,`created`,`hits` from `tiki_userfiles` where `user`=? $mid order by ".$this->convert_sortmode($sort_mode);
+		$query_cant = "select count(*) from `tiki_userfiles` where `user`=? $mid";
+		$result = $this->query($query,$bindvars,$maxRecords,$offset);
+		$cant = $this->getOne($query_cant,$bindvars);
 		$ret = array();
 
-		while ($res = $result->fetchRow(DB_FETCHMODE_ASSOC)) {
+		while ($res = $result->fetchRow()) {
 			$ret[] = $res;
 		}
 
@@ -61,24 +57,24 @@ class UserFilesLib extends TikiLib {
 	}
 
 	function get_userfile($user, $fileId) {
-		$query = "select * from tiki_userfiles where user='$user' and fileId='$fileId'";
+		$query = "select * from `tiki_userfiles` where `user`=? and `fileId`=?";
 
-		$result = $this->query($query);
-		$res = $result->fetchRow(DB_FETCHMODE_ASSOC);
+		$result = $this->query($query,array($user,(int) $fileId));
+		$res = $result->fetchRow();
 		return $res;
 	}
 
 	function remove_userfile($user, $fileId) {
 		global $uf_use_dir;
 
-		$path = $this->getOne("select path from tiki_userfiles where user='$user' and fileId=$fileId");
+		$path = $this->getOne("select `path` from `tiki_userfiles` where `user`=? and `fileId`=?",array($user,(int) $fileId));
 
 		if ($path) {
 			@unlink ($uf_use_dir . $path);
 		}
 
-		$query = "delete from tiki_userfiles where user='$user' and fileId=$fileId";
-		$this->query($query);
+		$query = "delete from `tiki_userfiles` where `user`=? and fileId=?";
+		$this->query($query,array($user,(int) $fileId));
 	}
 }
 
