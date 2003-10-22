8a913b3c7d69dea6d00e1f96965fd981e769f49a
sylvieg <sylvieg@users.sourceforge.net>
2003-10-22 18:38:21 +0000

    possibility to give the max number of characters to be displayed as mod-last_modif_page
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2019 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-breadcrumb.php
+++ modules/mod-breadcrumb.php
@@ -1,10 +1,12 @@
 <?php
-
+/** $Header: /cvsroot/tikiwiki/tiki/modules/mod-breadcrumb.php,v 1.3 2003-10-22 18:38:21 sylvieg Exp $
+ * \param maxlen = max number of displayed characters for the page name
+ */
 if (!isset($_SESSION["breadCrumb"])) {
 	$_SESSION["breadCrumb"] = array();
 }
 
 $bbreadCrumb = array_reverse($_SESSION["breadCrumb"]);
 $smarty->assign('breadCrumb', $bbreadCrumb);
-
+$smarty->assign('maxlen', isset($module_params["maxlen"]) ? $module_params["maxlen"] : 0);
 ?>
