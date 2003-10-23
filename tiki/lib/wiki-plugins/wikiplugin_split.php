23100f5fa015a1c7e7edbf2d870a0533728aa8a5
zaufi <zaufi@users.sourceforge.net>
2003-10-23 22:58:12 +0000

    {SPLIT} refactored. Fixed broken wiki synataxes.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@2037 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/wiki-plugins/wikiplugin_split.php
+++ tiki/lib/wiki-plugins/wikiplugin_split.php
@@ -1,33 +1,59 @@
 <?php
+/**
+ * \file
+ * $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_split.php,v 1.9 2003-10-23 22:58:12 zaufi Exp $
+ * 
+ * \brief {SPLIT} wiki plugin implementation
+ *
+ */
 
 function wikiplugin_split_help() {
 	return tra("Split a page into columns").":<br />~np~{SPLIT()}".tra("column")."---".tra("column")."---".tra("column")."{SPLIT}~/np~";
 }
+
+/*
+ * \note This plugin should carefuly change text it have
+ *       bcose some of wiki synataxes are sensitive for
+ *       start of line (like lists and headers)... such
+ *       user lines must stay with the same layout after
+ *       this plugin...
+ *
+ * Attention: (by zaufi)
+ *  Smbd can explain what for this __nasty__ hack needed?
+ *  The only result I see is that many wiki syntaxes become
+ *  broken... like lists, -=titles=- and some others...
+ *
+ * $sections[$i] = preg_replace("/([\r\n])/","<br />",$sections[$i]);
+ *
+ * so in refactored plugin this code not present...
+ */
 function wikiplugin_split($data, $params) {
 	global $tikilib;
-
 	global $replacement;
 
-	if (substr($data, 0, 1) == "\n") {
-		$data = substr($data, 1);
-	}
+    // Remove first <ENTER> if exists...
+    // it may be here if present after {SPLIT()} in original text
+	if (substr($data, 0, 1) == "\n") $data = substr($data, 1);
 
 	extract ($params);
+	$sections = preg_split("/---+/", $data);
+
+    // Is there split sections present?
+    // Do not touch anything if no... even don't generate <table>
+    if (!is_array($sections) || count($sections) <= 1)
+        return $data;
+
+	$columnSize = floor(100 / count($sections));
 	$result = "<table border='0' width='100%'><tr>";
-	$sections = preg_split("/---+/\n", $data);
-	$count = count($sections);
-	$columnSize = floor(100 / $count);
 
-	for ($i = 0; $i < $count; $i++) {
-		$result .= "<td valign='top' width='" . $columnSize . "%'>\n";
+    // Attention: Dont forget to remove leading empty line in section ...
+    //            it should remain from previous '---' line...
+    // Attention: origianl text must be placed between \n's!!!
+	foreach ($sections as $i)
+		$result .= "<td valign='top' width='" . $columnSize . "%'>
+                   \n".substr($i, 1)."\n</td>";
 
-		if (substr($sections[$i], 0, 1) == "\n")
-			$sections[$i] = substr($sections[$i], 1);
-		$sections[$i] = preg_replace("/([\r\n])/","<br />",$sections[$i]);
-		$result .= $sections[$i];
-		$result .= "\n</td>";
-	}
-
+    // Close HTML table (no \n at end!)
 	$result .= "</tr></table>";
 
 	return $result;
