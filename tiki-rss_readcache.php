e4d2025f535a78772b111c08d18696ae83dfe08e
ohertel <ohertel@users.sourceforge.net>
2003-10-24 23:20:44 +0000

    read default rss version from database now
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2054 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-rss_readcache.php
+++ tiki-rss_readcache.php
@@ -1,10 +1,13 @@
 <?php
 
-$rss_version = 1; // default is: rss v1.0 - TODO: make this configurable
+// get default rss feed version from database or set to 1.0 if none in there
+$rss_version = $tikilib->get_preference("rssfeed_default_version",1);
+
+// override version if set as request parameter
 if (isset($_REQUEST["ver"]))
 	if (substr($_REQUEST["ver"],0,1) == '2') {
 		$rss_version = 2;
-	}
+	} else $rss_version = 1;
 
 $query = "select * from `tiki_rss_feeds` where `name`=? and `rssVer`=?";
 $bindvars=array($feed, $rss_version);
@@ -18,6 +21,7 @@ if (!$result->numRows())
   // nothing found, then insert row for this feed+rss_ver
   $now = date("U");
   $query = "insert into `tiki_rss_feeds`(`name`,`rssVer`,`refresh`,`lastUpdated`,`cache`) values(?,?,?,?,?)";
+  
   // default value for cache timeout is 300 (5 minutes)
   $bindvars=array($feed,(int) $rss_version,(int) 300 ,(int) $now, $output);
   $result = $tikilib->query($query, $bindvars);
