dec683e7f2286e1f10045bb7e011f52df0701ae6
mose <mose@users.sourceforge.net>
2003-10-25 12:52:12 +0000

    db abstract
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2075 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/drawings/drawlib.php
+++ lib/drawings/drawlib.php
@@ -5,120 +5,79 @@ class DrawLib extends TikiLib {
 		if (!$db) {
 			die ("Invalid db object passed to DrawLib constructor");
 		}
-
 		$this->db = $db;
 	}
 
-	/*
-		drawId integer(12) not null auto_increment,
-		name varchar(250),
-		filename varchar(250),
-		timestamp integer(14),
-		user varchar(200),
-		primary key(drawId)
-	*/
 	function replace_drawing($drawId, $name, $filename_draw, $filename_pad, $user, $version) {
-		$name = addslashes($name);
-
 		$now = date("U");
-
 		if ($drawId) {
-			$query = "update tiki_drawings set
-  		name = '$name',
-  		$version = $version,
-  		filename_draw = '$filename_draw',
-  		filename_pad = '$filename_pad',
-  		timestamp = '$now',
-  		version = $version,
-  		user='$user'
-  		where drawId = $drawId";
-
-			$this->qeury($query);
+			$query = "update `tiki_drawings` set `name`=?, `filename_draw`=?, `filename_pad`=?, `timestamp`=?, `version`=?, `user`='?  where `drawId`=?";
+			$this->query($query,array($name,$filename_draw,$filename_pad,(int)$now,(int)$version,$user,(int)$drawId));
 		} else {
-			$query = "insert into tiki_drawings(name,filename_draw, filename_pad,timestamp,user, version)
-    	values('$name','$filename_draw','$filename_pad',$now,'$user',$version)";
-
-			$this->query($query);
+			$query = "insert into `tiki_drawings`(`name`,`filename_draw`,`filename_pad`,`timestamp`,`version`,`user`) values(?,?,?,?,?,?)";
+			$this->query($query,array($name,$filename_draw,$filename_pad,(int)$now,(int)$version,$user));
 		}
 
 		return true;
 	}
 
 	function update_drawing($name, $hash, $user) {
-		// Updates a drawing, the last version (if existed) is changed to 
-		// $hash while a new version is inserted with $name
-		$name = addslashes($name);
-
-		$version = $this->getOne("select max(version) from tiki_drawings where name='$name'");
-
-		if (!$version)
-			$version = 0;
-
+		$version = $this->getOne("select max(`version`) from `tiki_drawings` where `name`=?",array($name));
+		if (!$version) $version = 0;
 		$version = $version + 1;
 		$this->replace_drawing(0, $name, '', $hash, $user, $version);
 		$maxversions = $this->get_preference("maxVersions", 0);
 		$keep = $this->get_preference('keep_versions', 0);
-		$cant = $this->getOne("select count(*) from tiki_drawings where name='$name'");
+		$cant = $this->getOne("select count(*) from `tiki_drawings` where `name`=?",array($name));
 		$now = date("U");
 		$oktodel = $now - ($keep * 24 * 3600);
 
 		if ($cant > $maxversions) {
-			$query = "select * from tiki_drawings where name='$name' and timestamp <= $oktodel limit $maxversions,-1";
-
-			$result = $this->query($query);
-
-			while ($res = $result->fetchRow(DB_FETCHMODE_ASSOC)) {
-				$query = "delete from tiki_drawings where drawId=" . $res['drawId'];
-
-				$this->query($query);
+			$query = "select * from `tiki_drawings` where `name`=? and `timestamp` <= ? ";
+			$result = $this->query($query,array($name,(int)$oktodel),-1,$maxversions);
+			while ($res = $result->fetchRow()) {
+				$query = "delete from `tiki_drawings` where `drawId`=?";
+				$this->query($query,array($res['drawId']));
 			}
 		}
 	}
 
 	function set_drawing_gif($name, $hash) {
-		$name = addslashes($name);
-
-		$id = $this->getOne("select max(drawId) from tiki_drawings where name='$name'");
-
+		$id = $this->getOne("select max(`drawId`) from `tiki_drawings` where `name`=?",array($name));
 		if ($id) {
-			$query = "update tiki_drawings set filename_draw='$hash' where drawId=$id";
-
-			$this->query($query);
+			$query = "update `tiki_drawings` set `filename_draw`=? where `drawId`=?";
+			$this->query($query,array($hash,(int)$id));
 		}
 	}
 
 	function get_drawing($drawId) {
-		$query = "select * from tiki_drawings where drawId=$drawId";
-
-		$result = $this->query($query);
-		$res = $result->fetchRow(DB_FETCHMODE_ASSOC);
+		$query = "select * from `tiki_drawings` where `drawId`=?";
+		$result = $this->query($query,array((int)$drawId));
+		$res = $result->fetchRow();
 		return $res;
 	}
 
 	function remove_drawing($drawId) {
 		global $tikidomain;
-
 		$info = $this->get_drawing($drawId);
 		$f1 = "img/wiki/$tikidomain" . $info['filename_draw'];
 		$f2 = "img/wiki/$tikidomain" . $info['filename_pad'];
-		$max = $this->getOne("select count(*) from tiki_drawings where name='" . $info['name'] . "'");
+		$max = $this->getOne("select count(*) from `tiki_drawings` where `name`=?",array($info['name']));
 		@unlink ($f1);
 		@unlink ($f2);
-
 		if ($max == 1) {
 			$f1 = "img/wiki/$tikidomain" . $name . ".pad_xml";
-
 			unlink ($f1);
 			$f1 = "img/wiki/$tikidomain" . $name . ".gif";
 			unlink ($f1);
 		}
 
-		$query = "delete from tiki_drawings where drawId=$drawId";
-		$this->query($query);
-		$max = $this->getOne("select max(version) from tiki_drawings where name='" . $info['name'] . "'");
-		$query = "select * from tiki_drawings where name = '" . $info['name'] . "' and version=$max";
-		$result = $this->query($query);
-		$res = $result->fetchRow(DB_FETCHMODE_ASSOC);
+		$query = "delete from `tiki_drawings` where `drawId`=?";
+		$this->query($query,array((int)$drawId));
+		$max = $this->getOne("select max(`version`) from `tiki_drawings` where `name`=?",array($info['name']));
+		$query = "select * from `tiki_drawings` where `name`=? and `version`=?";
+		$result = $this->query($query,array($info["name"],(int)$max));
+		$res = $result->fetchRow();
 		$f1 = "img/wiki/$tikidomain" . $res['filename_draw'];
 		$f2 = "img/wiki/$tikidomain" . $res['name'] . '.gif';
 		copy($f1, $f2);
@@ -128,39 +87,32 @@ class DrawLib extends TikiLib {
 	}
 
 	function remove_all_drawings($name) {
-		$name = addslashes($name);
-
-		$query = "delete from tiki_drawings where name='$name'";
-		$this->query($query);
+		$query = "delete from `tiki_drawings` where `name`=?";
+		$this->query($query,array($name));
 	}
 
 	function list_drawings($offset, $maxRecords, $sort_mode, $find) {
-		$sort_mode = str_replace("_", " ", $sort_mode);
-
+		$bindvars = array();
 		if ($find) {
-			$findesc = $this->qstr('%' . $find . '%');
-
-			$mid = " where  (name like $findesc)";
+			$findesc = '%'.$find.'%';
+			$mid = " where  (`name` like ?)";
+			$bindvars[] = $findesc;
 		} else {
-			$mid = " ";
+			$mid = "";
 		}
-
-		$query = "select * from tiki_drawings $mid order by $sort_mode limit $offset,$maxRecords";
-		$query_cant = "select count(*) from tiki_drawings $mid";
-		$result = $this->query($query);
-		$cant = $this->getOne($query_cant);
+		$query = "select * from `tiki_drawings` $mid order by ".$this->convert_sortmode($sort_mode);
+		$query_cant = "select count(*) from `tiki_drawings` $mid";
+		$result = $this->query($query,$bindvars,$maxRecords,$offset);
+		$cant = $this->getOne($query_cant,$bindvars);
 		$ret = array();
 
-		while ($res = $result->fetchRow(DB_FETCHMODE_ASSOC)) {
-			$last_version = $this->getOne("select max(version) from tiki_drawings where name='" . $res['name'] . "'");
-
+		while ($res = $result->fetchRow()) {
+			$last_version = $this->getOne("select max(`version`) from `tiki_drawings` where `name`=?",array($res['name']));
 			if ($res['version'] == $last_version) {
-				$res['versions'] = $this->getOne("select count(*) from tiki_drawings where name='" . $res['name'] . "'");
-
+				$res['versions'] = $this->getOne("select count(*) from `tiki_drawings` where `name`=?",array($res['name']));
 				$ret[] = $res;
 			}
 		}
-
 		$retval = array();
 		$retval["data"] = $ret;
 		$retval["cant"] = $cant;
@@ -168,30 +120,21 @@ class DrawLib extends TikiLib {
 	}
 
 	function list_drawing_history($name, $offset, $maxRecords, $sort_mode, $find) {
-		$name = addslashes($name);
-
-		$sort_mode = str_replace("_", " ", $sort_mode);
-
+		$bindvars = array($name);
+		$mid = " where `name`=? ";
 		if ($find) {
-			$findesc = $this->qstr('%' . $find . '%');
-
-			$mid = " where name='$name' and (name like $findesc)";
-		} else {
-			$mid = " where name='$name' ";
+			$mid.= " and (`name` like ?)";
+			$bindvars[] = '%'.$find.'%';
 		}
-
-		$query = "select * from tiki_drawings $mid order by $sort_mode,version desc limit $offset,$maxRecords";
-		$query_cant = "select count(*) from tiki_drawings $mid";
-		$result = $this->query($query);
-		$cant = $this->getOne($query_cant);
+		$query = "select * from `tiki_drawings` $mid order by ".$this->convert_sortmode($sort_mode).",".$this->convert_sortmode("version_desc");
+		$query_cant = "select count(*) from `tiki_drawings` $mid";
+		$result = $this->query($query,$bindvars,$maxRecords,$offset);
+		$cant = $this->getOne($query_cant,$bindvars);
 		$ret = array();
-
-		while ($res = $result->fetchRow(DB_FETCHMODE_ASSOC)) {
-			$res['versions'] = $this->getOne("select count(*) from tiki_drawings where name='" . $res['name'] . "'");
-
+		while ($res = $result->fetchRow()) {
+			$res['versions'] = $this->getOne("select count(*) from `tiki_drawings` where `name`=?",array($res['name']));
 			$ret[] = $res;
 		}
-
 		$retval = array();
 		$retval["data"] = $ret;
 		$retval["cant"] = $cant;
