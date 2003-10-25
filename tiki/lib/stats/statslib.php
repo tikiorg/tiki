eb03592e48cb2a6535d02d9001f5527238158ec3
mose <mose@users.sourceforge.net>
2003-10-25 14:21:31 +0000

    db abstract (database update required)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@2083 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/stats/statslib.php
+++ tiki/lib/stats/statslib.php
@@ -6,44 +6,41 @@ class StatsLib extends TikiLib {
 		if (!$db) {
 			die ("Invalid db object passed to StatsLib constructor");
 		}
-
 		$this->db = $db;
 	}
 
 	function list_orphan_pages($offset = 0, $maxRecords = -1, $sort_mode = 'pageName_desc', $find = '') {
-		$sort_mode = str_replace("_", " ", $sort_mode);
 
-		if ($sort_mode == 'size desc') {
-			$sort_mode = ' length(data) desc';
+		if ($sort_mode == 'size_desc') {
+			$sort_mode = 'page_size_desc';
 		}
 
-		if ($sort_mode == 'size asc') {
-			$sort_mode = ' length(data) asc';
+		if ($sort_mode == 'size_asc') {
+			$sort_mode = 'page_size_asc';
 		}
 
 		$old_sort_mode = '';
 
 		if (in_array($sort_mode, array(
-			'versions desc',
-			'versions asc',
-			'links asc',
-			'links desc',
-			'backlinks asc',
-			'backlinks desc'
+			'versions_desc',
+			'versions_asc',
+			'links_asc',
+			'links_desc',
+			'backlinks_asc',
+			'backlinks_desc'
 		))) {
 			$old_offset = $offset;
 
 			$old_maxRecords = $maxRecords;
 			$old_sort_mode = $sort_mode;
-			$sort_mode = 'user desc';
+			$sort_mode = 'user_desc';
 			$offset = 0;
 			$maxRecords = -1;
 		}
-
+		$bindvars = array();
 		if ($find) {
-			$findesc = $this->qstr('%' . $find . '%');
-
-			$mid = " where pageName like $findesc ";
+			$mid = " where `pageName` like $findesc ";
+			$bindvars[] = "%$find%";
 		} else {
 			$mid = "";
 		}
@@ -51,22 +48,20 @@ class StatsLib extends TikiLib {
 		// If sort mode is versions then offset is 0, maxRecords is -1 (again) and sort_mode is nil
 		// If sort mode is links then offset is 0, maxRecords is -1 (again) and sort_mode is nil
 		// If sort mode is backlinks then offset is 0, maxRecords is -1 (again) and sort_mode is nil
-		$query = "select pageName, hits, length(data) as len ,lastModif, user, ip, comment, version, flag from tiki_pages $mid order by $sort_mode limit 0,-1";
-		$query_cant = "select count(*) from tiki_pages $mid";
-		$result = $this->query($query);
-		$cant = $this->getOne($query_cant);
+		$query = "select `pageName`, `hits`, `page_size` as len ,`lastModif`, `user`, `ip`, `comment`, `version`, `flag` from `tiki_pages` $mid order by ".$this->convert_sortmode($sort_mode);
+		$query_cant = "select count(*) from `tiki_pages` $mid";
+		$result = $this->query($query,$bindvars,-1,0);
+		$cant = $this->getOne($query_cant,$bindvars);
 		$ret = array();
 		$num_or = 0;
 
-		while ($res = $result->fetchRow(DB_FETCHMODE_ASSOC)) {
+		while ($res = $result->fetchRow()) {
 			$pageName = $res["pageName"];
-
-			$queryc = "select count(*) from tiki_links where toPage='" . addslashes($pageName). "'";
-			$cant = $this->getOne($queryc);
+			$queryc = "select count(*) from `tiki_links` where `toPage`=?";
+			$cant = $this->getOne($queryc,array($pageName));
 
 			if ($cant == 0) {
 				$num_or++;
-
 				$aux = array();
 				$aux["pageName"] = $pageName;
 				$page = $aux["pageName"];
@@ -79,45 +74,45 @@ class StatsLib extends TikiLib {
 				$aux["comment"] = $res["comment"];
 				$aux["version"] = $res["version"];
 				$aux["flag"] = $res["flag"] == 'y' ? tra('locked') : tra('unlocked');
-				$aux["versions"] = $this->getOne("select count(*) from tiki_history where pageName='$page_as'");
-				$aux["links"] = $this->getOne("select count(*) from tiki_links where fromPage='$page_as'");
-				$aux["backlinks"] = $this->getOne("select count(*) from tiki_links where toPage='$page_as'");
+				$aux["versions"] = $this->getOne("select count(*) from `tiki_history` where `pageName`=?",array($page_as));
+				$aux["links"] = $this->getOne("select count(*) from `tiki_links` where `fromPage`=?",array($page_as));
+				$aux["backlinks"] = $this->getOne("select count(*) from `tiki_links` where `toPage`=?",array($page_as));
 				$ret[] = $aux;
 			}
 		}
 
 		// If sortmode is versions, links or backlinks sort using the ad-hoc function and reduce using old_offse and old_maxRecords
-		if ($old_sort_mode == 'versions asc') {
+		if ($old_sort_mode == 'versions_asc') {
 			usort($ret, 'compare_versions');
 		}
 
-		if ($old_sort_mode == 'versions desc') {
+		if ($old_sort_mode == 'versions_desc') {
 			usort($ret, 'r_compare_versions');
 		}
 
-		if ($old_sort_mode == 'links desc') {
+		if ($old_sort_mode == 'links_desc') {
 			usort($ret, 'compare_links');
 		}
 
-		if ($old_sort_mode == 'links asc') {
+		if ($old_sort_mode == 'links_asc') {
 			usort($ret, 'r_compare_links');
 		}
 
-		if ($old_sort_mode == 'backlinks desc') {
+		if ($old_sort_mode == 'backlinks_desc') {
 			usort($ret, 'compare_backlinks');
 		}
 
-		if ($old_sort_mode == 'backlinks asc') {
+		if ($old_sort_mode == 'backlinks_asc') {
 			usort($ret, 'r_compare_backlinks');
 		}
 
 		if (in_array($old_sort_mode, array(
-			'versions desc',
-			'versions asc',
-			'links asc',
-			'links desc',
-			'backlinks asc',
-			'backlinks desc'
+			'versions_desc',
+			'versions_asc',
+			'links_asc',
+			'links_desc',
+			'backlinks_asc',
+			'backlinks_desc'
 		))) {
 			$ret = array_slice($ret, $old_offset, $old_maxRecords);
 		}
@@ -131,163 +126,144 @@ class StatsLib extends TikiLib {
 	function wiki_stats() {
 		$stats = array();
 
-		$stats["pages"] = $this->getOne("select count(*) from tiki_pages");
-		$stats["versions"] = $this->getOne("select count(*) from tiki_history");
+		$stats["pages"] = $this->getOne("select count(*) from `tiki_pages`",array());
+		$stats["versions"] = $this->getOne("select count(*) from `tiki_history`",array());
 
-		if ($stats["pages"])
+		if ($stats["pages"]) {
 			$stats["vpp"] = $stats["versions"] / $stats["pages"];
-		else
+		} else {
 			$stats["vpp"] = 0;
-
-		$stats["visits"] = $this->getOne("select sum(hits) from tiki_pages");
+		}
+		$stats["visits"] = $this->getOne("select sum(`hits`) from `tiki_pages`",array());
 		$or = $this->list_orphan_pages(0, -1, 'pageName_desc', '');
 		$stats["orphan"] = $or["cant"];
-		$links = $this->getOne("select count(*) from tiki_links");
+		$links = $this->getOne("select count(*) from `tiki_links`",array());
 
-		if ($stats["pages"])
+		if ($stats["pages"]) {
 			$stats["lpp"] = $links / $stats["pages"];
-		else
+		} else {
 			$stats["lpp"] = 0;
+		}
+		$stats["size"] = $this->getOne("select sum(`page_size`) from `tiki_pages`",array());
 
-		$stats["size"] = $this->getOne("select sum(length(data)) from tiki_pages");
-
-		if ($stats["pages"])
+		if ($stats["pages"]) {
 			$stats["bpp"] = $stats["size"] / $stats["pages"];
-		else
+		} else {
 			$stats["bpp"] = 0;
-
+		}
 		$stats["size"] = $stats["size"] / 1000000;
 		return $stats;
 	}
 
 	function quiz_stats() {
 		$this->compute_quiz_stats();
-
 		$stats = array();
-		$stats["quizzes"] = $this->getOne("select count(*) from tiki_quizzes");
-		$stats["questions"] = $this->getOne("select count(*) from tiki_quiz_questions");
-
-		if ($stats["quizzes"])
+		$stats["quizzes"] = $this->getOne("select count(*) from `tiki_quizzes`",array());
+		$stats["questions"] = $this->getOne("select count(*) from `tiki_quiz_questions`",array());
+		if ($stats["quizzes"]) {
 			$stats["qpq"] = $stats["questions"] / $stats["quizzes"];
-		else
+		} else {
 			$stats["qpq"] = 0;
-
-		$stats["visits"] = $this->getOne("select sum(timesTaken) from tiki_quiz_stats_sum");
-		$stats["avg"] = $this->getOne("select avg(avgavg) from tiki_quiz_stats_sum");
-		$stats["avgtime"] = $this->getOne("select avg(avgtime) from tiki_quiz_stats_sum");
+		}
+		$stats["visits"] = $this->getOne("select sum(`timesTaken`) from `tiki_quiz_stats_sum`",array());
+		$stats["avg"] = $this->getOne("select avg(`avgavg`) from `tiki_quiz_stats_sum`",array());
+		$stats["avgtime"] = $this->getOne("select avg(`avgtime`) from `tiki_quiz_stats_sum`",array());
 		return $stats;
 	}
 
 	function image_gal_stats() {
 		$stats = array();
-
-		$stats["galleries"] = $this->getOne("select count(*) from tiki_galleries");
-		$stats["images"] = $this->getOne("select count(*) from tiki_images");
+		$stats["galleries"] = $this->getOne("select count(*) from `tiki_galleries`",array());
+		$stats["images"] = $this->getOne("select count(*) from `tiki_images`",array());
 		$stats["ipg"] = ($stats["galleries"] ? $stats["images"] / $stats["galleries"] : 0);
-		$stats["size"] = $this->getOne("select sum(filesize) from tiki_images_data where type='o'");
-		//$stats["bpi"] = ($stats["galleries"]?$stats["size"]/$stats["galleries"]:0);
+		$stats["size"] = $this->getOne("select sum(`filesize`) from `tiki_images_data` where `type`=?",array('o'));
 		$stats["bpi"] = ($stats["images"] ? $stats["size"] / $stats["images"] : 0);
 		$stats["size"] = $stats["size"] / 1000000;
-		$stats["visits"] = $this->getOne("select sum(hits) from tiki_galleries");
+		$stats["visits"] = $this->getOne("select sum(`hits`) from `tiki_galleries`",array());
 		return $stats;
 	}
 
 	function file_gal_stats() {
 		$stats = array();
-
-		$stats["galleries"] = $this->getOne("select count(*) from tiki_file_galleries");
-		$stats["files"] = $this->getOne("select count(*) from tiki_files");
+		$stats["galleries"] = $this->getOne("select count(*) from `tiki_file_galleries`",array());
+		$stats["files"] = $this->getOne("select count(*) from `tiki_files`",array());
 		$stats["fpg"] = ($stats["galleries"] ? $stats["files"] / $stats["galleries"] : 0);
-		$stats["size"] = $this->getOne("select sum(filesize) from tiki_files");
+		$stats["size"] = $this->getOne("select sum(`filesize`) from `tiki_files`",array());
 		$stats["size"] = $stats["size"] / 1000000;
 		$stats["bpf"] = ($stats["galleries"] ? $stats["size"] / $stats["galleries"] : 0);
-		$stats["visits"] = $this->getOne("select sum(hits) from tiki_file_galleries");
-		$stats["downloads"] = $this->getOne("select sum(downloads) from tiki_files");
+		$stats["visits"] = $this->getOne("select sum(`hits`) from `tiki_file_galleries`",array());
+		$stats["downloads"] = $this->getOne("select sum(`downloads`) from `tiki_files`",array());
 		return $stats;
 	}
 
 	function cms_stats() {
 		$stats = array();
 
-		$stats["articles"] = $this->getOne("select count(*) from tiki_articles");
-		$stats["reads"] = $this->getOne("select sum(reads) from tiki_articles");
+		$stats["articles"] = $this->getOne("select count(*) from `tiki_articles`",array());
+		$stats["reads"] = $this->getOne("select sum(`reads`) from `tiki_articles`",array());
 		$stats["rpa"] = ($stats["articles"] ? $stats["reads"] / $stats["articles"] : 0);
-		$stats["size"] = $this->getOne("select sum(size) from tiki_articles");
+		$stats["size"] = $this->getOne("select sum(`size`) from `tiki_articles`",array());
 		$stats["bpa"] = ($stats["articles"] ? $stats["size"] / $stats["articles"] : 0);
-		$stats["topics"] = $this->getOne("select count(*) from tiki_topics where active='y'");
+		$stats["topics"] = $this->getOne("select count(*) from `tiki_topics` where `active`=?",array('y'));
 		return $stats;
 	}
 
 	function forum_stats() {
 		$stats = array();
-
-		$stats["forums"] = $this->getOne("select count(*) from tiki_forums");
-		$stats["topics"] = $this->getOne(
-			"select count(*) from
-			tiki_comments,tiki_forums where
-			object=forumId and `objectType` = 'forum' and
-			parentId=0");
-		$stats["threads"] = $this->getOne(
-			"select count(*) from
-			tiki_comments,tiki_forums where
-			object=forumId and `objectType` = 'forum' and
-			parentId<>0");
+		$stats["forums"] = $this->getOne("select count(*) from `tiki_forums`",array());
+		$stats["topics"] = $this->getOne( "select count(*) from `tiki_comments`,`tiki_forums` where `object`=`forumId` and `objectType`=? and `parentId`=?",array('forum',0));
+		$stats["threads"] = $this->getOne( "select count(*) from `tiki_comments`,`tiki_forums` where `object`=`forumId` and `objectType`=? and `parentId`<>?",array('formu',0));
 		$stats["tpf"] = ($stats["forums"] ? $stats["topics"] / $stats["forums"] : 0);
 		$stats["tpt"] = ($stats["topics"] ? $stats["threads"] / $stats["topics"] : 0);
-		$stats["visits"] = $this->getOne("select sum(hits) from tiki_forums");
+		$stats["visits"] = $this->getOne("select sum(`hits`) from `tiki_forums`",array());
 		return $stats;
 	}
 
 	function blog_stats() {
 		$stats = array();
-
-		$stats["blogs"] = $this->getOne("select count(*) from tiki_blogs");
-		$stats["posts"] = $this->getOne("select count(*) from tiki_blog_posts");
+		$stats["blogs"] = $this->getOne("select count(*) from `tiki_blogs`",array());
+		$stats["posts"] = $this->getOne("select count(*) from `tiki_blog_posts`",array());
 		$stats["ppb"] = ($stats["blogs"] ? $stats["posts"] / $stats["blogs"] : 0);
-		$stats["size"] = $this->getOne("select sum(length(data)) from tiki_blog_posts");
+		$stats["size"] = $this->getOne("select sum(`data_size`) from `tiki_blog_posts`",array());
 		$stats["bpp"] = ($stats["posts"] ? $stats["size"] / $stats["posts"] : 0);
-		$stats["visits"] = $this->getOne("select sum(hits) from tiki_blogs");
+		$stats["visits"] = $this->getOne("select sum(`hits`) from `tiki_blogs`",array());
 		return $stats;
 	}
 
 	function poll_stats() {
 		$stats = array();
-
-		$stats["polls"] = $this->getOne("select count(*) from tiki_polls");
-		$stats["votes"] = $this->getOne("select sum(votes) from tiki_poll_options");
+		$stats["polls"] = $this->getOne("select count(*) from `tiki_polls`",array());
+		$stats["votes"] = $this->getOne("select sum(`votes`) from `tiki_poll_options`",array());
 		$stats["vpp"] = ($stats["polls"] ? $stats["votes"] / $stats["polls"] : 0);
 		return $stats;
 	}
 
 	function faq_stats() {
 		$stats = array();
-
-		$stats["faqs"] = $this->getOne("select count(*) from tiki_faqs");
-		$stats["questions"] = $this->getOne("select count(*) from tiki_faq_questions");
+		$stats["faqs"] = $this->getOne("select count(*) from `tiki_faqs`",array());
+		$stats["questions"] = $this->getOne("select count(*) from `tiki_faq_questions`",array());
 		$stats["qpf"] = ($stats["faqs"] ? $stats["questions"] / $stats["faqs"] : 0);
 		return $stats;
 	}
 
 	function user_stats() {
 		$stats = array();
-
-		$stats["users"] = $this->getOne("select count(*) from users_users");
-		$stats["bookmarks"] = $this->getOne("select count(*) from tiki_user_bookmarks_urls");
+		$stats["users"] = $this->getOne("select count(*) from `users_users`",array());
+		$stats["bookmarks"] = $this->getOne("select count(*) from `tiki_user_bookmarks_urls`",array());
 		$stats["bpu"] = ($stats["users"] ? $stats["bookmarks"] / $stats["users"] : 0);
 		return $stats;
 	}
 
 	function site_stats() {
 		$stats = array();
-
-		$stats["started"] = $this->getOne("select min(day) from tiki_pageviews");
-		$stats["days"] = $this->getOne("select count(*) from tiki_pageviews");
-		$stats["pageviews"] = $this->getOne("select sum(pageviews) from tiki_pageviews");
+		$stats["started"] = $this->getOne("select min(`day`) from `tiki_pageviews`",array());
+		$stats["days"] = $this->getOne("select count(*) from `tiki_pageviews`",array());
+		$stats["pageviews"] = $this->getOne("select sum(`pageviews`) from `tiki_pageviews`");
 		$stats["ppd"] = ($stats["days"] ? $stats["pageviews"] / $stats["days"] : 0);
-		$stats["bestpvs"] = $this->getOne("select max(pageviews) from tiki_pageviews");
-		$stats["bestday"] = $this->getOne("select day from tiki_pageviews where pageviews=" . $stats["bestpvs"]);
-		$stats["worstpvs"] = $this->getOne("select min(pageviews) from tiki_pageviews");
-		$stats["worstday"] = $this->getOne("select day from tiki_pageviews where pageviews=" . $stats["worstpvs"]);
+		$stats["bestpvs"] = $this->getOne("select max(`pageviews`) from `tiki_pageviews`",array());
+		$stats["bestday"] = $this->getOne("select `day` from `tiki_pageviews` where `pageviews`=?",array((int)$stats["bestpvs"]));
+		$stats["worstpvs"] = $this->getOne("select min(`pageviews`) from `tiki_pageviews`",array());
+		$stats["worstday"] = $this->getOne("select `day` from `tiki_pageviews` where `pageviews`=?",array((int)$stats["worstpvs"]));
 		return $stats;
 	}
 }
