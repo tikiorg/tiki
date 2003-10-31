7a69e5df440efc26afd0ac54c999e1c6cfce7f53
redflo <redflo@users.sourceforge.net>
2003-10-31 11:12:23 +0000

    strip blank lines. they cause strange effects in oracle's install ...
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2193 b456876b-0849-0410-b77d-98878d47e9d5

--- db/convertscripts/mysql3tooci8.php
+++ db/convertscripts/mysql3tooci8.php
@@ -85,6 +85,8 @@ function parse($stmt)
   $stmt=preg_replace("/insert into ([a-zA-Z0-9_]*).*\(([^\)]+)\) values(.*)/e","do_inserts('$1','$2','$3')",$stmt);
   // the update
   $stmt=preg_replace("/update ([a-zA-Z0-9_]+) set (.*)/e","do_updates('$1','$2')",$stmt);
+  // remove blank lines. this causes errors in oracle
+  $stmt=preg_replace("/\n+/","\n",$stmt);
   return $prestmt.$stmt.";".$poststmt;
 }
 
