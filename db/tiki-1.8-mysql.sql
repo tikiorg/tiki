2def3572887578701bdda1bff9b8a3c7a2a6434a
redflo <redflo@users.sourceforge.net>
2003-11-04 01:21:31 +0000

    db abstraction for integrator
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2249 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki-1.8-mysql.sql
+++ db/tiki-1.8-mysql.sql
@@ -3699,6 +3699,7 @@ INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_usermenu','n');
 INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_userPreferences','n');
 INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_userVersions','n');
 INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_user_watches','n');
+INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_view_tpl','y');
 INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_warn_on_edit','n');
 INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_webmail','n');
 INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_wiki_attachments','n');
@@ -3877,3 +3878,53 @@ INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_modulecontrols'
 
 # Dynamic variables
 INSERT INTO users_permissions (permName, permDesc, level, type) VALUES ('tiki_p_edit_dynvar', 'Can edit dynamic variables', 'editors', 'wiki');
+
+#
+# Table structure for table 'tiki_integrator_reps'
+#
+DROP TABLE IF EXISTS tiki_integrator_reps;
+CREATE TABLE tiki_integrator_reps (
+  repID int(11) NOT NULL auto_increment,
+  name varchar(255) NOT NULL default '',
+  path varchar(255) NOT NULL default '',
+  start_page varchar(255) NOT NULL default '',
+  css_file varchar(255) NOT NULL default '',
+  visibility char(1) NOT NULL default 'y',
+  cacheable char(1) NOT NULL default 'y',
+  description text NOT NULL,
+  PRIMARY KEY  (repID)
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'tiki_integrator_reps'
+#
+INSERT INTO tiki_integrator_reps VALUES (1,'Doxygened (1.3.4) Documentation','','index.html','doxygen.css','n','y','Use this repository as rule source for all your repositories based on doxygened docs. To setup yours just add new repository and copy rules from this repository :)');
+
+#
+# Table structure for table 'tiki_integrator_rules'
+#
+DROP TABLE IF EXISTS tiki_integrator_rules;
+CREATE TABLE tiki_integrator_rules (
+  ruleID int(11) NOT NULL auto_increment,
+  repID int(11) NOT NULL default '0',
+  ord int(2) unsigned NOT NULL default '0',
+  srch blob NOT NULL,
+  repl blob NOT NULL,
+  type char(1) NOT NULL default 'n',
+  casesense char(1) NOT NULL default 'y',
+  rxmod varchar(20) NOT NULL default '',
+  description text NOT NULL,
+  PRIMARY KEY (ruleID),
+  KEY repID (repID)
+) TYPE=MyISAM;
+
+#
+# Dumping data for table 'tiki_integrator_rules'
+#
+INSERT INTO tiki_integrator_rules VALUES (1,1,1,'<\\!DOCTYPE','<!-- Commented by Tiki integrator <!DOCTYPE','y','n','i','Start comment from the begining of document');
+INSERT INTO tiki_integrator_rules VALUES (2,1,2,'</html>','','y','n','i','Remove </html>');
+INSERT INTO tiki_integrator_rules VALUES (3,1,3,'<body.*>','-->','y','n','i','End of comment just after <body>');
+INSERT INTO tiki_integrator_rules VALUES (4,1,4,'</body>','','y','n','i','Remove </body>');
+INSERT INTO tiki_integrator_rules VALUES (5,1,5,'img src=\"(?!http://)','img src=\"/{path}/','y','n','i','Fix images path');
+INSERT INTO tiki_integrator_rules VALUES (6,1,6,'href=\"(?!(http|ftp)://)','href=\"tiki-integrator.php?repID={repID}&file=','y','n','i','Relace internal links to integrator. Dont touch an external links.');
+
--- db/tiki-1.8-pgsql.sql
+++ db/tiki-1.8-pgsql.sql
@@ -5051,6 +5051,9 @@ INSERT INTO "tiki_preferences" ("name","value") VALUES ('feature_userVersions','
 INSERT INTO "tiki_preferences" ("name","value") VALUES ('feature_user_watches','n');
 
 
+INSERT INTO "tiki_preferences" ("name","value") VALUES ('feature_view_tpl','y');
+
+
 INSERT INTO "tiki_preferences" ("name","value") VALUES ('feature_warn_on_edit','n');
 
 
@@ -5579,6 +5582,79 @@ INSERT INTO "tiki_preferences" ("name","value") VALUES ('feature_modulecontrols'
 
 -- Dynamic variables
 INSERT INTO "users_permissions" ("permName","permDesc","level","type") VALUES ('tiki_p_edit_dynvar', 'Can edit dynamic variables', 'editors', 'wiki');
+
+
+
+--
+-- Table structure for table 'tiki_integrator_reps'
+--
+DROP TABLE "tiki_integrator_reps";
+
+
+CREATE TABLE "tiki_integrator_reps" (
+  "repID" bigserial,
+  "name" varchar(255) NOT NULL default '',
+  "path" varchar(255) NOT NULL default '',
+  "start_page" varchar(255) NOT NULL default '',
+  "css_file" varchar(255) NOT NULL default '',
+  "visibility" char(1) NOT NULL default 'y',
+  "cacheable" char(1) NOT NULL default 'y',
+  "description" text NOT NULL,
+  PRIMARY KEY ("repID")
+) ;
+
+
+
+--
+-- Dumping data for table 'tiki_integrator_reps'
+--
+INSERT INTO tiki_integrator_reps VALUES (1,'Doxygened (1.3.4) Documentation','','index.html','doxygen.css','n','y','Use this repository as rule source for all your repositories based on doxygened docs. To setup yours just add new repository and copy rules from this repository :)');
+
+
+
+--
+-- Table structure for table 'tiki_integrator_rules'
+--
+DROP TABLE "tiki_integrator_rules";
+
+
+CREATE TABLE "tiki_integrator_rules" (
+  "ruleID" bigserial,
+  "repID" bigint NOT NULL default '0',
+  "ord" smallint NOT NULL default '0',
+  "srch" bytea NOT NULL,
+  "repl" bytea NOT NULL,
+  "type" char(1) NOT NULL default 'n',
+  "casesense" char(1) NOT NULL default 'y',
+  "rxmod" varchar(20) NOT NULL default '',
+  "description" text NOT NULL,
+  PRIMARY KEY (ruleID),
+
+) ;
+
+CREATE  INDEX "tiki_integrator_rules_repID" ON "tiki_integrator_rules"("repID");
+
+
+--
+-- Dumping data for table 'tiki_integrator_rules'
+--
+INSERT INTO tiki_integrator_rules VALUES (1,1,1,'<\\!DOCTYPE','<!-- Commented by Tiki integrator <!DOCTYPE','y','n','i','Start comment from the begining of document');
+
+
+INSERT INTO tiki_integrator_rules VALUES (2,1,2,'</html>','','y','n','i','Remove </html>');
+
+
+INSERT INTO tiki_integrator_rules VALUES (3,1,3,'<body.*>','-->','y','n','i','End of comment just after <body>');
+
+
+INSERT INTO tiki_integrator_rules VALUES (4,1,4,'</body>','','y','n','i','Remove </body>');
+
+
+INSERT INTO tiki_integrator_rules VALUES (5,1,5,'img src=\"(?!http://)','img src=\"/{path}/','y','n','i','Fix images path');
+
+
+INSERT INTO tiki_integrator_rules VALUES (6,1,6,'href=\"(?!(http|ftp)://)','href=\"tiki-integrator.php?repID={repID}&file=','y','n','i','Relace internal links to integrator. Dont touch an external links.');
+
 
 
 ;
