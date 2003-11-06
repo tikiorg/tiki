5f358cad6a9bb57603a7edfd1f244f60129688d2
sylvieg <sylvieg@users.sourceforge.net>
2003-11-06 16:01:24 +0000

    to avoid edition of tikilib.php with non utf8 editors, the language names are isolated in a specific file
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2268 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lang/langmapping.php
@@ -0,0 +1,71 @@
+<?php // -*- coding:utf-8 -*-
+/** \brief this table associates language extension and language name in the current language and language name in the native language
+ * CAREFULL: is utf8 encoding
+ **/
+    $langmapping = array(
+	    'cs' => array(
+		'Český',
+		tra("Czech")
+		),
+	    'da' => array(
+		'Dansk',
+		tra("Danish")
+		),
+	    'de' => array(
+		'Deutsch',
+		tra("German")
+		),
+	    'en' => array(
+		'English',
+		tra("English")
+		),
+	    'es' => array(
+		'Español',
+		tra("Spanish")
+		),
+	    'el' => array(
+		'Greek',
+		tra("Greek")
+		),
+	    'fr' => array(
+		    'Français',
+		    tra("French")
+		    ),
+	    'it' => array(
+		    'Italiano',
+		    tra("Italian")
+		    ),
+	    'ja' => array(
+		    'Japanese',
+		    tra("Japanese")
+		    ),
+	    'nl' => array(
+		    'Dutch',
+		    tra("Dutch")
+		    ),
+	    'no' => array(
+		    'Norwegian',
+		    tra("Norwegian")
+		    ),
+	    'pl' => array(
+		    'Polish',
+		    tra("Polish")
+		    ),
+	    'ru' => array(
+		    'Russian',
+		    tra("Russian")
+		    ),
+	    'sv' => array(
+		    'Swedish',
+		    tra("Swedish")
+		    ),
+	    'tw' => array(
+		    'Twi',
+		    tra("Twi")
+		    ),
+	    'zh' => array(
+		    'Chinese',
+		    tra("Chinese")
+		    )
+		);
+?>
