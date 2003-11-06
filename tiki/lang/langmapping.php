b35847dfcef546a8e66d8b54cec84ba8195605fa
sylvieg <sylvieg@users.sourceforge.net>
2003-11-06 16:06:16 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@2269 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lang/langmapping.php
+++ tiki/lang/langmapping.php
@@ -1,71 +1,24 @@
 <?php // -*- coding:utf-8 -*-
 /** \brief this table associates language extension and language name in the current language and language name in the native language
  * CAREFULL: is utf8 encoding
+ * PLEASE : translators update this file will your language name in your own language
  **/
     $langmapping = array(
-	    'cs' => array(
-		'Český',
-		tra("Czech")
-		),
-	    'da' => array(
-		'Dansk',
-		tra("Danish")
-		),
-	    'de' => array(
-		'Deutsch',
-		tra("German")
-		),
-	    'en' => array(
-		'English',
-		tra("English")
-		),
-	    'es' => array(
-		'Español',
-		tra("Spanish")
-		),
-	    'el' => array(
-		'Greek',
-		tra("Greek")
-		),
-	    'fr' => array(
-		    'Français',
-		    tra("French")
-		    ),
-	    'it' => array(
-		    'Italiano',
-		    tra("Italian")
-		    ),
-	    'ja' => array(
-		    'Japanese',
-		    tra("Japanese")
-		    ),
-	    'nl' => array(
-		    'Dutch',
-		    tra("Dutch")
-		    ),
-	    'no' => array(
-		    'Norwegian',
-		    tra("Norwegian")
-		    ),
-	    'pl' => array(
-		    'Polish',
-		    tra("Polish")
-		    ),
-	    'ru' => array(
-		    'Russian',
-		    tra("Russian")
-		    ),
-	    'sv' => array(
-		    'Swedish',
-		    tra("Swedish")
-		    ),
-	    'tw' => array(
-		    'Twi',
-		    tra("Twi")
-		    ),
-	    'zh' => array(
-		    'Chinese',
-		    tra("Chinese")
-		    )
+	    'cs' => array(	'Český',		tra("Czech")		),
+	    'da' => array(	'Dansk',		tra("Danish")		),
+	    'de' => array(	'Deutsch',		tra("German")		),
+	    'en' => array(	'English',		tra("English")		),
+	    'es' => array(	'Español',		tra("Spanish")		),
+	    'el' => array(	'Greek',		tra("Greek")		),
+	    'fr' => array(	'Français',		tra("French")		),
+	    'it' => array(	'Italiano',		tra("Italian")		),
+	    'ja' => array(	'Japanese',		tra("Japanese")		),
+	    'nl' => array(	'Dutch',		tra("Dutch")		),
+	    'no' => array(	'Norwegian',	tra("Norwegian")	),
+	    'pl' => array(	'Polish',		tra("Polish")		),
+	    'ru' => array(	'Russian',		tra("Russian")		),
+	    'sv' => array(	'Swedish',		tra("Swedish")		),
+	    'tw' => array(	'Twi',			tra("Twi")			),
+	    'zh' => array(	'Chinese',		tra("Chinese")		)
 		);
 ?>
