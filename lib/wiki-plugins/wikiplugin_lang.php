f77564e8fb5a1b2b6c18043fc872feedf73dd14a
sylvieg <sylvieg@users.sourceforge.net>
2003-11-09 13:56:14 +0000

    typo
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2303 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_lang.php
+++ lib/wiki-plugins/wikiplugin_lang.php
@@ -1,11 +1,11 @@
 <?php
-/* $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_lang.php,v 1.1 2003-10-28 10:36:18 sylvieg Exp $
+/* $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_lang.php,v 1.2 2003-11-09 13:56:14 sylvieg Exp $
  * Displays the content data only if the language matched the current language
  * {LANG(lang=fr)}Bon appétit{LANG}
  * 
  */
 function wikiplugin_lang_help() {
-	return tra("Displays the text only if the language matchs").":<br :>~np~{LANG(lang=>)}".tra("texte")."{LANG}~/np~";
+	return tra("Displays the text only if the language matchs").":<br :>~np~{LANG(lang=>)}".tra("text")."{LANG}~/np~";
 }
 
 function wikiplugin_lang($data, $params) {
