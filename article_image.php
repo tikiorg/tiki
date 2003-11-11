938cdf7fb5a236447c34b2ba992e6dad48d33dee
dheltzel <dheltzel@users.sourceforge.net>
2003-11-11 21:27:21 +0000

    fix to remove un-needed extra var assignment
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2360 b456876b-0849-0410-b77d-98878d47e9d5

--- article_image.php
+++ article_image.php
@@ -1,12 +1,12 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/article_image.php,v 1.3 2003-08-07 04:33:56 rossta Exp $
+// $Header: /cvsroot/tikiwiki/tiki/article_image.php,v 1.4 2003-11-11 21:27:21 dheltzel Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-# $Header: /cvsroot/tikiwiki/tiki/article_image.php,v 1.3 2003-08-07 04:33:56 rossta Exp $
+# $Header: /cvsroot/tikiwiki/tiki/article_image.php,v 1.4 2003-11-11 21:27:21 dheltzel Exp $
 
 // application to display an image from the database with 
 // option to resize the image dynamically creating a thumbnail on the fly.
@@ -19,8 +19,8 @@ include_once ('lib/tikilib.php');
 $tikilib = new Tikilib($dbTiki);
 $data = $tikilib->get_article($_REQUEST["id"]);
 $type = $data["image_type"];
-$data = $data["image_data"];
+//$data = $data["image_data"];
 header ("Content-type: $type");
-echo $data;
+echo $data["image_data"];
 
 ?>
