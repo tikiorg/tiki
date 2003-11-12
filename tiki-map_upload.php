be21fe0613974d74facce194c6ec411733f615c0
franck <franck@users.sourceforge.net>
2003-11-12 00:09:06 +0000

    various fixes?
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2368 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-map_upload.php
+++ tiki-map_upload.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-map_upload.php,v 1.2 2003-10-12 19:49:15 franck Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-map_upload.php,v 1.3 2003-11-12 00:09:05 franck Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
