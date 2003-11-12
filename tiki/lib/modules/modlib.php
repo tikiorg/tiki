32045af3689cba508b83e261c258aa9f88f28776
redflo <redflo@users.sourceforge.net>
2003-11-12 14:58:50 +0000

    bug: "ghost" module "le-title"
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@2377 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/modules/modlib.php
+++ tiki/lib/modules/modlib.php
@@ -105,7 +105,7 @@ class ModLib extends TikiLib {
 		$h = opendir("templates/modules");
 
 		while (($file = readdir($h)) !== false) {
-			if (substr($file, 0, 3) == 'mod') {
+			if (substr($file, 0, 4) == 'mod-') {
 				if (!strstr($file, "nocache")) {
 					$name = substr($file, 4, strlen($file) - 8);
 
--- tiki/tiki-admin_modules.php
+++ tiki/tiki-admin_modules.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_modules.php,v 1.24 2003-09-28 14:06:05 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_modules.php,v 1.25 2003-11-12 14:58:49 redflo Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
