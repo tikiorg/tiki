b7dc5d9ccade597a34e440fb511980dc63acbebb
awcolley <awcolley@users.sourceforge.net>
2003-11-17 17:08:24 +0000

    all directory related pages should obey directory section display options
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@2486 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-directory_admin_categories.php
+++ tiki/tiki-directory_admin_categories.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-directory_admin_categories.php,v 1.8 2003-11-17 15:44:28 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-directory_admin_categories.php,v 1.9 2003-11-17 17:08:24 awcolley Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -151,6 +151,10 @@ $smarty->assign('categs', $categs);
 $cat_type = 'directory';
 $cat_objid = $_REQUEST['categId'];
 include_once("categorize_list.php");
+
+// This page should be displayed with Directory section options
+$section='directory';
+include_once('tiki-section_options.php');
 
 // Display the template
 $smarty->assign('mid', 'tiki-directory_admin_categories.tpl');
--- tiki/tiki-directory_admin_related.php
+++ tiki/tiki-directory_admin_related.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-directory_admin_related.php,v 1.6 2003-11-17 15:44:28 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-directory_admin_related.php,v 1.7 2003-11-17 17:08:24 awcolley Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -113,6 +113,10 @@ $categs = $dirlib->dir_get_all_categories_np(0, -1, 'name asc', $find, $_REQUEST
 $smarty->assign('categs', $categs);
 $all_categs = $dirlib->dir_get_all_categories(0, -1, 'name asc', $find);
 $smarty->assign('all_categs', $all_categs);
+
+// This page should be displayed with Directory section options
+$section='directory';
+include_once('tiki-section_options.php');
 
 // Display the template
 $smarty->assign('mid', 'tiki-directory_admin_related.tpl');
