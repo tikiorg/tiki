bb91047ba792701412651c03aaeef153e2aa56a6
ohertel <ohertel@users.sourceforge.net>
2003-11-18 18:46:18 +0000

    new feature in admin general: add this site to tw.org directory.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2518 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki-register_site.php
@@ -0,0 +1,35 @@
+<?php
+// Initialization
+require_once('tiki-setup.php');
+include_once('lib/directory/dirlib.php');
+
+$tmp1 = isset($_SERVER["SERVER_NAME"]) ? $_SERVER["SERVER_NAME"] : "";
+$tmp2 = isset($_SERVER["PHP_SELF"]) ? $_SERVER["PHP_SELF"] : "";
+
+// concat all, remove the // between server and path and then
+// remove the name of the script itself:
+$url = str_replace("tiki-register_site.php", "",$tmp1.substr($tmp2,1));
+
+$info = Array();
+$info["name"] = $tikilib->get_preference( "siteTitle", "" );
+$info["description"] = '';
+$info["url"] = $url;
+$info["country"] = 'None';
+$info["isValid"] = 'n';
+$smarty->assign_by_ref('info',$info);
+
+$countries=Array();
+$h=opendir("img/flags");
+while($file=readdir($h)) {
+  if(is_file('img/flags/'.$file)) {
+    $name=explode('.',$file);
+    $countries[]=$name[0];
+  }
+}
+closedir($h);
+$smarty->assign_by_ref('countries',$countries);
+
+// Display the template
+$smarty->assign('mid','tiki-register_site.tpl');
+$smarty->display("styles/$style_base/tiki.tpl");
+?>
