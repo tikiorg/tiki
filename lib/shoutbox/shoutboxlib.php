6d3f70ac3408b904162d9819d6709d218ef67c58
luciash <luciash@users.sourceforge.net>
2003-11-20 10:49:24 +0000

    corrected position of htmlspecialchars() function
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2574 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/shoutbox/shoutboxlib.php
+++ lib/shoutbox/shoutboxlib.php
@@ -29,13 +29,14 @@ class ShoutboxLib extends TikiLib {
 			if (!$res["user"]) {
 				$res["user"] = tra('Anonymous');
 			}
-			if ($shoutbox_autolink == 'y') {
-				$res["message"] = htmlspecialchars($res["message"]);
-				/* we replace urls starting with http(s)|ftp(s) to active links */
+      // convert ampersands and other stuff to xhtml compliant entities
+      $res["message"] = htmlspecialchars($res["message"]);
+			if ($shoutbox_autolink == 'y') {				
+				// we replace urls starting with http(s)|ftp(s) to active links
 				$res["message"] = preg_replace("/((http|ftp)+(s)?:\/\/[^<>\s]+)/i", "<a href=\"\\0\">\\0</a>", $res["message"]);
-				/* we replace also urls starting with www. only to active links */
+				// we replace also urls starting with www. only to active links
 				$res["message"] = preg_replace("/(?<!http|ftp)(?<!s)(?<!:\/\/)(www\.[^ )\s\r\n]+)/i","<a href=\"http://\\0\">\\0</a>",$res["message"]);
-				/* we replace also urls longer than 30 chars with translantable string as link description instead the URL itself to prevent breaking the layout */
+				// we replace also urls longer than 30 chars with translantable string as link description instead the URL itself to prevent breaking the layout
 				$res["message"] = preg_replace("/(<a href=\")((http|ftp)+(s)?:\/\/[^\"]+)(\">)([^<]){30,}<\/a>/i", "<a href=\"\\2\">[".tra('Link')."]</a>", $res["message"]);
       }
 			$ret[] = $res;
