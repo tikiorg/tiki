2dba2fa9edf2e7698b23f34611983340d17a9af7
gmuslera <gmuslera@users.sourceforge.net>
2003-11-22 05:36:17 +0000

    Changed wrong title from Mail-in to HTML pages the application menu as user menu
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2630 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki-1.8-sybase.sql
+++ db/tiki-1.8-sybase.sql
@@ -3445,7 +3445,7 @@ go
 
 
 
-INSERT INTO "tiki_menu_options" ("menuId","type","name","url","position","section","perm","groupname") VALUES (42,'o','Mail-in','tiki-admin_html_pages.php',1185,'','tiki_p_edit_html_pages','')
+INSERT INTO "tiki_menu_options" ("menuId","type","name","url","position","section","perm","groupname") VALUES (42,'o','HTML pages','tiki-admin_html_pages.php',1185,'','tiki_p_edit_html_pages','')
 go
 
 
