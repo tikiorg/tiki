e22a99670d3b5d82c9121a6328f10c0c57c6fbec
gongo <gongo@users.sourceforge.net>
2003-11-23 21:27:31 +0000

    xhtml compliance fix (& in urls to &amp; )
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2656 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-google.php
+++ modules/mod-google.php
@@ -9,7 +9,7 @@ if(isset($_POST['sgoogle'])){
 
 	echo "<script language='Javascript' type='text/javascript'>\n"
 	. "<!--\n"
-	. "  window.open ('http://www.google.com/search?hl=en&ie=UTF-8&amp;oe=UTF-8&amp;q=".$_POST['sgoogle']."&amp;btnG=Google+Search','Google');\n"
+	. "  window.open ('http://www.google.com/search?hl=en&amp;ie=UTF-8&amp;oe=UTF-8&amp;q=".$_POST['sgoogle']."&amp;btnG=Google+Search','Google');\n"
 	. "-->\n"
 	. "</SCRIPT>\n";
 	$smarty->assign('ownurl',$_SERVER["REQUEST_URI"]);
