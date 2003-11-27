d35f9133159c50152e6cd4326723d8f215d0b2b4
redflo <redflo@users.sourceforge.net>
2003-11-27 00:48:27 +0000

    crerr bug
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@2733 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-edit_languages.php
+++ tiki/tiki-edit_languages.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-edit_languages.php,v 1.11 2003-11-27 00:19:16 redflo Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-edit_languages.php,v 1.12 2003-11-27 00:48:27 redflo Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -33,6 +33,7 @@ if (isset($_REQUEST["createlang"])) {
 		$smarty->assign('crmsg', tra("You must provide a longname"));
 	} else {
 		// Now we can create it
+		$crerror = false;
 		$query = "insert into `tiki_languages`(`lang`,`language`) values (?,?)";
 		$result = $tikilib->query($query,array($_REQUEST["cr_lang_short"],$_REQUEST["cr_lang_long"]));
 	}
