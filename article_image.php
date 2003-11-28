cf856dd3b7a0106b08924a383264579daf131038
mose <mose@users.sourceforge.net>
2003-11-28 04:47:38 +0000

    fixed the display of images in articles
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2745 b456876b-0849-0410-b77d-98878d47e9d5

--- article_image.php
+++ article_image.php
@@ -1,12 +1,12 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/article_image.php,v 1.5 2003-11-23 22:48:58 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/article_image.php,v 1.6 2003-11-28 04:47:38 mose Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-# $Header: /cvsroot/tikiwiki/tiki/article_image.php,v 1.5 2003-11-23 22:48:58 mose Exp $
+# $Header: /cvsroot/tikiwiki/tiki/article_image.php,v 1.6 2003-11-28 04:47:38 mose Exp $
 
 // application to display an image from the database with 
 // option to resize the image dynamically creating a thumbnail on the fly.
@@ -21,6 +21,7 @@ if (is_file($topiccachefile) and (!isset($_REQUEST["reload"]))) {
 	header ("Content-type: ".$size['mime']);
 	readfile($topiccachefile);
 } else {
+	include_once ('lib/init/initlib.php');
 	include_once ('db/tiki-db.php');
 	include_once ('lib/tikilib.php');
 	$tikilib = new Tikilib($dbTiki);
