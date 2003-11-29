69acc3bd380f95b110fd564c0e45dd8406141a02
mose <mose@users.sourceforge.net>
2003-11-29 11:10:44 +0000

    added an optional status params to trackers module
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2755 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-last_tracker_items.php
+++ modules/mod-last_tracker_items.php
@@ -2,15 +2,19 @@
 
 if ($feature_trackers == 'y') {
 	$smarty->assign('modlifn', $module_params["name"]);
-
+	if (isset($module_params["status"])) {
+		$status = $module_params["status"];
+	} else {
+		$status = '';
+	}
 	if (isset($module_params["trackerId"])) {
-		$ranking = $tikilib->list_tracker_items($module_params["trackerId"], 0, $module_rows, 'created_desc', '');
+		$ranking = $tikilib->list_tracker_items($module_params["trackerId"], 0, $module_rows, 'created_desc', $status);
 	} else {
 		$ranking = array();
 	}
 
 	$smarty->assign('modLastItems', $ranking["data"]);
-    $smarty->assign('nonums', isset($module_params["nonums"]) ? $module_params["nonums"] : 'n');
+  $smarty->assign('nonums', isset($module_params["nonums"]) ? $module_params["nonums"] : 'n');
 }
 
 ?>
