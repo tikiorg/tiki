9011ab19f0774c498a95c6d87616959e64944c3c
mose <mose@users.sourceforge.net>
2003-12-04 10:40:53 +0000

    fixed general download problem that occured after TikiInit class apparition
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@2816 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-download_forum_attachment.php
+++ tiki/tiki-download_forum_attachment.php
@@ -1,12 +1,13 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-download_forum_attachment.php,v 1.4 2003-11-09 12:29:50 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-download_forum_attachment.php,v 1.5 2003-12-04 10:40:53 mose Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
 // Initialization
+include_once("lib/init/initlib.php");
 require_once ('tiki-setup_base.php');
 
 include_once ('lib/commentslib.php');
