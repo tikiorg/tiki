af27c8fc378144aafb4c90de8311a9c17f7b659b
mose <mose@users.sourceforge.net>
2003-12-16 14:23:41 +0000

    added tha most wanted feature of all times : the tiki system admin swiss knife, with only one feature in it right now, but not the least : empty templates_c with one click
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@2923 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/tiki-admin_system.php
@@ -0,0 +1,64 @@
+<?php
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_system.php,v 1.1 2003-12-16 14:23:41 mose Exp $
+
+// Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+require_once ('tiki-setup.php');
+
+function du($location) {
+	if (!$location or !is_dir($location)) return 0;
+	$total = 0;
+	$all = opendir($location);
+	while ($file = readdir($all)) {
+		if (is_dir($location.'/'.$file) and $file <> ".." and $file <> ".") {
+			$total += du($location.'/'.$file);
+			unset($file);
+		} elseif (!is_dir($location.'/'.$file)) {
+			$stats = stat($location.'/'.$file);
+			$total += $stats['size'];
+			unset($file);
+		}
+	}
+	closedir($all);
+	unset($all);
+	return $total;
+}
+
+function erase_all_dir($path) {
+	if ($dir = opendir($path)) {
+		while (false !== ($file = readdir($dir))) {
+			if (substr($file,0,1) == ".") continue;
+			if (is_dir($path."/".$file)) {
+				erase_all_dir($path."/".$file);
+			} else {
+				unlink($path."/".$file);
+			}
+		}
+		closedir($dir);
+	}
+}
+
+if ($tiki_p_admin != 'y') {
+	$smarty->assign('msg', tra("You dont have permission to use this feature"));
+	$smarty->display("error.tpl");
+	die;
+}
+
+$done = '';
+$output = '';
+$buf = '';
+
+if (isset($_POST['do'])) {
+	if ($_POST['do'] == 'templates_c') {
+		erase_all_dir('templates_c');
+	}
+}
+
+$templates_c_size = du('templates_c');
+$smarty->assign('templates_c_size', $templates_c_size);
+
+$smarty->assign('mid', 'tiki-admin_system.tpl');
+$smarty->display("tiki.tpl");
+?>
