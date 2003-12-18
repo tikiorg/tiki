2db82bea8dae0b3775a708130ab90f2c68b02fef
redflo <redflo@users.sourceforge.net>
2003-12-18 19:41:52 +0000

    have to use relative path
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@2958 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-admin_system.php
+++ tiki/tiki-admin_system.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_system.php,v 1.4 2003-12-18 18:45:18 redflo Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_system.php,v 1.5 2003-12-18 19:41:52 redflo Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -64,7 +64,7 @@ function cache_templates($path,$newlang) {
 				cache_templates($path."/".$file,$newlang);
 			} else {
 				//todo: check if template is compiled already
-				$smarty->fetch($path."/".$file);
+				$smarty->fetch($file);
 			}
 		}
 		closedir($dir);
