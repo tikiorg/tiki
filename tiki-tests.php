6fed6d2e7db317adadb766e81e1e70e134474e92
epolidor <epolidor@users.sourceforge.net>
2003-12-19 15:11:59 +0000

    blank space removed
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2981 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-tests.php
+++ tiki-tests.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-tests.php,v 1.4 2003-08-07 04:33:57 rossta Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-tests.php,v 1.5 2003-12-19 15:11:59 epolidor Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -22,5 +22,4 @@ while ($file = readdir($h)) {
 }
 
 closedir ($h);
-
 ?>
