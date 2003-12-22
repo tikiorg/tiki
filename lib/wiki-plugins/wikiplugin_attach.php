7423361a38991633998ca00b2ceae6754b4bf41a
mose <mose@users.sourceforge.net>
2003-12-22 02:30:39 +0000

    added a new plugin to dispaly attachments linked to the current wiki page
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@2992 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_attach.php
@@ -0,0 +1,65 @@
+<?php
+// $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_attach.php,v 1.1 2003-12-22 02:30:38 mose Exp $
+// Displays an attachment or a list of attachments
+// Parameters: ln => line numbering (default false)
+// Example:
+// {ATTACH(id=>22)}
+//  comment about attachment, that will be display under attachment informations
+// {ATTACH}
+function wikiplugin_attach_help() {
+	$help = tra("Displays an attachment or a list of them").":<br />";
+	$help.= "~np~{ATTACH(num=>1)}".tra("comment")."{ATTACH}~/np~ ";
+	return tra($help);
+}
+
+function wikiplugin_attach($data, $params) {
+	global $atts;
+	extract ($params);
+	if (!isset($num)) $num = '-1';
+	if (!$atts['cant']) {
+		$data = "''".tra('no attachment on this page.')."''";
+	} elseif ($num >= 0 and $num < $atts['cant']) {
+		$loop[] = $num--;
+	} else {
+		$loop = range(1,$atts['cant']);
+	}
+	
+	$out = array();
+	$separator = "<br />";
+	if ($data) {
+		$out[] = $data;
+	}
+	foreach ($loop as $n=>$x) {
+		$link = '<a href="tiki-download_wiki_attachment.php?attId='.$atts['data'][$n]['attId'].'" class="link"';
+		$link.= ' title="';
+		if (isset($showdesc)) {
+			$link.= $atts['data'][$n]['filename'];
+		} else {
+			$link.= $atts['data'][$n]['comment'];
+		}
+		if (isset($dls)) {
+			$link.= " ".$atts['data'][$n]['downloads'];
+		}
+		$link.= '">';
+		if (isset($icon)) {
+			include_once("lib/mime/mimeextensions.php");
+			$ext = $atts['data'][$n]['filetype'];
+			if (isset($mimeextensions["$ext"]) and (is_file("img/icn/".$mimeextensions["$ext"].".gif"))) {
+				$link.= '<img src="img/icn/'.$mimeextensions["$ext"].'.gif" border="0" />&nbsp;';
+			} else {
+				$link.= '<img src="img/icn/else.gif" border="0" />&nbsp;';
+			}
+		}
+		if (isset($showdesc)) {
+			$link.= strip_tags($atts['data'][$n]['comment']);
+		} else {
+			$link.= strip_tags($atts['data'][$n]['filename']);
+		}
+		$link.= '</a>';
+		$out[] = $link;
+	}
+	$data = implode($separator,$out);
+	return $data;
+}
+
+?>
