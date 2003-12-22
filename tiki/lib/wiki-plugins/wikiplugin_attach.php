5107b18a957fede8c45725a5996346a9999e1957
mose <mose@users.sourceforge.net>
2003-12-22 07:27:14 +0000

    debugged ATTACH plugin
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@2997 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/wiki-plugins/wikiplugin_attach.php
+++ tiki/lib/wiki-plugins/wikiplugin_attach.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_attach.php,v 1.2 2003-12-22 02:40:31 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_attach.php,v 1.3 2003-12-22 07:27:14 mose Exp $
 // Displays an attachment or a list of attachments
 // Parameters: ln => line numbering (default false)
 // Example:
@@ -8,29 +8,32 @@
 // {ATTACH}
 function wikiplugin_attach_help() {
 	$help = tra("Displays an attachment or a list of them").": ";
-	$help.= "~np~{ATTACH(num=>1,showdesc=>0|1,dls=>0|1,icon=>0|1)}".tra("comment")."{ATTACH}~/np~ ";
+	$help.= "~np~{ATTACH(num=1,showdesc=0|1,dls=0|1,icon=0|1)}".tra("comment")."{ATTACH}~/np~ ";
 	$help.= "num is optionnal and is the order number of the attachment in the list. If not provided a list of all attachments is displayed.";
 	return tra($help);
 }
 
 function wikiplugin_attach($data, $params) {
 	global $atts;
+	global $mimeextensions;
 	extract ($params);
-	if (!isset($num)) $num = '-1';
+	$loop = array();
+	if (!isset($num)) $num = '0';
 	if (!$atts['cant']) {
-		$data = "''".tra('no attachment on this page.')."''";
-	} elseif ($num >= 0 and $num < $atts['cant']) {
+		return "''".tra('no attachment on this page.')."''";
+	} elseif ($num > 0 and $num <= $atts['cant']) {
 		$loop[] = $num--;
 	} else {
 		$loop = range(1,$atts['cant']);
 	}
 	
 	$out = array();
-	$separator = "<br />";
+	$separator = "\n";
 	if ($data) {
 		$out[] = $data;
 	}
-	foreach ($loop as $n=>$x) {
+	foreach ($loop as $n) {
+		$n--;
 		$link = '<a href="tiki-download_wiki_attachment.php?attId='.$atts['data'][$n]['attId'].'" class="link"';
 		$link.= ' title="';
 		if (isset($showdesc)) {
@@ -43,7 +46,9 @@ function wikiplugin_attach($data, $params) {
 		}
 		$link.= '">';
 		if (isset($icon)) {
-			include_once("lib/mime/mimeextensions.php");
+			if (!isset($mimeextensions)) {
+				require("lib/mime/mimeextensions.php");
+			}
 			$ext = $atts['data'][$n]['filetype'];
 			if (isset($mimeextensions["$ext"]) and (is_file("img/icn/".$mimeextensions["$ext"].".gif"))) {
 				$link.= '<img src="img/icn/'.$mimeextensions["$ext"].'.gif" border="0" />&nbsp;';
