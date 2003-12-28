c5ec293cdb6640d975563c2467c662959f5d4f53
wolff_borg <wolff_borg@users.sourceforge.net>
2003-12-28 07:29:22 +0000

    Polished script
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@3022 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/doc/devtools/tikilight.sh
+++ tiki/doc/devtools/tikilight.sh
@@ -2,12 +2,33 @@
 # originall written by mose@feu.org
 # maintained by wolff_borg@yahoo.com.au
 
-# HOWTO create TikiWiki-Light ?
+# HOWTO create TikiWiki-Lite ?
 # --------------------------
+#
+# The following are instructions on how to use the TikiLite script.
+#
+# The current defaults are english language and MoreNeat theme.
+# These can be modified from the script.
+#
+#To Install
+#
+#   1. Check out a new installation of TikiWiki into a directory of your choice.
+#   2. Move to the top of your TikiWiki installation.
+#   3. Modify doc/devtools/tikilight.sh to point to set your defaults.
+#   4. Execute doc/devtools/tikilight.sh - this will remove all non-core files and directories. 
+#
+#To Update
+#
+#   1. Do a cvs up in your TikiLite directory
+#   2. Execute doc/devtools/tikilight.sh script again 
 #
 # ############################################################
 # special operation for a lighter tikiwiki
-# 
+#
+LANG_DEF="en"
+THEME_DEF="moreneat"
+
+
 echo "Removing Thumbs.db ..." 
 find -name Thumbs.db -exec rm -f {} \;
 
@@ -20,8 +41,8 @@ find img/avatars/ -type f -name '*.gif' | grep -v 000 | xargs -- rm -f
 echo "Removing custom images..."
 rm -rf img/custom
 
-echo "Remove all languages except english (default)..."
-find lang -type f -name language.php | grep -v "/en/" | xargs -- rm -rf
+echo "Remove languages except default..."
+find lang -type f -name language.php | grep -v "/$LANG_DEF/" | xargs -- rm -rf
 
 echo "Remove Galaxia..."
 rm -rf lib/Galaxia
@@ -35,10 +56,10 @@ rm -rf lib/pear/SOAP/example
 echo "Remove PDF fonts..."
 find lib/pdflib/fonts -type f -name "*.afm" | grep -v php_Helvetica | grep -v php_Courier | xargs -- rm -f
 
-echo "Remove styles..."
-find templates/styles/* -type d | grep -v moreneat | xargs -- rm -rf
-find styles/* -type d | grep -v moreneat | xargs -- rm -rf
-find styles/ -type f -name "*.css" | grep -v moreneat | xargs -- rm -f
+echo "Remove styles except default..."
+find templates/styles/* -type d | grep -v $THEME_DEF | xargs -- rm -rf
+find styles/* -type d | grep -v $THEME_DEF | xargs -- rm -rf
+find styles/ -type f -name "*.css" | grep -v $THEME_DEF | xargs -- rm -f
 
 # ############################################################
 
