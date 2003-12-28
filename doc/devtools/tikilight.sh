49b8b620e165038c309033cf1a88f0bf78ce0aee
wolff_borg <wolff_borg@users.sourceforge.net>
2003-12-28 03:32:59 +0000

    *** empty log message ***
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@3017 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ doc/devtools/tikilight.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# originall written by mose@feu.org
+# maintained by wolff_borg@yahoo.com.au
+
+# HOWTO create TikiWiki-Light ?
+# --------------------------
+#
+# ############################################################
+# special operation for a lighter tikiwiki
+# 
+echo "Removing Thumbs.db ..." 
+find -name Thumbs.db -exec rm -f {} \;
+
+echo "Removing DB extras..."
+rm -rf db/pgsql
+
+echo "Removing Avatars..."
+find img/avatars/ -type f -name '*.gif' | grep -v 000 | xargs -- rm -f
+
+echo "Removing custom images..."
+rm -rf img/custom
+
+echo "Remove all languages except english (default)..."
+find lang -type f -name language.php | grep -v "/en/" | xargs -- rm -rf
+
+echo "Remove Galaxia..."
+rm -rf lib/Galaxia
+
+echo "Remove iCal..."
+rm -rf lib/calendar/iCal
+
+echo "Remove SOAP example..."
+rm -rf lib/pear/SOAP/example
+
+echo "Remove PDF fonts..."
+find lib/pdflib/fonts -type f -name "*.afm" | grep -v php_Helvetica | grep -v php_Courier | xargs -- rm -f
+
+echo "Remove styles..."
+find templates/styles/* -type d | grep -v default | xargs -- rm -rf
+find styles/* -type d | grep -v default | xargs -- rm -rf
+find styles/ -type f -name "*.css" | grep -v default | xargs -- rm -f
+
+# ############################################################
+
+echo "Setting directory perms..."
+find -type d -exec chmod 775 {} \;
+
+echo "Setting file perms..."
+find -type f -exec chmod 664 {} \;
+find -type f -name '*.sh' -exec chmod 775 {} \;
+
+echo "Done."
