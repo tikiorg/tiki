99d5269b59c777045c40695cf344a0b824bc9829
mose <mose@users.sourceforge.net>
2003-12-30 08:08:46 +0000

    improved options for tracker plugin (but it still don't work)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@3044 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_tracker.php
+++ lib/wiki-plugins/wikiplugin_tracker.php
@@ -5,7 +5,7 @@
 // {ARTICLE(Id=>articleId, Field=>FieldName)}{ARTICLE}
 // FieldName can be any field in the tiki_articles table, but title,heading, or body are probably the most useful.
 function wikiplugin_tracker_help() {
-	return tra("Displays an input form for tracker submit").":<br />~np~{TRACKER(id=>)}Notice{TRACKER}~/np~";
+	return tra("Displays an input form for tracker submit").":<br />~np~{TRACKER(trackerId=>1,fields=>login:email:-optionalfield)}Notice{TRACKER}~/np~";
 }
 function wikiplugin_tracker($data, $params) {
 	global $tikilib, $dbTiki;
@@ -18,7 +18,6 @@ function wikiplugin_tracker($data, $params) {
 	if (!isset($action)) {
 		$action = tra("Save");
 	}
-
 	$tracker = $tikilib->get_tracker($trackerId);
 	if ($tracker) {
 		include "lib/trackers/trackerlib.php";
@@ -30,34 +29,54 @@ function wikiplugin_tracker($data, $params) {
 			//$trklib->replace_item($trackerId,0,$ins_fields);
 			return "<div>Done!</div>";
 		}
-		$fields = $trklib->list_tracker_fields($trackerId,0,-1,"position_asc","");
+		$flds = $trklib->list_tracker_fields($trackerId,0,-1,"position_asc","");
+		$optionnal = array();
+		if (isset($fields)) {
+			$outf = array();
+			$fl = split(":",$fields);
+			
+			foreach ($fl as $l) {
+				if (substr($l,0,1) == '-') {
+					$l = substr($l,1);
+					$optional[] = $l;
+				}
+				$outf[] = $l;
+			}
+		}
 		
-		$back = '<form><input type="hidden" name="trackit" value="1" />';
+	
+		$back = '~np~<form><input type="hidden" name="trackit" value="1" />';
+		$back.= '<input type="hidden" name="page" value="'.$_REQUEST["page"].'" />';
 		$back.= '<div class="titlebar">'.$tracker["name"].'</div>';
 		$back.= '<div class="wikitext">'.$tracker["description"].'</div>';
 		$back.= '<table>';
-		foreach ($fields['data'] as $f) {
-			if ($f['type'] == 't') {
-				$back.= "<tr><td>".$f['name']."</td><td>";
-				$back.= '<input type="text" size="30" name="track['.$f["fieldId"].']" value=""/>';
-			} elseif ($f['type'] == 'a') {
-				$back.= "<tr><td>".$f['name']."</td><td>";
-				$back.= '<textarea cols="29" rows="7" name="track['.$f["fieldId"].']"></textarea>';
-			} elseif ($f['type'] == 'd') {
-				$list = split(',',$f['options']);
-				$back.= "<tr><td>".$f['name']."</td><td>";
-				$back.= '<select name="track['.$f["name"].']">';
-				foreach ($list as $item) {
-					$back.= '<option value="'.$item.'">'.$item.'</option>';
+		foreach ($flds['data'] as $f) {
+			if (in_array($f['name'],$outf)) {
+				if (in_array($f['name'],$optional)) {
+					$f['name'] = "<i>".$f['name']."</i>";
 				}
-				$back.= "</select>";
+				if ($f['type'] == 't') {
+					$back.= "<tr><td>".$f['name']."</td><td>";
+					$back.= '<input type="text" size="30" name="track['.$f["fieldId"].']" value=""/>';
+				} elseif ($f['type'] == 'a') {
+					$back.= "<tr><td>".$f['name']."</td><td>";
+					$back.= '<textarea cols="29" rows="7" name="track['.$f["fieldId"].']"></textarea>';
+				} elseif ($f['type'] == 'd') {
+					$list = split(',',$f['options']);
+					$back.= "<tr><td>".$f['name']."</td><td>";
+					$back.= '<select name="track['.$f["name"].']">';
+					foreach ($list as $item) {
+						$back.= '<option value="'.$item.'">'.$item.'</option>';
+					}
+					$back.= "</select>";
+				}
+				$back.= "</td></tr>";
 			}
-			$back.= "</td></tr>";
 		}
 		
 		$back.= "<tr><td></td><td><input type='submit' name='action' value='".$action."'></td></tr>";
 		$back.= "</table>";
-		$back.= "</form>";
+		$back.= "</form>~/np~";
 	} else {
 		$back = "No such id in trackers.";
 	}
