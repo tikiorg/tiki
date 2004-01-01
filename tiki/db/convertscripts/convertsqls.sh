46ef55e338144db66302423971f8795a13a22ffb
mose <mose@users.sourceforge.net>
2004-01-01 16:43:35 +0000

    updated convertscript for use with 1.9
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@3054 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/db/convertscripts/convertsqls.sh
+++ tiki/db/convertscripts/convertsqls.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-TIKISERVER="tiki.osgiliath.int"
+TIKISERVER="tikigmsih"
+VERSION="1.9"
 
 cd ..
-cp tiki.sql tiki-1.8-mysql.sql
+cp tiki.sql tiki-$VERSION-mysql.sql
 
 wget http://$TIKISERVER/db/convertscripts/mysql3topgsql72.php
 wget http://$TIKISERVER/db/convertscripts/mysql3tosybase.php
@@ -15,9 +16,11 @@ rm mysql3tosybase.php
 rm mysql3tosqlite.php
 rm mysql3tooci8.php
 
-mv tiki-1.8-mysql.sql_to_pgsql72.sql tiki-1.8-pgsql.sql
-mv tiki-1.8-mysql.sql_to_sybase.sql tiki-1.8-sybase.sql
-mv tiki-1.8-mysql.sql_to_sqlite.sql tiki-1.8-sqlite.sql
-mv tiki-1.8-mysql.sql_to_oci8.sql tiki-1.8-oci8.sql
+rm -f tiki-$VERSION-pgsql.sql tiki-$VERSION-sybase.sql tiki-$VERSION-sqlite.sql tiki-$VERSION-oci8.sql
+
+mv tiki-1.8-mysql.sql_to_pgsql72.sql tiki-$VERSION-pgsql.sql
+mv tiki-1.8-mysql.sql_to_sybase.sql tiki-$VERSION-sybase.sql
+mv tiki-1.8-mysql.sql_to_sqlite.sql tiki-$VERSION-sqlite.sql
+mv tiki-1.8-mysql.sql_to_oci8.sql tiki-$VERSION-oci8.sql
 
 cd convertscripts
