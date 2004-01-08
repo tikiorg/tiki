1ae0bcbdd01fd40f7723456e96ec9477132349e0
ggeller <ggeller@users.sourceforge.net>
2004-01-08 18:12:16 +0000

    Homework feature.  Just a stub for now.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@3111 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki-hw_teacher_last_changes.php
@@ -0,0 +1,11 @@
+<?php
+
+// Initialization
+
+// Check permissions
+
+print __FILE__."<br>";
+print "The homework feature is not yet implemented.  For the latest status visit:<br>";
+print '<a href="http://tikiwiki.org/tiki-index.php?page=Homework">http://tikiwiki.org/tiki-index.php?page=Homework</a><br>';
+
+?>
