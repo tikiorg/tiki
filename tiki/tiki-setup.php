8be739af60ec4d95a24ae9ae7a49fbcd70ba0d88
mose <mose@users.sourceforge.net>
2004-01-21 23:57:50 +0000

    added usertracker and grouptracker defaults
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@3357 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-setup.php
+++ tiki/tiki-setup.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-setup.php,v 1.192 2004-01-18 19:00:45 damosoft Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-setup.php,v 1.193 2004-01-21 23:57:50 mose Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -585,6 +585,10 @@ $t_use_db = 'y';
 $t_use_dir = '';
 $smarty->assign('t_use_db', $t_use_db);
 $smarty->assign('t_use_dir', $t_use_dir);
+$groupTracker = 'n';
+$smarty->assign('groupTracker', $groupTracker);
+$feature_trackers = 'n';
+$smarty->assign('userTracker', $userTracker);
 $feature_trackers = 'n';
 $smarty->assign('feature_trackers', $feature_trackers);
 
