4a7c692e9b9fcf5c77240f581c4884d7a61373d5
mose <mose@users.sourceforge.net>
2004-01-28 05:10:55 +0000

    removed a debug message
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@3439 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/trackers/trackerlib.php
+++ tiki/lib/trackers/trackerlib.php
@@ -249,7 +249,6 @@ class TrackerLib extends TikiLib {
 		$query = "select distinct ttif.`itemid` from `tiki_tracker_items` tti, `tiki_tracker_fields` ttf, `tiki_tracker_item_fields` ttif ";
 		$query.= " where tti.`trackerId`=ttf.`trackerId` and ttif.`fieldId`=ttf.`fieldId` and ttf.`trackerId`=? and ttf.`fieldId`=? and ttif.`value`=?";
 		$itemId = $this->getOne($query,array((int) $trackerId,$fieldId,$value));
-		var_dump($query);
 		return $itemId;
 	}
 
