fe7a0246de1ab1145b066ce519c9df22989f71dd
musus <musus@users.sourceforge.net>
2004-01-28 02:45:14 +0000

    replacing "br" tags with "br /" for xhtml 1.0 compliance
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@3429 b456876b-0849-0410-b77d-98878d47e9d5

--- categorize.php
+++ categorize.php
@@ -1,6 +1,5 @@
-<?php 
-
-// $Header: /cvsroot/tikiwiki/tiki/categorize.php,v 1.5 2003-11-16 00:17:16 xenfasa Exp $
+<?php
+// $Header: /cvsroot/tikiwiki/tiki/categorize.php,v 1.6 2004-01-28 02:43:25 musus Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
