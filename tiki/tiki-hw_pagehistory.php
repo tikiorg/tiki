05c180382f1e99fd672a5525fcbb0a1bd85a25a6
ggeller <ggeller@users.sourceforge.net>
2004-02-06 20:47:53 +0000

    Mostly working for students and teachers.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@3613 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-hw_pagehistory.php
+++ tiki/tiki-hw_pagehistory.php
@@ -1,121 +1,98 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-hw_pagehistory.php,v 1.1 2004-02-06 15:51:06 ggeller Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-hw_pagehistory.php,v 1.2 2004-02-06 20:47:53 ggeller Exp $
 
 // Copyright (c) 2004 George G. Geller
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-error_reporting (E_ALL);
+// Need tiki-hw_rollback.php
+
+error_reporting(E_ALL);
 
 // Initialization
 require_once ('tiki-setup.php');
-include_once ('lib/wiki/histlib.php');
 
 require_once("lib/homework/homeworklib.php");
 $homeworklib = new HomeworkLib($dbTiki);
 
 
-if ($feature_wiki != 'y') {
-  $smarty->assign('msg', tra("This feature is disabled").": feature_wiki");
-  $smarty->display("error.tpl");
-  die;
-}
-
-if ($feature_history != 'y') {
-  $smarty->assign('msg', tra("This feature is disabled").": feature_history");
+if ($feature_homework != 'y') {
+  $smarty->assign('msg', tra("This feature is disabled").": feature_homework");
   $smarty->display("error.tpl");
   die;
 }
 
 if (!isset($_REQUEST["id"])) {
-  $smarty->assign('msg', tra("No page indicated"));
+  $smarty->assign('msg', tra("No homework page indicated!"));
   $smarty->display("error.tpl");
   die;
 }
 
 $pageId = $_REQUEST["id"];
-
-/*
-// Get the page from the request var or default it to HomePage
-if (!isset($_REQUEST["page"])) {
-  $_REQUEST["page"] = "HomePage";
-}
-if (!isset($_REQUEST["page"])) {
-  $smarty->assign('msg', tra("No page indicated"));
-  $smarty->display("error.tpl");
-  die;
-} else {
-  $page = $_REQUEST["page"];
-  $smarty->assign_by_ref('page', $_REQUEST["page"]);
-}
-*/
-
-// $info = $tikilib->get_page_info($page);
-$status = $homeworklib->hw_page_fetch_by_id(&$info, $pageId, false);
-if ($status == "HW_INVALID_ID"){
-  $smarty->assign('msg', __FILE__.tra(" line ").__LINE__.", \ 
-    ".tra("Error: Invalid")." pageId ");
-  $smarty->display("error.tpl");
-  die;
-}
-$smarty->assign_by_ref('info', $info);
-
+$smarty->assign('pageId',$pageId);
 
 // Get the page data
 $status = $homeworklib->hw_page_fetch_by_id(&$page_data, $pageId, false);
-if ($status != 'HW_OK' && $status != 'HW_PAGE_LOCKED'){
-  $smarty->assign('msg', tra("No page indicated"));
+if (!($status == 'HW_OK' || $status == 'HW_PAGE_LOCKED')){
+  $smarty->assign('msg', __FILE__.tra(" line ").__LINE__.", \ 
+    ".tra("Error: Could not fetch page")." pageId ");
   $smarty->display("error.tpl");
   die;
 }
+$smarty->assign_by_ref('info', $page_data);
 
 // Get the assignment data
-$assignment_data = $homeworklib->get_assignment($page_data['assignmentId']);
-// Should check the return result
-// $ggg_tracer->out(__FILE__." line: ".__LINE__.' $assignment_data = ');
-// $ggg_tracer->outvar($assignment_data);
+$assignmentId = $page_data['assignmentId'];
+$smarty->assign("assignmentId",$assignmentId);
+$assignment_data = $homeworklib->get_assignment($assignmentId);
+if ($assignment_data == false){
+  $smarty->assign('msg', __FILE__.tra(" line ").__LINE__.", \ 
+    ".tra("Error: Could not fetch assignment ").$assignmentId);
+  $smarty->display("error.tpl");
+  die;
+}
 
 // Get the assignment title from the hw_assignment table.
 $_REQUEST["page"] = $assignment_data["title"];
 $page = $_REQUEST["page"];
 $smarty->assign_by_ref('page', $_REQUEST["page"]);
 
-include_once ("tiki-pagesetup.php");
-
 // Now check permissions to access this page
-if ($tiki_p_view != 'y') {
-  $smarty->assign('msg', tra("Permission denied you cannot browse this page history"));
+if ($tiki_p_hw_student != 'y') {
+  $smarty->assign('msg', tra("You must be a student to view this page!"));
   $smarty->display("error.tpl");
   die;
 }
 
-/*
-// If the page doesn't exist then display an error
-if (!$tikilib->page_exists($page)) {
-  $smarty->assign('msg', tra("Page cannot be found"));
-  $smarty->display("error.tpl");
-  die;
+// $ggg_tracer->out(__FILE__." line ".__LINE__.' $page_data = ');
+// $ggg_tracer->outvar($page_data);
+$studentName = $page_data["studentName"];
+$smarty->assign("studentName",$studentName);
+if ($tiki_p_hw_grader != 'y') {
+  if ($user != $page_data["studentName"]){
+	$smarty->assign('msg', tra("Students may only view their own work!"));
+	$smarty->display("error.tpl");
+	die;
+  }
 }
-*/
 
 if (isset($_REQUEST["delete"]) && isset($_REQUEST["hist"])) {
   check_ticket('page-history');
   foreach (array_keys($_REQUEST["hist"])as $version) {
-	$histlib->remove_version($_REQUEST["page"], $version);
+	$homeworklib->hw_page_remove_version($pageId, $version);
   }
 }
 
 $smarty->assign('source', 0);
 
-/*
 if (isset($_REQUEST['source'])) {
+  $ggg_tracer->outln(__FILE__." line: ".__LINE__.' $_REQUEST["source"] = '.$_REQUEST["source"]);
   $smarty->assign('source', $_REQUEST['source']);
   $version = $histlib->get_version($page, $_REQUEST["source"]);
   $smarty->assign('sourcev', nl2br(htmlentities($version["data"])));
 }
-*/
 
 // If we have to include a preview please show it
 $smarty->assign('preview', false);
