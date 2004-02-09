c73dff5d1428072e6d6e696fef20c4a341c86f9c
mose <mose@users.sourceforge.net>
2004-02-09 05:36:15 +0000

    added a plugin to list tracker items
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@3627 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/lib/wiki-plugins/wikiplugin_trackerlist.php
@@ -0,0 +1,174 @@
+<?php
+// Includes a tracker field
+// Usage:
+// {TRACKER()}{TRACKER}
+
+function wikiplugin_trackerlist_help() {
+	$help = tra("Displays the output of a tracker content").":\n";
+	$help.= "~np~{TRACKERLIST(trackerId=>1,fields=>login:email:name)}Notice{TRACKERLIST}~/np~";
+	return $help;
+}
+function wikiplugin_trackerlist($data, $params) {
+	global $smarty, $tikilib, $dbTiki, $userlib, $tiki_p_admin, $maxRecords, $_REQUEST, $tiki_p_view_trackers;
+
+	extract ($params);
+
+	if (!isset($trackerId)) {
+		$smarty->assign('msg', tra("missing tracker ID for plugin TRACKER"));
+		return $smarty->fetch("error_simple.tpl");
+	} else {
+
+		if ($userlib->object_has_one_permission($trackerId, 'tracker')) {
+			$smarty->assign('individual', 'y');
+			if ($tiki_p_admin != 'y') {
+				$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'trackers');
+				foreach ($perms["data"] as $perm) {
+					$permName = $perm["permName"];
+					if ($userlib->object_has_permission($user, $trackerId, 'tracker', $permName)) {
+						$$permName = 'y';
+						$smarty->assign("$permName", 'y');
+					} else {
+						$$permName = 'n';
+						$smarty->assign("$permName", 'n');
+					}
+				}
+			}
+		}
+		if ($tiki_p_view_trackers != 'y') {
+			$smarty->assign('msg', tra("You dont have permission to use this feature"));
+			return $smarty->fetch("error_simple.tpl");
+		}
+
+		include "lib/trackers/trackerlib.php";
+		if (!isset($fields)) {
+			$smarty->assign('msg', tra("missing fields list"));
+			return $smarty->fetch("error_simple.tpl");
+		} else {
+			$listfields = split(':',$fields);
+		}
+		$query_array = array();
+		$quarray = array();
+		parse_str($_SERVER['QUERY_STRING'],$query_array);
+		
+		if (!isset($showtitle)) {
+			$showtitle = "n";
+		}
+		$smarty->assign_by_ref('showtitle', $showtitle);
+		
+		if (!isset($showlinks)) {
+			$showdesc = "n";
+		}
+		$smarty->assign_by_ref('showlinks', $showlinks);
+		
+		if (!isset($showdesc)) {
+			$showdesc = "n";
+		}
+		$smarty->assign_by_ref('showdesc', $showdesc);
+		
+		if (!isset($status)) {
+			$status = "o";
+		}
+		$tr_status = $status;
+		$smarty->assign_by_ref('tr_status', $tr_status);
+			
+		if (isset($_REQUEST['tr_sort_mode'])) {
+			$query_array['tr_sort_mode'] = $_REQUEST['tr_sort_mode'];
+			$tr_sort_mode = $_REQUEST['tr_sort_mode'];
+		} else {
+			if (!isset($sort_mode)) {
+				$sort_mode = "";
+			}
+			$tr_sort_mode = $sort_mode;
+		}
+		$smarty->assign_by_ref('sort_mode', $tr_sort_mode);
+		
+		if (!isset($max)) {
+			$max = $maxRecords;
+		}
+
+		if (isset($_REQUEST['tr_offset'])) {
+			$query_array['tr_offset'] = $_REQUEST['tr_offset'];
+			$tr_offset = $_REQUEST['tr_offset'];
+		} else {
+			$tr_offset = 0;
+		}
+		$smarty->assign_by_ref('tr_offset',$tr_offset);
+
+		if (isset($_REQUEST["tr_initial"])) {
+			$query_array['tr_initial'] = $_REQUEST['tr_initial'];
+			$tr_initial = $_REQUEST["tr_initial"];
+		} else {
+			$tr_initial = '';
+		}
+		$smarty->assign_by_ref('tr_initial', $tr_initial);
+		$smarty->assign('initials', split(' ','a b c d e f g h i j k l m n o p q r s t u v w x y z'));
+
+		if (!isset($filterfield)) {
+			$filterfield = '';
+		}
+		$smarty->assign_by_ref('filterfield',$filterfield);
+
+		if (!isset($filtervalue)) {
+			$filtervalue = '';
+		}
+		$smarty->assign_by_ref('filterfield',$filtervalue);
+
+		$status_types = $trklib->status_types();
+		$smarty->assign('status_types', $status_types);
+
+		$tracker_info = $trklib->get_tracker($trackerId);
+		$tracker_info = array_merge($tracker_info,$trklib->get_tracker_options($trackerId));
+		$smarty->assign_by_ref('tracker_info', $tracker_info);
+
+		$allfields = $trklib->list_tracker_fields($trackerId, 0, -1, 'position_asc', '');
+
+		for ($i = 0; $i < count($allfields["data"]); $i++) {
+			if (in_array($allfields["data"][$i]['fieldId'],$listfields)) {
+				$passfields["{$allfields["data"][$i]['fieldId']}"] = $allfields["data"][$i];
+			}
+		}
+		$smarty->assign_by_ref('fields', $passfields);
+		
+		$items = $trklib->list_items($trackerId, $tr_offset, $max, $tr_sort_mode, $passfields, $filterfield, $filtervalue, $tr_status, $tr_initial);
+		//var_dump($items);
+
+		$cant_pages = ceil($items["cant"] / $max);
+		$smarty->assign_by_ref('cant_pages', $cant_pages);
+		$smarty->assign('actual_page', 1 + ($tr_offset / $max));
+
+		if ($items["cant"] > ($tr_offset + $max)) {
+			$smarty->assign('tr_next_offset', $tr_offset + $max);
+		} else {
+			$smarty->assign('tr_next_offset', -1);
+		}
+		if ($tr_offset > 0) {
+			$smarty->assign('tr_prev_offset', $tr_offset - $max);
+		} else {
+			$smarty->assign('tr_prev_offset', -1);
+		}
+		$smarty->assign_by_ref('items', $items["data"]);
+		$smarty->assign('daformat', $tikilib->get_long_date_format()." ".tra("at")." %H:%M"); 
+		
+		$tracker = $tikilib->get_tracker($trackerId,0,-1);
+
+		foreach ($query_array as $k=>$v) {
+			$quarray[] = urlencode($k) ."=". urlencode($v);
+		}
+		if (is_array($quarray)) {
+			$query_string = implode("&amp;",$quarray);
+		} else {
+			$quering_string = '';
+		}
+		$smarty->assign('query_string', $query_string);
+		
+		if (!$tracker) {
+			$smarty->assign('msg', tra("Error in tracker ID"));
+			return $smarty->fetch("error_simple.tpl");
+		} else {
+			return "~np~".$smarty->fetch('tiki-plugin_trackerlist.tpl')."~/np~";
+		}
+	}
+	return $back;
+}
+
+?>
