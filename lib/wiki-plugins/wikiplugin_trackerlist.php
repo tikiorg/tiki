aab53321dfa802cfe32088669cbfb2aec28c941c
mose <mose@users.sourceforge.net>
2004-02-17 01:02:16 +0000

    added a protection using isPublic option for tracker field
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@3690 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_trackerlist.php
+++ lib/wiki-plugins/wikiplugin_trackerlist.php
@@ -110,49 +110,55 @@ function wikiplugin_trackerlist($data, $params) {
 		$allfields = $trklib->list_tracker_fields($trackerId, 0, -1, 'position_asc', '');
 
 		for ($i = 0; $i < count($allfields["data"]); $i++) {
-			if (in_array($allfields["data"][$i]['fieldId'],$listfields)) {
+			if (in_array($allfields["data"][$i]['fieldId'],$listfields) and $allfields["data"][$i]['isPublic'] == 'y') {
 				$passfields["{$allfields["data"][$i]['fieldId']}"] = $allfields["data"][$i];
 			}
 		}
 		$smarty->assign_by_ref('fields', $passfields);
 		
-		$items = $trklib->list_items($trackerId, $tr_offset, $max, $tr_sort_mode, $passfields, $filterfield, $filtervalue, $tr_status, $tr_initial);
-		//var_dump($items);
+		if (count($passfields)) {
+			$items = $trklib->list_items($trackerId, $tr_offset, $max, $tr_sort_mode, $passfields, $filterfield, $filtervalue, $tr_status, $tr_initial);
+			
+			//var_dump($items);
 
-		$cant_pages = ceil($items["cant"] / $max);
-		$smarty->assign_by_ref('cant_pages', $cant_pages);
-		$smarty->assign('actual_page', 1 + ($tr_offset / $max));
+			$cant_pages = ceil($items["cant"] / $max);
+			$smarty->assign_by_ref('cant_pages', $cant_pages);
+			$smarty->assign('actual_page', 1 + ($tr_offset / $max));
 
-		if ($items["cant"] > ($tr_offset + $max)) {
-			$smarty->assign('tr_next_offset', $tr_offset + $max);
-		} else {
-			$smarty->assign('tr_next_offset', -1);
-		}
-		if ($tr_offset > 0) {
-			$smarty->assign('tr_prev_offset', $tr_offset - $max);
-		} else {
-			$smarty->assign('tr_prev_offset', -1);
-		}
-		$smarty->assign_by_ref('items', $items["data"]);
-		$smarty->assign('daformat', $tikilib->get_long_date_format()." ".tra("at")." %H:%M"); 
-		
-		$tracker = $tikilib->get_tracker($trackerId,0,-1);
+			if ($items["cant"] > ($tr_offset + $max)) {
+				$smarty->assign('tr_next_offset', $tr_offset + $max);
+			} else {
+				$smarty->assign('tr_next_offset', -1);
+			}
+			if ($tr_offset > 0) {
+				$smarty->assign('tr_prev_offset', $tr_offset - $max);
+			} else {
+				$smarty->assign('tr_prev_offset', -1);
+			}
+			$smarty->assign_by_ref('items', $items["data"]);
+			$smarty->assign('daformat', $tikilib->get_long_date_format()." ".tra("at")." %H:%M"); 
+			
+			$tracker = $tikilib->get_tracker($trackerId,0,-1);
 
-		foreach ($query_array as $k=>$v) {
-			$quarray[] = urlencode($k) ."=". urlencode($v);
-		}
-		if (is_array($quarray)) {
-			$query_string = implode("&amp;",$quarray);
+			foreach ($query_array as $k=>$v) {
+				$quarray[] = urlencode($k) ."=". urlencode($v);
+			}
+			if (is_array($quarray)) {
+				$query_string = implode("&amp;",$quarray);
+			} else {
+				$quering_string = '';
+			}
+			$smarty->assign('query_string', $query_string);
+			
+			if (!$tracker) {
+				$smarty->assign('msg', tra("Error in tracker ID"));
+				return "~np~".$smarty->fetch("error_simple.tpl")."~/np~";
+			} else {
+				return "~np~".$smarty->fetch('tiki-plugin_trackerlist.tpl')."~/np~";
+			}
 		} else {
-			$quering_string = '';
-		}
-		$smarty->assign('query_string', $query_string);
-		
-		if (!$tracker) {
-			$smarty->assign('msg', tra("Error in tracker ID"));
-			return $smarty->fetch("error_simple.tpl");
-		} else {
-			return "~np~".$smarty->fetch('tiki-plugin_trackerlist.tpl')."~/np~";
+			$smarty->assign('msg', tra("No field indicated"));
+			return "~np~".$smarty->fetch("error_simple.tpl")."~/np~";
 		}
 	}
 	return $back;
