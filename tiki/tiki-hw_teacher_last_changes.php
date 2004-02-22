9256d49cfedaaaf05b8621de3ead3d6eed0c744e
ggeller <ggeller@users.sourceforge.net>
2004-02-22 14:59:02 +0000

    Change error message.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@3773 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-hw_teacher_last_changes.php
+++ tiki/tiki-hw_teacher_last_changes.php
@@ -1,11 +1,7 @@
 <?php
 
-// Initialization
-
-// Check permissions
-
 print __FILE__."<br>";
-print "The homework feature is not yet implemented.  For the latest status visit:<br>";
+print "This portion of the  homework feature is not yet implemented.  For the latest status visit:<br>";
 print '<a href="http://tikiwiki.org/tiki-index.php?page=Homework">http://tikiwiki.org/tiki-index.php?page=Homework</a><br>';
 
 ?>
