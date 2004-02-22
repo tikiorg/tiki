ef52c3a8826be20f36afdb393d034c83da8adea1
ggeller <ggeller@users.sourceforge.net>
2004-02-22 14:53:43 +0000

    Namespace issues, code cleanup.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@3771 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-hw_pagehistory.php
+++ tiki-hw_pagehistory.php
@@ -1,13 +1,12 @@
 <?php
+// $Header: /cvsroot/tikiwiki/tiki/tiki-hw_pagehistory.php,v 1.3 2004-02-22 14:53:43 ggeller Exp $
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-hw_pagehistory.php,v 1.2 2004-02-06 20:47:53 ggeller Exp $
+// 20040207 Fixed typo in error message, clean up comments.
 
 // Copyright (c) 2004 George G. Geller
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-
-// Need tiki-hw_rollback.php
 
 error_reporting(E_ALL);
 
@@ -16,7 +15,6 @@ require_once ('tiki-setup.php');
 
 require_once("lib/homework/homeworklib.php");
 $homeworklib = new HomeworkLib($dbTiki);
-
 
 if ($feature_homework != 'y') {
   $smarty->assign('msg', tra("This feature is disabled").": feature_homework");
@@ -29,15 +27,14 @@ if (!isset($_REQUEST["id"])) {
   $smarty->display("error.tpl");
   die;
 }
-
 $pageId = $_REQUEST["id"];
 $smarty->assign('pageId',$pageId);
 
 // Get the page data
 $status = $homeworklib->hw_page_fetch_by_id(&$page_data, $pageId, false);
 if (!($status == 'HW_OK' || $status == 'HW_PAGE_LOCKED')){
-  $smarty->assign('msg', __FILE__.tra(" line ").__LINE__.", \ 
-    ".tra("Error: Could not fetch page")." pageId ");
+  $smarty->assign('msg', __FILE__.tra(" line ").__LINE__.",
+    ".tra("Error: Could not fetch page")." $pageId ");
   $smarty->display("error.tpl");
   die;
 }
@@ -48,7 +45,7 @@ $assignmentId = $page_data['assignmentId'];
 $smarty->assign("assignmentId",$assignmentId);
 $assignment_data = $homeworklib->get_assignment($assignmentId);
 if ($assignment_data == false){
-  $smarty->assign('msg', __FILE__.tra(" line ").__LINE__.", \ 
+  $smarty->assign('msg', __FILE__.tra(" line ").__LINE__.",
     ".tra("Error: Could not fetch assignment ").$assignmentId);
   $smarty->display("error.tpl");
   die;
@@ -66,8 +63,6 @@ if ($tiki_p_hw_student != 'y') {
   die;
 }
 
-// $ggg_tracer->out(__FILE__." line ".__LINE__.' $page_data = ');
-// $ggg_tracer->outvar($page_data);
 $studentName = $page_data["studentName"];
 $smarty->assign("studentName",$studentName);
 if ($tiki_p_hw_grader != 'y') {
@@ -81,7 +76,7 @@ if ($tiki_p_hw_grader != 'y') {
 if (isset($_REQUEST["delete"]) && isset($_REQUEST["hist"])) {
   check_ticket('page-history');
   foreach (array_keys($_REQUEST["hist"])as $version) {
-	$homeworklib->hw_page_remove_version($pageId, $version);
+    $homeworklib->hw_page_remove_version($pageId, $version);
   }
 }
 
@@ -90,57 +85,43 @@ $smarty->assign('source', 0);
 if (isset($_REQUEST['source'])) {
   $ggg_tracer->outln(__FILE__." line: ".__LINE__.' $_REQUEST["source"] = '.$_REQUEST["source"]);
   $smarty->assign('source', $_REQUEST['source']);
-  $version = $histlib->get_version($page, $_REQUEST["source"]);
+  $version = $homeworklib->hw_page_get_version($pageId, $_REQUEST["source"]);
   $smarty->assign('sourcev', nl2br(htmlentities($version["data"])));
 }
 
 // If we have to include a preview please show it
 $smarty->assign('preview', false);
 
-/*
 if (isset($_REQUEST["preview"])) {
-  $version = $histlib->get_version($page, $_REQUEST["preview"]);
+  $version = $homeworklib->hw_page_get_version($pageId, $_REQUEST["preview"]);
   $version["data"] = $tikilib->parse_data($version["data"]);
-  
   if ($version) {
     $smarty->assign_by_ref('preview', $version);
-    
     $smarty->assign_by_ref('version', $_REQUEST["preview"]);
   }
 }
-*/
 
 $smarty->assign('diff2', 'n');
 
-/*
 if (isset($_REQUEST["diff2"])) {
-  $diff = $histlib->get_version($page, $_REQUEST["diff2"]);
-
-  $info = $tikilib->get_page_info($page);
-  $html = $tikilib->diff2($diff["data"], $info["data"]);
+  $diff = $homeworklib->hw_page_get_version($pageId, $_REQUEST["diff2"]);
+  $html = $tikilib->diff2($diff["data"], $page_data["data"]);
   $smarty->assign('diffdata', $html);
   $smarty->assign('diff2', 'y');
   $smarty->assign_by_ref('version', $_REQUEST["diff2"]);
 }
-*/
 
-// We are going to change this to "compare" instead of diff
 $smarty->assign('diff', false);
 
-/*
 if (isset($_REQUEST["diff"])) {
-  $diff = $histlib->get_version($page, $_REQUEST["diff"]);
-
+  $diff = $homeworklib->hw_page_get_version($pageId, $_REQUEST["diff"]);
   $diff["data"] = $tikilib->parse_data($diff["data"]);
   $smarty->assign_by_ref('diff', $diff["data"]);
-  $info = $tikilib->get_page_info($page);
-  $pdata = $tikilib->parse_data($info["data"]);
+  $pdata = $tikilib->parse_data($page_data["data"]);
   $smarty->assign_by_ref('parsed', $pdata);
   $smarty->assign_by_ref('version', $_REQUEST["diff"]);
 }
-*/
 
-// $history = $histlib->get_page_history($page);
 $history = $homeworklib->hw_page_get_history($pageId);
 $smarty->assign_by_ref('history', $history);
 
