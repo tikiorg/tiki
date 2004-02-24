0c4dab8e4463aee5d3a95d34c4eb6454c8959345
lfagundes <lfagundes@users.sourceforge.net>
2004-02-24 08:16:17 +0000

    Added the admin page to control scoring in karma system and the database structure. There are some suggested actions to be scored in database, but triggers for them in code are still missing
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@3804 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/tiki-admin_score.php
@@ -0,0 +1,14 @@
+<?php
+require_once('tiki-setup.php');
+include_once('lib/score/scorelib.php');
+
+if (isset($_REQUEST['events']) && is_array($_REQUEST['events'])) {
+    $scorelib->update_events($_REQUEST['events']);
+}
+
+$smarty->assign('events',$scorelib->get_all_events());
+
+$smarty->assign('mid','tiki-admin_score.tpl');
+$smarty->display("styles/$style_base/tiki.tpl");
+
+?>
