8843602583efaa87f8a9989151b5c4a4b247eb5f
florianlink <florianlink@users.sourceforge.net>
2004-03-03 07:25:29 +0000

    Added post edit filter example
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@3894 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-handlers.php
+++ tiki-handlers.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-handlers.php,v 1.4 2003-08-07 04:33:57 rossta Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-handlers.php,v 1.5 2004-03-03 07:25:29 florianlink Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -16,5 +16,6 @@
 //
 //$tikilib->add_pre_handler("foo");
 //$tikilib->add_pos_handler("foo");
+//$tikilib->add_postedit_handler("foo");
 
 ?>
