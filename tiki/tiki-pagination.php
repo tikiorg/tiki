dae172f57962f73badf0a08e71227e2e943b3655
mose <mose@users.sourceforge.net>
2004-03-07 21:28:50 +0000

    added some devel help in comments
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@3938 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-pagination.php
+++ tiki/tiki-pagination.php
@@ -1,6 +1,14 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-pagination.php,v 1.1 2004-03-07 19:35:58 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-pagination.php,v 1.2 2004-03-07 21:28:50 mose Exp $
+
+// you need to setup $cant var to the total number of items in list
+// you need to setup an array including all GET variable that have to be passed to link
+// typical declaration is $urlquery = array('find'=>$find,'trackerId'=>$trackerId);
+// the smarty var $urlquery has to be declared.
+// the url is built using $smarty.server.PHP_SELF
+// the template has to {include file="tiki-pagination.tpl"}
+// see tiki-view_tracker.php and tpl for a working example
 
 $cant_pages = ceil($cant / $maxRecords);
 $smarty->assign_by_ref('cant_pages', $cant_pages);
