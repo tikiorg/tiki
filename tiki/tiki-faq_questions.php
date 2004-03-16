c10e210bd97f71130ac16b6e2e9dc37699add53b
chealer <chealer@users.sourceforge.net>
2004-03-16 07:47:45 +0000

    Minor : Error message read "menu" instead of "FAQ".
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4028 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-faq_questions.php
+++ tiki/tiki-faq_questions.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-faq_questions.php,v 1.12 2003-12-28 20:12:52 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-faq_questions.php,v 1.13 2004-03-16 07:47:45 chealer Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -26,7 +26,7 @@ if ($tiki_p_admin_faqs != 'y') {
 }
 
 if (!isset($_REQUEST["faqId"])) {
-	$smarty->assign('msg', tra("No menu indicated"));
+	$smarty->assign('msg', tra("No FAQ indicated"));
 
 	$smarty->display("error.tpl");
 	die;
