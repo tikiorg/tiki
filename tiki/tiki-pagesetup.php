3facc41ed096d9264f4b14c6b08a9b427c670ca5
chealer <chealer@users.sourceforge.net>
2004-03-18 22:23:04 +0000

    Major rewrite, almost from scratch :S Wiki permissions are now determined dynamically with a DB query (were previously very outdated) object_has_one_permission is not anywhere called in a foreach (buggy coding, was keeping from getting the optimization looked for). tiki_p_admin_wiki is now handled as if no custom permission would exist (bugfix and optimization) Estimated performance improvement : average 5 queries per Wiki-related pageview. I noted I can't figure out why a check on $page is required.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4058 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-pagesetup.php
+++ tiki/tiki-pagesetup.php
@@ -1,47 +1,34 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-pagesetup.php,v 1.7 2004-01-04 14:18:18 redflo Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-pagesetup.php,v 1.8 2004-03-18 22:23:04 chealer Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-# $Header: /cvsroot/tikiwiki/tiki/tiki-pagesetup.php,v 1.7 2004-01-04 14:18:18 redflo Exp $
-$check = isset($page);
+//This file determines permissions in the Wiki area when page-specific permissions are set
 
-$ppps = array(
-	'tiki_p_view',
-	'tiki_p_edit',
-	'tiki_p_rollback',
-	'tiki_p_remove',
-	'tiki_p_rename',
-	'tiki_p_lock',
-	'tiki_p_admin_wiki',
-	'tiki_p_view_attachments'
-);
+//I can't figure out how calling this page without $page set would make sense Chealer 20040318
+if ($tiki_p_admin != 'y' && isset($page) && $userlib->object_has_one_permission($page, 'wiki page')) {
+	$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'wiki');
+	if ($userlib->object_has_permission($user, $page, 'wiki page', 'tiki_p_admin_wiki')) {
+		foreach ($perms["data"] as $perm) {
+			$perm = $perm["permName"];
 
-// If we are in a page then get individual permissions
-foreach ($ppps as $perm) {
-		if ($tiki_p_admin != 'y') {
-			// Check for individual permissions if this is a page
-			if ($check) {
-				if ($userlib->object_has_one_permission($page, 'wiki page')) {
-					if ($userlib->object_has_permission($user, $page, 'wiki page', $perm)) {
-						$$perm = 'y';
-
-						$smarty->assign("$perm", 'y');
-					} else {
-						$$perm = 'n';
-
-						$smarty->assign("$perm", 'n');
-					}
-				}
-			}
-		} else {
-			$$perm = 'y';
-
-			$smarty->assign("$perm", 'y');
+  			$smarty->assign("$perm", 'y');
+    			$$perm = 'y';
 		}
+	} else {
+		foreach ($perms["data"] as $perm) {
+			$perm = $perm["permName"];
+			if ($userlib->object_has_permission($user, $page, 'wiki page', $perm)) {
+ 	 			$smarty->assign("$perm", 'y');
+	    			$$perm = 'y';
+			} else {
+ 	 			$smarty->assign("$perm", 'n');
+	    			$$perm = 'n';
+			}
+		}
+	}
 }
-
 ?>
