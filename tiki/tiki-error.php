e5470c878730a0fcc4d19b1df2e8dc6abf85bcd5
mose <mose@users.sourceforge.net>
2004-03-23 22:39:25 +0000

    Instant-Auto-Merge from BRANCH to HEAD
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4085 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-error.php
+++ tiki/tiki-error.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-error.php,v 1.6 2003-12-28 20:12:52 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-error.php,v 1.7 2004-03-23 22:39:25 mose Exp $
 
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -11,8 +11,9 @@ require_once ('tiki-setup.php');
 
 ask_ticket('error');
 
+if (!isset($_REQUEST["error"])) $_REQUEST["error"] = tra('unknown error');
+
 // Display the template
 $smarty->assign('msg', strip_tags($_REQUEST["error"]));
 $smarty->display("error.tpl");
-
 ?>
