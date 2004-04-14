a6af4c90601d4dc38f1ee5b3ff4c1f5ccb6ecca7
damosoft <damosoft@users.sourceforge.net>
2004-04-14 14:48:11 +0000

    Tweaking...
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4196 b456876b-0849-0410-b77d-98878d47e9d5

--- _phplayers/index.php
+++ _phplayers/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/_phplayers/index.php,v 1.2 2004-04-14 14:46:41 damosoft Exp $
+// $Header: /cvsroot/tikiwiki/_phplayers/index.php,v 1.3 2004-04-14 14:48:11 damosoft Exp $
 
 // Copyright (c) 2002-2004, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -8,7 +8,7 @@
 
 // This redirects to the sites root to prevent directory browsing
 
-header ("location: ../tiki-index.php");
+header ("location: ../index.php");
 die;
 
 ?>
