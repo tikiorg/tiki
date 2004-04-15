79fcb68486ac9e9fe4a40912346e6d60a3182735
mose <mose@users.sourceforge.net>
2004-04-15 23:00:18 +0000

    reverted to the previous last_changes because the new one raises load very nastily in some cases
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4208 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/modules/mod-last_modif_pages.php
+++ tiki/modules/mod-last_modif_pages.php
@@ -1,6 +1,7 @@
 <?php
 
-$ranking = $tikilib->last_major_pages($module_rows);
+#$ranking = $tikilib->last_major_pages($module_rows);
+$ranking = $tikilib->last_pages($module_rows);
 
 $smarty->assign('modLastModif', $ranking);
 $smarty->assign('maxlen', isset($module_params["maxlen"]) ? $module_params["maxlen"] : 0);
