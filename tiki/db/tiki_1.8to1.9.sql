eddbe9422a88c7aeaa665365b403445282baca59
sylvieg <sylvieg@users.sourceforge.net>
2004-04-16 15:42:19 +0000

    typo
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4222 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/db/tiki_1.8to1.9.sql
+++ tiki/db/tiki_1.8to1.9.sql
@@ -1,4 +1,4 @@
-# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.8to1.9.sql,v 1.43 2004-04-16 06:06:27 franck Exp $
+# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.8to1.9.sql,v 1.44 2004-04-16 15:42:19 sylvieg Exp $
 
 # The following script will update a tiki database from verion 1.7 to 1.8
 # 
@@ -314,5 +314,5 @@ ALTER TABLE `tiki_untranslated` CHANGE `lang` `lang` char(16) NOT NULL default '
 
 #added on 2004-04-16 franck
 ALTER TABLE `tiki_quicktags` ADD `tagcategory` CHAR( 255 ) AFTER `tagicon` ;
-ALTER TABLE `tiki_quicktags` ADD INDEX `tagcategory` (`tagcategory`)
+ALTER TABLE `tiki_quicktags` ADD INDEX `tagcategory` (`tagcategory`);
 UPDATE `tiki_quicktags` set `tagcategory`='wiki';
