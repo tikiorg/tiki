e3331fd96519329369fc325c89744fd7165b2ba7
sylvieg <sylvieg@users.sourceforge.net>
2004-04-16 15:33:33 +0000

    use smarty cache for the user menus: a template/cache is now created form each user menu displayed: the cache memorizes the menu item values so no more translation - better performance (the only problem is that we need one refresh to update the modules when you modify the menu - working on it)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@4221 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.menu.php
+++ lib/smarty_tiki/function.menu.php
@@ -9,12 +9,15 @@ function smarty_function_menu($params, &$smarty)
         $smarty->trigger_error("assign: missing id");
         return;
     }
-    $menu_info = $tikilib->get_menu($id);
-    $channels = $tikilib->list_menu_options($id,0,-1,'position_asc','');
-    $smarty->assign('menu_info',$menu_info);
-    $smarty->assign('channels',$channels["data"]);
-    
-    $smarty->display('tiki-user_menu.tpl');
+    $smarty->caching = true;
+    if (!$smarty->is_cached('tiki-user_menu.tpl', "$id")) {
+       $menu_info = $tikilib->get_menu($id);
+       $channels = $tikilib->list_menu_options($id,0,-1,'position_asc','');
+       $smarty->assign('menu_info',$menu_info);
+       $smarty->assign('channels',$channels["data"]);
+    }
+    $smarty->display('tiki-user_menu.tpl', "$id");
+    $smarty->caching = false;
 }
 
 /* vim: set expandtab: */
