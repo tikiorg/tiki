b46b0a6fe83953781022a8247d533f75ebc3b0d1
mose <mose@users.sourceforge.net>
2004-04-26 12:02:21 +0000

    fixed the jscalendar in french
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4302 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-jscalendar.php
+++ tiki/tiki-jscalendar.php
@@ -1,7 +1,12 @@
 <?php
+//this script may only be included - so its better to die if called directly.
+if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
+  header("location: index.php");
+}	
+
 if (isset($feature_jscalendar) and $feature_jscalendar == 'y') {
 	$jslang['en'] = "en";
-	$jslang['fr'] = "fr";
+	$jslang['fr'] = "fr-utf8";
   $smarty->assign('uses_jscalendar', 'y'); 
   $smarty->assign('feature_jscalendar', 'y'); 
 	if (isset($language) and isset($jslang["$language"])) {
