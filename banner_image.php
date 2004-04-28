0fe001a52dab3e456c33ff8270150b99e84fa7d0
mose <mose@users.sourceforge.net>
2004-04-28 13:05:14 +0000

    added a cache system in banner image
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@4334 b456876b-0849-0410-b77d-98878d47e9d5

--- banner_image.php
+++ banner_image.php
@@ -1,12 +1,12 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/banner_image.php,v 1.7 2004-03-28 07:32:22 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/banner_image.php,v 1.8 2004-04-28 13:05:14 mose Exp $
 
 // Copyright (c) 2002-2004, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-# $Header: /cvsroot/tikiwiki/tiki/banner_image.php,v 1.7 2004-03-28 07:32:22 mose Exp $
+# $Header: /cvsroot/tikiwiki/tiki/banner_image.php,v 1.8 2004-04-28 13:05:14 mose Exp $
 
 // application to display an image from the database with 
 // option to resize the image dynamically creating a thumbnail on the fly.
@@ -14,20 +14,35 @@ if (!isset($_REQUEST["id"])) {
 	die;
 }
 
-include_once("lib/init/initlib.php");
-include_once ('db/tiki-db.php');
-include_once ('lib/tikilib.php');
-$tikilib = new Tikilib($dbTiki);
-include_once ('lib/banners/bannerlib.php');
+$bannercachefile = "temp" . "/banner.".$_REQUEST["id"];
 
-if (!isset($bannerlib)) {
-	$bannerlib = new BannerLib($dbTiki);
+if (is_file($bannerchefile) and (!isset($_REQUEST["reload"]))) {
+	$size = getimagesize($bannercachefile);
+	$type = $size['mime'];
+} else {
+	include_once("lib/init/initlib.php");
+	include_once ('db/tiki-db.php');
+	include_once ('lib/tikilib.php');
+	$tikilib = new Tikilib($dbTiki);
+	include_once ('lib/banners/bannerlib.php');
+	if (!isset($bannerlib)) {
+		$bannerlib = new BannerLib($dbTiki);
+	}
+	$data = $bannerlib->get_banner($_REQUEST["id"]);
+	$type = $data["imageType"];
+	$data = $data["imageData"];
+	if ($data) {
+		$fp = fopen($bannercachefile,"wb");
+		fputs($fp,$data);
+		fclose($fp);
+	}
 }
 
-$data = $bannerlib->get_banner($_REQUEST["id"]);
-$type = $data["imageType"];
-$data = $data["imageData"];
 header ("Content-type: $type");
-echo $data;
+if (is_file($bannercachefile)) {
+	readfile($bannercachefile);
+} else {
+	echo $data;
+}
 
 ?>
