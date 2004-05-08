7f8308f815efadd97b5b67bb3b2498f33e1df0d1
ggeller <ggeller@users.sourceforge.net>
2004-05-08 22:11:48 +0000

    Don't show UTC when time zone is local.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4503 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/smarty_tiki/modifier.tiki_date_format.php
+++ tiki/lib/smarty_tiki/modifier.tiki_date_format.php
@@ -15,22 +15,23 @@ require_once $smarty->_get_plugin_filepath('shared','make_timestamp');
 function smarty_modifier_tiki_date_format($string, $format = "%b %e, %Y", $default_date=null, $tra_format=null)
 {
 	global $tikilib, $user;
-    $dc =& $tikilib->get_date_converter($user);
-
-    $disptime = $dc->getDisplayDateFromServerDate($string);
-    if ($dc->getTzName() != "UTC") $format = preg_replace("/ ?%Z/","",$format);
-    else $format = preg_replace("/%Z/","UTC",$format);
-
-    // strftime doesn't do translations right
-	//return strftime($format, $disptime);
-
-	global $language; //$language = $tikilib->get_preference("language", "en");
+	$dc =& $tikilib->get_date_converter($user);
+	
+	$disptime = $dc->getDisplayDateFromServerDate($string);
+	if ($tikilib->get_display_offset($user)) {
+		$format = preg_replace("/ ?%Z/","",$format);
+	}
+	else {
+		$format = preg_replace("/%Z/","UTC",$format);
+	}
+	
+	global $language;
 	if ($tikilib->get_preference("language", "en") != $language && $tra_format) {
 		$format = $tra_format;
 	}
-
-    $date = new Date($disptime);
-    return $date->format($format);
+	
+	$date = new Date($disptime);
+	return $date->format($format);
 }
 
 /* vim: set expandtab: */
