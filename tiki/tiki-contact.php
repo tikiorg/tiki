d447a51062486bf6fd79418f4e8decda4d6e62ac
russwilde <russwilde@users.sourceforge.net>
2004-05-13 08:16:05 +0000

    Contact Anonymous working *properly*.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4554 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-contact.php
+++ tiki/tiki-contact.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-contact.php,v 1.11 2004-03-28 07:32:23 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-contact.php,v 1.12 2004-05-13 08:16:05 russwilde Exp $
 
 // Copyright (c) 2002-2004, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -32,7 +32,25 @@ $smarty->assign('email', $email);
 
 if ($user == '' and $contact_anon == 'y') {
   $user = 'anonymous';
+	$smarty->assign('sent', 0);
+	if (isset($_REQUEST['send'])) {
+		check_ticket('contact');
+		$smarty->assign('sent', 1);
+		$message = '';
+		// Validation:
+		// must have a subject or body non-empty (or both)
+		if (empty($_REQUEST['subject']) && empty($_REQUEST['body'])) {
+			$smarty->assign('message', tra('ERROR: you must include a subject or a message at least'));
+			$smarty->display("tiki.tpl");
+			die;
+		}
+		$messulib->post_message($contact_user, $user, $_REQUEST['to'],
+			'', $_REQUEST['subject'], $_REQUEST['body'], $_REQUEST['priority']);
+		$message = tra('Message sent to'). ':' . $contact_user . '<br/>';
+		$smarty->assign('message', $message);
+	}
 }
+
 
 if ($user and $feature_messages == 'y' and $tiki_p_messages == 'y') {
 	$smarty->assign('sent', 0);
