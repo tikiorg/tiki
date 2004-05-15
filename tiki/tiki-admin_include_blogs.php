6b266f97b5ece2af29a5b7bd58248929b63742b5
chealer <chealer@users.sourceforge.net>
2004-05-15 22:40:28 +0000

    Fixed user column in blogs listing
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4579 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-admin_include_blogs.php
+++ tiki/tiki-admin_include_blogs.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_include_blogs.php,v 1.7 2004-03-29 21:26:28 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_include_blogs.php,v 1.8 2004-05-15 22:40:27 chealer Exp $
 
 // Copyright (c) 2002-2004, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -20,148 +20,44 @@ if (isset($_REQUEST["blogset"]) && isset($_REQUEST["homeBlog"])) {
 
 if (isset($_REQUEST["blogfeatures"])) {
 	check_ticket('admin-inc-blogs');
-	if (isset($_REQUEST["feature_blog_rankings"]) && $_REQUEST["feature_blog_rankings"] == "on") {
-		$tikilib->set_preference("feature_blog_rankings", 'y');
+	$pref_toggles = array(
+	"feature_blog_rankings",
+	"blog_spellcheck",
+	"feature_blog_comments",
+	"feature_blogposts_comments"
+	);
 
-		$smarty->assign("feature_blog_rankings", 'y');
-	} else {
-		$tikilib->set_preference("feature_blog_rankings", 'n');
-
-		$smarty->assign("feature_blog_rankings", 'n');
+	foreach ($pref_toggles as $toggle) {
+		simple_set_toggle ($toggle);
 	}
 
-	if (isset($_REQUEST["blog_spellcheck"]) && $_REQUEST["blog_spellcheck"] == "on") {
-		$tikilib->set_preference("blog_spellcheck", 'y');
-
-		$smarty->assign("blog_spellcheck", 'y');
-	} else {
-		$tikilib->set_preference("blog_spellcheck", 'n');
-
-		$smarty->assign("blog_spellcheck", 'n');
-	}
-
-	if (isset($_REQUEST["feature_blog_comments"]) && $_REQUEST["feature_blog_comments"] == "on") {
-		$tikilib->set_preference("feature_blog_comments", 'y');
-
-		$smarty->assign("feature_blog_comments", 'y');
-	} else {
-		$tikilib->set_preference("feature_blog_comments", 'n');
-
-		$smarty->assign("feature_blog_comments", 'n');
-	}
-
-	if (isset($_REQUEST["feature_blogposts_comments"]) && $_REQUEST["feature_blogposts_comments"] == "on") {
-		$tikilib->set_preference("feature_blogposts_comments", 'y');
-
-		$smarty->assign("feature_blogposts_comments", 'y');
-	} else {
-		$tikilib->set_preference("feature_blogposts_comments", 'n');
-
-		$smarty->assign("feature_blogposts_comments", 'n');
-	}
-
-	$tikilib->set_preference("blog_list_order", $_REQUEST["blog_list_order"]);
-	$tikilib->set_preference("blog_list_user", $_REQUEST["blog_list_user"]);
-	$smarty->assign('blog_list_order', $_REQUEST["blog_list_order"]);
-	$smarty->assign('blog_list_user', $_REQUEST['blog_list_user']);
+	simple_set_value ("blog_list_order");
+	simple_set_value ("blog_list_user");
 }
 
 if (isset($_REQUEST['bloglistconf'])) {
 	check_ticket('admin-inc-blogs');
-	if (isset($_REQUEST["blog_list_title"]) && $_REQUEST["blog_list_title"] == "on") {
-		$tikilib->set_preference("blog_list_title", 'y');
+	$pref_toggles = array(
+	"blog_list_title",
+	"blog_list_description",
+	"blog_list_activity",
+	"blog_list_visits",
+	"blog_list_posts",
+	"blog_list_created",
+	"blog_list_lastmodif"
+	);
 
-		$smarty->assign("blog_list_title", 'y');
-	} else {
-		$tikilib->set_preference("blog_list_title", 'n');
-
-		$smarty->assign("blog_list_title", 'n');
+	foreach ($pref_toggles as $toggle) {
+		simple_set_toggle ($toggle);
 	}
 
-	if (isset($_REQUEST["blog_list_description"]) && $_REQUEST["blog_list_description"] == "on") {
-		$tikilib->set_preference("blog_list_description", 'y');
-
-		$smarty->assign("blog_list_description", 'y');
-	} else {
-		$tikilib->set_preference("blog_list_description", 'n');
-
-		$smarty->assign("blog_list_description", 'n');
-	}
-
-	if (isset($_REQUEST["blog_list_activity"]) && $_REQUEST["blog_list_activity"] == "on") {
-		$tikilib->set_preference("blog_list_activity", 'y');
-
-		$smarty->assign("blog_list_activity", 'y');
-	} else {
-		$tikilib->set_preference("blog_list_activity", 'n');
-
-		$smarty->assign("blog_list_activity", 'n');
-	}
-
-	if (isset($_REQUEST["blog_list_visits"]) && $_REQUEST["blog_list_visits"] == "on") {
-		$tikilib->set_preference("blog_list_visits", 'y');
-
-		$smarty->assign("blog_list_visits", 'y');
-	} else {
-		$tikilib->set_preference("blog_list_visits", 'n');
-
-		$smarty->assign("blog_list_visits", 'n');
-	}
-
-	if (isset($_REQUEST["blog_list_posts"]) && $_REQUEST["blog_list_posts"] == "on") {
-		$tikilib->set_preference("blog_list_posts", 'y');
-
-		$smarty->assign("blog_list_posts", 'y');
-	} else {
-		$tikilib->set_preference("blog_list_posts", 'n');
-
-		$smarty->assign("blog_list_posts", 'n');
-	}
-
-	if (isset($_REQUEST["blog_list_lastmodif"]) && $_REQUEST["blog_list_lastmodif"] == "on") {
-		$tikilib->set_preference("blog_list_lastmodif", 'y');
-
-		$smarty->assign("blog_list_lastmodif", 'y');
-	} else {
-		$tikilib->set_preference("blog_list_lastmodif", 'n');
-
-		$smarty->assign("blog_list_lastmodif", 'n');
-	}
-
-	if (isset($_REQUEST["blog_list_user"]) && $_REQUEST["blog_list_user"] == "on") {
-		$tikilib->set_preference("blog_list_user", 'y');
-
-		$smarty->assign("blog_list_user", 'y');
-	} else {
-		$tikilib->set_preference("blog_list_user", 'n');
-
-		$smarty->assign("blog_list_user", 'n');
-	}
-
-	if (isset($_REQUEST["blog_list_created"]) && $_REQUEST["blog_list_created"] == "on") {
-		$tikilib->set_preference("blog_list_created", 'y');
-
-		$smarty->assign("blog_list_created", 'y');
-	} else {
-		$tikilib->set_preference("blog_list_created", 'n');
-
-		$smarty->assign("blog_list_created", 'n');
-	}
+	simple_set_value ("blog_list_user");
 }
 
 if (isset($_REQUEST["blogcomprefs"])) {
 	check_ticket('admin-inc-blogs');
-	if (isset($_REQUEST["blog_comments_per_page"])) {
-		$tikilib->set_preference("blog_comments_per_page", $_REQUEST["blog_comments_per_page"]);
-
-		$smarty->assign('blog_comments_per_page', $_REQUEST["blog_comments_per_page"]);
-	}
-
-	if (isset($_REQUEST["blog_comments_default_ordering"])) {
-		$tikilib->set_preference("blog_comments_default_ordering", $_REQUEST["blog_comments_default_ordering"]);
-
-		$smarty->assign('blog_comments_default_ordering', $_REQUEST["blog_comments_default_ordering"]);
-	}
+	simple_set_value ("blog_comments_per_page");
+	simple_set_value ("blog_comments_default_ordering");
 }
 ask_ticket('admin-inc-blogs');
 
