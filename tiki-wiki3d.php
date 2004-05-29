bbe82aee57f853e075bbff32ea238b853e294077
lfagundes <lfagundes@users.sourceforge.net>
2004-05-29 20:50:39 +0000

    removed server-specific code to work in development server
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@4621 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-wiki3d.php
+++ tiki-wiki3d.php
@@ -8,9 +8,7 @@ $base_url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 $base_url = preg_replace('/\/tiki-wiki3d.php.+$/','',$base_url);
 
-// This is just during early development for it to work
-// in my domain to remote users, should be removed later
-$smarty->assign('base_url',preg_replace('/kriconet.prv/','kriconet.com.br',$base_url));
+$smarty->assign('base_url',$base_url);
 
 $smarty->assign('page',$_REQUEST['page']);
 $smarty->display('tiki-wiki3d.tpl');
