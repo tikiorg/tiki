184ca597e05d3b936e46405ad419a11e810474dd
lphuberdeau <lphuberdeau@users.sourceforge.net>
2004-05-30 15:32:00 +0000

    Added a plugin for TikiSheet. Note: Totally untested.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@4629 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_sheet.php
@@ -0,0 +1,47 @@
+<?php
+/* Tiki-Wiki plugin example 
+ *
+ * This is an example plugin to let you know how to create
+ * a plugin. Plugins are called using the syntax
+ * {NAME(params)}content{NAME}
+ * Name must be in uppercase!
+ * params is in the form: name=>value,name2=>value2 (don't use quotes!)
+ * If the plugin doesn't use params use {NAME()}content{NAME}
+ *
+ * The function will receive the plugin content in $data and the params
+ * in the asociative array $params (using extract to pull the arguments
+ * as in the example is a good practice)
+ * The function returns some text that will replace the content in the
+ * wiki page.
+ */
+function wikiplugin_sheet_help() {
+	return tra("TikiSheet").":<br />~np~{SHEET(id=>)}".tra("Sheet Heading")."{SHEET}~/np~";
+}
+function wikiplugin_example($data, $params) {
+	extract ($params);
+
+	if (!isset($id)) {
+		return ("<b>missing face parameter for plugin</b><br/>");
+	}
+
+	if( !class_exists( 'TikiSheet' ) )
+		require "lib/sheet/grid.php";
+
+	// Build required objects
+	$sheet = &new TikiSheet;
+	$db = &new TikiSheetDatabaseHandler( $id );
+	$out = &new TikiSheetOutputHandler( $data );
+
+	// Fetch sheet from database
+	$sheet->import( $db );
+	
+	// Grab sheet output
+	ob_start();
+	$sheet->export( $out );
+	$ret = ob_get_contents();
+	ob_end_clean();
+	
+	return $ret;
+}
+
+?>
