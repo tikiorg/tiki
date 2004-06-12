329eee006ff316527762c85f67193708ed37cb75
damosoft <damosoft@users.sourceforge.net>
2004-06-12 07:03:05 +0000

    Instant-Auto-Merge from BRANCH to HEAD
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@4770 b456876b-0849-0410-b77d-98878d47e9d5

--- lang/langmapping.php
+++ lang/langmapping.php
@@ -23,6 +23,7 @@ $langmapping = array(
     'hr' => array(  'Hrvatski',     tra("Croatian")   ),
     'it' => array(  'Italiano',     tra("Italian")      ),
     'ja' => array(  '日本語',    tra("Japanese")     ),
+    'ko' => array(  '한국말',    tra("Korean")     ),
     'nl' => array(  'Nederlands',   tra("Dutch")        ),
     'no' => array(  'Norwegian',    tra("Norwegian")    ),
     'pl' => array(  'Polish',       tra("Polish")       ),
--- lib/webmail/mail.php
+++ lib/webmail/mail.php
@@ -1,4 +1,3 @@
-<<<<<<< mail.php
 <?php
 /* TikiMail class
  *
