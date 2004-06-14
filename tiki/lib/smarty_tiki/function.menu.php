9b5688a03884a29bc78544fc6e15f49d09a15c9e
lfagundes <lfagundes@users.sourceforge.net>
2004-06-14 17:19:59 +0000

    working on an equivalent of {sortlinks} for custom menus
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4822 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/smarty_tiki/function.menu.php
+++ tiki/lib/smarty_tiki/function.menu.php
@@ -10,15 +10,42 @@ function smarty_function_menu($params, &$smarty)
         return;
     }
     $smarty->caching = true;
-    if (!$smarty->is_cached('tiki-user_menu.tpl', "$id")) {
+    if (true || !$smarty->is_cached('tiki-user_menu.tpl', "$id")) {
        $menu_info = $tikilib->get_menu($id);
        $channels = $tikilib->list_menu_options($id,0,-1,'position_asc','');
+
+       
+       // Let's sort sorted sections options
+       $sorted_channels = array();
+       $channels = $channels['data'];
+
+       for ($i=0; $i < sizeof($channels); $i++) {
+	   $sorted_channels[$i] = $channels[$i];
+	   if ($sorted_channels[$i]['type'] == 'r') { // sorted section
+       	       $sorted_channels[$i]['type'] = 's'; // common section, let's make it transparent
+	       $i++;
+	       $section = array();
+	       while ($i < sizeof($channels) && $channels[$i]['type'] == 'o') {
+		   $section[] = $channels[$i];
+		   $i++;
+	       }
+	       $i--;
+	       usort($section, "compare_menu_options");
+	       $sorted_channels = array_merge($sorted_channels, $section);
+	   }
+       }
+       $smarty->assign('channels',$sorted_channels);
+       
+       //       $smarty->assign('channels',$channels['data']);
+
        $smarty->assign('menu_info',$menu_info);
-       $smarty->assign('channels',$channels["data"]);
+       
     }
     $smarty->display('tiki-user_menu.tpl', "$id");
     $smarty->caching = false;
 }
+
+function compare_menu_options($a, $b) { return strcmp(tra($a['name']), tra($b['name'])); }
 
 /* vim: set expandtab: */
 
