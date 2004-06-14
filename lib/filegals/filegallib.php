23da2497e5c38dd285a24e8c92ebe7a406401887
teedog <teedog@users.sourceforge.net>
2004-06-14 20:01:25 +0000

    fixed major bug: $tiki_p_view_categories should only be checked if an object is actually categorized.  thx rlpowell!
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@4827 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/filegals/filegallib.php
+++ lib/filegals/filegallib.php
@@ -126,12 +126,15 @@ class FileGalLib extends TikiLib {
 		    	unset($tiki_p_view_categories); // unset this var in case it was set previously
 		    	$perms_array = $categlib->get_object_categories_perms($user, 'file gallery', $res['galleryId']);
 		    	if ($perms_array) {
+		    		$is_categorized = TRUE;
 			    	foreach ($perms_array as $perm => $value) {
 			    		$$perm = $value;
 			    	}
+		    	} else {
+		    		$is_categorized = FALSE;
 		    	}
 
-		    	if (isset($tiki_p_view_categories) && $tiki_p_view_categories != 'y') {
+		    	if ($is_categorized && isset($tiki_p_view_categories) && $tiki_p_view_categories != 'y') {
 		    		$add = FALSE;
 		    	}
 		    }
