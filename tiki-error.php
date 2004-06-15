2ffab03127fc2a59bd48c9bc680dcdc7d86054c5
lfagundes <lfagundes@users.sourceforge.net>
2004-06-15 17:38:25 +0000

    support for custom error pages according to HTTP_REFERER
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@4848 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-error.php
+++ tiki-error.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-error.php,v 1.8 2004-03-28 07:32:23 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-error.php,v 1.9 2004-06-15 17:38:25 lfagundes Exp $
 
 // Copyright (c) 2002-2004, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -12,6 +12,14 @@ require_once ('tiki-setup.php');
 ask_ticket('error');
 
 if (!isset($_REQUEST["error"])) $_REQUEST["error"] = tra('unknown error');
+
+// This can be useful for putting custom code inside error page.
+// ie: in error.tpl {$referer) will hold "login" if user came from tiki-login.php
+// if this gets useful we can integrate with tickets, this was just a hack to show to LarsKl
+// during a chat.
+if (preg_match('/tiki-([a-z_]+?)\.php/', $_SERVER['HTTP_REFERER'], $m)) {
+    $smarty->assign('referer',$m[1]);
+}
 
 // Display the template
 $smarty->assign('msg', strip_tags($_REQUEST["error"]));
