97cf9fc8d5024c8bc46dd07968a0fc9bf247f204
mose <mose@users.sourceforge.net>
2004-06-20 15:58:18 +0000

    added a filter for apachelogs
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@4968 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/tail/taillib.php
+++ tiki/lib/tail/taillib.php
@@ -5,7 +5,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   header("location: index.php");
 }
 
-/* $Id: taillib.php,v 1.4 2004-03-29 21:26:40 mose Exp $*/
+/* $Id: taillib.php,v 1.5 2004-06-20 15:58:18 mose Exp $*/
 function tail_filter_irc(&$item, $key) {
 	# here is a sample line
 	# [06-24-03/05:13] <mose> how is everyone ?
@@ -24,13 +24,40 @@ function tail_filter_irc(&$item, $key) {
 
 function tail_filter_apache_errorlog(&$item, $key) {
 	$search[] = "/\/home\/mose\/var\/tikicvs/";
-
 	$replace[] = "";
 	$search[] = "/^\[([^\]]*)\] \[[^\]]*\] \[[^\]]*\] (.*): (.*)$/";
 	$replace[] = "<div href=\"#\" onmouseover='return overlib(\"<b>\\2</b><br/>\\1\");' onmouseout='return nd();' class=\"linkmenu\">\\3</div>";
 	$search[] = "/\[|\]/";
 	$replace[] = "";
 	$item = preg_replace($search, $replace, $item);
+}
+
+function tail_filter_mask(&$item,$key) {
+	$search[] = "/^\[([^\]]*)\] \[[^\]]*\] \[[^\]]*\] (.*): (.*)$/";
+	$replace[] = "<div href=\"#\" onmouseover='return overlib(\"<b>\\2</b><br/>\\1\");' onmouseout='return nd();' class=\"linkmenu\">\\3</div>";
+
+	$search[] = "~on line ([0-9]*)~";
+	$replace[] = "on line <b'>\\1</b>";
+	
+	$search[] = "~\[([^\]]*)\]~";
+	$replace[] = "<span style='font-size:x-small;'>\\1</span>";
+	
+	$search[] = "~/usr/local/tiki18/([^ ]*)~";
+	$replace[] = "tikiwiki.org: <b>\\1</b>";
+	$search[] = "~/home/multitiki/([^ ]*)~";
+	$replace[] = "multitiki: <b>\\1</b>";
+	$search[] = "~/var/www/([^ ]*)~";
+	$replace[] = "www: <b>\\1</b>";
+	$search[] = "~/home/mose/([^ ]*)~";
+	$replace[] = "mose: <b>\\1</b>";
+	
+	$search[] = "~^(.*PHP Fatal error.*)$~";
+	$replace[] = "<div style='background-color:#eeaa99;border-bottom:1px solid #999966;'>\\1</div>";
+	$search[] = "~^(.*PHP Warning.*)$~";
+	$replace[] = "<div style='background-color:#eedd99;border-bottom:1px solid #999966;'>\\1</div>";
+	$search[] = "~^(.*PHP Notice.*)$~";
+	$replace[] = "<div style='background-color:#eeeebb;border-bottom:1px solid #999966;'>\\1</div>";
+  $item = preg_replace($search, $replace, $item);
 }
 
 function tail_read($file, $lines) {
