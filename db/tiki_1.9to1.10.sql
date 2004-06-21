065ad0ef36131b1239bfb8b6ce6e86c9f568d42a
damosoft <damosoft@users.sourceforge.net>
2004-06-21 13:39:04 +0000

    new update script
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5024 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ db/tiki_1.9to1.10.sql
@@ -0,0 +1,17 @@
+# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.1 2004-06-21 13:39:04 damosoft Exp $
+                                                                                               
+# The following script will update a tiki database from verion 1.9 to 1.10
+#
+# To execute this file do the following:
+#
+# $ mysql -f dbname < tiki_1.9to1.10.sql
+#
+# where dbname is the name of your tiki database.
+#
+# For example, if your tiki database is named tiki (not a bad choice), type:
+#
+# $ mysql -f tiki < tiki_1.9to1.10.sql
+#
+# You may execute this command as often as you like,
+# and may safely ignore any error messages that appear.
+
