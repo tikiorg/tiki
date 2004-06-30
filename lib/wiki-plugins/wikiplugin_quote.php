5e018b854cf14fa26a0707c74330edb7a09390e5
teedog <teedog@users.sourceforge.net>
2004-06-30 22:38:11 +0000

    added a nestable QUOTE plugin to emulate the [quote][/quote] BBCode (from phpBB/vBB etc) that should replace the use of ">"; QUOTE plugin is parsed in forums even if wiki syntax is disabled the QUOTE plugin is currently exactly the same as BOX; will improve it tomorrow
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5073 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_quote.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_quote.php,v 1.1 2004-06-30 22:38:10 teedog Exp $
+ *
+ * TikiWiki QUOTE plugin.
+ * 
+ * Syntax:
+ * 
+ *  {QUOTE([title=>Title],[bg=>color|#999fff],[width=>num[%]],[align=>left|right|center])}
+ *   Content inside box
+ *  {QUOTE}
+ * 
+ */
+function wikiplugin_quote_help() {
+	return tra("Quote text by surrounding the text with a box, like the [QUOTE] BBCode").":<br />~np~{QUOTE(title=>Title, bg=>color, width=>num[%], align=>left|right|center)}".tra("text")."{QUOTE}~/np~";
+}
+
+function wikiplugin_quote($data, $params) {
+	/* set default values for some args */
+	$title = tra("Message box");
+	
+	// Remove first <ENTER> if exists...
+	if (substr($data, 0, 2) == "\r\n") $data = substr($data, 2);
+    
+	extract ($params);
+	$w    = (isset($width)) ? " width=\"$width\""  : "";
+	$bg   = (isset($bg))    ? " background:$bg;" : "";
+    $al   = (isset($align) && ($align == 'right' || $align == "center")) ? " align=\"$align\"" : "";
+    
+	$begin  = "<table$al$w><tr><td$w><div class='cbox'".(strlen($bg) > 0 ? " style='$bg'" : "").">";
+    $begin .= "<div class='cbox-title'>$title</div><div class='cbox-data'".(strlen($bg) > 0 ? " style=\"$bg\"" : "").">";
+	$end = "</div></div></td></tr></table>";
+		// Prepend any newline char with br
+		$data = preg_replace("/\\n/", "<br />\n", $data);
+    // Insert "\n" at data begin if absent (so start-of-line-sensitive syntaxes will be parsed OK)
+    if (substr($data, 0, 1) != "\n") $data = "\n".$data;
+	return $begin . $data . $end;
+}
+
+?>
