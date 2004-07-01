f7e34b39bc73f4ebb5d2296ecb9453618d91978d
telenieko <telenieko@users.sourceforge.net>
2004-07-01 22:54:18 +0000

    plugin BOX is now smarty based
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@5092 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/wiki-plugins/wikiplugin_box.php
+++ tiki/lib/wiki-plugins/wikiplugin_box.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_box.php,v 1.10 2004-06-17 21:47:14 ohertel Exp $
+ * $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_box.php,v 1.11 2004-07-01 22:54:18 telenieko Exp $
  *
  * Tiki-Wiki BOX plugin.
  * 
@@ -11,30 +11,38 @@
  *  {BOX}
  * 
  */
+
 function wikiplugin_box_help() {
 	return tra("Insert theme styled box on wiki page").":<br />~np~{BOX(title=>Title, bg=>color, width=>num[%], align=>left|right|center)}".tra("text")."{BOX}~/np~";
 }
 
 function wikiplugin_box($data, $params) {
+	global $smarty;
+	
 	/* set default values for some args */
 	$title = tra("Message box");
 	
 	// Remove first <ENTER> if exists...
-	if (substr($data, 0, 2) == "\r\n") $data = substr($data, 2);
-    
+	if (substr($data, 0, 2) == "\r\n") 
+	$data = substr($data, 2);
+	
 	extract ($params);
-	$w    = (isset($width)) ? " width=\"$width\""  : "";
-	$bg   = (isset($bg))    ? " background:$bg;" : "";
-    $al   = (isset($align) && ($align == 'right' || $align == "center")) ? " align=\"$align\"" : "";
-    
-	$begin  = "<table$al$w><tr><td$w><div class='cbox'".(strlen($bg) > 0 ? " style='$bg'" : "").">";
-    $begin .= "<div class='cbox-title'>$title</div><div class='cbox-data'".(strlen($bg) > 0 ? " style=\"$bg\"" : "").">";
-	$end = "</div></div></td></tr></table>";
-		// Prepend any newline char with br
-		$data = preg_replace("/\\n/", "<br />\n", $data);
-    // Insert "\n" at data begin if absent (so start-of-line-sensitive syntaxes will be parsed OK)
-    if (substr($data, 0, 1) != "\n") $data = "\n".$data;
-	return $begin . $data . $end;
+	
+	$smarty->assign('plugin_box_align', $align ? $align : '');
+	$smarty->assign('plugin_box_width', $width ? $width : '');
+	$smarty->assign('plugin_box_bg',  $bg ? $bg : '');
+	
+	$smarty->assign('plugin_box_title', $title);
+	
+	// Prepend any newline char with br
+	$data = preg_replace("/\\n/", "<br />\n", $data);
+	// Insert "\n" at data begin if absent (so start-of-line-sensitive syntaxes will be parsed OK)
+	if (substr($data, 0, 1) != "\n") 
+    	$data = "\n".$data;
+	
+	$smarty->assign('plugin_box_data', $data);
+	
+	return $smarty->fetch('plugins/plugin-box.tpl');
 }
 
 ?>
