3173b9710ce17c194491f1d0096547cd944a9178
teedog <teedog@users.sourceforge.net>
2004-07-01 16:43:17 +0000

    further improvements to QUOTE plugin
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5077 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_quote.php
+++ lib/wiki-plugins/wikiplugin_quote.php
@@ -1,40 +1,38 @@
 <?php
 /*
- * $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_quote.php,v 1.3 2004-07-01 16:09:43 teedog Exp $
+ * $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_quote.php,v 1.4 2004-07-01 16:43:17 teedog Exp $
  *
  * TikiWiki QUOTE plugin.
  * 
  * Syntax:
  * 
- *  {QUOTE([title=>Title],[bg=>color|#999fff],[width=>num[%]],[align=>left|right|center])}
+ *  {QUOTE([title=>Title])}
  *   Content inside box
  *  {QUOTE}
  * 
  */
 function wikiplugin_quote_help() {
-	return tra("Quote text by surrounding the text with a box, like the [QUOTE] BBCode").":<br />~np~{QUOTE(title=>Title, bg=>color, width=>num[%], align=>left|right|center)}".tra("text")."{QUOTE}~/np~";
+	return tra("Quote text by surrounding the text with a box, like the [QUOTE] BBCode").":<br />~np~{QUOTE(title=>Title)}".tra("text")."{QUOTE}~/np~";
 }
 
 function wikiplugin_quote($data, $params) {
 	/* set default values for some args */
 	$title = tra("Quote:");
-	$width = '100%';
 	
 	// Remove first <ENTER> if exists...
-	if (substr($data, 0, 2) == "\r\n") $data = substr($data, 2);
+//	if (substr($data, 0, 2) == "\r\n") $data = substr($data, 2);
+	// trim space/returns from beginning and end
+	$data = trim($data);
     
 	extract ($params);
-	$w    = (isset($width)) ? " width=\"$width\""  : "";
-	$bg   = (isset($bg))    ? " background:$bg;" : "";
-    $al   = (isset($align) && ($align == 'right' || $align == 'center')) ? " align=\"$align\"" : "";
     
-	$begin  = "<table$al$w><tr><td$w><div class='quote'".(strlen($bg) > 0 ? " style='$bg'" : "").">";
-    $begin .= "$title";
-	$end = "</div></td></tr></table>";
+	$begin  = "<div class='quoteheader'>";
+    $begin .= "$title</div><div class='quotebody'>";
+	$end = "</div>";
 		// Prepend any newline char with br
 		$data = preg_replace("/\\n/", "<br />", $data);
     // Insert "\n" at data begin if absent (so start-of-line-sensitive syntaxes will be parsed OK)
-    if (substr($data, 0, 1) != "\n") $data = "\n".$data;
+//    if (substr($data, 0, 1) != "\n") $data = "\n".$data;
 	return $begin . $data . $end;
 }
 
