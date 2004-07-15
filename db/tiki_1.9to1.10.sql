bb73bb39c92b7553cec36b7a30d3296125d93a7c
giottomx <giottomx@users.sourceforge.net>
2004-07-15 20:25:27 +0000

    added database tables for the Opinion-Network
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5149 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki_1.9to1.10.sql
+++ db/tiki_1.9to1.10.sql
@@ -1,4 +1,4 @@
-# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.3 2004-07-02 16:05:12 teedog Exp $
+# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.4 2004-07-15 20:25:27 giottomx Exp $
                                                                                                
 # The following script will update a tiki database from verion 1.9 to 1.10
 #
@@ -16,3 +16,44 @@
 # and may safely ignore any error messages that appear.
 
 INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_wiki_pageid','n');
+
+#
+# Tables of the Opinion-Network
+#
+
+DROP TABLE IF EXISTS tiki_opnet_question;
+CREATE TABLE tiki_opnet_question (
+id INT( 10 ) NOT NULL AUTO_INCREMENT ,
+which_formtype INT(10) NOT NULL,
+question_str VARCHAR( 100 ) NOT NULL ,
+PRIMARY KEY ( id ) 
+);
+
+DROP TABLE IF EXISTS tiki_opnet_formtype;
+CREATE TABLE tiki_opnet_formtype (
+id INT( 10 ) NOT NULL AUTO_INCREMENT ,
+name VARCHAR( 30 ) NOT NULL ,
+PRIMARY KEY ( id ) 
+);
+
+DROP TABLE IF EXISTS tiki_opnet_answer;
+CREATE TABLE tiki_opnet_answer (
+id INT( 10 ) NOT NULL AUTO_INCREMENT ,
+id_question INT( 10 ) NOT NULL ,
+id_filledform INT( 10 ) NOT NULL ,
+value INT( 10 ) NOT NULL ,
+PRIMARY KEY ( id ) 
+);
+
+DROP TABLE IF EXISTS tiki_opnet_filledform;
+CREATE TABLE tiki_opnet_filledform (
+id INT( 10 ) NOT NULL AUTO_INCREMENT ,
+who INT( 10 ) NOT NULL ,
+about_who INT( 10 ) NOT NULL ,
+which_form INT( 10 ) NOT NULL ,
+PRIMARY KEY ( id ) 
+);
+
+#
+# Opinion-Network tables END
+#
