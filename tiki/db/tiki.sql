f5636e290fc7e14e726f46c96afe22e17eea286e
giottomx <giottomx@users.sourceforge.net>
2004-07-15 19:13:51 +0000

    added database tables for the Opinion-Network
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@5143 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/db/tiki.sql
+++ tiki/db/tiki.sql
@@ -1,6 +1,6 @@
 # $Rev$
-# $Date: 2004-07-01 00:07:03 $
-# $Author: damosoft $
+# $Date: 2004-07-15 19:13:51 $
+# $Author: giottomx $
 # $Name: not supported by cvs2svn $
 # phpMyAdmin MySQL-Dump
 # version 2.5.1
@@ -4688,4 +4688,45 @@ CREATE TABLE tiki_users_score (
 
 #
 # Community tables end
+#
+
+#
+# Tables of the Opinion-Network
+#
+
+DROP TABLE IF EXISTS tiki_opnet_question;
+CREATE TABLE tiki_opnet_question (
+id INT( 10 ) NOT NULL AUTO_INCREMENT ,
+which_formtype INT(10) NOT NULL,
+question_str VARCHAR( 100 ) NOT NULL ,
+PRIMARY KEY ( id ) 
+);
+
+DROP TABLE IF EXISTS tiki_opnet_formtype;
+CREATE TABLE tiki_opnet_formtype (
+id INT( 10 ) NOT NULL AUTO_INCREMENT ,
+name VARCHAR( 30 ) NOT NULL ,
+PRIMARY KEY ( id ) 
+);
+
+DROP TABLE IF EXISTS tiki_opnet_answer;
+CREATE TABLE tiki_opnet_answer (
+id INT( 10 ) NOT NULL AUTO_INCREMENT ,
+id_question INT( 10 ) NOT NULL ,
+id_filledform INT( 10 ) NOT NULL ,
+value INT( 10 ) NOT NULL ,
+PRIMARY KEY ( id ) 
+);
+
+DROP TABLE IF EXISTS tiki_opnet_filledform;
+CREATE TABLE tiki_opnet_filledform (
+id INT( 10 ) NOT NULL AUTO_INCREMENT ,
+who INT( 10 ) NOT NULL ,
+about_who INT( 10 ) NOT NULL ,
+which_form INT( 10 ) NOT NULL ,
+PRIMARY KEY ( id ) 
+);
+
+#
+# Opinion-Network tables END
 #
