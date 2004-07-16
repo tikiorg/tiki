cda25a96d00f89391468d3f729cada7d66e06c6e
teedog <teedog@users.sourceforge.net>
2004-07-16 20:56:22 +0000

    dropping tables in the upgrade script is probably a bad idea
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@5165 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/db/tiki_1.9to1.10.sql
+++ tiki/db/tiki_1.9to1.10.sql
@@ -1,4 +1,4 @@
-# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.6 2004-07-16 20:51:01 giottomx Exp $
+# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.7 2004-07-16 20:56:22 teedog Exp $
                                                                                                
 # The following script will update a tiki database from verion 1.9 to 1.10
 #
@@ -21,7 +21,6 @@ INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_wiki_pageid','n
 # Tables of the Opinion-Network
 #
 
-DROP TABLE IF EXISTS tiki_opnet_question;
 CREATE TABLE tiki_opnet_question (
 id INT( 10 ) NOT NULL AUTO_INCREMENT ,
 which_formtype INT(10) NOT NULL,
@@ -29,7 +28,6 @@ question_str VARCHAR( 100 ) NOT NULL ,
 PRIMARY KEY ( id ) 
 );
 
-DROP TABLE IF EXISTS tiki_opnet_formtype;
 CREATE TABLE tiki_opnet_formtype (
 id INT( 10 ) NOT NULL AUTO_INCREMENT ,
 name VARCHAR( 30 ) NOT NULL ,
@@ -37,7 +35,6 @@ timestamp DATE NOT NULL,
 PRIMARY KEY ( id ) 
 );
 
-DROP TABLE IF EXISTS tiki_opnet_answer;
 CREATE TABLE tiki_opnet_answer (
 id INT( 10 ) NOT NULL AUTO_INCREMENT ,
 id_question INT( 10 ) NOT NULL ,
@@ -46,7 +43,6 @@ value TEXT NOT NULL ,
 PRIMARY KEY ( id ) 
 );
 
-DROP TABLE IF EXISTS tiki_opnet_filledform;
 CREATE TABLE tiki_opnet_filledform (
 id INT( 10 ) NOT NULL AUTO_INCREMENT ,
 who INT( 10 ) NOT NULL ,
