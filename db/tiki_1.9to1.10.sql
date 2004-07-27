84ad6f9600eae20d399e2815b4d6cc1f6a233200
teedog <teedog@users.sourceforge.net>
2004-07-27 21:12:54 +0000

    added tiki_p_wiki_view_header permission: users have it by default
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5215 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki_1.9to1.10.sql
+++ db/tiki_1.9to1.10.sql
@@ -1,4 +1,4 @@
-# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.13 2004-07-27 21:09:29 teedog Exp $
+# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.14 2004-07-27 21:12:53 teedog Exp $
                                                                                                
 # The following script will update a tiki database from verion 1.9 to 1.10
 #
@@ -19,6 +19,8 @@ INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('feature_wiki_pageid','n
 INSERT IGNORE INTO users_permissions (permName, permDesc, level, type) VALUES ('tiki_p_view_wiki_history', 'Can view wiki page history', 'registered', 'wiki');
 INSERT IGNORE INTO users_permissions (permName, permDesc, level, type) VALUES ('tiki_p_wiki_view_author', 'Can view wiki page authors', 'basic', 'wiki');
 INSERT IGNORE INTO users_grouppermissions (groupName, permName) VALUES ('Anonymous', 'tiki_p_wiki_view_author');
+INSERT IGNORE INTO users_permissions (permName, permDesc, level, type) VALUES ('tiki_p_wiki_view_header', 'Can view page wiki page headers, like pagename, description, wiki bar, etc.', 'basic', 'wiki');
+INSERT IGNORE INTO users_grouppermissions (groupName, permName) VALUES ('Anonymous', 'tiki_p_wiki_view_header');
 
 #
 # Tables of the Opinion-Network
