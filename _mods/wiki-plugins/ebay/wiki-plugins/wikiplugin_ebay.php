5297ec3922a8209f2bf1359239f4ec16515fc692
damosoft <damosoft@users.sourceforge.net>
2004-09-13 14:44:58 +0000

    slight refinement to button label
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@5331 b456876b-0849-0410-b77d-98878d47e9d5

--- _mods/wiki-plugins/ebay/wiki-plugins/wikiplugin_ebay.php
+++ _mods/wiki-plugins/ebay/wiki-plugins/wikiplugin_ebay.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/ebay/wiki-plugins/wikiplugin_ebay.php,v 1.2 2004-09-13 11:22:26 damosoft Exp $
+// $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/ebay/wiki-plugins/wikiplugin_ebay.php,v 1.3 2004-09-13 14:44:58 damosoft Exp $
 
 // Wiki plugin to display ebay search results file
 // damian aka damosoft 13 Sep 2004
@@ -16,7 +16,7 @@ function wikiplugin_ebay($data, $params) {
 	$asetup = '<form action="http://search.ebay.co.uk/search/search.dll" method="get">';
 	$asetup .='<input type="hidden" name="sokeywordredirect">';
 	$asetup .='<input type="hidden" name="satitle" value="'.$search.'">';
-	$asetup .= '<input type="submit" value="Find It" alt="Go!">';
+	$asetup .= '<input type="submit" value="Find It on eBay" alt="Go!">';
 	$asetup .= '</form>';
 
 	return $asetup;
