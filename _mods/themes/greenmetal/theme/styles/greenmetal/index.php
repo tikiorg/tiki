2617833655bbbe8d13fc7cd28abda4be8b762819
damosoft <damosoft@users.sourceforge.net>
2004-09-22 09:39:58 +0000

    GreenMetal being evicted to mods.tw.o
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@5371 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ _mods/themes/greenmetal/theme/styles/greenmetal/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// $Header: /cvsroot/tikiwiki/_mods/themes/greenmetal/theme/styles/greenmetal/index.php,v 1.1 2004-09-22 09:37:45 damosoft Exp $
+
+// Copyright (c) 2002-2004, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+// This redirects to the sites root to prevent directory browsing
+
+header ("location: ../../tiki-index.php");
+die;
+
+?>
