db2ecbcad7974fd84cab3351e2d90b01140a97d8
mose <mose@users.sourceforge.net>
2004-10-07 06:29:35 +0000

    more fixes
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@5419 b456876b-0849-0410-b77d-98878d47e9d5

--- _mods/thirdparty/sample:tiki-mailman.php
+++ _mods/thirdparty/sample:tiki-mailman.php
@@ -8,7 +8,6 @@ a { text-decoration : none; color : #446688; }
 <table border="0" width="100%" height="100%">
 <tr><td valign="top" width="130" style="width:130px;border-right:1px dashed #999999;padding:10px;" rowspan="2">
 <?
-$domain = "[:::[domain]:::]";
 $mldomain = "[:::[mldomain]:::]";
 $options = "-a -V $mldomain";
 
@@ -21,12 +20,12 @@ $urls['queue'] = "http://$mldomain/cgi-bin/mailman/admindb/%s";
 
 exec("sudo /usr/sbin/list_lists $options",$ls);
 echo "<div style='font-weight:bold;font-size:1.2em;'><a href='http://nguild.org'>N'GUILD</a>LISTS</div>";
-echo "<div style='font-size:.8em;'><a href='list.php'>". array_shift($ls). "</a></div><br />\n";
+echo "<div style='font-size:.8em;'><a href='".$_SERVER['PHP_SELF']."'>". array_shift($ls). "</a></div><br />\n";
 foreach ($ls as $l) {
 	list($name,$desc) = split('-',$l);
 	$name = trim($name);
 	$desc = trim($desc);
-	echo "<div><a href='list.php?list=$name";
+	echo "<div><a href='".$_SERVER['PHP_SELF']."?list=$name";
 	if (isset($_REQUEST['panel']) and isset($urls["{$_REQUEST['panel']}"])) {
 		echo "&panel=".$_REQUEST['panel'];
 	}
@@ -40,17 +39,17 @@ if (isset($_REQUEST['list']) and in_array($_REQUEST['list'],$lists)) {
 	$list = $_REQUEST['list'];
 	echo "<td valign='top' height='34'>\n";
 	echo "<h2>$list ";
-	echo "<a href='list.php?list=$list&panel=info' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>info</a> ";
-	echo "<a href='list.php?list=$list&panel=parchives' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>public archives</a> ";
-	echo "<a href='list.php?list=$list&panel=archives' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>archives</a> ";
-	echo "<a href='list.php?list=$list&panel=members' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>members</a> ";
-	echo "<a href='list.php?list=$list&panel=queue' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>queue</a> ";
-	echo "<a href='list.php?list=$list&panel=admin' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>admin</a> ";
+	echo "<a href='".$_SERVER['PHP_SELF']."?list=$list&panel=info' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>info</a> ";
+	echo "<a href='".$_SERVER['PHP_SELF']."?list=$list&panel=parchives' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>public archives</a> ";
+	echo "<a href='".$_SERVER['PHP_SELF']."?list=$list&panel=archives' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>archives</a> ";
+	echo "<a href='".$_SERVER['PHP_SELF']."?list=$list&panel=members' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>members</a> ";
+	echo "<a href='".$_SERVER['PHP_SELF']."?list=$list&panel=queue' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>queue</a> ";
+	echo "<a href='".$_SERVER['PHP_SELF']."?list=$list&panel=admin' style='font-size:.5em;padding:2px 10px;border:1px solid black;'>admin</a> ";
 	echo "</h2>";
 	echo "</td></tr><tr><td>";
 	if (isset($_REQUEST['panel']) and isset($urls["{$_REQUEST['panel']}"])) {
 		$url = sprintf($urls["{$_REQUEST['panel']}"],$list);
-		echo "<iframe src='$url' name='$list' height='100%' width='100%' frameborder='1' scrolling='auto' style='widt:100%;'></iframe>";
+		echo "<iframe src='$url' name='$list' height='100%' width='100%' frameborder='1' scrolling='auto' style='width:100%;'></iframe>";
 	} else {
 		echo "<i>choose a command</i>";
 	}
