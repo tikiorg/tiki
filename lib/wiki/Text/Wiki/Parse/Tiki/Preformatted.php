cdebeb1b4acc3ed75d6b3166e94dc13d65858af2
papercrane <papercrane@users.sourceforge.net>
2005-06-16 06:01:11 +0000

    Simplify rule
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5718 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki/Text/Wiki/Parse/Tiki/Preformatted.php
+++ lib/wiki/Text/Wiki/Parse/Tiki/Preformatted.php
@@ -13,7 +13,7 @@
 * 
 * @license LGPL
 * 
-* @version $Id: Preformatted.php,v 1.1 2005-05-18 23:43:16 papercrane Exp $
+* @version $Id: Preformatted.php,v 1.2 2005-06-16 06:01:11 papercrane Exp $
 * 
 */
 
@@ -48,7 +48,7 @@ class Text_Wiki_Parse_Preformatted extends Text_Wiki_Parse {
     * 
     */
     
-    var $regex = '/~pp~(.*?)~\/pp~/s';
+    var $regex = '!~pp~(.*?)~/pp~!s';
     
     /**
     * 
@@ -67,7 +67,7 @@ class Text_Wiki_Parse_Preformatted extends Text_Wiki_Parse {
     
     function process(&$matches)
     {
-        $options = array('text' => strlen($matches[1]) ? $matches[1] : $matches[2]);
+        $options = array('text' => $matches[1]);
         return $this->wiki->addToken($this->rule, $options);
     }
 }
