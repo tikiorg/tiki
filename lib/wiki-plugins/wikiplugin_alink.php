ae5d9646fc2c12674849aaa5c5c302b56b2d61db
rlpowell <rlpowell@users.sourceforge.net>
2005-07-12 22:40:18 +0000

    Just a help change.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5759 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_alink.php
+++ lib/wiki-plugins/wikiplugin_alink.php
@@ -20,7 +20,7 @@
 
 
 function wikiplugin_alink_help() {
-        return tra("Puts a link to an anchor in a wiki page. Use in conjunction with the ANAME plugin, which sets the location and name of the anchor").":<br />~np~{ALINK(aname=anchorname,pagename=Wiki Page Name)}".tra("linktext")."{ALINK}~/np~";
+        return tra("Puts a link to an anchor in a wiki page. Use in conjunction with the ANAME plugin, which sets the location and name of the anchor").":<br />~np~{ALINK(aname=>anchorname,pagename=>Wiki Page Name)}".tra("linktext")."{ALINK}~/np~<br />pagename is optional; if it is not present, links into the current file.";
 }
 
 function wikiplugin_alink($data, $params)
@@ -38,7 +38,7 @@ function wikiplugin_alink($data, $params)
 		return ("<b>missing parameter for aname</b><br />");
 	}
 
-	if (isset($pagename)) {
+	if( isset($pagename) && $pagename ) {
 	    // Stolen, with some modifications, from tikilib.php line 4717-4723
 	    if( $desc = $tikilib->page_exists_desc($pagename) )
 	    {
