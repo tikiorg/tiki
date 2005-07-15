5a1331be3b0d31b6f05a369743d39ffbe864534e
rlpowell <rlpowell@users.sourceforge.net>
2005-07-15 23:54:25 +0000

    Made tiki_pages' data field a mediumtext instead of just text.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@5768 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/db/tiki_1.9to1.10.sql
+++ tiki/db/tiki_1.9to1.10.sql
@@ -1,4 +1,4 @@
-# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.30 2005-07-05 21:49:37 rlpowell Exp $
+# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.31 2005-07-15 23:54:25 rlpowell Exp $
 
 # The following script will update a tiki database from verion 1.9 to 1.10
 # 
@@ -19,3 +19,7 @@ INSERT IGNORE INTO tiki_preferences(name,value) VALUES ('pear_wiki_parser','n');
 
 #2005-06-22 rlpowell: available_languages was getting truncated if all languages were selected
 ALTER TABLE `tiki_preferences` CHANGE value value text;
+
+#2005-07-15 rlpowell: Had a wiki page get truncated! Very annoying.
+# This will allow up to 16777216 bytes instead of 65536
+ALTER TABLE `tiki_pages` CHANGE data data mediumtext;
