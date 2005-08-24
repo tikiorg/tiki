e023831247337291ffa88c64569c0bd151eb255a
sylvieg <sylvieg@users.sourceforge.net>
2005-08-24 22:23:56 +0000

    style in module categories(ex:tree|vert) + 2 phplayers user menus can be displayed
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@5825 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/smarty_tiki/function.phplayers.php
+++ tiki/lib/smarty_tiki/function.phplayers.php
@@ -10,103 +10,27 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 PhpLayers in tikiwiki !
 
 That smarty function is mostly intended to be used in .tpl files
-syntax: {phplayers [type=tree|phptree|plain] [id=1] [file=/path/to/menufile]}
+syntax: {phplayers [type=tree|phptree|plain|hort|vert] [id=1] [file=/path/to/menufile]}
 
 */
 function smarty_function_phplayers($params, &$smarty) {
-	global $tikilib,$smarty,$feature_phplayers;
+	global $feature_phplayers;
+	global $tikiphplayers; include_once('lib/phplayers_tiki/tiki-phplayers.php');
 	if ($feature_phplayers != 'y') {
-	  echo "phplayers are not available on this site";
-	} else {
-	$smarty->assign('uses_phplayers','y');
+	  echo tra("phplayers are not available on this site");
+	  return;
+	}
+	//$smarty->assign('uses_phplayers','y'); doesn't seem to be use
 	extract($params);
-
-	$types['vert'] = 'layersmenu.inc.php';
-        $types['horiz'] = 'layersmenu.inc.php';
-	$types['tree'] = 'treemenu.inc.php';
-	$types['phptree'] = 'phptreemenu.inc.php';
-	$types['plain'] = 'phptreemenu.inc.php';
-
-	$classes['vert'] = 'LayersMenu';
-        $classes['horiz'] = 'LayersMenu';
-	$classes['tree'] = 'TreeMenu';
-	$classes['phptree'] = 'PHPTreeMenu';
-	$classes['plain'] = 'PlainMenu';
-	
-	$struct['vert'] = 'vermenu2';
-        $struct['horiz'] = 'hormenu3';
-	$struct['tree'] = 'treemenu1';
-	$struct['phptree'] = 'treemenu1';
-	$struct['plain'] = 'treemenu1';
-
-	$new['vert'] = 'newVerticalMenu';
-        $new['horiz'] = 'newHorizontalMenu';
-	$new['tree'] = 'newTreeMenu';
-	$new['phptree'] = 'newTreeMenu';
-	$new['plain'] = 'newTreeMenu';
 
 	if (empty($type)) {
 		$type = 'tree';
 	}
-
-	include_once ("lib/phplayers/lib/PHPLIB.php");
-	include_once ("lib/phplayers/lib/layersmenu-common.inc.php");
-	include_once ("lib/phplayers/lib/layersmenu.inc.php");
-	include_once ("lib/phplayers/lib/".$types["$type"]);
-	// beware ! that below is a variable class declaration
-	$class = $classes["$type"];
-	$phplayers = new $class();
-	$phplayers->setDirrootCommon("lib/phplayers");
-	$phplayers->setLibjsdir("lib/phplayers/libjs/");
-	$phplayers->setImgdir("lib/phplayers/images/");
-	$phplayers->setImgwww("lib/phplayers/images/");
-	$phplayers->setTpldirCommon("lib/phplayers/templates/");
-	
 	if (!empty($id)) {
-		$menu_info = $tikilib->get_menu($id);
-		$channels = $tikilib->list_menu_options($id,0,-1,'position_asc','');
-		$intended = false;
-		$output = '';
-		foreach ($channels["data"] as $cd) {
-		        $cd["name"] = tra($cd["name"]);
-			if ($cd["type"] == 'o' and $indented) {
-				$output.= ".";
-			} elseif ($cd["type"] == 's') {
-				$indented = true;
-			}
-			$output.= ".|".$cd["name"]."|".$cd["url"]."\n";
-		}
-		$phplayers->setMenuStructureString($output);
-	} elseif (!empty($file)) {
-		if (is_file($file)) {	
-			$phplayers->setMenuStructureFile($file);
-		} else {
-			$phplayers->setMenuStructureFile("lib/phplayers/layersmenu-vertical-2.txt");
-		}
+		$output = $tikiphplayers->mkMenuEntry($id);
 	}
-	
-	$phplayers->parseStructureForMenu($struct["$type"]);
-	if ($type == 'vert') {
-		$phplayers->setDownArrowImg("down-galaxy.png");
-		$phplayers->setForwardArrowImg("forward-galaxy.png");
-		$phplayers->setVerticalMenuTpl("layersmenu-vertical_menu-galaxy.ihtml");
-		$phplayers->setSubMenuTpl("layersmenu-sub_menu-galaxy.ihtml");
-		$phplayers->$new["$type"]($struct["$type"]);
-		$phplayers->printHeader();
-		$phplayers->printMenu($struct["$type"]);
-		$phplayers->printFooter();
-	} elseif ($type == 'horiz') {
-		$phplayers->setDownArrowImg("down-galaxy.png");
-		$phplayers->setForwardArrowImg("forward-galaxy.png");
-		$phplayers->setHorizontalMenuTpl("layersmenu-horizontal_menu.ihtml");
-		$phplayers->setSubMenuTpl("layersmenu-sub_menu-galaxy.ihtml");
-		$phplayers->$new["$type"]($struct["$type"]);
-		$phplayers->printHeader();
-		$phplayers->printMenu($struct["$type"]);
-		$phplayers->printFooter();
-	} else {
-		echo $phplayers->$new["$type"]($struct["$type"]);
-	}
-	}
+	$name = 'usermenu'.$id;
+
+	echo $tikiphplayers->mkmenu($output, $name, $type, $file);
 }
 ?>
--- tiki/templates/footer.tpl
+++ tiki/templates/footer.tpl
@@ -1,7 +1,11 @@
-{* $Header: /cvsroot/tikiwiki/tiki/templates/footer.tpl,v 1.3 2004-06-11 05:20:41 mose Exp $ *}
+{* $Header: /cvsroot/tikiwiki/tiki/templates/footer.tpl,v 1.4 2005-08-24 22:23:54 sylvieg Exp $ *}
 
 {*if $tiki_p_admin eq 'y' and $feature_debug_console eq 'y'*}
   {* Include debugging console. Note it shoudl be processed as near as possible to the end of file *}
+
+{if $feature_phplayers eq 'y'}
+{php} global $LayersMenu; if (isset($LayersMenu)) {$LayersMenu->printHeader();$LayersMenu->printFooter();}{/php}
+{/if}
 
   {php}  include_once("tiki-debug_console.php"); {/php}
   {include file="tiki-debug_console.tpl"}
