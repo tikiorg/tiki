677ac39ece7a7a5a8b619d68b491eb5f2c5e9a9e
luciash <luciash@users.sourceforge.net>
2005-08-26 00:51:14 +0000

    + img/trackers
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5835 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/tiki-installer.php
+++ installer/tiki-installer.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/installer/tiki-installer.php,v 1.1 2005-08-26 00:30:40 luciash Exp $
+// $Header: /cvsroot/tikiwiki/tiki/installer/tiki-installer.php,v 1.2 2005-08-26 00:51:14 luciash Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -136,6 +136,7 @@ function create_dirs($domain=''){
 		'dump',
 		'img/wiki',
 		'img/wiki_up',
+		'img/trackers',
 		'modules/cache',
 		'temp',
 		'temp/cache',
