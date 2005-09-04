f95f4054ab03845837669d6eedc2cc27e263d868
rlpowell <rlpowell@users.sourceforge.net>
2005-09-04 01:20:28 +0000

    Stopped attachments from forcing download on the browser.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5874 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-download_wiki_attachment.php
+++ tiki-download_wiki_attachment.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-download_wiki_attachment.php,v 1.10 2005-05-18 10:58:55 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-download_wiki_attachment.php,v 1.11 2005-09-04 01:20:28 rlpowell Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -33,7 +33,13 @@ session_write_close();
 //print("File:$file<br />");
 //die;
 header ("Content-type: $type");
-header ("Content-Disposition: attachment; filename=\"$file\"");
+
+// This used to say "header ("Content-Disposition: attachment; filename=\"$file\"");"
+// which made everything try to download instead of the browser picking what to do.
+// If people want the old behaviour, the right thing is probably to add an argument to 
+// tiki-download_wiki_attachment.php, such as "&download", and then code in the attachment
+// plugin, but the old behaviour really seems like The Wrong Thing to me.  -rlpowell
+header ("Content-Disposition: filename=\"$file\"");
 
 // Added March04 Damian, Akira123 reported test
 header ("Expires: 0");
