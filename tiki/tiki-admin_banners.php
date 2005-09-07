95c904116682e6d65dcd09d549ef2601e753f41e
damosoft <damosoft@users.sourceforge.net>
2005-09-07 21:25:52 +0000

    bringing back some security, I think banners has its own perm tho and the whole file is missing anyway!
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@5889 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-admin_banners.php
+++ tiki/tiki-admin_banners.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_banners.php,v 1.8 2005-05-18 10:58:52 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_banners.php,v 1.9 2005-09-07 21:25:52 damosoft Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -9,15 +9,12 @@
 // Initialization
 require_once ('tiki-setup.php');
 
-/*
-if($user != 'admin') {
   if($tiki_p_admin != 'y') {
 	$smarty->assign('msg',tra("You do not have permission to use this feature"));
 	$smarty->display("error.tpl");
 	die;
   }
-}
-*/
+
 
 // Display the template
 $smarty->assign('mid', 'tiki-edit_banner.tpl');
