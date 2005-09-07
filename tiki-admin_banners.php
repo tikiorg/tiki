0f352e70d0f47b8a3e6514efaac394765d9b9d88
michael_davey <michael_davey@users.sourceforge.net>
2005-09-07 22:15:26 +0000

    use accesslib
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5893 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_banners.php
+++ tiki-admin_banners.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_banners.php,v 1.10 2005-09-07 21:37:19 damosoft Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_banners.php,v 1.11 2005-09-07 22:15:26 michael_davey Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -9,19 +9,9 @@
 // Initialization
 require_once ('tiki-setup.php');
 
-
-if ($feature_banners != 'y') {
-	$smarty->assign('msg', tra("This feature is disabled").": feature_banners");
-	$smarty->display("error.tpl");
-	die;
-}
-
-if ($tiki_p_admin_banners != 'y') {
-	$smarty->assign('msg', tra("You do not have permission to edit banners"));
-	$smarty->display("error.tpl");
-	die;
-}
-
+// $access->check_admin();
+$access->check_feature('feature_banners');
+$access->check_permission('tiki_p_admin_banners');
 
 // Display the template
 $smarty->assign('mid', 'tiki-edit_banner.tpl');
