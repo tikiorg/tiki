92ca5990061636c19aa8044d2c294aa71d09a19c
damosoft <damosoft@users.sourceforge.net>
2005-09-07 21:37:19 +0000

    copied over from damians cvs server, correct permissions
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@5890 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-admin_banners.php
+++ tiki/tiki-admin_banners.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_banners.php,v 1.9 2005-09-07 21:25:52 damosoft Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_banners.php,v 1.10 2005-09-07 21:37:19 damosoft Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -9,11 +9,18 @@
 // Initialization
 require_once ('tiki-setup.php');
 
-  if($tiki_p_admin != 'y') {
-	$smarty->assign('msg',tra("You do not have permission to use this feature"));
+
+if ($feature_banners != 'y') {
+	$smarty->assign('msg', tra("This feature is disabled").": feature_banners");
 	$smarty->display("error.tpl");
 	die;
-  }
+}
+
+if ($tiki_p_admin_banners != 'y') {
+	$smarty->assign('msg', tra("You do not have permission to edit banners"));
+	$smarty->display("error.tpl");
+	die;
+}
 
 
 // Display the template
