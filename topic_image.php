017373c295d64d5d003bf216a359a7fa1ec2914c
damosoft <damosoft@users.sourceforge.net>
2005-09-08 01:48:45 +0000

    topic_image, article_image and banner_image should all become one script to lower the amount of maintenance required :?
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5905 b456876b-0849-0410-b77d-98878d47e9d5

--- topic_image.php
+++ topic_image.php
@@ -1,12 +1,12 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.14 2005-05-18 10:59:01 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.15 2005-09-08 01:48:45 damosoft Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
-# $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.14 2005-05-18 10:59:01 mose Exp $
+# $Header: /cvsroot/tikiwiki/tiki/topic_image.php,v 1.15 2005-09-08 01:48:45 damosoft Exp $
 
 // application to display an image from the database with 
 // option to resize the image dynamically creating a thumbnail on the fly.
@@ -17,7 +17,7 @@ if (!isset($_REQUEST["id"])) {
 include_once ('lib/init/initlib.php');
 include_once ('tiki-setup_base.php');
 
-$topiccachefile = "temp";
+$topiccachefile = $tikilib->get_preference("tmpdir", "temp");
 if ($tikidomain) { $topiccachefile.= "/$tikidomain"; }
 $topiccachefile.= "/topic.".$_REQUEST["id"];
 
@@ -28,6 +28,11 @@ if (is_file($topiccachefile) and (!isset($_REQUEST["reload"]))) {
 	die();
 } else {
 	$data = $tikilib->get_topic_image($_REQUEST["id"]);
+	// if blank then die, otherwise we offer to download strangeness
+	// this also catches invalid id's
+	if (!$data) {
+		die;
+	}
 	$type = $data["image_type"];
 	$data = $data["image_data"];
 	if ($data["image_data"]) {
