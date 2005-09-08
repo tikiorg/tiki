366a0bee068558eac264b63809832e8149480de3
damosoft <damosoft@users.sourceforge.net>
2005-09-08 01:13:15 +0000

    If a invalid id or empty article data is returned stop before you download junk
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@5899 b456876b-0849-0410-b77d-98878d47e9d5

--- article_image.php
+++ article_image.php
@@ -1,12 +1,10 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/article_image.php,v 1.15 2005-09-08 01:08:59 damosoft Exp $
+// $Header: /cvsroot/tikiwiki/tiki/article_image.php,v 1.16 2005-09-08 01:13:15 damosoft Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-
-# $Header: /cvsroot/tikiwiki/tiki/article_image.php,v 1.15 2005-09-08 01:08:59 damosoft Exp $
 
 // application to display an image from the database with 
 // option to resize the image dynamically creating a thumbnail on the fly.
@@ -30,6 +28,11 @@ if (is_file($topiccachefile) and (!isset($_REQUEST["reload"]))) {
 	die();
 } else {
 	$data = $tikilib->get_article_image($_REQUEST["id"]);
+	// if blank then die, otherwise we offer to download strangeness
+	// this also catches invalid id's
+	if (!$data) {
+		die;
+	}
 	$type = $data["image_type"];
 	$data = $data["image_data"];
 	if ($data["image_data"]) {
