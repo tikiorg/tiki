125e61a9bceb8d761003b1b01e0d83e68f9a24da
damosoft <damosoft@users.sourceforge.net>
2005-09-12 09:06:51 +0000

    Added search field check + info message
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@5963 b456876b-0849-0410-b77d-98878d47e9d5

--- _mods/wiki-plugins/ebay/wiki-plugins/wikiplugin_ebay.php
+++ _mods/wiki-plugins/ebay/wiki-plugins/wikiplugin_ebay.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/ebay/wiki-plugins/wikiplugin_ebay.php,v 1.4 2005-09-08 17:22:15 damosoft Exp $
+// $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/ebay/wiki-plugins/wikiplugin_ebay.php,v 1.5 2005-09-12 09:06:51 damosoft Exp $
 
 // Wiki plugin to display ebay search results file
 // damian aka damosoft 13 Sep 2004
@@ -13,11 +13,15 @@ function wikiplugin_ebay($data, $params) {
 	
 	extract ($params, EXTR_SKIP);
 
-	$asetup = '<form action="http://search.ebay.co.uk/search/search.dll" method="get">';
-	$asetup .='<input type="hidden" name="sokeywordredirect">';
-	$asetup .='<input type="hidden" name="satitle" value="'.$search.'">';
-	$asetup .= '<input type="submit" value="Find It on eBay" alt="Go!">';
-	$asetup .= '</form>';
+	if (!$search) {
+		$asetup = "Keywords are missing in the search parameter";
+	} else {
+		$asetup = '<form action="http://search.ebay.co.uk/search/search.dll" method="get">';
+		$asetup .='<input type="hidden" name="sokeywordredirect">';
+		$asetup .='<input type="hidden" name="satitle" value="'.$search.'">';
+		$asetup .= '<input type="submit" value="Find It on eBay" alt="Go!">';
+		$asetup .= '</form>';
+	}
 
 	return $asetup;
 }
