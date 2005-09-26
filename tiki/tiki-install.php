5b719d741d26b922f940bac2b7a0bb074017d303
michael_davey <michael_davey@users.sourceforge.net>
2005-09-26 12:56:13 +0000

    be more helpful to automated programs
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6015 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-install.php
+++ tiki/tiki-install.php
@@ -1,13 +1,16 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-install.php,v 1.75 2005-09-07 12:35:39 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-install.php,v 1.76 2005-09-26 12:56:13 michael_davey Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 
 if (!file_exists("installer/tiki-installer.php")) {
-	echo "Tiki installer is disabled.";
+	header ("Status: 410 Gone"); /* PHP3 */
+	header ("HTTP/1.0 410 Gone"); /* PHP4 */
+	header ('location: index.php');
+	die('TikiWiki installer has been disabled.');
 } else {
 	include_once("installer/tiki-installer.php");
 }
