d5283dc305f2885594447ebff21c0a9d8fa6a6c6
lfagundes <lfagundes@users.sourceforge.net>
2005-12-10 14:52:52 +0000

    [ADD] list of objects from a tag [MOD] renamed this list of objects from freetags_browse to freetag_list_object, the first will be a list of tags. have to rename everywhere
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@6214 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki-freetag_list_objects.php
@@ -0,0 +1,94 @@
+<?php
+
+// $Header: /cvsroot/tikiwiki/tiki/tiki-freetag_list_objects.php,v 1.1 2005-12-10 14:52:50 lfagundes Exp $
+
+// Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+//
+// $Header: /cvsroot/tikiwiki/tiki/tiki-freetag_list_objects.php,v 1.1 2005-12-10 14:52:50 lfagundes Exp $
+//
+
+// Initialization
+require_once ('tiki-setup.php');
+
+include_once ('lib/freetag/freetaglib.php');
+
+if ($feature_freetags != 'y') {
+	$smarty->assign('msg', tra("This feature is disabled").": feature_freetags");
+
+	$smarty->display("error.tpl");
+	die;
+}
+
+if (!isset($_REQUEST['tag'])) {
+    header('Location: tiki-freetags_browse.php');
+    exit;
+}
+
+if (!isset($_REQUEST["sort_mode"])) {
+	$sort_mode = 'name_asc';
+} else {
+	$sort_mode = $_REQUEST["sort_mode"];
+}
+
+$smarty->assign_by_ref('sort_mode', $sort_mode);
+
+if (!isset($_REQUEST["offset"])) {
+	$offset = 0;
+} else {
+	$offset = $_REQUEST["offset"];
+}
+
+$smarty->assign_by_ref('offset', $offset);
+
+if (!isset($_REQUEST["type"])) {
+	$type = '';
+} else {
+	$type = $_REQUEST["type"];
+}
+
+$smarty->assign('type', $type);
+
+if (isset($_REQUEST["user_only"]) && $_REQUEST["user_only"] == 'on') {
+    $view_user = $user;
+    $smarty->assign('user_only', 'on');
+} else {
+    $view_user = '';
+    $smarty->assign('user_only', 'off');
+}
+
+$smarty->assign('tag', $_REQUEST['tag']);
+
+$objects = $freetaglib->get_objects_with_tag($_REQUEST['tag'], $view_user($type), $offset, $maxRecords); //, $sort_mode, $find);
+
+$smarty->assign_by_ref('objects', $objects["data"]);
+$smarty->assign_by_ref('cantobjects', $objects["cant"]);
+
+$cant_pages = ceil($objects["cant"] / $maxRecords);
+$smarty->assign_by_ref('cant_pages', $cant_pages);
+$smarty->assign('actual_page', 1 + ($offset / $maxRecords));
+
+if ($objects["cant"] > ($offset + $maxRecords)) {
+	$smarty->assign('next_offset', $offset + $maxRecords);
+} else {
+	$smarty->assign('next_offset', -1);
+}
+
+// If offset is > 0 then prev_offset
+if ($offset > 0) {
+	$smarty->assign('prev_offset', $offset - $maxRecords);
+} else {
+	$smarty->assign('prev_offset', -1);
+}
+
+$section = 'freetags';
+include_once ('tiki-section_options.php');
+ask_ticket('browse-freetags');
+
+// Display the template
+$smarty->assign('mid', 'tiki-freetag_list_objects.tpl');
+$smarty->display("tiki.tpl");
+
+?>
