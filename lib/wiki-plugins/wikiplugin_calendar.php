6b70947fd0f3d4f1f5edc873322ecd4946a72bbb
sampaioprimo <sampaioprimo@users.sourceforge.net>
2005-12-13 01:37:04 +0000

    wesley and I commited the wikiplugin_calendar by mistake on 1.9, this is supposed to be on 1.10. fixing it : )
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@6229 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_calendar.php
+++ lib/wiki-plugins/wikiplugin_calendar.php
@@ -1,12 +1,33 @@
 <?php
 
 function wikiplugin_calendar_help() {
-  return tra("Ajuda");
+	return tra("Calendar").":<br />~np~{CALENDAR(calendarId=>3,viewmode=>topicName)}{CALENDAR}<br />Id is optional. if not given, last article is used. default field is
+	heading.~/np~";
 }
 
 function wikiplugin_calendar($data, $params) {
+	global $smarty;
+	global $tikilib;
+	global $feature_calendar;
+	global $tiki_p_admin;
+	global $tiki_p_view_calendar;
+	global $dbTiki;
 
- 
+	extract ($params,EXTR_SKIP);
+	include_once("tiki-show_calendar.php");
+	
+	if(!isset($dc))
+	$dc = $tikilib->get_date_converter($user);
+
+	if(!isset($calendarId))
+	$calendarId = 1;
+
+	if(!isset($viewmode))
+	$viewmode = "semester";
+
+	return $smarty->fetch('tiki-show_calendar.tpl');
+	//return $viewmode;
+
 }
 
 ?>
