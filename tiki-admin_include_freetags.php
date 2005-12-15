92ad5ad35864a77d8dc2c21f3b55db174248f789
amette <amette@users.sourceforge.net>
2005-12-15 21:56:20 +0000

    Freetag-3D-browser has it's own set of settings
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@6257 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_include_freetags.php
+++ tiki-admin_include_freetags.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_include_freetags.php,v 1.1 2005-12-15 21:41:44 amette Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_include_freetags.php,v 1.2 2005-12-15 21:56:20 amette Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -11,105 +11,105 @@
 require_once('tiki-setup.php');  
 $access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__));
 
-if (isset($_REQUEST["wikiset3d"])) {
+if (isset($_REQUEST["freetagsset3d"])) {
         check_ticket('admin-inc-freetags');
-	if (isset($_REQUEST["wiki_feature_3d"]) && $_REQUEST["wiki_feature_3d"] == "on") {
-	    $tikilib->set_preference("wiki_feature_3d", 'y');
-	    $smarty->assign("wiki_feature_3d", 'y');
+	if (isset($_REQUEST["freetags_feature_3d"]) && $_REQUEST["freetags_feature_3d"] == "on") {
+	    $tikilib->set_preference("freetags_feature_3d", 'y');
+	    $smarty->assign("freetags_feature_3d", 'y');
 	} else {
-	    $tikilib->set_preference("wiki_feature_3d", 'n');
-	    $smarty->assign("wiki_feature_3d", 'n');
+	    $tikilib->set_preference("freetags_feature_3d", 'n');
+	    $smarty->assign("freetags_feature_3d", 'n');
 	}
 
-	if (isset($_REQUEST["wiki_3d_width"])) {
-		$tikilib->set_preference("wiki_3d_width", $_REQUEST["wiki_3d_width"]);
-		$smarty->assign('wiki_3d_width', $_REQUEST["wiki_3d_width"]);
+	if (isset($_REQUEST["freetags_3d_width"])) {
+		$tikilib->set_preference("freetags_3d_width", $_REQUEST["freetags_3d_width"]);
+		$smarty->assign('freetags_3d_width', $_REQUEST["freetags_3d_width"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_height"])) {
-		$tikilib->set_preference("wiki_3d_height", $_REQUEST["wiki_3d_height"]);
-		$smarty->assign('wiki_3d_height', $_REQUEST["wiki_3d_height"]);
+	if (isset($_REQUEST["freetags_3d_height"])) {
+		$tikilib->set_preference("freetags_3d_height", $_REQUEST["freetags_3d_height"]);
+		$smarty->assign('freetags_3d_height', $_REQUEST["freetags_3d_height"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_navigation_depth"])) {
-		$tikilib->set_preference("wiki_3d_navigation_depth", $_REQUEST["wiki_3d_navigation_depth"]);
-		$smarty->assign('wiki_3d_navigation_depth', $_REQUEST["wiki_3d_navigation_depth"]);
+	if (isset($_REQUEST["freetags_3d_navigation_depth"])) {
+		$tikilib->set_preference("freetags_3d_navigation_depth", $_REQUEST["freetags_3d_navigation_depth"]);
+		$smarty->assign('freetags_3d_navigation_depth', $_REQUEST["freetags_3d_navigation_depth"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_feed_animation_interval"])) {
-		$tikilib->set_preference("wiki_3d_feed_animation_interval", $_REQUEST["wiki_3d_feed_animation_interval"]);
-		$smarty->assign('wiki_3d_feed_animation_interval', $_REQUEST["wiki_3d_feed_animation_interval"]);
+	if (isset($_REQUEST["freetags_3d_feed_animation_interval"])) {
+		$tikilib->set_preference("freetags_3d_feed_animation_interval", $_REQUEST["freetags_3d_feed_animation_interval"]);
+		$smarty->assign('freetags_3d_feed_animation_interval', $_REQUEST["freetags_3d_feed_animation_interval"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_existing_page_color"])) {
-		$tikilib->set_preference("wiki_3d_existing_page_color", $_REQUEST["wiki_3d_existing_page_color"]);
-		$smarty->assign('wiki_3d_existing_page_color', $_REQUEST["wiki_3d_existing_page_color"]);
+	if (isset($_REQUEST["freetags_3d_existing_page_color"])) {
+		$tikilib->set_preference("freetags_3d_existing_page_color", $_REQUEST["freetags_3d_existing_page_color"]);
+		$smarty->assign('freetags_3d_existing_page_color', $_REQUEST["freetags_3d_existing_page_color"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_missing_page_color"])) {
-		$tikilib->set_preference("wiki_3d_missing_page_color", $_REQUEST["wiki_3d_missing_page_color"]);
-		$smarty->assign('wiki_3d_missing_page_color', $_REQUEST["wiki_3d_missing_page_color"]);
+	if (isset($_REQUEST["freetags_3d_missing_page_color"])) {
+		$tikilib->set_preference("freetags_3d_missing_page_color", $_REQUEST["freetags_3d_missing_page_color"]);
+		$smarty->assign('freetags_3d_missing_page_color', $_REQUEST["freetags_3d_missing_page_color"]);
 	}
 
 	/* new fields */
-	if (isset($_REQUEST["wiki_3d_autoload"])) {
-		$tikilib->set_preference("wiki_3d_autoload", $_REQUEST["wiki_3d_autoload"]);
-		$smarty->assign('wiki_3d_autoload', $_REQUEST["wiki_3d_autoload"]);
+	if (isset($_REQUEST["freetags_3d_autoload"])) {
+		$tikilib->set_preference("freetags_3d_autoload", $_REQUEST["freetags_3d_autoload"]);
+		$smarty->assign('freetags_3d_autoload', $_REQUEST["freetags_3d_autoload"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_camera_distance"])) {
-		$tikilib->set_preference("wiki_3d_camera_distance", $_REQUEST["wiki_3d_camera_distance"]);
-		$smarty->assign('wiki_3d_camera_distance', $_REQUEST["wiki_3d_camera_distance"]);
+	if (isset($_REQUEST["freetags_3d_camera_distance"])) {
+		$tikilib->set_preference("freetags_3d_camera_distance", $_REQUEST["freetags_3d_camera_distance"]);
+		$smarty->assign('freetags_3d_camera_distance', $_REQUEST["freetags_3d_camera_distance"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_adjust_camera"])) {
-		$tikilib->set_preference("wiki_3d_adjust_camera", $_REQUEST["wiki_3d_adjust_camera"]);
-		$smarty->assign('wiki_3d_adjust_camera', $_REQUEST["wiki_3d_adjust_camera"]);
+	if (isset($_REQUEST["freetags_3d_adjust_camera"])) {
+		$tikilib->set_preference("freetags_3d_adjust_camera", $_REQUEST["freetags_3d_adjust_camera"]);
+		$smarty->assign('freetags_3d_adjust_camera', $_REQUEST["freetags_3d_adjust_camera"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_fov"])) {
-		$tikilib->set_preference("wiki_3d_fov", $_REQUEST["wiki_3d_fov"]);
-		$smarty->assign('wiki_3d_fov', $_REQUEST["wiki_3d_fov"]);
+	if (isset($_REQUEST["freetags_3d_fov"])) {
+		$tikilib->set_preference("freetags_3d_fov", $_REQUEST["freetags_3d_fov"]);
+		$smarty->assign('freetags_3d_fov', $_REQUEST["freetags_3d_fov"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_node_size"])) {
-		$tikilib->set_preference("wiki_3d_node_size", $_REQUEST["wiki_3d_node_size"]);
-		$smarty->assign('wiki_3d_node_size', $_REQUEST["wiki_3d_node_size"]);
+	if (isset($_REQUEST["freetags_3d_node_size"])) {
+		$tikilib->set_preference("freetags_3d_node_size", $_REQUEST["freetags_3d_node_size"]);
+		$smarty->assign('freetags_3d_node_size', $_REQUEST["freetags_3d_node_size"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_text_size"])) {
-		$tikilib->set_preference("wiki_3d_text_size", $_REQUEST["wiki_3d_text_size"]);
-		$smarty->assign('wiki_3d_text_size', $_REQUEST["wiki_3d_text_size"]);
+	if (isset($_REQUEST["freetags_3d_text_size"])) {
+		$tikilib->set_preference("freetags_3d_text_size", $_REQUEST["freetags_3d_text_size"]);
+		$smarty->assign('freetags_3d_text_size', $_REQUEST["freetags_3d_text_size"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_friction_constant"])) {
-		$tikilib->set_preference("wiki_3d_friction_constant", $_REQUEST["wiki_3d_friction_constant"]);
-		$smarty->assign('wiki_3d_friction_constant', $_REQUEST["wiki_3d_friction_constant"]);
+	if (isset($_REQUEST["freetags_3d_friction_constant"])) {
+		$tikilib->set_preference("freetags_3d_friction_constant", $_REQUEST["freetags_3d_friction_constant"]);
+		$smarty->assign('freetags_3d_friction_constant', $_REQUEST["freetags_3d_friction_constant"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_elastic_constant"])) {
-		$tikilib->set_preference("wiki_3d_elastic_constant", $_REQUEST["wiki_3d_elastic_constant"]);
-		$smarty->assign('wiki_3d_elastic_constant', $_REQUEST["wiki_3d_elastic_constant"]);
+	if (isset($_REQUEST["freetags_3d_elastic_constant"])) {
+		$tikilib->set_preference("freetags_3d_elastic_constant", $_REQUEST["freetags_3d_elastic_constant"]);
+		$smarty->assign('freetags_3d_elastic_constant', $_REQUEST["freetags_3d_elastic_constant"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_eletrostatic_constant"])) {
-		$tikilib->set_preference("wiki_3d_eletrostatic_constant", $_REQUEST["wiki_3d_eletrostatic_constant"]);
-		$smarty->assign('wiki_3d_eletrostatic_constant', $_REQUEST["wiki_3d_eletrostatic_constant"]);
+	if (isset($_REQUEST["freetags_3d_eletrostatic_constant"])) {
+		$tikilib->set_preference("freetags_3d_eletrostatic_constant", $_REQUEST["freetags_3d_eletrostatic_constant"]);
+		$smarty->assign('freetags_3d_eletrostatic_constant', $_REQUEST["freetags_3d_eletrostatic_constant"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_spring_size"])) {
-		$tikilib->set_preference("wiki_3d_spring_size", $_REQUEST["wiki_3d_spring_size"]);
-		$smarty->assign('wiki_3d_spring_size', $_REQUEST["wiki_3d_spring_size"]);
+	if (isset($_REQUEST["freetags_3d_spring_size"])) {
+		$tikilib->set_preference("freetags_3d_spring_size", $_REQUEST["freetags_3d_spring_size"]);
+		$smarty->assign('freetags_3d_spring_size', $_REQUEST["freetags_3d_spring_size"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_node_mass"])) {
-		$tikilib->set_preference("wiki_3d_node_mass", $_REQUEST["wiki_3d_node_mass"]);
-		$smarty->assign('wiki_3d_node_mass', $_REQUEST["wiki_3d_node_mass"]);
+	if (isset($_REQUEST["freetags_3d_node_mass"])) {
+		$tikilib->set_preference("freetags_3d_node_mass", $_REQUEST["freetags_3d_node_mass"]);
+		$smarty->assign('freetags_3d_node_mass', $_REQUEST["freetags_3d_node_mass"]);
 	}
 
-	if (isset($_REQUEST["wiki_3d_node_charge"])) {
-		$tikilib->set_preference("wiki_3d_node_charge", $_REQUEST["wiki_3d_node_charge"]);
-		$smarty->assign('wiki_3d_node_charge', $_REQUEST["wiki_3d_node_charge"]);
+	if (isset($_REQUEST["freetags_3d_node_charge"])) {
+		$tikilib->set_preference("freetags_3d_node_charge", $_REQUEST["freetags_3d_node_charge"]);
+		$smarty->assign('freetags_3d_node_charge', $_REQUEST["freetags_3d_node_charge"]);
 	}
 }
 
