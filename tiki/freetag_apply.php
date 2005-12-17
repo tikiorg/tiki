a64cdf85c72e2133187336037a4808c01da97ac5
lfagundes <lfagundes@users.sourceforge.net>
2005-12-17 19:32:51 +0000

    avoid warnings
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6276 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/freetag_apply.php
+++ tiki/freetag_apply.php
@@ -1,5 +1,5 @@
 <?php 
-// $Header: /cvsroot/tikiwiki/tiki/freetag_apply.php,v 1.4 2005-12-16 00:05:45 amette Exp $
+// $Header: /cvsroot/tikiwiki/tiki/freetag_apply.php,v 1.5 2005-12-17 19:32:51 lfagundes Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -27,6 +27,10 @@ if ($feature_freetags == 'y' and $tiki_p_freetags_tag == 'y') {
     $old_tags = $freetaglib->get_tags_on_object($cat_objid, $cat_type);
 
     global $user;
+
+    if (!isset($cat_desc)) $cat_desc = '';
+    if (!isset($cat_name)) $cat_name = '';
+    if (!isset($cat_href)) $cat_href = '';
 
     $freetaglib->add_object($cat_type, $cat_objid, $cat_desc, $cat_name, $cat_href);	
     $freetaglib->update_tags($user, $cat_objid, $cat_type, $tag_string);
