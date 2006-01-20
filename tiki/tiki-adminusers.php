25ca38b1a39a3b6d24c556d579299157f44b02b5
sylvieg <sylvieg@users.sourceforge.net>
2006-01-20 10:13:26 +0000

    bad merge
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6317 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-adminusers.php
+++ tiki/tiki-adminusers.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-adminusers.php,v 1.57 2006-01-20 09:54:53 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-adminusers.php,v 1.58 2006-01-20 10:13:26 sylvieg Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -119,7 +119,6 @@ function batchImportUsers() {
 					}
 				}
 			}
-		}
 		$added++;
 	}
 	$smarty->assign('added', $added);
