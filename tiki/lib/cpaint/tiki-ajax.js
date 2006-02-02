91dfc112886f3db31ea0542d1bf92fab2ba13f15
amette <amette@users.sourceforge.net>
2006-02-02 22:57:45 +0000

    Splitting off generic AJAX-pagination part into lib/cpaint/tiki-ajax.js
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6360 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/lib/cpaint/tiki-ajax.js
@@ -0,0 +1,40 @@
+/*
+	Functions for pagination of results
+*/
+
+function setOffset(deltaOffset) {
+	if (((offset + deltaOffset) < count) && ((offset + deltaOffset) >= 0)) {
+		offset = offset + deltaOffset;
+		listObjects(currentTag);
+	}
+}
+
+function resetOffset() {
+	offset = 0;
+	document.getElementById('actual_page').innerHTML = 1;
+}
+
+function wipeList(start) {
+	for (var i=start; i<maxRecords; i++) {
+		document.getElementById('freetagObjectLink_' + (i) ).href = '';
+		document.getElementById('freetagObjectLink_' + (i) ).innerHTML = '';
+		document.getElementById('freetagObjectType_' + (i) ).innerHTML = '';
+		document.getElementById('freetagObjectDescription_' + (i) ).innerHTML = '';
+	}
+}
+
+function updatePageCount(result) {
+	count = result.ajaxResponse[0].cant[0].data;
+	var cant_pages = Math.ceil(count / maxRecords);
+	if (cant_pages == 0) cant_pages = '1';
+	document.getElementById('cant_pages').innerHTML = cant_pages;
+	curPage = 1 + (offset / maxRecords);
+	document.getElementById('actual_page').innerHTML = curPage;
+	if (directPagination == 'y') {
+		var pageLink = '';
+		for (var i=0; i<cant_pages; i++) {
+			pageLink += '<a href="javascript:setOffset('+( i * maxRecords - offset ) +')";>'+(i+1)+'&nbsp;'+'</a>';
+		}
+		document.getElementById('direct_pagination').innerHTML = pageLink;
+	}
+}
--- tiki/lib/freetag/freetag_ajax.js
+++ tiki/lib/freetag/freetag_ajax.js
@@ -1,4 +1,4 @@
-// $Header: /cvsroot/tikiwiki/tiki/lib/freetag/freetag_ajax.js,v 1.20 2006-01-31 00:32:52 amette Exp $
+// $Header: /cvsroot/tikiwiki/tiki/lib/freetag/freetag_ajax.js,v 1.21 2006-02-02 22:57:45 amette Exp $
 
 //var maxRecords gets defined in the template !
 var objectType = '';
@@ -86,45 +86,4 @@ function renderObjectList(result) {
 
 	updatePageCount(result);
 	document.getElementById('ajaxLoading').style.display = 'none';
-}
-
-/*
-	Functions for pagination of results
-*/
-
-function setOffset(deltaOffset) {
-	if (((offset + deltaOffset) < count) && ((offset + deltaOffset) >= 0)) {
-		offset = offset + deltaOffset;
-		listObjects(currentTag);
-	}
-}
-
-function resetOffset() {
-	offset = 0;
-	document.getElementById('actual_page').innerHTML = 1;
-}
-
-function wipeList(start) {
-	for (var i=start; i<maxRecords; i++) {
-		document.getElementById('freetagObjectLink_' + (i) ).href = '';
-		document.getElementById('freetagObjectLink_' + (i) ).innerHTML = '';
-		document.getElementById('freetagObjectType_' + (i) ).innerHTML = '';
-		document.getElementById('freetagObjectDescription_' + (i) ).innerHTML = '';
-	}
-}
-
-function updatePageCount(result) {
-	count = result.ajaxResponse[0].cant[0].data;
-	var cant_pages = Math.ceil(count / maxRecords);
-	if (cant_pages == 0) cant_pages = '1';
-	document.getElementById('cant_pages').innerHTML = cant_pages;
-	curPage = 1 + (offset / maxRecords);
-	document.getElementById('actual_page').innerHTML = curPage;
-	if (directPagination == 'y') {
-		var pageLink = '';
-		for (var i=0; i<cant_pages; i++) {
-			pageLink += '<a href="javascript:setOffset('+( i * maxRecords - offset ) +')";>'+(i+1)+'&nbsp;'+'</a>';
-		}
-		document.getElementById('direct_pagination').innerHTML = pageLink;
-	}
 }
