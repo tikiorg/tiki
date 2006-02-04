b50d34b7f52ec1eb82c2713822c67daede918f78
jdrexler <jdrexler@users.sourceforge.net>
2006-02-04 17:16:47 +0000

    removed PHP warnings about array_merge for add/delete blocks
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@6364 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/diff/renderer_inline.php
+++ lib/diff/renderer_inline.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/tikiwiki/tiki/lib/diff/renderer_inline.php,v 1.2 2006-02-01 21:41:01 jdrexler Exp $
+// $Header: /cvsroot/tikiwiki/tiki/lib/diff/renderer_inline.php,v 1.3 2006-02-04 17:16:47 jdrexler Exp $
 /**
  * "Inline" diff renderer.
  *
@@ -26,8 +26,12 @@ class Text_Diff_Renderer_inline extends Text_Diff_Renderer_sidebyside {
 	$orig = array();
 	$final = array();
 	foreach ($edits as $edit) {
-		$orig = array_merge($orig, $edit->orig);
-		$final = array_merge($final, $edit->final);
+		if (is_array($edit->orig)) {
+			$orig = array_merge($orig, $edit->orig);
+		}
+		if (is_array($edit->final)) {
+			$final = array_merge($final, $edit->final);
+		}
 	}
 	$lines = diffChar($orig, $final, $this->_words, "character_inline");
 	echo "<tr class='diffbody'><td colspan='3'>$lines[0]</td></tr>\n";
