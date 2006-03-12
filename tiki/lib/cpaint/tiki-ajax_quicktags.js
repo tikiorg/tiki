ac9e858cc55840f281030fa9b7e4c4f25087e26c
lfagundes <lfagundes@users.sourceforge.net>
2006-03-12 13:16:53 +0000

    changed quicktags ajax so to use the framework
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6439 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/cpaint/tiki-ajax_quicktags.js
+++ tiki/lib/cpaint/tiki-ajax_quicktags.js
@@ -1,10 +1,17 @@
 var category = '';
 var offset = 0;
-var sort_mode = 'taglabel_asc';
 var filter = '';
+sort_mode = 'taglabel_asc';
+
+function listObjects(tag) {
+    load('quicktags_list_objects',category,offset,sort_mode,filter);
+
+    document.getElementById('ajaxLoading').style.display = 'block';
+
+    currentTag = tag;
+}
 
 function handle_quicktags_list_objects(result) {
-    alert(1);
 	if (!result[0]) {
 		wipeList(0);
 		updatePageCount(result);
@@ -15,6 +22,7 @@ function handle_quicktags_list_objects(result) {
 	for (i=0; i < result.length; i++) {
 	    for (var j=0; j<ajax_cols.length; j++) {
 		name = ajax_cols[j][0];
+		id = ajax_cols[j][0] + '_' + i;
 		
 		if ( ajax_cols[j][1] == 'innerHTML' ) {
 		    document.getElementById( id ).innerHTML = result[i][name];
@@ -33,47 +41,3 @@ function handle_quicktags_list_objects(result) {
 	document.getElementById('ajaxLoading').style.display = 'none';
 }
 
-function listObjects(tag) {
-    load('quicktags_list_objects',category,offset,sort_mode,filter);
-    document.getElementById('ajaxLoading').style.display = 'block';
-    return;
-
-	currentTag = tag;
-	var cp = new cpaint();
-	//cp.set_debug(2);
-
-	document.getElementById('ajaxLoading').style.display = 'block';
-
-	cp.call('tiki-ajax_quicktags.php', 'list_objects', renderObjectList, category, offset, sort_mode, filter);
-}
-
-function renderObjectList(result) {
-	var objects = result.ajaxResponse[0].object;
-
-	if (!objects) {
-		wipeList(0);
-		updatePageCount(result);
-		document.getElementById('ajaxLoading').style.display = 'none';
-		return;
-	}
-
-	for (i=0; i < objects.length; i++) {
-		for (var j=0; j<ajax_cols.length; j++) {
-			name = ajax_cols[j][0];
-			id = ajax_cols[j][0] + '_' + i;
-			if ( ajax_cols[j][1] == 'innerHTML' ) {
-				document.getElementById( id ).innerHTML = objects[i].find_item_by_id(name, id).data;
-			} else if ( ajax_cols[j][1] == 'a' ) {
-				document.getElementById( id ).innerHTML = objects[i].find_item_by_id(name, id).data;
-				document.getElementById( id ).href = objects[i].find_item_by_id(name, id).data;
-			}
-		}
-	}
-
-	if (objects.length < maxRecords) { // Need to wipe out the rest
-		wipeList(objects.length)
-	}
-
-	updatePageCount(result);
-	document.getElementById('ajaxLoading').style.display = 'none';
-}
