dd367b77a129e2fefc12d1ad209859455a1678b6
lfagundes <lfagundes@users.sourceforge.net>
2006-03-20 04:49:21 +0000

    [NEW] ajax_href smarty block that makes a common url into a js call to dinamically load a component in screen.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6457 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/lib/smarty_tiki/block.ajax_href.php
@@ -0,0 +1,26 @@
+<?php
+
+//this script may only be included - so its better to die if called directly.
+if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
+  header("location: index.php");
+  exit;
+}
+
+function smarty_block_ajax_href($params, $content, &$smarty)
+{
+    global $feature_ajax;
+
+    $url = $content;
+    $template = $params['template'];
+    $htmlelement = $params['htmlelement'];
+
+    if ($feature_ajax != 'y') {
+	return " href=\"$url\" ";
+    } else {
+	return " href=\"#\" onclick=\"loadComponent('$url','$template','$htmlelement');\" ";
+    }
+}
+
+/* vim: set expandtab: */
+
+?>
