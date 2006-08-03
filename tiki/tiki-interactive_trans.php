7460add2cdf96f7e80d405a86eea57dfb43bbea5
hangerman <hangerman@users.sourceforge.net>
2006-08-03 14:59:10 +0000

    moove presentation to tpl
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6620 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-interactive_trans.php
+++ tiki/tiki-interactive_trans.php
@@ -1,133 +1,123 @@
-<?php require_once ('tiki-setup.php');?>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"  />
-<script type="text/javascript" src="lib/tiki-js.js"></script>
-<title>Interactive Translator</title>
-<link rel="StyleSheet"  href="styles/tikineat.css" type="text/css" />
-</head>
-<body class="tiki_wiki">
-<?php
+<?php 
+require_once ('tiki-setup.php');
+if ($lang_use_db != 'y') {
+	$smarty->assign('msg', tra("This feature is disabled").": lang_use_db");
+	$smarty->assign('error', 'y');
+	$smarty->display("tiki-interactive_trans.tpl");
+	die;
+}
+
+if ($tiki_p_edit_languages != 'y') {
+	$smarty->assign('msg', tra("Permission denied to use this feature"));
+	$smarty->assign('error', 'y');
+	$smarty->display("tiki-interactive_trans.tpl");
+	die;
+}
 
 
-echo "<center><h1>Interactive translator</h1>";
 if (isset($_REQUEST['src']))	$_REQUEST['content']=$_REQUEST['src'];
-echo "<b><i>'".$_REQUEST['content']."'</i></b></center>";
+$smarty->assign('analysed_word', $_REQUEST['content']);
+
 
 function update_trans($add_tran_source,$add_tran_tran,$edit_language){
-	global $tikilib;
+	global $tikilib,$smarty;
 	$i=0;
-	$query="select * from tiki_language where lang='$edit_language' and (source = '".$add_tran_source."' )";
-	$result=mysql_query($query);
-	$exist=($row = mysql_fetch_assoc($result));
-	if (!$exist){
-		$query = "insert into `tiki_language` values(binary '$add_tran_source','$edit_language',binary '$add_tran_tran' )";
-		$result=mysql_query($query);
-		if (mysql_errno())
-        	{
-           		echo "MySQL error ".mysql_errno().": ".mysql_error()."\nWhen 	executing:<b>$query</b><br>";
-	   		exit;
- 		}
+	$query="select * from `tiki_language` where `lang`=? and `source` = ?";
+	$result=$tikilib->query($query,array($edit_language,$add_tran_source));
+	if (!$result->numRows()){
+		$query = "insert into `tiki_language` values(binary ?,?,binary ? )";
+		$result= $tikilib->query($query,array($add_tran_source,$edit_language,$add_tran_tran));
 	}else {
-		$query = "update `tiki_language` set `tran`='$add_tran_tran' where `source`=binary '$add_tran_source' and `lang`='$edit_language'";
-		$result=mysql_query($query);
-		if (mysql_errno())
-		{
-		echo "MySQL error ".mysql_errno().": ".mysql_error()."\nWhen executing:<b>$query</b><br>";
-		exit;
+		if (strlen($add_tran_tran)==0){
+			$query="delete from `tiki_language` where `source`=binary ? and `lang`=?";
+			$result=$tikilib->query($query,array($add_tran_source,$edit_language));
+		}else{
+			$query = "update `tiki_language` set `tran`=binary ? where `source`=binary ? and `lang`=?";
+			$result=$tikilib->query($query,array($add_tran_tran,$add_tran_source,$edit_language));
 		}
+		
+	}
+	if (!isset($result)){
+		$smarty->assign('msg', tra("Error writing in the databse: $query"));
+		$smarty->assign('error', 'y');
+		$smarty->display("tiki-interactive_trans.tpl");
 	}	
 }
 
+
 function getTrans($trans,$lang){
-
+	global $tikilib,$smarty,$entries;
 	//First do the exact matching
-	$query="select * from tiki_language where lang='$lang' and (source like '".$trans."' or tran like '".$trans."') order by lang";
-	$res=mysql_query($query);
-	if (mysql_errno())
-	{
-		echo "MySQL error ".mysql_errno().": ".mysql_error()."\nWhen executing:<b>$query</b><br>";
-		//exit;
-		}
+	$query="select * from `tiki_language` where `lang`=? and (`source` = ? or `tran` = ?) order by lang";
+	$result=$tikilib->query($query,array($lang,$trans,$trans));
 	$compteur=0;
-	while ($row = mysql_fetch_assoc($res)){
-	$compteur++;
-	$class=($compteur % 2)?"odd":"even";
-	echo "<form><tr class='$class'><td width='40%'>";
-	echo "<input type=hidden name='lang' value='$lang'>";
-	echo "<input type=hidden name='src' value='".urlencode($row['source'])."'>";
-	echo $row['source']."</td><td>";
-	echo "<input type=text name='dst' value='".$row['tran']."' style='width:190px;' >&nbsp;&nbsp;";
-	echo "<input type=submit name='submit' value='Submit'>";
-	echo "</td></tr></form>";
-	}
-
-	//Let's find the other one
-	$query="select * from tiki_language where lang='$lang' and (source like '%".$trans."%' or tran like '%".$trans."%') order by lang";
-	$res=mysql_query($query);
-	if (mysql_errno())
-	{
-		echo "MySQL error ".mysql_errno().": ".mysql_error()."\nWhen executing:<b>$query</b><br>";
-		//exit;
-		}
-	while ($row = mysql_fetch_assoc($res))
-	if (!(strlen($row['source'])==strlen($trans))){
-	$compteur++;
-	$class=($compteur % 2)?"odd":"even";
-	echo "<form><tr class='$class'><td width='40%'>";
-	echo "<input type=hidden name='lang' value='$lang'>";
-	echo "<input type=hidden name='src' value='".urlencode($row['source'])."'>";
-	echo $row['source']."</td><td>";
-	echo "<input type=text name='dst' value='".$row['tran']."' style='width:190px;' >&nbsp;&nbsp;";
-	echo "<input type=submit name='submit' value='Submit'>";
-	echo "</td></tr></form>";
-	}
-
-
+	$exact=false;
+	while ($res = $result->fetchRow()) {
+		$compteur++;	
+		$exact=true;
+		$entry=array();
+		$entry["lang"] = $res["lang"];
+		$entry["source"] = $res["source"];
+		$entry["urlsource"] = urlencode($res["source"]);
+		$entry["trans"] = $res["tran"]."";
+		$entries[]=$entry;
+	}	
 	
-	if ($compteur==0){
-	$class=($compteur % 2)?"odd":"even";
-	echo "<form><tr class='$class'><td width='40%'>";
-	echo "<input type=hidden name='lang' value='$lang'>";
-	echo "<input type=hidden name='src' value='".urlencode($trans)."'>";
-	echo "$trans:"."</td><td>";
-	echo "<input type=text name='dst' value='' style='width:190px;'>&nbsp;&nbsp;";
-	echo "<input type=submit name='submit' value='Submit'>";
-	echo "</td></tr></form>";
+	$query="select * from `tiki_language` where `lang`=? and (`source` like ? or `tran` like ?) order by lang";
+	$result=$tikilib->query($query,array($lang,"%".$trans."%","%".$trans."%"));
+	while ($res = $result->fetchRow())
+		if (strlen($res["source"])!=strlen($trans)){
+		$compteur++;
+		$entry=array();
+		$entry["lang"] = $res["lang"];
+		$entry["source"] = $res["source"];
+		$entry["urlsource"] = urlencode($res["source"]);
+		$entry["trans"] = $res["tran"];
+		$entries[]=$entry;
+	}	
+	
+	if (!$exact){
+		$compteur++;
+		$entry=array();
+		$entry["lang"] = $lang;
+		$entry["source"] = $trans;
+		$entry["urlsource"] = urlencode($trans);
+		$entry["trans"] = "";
+		$entries[]=$entry;
+		
 	}
 }
 
 function getLanguages(){
+	global $tikilib;
 	$query = "select `lang` from `tiki_languages`";
-	$res=mysql_query($query);
+	$result=$tikilib->query($query);
 	$languages = array();
-	while ($row = mysql_fetch_assoc($res))
+	while ($row = $result->fetchRow())
 		$languages[] = $row["lang"];
 	return $languages;
 }
 
 //Update the translation
-if (isset($_REQUEST['src'])&&isset($_REQUEST['lang'])&&isset($_REQUEST['dst'])){
+if (!isset($_REQUEST['dst']))$_REQUEST['dst']="";
+if (isset($_REQUEST['src'])&&isset($_REQUEST['lang'])){
 	$_REQUEST['src']=urldecode($_REQUEST['src']);
 	$_REQUEST['dst']=urldecode($_REQUEST['dst']);
 	$_REQUEST['src'] = htmlentities($_REQUEST['src'], ENT_NOQUOTES, "UTF-8");
 	$_REQUEST['dst'] = htmlentities($_REQUEST['dst'], ENT_NOQUOTES, "UTF-8");
 	update_trans( $_REQUEST['src'],$_REQUEST['dst'],$_REQUEST['lang']);
-	echo  "<center>&nbsp;&nbsp;&nbsp; has been updated <br> <center><input type='submit' value ='Close this window' onclick='window.opener.location.reload();self.close();'>&nbsp;&nbsp;<input type='submit' value ='Go Back' onclick='javascript:history.go(-1);'>";
-	echo "<br>*Closing the window will reload your main browser";
+	$smarty->assign('update', 'y');
+	$smarty->display("tiki-interactive_trans.tpl");
 	die;
 }
 
 //Main windows 
-echo "<center><input type='submit' value ='Close this window' onclick='self.close();'></center>";
-echo "<table class='normal'>";
-$languages[]= getLanguages();
-foreach ($languages as $key1 => $value1)
-	foreach ($value1 as $key => $value){
-	echo "<tr><td colspan=2 class='heading'><b>Language:<i> $value</i></b></td></tr>";
-	echo getTrans(urldecode($_REQUEST['content']),$value);
-	echo "<tr><td colspan='2'>&nbsp;</td></tr>";
-}
-echo "</table>";
+$languages= getLanguages();
+$entries=array();
+foreach ($languages as $key => $value)
+	getTrans(urldecode($_REQUEST['content']),$value);
 
+$smarty->assign('languages', $languages);
+$smarty->assign('entries', $entries);
+$smarty->display("tiki-interactive_trans.tpl");
 ?>
