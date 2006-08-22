08da6ff517e91bfe889731886906f8ea37a5684c
jreyesg <jreyesg@users.sourceforge.net>
2006-08-22 19:39:14 +0000

    Workspace zones
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@6632 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/workspaces/workspacemoduleslib.php
+++ lib/workspaces/workspacemoduleslib.php
@@ -43,10 +43,10 @@ class WorkspaceModulesLib extends TikiLib {
 		$result = $this->query($query,array($position,$moduleId));
 	}
 
-	function assign_workspace_module($name,$position,$ord,$type,$workspaceId,$title,$cache_time,$rows,$params,$groups,$style_title,$style_data) {
+	function assign_workspace_module($name,$position,$ord,$zoneId,$title,$cache_time,$rows,$params,$groups,$style_title,$style_data) {
 		$uid = md5(uniqid(rand()));
-		$query = 'insert into `tiki_workspace_modules`(`name`,`position`,`ord`,`type`,`workspaceId`,`title`,`cache_time`,`rows`,`params`,`groups`,`style_title`,`style_data`,`uid`) values(?,?,?,?,?,?,?,?,?,?,?,?,?)';
-		$bindvars = array($name,$position,$ord,$type,$workspaceId,$title,$cache_time,$rows,$params,$groups,$style_title,$style_data,$uid);
+		$query = 'insert into `tiki_workspace_modules`(`name`,`position`,`ord`,`zoneId`,`title`,`cache_time`,`rows`,`params`,`groups`,`style_title`,`style_data`,`uid`) values(?,?,?,?,?,?,?,?,?,?,?,?)';
+		$bindvars = array($name,$position,$ord,$zoneId,$title,$cache_time,$rows,$params,$groups,$style_title,$style_data,$uid);
 		$result = $this->query($query, $bindvars);
 		return $uid;
 	}
@@ -67,10 +67,10 @@ class WorkspaceModulesLib extends TikiLib {
 		return $res;
 	}
 	
-	function get_workspace_assigned_modules($workspaceId,$type) {
-		$query = "select * from `tiki_workspace_modules` where `workspaceId`=? and `type`=? order by `position` asc,`ord` asc";
+	function get_workspace_assigned_modules($zoneId) {
+		$query = "select * from `tiki_workspace_modules` where `zoneId`=? order by `position` asc,`ord` asc";
 
-		$result = $this->query($query,array($workspaceId,$type));
+		$result = $this->query($query,array($zoneId));
 		$ret = array();
 
 		while ($res = $result->fetchRow()) {
@@ -80,8 +80,8 @@ class WorkspaceModulesLib extends TikiLib {
 		return $ret;
 	}
 
-	function get_ws_assigned_modules_by_cols($workspaceId,$type) {
-		$ret = $this->get_workspace_assigned_modules($workspaceId,$type);
+	function get_ws_assigned_modules_by_cols($zoneId) {
+		$ret = $this->get_workspace_assigned_modules($zoneId);
 		$cols = array();
 		foreach ($ret as $key => $module){
 			if (!isset($cols[$module["position"]])){
@@ -92,10 +92,10 @@ class WorkspaceModulesLib extends TikiLib {
 		return $cols;
 	}
 	
-	function get_workspace_assigned_modules_pos($workspaceId,$type, $pos) {
-		$query = "select * from `tiki_workspace_modules` where `workspaceId`=? and `type`=? and `position`=? order by `ord` asc";
+	function get_workspace_assigned_modules_pos($zoneId, $pos) {
+		$query = "select * from `tiki_workspace_modules` where `zoneId`=? and `position`=? order by `ord` asc";
 
-		$result = $this->query($query,array($workspaceId,$type, $pos));
+		$result = $this->query($query,array($zoneId, $pos));
 		$ret = array();
 
 		while ($res = $result->fetchRow()) {
@@ -105,8 +105,8 @@ class WorkspaceModulesLib extends TikiLib {
 		return $ret;
 	}
 
-	function workspace_has_assigned_modules($workspaceId,$type) {
-		$query = "select count(`name`) from `tiki_workspace_modules` where `workspaceId`=? and `type`=?";
+	function workspace_has_assigned_zones($workspaceId,$type) {
+		$query = "select count(`name`) from `tiki_workspace_zones` where `workspaceId`=? and `type`=?";
 
 		$result = $this->getOne($query,array($workspaceId,$type));
 		return $result;
@@ -122,6 +122,69 @@ class WorkspaceModulesLib extends TikiLib {
         $res = $r->fetchRow();
         $this->set_column_workspace_module($name, $user, ($res['position'] == 'r' ? 'l' : 'r'));
     }
+    
+	function get_zones($workspaceId,$type){
+		$query = "SELECT * FROM `tiki_workspace_zones` WHERE `workspaceId` = ? and `type`=? order by `ord` asc";
+		$result = $this->query($query,array($workspaceId,$type));
+		$ret = array();
+		while ($res = $result->fetchRow()) {
+			$ret[$res["zoneId"]] = $res;
+		}
+
+		return $ret;
+	}
+	
+	function get_zone_by_uid($uid){
+		$query = "SELECT * FROM `tiki_workspace_zones` WHERE `uid`=? ";
+		$result = $this->query($query,array($uid));
+		$ret = array();
+
+		while ($res = $result->fetchRow()) {
+			$ret[] = $res;
+		}
+
+		return $ret;
+	}
+	
+	function get_zone_by_id($id){
+		$query = "SELECT * FROM `tiki_workspace_zones` WHERE `zoneId`=? ";
+		$result = $this->query($query,array($id));
+		$ret = array();
+
+		while ($res = $result->fetchRow()) {
+			$ret[] = $res;
+		}
+
+		return $ret;
+	}
+	
+	function add_zone($name, $description, $workspaceId, $type, $ord){
+		$uid = md5(uniqid(rand()));
+		$query = 'INSERT INTO `tiki_workspace_zones` ( `name`, `description`, `workspaceId`, `type`, `ord`, `uid`) VALUES (?, ?, ?, ?, ?, ?)';
+				
+		$bindvars = array($name, $description, $workspaceId, $type, $ord,$uid);
+		$result = $this->query($query, $bindvars);
+		return $uid;
+	}
+	
+	function update_zone($zoneId, $name, $description, $workspaceId, $type, $ord){
+		$query = "update `tiki_workspace_zones` set `name` = ?, `description` = ?, `workspaceId` = ?, `type` = ?, `ord` = ? WHERE `zoneId` = ? ";
+
+		$result = $this->query($query,array($name, $description, $workspaceId, $type, $ord,$zoneId));
+	}
+	
+	function delete_zone($zoneId){
+		$query = "delete from `tiki_workspace_zones` where `zoneId`=?";
+
+		$result = $this->query($query,array($zoneId));
+	}	
+	
+	function delete_assigned_zone_modules($zoneId){
+		$modules = $this->get_workspace_assigned_modules($zoneId);	
+		foreach ($modules as $key => $module) {
+				$this->unassign_workspace_module($module["moduleId"]);
+	}
+}
 }
 
 $wsmoduleslib = new WorkspaceModulesLib($dbTiki);
--- tiki-workspaces_assigned_modules.php
+++ tiki-workspaces_assigned_modules.php
@@ -24,7 +24,6 @@ if (!isset($_SESSION["currentWorkspace"]) && !isset($_REQUEST["wstypeId"])) {
 	die;
 }
 
-$wsuser = "";
 $workspacesLib = new WorkspaceLib($dbTiki);
 $wsTypesLib = new WorkspaceTypesLib($dbTiki);
 
@@ -36,7 +35,6 @@ if(isset($_REQUEST["wstypeId"]) && $_REQUEST["wsmodtype"]=="workspace"){
 	$wstypeId=$_REQUEST["wstypeId"];
 	$workspace = $workspacesLib->get_workspace_by_id($_REQUEST["wstypeId"]);
 	if (isset($workspace)){
-		$wsuser = "WSUSER-".$workspace["code"];
 		$wstypeId = $_REQUEST["wstypeId"];
 		$wsmodtype  = "workspace";
 		$title = "Workspace: (".$workspace["code"].") ".$workspace["name"];
@@ -45,7 +43,6 @@ if(isset($_REQUEST["wstypeId"]) && $_REQUEST["wsmodtype"]=="workspace"){
 }elseif(isset($_REQUEST["wstypeId"]) && $_REQUEST["wsmodtype"]=="workspace type"){
 	$wstype = $wsTypesLib->get_workspace_type_by_id($_REQUEST["wstypeId"]);
 	if (isset($wstype)){
-		$wsuser = "WSTUSER-".$wstype["code"];
 		$wstypeId = $_REQUEST["wstypeId"];
 		$wsmodtype  = "workspace type";
 		$title = "Workspace Type: ".$wstype["name"];
@@ -53,7 +50,6 @@ if(isset($_REQUEST["wstypeId"]) && $_REQUEST["wsmodtype"]=="workspace"){
 }elseif(isset($_SESSION["currentWorkspace"])){
 	$workspace = $workspacesLib->get_workspace_by_id($_SESSION["currentWorkspace"]["wstypeId"]);
 	if (isset($workspace)){
-		$wsuser = "WSUSER-".$workspace["code"];
 		$wstypeId = $_REQUEST["wstypeId"];
 		$wsmodtype  = "workspace";
 		$title = "Workspace: (".$workspace["code"].") ".$workspace["name"];
@@ -67,106 +63,143 @@ if (!isset($wstypeId) || $wstypeId=="") {
 	die;
 }
 
-$max_columns = $max_columns = array_pop(array_keys($wsmoduleslib->get_ws_assigned_modules_by_cols($wstypeId,$wsmodtype)));
-
-$smarty->assign('title', $title);
-
-if (isset($_REQUEST["recreate"])) {
-	check_ticket('user-modules');
-	assign_wstype_modules($wstypeId,$wsmodtype,$wsmoduleslib);
-}
-if (isset($_REQUEST["clean"])) {
-	check_ticket('user-modules');
-	clean_assigned_modules($wstypeId,$wsmodtype,$wsmoduleslib);
+if (isset($_REQUEST["createZone"]) && isset($_REQUEST["editZoneId"]) && $_REQUEST["editZoneId"]!="" ){
+	$wsmoduleslib->update_zone($_REQUEST["editZoneId"], $_REQUEST["zoneName"], $_REQUEST["zoneDesc"], $wstypeId, $wsmodtype, $_REQUEST["zoneOrder"]);
+}elseif(isset($_REQUEST["createZone"])){
+	$uid = $wsmoduleslib->add_zone($_REQUEST["zoneName"], $_REQUEST["zoneDesc"], $wstypeId, $wsmodtype, $_REQUEST["zoneOrder"]);
+}elseif(isset($_REQUEST["removeZone"])){
+	$wsmoduleslib->delete_assigned_zone_modules($_REQUEST["removeZone"]);
+	$wsmoduleslib->delete_zone($_REQUEST["removeZone"]);
 }
 
-if (isset($_REQUEST["unassign"])) {
-	check_ticket('user-modules');
-	$wsmoduleslib->unassign_workspace_module($_REQUEST["moduleId"]);
+$zones = $wsmoduleslib->get_zones($wstypeId,$wsmodtype);
+
+$activeZone = "";
+
+if (isset($_REQUEST["zoneId"]) && isset($zones[$_REQUEST["zoneId"]]) && $zones[$_REQUEST["zoneId"]]!=""){
+	$activeZone = $zones[$_REQUEST["zoneId"]];
+}elseif(isset($zones) && count($zones)>0){
+	$zonevalues = array_values($zones);
+	$activeZone = $zonevalues[0];
 }
 
-if (isset($_REQUEST["assign"]) && isset($_REQUEST["moduleId"]) && $_REQUEST["moduleId"]!="" ) {
-	check_ticket('user-modules');
-	//$wsmoduleslib->update_workspace_module($_REQUEST["module"],$_REQUEST["position"],$_REQUEST["order"],$_REQUEST["title"],$_REQUEST["cache_time"],$_REQUEST["rows"],$_REQUEST["params"],$_REQUEST["groups"],$_REQUEST["moduleId"]);
-	$params = get_request_params();
-	$wsmoduleslib->update_workspace_module($_REQUEST["module"],$_REQUEST["position"],$_REQUEST["order"],$_REQUEST["title"],"","",$params,"",$_REQUEST["style_title"],$_REQUEST["style_data"],$_REQUEST["moduleId"]);
-}else if(isset($_REQUEST["assign"])){
-	check_ticket('user-modules');
-	$params = get_request_params();
-	//$wsmoduleslib->assign_workspace_module($_REQUEST["module"],$_REQUEST["position"],$_REQUEST["order"],$wsmodtype,$wstypeId,$_REQUEST["title"],$_REQUEST["cache_time"],$_REQUEST["rows"],$_REQUEST["params"],$_REQUEST["groups"]);
-	$wsmoduleslib->assign_workspace_module($_REQUEST["module"],$_REQUEST["position"],$_REQUEST["order"],$wsmodtype,$wstypeId,$_REQUEST["title"],"","",$params,"",$_REQUEST["style_title"],$_REQUEST["style_data"]);
-}else if (isset($_REQUEST["edit"])) {
-	$editmod = $wsmoduleslib->get_workspace_assigned_module($_REQUEST["edit"]);
-	if (isset($editmod) && $editmod!=""){
-		$arrayparams = get_module_params($editmod["name"]);
-		if ($editmod["params"] && $editmod["params"]!="" && isset($arrayparams) && count($arrayparams)>0){
-			parse_str($editmod["params"], $module_params);
-			foreach ($arrayparams as $key => $param) {
-				if (isset($module_params[$key])){
-					$arrayparams[$key]["defaultValue"] = $module_params[$key];
-				}else{
-					$arrayparams[$key]["defaultValue"] ="";
-				}
-			}
-			$smarty->assign('arrayparams', $arrayparams);
+if (isset($activeZone) && $activeZone!=""){
+		$smarty->assign_by_ref('zones', $zones);
+		
+		$max_columns = array_pop(array_keys($wsmoduleslib->get_ws_assigned_modules_by_cols($activeZone["zoneId"])));
+		
+		$smarty->assign('title', $title);
+		
+		if (isset($_REQUEST["clean"])) {
+			check_ticket('user-modules');
+			$wsmoduleslib->delete_assigned_zone_modules($activeZone["zoneId"]);
 		}
-		$smarty->assign('module', $editmod);
-	}
-}else if(isset($_REQUEST["selectModule"]) && $_REQUEST["selectModule"]=="y"){
-	$phpfile = 'modules/conf/modconf-'.$_REQUEST["module"].'.php';
-	$module = array();
-	$module["name"] = $_REQUEST["module"];
-	if (file_exists($phpfile)) {
-		include ($phpfile);
-		//$fun = create_function('', "module_".$_REQUEST["module"]."_help");
-        //$ret = $fun();
-        $funHelp = "module_".$_REQUEST["module"]."_help";
-        $funTitle = "module_".$_REQUEST["module"]."_title";
-        $funParams = "module_".$_REQUEST["module"]."_params";
-        $help = $funHelp();
-        $title = $funTitle();
-        $params = $funParams();
-        $module["title"] = $title;
-        $module["help"] = $help;
-        $smarty->assign('arrayparams', $params);
-	}
-	$smarty->assign('module', $module);
-}
+		
+		if (isset($_REQUEST["unassign"])) {
+			check_ticket('user-modules');
+			$wsmoduleslib->unassign_workspace_module($_REQUEST["moduleId"]);
+		}
+		
+		if (isset($_REQUEST["assign"]) && isset($_REQUEST["moduleId"]) && $_REQUEST["moduleId"]!="" ) {
+			check_ticket('user-modules');
+			//$wsmoduleslib->update_workspace_module($_REQUEST["module"],$_REQUEST["position"],$_REQUEST["order"],$_REQUEST["title"],$_REQUEST["cache_time"],$_REQUEST["rows"],$_REQUEST["params"],$_REQUEST["groups"],$_REQUEST["moduleId"]);
+			$params = get_request_params();
+			$wsmoduleslib->update_workspace_module($_REQUEST["module"],$_REQUEST["position"],$_REQUEST["order"],$_REQUEST["title"],"","",$params,"",$_REQUEST["style_title"],$_REQUEST["style_data"],$_REQUEST["moduleId"]);
+		}else if(isset($_REQUEST["assign"])){
+			check_ticket('user-modules');
+			$params = get_request_params();
+			//$wsmoduleslib->assign_workspace_module($_REQUEST["module"],$_REQUEST["position"],$_REQUEST["order"],$wsmodtype,$wstypeId,$_REQUEST["title"],$_REQUEST["cache_time"],$_REQUEST["rows"],$_REQUEST["params"],$_REQUEST["groups"]);
+			$wsmoduleslib->assign_workspace_module($_REQUEST["module"],$_REQUEST["position"],$_REQUEST["order"],$activeZone["zoneId"],$_REQUEST["title"],"","",$params,"",$_REQUEST["style_title"],$_REQUEST["style_data"]);
+		}else if (isset($_REQUEST["edit"])) {
+			$editmod = $wsmoduleslib->get_workspace_assigned_module($_REQUEST["edit"]);
+			if (isset($editmod) && $editmod!=""){
+				$arrayparams = get_module_params($editmod["name"]);
+				if ($editmod["params"] && $editmod["params"]!="" && isset($arrayparams) && count($arrayparams)>0){
+					parse_str($editmod["params"], $module_params);
+					foreach ($arrayparams as $key => $param) {
+						if (isset($module_params[$key])){
+							$arrayparams[$key]["defaultValue"] = $module_params[$key];
+						}else{
+							$arrayparams[$key]["defaultValue"] ="";
+						}
+					}
+					$smarty->assign('arrayparams', $arrayparams);
+				}
+				$smarty->assign('module', $editmod);
+			}
+		}else if(isset($_REQUEST["selectModule"]) && $_REQUEST["selectModule"]=="y"){
+			$phpfile = 'modules/conf/modconf-'.$_REQUEST["module"].'.php';
+			$module = array();
+			$module["name"] = $_REQUEST["module"];
+			if (file_exists($phpfile)) {
+				include ($phpfile);
+				//$fun = create_function('', "module_".$_REQUEST["module"]."_help");
+		        //$ret = $fun();
+		        $funHelp = "module_".$_REQUEST["module"]."_help";
+		        $funTitle = "module_".$_REQUEST["module"]."_title";
+		        $funParams = "module_".$_REQUEST["module"]."_params";
+		        $help = $funHelp();
+		        $title = $funTitle();
+		        $params = $funParams();
+		        $module["title"] = $title;
+		        $module["help"] = $help;
+		        $smarty->assign('arrayparams', $params);
+			}
+			$smarty->assign('module', $module);
+		}
+			
+		if (isset($_REQUEST["up"]) && isset($_REQUEST["moduleId"])) {
+			check_ticket('user-modules');
+			$wsmoduleslib->up_workspace_module($_REQUEST["moduleId"]);
+		}
+		
+		if (isset($_REQUEST["down"]) && isset($_REQUEST["moduleId"])) {
+			check_ticket('user-modules');
+			$wsmoduleslib->down_workspace_module($_REQUEST["moduleId"]);
+		}
+		
+		if (isset($_REQUEST["left"]) && isset($_REQUEST["moduleId"])) {
+			check_ticket('user-modules');
+			$module = $wsmoduleslib->get_workspace_assigned_module($_REQUEST["moduleId"]);
+			if (isset($module) && $module["position"]>1){
+				$wsmoduleslib->set_column_workspace_module($_REQUEST["moduleId"], $module["position"]-1);
+			}
+		}
+		
+		if (isset($_REQUEST["right"]) && isset($_REQUEST["moduleId"])) {
+			check_ticket('user-modules');
+			$module = $wsmoduleslib->get_workspace_assigned_module($_REQUEST["moduleId"]);
+			if (isset($module)){
+				$wsmoduleslib->set_column_workspace_module($_REQUEST["moduleId"], $module["position"]+1);
+			}
+		}
+		
+		$smarty->assign_by_ref('activeZone',$activeZone);
+		if (isset($_REQUEST["editZone"])){
+			$smarty->assign_by_ref('editZone',$activeZone);
+		}
+		
+		$modules = $wsmoduleslib->get_ws_assigned_modules_by_cols($activeZone["zoneId"]);
 	
-if (isset($_REQUEST["up"]) && isset($_REQUEST["moduleId"])) {
-	check_ticket('user-modules');
-	$wsmoduleslib->up_workspace_module($_REQUEST["moduleId"]);
+		$max_columns = array_pop(array_keys($modules)) + 1 ;
+	
+		/*$columns = array();
+		for($i=1;$i<=$max_columns+1;$i++){
+			$columns[$i] = tra("Column ").$i;
+		} */
+
+}else{
+	$max_columns = 1;
 }
 
-if (isset($_REQUEST["down"]) && isset($_REQUEST["moduleId"])) {
-	check_ticket('user-modules');
-	$wsmoduleslib->down_workspace_module($_REQUEST["moduleId"]);
-}
-
-if (isset($_REQUEST["left"]) && isset($_REQUEST["moduleId"])) {
-	check_ticket('user-modules');
-	$module = $wsmoduleslib->get_workspace_assigned_module($_REQUEST["moduleId"]);
-	if (isset($module) && $module["position"]>1){
-		$wsmoduleslib->set_column_workspace_module($_REQUEST["moduleId"], $module["position"]-1);
-	}
-}
-
-if (isset($_REQUEST["right"]) && isset($_REQUEST["moduleId"])) {
-	check_ticket('user-modules');
-	$module = $wsmoduleslib->get_workspace_assigned_module($_REQUEST["moduleId"]);
-	if (isset($module)){
-		$wsmoduleslib->set_column_workspace_module($_REQUEST["moduleId"], $module["position"]+1);
-	}
-}
-
+/* Orders array */
 $orders = array();
-
 for ($i = 1; $i < 20; $i++) {
 	$orders[] = $i;
 }
-
 $smarty->assign_by_ref('orders', $orders);
 
+		
 $assignables = $modlib->get_all_modules();
 sort ($assignables);
 
@@ -176,22 +209,7 @@ if (count($assignables) > 0) {
 	$smarty->assign('canassign', 'n');
 }
 
-//print_r($assignables);
-$modules = $wsmoduleslib->get_ws_assigned_modules_by_cols($wstypeId,$wsmodtype);
-
-$max_columns = array_pop(array_keys($modules));
-
-$columns = array();
-for($i=1;$i<=$max_columns+1;$i++){
-	$columns[$i] = tra("Column ").$i;
-} 
-
-//array("1" => "Column 1", "2" => "Column 2", "3" => "Column 3","4" => "Column 4");
-//$modules = $wsmoduleslib->get_workspace_assigned_modules($wstypeId,$wsmodtype);
-
-$smarty->assign('modules_l', $wsmoduleslib->get_workspace_assigned_modules_pos($wstypeId,$wsmodtype, '1'));
-$smarty->assign('modules_r', $wsmoduleslib->get_workspace_assigned_modules_pos($wstypeId,$wsmodtype, '2'));
-$smarty->assign('columns',$columns);
+$smarty->assign('max_columns',$max_columns);
 $smarty->assign_by_ref('assignables', $assignables);
 $smarty->assign_by_ref('modules', $modules);
 $smarty->assign("wstypeId",$wstypeId);
@@ -205,20 +223,7 @@ $smarty->assign('mid', 'tiki-workspaces_assigned_modules.tpl');
 $smarty->display("tiki.tpl");
 
 
-function clean_assigned_modules($wstypeId,$wsmodtype,$wsmoduleslib){
-	$modules = $wsmoduleslib->get_workspace_assigned_modules($wstypeId,$wsmodtype);	
-	foreach ($modules as $key => $module) {
-			$wsmoduleslib->unassign_workspace_module($module["moduleId"]);
-	}
-}
 
-function assign_wstype_modules($wstypeId,$wsmodtype,$wsmoduleslib){
-	clean_assigned_modules($wstypeId,$wsmodtype,$wsmoduleslib);
-	$modules = $wsmoduleslib->get_workspace_assigned_modules($wstypeId,$wsmodtype);
-	foreach ($modules as $key => $module) {
-		$wsmoduleslib->assign_workspace_module($module["name"], $module["position"], $module["ord"],$wsmodtype,$wstypeId,$module["title"],$module["cache_time"],$module["rows"],$module["params"],$module["groups"]);
-	}	
-}
 
 function get_module_params($module_name){
 	$phpfile = 'modules/conf/modconf-'.$module_name.'.php';
