3a2ef83937857468c1c91a646adbf089ae8a03b5
ohertel <ohertel@users.sourceforge.net>
2006-09-09 00:40:44 +0000

    update: layout=>table shows list as sortable list (click on the table header!)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6672 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/wiki-plugins-dist/wikiplugin_userlist.php
+++ tiki/lib/wiki-plugins-dist/wikiplugin_userlist.php
@@ -2,12 +2,12 @@
 
 // Displays a list of users
 // Use:
-// {USERLIST(sep=>", ",max=>10,sort=>asc|desc)}substring{USERLIST}
+// {USERLIST(sep=>", ",max=>10,sort=>asc|desc,layout=>table)}substring{USERLIST}
 //
 // If no pattern is given returns all users or all that contain 'substring'
 
 function wikiplugin_userlist_help() {
-        return tra("Displays a list of registered users").":<br />~np~{USERLIST(sep=>\"SEPARATOR\",max=>MAXROWS,sort=>asc|desc)}substring{USERLIST}~/np~";
+        return tra("Displays a list of registered users").":<br />~np~{USERLIST(sep=>\"SEPARATOR\",max=>MAXROWS,sort=>asc|desc,layout=>table)}substring{USERLIST}~/np~";
 }
 
 function wikiplugin_userlist($data, $params) {
@@ -16,32 +16,40 @@ function wikiplugin_userlist($data, $params) {
 
     extract ($params,EXTR_SKIP);
 
-    if (!isset($sep)) $sep=", ";
+    if (!isset($sep)) $sep=', ';
     if (!isset($max)) { $numRows = -1; } else { $numRows = (int) $max; }
 
     if ($data) {
-        $mid = "`login` like ?";
-        $findesc = "%" . $data . '%';
+        $mid = '`login` like ?';
+        $findesc = '%' . $data . '%';
 	 $bindvars=array($findesc);
     } else {
-        $mid = "1";
+        $mid = '1';
         $bindvars=array();
     }
     if (isset($sort)) {
     	$sort=strtolower($sort);
-        if (($sort=="asc") || ($sort=="desc")) {
-            $mid .= " ORDER BY `login` ".$sort;
+        if (($sort=='asc') || ($sort=='desc')) {
+            $mid .= ' ORDER BY `login` '.$sort;
+        }
+    }
+    $pre=''; $post='';
+    if (isset($layout)) {
+        if ($layout=='table') {
+        	$pre='<table class=\'sortable\' id=\''.date('U').'\'><tr><th>'.tra('users').'</th></tr><tr><td>';
+        	$sep = '</td></tr><tr><td>';
+        	$post='</td></tr></table>';
         }
     }
     
-    $query = "select `login` from `users_users` where ".$mid;
+    $query = 'select `login` from `users_users` where '.$mid;
     $result = $tikilib->query($query, $bindvars, $numRows);
     $ret = array();
 
     while ($row = $result->fetchRow()) {
         $ret[] = $row['login'];
     }
-    return implode ( $sep, $ret );
+    return $pre.implode ( $sep, $ret ).$post;
 }
 
 ?>
