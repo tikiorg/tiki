3566c8e05293775be85ac63a5fe4ad71eed15492
ohertel <ohertel@users.sourceforge.net>
2006-09-09 00:14:22 +0000

    new plugin, that returns a full or filtered tiki user list: {USERLIST(sep=>", ",max=>10)}substring{USERLIST}
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@6670 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins-dist/wikiplugin_userlist.php
@@ -0,0 +1,40 @@
+<?php
+
+// Displays a list of users
+// Use:
+// {USERLIST(sep=>", ",max=>10)}substring{USERLIST}
+//
+// If no pattern is given returns all users or all that contain 'substring'
+
+function wikiplugin_userlist_help() {
+        return tra("Displays a list of registered users").":<br />~np~{USERLIST(sep=>\"SEPARATOR\",max=>MAXROWS)}substring{USERLIST}~/np~";
+}
+
+function wikiplugin_userlist($data, $params) {
+    global $tikilib;
+    global $userlib;
+
+    extract ($params,EXTR_SKIP);
+
+    if (!isset($sep)) $sep=", ";
+    if (!isset($max)) { $numRows = -1; } else { $numRows = (int) $max; }
+
+    if ($data) {
+        $mid = "`login` like ?";
+        $findesc = "%" . $data . '%';
+	 $bindvars=array($findesc);
+    } else {
+        $mid = "1";
+        $bindvars=array();
+    }
+    $query = "select `login` from `users_users` where ".$mid;
+    $result = $tikilib->query($query, $bindvars, $numRows);
+    $ret = array();
+
+    while ($row = $result->fetchRow()) {
+        $ret[] = $row['login'];
+    }
+    return implode ( $sep, $ret );
+}
+
+?>
