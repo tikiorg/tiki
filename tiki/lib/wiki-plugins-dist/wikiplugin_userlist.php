1f7e87c9d94a9daa0c0c9b04833cdcd4578461c2
ohertel <ohertel@users.sourceforge.net>
2006-09-09 00:29:38 +0000

    added sort: {USERLIST(sep=>", ",max=>10,sort=>asc|desc)}substring{USERLIST}
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6671 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/wiki-plugins-dist/wikiplugin_userlist.php
+++ tiki/lib/wiki-plugins-dist/wikiplugin_userlist.php
@@ -2,12 +2,12 @@
 
 // Displays a list of users
 // Use:
-// {USERLIST(sep=>", ",max=>10)}substring{USERLIST}
+// {USERLIST(sep=>", ",max=>10,sort=>asc|desc)}substring{USERLIST}
 //
 // If no pattern is given returns all users or all that contain 'substring'
 
 function wikiplugin_userlist_help() {
-        return tra("Displays a list of registered users").":<br />~np~{USERLIST(sep=>\"SEPARATOR\",max=>MAXROWS)}substring{USERLIST}~/np~";
+        return tra("Displays a list of registered users").":<br />~np~{USERLIST(sep=>\"SEPARATOR\",max=>MAXROWS,sort=>asc|desc)}substring{USERLIST}~/np~";
 }
 
 function wikiplugin_userlist($data, $params) {
@@ -27,6 +27,13 @@ function wikiplugin_userlist($data, $params) {
         $mid = "1";
         $bindvars=array();
     }
+    if (isset($sort)) {
+    	$sort=strtolower($sort);
+        if (($sort=="asc") || ($sort=="desc")) {
+            $mid .= " ORDER BY `login` ".$sort;
+        }
+    }
+    
     $query = "select `login` from `users_users` where ".$mid;
     $result = $tikilib->query($query, $bindvars, $numRows);
     $ret = array();
