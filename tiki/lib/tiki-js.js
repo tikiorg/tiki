0379faef9c0c5c03f3d8e04af775966f382d59a8
ohertel <ohertel@users.sourceforge.net>
2006-09-29 18:46:15 +0000

    IE fix: transparency for png images
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6776 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/tiki-js.js
+++ tiki/lib/tiki-js.js
@@ -1,4 +1,4 @@
-// $Header: /cvsroot/tikiwiki/tiki/lib/tiki-js.js,v 1.70 2006-04-12 20:39:30 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/lib/tiki-js.js,v 1.71 2006-09-29 18:46:15 ohertel Exp $
 var feature_no_cookie = 'n';
 
 function browser() {
@@ -1036,6 +1036,37 @@ function addEvent(elm, evType, fn, useCapture)
   }
 } 
 // --- end of sorttable ---
+
+function correctPNG() // correctly handle PNG transparency in Win IE 5.5 & 6.
+{
+   var arVersion = navigator.appVersion.split("MSIE")
+   var version = parseFloat(arVersion[1])
+   if ((version >= 5.5) && (document.body.filters)) 
+   {
+      for(var i=0; i<document.images.length; i++)
+      {
+         var img = document.images[i]
+         var imgName = img.src.toUpperCase()
+         if (imgName.substring(imgName.length-3, imgName.length) == "PNG")
+         {
+            var imgID = (img.id) ? "id='" + img.id + "' " : ""
+            var imgClass = (img.className) ? "class='" + img.className + "' " : ""
+            var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
+            var imgStyle = "display:inline-block;" + img.style.cssText 
+            if (img.align == "left") imgStyle = "float:left;" + imgStyle
+            if (img.align == "right") imgStyle = "float:right;" + imgStyle
+            if (img.parentElement.href) imgStyle = "cursor:hand;" + imgStyle
+            var strNewHTML = "<span " + imgID + imgClass + imgTitle
+            + " style=\"" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";"
+            + "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader"
+            + "(src=\'" + img.src + "\', sizingMethod='scale');\"></span>" 
+            img.outerHTML = strNewHTML
+            i = i-1
+         }
+      }
+   }    
+}
+window.attachEvent("onload", correctPNG);
 
 // This was added to allow wiki3d to change url on tiki's window
 window.name = 'tiki';
