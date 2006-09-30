85b462bbe7be1743f24be24a7387961a07404870
fumphco <fumphco@users.sourceforge.net>
2006-09-30 05:56:04 +0000

    Added param options and help
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6794 b456876b-0849-0410-b77d-98878d47e9d5

--- _mods/wiki-plugins/mindmap/mindmap/wikiplugin-mindmap.mm
+++ _mods/wiki-plugins/mindmap/mindmap/wikiplugin-mindmap.mm
@@ -1,22 +1,30 @@
 <map version="0.8.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1159253738226" ID="Freemind_Link_282881935" MODIFIED="1159254010127" TEXT="MindMap WikiPlugin">
+<node BACKGROUND_COLOR="#ccffcc" CREATED="1159253738226" ID="Freemind_Link_282881935" MODIFIED="1159593259597" TEXT="MindMap WikiPlugin">
 <icon BUILTIN="idea"/>
 <node CREATED="1159253778684" ID="_" MODIFIED="1159253790361" POSITION="right" TEXT="Plugin types">
-<node CREATED="1159253792183" ID="Freemind_Link_1004441638" MODIFIED="1159253795558" TEXT="Java applet"/>
-<node CREATED="1159253799664" ID="Freemind_Link_1209482531" MODIFIED="1159253804101" TEXT="Flash plugin"/>
+<node BACKGROUND_COLOR="#ccccff" CREATED="1159253792183" ID="Freemind_Link_1004441638" LINK="http://java.sun.com/" MODIFIED="1159593793817" STYLE="bubble" TEXT="Java applet"/>
+<node BACKGROUND_COLOR="#ff9966" CREATED="1159253799664" ID="Freemind_Link_1209482531" LINK="http://www.adobe.com/" MODIFIED="1159593856900" STYLE="bubble" TEXT="Flash plugin"/>
 </node>
-<node CREATED="1159253808767" ID="Freemind_Link_1487615996" MODIFIED="1159253812132" POSITION="left" TEXT="params">
+<node CREATED="1159253808767" ID="Freemind_Link_1487615996" MODIFIED="1159593104768" POSITION="left" STYLE="fork" TEXT="params">
+<edge STYLE="bezier"/>
 <node CREATED="1159253814766" ID="Freemind_Link_1252864041" MODIFIED="1159253820674" TEXT="src">
-<node CREATED="1159253865589" ID="Freemind_Link_118945375" MODIFIED="1159253875904" TEXT="URL that finishes with &quot;.mm&quot;"/>
+<node CREATED="1159592097580" ID="Freemind_Link_397409411" MODIFIED="1159594313685" STYLE="bubble" TEXT="absolute url"/>
+<node CREATED="1159592105391" ID="Freemind_Link_55889449" MODIFIED="1159592115687" STYLE="bubble" TEXT="relative url beginning with ./"/>
+<node CREATED="1159592117830" ID="Freemind_Link_726171524" MODIFIED="1159592133613" STYLE="bubble" TEXT="numeric attachment id (flash only)"/>
 </node>
 <node CREATED="1159253824620" ID="Freemind_Link_248479514" MODIFIED="1159253827995" TEXT="plugin">
-<node CREATED="1159253879339" ID="Freemind_Link_1908440290" MODIFIED="1159253885598" TEXT="java"/>
-<node CREATED="1159253888011" ID="Freemind_Link_1158514736" MODIFIED="1159253890565" TEXT="flash"/>
+<node CREATED="1159253879339" ID="Freemind_Link_1908440290" MODIFIED="1159253885598" STYLE="bubble" TEXT="java"/>
+<node CREATED="1159253888011" ID="Freemind_Link_1158514736" MODIFIED="1159592155816" STYLE="bubble" TEXT=" flash (default)"/>
 </node>
-<node CREATED="1159253834374" ID="Freemind_Link_839208998" MODIFIED="1159253836357" TEXT="width"/>
-<node CREATED="1159253838720" ID="Freemind_Link_988270746" MODIFIED="1159253841434" TEXT="height"/>
+<node CREATED="1159253834374" ID="Freemind_Link_839208998" MODIFIED="1159253836357" TEXT="width">
+<node CREATED="1159592168855" ID="Freemind_Link_268341405" MODIFIED="1159592171609" STYLE="bubble" TEXT="number"/>
+<node CREATED="1159592176657" ID="Freemind_Link_1393840820" MODIFIED="1159592179821" STYLE="bubble" TEXT="percentage"/>
 </node>
-<node CREATED="1159253929901" ID="Freemind_Link_1526969228" LINK="http://freemind.sourceforge.net/" MODIFIED="1159254034812" POSITION="right" TEXT="Freemind web site"/>
+<node CREATED="1159253838720" ID="Freemind_Link_988270746" MODIFIED="1159253841434" TEXT="height">
+<node CREATED="1159592181414" ID="Freemind_Link_635545654" MODIFIED="1159592184108" STYLE="bubble" TEXT="number"/>
+</node>
+</node>
+<node BACKGROUND_COLOR="#ffff99" CREATED="1159253929901" ID="Freemind_Link_1526969228" LINK="http://freemind.sourceforge.net/" MODIFIED="1159593222963" POSITION="right" STYLE="bubble" TEXT="Freemind web site"/>
 </node>
 </map>
--- _mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php
+++ _mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php
@@ -1,41 +1,47 @@
 <?php
-/* $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php,v 1.1 2006-09-28 01:06:16 fumphco Exp $
+/* $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php,v 1.2 2006-09-30 05:56:04 fumphco Exp $
  *
  * The freemind plugin source is available at http://freemind.sourceforge.net
  * Can use either the freemind java applet or the much lighter flash plugin
- *
- * Wikiplugin Params:
- *  src    : URL of mindmap (required, must finish with .mm)
- *         : (Absolute or relative urls allowed, relative urls should begin with ./)
- *  plugin : flash|java (optional, default is flash)
- *  width  : width (optional, default is 600)
- *  height : height (optional, default is 400)
  *
  * The function returns some text that will replace the content in the
  * wiki page.
  */
 function wikiplugin_mindmap_help() {
-	return tra("Browse a mindmap using freemind flash plugin or java applet").":<br />~np~{MINDMAP(src=>./lib/mindmap/wikiplugin_mindmap.mm, [plugin=>flash*|java], [height=>600], [width=400])}{MINDMAP}~/np~";
+	$help = tra("Browse a mindmap using freemind flash plugin or java applet");
+	$help.=":<br />~np~{MINDMAP(";
+	$help.="src=>99|./lib/mindmap/wikiplugin_mindmap.mm";
+	$help.="[,plugin=>flash|java]";
+	$help.="[,height=>700]";
+	$help.="[,width=>400]";
+	$help.=")}{MINDMAP}~/np~";
+	$help.= tra("^Parameters: key=>value,...\n");
+	$help.= tra("|| __key__ | __default__ | __comments__\n");
+	$help.= tra("src |  ./lib/mindmap/wikiplugin-mindmap.mm |  The location of the mindmap. This can be either an absolute URL, a relative URL, or the numeric ID of a wiki attachment. Wiki attachments currently work with the flash plugin only. A relative URL must begin with ./\n");
+	$help.="plugin | flash | This selects the type of plugin to display the mindmap. Valid values are 'flash' or 'java'. Your browser must have a compatible flash plugin or java runtime.\n";
+	$help.="width  | 700 | numeric or percentage width\n";
+	$help.="height | 400 | numeric height\n";
+	$help.= "||^";
+	return $help;
 }
 function wikiplugin_mindmap($data, $params) {
 	extract ($params, EXTR_SKIP);
 // required parameters
 	if (!isset($src)) {
-	  return ("<b> MINDMAP src parameter is required</b><br/>");
+		$src = "./lib/mindmap/wikiplugin-mindmap.mm";
 	}
-        if (substr($src,-3) == ".mm") {
-	} else {
-	 return ("<b> MINDMAP src parameter must finish with '.mm'</b><br/>");
+	if (is_numeric($src)) {
+		$src = "./tiki-download_wiki_attachment.php?attId=".$src;
 	}
 // optional parameters
   	if (!isset($width)) {
-    	  $width = 600;
+			$width = 700;
   	}
   	if (!isset($height)) {
-    	  $height = 400;
+			$height = 400;
   	}
 	if ((isset($plugin)) and ($plugin == "java")) {
-          $ret='<APPLET CODE="freemind.main.FreeMindApplet.class" ARCHIVE="lib/mindmap/freemindbrowser.jar"';
+		$ret='<APPLET CODE="freemind.main.FreeMindApplet.class" ARCHIVE="lib/mindmap/freemindbrowser.jar"';
 	  $ret.=' WIDTH="'.$width.'" HEIGHT="'.$height.'">';
 	  $ret.=' <PARAM NAME="type" VALUE="application/x-java-applet;version=1.4">';
 	  $ret.=' <PARAM NAME="scriptable" VALUE="false">';
