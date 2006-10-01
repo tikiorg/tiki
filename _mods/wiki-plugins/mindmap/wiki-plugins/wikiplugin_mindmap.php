e48bf19d56465908ab0a408f5e7c455c9b6461ff
fumphco <fumphco@users.sourceforge.net>
2006-10-01 01:22:03 +0000

    Correct wikiplugin-help
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6818 b456876b-0849-0410-b77d-98878d47e9d5

--- _mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php
+++ _mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php
@@ -1,5 +1,5 @@
 <?php
-/* $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php,v 1.3 2006-10-01 00:47:33 fumphco Exp $
+/* $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php,v 1.4 2006-10-01 01:22:03 fumphco Exp $
  *
  * The freemind plugin source is available at http://freemind.sourceforge.net
  * Both the freemind java applet and the much lighter flash plugin are supported.
@@ -12,14 +12,14 @@ function wikiplugin_mindmap_help() {
 	$help.=":<br />~np~{MINDMAP(";
 	$help.="src=>99|./lib/mindmap/wikiplugin_mindmap.mm";
 	$help.="[,plugin=>flash|java]";
-	$help.="[,height=>700]";
-	$help.="[,width=>400]";
+	$help.="[,width=>700]";
+	$help.="[,height=>400]";
 	$help.=")}{MINDMAP}~/np~";
 	$help.= tra("^Parameters: key=>value,...\n");
 	$help.= tra("|| __key__ | __default__ | __comments__\n");
 	$help.= tra("src |  ./lib/mindmap/wikiplugin-mindmap.mm |  The location of the mindmap. This can be either an absolute URL, a relative URL, or the numeric ID of a wiki attachment. Wiki attachments currently work with the flash plugin only. A relative URL must begin with ./\n");
 	$help.="plugin | flash | This selects the type of plugin to display the mindmap. Valid values are 'flash' or 'java'. Your browser must have a compatible flash plugin or java runtime.\n";
-	$help.="width  | 700 | numeric or percentage width\n";
+	$help.="width  | 700 | numeric width\n";
 	$help.="height | 400 | numeric height\n";
 	$help.= "||^";
 	return $help;
