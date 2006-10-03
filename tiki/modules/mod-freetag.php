ef07c2a8a8e204e5d0c59846f281c7379b470dac
mose <mose@users.sourceforge.net>
2006-10-03 15:22:29 +0000

    fixed call to wrong libname
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6847 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/modules/mod-freetag.php
+++ tiki/modules/mod-freetag.php
@@ -8,17 +8,17 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 
 global $page;
 
-include_once ('lib/freetag/freetag.class.php');
+include_once ('lib/freetag/freetaglib.php');
 
 $userid = $userlib->get_user_id($user);
 $smarty->assign('userid',$userid);
 
 $pageid = $tikilib->get_page_id_from_name($page);
-$smarty->assign('page',$pageid);
+$smarty->assign('pageid',$pageid);
+$smarty->assign('page',$page);
 
-$freetag = new freetag();
 if (isset($_POST['tags']) && trim($_POST['tags']) != "") {
-	$freetag->tag_object($userid, $_POST['page_id'], $_POST['tags']);
+	$freetaglib->tag_object($userid, $_POST['page_id'], $_POST['tags']);
 	}
 
 ?>
