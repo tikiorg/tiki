918ed7e0acb1a87c0ad2f1ebf1fd16e1c62ab75b
hangerman <hangerman@users.sourceforge.net>
2006-10-03 09:42:21 +0000

    Admin can change tracker_creator
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6838 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-edit_blog.php
+++ tiki/tiki-edit_blog.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-edit_blog.php,v 1.32 2006-09-19 16:33:15 ohertel Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-edit_blog.php,v 1.33 2006-10-03 09:41:54 hangerman Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -122,6 +122,22 @@ if (isset($_REQUEST["blogId"]) && $_REQUEST["blogId"] > 0) {
 	$smarty->assign('use_find', $data["use_find"]);
 	$smarty->assign('maxPosts', $data["maxPosts"]);
 	$smarty->assign('heading', $data["heading"]);
+	if ($tiki_p_admin_trackers =='y') {
+              $user_list=$userlib->list_all_users();
+              $creators="<select name='creators'>";
+              foreach ($user_list as $key => $value) {
+                  $creators.="<option value='$value' ";
+                  if ($value==$data["user"])$creators.=" selected "; 
+                  $creators.=">$value</option>";
+              }
+              $creators.="</select>";
+	} else {
+	       $creators=$data["user"];
+	}
+	$smarty->assign('creators',$creators);
+	$smarty->assign('creator',$data["user"]);
+	if (!isset($_REQUEST["creators"])) 
+            $_REQUEST["creators"]=$data["user"];
 }
 
 if (isset($_REQUEST['preview'])) {
@@ -159,7 +175,7 @@ if (isset($_REQUEST["save"]) && $feature_categories == 'y' && $feature_blog_mand
 	$heading = isset($_REQUEST['heading']) ? $_REQUEST['heading'] : '';
 
 	$bid = $bloglib->replace_blog($_REQUEST["title"],
-	    $_REQUEST["description"], $user, $public,
+	    $_REQUEST["description"], $_REQUEST["creators"], $public,
 	    $_REQUEST["maxPosts"], $_REQUEST["blogId"],
 	    $heading, $use_title, $use_find,
 	    $allow_comments, $show_avatar);
