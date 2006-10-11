3d62966f5d9df2d4bd7ad3ef4cb6d10456e553a2
sylvieg <sylvieg@users.sourceforge.net>
2006-10-11 13:03:07 +0000

    the current IP in the smarty var
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6864 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-setup.php
+++ tiki/tiki-setup.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-setup.php,v 1.312 2006-09-23 20:38:23 ohertel Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-setup.php,v 1.313 2006-10-11 13:03:07 sylvieg Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -1693,6 +1693,8 @@ $smarty->assign('user_dbl', $user_dbl);
 
 $smarty->assign('user', $user);
 $smarty->assign('group', $group);
+if (isset($_SERVER['REMOTE_ADDR']))
+	$smarty->assign('IP', $_SERVER['REMOTE_ADDR']);
 $smarty->assign('lock', false);
 $smarty->assign('title', $title);
 $smarty->assign('maxRecords', $maxRecords);
