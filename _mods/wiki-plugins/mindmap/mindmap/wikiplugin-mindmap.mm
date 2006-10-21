6c922caada1c3687c342e685f7137f04b13fa313
fumphco <fumphco@users.sourceforge.net>
2006-10-21 00:36:46 +0000

    Added options for percentage width and height; added options for window and fullscreen modes (java plugin only).
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@6906 b456876b-0849-0410-b77d-98878d47e9d5

--- _mods/wiki-plugins/mindmap/mindmap/wikiplugin-mindmap.mm
+++ _mods/wiki-plugins/mindmap/mindmap/wikiplugin-mindmap.mm
@@ -1,13 +1,13 @@
 <map version="0.8.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node BACKGROUND_COLOR="#ccffcc" COLOR="#00ffff" CREATED="1159253738226" ID="Freemind_Link_282881935" MODIFIED="1159665671947" TEXT="{MINDMAP(  )} {MINDMAP}">
+<node BACKGROUND_COLOR="#ccffcc" COLOR="#ff0000" CREATED="1159253738226" ID="Freemind_Link_282881935" MODIFIED="1161390601078" TEXT="{MINDMAP(  )} {MINDMAP}">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="idea"/>
 <hook NAME="accessories/plugins/BlinkingNodeHook.properties"/>
 <node CREATED="1159253778684" ID="_" MODIFIED="1159665099314" POSITION="right" TEXT="Plugin types">
 <edge STYLE="sharp_linear"/>
 <cloud/>
-<node BACKGROUND_COLOR="#ccccff" CREATED="1159253792183" ID="Freemind_Link_1004441638" LINK="http://java.sun.com/" MODIFIED="1159593793817" STYLE="bubble" TEXT="Java applet"/>
+<node BACKGROUND_COLOR="#00cccc" CREATED="1159253792183" ID="Freemind_Link_1004441638" LINK="http://java.sun.com/" MODIFIED="1161390326864" STYLE="bubble" TEXT="Java applet"/>
 <node BACKGROUND_COLOR="#ff9966" CREATED="1159253799664" ID="Freemind_Link_1209482531" LINK="http://www.adobe.com/" MODIFIED="1159593856900" STYLE="bubble" TEXT="Flash plugin"/>
 </node>
 <node CREATED="1159253808767" ID="Freemind_Link_1487615996" MODIFIED="1159665460233" POSITION="left" STYLE="fork" TEXT="params">
@@ -17,17 +17,22 @@
 <node CREATED="1159253814766" ID="Freemind_Link_1252864041" MODIFIED="1159253820674" TEXT="src">
 <node BACKGROUND_COLOR="#ffffff" CREATED="1159592097580" ID="Freemind_Link_397409411" MODIFIED="1159665149693" STYLE="bubble" TEXT="absolute url"/>
 <node BACKGROUND_COLOR="#ffffff" CREATED="1159592105391" ID="Freemind_Link_55889449" MODIFIED="1159665159839" STYLE="bubble" TEXT="relative url beginning with ./"/>
-<node BACKGROUND_COLOR="#ffffff" CREATED="1159592117830" ID="Freemind_Link_726171524" MODIFIED="1159665172419" STYLE="bubble" TEXT="numeric attachment id (flash only)"/>
+<node BACKGROUND_COLOR="#ff9966" CREATED="1159592117830" ID="Freemind_Link_726171524" MODIFIED="1161390280893" STYLE="bubble" TEXT="numeric attachment id (flash only)"/>
 </node>
 <node CREATED="1159253824620" ID="Freemind_Link_248479514" MODIFIED="1159253827995" TEXT="plugin">
-<node BACKGROUND_COLOR="#ffffff" CREATED="1159253879339" ID="Freemind_Link_1908440290" MODIFIED="1159665220685" STYLE="bubble" TEXT="java"/>
-<node BACKGROUND_COLOR="#ffffff" CREATED="1159253888011" ID="Freemind_Link_1158514736" MODIFIED="1159665210149" STYLE="bubble" TEXT=" flash (default)"/>
+<node BACKGROUND_COLOR="#00cccc" CREATED="1159253879339" ID="Freemind_Link_1908440290" MODIFIED="1161390561177" STYLE="bubble" TEXT="java"/>
+<node BACKGROUND_COLOR="#ff9966" CREATED="1159253888011" ID="Freemind_Link_1158514736" MODIFIED="1161390569159" STYLE="bubble" TEXT=" flash (default)"/>
 </node>
 <node CREATED="1159253834374" ID="Freemind_Link_839208998" MODIFIED="1159253836357" TEXT="width">
-<node BACKGROUND_COLOR="#ffffff" CREATED="1159592168855" ID="Freemind_Link_268341405" MODIFIED="1159665197188" STYLE="bubble" TEXT="number (default=700)"/>
+<node BACKGROUND_COLOR="#ffffff" CREATED="1159592168855" ID="Freemind_Link_268341405" MODIFIED="1161390423984" STYLE="bubble" TEXT="decimal or % (default=100%)"/>
 </node>
 <node CREATED="1159253838720" ID="Freemind_Link_988270746" MODIFIED="1159253841434" TEXT="height">
-<node BACKGROUND_COLOR="#ffffff" CREATED="1159592181414" ID="Freemind_Link_635545654" MODIFIED="1159665187012" STYLE="bubble" TEXT="number (default=400)"/>
+<node BACKGROUND_COLOR="#ffffff" CREATED="1159592181414" ID="Freemind_Link_635545654" MODIFIED="1161390431937" STYLE="bubble" TEXT="decimal or % (default=400)"/>
+</node>
+<node CREATED="1161390072611" ID="Freemind_Link_27516788" MODIFIED="1161390474262" TEXT="mode">
+<node BACKGROUND_COLOR="#ffffff" CREATED="1161390085000" ID="Freemind_Link_113018018" MODIFIED="1161390237876" STYLE="bubble" TEXT=" inline (default)"/>
+<node BACKGROUND_COLOR="#00cccc" CREATED="1161390096227" ID="Freemind_Link_1579763033" MODIFIED="1161390338071" STYLE="bubble" TEXT="window (java only)"/>
+<node BACKGROUND_COLOR="#00cccc" CREATED="1161390102387" ID="Freemind_Link_1810482907" MODIFIED="1161390346935" STYLE="bubble" TEXT="fullscreen (java only)"/>
 </node>
 </node>
 <node BACKGROUND_COLOR="#ffff99" CREATED="1159253929901" ID="Freemind_Link_1526969228" LINK="http://freemind.sourceforge.net/" MODIFIED="1159593222963" POSITION="right" STYLE="bubble" TEXT="Freemind web site"/>
--- _mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php
+++ _mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php
@@ -1,5 +1,5 @@
 <?php
-/* $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php,v 1.4 2006-10-01 01:22:03 fumphco Exp $
+/* $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/mindmap/wiki-plugins/wikiplugin_mindmap.php,v 1.5 2006-10-21 00:36:46 fumphco Exp $
  *
  * The freemind plugin source is available at http://freemind.sourceforge.net
  * Both the freemind java applet and the much lighter flash plugin are supported.
@@ -12,15 +12,17 @@ function wikiplugin_mindmap_help() {
 	$help.=":<br />~np~{MINDMAP(";
 	$help.="src=>99|./lib/mindmap/wikiplugin_mindmap.mm";
 	$help.="[,plugin=>flash|java]";
-	$help.="[,width=>700]";
+	$help.="[,mode=>inline|window|fullscreen]";
+	$help.="[,width=>100%]";
 	$help.="[,height=>400]";
 	$help.=")}{MINDMAP}~/np~";
 	$help.= tra("^Parameters: key=>value,...\n");
 	$help.= tra("|| __key__ | __default__ | __comments__\n");
 	$help.= tra("src |  ./lib/mindmap/wikiplugin-mindmap.mm |  The location of the mindmap. This can be either an absolute URL, a relative URL, or the numeric ID of a wiki attachment. Wiki attachments currently work with the flash plugin only. A relative URL must begin with ./\n");
 	$help.="plugin | flash | This selects the type of plugin to display the mindmap. Valid values are 'flash' or 'java'. Your browser must have a compatible flash plugin or java runtime.\n";
-	$help.="width  | 700 | numeric width\n";
-	$help.="height | 400 | numeric height\n";
+	$help.="mode  | inline | Valid values are 'inline', 'window' or 'fullscreen'. Currently, window and fullscreen modes only work with the java plugin.\n";
+	$help.="width  | 100% | numeric or percentage width\n";
+	$help.="height | 400 | numeric or percentage height\n";
 	$help.= "||^";
 	return $help;
 }
@@ -35,38 +37,67 @@ function wikiplugin_mindmap($data, $params) {
 	}
 // optional parameters
   if (!isset($width)) {
-		$width = 700;
+		$width = "100%";
   }
   if (!isset($height)) {
 		$height = 400;
   }
+	if ($plugin=="java" and isset($mode) and ($mode == "window" or $mode == "fullscreen")) {
+		$width="100%";
+		$height="100%";
+		$script_begin="
+<script type='text/javascript'>
+	fmw = window.open('','fmw'";
+		if ($mode == "fullscreen") {
+			$script_begin.=",'fullscreen=yes')";
+		} else {
+			$script_begin.=")";
+		}
+		$line_begin="
+	fmw.document.write('";
+		$line_end="');";
+		$script_end="
+	fmw.document.close();
+</script>";
+		$script_tag="scr'+'ipt";
+	} else {
+		$script_begin="
+";
+		$line_begin="
+";
+		$line_end="";
+		$script_end="";
+		$script_tag="script";
+	}
 	$id = uniqid("mm_");
 	if ((isset($plugin)) and ($plugin == "java")) {
-		$ret='
-		<APPLET ID="'.$id.'" CODE="freemind.main.FreeMindApplet.class" ARCHIVE="lib/mindmap/freemindbrowser.jar" WIDTH="'.$width.'" HEIGHT="'.$height.'">
-			<PARAM NAME="type" VALUE="application/x-java-applet;version=1.4">
-			<PARAM NAME="scriptable" VALUE="false">
-			<PARAM NAME="modes" VALUE="freemind.modes.browsemode.BrowseMode">
-			<PARAM NAME="browsemode_initial_map" VALUE="'.$src.'">
-			<PARAM NAME="initial_mode" VALUE="Browse">
-			<PARAM NAME="selection_method" VALUE="selection_method_direct">
-	  </APPLET>';
+		$ret=$script_begin;
+		$ret.=$line_begin.'<APPLET ID="'.$id.'" CODE="freemind.main.FreeMindApplet.class" ARCHIVE="lib/mindmap/freemindbrowser.jar" WIDTH="'.$width.'" HEIGHT="'.$height.'">'.$line_end;
+		$ret.=$line_begin.'	<PARAM NAME="type" VALUE="application/x-java-applet;version=1.4">'.$line_end;
+		$ret.=$line_begin.'	<PARAM NAME="scriptable" VALUE="false">'.$line_end;
+		$ret.=$line_begin.'	<PARAM NAME="modes" VALUE="freemind.modes.browsemode.BrowseMode">'.$line_end;
+		$ret.=$line_begin.'	<PARAM NAME="browsemode_initial_map" VALUE="'.$src.'">'.$line_end;
+		$ret.=$line_begin.'	<PARAM NAME="initial_mode" VALUE="Browse">'.$line_end;
+		$ret.=$line_begin.'	<PARAM NAME="selection_method" VALUE="selection_method_direct">'.$line_end;
+	  $ret.=$line_begin.'</APPLET>'.$line_end;
+		$ret.=$script_end;
 	} else {
-		$ret='
-		<script type="text/javascript" src="lib/mindmap/flashobject.js"></script>
-		<div id="flashcontent_'.$id.'">
-		  A problem occurred while trying to display the mindmap.
-		  Check if both Flash plugin and Javascript are activated.
-		</div>
-		<script type="text/javascript">
-			var '.$id.' = new FlashObject("lib/mindmap/visorFreemind.swf", "visorFreeMind", '.$width.', '.$height.', 6, "#9999ff");
-			'.$id.'.addParam("quality", "high");
-			'.$id.'.addParam("bgcolor", "#ffffff");
-			'.$id.'.addVariable("openUrl", "_blank");
-			'.$id.'.addVariable("initLoadFile", "'.$src.'");
-			'.$id.'.addVariable("startCollapsedToLevel","5");
-			'.$id.'.write("flashcontent_'.$id.'");
-		</script>';
+		$ret=$script_begin;
+		$ret.=$line_begin.'<'.$script_tag.' type="text/javascript" src="lib/mindmap/flashobject.js"></'.$script_tag.'>'.$line_end;
+		$ret.=$line_begin.'<div id="flashcontent_'.$id.'">'.$line_end;
+		$ret.=$line_begin.'		  A problem occurred while trying to display the mindmap. '.$line_end;
+		$ret.=$line_begin.'		  Check if both Flash plugin and Javascript are activated.'.$line_end;
+		$ret.=$line_begin.'</div>'.$line_end;
+		$ret.=$line_begin.'<'.$script_tag.' type="text/javascript">'.$line_end;
+		$ret.=$line_begin.'		var '.$id.' = new FlashObject("lib/mindmap/visorFreemind.swf", "visorFreeMind", "'.$width.'", "'.$height.'", 6, "#9999ff");'.$line_end;
+		$ret.=$line_begin.'		'.$id.'.addParam("quality", "high");'.$line_end;
+		$ret.=$line_begin.'		'.$id.'.addParam("bgcolor", "#ffffff");'.$line_end;
+		$ret.=$line_begin.'		'.$id.'.addVariable("openUrl", "_blank");'.$line_end;
+		$ret.=$line_begin.'		'.$id.'.addVariable("initLoadFile", "'.$src.'");'.$line_end;
+		$ret.=$line_begin.'		'.$id.'.addVariable("startCollapsedToLevel","5");'.$line_end;
+		$ret.=$line_begin.'		'.$id.'.write("flashcontent_'.$id.'");'.$line_end;
+		$ret.=$line_begin.'</'.$script_tag.'>'.$line_end;
+	$ret.=$script_end;
 	}
 	return $ret;
 }
