f3f33d999d4803f252442e730f8703487cd48135
bluestrain <bluestrain@users.sourceforge.net>
2006-11-20 02:32:26 +0000

    Changed help URL to Admin+Home page on d.tw.o
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@7057 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin.php
+++ tiki-admin.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin.php,v 1.113 2006-10-04 02:12:53 mose Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin.php,v 1.114 2006-11-20 02:32:26 bluestrain Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -87,7 +87,7 @@ $smarty->assign('home_file_gallery', $home_file_gallery);
 $crumbs[] = new Breadcrumb('Administration',
                               'Sections',
                               'tiki-admin.php',
-                              'Tiki+Admin',
+                              'Admin+Home',
                               'Help on Configuration Sections');
 
 if (isset($_REQUEST["page"])) {
