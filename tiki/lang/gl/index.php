92cece2fcf1f0dd708e5091ec4e395d6e8323bab
josefgc <josefgc@users.sourceforge.net>
2006-11-20 08:49:06 +0000

    Galician language files
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@7062 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/lang/gl/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// $Header: /cvsroot/tikiwiki/tiki/lang/gl/index.php,v 1.2 2006-11-20 08:49:06 josefgc Exp $
+
+// Copyright (c) 2002-2006, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+// This redirects to the sites root to prevent directory browsing
+
+header ("location: ../../tiki-index.php");
+die;
+
+?>
