ccd8f03a0eec72b8e7cb2e6c544229c832c6c048
mose <mose@users.sourceforge.net>
2006-11-20 04:12:30 +0000

    [FIX] freetags: made modules work
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@7058 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-freetags_most_popular.php
+++ modules/mod-freetags_most_popular.php
@@ -6,16 +6,9 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   exit;
 }
 
-require_once("lib/freetag/freetaglib.php");
-//global $freetaglib; This one helped for tiki-index.php, but not for e.g. tiki-admin_system.php 
-//instantiating the object(like on the next line) helped. I'm unsure about this one - please check it!
-//Initial problem was, "Call to a member-function on a non-object"
-//Probably related to evaluating modules first - dunno.
 global $dbTiki;
-$freetaglib = new FreetagLib($dbTiki);
-
+global $freetaglib;
+if (!isset($freetaglib) or !is_object($freetaglib)) { include_once 'lib/freetag/freetaglib.php'; }
 $most_popular_tags = $freetaglib->get_most_popular_tags('', 0, $module_rows);
-
 $smarty->assign('most_popular_tags', $most_popular_tags);
-
 ?>
