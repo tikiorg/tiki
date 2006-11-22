aac88b0933a832ab7b63fed865f0bbf45be35024
mose <mose@users.sourceforge.net>
2006-11-22 00:26:49 +0000

    [MOD] translation: made {tr} use tra() rather than duplicating code, and added a possibility to have a lang/xx/custom.php for extra language declaration
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@7094 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/smarty_tiki/block.tr.php
+++ tiki/lib/smarty_tiki/block.tr.php
@@ -18,41 +18,7 @@ if (strpos($_SERVER['SCRIPT_NAME'],basename(__FILE__)) !== false) {
 //global $lang;
 //include_once('lang/language.php');
 
-function smarty_block_tr($params, $content, &$smarty)
-{
-  global $lang_use_db;
-  if ($lang_use_db!='y') {
-    if ($content) {
-    global $language;
-    global $lang;
-    @include_once("lang/$language/language.php");
-      if(isset($lang[$content])) {
-        echo $lang[$content];  
-      } else {
-        echo $content;
-      }
-    }
-  } else {
-    global $tikilib;
-    global $language,$multilinguallib;
-    $tag=isset($multilinguallib)?$multilinguallib->getInteractiveTag($content):"";
-    $query="select `tran` from `tiki_language` where `source`=? and `lang`=?";
-    $result=$tikilib->query($query,array($content,$language));
-    $res=$result->fetchRow();
-    if(!$res) { echo $content.$tag ; return; }
-    if(!isset($res["tran"])) {
-      global $record_untranslated;
-      if ($record_untranslated=='y') {
-        $query="insert into `tiki_untranslated` (`source`,`lang`) values(?,?)";
-        //No eror checking here
-        $tikilib->query($query,array($content,$language),-1,-1,false);
-        }
-      echo $content.$tag;
-    }else{ 
-	//To allow multiline translation
-	$res["tran"]=ereg_replace("&lt;br&gt;","<br>",$res["tran"]);
-    	echo $res["tran"].$tag;
-	}
-  }
+function smarty_block_tr($params, $content, &$smarty) {
+	echo tra($content);
 }
 ?>
