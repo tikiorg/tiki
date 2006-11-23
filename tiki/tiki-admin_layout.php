9f04051e07c562bfaa25aeb4c55bac1c6a1d9c88
mose <mose@users.sourceforge.net>
2006-11-23 18:33:25 +0000

    [MOD] layout control: made list of section extracted from setup rather than arbitrary list_sections
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@7105 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-admin_layout.php
+++ tiki/tiki-admin_layout.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_layout.php,v 1.16 2006-09-19 16:33:12 ohertel Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_layout.php,v 1.17 2006-11-23 18:33:25 mose Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -23,84 +23,56 @@ if ($tiki_p_admin != 'y') {
 	die;
 }
 
-$sections = $tikilib->list_sections();
 
-$sections_smt = array();
-
-$temp_max = count($sections);
-for ($i = 0; $i < $temp_max; $i++) {
-	$aux["name"] = $sections[$i];
-
-	$name = $sections[$i];
-	$aux["left_column"] = $tikilib->get_preference("${name}_left_column", 'y');
-	$aux["right_column"] = $tikilib->get_preference("${name}_right_column", 'y');
-	$aux["top_bar"] = $tikilib->get_preference("${name}_top_bar", 'y');
-	$aux["bot_bar"] = $tikilib->get_preference("${name}_bot_bar", 'y');
-	$sections_smt[] = $aux;
-}
-
-$smarty->assign('sections', $sections_smt);
-
-foreach ($sections as $section) {
+foreach ($sections_enabled as $section=>$data) {
 	if (isset($_REQUEST["${section}_layout"])) {
 		check_ticket('admin-layout');
 		if (isset($_REQUEST["${section}_left_column"]) && $_REQUEST["${section}_left_column"] == "on") {
 			$tikilib->set_preference("${section}_left_column", 'y');
-
 			$smarty->assign("${section}_left_column", 'y');
 		} else {
 			$tikilib->set_preference("${section}_left_column", 'n');
-
 			$smarty->assign("${section}_left_column", 'n');
 		}
 
 		if (isset($_REQUEST["${section}_right_column"]) && $_REQUEST["${section}_right_column"] == "on") {
 			$tikilib->set_preference("${section}_right_column", 'y');
-
 			$smarty->assign("${section}_right_column", 'y');
 		} else {
 			$tikilib->set_preference("${section}_right_column", 'n');
-
 			$smarty->assign("${section}_right_column", 'n');
 		}
 
 		if (isset($_REQUEST["${section}_top_bar"]) && $_REQUEST["${section}_top_bar"] == "on") {
 			$tikilib->set_preference("${section}_top_bar", 'y');
-
 			$smarty->assign("${section}_top_bar", 'y');
 		} else {
 			$tikilib->set_preference("${section}_top_bar", 'n');
-
 			$smarty->assign("${section}_top_bar", 'n');
 		}
 
 		if (isset($_REQUEST["${section}_bot_bar"]) && $_REQUEST["${section}_bot_bar"] == "on") {
 			$tikilib->set_preference("${section}_bot_bar", 'y');
-
 			$smarty->assign("${section}_bot_bar", 'y');
 		} else {
 			$tikilib->set_preference("${section}_bot_bar", 'n');
-
 			$smarty->assign("${section}_bot_bar", 'n');
 		}
 	}
 }
 
 $sections_smt = array();
-
-$temp_max = count($sections);
-for ($i = 0; $i < $temp_max; $i++) {
-	$aux["name"] = $sections[$i];
-
-	$name = $sections[$i];
-	$aux["left_column"] = $tikilib->get_preference("${name}_left_column", 'y');
-	$aux["right_column"] = $tikilib->get_preference("${name}_right_column", 'y');
-	$aux["top_bar"] = $tikilib->get_preference("${name}_top_bar", 'y');
-	$aux["bot_bar"] = $tikilib->get_preference("${name}_bot_bar", 'y');
+$temp_max = count($sections_enabled);
+foreach ($sections_enabled as $sec=>$dat) {
+	$aux["name"] = $sec;
+	$aux["left_column"] = $tikilib->get_preference("${sec}_left_column", 'y');
+	$aux["right_column"] = $tikilib->get_preference("${sec}_right_column", 'y');
+	$aux["top_bar"] = $tikilib->get_preference("${sec}_top_bar", 'y');
+	$aux["bot_bar"] = $tikilib->get_preference("${sec}_bot_bar", 'y');
 	$sections_smt[] = $aux;
 }
-
 $smarty->assign('sections', $sections_smt);
+
 ask_ticket('admin-layout');
 
 // disallow robots to index page:
