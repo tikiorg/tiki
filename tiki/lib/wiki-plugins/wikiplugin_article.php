2242a063ab2a7e4c3e94eef8b9deae64f3b2630d
hangerman <hangerman@users.sourceforge.net>
2006-11-24 17:30:52 +0000

    Add shortcut for direct editing
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@7118 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/wiki-plugins/wikiplugin_article.php
+++ tiki/lib/wiki-plugins/wikiplugin_article.php
@@ -9,7 +9,7 @@ function wikiplugin_article_help() {
 	heading.~/np~";
 }
 function wikiplugin_article($data, $params) {
-	global $tikilib;
+	global $tikilib,$user,$userlib;
 	include_once('lib/stats/statslib.php');
 
 	extract ($params,EXTR_SKIP);
@@ -26,8 +26,14 @@ function wikiplugin_article($data, $params) {
 		$Field = 'heading';
 	} 
 
+	if ($tiki_p_admin_cms == 'y' || $tikilib->user_has_perm_on_object($user, $articleId, 'article', 'tiki_p_edit_article') || ($article_data["author"] != $user && $article_data["creator_edit"] != 'y')) {
+	      $add="&nbsp;<a href='tiki-edit_article.php?articleId=$Id'><img src='/pics/icons/page_edit.png' style='border:0px;'></a>";
+	} else {
+	      $add="";
+	}
+
 	$article_data = $tikilib->get_article($Id);
-	return $article_data[$Field];
+	return $article_data[$Field].$add;
 }
 
 ?>
