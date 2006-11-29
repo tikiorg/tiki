f4573860a2f413235a138676d3c17e93dd03699f
mose <mose@users.sourceforge.net>
2006-11-29 10:23:41 +0000

    [NEW] fckeditor: new files for fckeditor wysiwyg feature
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@7159 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/lib/smarty_tiki/function.editform.php
@@ -0,0 +1,28 @@
+<?php
+
+//this script may only be included - so its better to die if called directly.
+if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
+  header("location: index.php");
+  exit;
+}
+
+function smarty_function_editform($params, &$smarty) {
+	global $feature_detect_language;
+	global $language;
+	include_once 'lib/tikifck.php';
+	if (!isset($params['InstanceName']))       $params['InstanceName'] = 'fckedit';
+	$fcked = new TikiFCK($params['InstanceName']);
+	if (isset($params['Meat']))       $fcked->Meat = $params['Meat'];
+	if (isset($params['Width']))      $fcked->Width = $params['Width'];
+	if (isset($params['Height']))     $fcked->Height = $params['Height'];
+	if (isset($params['ToolbarSet'])) $fcked->ToolbarSet = $params['ToolbarSet'];
+	if ($feature_detect_language == 'y') {
+		$fcked->Config['AutoDetectLanguage'] = true;
+	} else {
+		$fcked->Config['AutoDetectLanguage'] = false;
+	}
+	$fcked->Config['DefaultLanguage'] = $language;
+	echo $fcked->CreateHtml();
+} 
+
+?>
