c9e11c6c113081feeaed09a628dd4490630ba2f6
sylvieg <sylvieg@users.sourceforge.net>
2006-12-01 18:15:47 +0000

    [MOD]perms: a select all button/checkbox to delete a set of perms
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@7198 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-objectpermissions.php
+++ tiki-objectpermissions.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-objectpermissions.php,v 1.17 2006-11-30 15:45:41 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-objectpermissions.php,v 1.18 2006-12-01 18:15:46 sylvieg Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -66,6 +66,14 @@ if (isset($_REQUEST["action"])) {
 	check_ticket('object-perms');
 	if ($_REQUEST["action"] == 'remove') {
 		$userlib->remove_object_permission($_REQUEST["group"], $_REQUEST["objectId"], $_REQUEST["objectType"], $_REQUEST["perm"]);
+	}
+}
+if (isset($_REQUEST['delsel_x']) && isset($_REQUEST['checked'])) {
+	check_ticket('object-perms');
+	foreach ($_REQUEST['checked'] as $perm) {
+		if (preg_match('/([^ ]*) (.*)/', $perm, $matches)) {
+			$userlib->remove_object_permission($matches[2], $_REQUEST["objectId"], $_REQUEST["objectType"], $matches[1]);
+		}
 	}
 }
 
