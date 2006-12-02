de9d7af82468b5a1fc44cb82ecf6924517af122a
sylvieg <sylvieg@users.sourceforge.net>
2006-12-02 12:40:28 +0000

    [mod]fgal icon: if not icon the alt img is the type not default + {tr} ne more actif there
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@7206 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/smarty_tiki/modifier.iconify.php
+++ tiki/lib/smarty_tiki/modifier.iconify.php
@@ -19,9 +19,9 @@ function smarty_modifier_iconify($string)
   
   $string=strtolower(substr($string,strlen($string)-3));
   if(file_exists("pics/icons/mime/$string".".png")) {
-    return "<img border='0' src='pics/icons/mime/{$string}.png' alt='{tr}{$string}{/tr}' width='16' height='16' />";
+    return "<img border='0' src='pics/icons/mime/{$string}.png' alt='{$string}' width='16' height='16' />";
   } else {
-    return "<img border='0' src='pics/icons/mime/default.png' alt='{tr}default{/tr}' width='16' height='16' />";
+    return "<img border='0' src='pics/icons/mime/default.png' alt='{$string}' width='16' height='16' />";
   }     
 	
 }
