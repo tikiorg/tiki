64c7cdc9e9567cf03f0cfe53d416702725cd30b0
mose <mose@users.sourceforge.net>
2006-12-04 14:23:20 +0000

    [FIX] fckeditor: made fck accept an external configuration file specifically used by tiki
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@7254 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ setup_fckeditor.php
@@ -0,0 +1,7 @@
+<?php
+header('Content-type: application/javascript');
+?>
+FCKConfig.FontNames = 'sans serif;serif;monospace;Arial;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana' ;
+var _FileBrowserLanguage      = 'php' ;
+var _QuickUploadLanguage      = 'php' ;
+FCKConfig.ToolbarSets["Tiki"] = FCKConfig.ToolbarSets["Default"] ;
