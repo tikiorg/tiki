333bb8ccdbc0844df551948cb62fcfa24571f755
mose <mose@users.sourceforge.net>
2006-12-15 06:22:40 +0000

    [MOD] wysiwyg: made tikilink plugin use ((wiki)) syntax for storage and html for display
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@7408 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/fckeditor_tiki/plugins/tikilink/fckplugin.js
+++ tiki/lib/fckeditor_tiki/plugins/tikilink/fckplugin.js
@@ -17,7 +17,7 @@ FCKCommands.RegisterCommand(
 		FCKConfig.tikilinkDlgTitle,
 		FCKPlugins.Items['tikilink'].Path + 'fck_tikilink.php',
 		400,
-		600
+		400
 	)
 );
  
@@ -25,3 +25,107 @@ var otikilinkItem = new FCKToolbarButton( 'tikilink', FCKConfig.tikilinkBtn, nul
 otikilinkItem.IconPath = _TikiRoot + 'pics/icons/page_white_link.png'; 
 
 FCKToolbarItems.RegisterItem( 'tikilink', otikilinkItem );
+
+var FCKTikiLinks = new Object() ;
+
+FCKTikiLinks.add = function( page, name ) {
+	var oA = FCK.CreateElement( 'A' ) ;
+	this.SetupLink( oA, page, name ) ;
+}
+
+FCKTikiLinks.SetupLink  = function( a, page, name ) {
+	a.innerHTML = name ;
+	a.contentEditable = 'false' ;
+	a.class = 'wiki' ;
+	a.href = page ;
+	a._wikilink = name ;
+	a.onresizestart = function() {
+		FCK.EditorWindow.event.returnValue = false ;
+		return false ;
+	}
+}
+
+FCKTikiLinks._SetupClickListener = function() {
+	FCKTikiLinks._ClickListener = function( e ) {
+		if ( e.target.tagName == 'A' && e.target._wikilink ) {
+			FCKSelection.SelectNode( e.target ) ;
+		}
+	}
+	FCK.EditorDocument.addEventListener( 'click', FCKTikiLinks._ClickListener, true ) ;
+}
+
+FCKTikiLinks.OnDoubleClick = function( a ) {
+	if ( a.tagName == 'A' && a._wikilink ) {
+		FCKCommands.GetCommand( 'tikilink' ).Execute() ;
+	}
+}
+
+FCK.RegisterDoubleClickHandler( FCKTikiLinks.OnDoubleClick, 'A' ) ;
+
+FCKTikiLinks.Exist = function( name ) {
+	var aA = FCK.EditorDocument.getElementsByTagName( 'A' ) ;
+	for ( var i = 0 ; i < aA.length ; i++ ) {
+		if ( aA[i]._wikilink == name ) {
+			return true ;
+		}
+	}
+}
+
+if ( FCKBrowserInfo.IsIE ) {
+	FCKTikiLinks.Redraw = function() {
+		var aWikiLinks = FCK.EditorDocument.body.innerText.match( /\(\([^\)]+\)\)/g ) ;
+		if ( !aWikiLinks )
+			return ;
+		var oRange = FCK.EditorDocument.body.createTextRange() ;
+		for ( var i = 0 ; i < aWikiLinks.length ; i++ ) {
+			if ( oRange.findText( aWikiLinks[i] ) ) {
+				var sName = aWikiLinks[i].match( /\(\(([^\)]*)\)\)/ )[1] ;
+				oRange.pasteHTML( '<a href="' + sName + '" class="wiki" contenteditable="false" _wikilink="' + sName + '">' + sName + '</a>' );
+			}
+		}
+	}
+} else {
+	FCKTikiLinks.Redraw = function() {
+		var oInteractor = FCK.EditorDocument.createTreeWalker( FCK.EditorDocument.body, NodeFilter.SHOW_TEXT, FCKTikiLinks._AcceptNode, true ) ;
+		var aNodes = new Array() ;
+		while ( oNode = oInteractor.nextNode() ) {
+			aNodes[ aNodes.length ] = oNode ;
+		}
+		for ( var n = 0 ; n < aNodes.length ; n++ ) {
+			var aPieces = aNodes[n].nodeValue.split( /(\(\([^\)]*\)\))/ );
+			for ( var i = 0 ; i < aPieces.length ; i++ ) {
+				if ( aPieces[i].length > 0 ) {
+					if ( aPieces[i].indexOf( '((' ) == 0 ) {
+						var sName = aPieces[i].match(  /\(\(([^\)]*)\)\)/ )[1] ;
+						var oA = FCK.EditorDocument.createElement( 'a' ) ;
+						FCKTikiLinks.SetupLink( oA, sName, sName ) ;
+						aNodes[n].parentNode.insertBefore( oA, aNodes[n] ) ;
+					} else {
+						aNodes[n].parentNode.insertBefore( FCK.EditorDocument.createTextNode( aPieces[i] ) , aNodes[n] ) ;
+					}
+				}
+			}
+			aNodes[n].parentNode.removeChild( aNodes[n] ) ;
+		}
+		FCKTikiLinks._SetupClickListener() ;
+	}
+	FCKTikiLinks._AcceptNode = function( node ) {
+		if ( /\(\(([^\)]+)\)\)/.test( node.nodeValue ) ) {
+			return NodeFilter.FILTER_ACCEPT ;
+		} else {
+			return NodeFilter.FILTER_SKIP ;
+		}
+	}
+}
+
+FCK.Events.AttachEvent( 'OnAfterSetHTML', FCKTikiLinks.Redraw ) ;
+
+FCKXHtml.TagProcessors['a'] = function( node, htmlNode ) {
+	if ( htmlNode._wikilink ) {
+		node = FCKXHtml.XML.createTextNode( '((' + htmlNode._wikilink + '))' ) ;
+	} else {
+		FCKXHtml._AppendChildNodes( node, htmlNode, false ) ;
+	}
+	return node ;
+}
+
