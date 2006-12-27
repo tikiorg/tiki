b18e9a9dfe5dca130bdc76540a73a1f95558c42e
mose <mose@users.sourceforge.net>
2006-12-27 05:05:43 +0000

    [FIX] admin modules: avoid notices
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@7530 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-admin_modules.php
+++ tiki/tiki-admin_modules.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_modules.php,v 1.44 2006-09-19 16:33:13 ohertel Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_modules.php,v 1.45 2006-12-27 05:05:43 mose Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -65,6 +65,9 @@ if (isset($_REQUEST["clear_cache"])) {
 }
 
 $module_groups = array();
+$smarty->assign('assign_selected','');
+$smarty->assign('assign_type','');
+$smarty->assign('assign_title','');
 
 if (isset($_REQUEST["edit_assign"])) {
 	check_ticket('admin-modules');
