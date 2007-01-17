51e0761fb01d3d10fd72e534ccdc400cb0a89080
sylvieg <sylvieg@users.sourceforge.net>
2007-01-17 15:15:50 +0000

    Instant-Auto-Merge from BRANCH-1-9 to HEAD
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@7656 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-edit_translation.php
+++ tiki-edit_translation.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-edit_translation.php,v 1.12 2007-01-17 14:55:53 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-edit_translation.php,v 1.13 2007-01-17 15:15:50 sylvieg Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -75,7 +75,7 @@ else if ($_REQUEST['id']) {
 }
 
 if ($type == "wiki page") {
-	if ($tiki_p_admin_wiki== 'y' || $tikilib->user_has_perm_on_object($user, $page, 'wiki page', 'tiki_p_edit') || ($wiki_creator_admin == 'y' && $user && $info['creator'] == $user)) {
+  if (!($tiki_p_admin_wiki== 'y' || $tikilib->user_has_perm_on_object($user, $page, 'wiki page', 'tiki_p_edit') || ($wiki_creator_admin == 'y' && $user && $info['creator'] == $user))) {
 		$smarty->assign('msg', tra("Permission denied you cannot edit this page"));
 		$smarty->display("error.tpl");
 		die;
