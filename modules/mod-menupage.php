ea1783e0cc97c91d2d7824c3ae18c652b0321d36
nyloth <nyloth@users.sourceforge.net>
2007-01-23 15:15:01 +0000

    [FIX] mod menu : correctly parse the wiki page used for the menu when the page is in HTML
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@7696 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-menupage.php
+++ modules/mod-menupage.php
@@ -7,7 +7,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 if (isset($module_params['page'])) {
 	if ($tikilib->page_exists($module_params['page'])) {
 		$info = $tikilib->get_page_info($module_params['page']);
-		$content = $tikilib->parse_data($info['data']);
+		$content = $tikilib->parse_data($info['data'], $info['is_html']);
 		$smarty->assign('tpl_module_title',$module_params['page']);
 		$smarty->assign('contentmenu',$content);
 	}
