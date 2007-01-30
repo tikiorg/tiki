a3e7ae2d6ac9d9db577994f0eb9ed37d384769fa
sylvieg <sylvieg@users.sourceforge.net>
2007-01-30 14:25:43 +0000

    some fixes + a smarty version
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@7717 b456876b-0849-0410-b77d-98878d47e9d5

--- _mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php
+++ _mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php,v 1.1 2007-01-23 21:10:37 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php,v 1.2 2007-01-30 14:25:43 sylvieg Exp $
 function wikiplugin_trackeritemfield_help() {
 	$help = tra("Displays the value of an tracker item field or the wiki text id test is true (if itemID not specified, the user tracker).").":\n";
 	$help .= "~np~{TRACKERITEMFIELD(itemId=>1, fieldId=>1, test=>1|0)}".tra('Wiki text')."{TRACKERITEMFIELD}~/np~";
@@ -13,31 +13,38 @@ function wikiplugin_trackeritemfield($data, $params) {
 	if (empty($itemId) && $userTracker == 'y' && !empty($group) && ($utid = $userlib->get_usertrackerid($group)) && $utid['usersTrackerId']) {
 		$trackerId = $utid['usersTrackerId'];
 		$itemId = $trklib->get_item_id($trackerId, $utid['usersFieldId'], $user);
+		$is_user_tracker = true;
 	} else if (empty($trackerId) && !empty($itemId)) {
 		$item = $trklib->get_tracker_item($itemId);
 		$trackerId = $item['trackerId'];
 	}
 
 	if (empty($itemId) || empty($trackerId) || empty($fieldId)) {
-		return '';
+		return false;
 	}
-	if ($tiki_p_admin_trackers != 'y' && !$userlib->user_has_perm_on_object($user, $trackerId, 'tracker','tiki_p_view_trackers')) {
-		return '';
+	if ($tiki_p_admin_trackers != 'y' && !$userlib->user_has_perm_on_object($user, $trackerId, 'tracker','tiki_p_view_trackers') && empty($is_user_tracker)) {
+		return false;
 	}
 
 	if (!($field = $trklib->get_tracker_field($fieldId))) {
-		return '';
+		return false;
 	}
-	if ($tiki_p_admin_trackers != 'y' && $field['options']['isHidden'] != 'n') {
-		return '';
+	if ($tiki_p_admin_trackers != 'y' && $field['isHidden'] != 'n') {
+		return false;
 	}
 	if (empty($test))
 		$test = false;
 	
-	if (($val = $trklib->get_item_value($trackerId, $itemId, $fieldId)) !== false && !empty($val)) {
-		return ($test?$data: $val);
+	if (($val = $trklib->get_item_value($trackerId, $itemId, $fieldId)) !== false) {
+		if ($test && empty($val)) {
+			return false;
+		} elseif ($test) {
+			return $data;
+		} else {
+			return $val;
+		}
 	} else {
-		return '';
+		return false;
 	}
 }
 ?>
