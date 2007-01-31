b8d09b4ad1dbaae94858ef077c2d9f82653bb833
franck <franck@users.sourceforge.net>
2007-01-31 02:20:43 +0000

    [FIX] TikiMaps can load mapfile >8kB
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@7720 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-map_edit.php
+++ tiki/tiki-map_edit.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-map_edit.php,v 1.24 2007-01-22 03:03:06 franck Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-map_edit.php,v 1.25 2007-01-31 02:20:43 franck Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -220,7 +220,7 @@ if ($tiki_p_map_edit != 'y') {
 		die;
 	}
 	if (filesize($mapfile)>0) {
-		$pagedata = fread($fp, filesize($mapfile));
+		$pagedata = file_get_contents($mapfile,FALSE);
 		fclose ($fp);
 	} else {
 		$pagedata="";
