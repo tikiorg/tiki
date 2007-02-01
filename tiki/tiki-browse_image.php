5368d225acce3d0b1cd7e8fa75049e78105613d6
luciash <luciash@users.sourceforge.net>
2007-02-01 12:17:40 +0000

    [FIX] next image generate correct id (nextimg offset)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@7728 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-browse_image.php
+++ tiki/tiki-browse_image.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-browse_image.php,v 1.42 2007-01-02 16:18:00 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-browse_image.php,v 1.43 2007-02-01 12:17:40 luciash Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -144,7 +144,7 @@ if ($offset) {
 }
 
 if (count($listImgId) > $offset + 1) {
-	$smarty->assign('nextimg', $listImgId[$offset+1]);
+	$smarty->assign('nextimg', $listImgId[$offset+2]);
 } else {
 	$smarty->assign('nextimg', '');
 }
