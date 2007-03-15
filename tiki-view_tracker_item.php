0244656d52c86e39ad1015f9643995e00d969645
gillesm <gillesm@users.sourceforge.net>
2007-03-15 09:05:09 +0000

    Correction of mktime to understand winter time. Now hour seems correct in tracker. Before there was a 2 hours shift.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@8014 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-view_tracker_item.php
+++ tiki-view_tracker_item.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-view_tracker_item.php,v 1.118 2007-03-06 19:29:52 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-view_tracker_item.php,v 1.119 2007-03-15 09:05:09 gillesm Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -303,7 +303,7 @@ foreach($xfields["data"] as $i=>$array) {
 			$ins_fields["data"][$i]["value"] = '';
 			if (isset($_REQUEST["$ins_id" . "Day"])) {
 				$ins_fields["data"][$i]["value"] = mktime($_REQUEST["$ins_id" . "Hour"], $_REQUEST["$ins_id" . "Minute"],
-				0, $_REQUEST["$ins_id" . "Month"], $_REQUEST["$ins_id" . "Day"], $_REQUEST["$ins_id" . "Year"]);
+				0, $_REQUEST["$ins_id" . "Month"], $_REQUEST["$ins_id" . "Day"], $_REQUEST["$ins_id" . "Year"],1);
 			} else {
 				$ins_fields["data"][$i]["value"] = $tikilib->now;
 			}
