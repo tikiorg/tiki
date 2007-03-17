297a42cdd4b768e667f03a5f6a9e8f6ebedf16fc
niclone <niclone@users.sourceforge.net>
2007-03-17 10:24:54 +0000

    [MOD]mods: added "show" command
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@8043 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/modsadm.php
+++ tiki/modsadm.php
@@ -29,6 +29,7 @@ $commands=array('help' => array(),
 		'list' => array('usage' => '[options] [regex]',
 				'options' => array('-l' => array('help' => 'Do not query for remote mods'),
 						   '-i' => array('help' => 'Show only installed mods'))),
+		'show' => array('usage' => 'mod1 [mod2...]'),
 		'install' => array('usage' => '[options] mod1 [mod2...]',
 				   'options' => array('-d' => array('help' => "Download only mods, don't install them"))),
 		'remove' => array('usage' => 'mod1 [mod2...]'),
@@ -203,7 +204,6 @@ function command_list($goption, $coption, $cparams) {
 		ksort(&$merged[$k]);
 	}
 	ksort(&$merged);
-
 	foreach($merged as $type => $meat) {
 		echo $type.":\n";
 		foreach($meat as $name => $submeat) {
@@ -212,6 +212,156 @@ function command_list($goption, $coption, $cparams) {
 			printf("  %-24.24s | %7.7s | %7.7s\n", $name, $rev_installed, $rev_remote);
 		}
 	}
+}
+
+function command_show($goption, $coption, $cparams) {
+	global $repos;
+	global $modslib;
+
+	foreach($cparams as $cparam) {
+		$found=NULL;
+		$mod=new TikiMod($cparam);
+		foreach($repos as $reponame => $repo) {
+			$content=$modslib->read_list($repo['url'], $reponame);
+			if (isset($content[$mod->type][$mod->name])) {
+				$found=$content[$mod->type][$mod->name];
+				break;
+			}
+		}
+		
+		if ($found === NULL) {
+			echo "mod '".$mod->modname."' not found\n";
+			continue;
+		} else $mod=$found;
+
+		if ($mod->repository !== NULL) echo "Repository:\n".$mod->repository."\n\n";
+		echo "modname;\n".$mod->modname."\n\n";
+		echo "Type:\n".$mod->type."\n\n";
+		echo "Name:\n".$mod->name."\n\n";
+		echo "Revision:\n".$mod->revision."\n\n";
+		
+		if (is_array($mod->author) && count($mod->author)) {
+			echo "Author:\n";
+			foreach($mod->author as $author) {
+				echo $author."\n";
+			}
+			echo "\n";
+		}
+		
+		if (is_array($mod->description) && count($mod->description)) {
+			echo "Description:\n";
+			foreach($mod->description as $description) {
+				echo $description."\n";
+			}
+			echo "\n";
+		}
+		
+		if ($mod->license !== NULL)
+			echo "License:\n".$mod->license."\n\n";
+		
+		if (is_array($mod->version) && count($mod->version)) {
+			echo "Version:\n";
+			foreach($mod->version as $version) {
+				echo $version."\n";
+			}
+			echo "\n";
+		}
+		
+		if ($mod->md5 !== NULL)
+			echo "md5:\n".$mod->md5."\n\n";
+		
+		if ($mod->lastmodif !== NULL)
+			echo "lastmodif:\n".$mod->lastmodif."\n\n";
+		
+		if (is_array($mod->configuration) && count($mod->configuration)) {
+			echo "Configuration:\n";
+			foreach($mod->configuration as $configuration) {
+				echo $configuration."\n";
+			}
+			echo "\n";
+		}
+		
+		if (is_array($mod->configuration_help) && count($mod->configuration_help)) {
+			echo "Configuration Help:\n";
+			foreach($mod->configuration_help as $configuration_help) {
+				echo $configuration_help."\n";
+			}
+			echo "\n";
+		}
+		
+		if (is_array($mod->files) && count($mod->files)) {
+			echo "Files:\n";
+			foreach($mod->files as $files) {
+				echo $files."\n";
+			}
+			echo "\n";
+		}
+		
+		if (is_array($mod->contributor) && count($mod->contributor)) {
+			echo "Contributor:\n";
+			foreach($mod->contributor as $contributor) {
+				echo $contributor."\n";
+			}
+			echo "\n";
+		}
+		
+		if (is_array($mod->devurl) && count($mod->devurl)) {
+			echo "devurl:\n";
+			foreach($mod->devurl as $devurl) {
+				echo $devurl."\n";
+			}
+			echo "\n";
+		}
+		
+		if (is_array($mod->docurl) && count($mod->docurl)) {
+			echo "docurl:\n";
+			foreach($mod->docurl as $docurl) {
+				echo $docurl."\n";
+			}
+			echo "\n";
+		}
+		
+		if (is_array($mod->help) && count($mod->help)) {
+			echo "Help:\n";
+			foreach($mod->help as $help) {
+				echo $help."\n";
+			}
+			echo "\n";
+		}
+		
+		if (is_array($mod->url) && count($mod->url)) {
+			echo "url:\n";
+			foreach($mod->url as $url) {
+				echo $url."\n";
+			}
+			echo "\n";
+		}
+		
+		if (is_array($mod->sql_upgrade) && count($mod->sql_upgrade)) {
+			echo "sql_Upgrade:\n";
+			foreach($mod->sql_upgrade as $sql_upgrade) {
+				echo $sql_upgrade."\n";
+			}
+			echo "\n";
+		}
+		
+		if (is_array($mod->sql_install) && count($mod->sql_install)) {
+			echo "sql_Install:\n";
+			foreach($mod->sql_install as $sql_install) {
+				echo $sql_install."\n";
+			}
+			echo "\n";
+		}
+		
+		if (is_array($mod->sql_remove) && count($mod->sql_remove)) {
+			echo "sql_Remove:\n";
+			foreach($mod->sql_remove as $sql_remove) {
+				echo $sql_remove."\n";
+			}
+			echo "\n";
+		}
+	}
+
 }
 
 function failure($errstr) {
