48b71d70ef33330604c00e5847fc4aa4e7fa7eb6
sylvieg <sylvieg@users.sourceforge.net>
2007-03-20 14:04:32 +0000

    [FIX]breadcrumb module: take into account the rows param
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@8069 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/modules/mod-breadcrumb.php
+++ tiki/modules/mod-breadcrumb.php
@@ -1,5 +1,5 @@
 <?php
-/** $Header: /cvsroot/tikiwiki/tiki/modules/mod-breadcrumb.php,v 1.7 2005-05-18 11:02:27 mose Exp $
+/** $Header: /cvsroot/tikiwiki/tiki/modules/mod-breadcrumb.php,v 1.8 2007-03-20 14:04:32 sylvieg Exp $
  * \param maxlen = max number of displayed characters for the page name
  */
 
@@ -12,8 +12,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 if (!isset($_SESSION["breadCrumb"])) {
 	$_SESSION["breadCrumb"] = array();
 }
-
-$bbreadCrumb = array_reverse($_SESSION["breadCrumb"]);
+$bbreadCrumb = array_slice(array_reverse($_SESSION["breadCrumb"]), 0, $module_rows);
 $smarty->assign('breadCrumb', $bbreadCrumb);
 $smarty->assign('maxlen', isset($module_params["maxlen"]) ? $module_params["maxlen"] : 0);
 ?>
