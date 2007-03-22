5069c12372260634451125e978d7dc9794405220
gillesm <gillesm@users.sourceforge.net>
2007-03-22 15:09:38 +0000

    extension of Copyright management for blogues, Articles, faq
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@8094 b456876b-0849-0410-b77d-98878d47e9d5

--- templates/tiki-admin-include-copyright.tpl
+++ templates/tiki-admin-include-copyright.tpl
@@ -1,4 +1,4 @@
-{* $Header: /cvsroot/tikiwiki/tiki/templates/tiki-admin-include-copyright.tpl,v 1.1 2007-03-07 14:23:11 gillesm Exp $ *}
+{* $Header: /cvsroot/tikiwiki/tiki/templates/tiki-admin-include-copyright.tpl,v 1.2 2007-03-22 15:09:38 gillesm Exp $ *}
 
 <div class="rbox" name="tip">
 <div class="rbox-title" name="tip">{tr}Tip{/tr}</div>  
@@ -11,12 +11,17 @@
     {tr}Copyright Management{/tr}
     </div>
     <div class="cbox-data">
-    <form action="tiki-admin.php?page=wiki" method="post">
+    <form action="tiki-admin.php?page=copyright" method="post">
     <table class="admin">
     <tr><td class="form">{tr}License Page{/tr}: </td><td><input type="text" name="wikiLicensePage" value="{$wikiLicensePage|escape}" /></td></tr>
     <tr><td class="form">{tr}Submit Notice{/tr}: </td><td><input type="text" name="wikiSubmitNotice" value="{$wikiSubmitNotice|escape}" /></td></tr>
    <tr><td class="form">{tr}Enable Feature for Wiki{/tr}:</td><td><input type="checkbox" name="wiki_feature_copyrights" {if $wiki_feature_copyrights eq 'y'}checked="checked"{/if}/></td></tr>
-    <tr><td colspan="2" class="button"><input type="submit" name="wikisetcopyright" value="{tr}Change preferences{/tr}" /></td></tr>    
+  <tr><td class="form">{tr}Enable Feature for Articles{/tr}:</td><td><input type="checkbox" name="articles_feature_copyrights" {if $articles_feature_copyrights eq 'y'}checked="checked"{/if}/></td></tr>
+  <tr><td class="form">{tr}Enable Feature for Blogues{/tr}:</td><td><input type="checkbox" name="blogues_feature_copyrights" {if $blogues_feature_copyrights eq 'y'}checked="checked"{/if}/></td></tr>
+  <tr><td class="form">{tr}Enable Feature for Faqs{/tr}:</td><td><input type="checkbox" name="faqs_feature_copyrights" {if $faqs_feature_copyrights eq 'y'}checked="checked"{/if}/></td></tr>
+  
+    <tr><td colspan="2" class="button"><input type="submit" name="setcopyright" value="{tr}Change preferences{/tr}" /></td></tr>    
+
 
      
 </table>
--- tiki-admin_include_copyright.php
+++ tiki-admin_include_copyright.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_include_copyright.php,v 1.1 2007-03-07 14:23:07 gillesm Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_include_copyright.php,v 1.2 2007-03-22 15:09:38 gillesm Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -8,27 +8,65 @@
 
 //this script may only be included - so its better to die if called directly.
 
+
 if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   header("location: index.php");
   exit;
 }
 
-if (isset($_REQUEST["copyrightfeature"])) {
-	check_ticket('admin-inc-copyright');
-	$pref_toggles = array(
-		"copyright_optional",
-		"copyright_default",
-		"copyright_wiki_parsed",
-		"copyright_wiki_semi_parsed"
-	);
+if (isset($_REQUEST["setcopyright"])) {
+        check_ticket('admin-inc-copyright');
+        if (isset($_REQUEST["wiki_feature_copyrights"]) && $_REQUEST["wiki_feature_copyrights"] == "on") {
+                $tikilib->set_preference("wiki_feature_copyrights", 'y');
 
-	foreach ($pref_toggles as $toggle) {
-		simple_set_toggle ($toggle);
-	}
+                $smarty->assign("wiki_feature_copyrights", 'y');
+        } else {
+                $tikilib->set_preference("wiki_feature_copyrights", 'n');
 
-	simple_set_value('copyright_toolbar_skin');
-	simple_set_value('copyright_toolbar');
+                $smarty->assign("wiki_feature_copyrights", 'n');
+        }
+       if (isset($_REQUEST["blogues_feature_copyrights"]) && $_REQUEST["blogues_feature_copyrights"] == "on") {
+                $tikilib->set_preference("blogues_feature_copyrights", 'y');
+
+                $smarty->assign("blogues_feature_copyrights", 'y');
+        } else {
+                $tikilib->set_preference("blogues_feature_copyrights", 'n');
+
+                $smarty->assign("blogues_feature_copyrights", 'n');
+        }
+ 	if (isset($_REQUEST["faqs_feature_copyrights"]) && $_REQUEST["faqs_feature_copyrights"] == "on") {
+                $tikilib->set_preference("faqs_feature_copyrights", 'y');
+
+                $smarty->assign("faqs_feature_copyrights", 'y');
+        } else {
+                $tikilib->set_preference("faqs_feature_copyrights", 'n');
+
+                $smarty->assign("faqs_feature_copyrights", 'n');
+        }
+       if (isset($_REQUEST["articles_feature_copyrights"]) && $_REQUEST["articles_feature_copyrights"] == "on") {
+                $tikilib->set_preference("articles_feature_copyrights", 'y');
+
+                $smarty->assign("articles_feature_copyrights", 'y');
+        } else {
+                $tikilib->set_preference("articles_feature_copyrights", 'n');
+
+                $smarty->assign("articles_feature_copyrights", 'n');
+        }
+
+        if (isset($_REQUEST["wikiLicensePage"])) {
+                $tikilib->set_preference("wikiLicensePage", $_REQUEST["wikiLicensePage"]);
+
+                $smarty->assign('wikiLicensePage', $_REQUEST["wikiLicensePage"]);
+        }
+
+        if (isset($_REQUEST["wikiSubmitNotice"])) {
+                $tikilib->set_preference("wikiSubmitNotice", $_REQUEST["wikiSubmitNotice"]);
+
+                $smarty->assign('wikiSubmitNotice', $_REQUEST["wikiSubmitNotice"]);
+        }
 }
+
+
 
 ask_ticket('admin-inc-copyright');
 ?>
