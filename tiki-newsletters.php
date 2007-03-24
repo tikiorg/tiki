03be3ebe2b6692389e538ca37f7d02af1856cc6b
gillesm <gillesm@users.sourceforge.net>
2007-03-24 13:38:51 +0000

    newsletters.php allows noshowlist option aollow subscription in a newsletter withouti showing list of all the other newsletter
    Syntax : tiki-newsletters.php?nlId=5&info=1&noshowlist
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@8109 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-newsletters.php
+++ tiki-newsletters.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-newsletters.php,v 1.25 2007-03-06 19:29:50 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-newsletters.php,v 1.26 2007-03-24 13:38:51 gillesm Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -131,6 +131,14 @@ if (!isset($_REQUEST["offset"])) {
 } else {
 	$offset = $_REQUEST["offset"];
 }
+
+if (isset($_REQUEST["noshowlist"])) {
+	$showlist = 'n';
+} else {
+	$showlist = 'y';
+}
+
+$smarty->assign('showlist',$showlist);
 
 $smarty->assign_by_ref('offset', $offset);
 
