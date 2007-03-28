9b4c385bc13a302f9445861b9ec4b121a6cf5e66
nyloth <nyloth@users.sourceforge.net>
2007-03-28 11:48:41 +0000

    [MOD] fckeditor: New upstream release (updating from 2.3.2 to 2.4.1). (Changelogs are available here: http://www.fckeditor.net/whatsnew/all )
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@8128 b456876b-0849-0410-b77d-98878d47e9d5

--- _fckeditor/editor/filemanager/upload/php/config.php
+++ _fckeditor/editor/filemanager/upload/php/config.php
@@ -1,48 +1,52 @@
-<?php 
+<?php
 /*
- * FCKeditor - The text editor for internet
- * Copyright (C) 2003-2006 Frederico Caldeira Knabben
- * 
- * Licensed under the terms of the GNU Lesser General Public License:
- * 		http://www.opensource.org/licenses/lgpl-license.php
- * 
- * For further information visit:
- * 		http://www.fckeditor.net/
- * 
- * "Support Open Source software. What about a donation today?"
- * 
- * File Name: config.php
- * 	Configuration file for the PHP File Uploader.
- * 
- * File Authors:
- * 		Frederico Caldeira Knabben (fredck@fckeditor.net)
+ * FCKeditor - The text editor for Internet - http://www.fckeditor.net
+ * Copyright (C) 2003-2007 Frederico Caldeira Knabben
+ *
+ * == BEGIN LICENSE ==
+ *
+ * Licensed under the terms of any of the following licenses at your
+ * choice:
+ *
+ *  - GNU General Public License Version 2 or later (the "GPL")
+ *    http://www.gnu.org/licenses/gpl.html
+ *
+ *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
+ *    http://www.gnu.org/licenses/lgpl.html
+ *
+ *  - Mozilla Public License Version 1.1 or later (the "MPL")
+ *    http://www.mozilla.org/MPL/MPL-1.1.html
+ *
+ * == END LICENSE ==
+ *
+ * Configuration file for the PHP File Uploader.
  */
 
 $tikiroot = dirname(dirname(dirname(dirname(dirname(dirname(dirname(dirname(getcwd()))))))));
 $tikidomain = '';
 if (is_file('db/virtuals.inc')) {
-  if (isset($_SERVER['TIKI_VIRTUAL']) and is_file($tikiroot.'/db/'.$_SERVER['TIKI_VIRTUAL'].'/local.php')) {
-    $tikidomain = $_SERVER['TIKI_VIRTUAL'];
-  } elseif (isset($_SERVER['SERVER_NAME']) and is_file($tikiroot.'/db/'.$_SERVER['SERVER_NAME'].'/local.php')) {
-    $tikidomain = $_SERVER['SERVER_NAME'];
-  } elseif (isset($_SERVER['HTTP_HOST']) and is_file($tikiroot.'/db/'.$_SERVER['HTTP_HOST'].'/local.php')) {
-    $tikidomain = $_SERVER['HTTP_HOST'];
-  }
+	if (isset($_SERVER['TIKI_VIRTUAL']) and is_file($tikiroot.'/db/'.$_SERVER['TIKI_VIRTUAL'].'/local.php')) {
+		$tikidomain = $_SERVER['TIKI_VIRTUAL'];
+	} elseif (isset($_SERVER['SERVER_NAME']) and is_file($tikiroot.'/db/'.$_SERVER['SERVER_NAME'].'/local.php')) {
+		$tikidomain = $_SERVER['SERVER_NAME'];
+	} elseif (isset($_SERVER['HTTP_HOST']) and is_file($tikiroot.'/db/'.$_SERVER['HTTP_HOST'].'/local.php')) {
+		$tikidomain = $_SERVER['HTTP_HOST'];
+	}
 }
 if ($tikidomain) $tikidomain.= '/';
 if ($tikiroot != $_SERVER['DOCUMENT_ROOT']) {
-  $tikipath = strrchr($tikiroot,$_SERVER['DOCUMENT_ROOT']).'/';
+	$tikipath = strrchr($tikiroot,$_SERVER['DOCUMENT_ROOT']).'/';
 } else {
-  $tikipath = '/';
+	$tikipath = '/';
 }
 
 global $Config ;
 
-// SECURITY: You must explicitelly enable this "uploader". 
+// SECURITY: You must explicitelly enable this "uploader".
 $Config['Enabled'] = true ;
 
-// Set if the file type must be considere in the target path. 
-// Ex: /UserFiles/Image/ or /UserFiles/File/
+// Set if the file type must be considere in the target path.
+// Ex: /userfiles/image/ or /userfiles/file/
 $Config['UseFileType'] = false ;
 
 // Path to uploaded files relative to the document root.
@@ -50,7 +54,7 @@ $Config['UserFilesPath'] = $tikipath.'img/wiki_up/'.$tikidomain ;
 
 // Fill the following value it you prefer to specify the absolute path for the
 // user files directory. Usefull if you are using a virtual directory, symbolic
-// link or alias. Examples: 'C:\\MySite\\UserFiles\\' or '/root/mysite/UserFiles/'.
+// link or alias. Examples: 'C:\\MySite\\userfiles\\' or '/root/mysite/userfiles/'.
 // Attention: The above 'UserFilesPath' must point to the same directory.
 $Config['UserFilesAbsolutePath'] = '' ;
 
@@ -59,7 +63,7 @@ $Config['UserFilesAbsolutePath'] = '' ;
 $Config['ForceSingleExtension'] = true ;
 
 $Config['AllowedExtensions']['File']	= array() ;
-$Config['DeniedExtensions']['File']		= array('php','php2','php3','php4','php5','phtml','pwml','inc','asp','aspx','ascx','jsp','cfm','cfc','pl','bat','exe','com','dll','vbs','js','reg','cgi') ;
+$Config['DeniedExtensions']['File']		= array('html','htm','php','php2','php3','php4','php5','phtml','pwml','inc','asp','aspx','ascx','jsp','cfm','cfc','pl','bat','exe','com','dll','vbs','js','reg','cgi','htaccess','asis') ;
 
 $Config['AllowedExtensions']['Image']	= array('jpg','gif','jpeg','png') ;
 $Config['DeniedExtensions']['Image']	= array() ;
