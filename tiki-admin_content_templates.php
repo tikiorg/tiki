8ceadf109305cc8d0a83692b5c49a8702c66034c
nyloth <nyloth@users.sourceforge.net>
2007-05-07 10:14:00 +0000

    [NEW] content templates: users can choose weither to use a wysiwyg editor or not when editing content templates.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@8276 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_content_templates.php
+++ tiki-admin_content_templates.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_content_templates.php,v 1.19 2007-03-06 19:29:45 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_content_templates.php,v 1.20 2007-05-07 10:13:59 nyloth Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -228,6 +228,9 @@ if ($offset > 0) {
 } else {
 	$smarty->assign('prev_offset', -1);
 }
+
+// wysiwyg decision
+include 'tiki-parsemode_setup.php';
 
 $smarty->assign_by_ref('channels', $channels["data"]);
 include_once("textareasize.php");
