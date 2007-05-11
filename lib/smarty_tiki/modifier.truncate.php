04e55c9a4557c134bed1a52403d330b2cdc89c59
sylvieg <sylvieg@users.sourceforge.net>
2007-05-11 15:43:03 +0000

    smarty: backport in tw truncate the smarty truncate - still have to be different because of the mb_substr
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@8312 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/modifier.truncate.php
+++ lib/smarty_tiki/modifier.truncate.php
@@ -1,11 +1,4 @@
 <?php
-
-//this script may only be included - so its better to die if called directly.
-if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
-  header("location: index.php");
-  exit;
-}
-
 /**
  * Smarty plugin
  * @package Smarty
@@ -19,35 +12,43 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
  * Type:     modifier<br>
  * Name:     truncate<br>
  * Purpose:  Truncate a string to a certain length if necessary,
- *           optionally splitting in the middle of a word, and 
- *           appending the $etc string.
+ *           optionally splitting in the middle of a word, and
+ *           appending the $etc string or inserting $etc into the middle.
  * @link http://smarty.php.net/manual/en/language.modifier.truncate.php
  *          truncate (Smarty online manual)
+ * @author   Monte Ohrt <monte at ohrt dot com>
  * @param string
  * @param integer
  * @param string
  * @param boolean
+ * @param boolean
  * @return string
  */
 function smarty_modifier_truncate($string, $length = 80, $etc = '...',
-                                  $break_words = false)
+                                  $break_words = false, $middle = false)
 {
     if ($length == 0)
         return '';
 
     if (strlen($string) > $length) {
-        $length -= strlen($etc);	
-       if ($break_words) // break the word
-        	$fragment = mb_substr($string, 0, $length);
-        else {
-            $fragment = mb_substr($string, 0, $length + 1);
-            $fragment = preg_replace('/\s+(\S+)?$/', '', $fragment);
+        $length -= min($length, strlen($etc));
+		if (function_exists('mb_substr'))
+			$func = 'mb_substr';
+		else
+			$func = 'substr';
+        if (!$break_words && !$middle) {
+            $string = preg_replace('/\s+?(\S+)?$/', '', $func($string, 0, $length+1));
         }
-	return $fragment.$etc;
-    } else
+        if(!$middle) {
+            return $func($string, 0, $length) . $etc;
+        } else {
+            return $func($string, 0, $length/2) . $etc . $func($string, -$length/2);
+        }
+    } else {
         return $string;
+    }
 }
 
-
+/* vim: set expandtab: */
 
 ?>
