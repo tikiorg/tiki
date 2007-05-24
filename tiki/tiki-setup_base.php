20fd0bf93cdf5731ffe6a28796d1b90b838ab67d
nyloth <nyloth@users.sourceforge.net>
2007-05-24 21:00:36 +0000

    [FIX] login and user creation:
      * Force a user to authenticate himself when he never logged in tiki, even if the session cookies seems to indicate the contrary. (This could happen, for example, when using the same PHP SESSION cookies for more than one tiki).
    
      * When using the "Tiki and PEAR::Auth" method : Do not ask for a password when creating a new user if tiki is configured neither to create the user in the Tiki auth system, nor in the PEAR Auth system. Also remove, in this case, the option to ask the user to change his password after the next login.
    
      * The user "real name" info (and maybe other params in the future), when retrieved from LDAP, will finally be updated after the complete user login validation since it should be done after the automated user creation (if used).
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@8392 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-setup_base.php
+++ tiki/tiki-setup_base.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-setup_base.php,v 1.131 2007-04-02 17:21:19 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-setup_base.php,v 1.132 2007-05-24 21:00:34 nyloth Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -333,6 +333,14 @@ if ($auth_method == 'shib' and isset($_SERVER['REMOTE_USER'])){
 // if the username is already saved in the session, pull it from there
 if (isset($_SESSION["$user_cookie_site"])) {
 	$user = $_SESSION["$user_cookie_site"];
+
+	// There could be a case where the session contains a user that doesn't exists in this tiki
+	// or that has never used the login step in this tiki.
+	// Example : If using the same PHP SESSION cookies for more than one tiki.
+	$user_details = $userlib->get_user_details($user);
+	if ( ! is_array($user_details) || ! is_array($user_details['info']) || (int)$user_details['info']['lastLogin'] <= 0 ) $user = NULL;
+	unset($user_details);
+
 } else {
 	$user = NULL;
 	
