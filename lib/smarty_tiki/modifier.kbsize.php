b52ed1304251869094d5d68f497b4fa76bf53d8b
nyloth <nyloth@users.sourceforge.net>
2007-05-26 16:12:26 +0000

    [FIX] smarty_tiki 'kbsize' modifier: 1 M = 1024 x 1024 (not 1000 x 1000) + now handle bits and bytes + number of decimals.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@8411 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/modifier.kbsize.php
+++ lib/smarty_tiki/modifier.kbsize.php
@@ -14,16 +14,14 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
  * Purpose:  returns size in Mb, Kb or bytes.
  * -------------------------------------------------------------
  */
-function smarty_modifier_kbsize($string)
+function smarty_modifier_kbsize($string, $bytes = false, $nb_decimals = 2)
 {
-  if($string>1000000) {
-    $string=number_format($string/1000000,2).' Mb';
-  } elseif($string>1000) {
-    $string=number_format($string/1000,2).' Kb';
-  } else {
-    $string=$string.' b';
-  }
-  return $string;	
+  // 1024 x 1024 = 1048576
+  if ( $string > 1048576 ) $string = number_format($string/1048576,$nb_decimals).' M';
+  elseif ( $string > 1024 ) $string = number_format($string/1024,$nb_decimals).' K';
+  else $string = $string.' ';
+
+  return $string.(($bytes) ? 'B' : 'b');
 }
 
 ?>
