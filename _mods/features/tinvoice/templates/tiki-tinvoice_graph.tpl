b642b42d4f4529a2f81aa89dbe02a5858e83e7e2
tombombadilom <tombombadilom@users.sourceforge.net>
2007-06-13 14:34:42 +0000

    invoice chart
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@8587 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ _mods/features/tinvoice/templates/tiki-tinvoice_graph.tpl
@@ -0,0 +1,35 @@
+<hr />
+<div class="navbar" style="height: 36px;"><div class="button2" style="text-align: center; width: 70px; float: left;"><a class="linkbut" {if $graphPeriod eq "week"} style="background-color: orange" {/if} href="tiki-tinvoice_list.php?graphPeriod=week&todate={$todate}">{tr}Week{/tr}</a>&nbsp;
+	</div><div class="button2" style="text-align: center; width: 70px; float: left;"><a class="linkbut"  {if $graphPeriod eq "month"} style="background-color: orange" {/if} href="tiki-tinvoice_list.php?graphPeriod=month&todate={$todate}">{tr}Month{/tr}</a>&nbsp;
+	</div><div class="button2" style="text-align: center; width: 70px; float: left;"><a class="linkbut"  {if $graphPeriod eq "trimester"} style="background-color: orange" {/if} href="tiki-tinvoice_list.php?graphPeriod=trimester&todate={$todate}" onclick="toggle('xtype');">{tr}Trimester{/tr}</a>&nbsp;
+	<form id="xtype" method="get" action="tiki-tinvoice_list.php" name="f" style="text-align: left; width: 220px; border: 1px solid black; display: {if $graphPeriod eq "trimester" || $graphPeriod eq "semester" || $graphPeriod eq "year"}block{else}none{/if};">
+<input type="hidden" name="graphPeriod" value="{$graphPeriod}" />
+<input type="hidden" name="todate" value="{$todate}" />
+<div class="calcheckbox"><span>View by</span><input type="radio" name="xtype" {if $xtype eq "day"}checked=checked {/if}  onclick="document.forms['xtype'].submit();" value="day" id="xtype_1"  />
+	<label for="xtype_1" class="linkbut">Day</label>
+<input type="radio" name="xtype" {if $xtype eq "week"}checked=checked {/if}  onclick="document.forms['xtype'].submit();" value="week" id="xtype_2"  />
+	<label for="xtype_2" class="linkbut">Week</label>
+<input type="radio" name="xtype" {if $xtype eq "month"}checked=checked {/if}  onclick="document.forms['xtype'].submit();" value="month" id="xtype_3"  />
+	<label for="xtype_3" class="linkbut">Month</label>
+</div>
+</form>
+</div><div  class="button2" style="text-align: center; width: 70px; float: left;"><a class="linkbut"  {if $graphPeriod eq "semester"} style="background-color: orange" {/if} href="tiki-tinvoice_list.php?graphPeriod=semester&todate={$todate}" onclick="toggle('xtype');">{tr}Semester{/tr}</a>&nbsp;
+	</div><div class="button2"  style="text-align: center; width: 70px; float: left;"><a class="linkbut"  {if $graphPeriod eq "year"} style="background-color: orange" {/if} href="tiki-tinvoice_list.php?graphPeriod=year&todate={$todate}" onclick="toggle('xtype');">{tr}Year{/tr}</a>&nbsp;
+	</div><div class="button2"  style="text-align: center; width: 100px; float: left;"><select name="graphFilter">
+		<option >Filter ---</option>
+		<option >Invoices</option>
+		<option >Paiements</option>
+	</select>
+</div><div class="button2" style="text-align: center; width: 125px; float: left;">{jscalendar id="start" date=$todate fieldname="todate" align="Bc" showtime='n'}
+</div>
+</div>
+
+<div class="navbar">
+	<a class="linkbut" href="tiki-tinvoice_list.php?graphPeriod={$graphPeriod}&todate={$prev}" alt="{tr}previous{/tr}">{tr}<< Previous {$graphPeriod}{/tr}</a>&nbsp;
+	<a class="linkbut" href="tiki-tinvoice_list.php?graphPeriod={$graphPeriod}&todate={$next}" alt="{tr}next{/tr}">{tr}Next {$graphPeriod}>>{/tr}</a>&nbsp;
+</div>
+
+<div align="center">
+	<img src="tiki-tinvoice_chart.php?graphPeriod={$graphPeriod}&todate={$todate}"  border=0 alt="tinvoice graphs" />
+</div>
+<hr />
---
+++ _mods/features/tinvoice/tiki-tinvoice_chart.php
@@ -0,0 +1,184 @@
+<?php
+// Initialization
+$section = 'tinvoice';
+require_once ('tiki-setup.php');
+
+require_once ('lib/tinvoice/tinvoicelib.php');
+require_once ('lib/webmail/contactlib.php');
+
+
+if ($feature_categories == 'y') {
+    include_once ('lib/categories/categlib.php');
+}
+
+//$owner=$tinvoicelib->get_owner_contact($contactId);
+
+$tinvoicelib=new TinvoiceLib($dbTiki);
+
+$id_emitter=isset($_REQUEST['id_emitter']) ? (int)$_REQUEST['id_emitter'] : NULL;
+$id_receiver=isset($_REQUEST['id_receiver']) ? (int)$_REQUEST['id_receiver'] : NULL;
+
+// get Period graph 
+$graphPeriod=$_REQUEST['graphPeriod'];
+if ($_REQUEST['todate']) {
+$todate=$_REQUEST['todate'];
+} else {
+$todate= date("%U");
+}
+// process graph 
+
+include ("lib/jpgraph/src/jpgraph.php");
+//include ("lib/jpgraph/src/jpgraph_line.php");
+include ("lib/jpgraph/src/jpgraph_bar.php");
+
+// datas
+$yvalue=array();
+$xvalue=array();
+
+//get timestamp dates array for extracted period  
+$period=$tinvoicelib->get_period_dates($todate,$graphPeriod);
+if ($graphPeriod == "week") {
+	// list week period 
+	// get week objects array
+	$invoices=$tinvoicelib->extract_Invoices($id_emitter, 'tiki', $id_receiver, 'tiki',$todate,$period);
+	// populate week array
+	for ($i=0; $i<count($period); $i++) {
+		$tdate= $period[$i]['date'];
+		array_push($xvalue,strftime("%a %d %h",$tdate));
+		$vb='';
+		foreach($invoices as $invoice) {
+			$a_date=$invoice->get_date_as_timestamp();
+			if (strftime("%w",$a_date) == $i) {	
+					$vb=$invoice->get_sum_amount();
+			}
+		}
+		if (!$vb) {		
+			array_push($yvalue,NULL);
+		} else {
+			
+			array_push($yvalue,$vb);
+		}
+
+	}
+
+} else if ($graphPeriod == "month") {
+	// list monthly period 
+	$invoices=$tinvoicelib->extract_Invoices($id_emitter, 'tiki', $id_receiver, 'tiki',$todate,$period);
+	// populate month array
+	for ($i=0; $i<=count($period)-1; $i++) {
+		$tdate= $period[$i]['date'];
+		array_push($xvalue,strftime("%d",$tdate));
+		$vb="";
+		foreach($invoices as $invoice) {
+			$a_date=$invoice->get_date_as_timestamp();
+			if (strftime("%d",$a_date) == $i+1) {	
+				$vb=$invoice->get_sum_amount();
+			}
+		}
+		if (!$vb) {		
+			array_push($yvalue,NULL);
+		} else {
+			
+			array_push($yvalue,$vb);
+		}
+	}
+} else if ($graphPeriod == "trimester") {
+	// list monthly period 
+	// remove this later
+	$xtype="day";
+	if ($xtype == "day") {	
+		$invoices=$tinvoicelib->extract_Invoices($id_emitter, 'tiki', $id_receiver, 'tiki',$todate,$period);
+	}
+	// populate trimester array
+	for ($i=0; $i<=count($period)-1; $i++) {
+		$tdate= $period[$i]['date'];
+		array_push($xvalue,strftime("%d-%h",$tdate));
+		$vb="";
+		foreach($invoices as $invoice) {
+			$a_date=$invoice->get_date_as_timestamp();
+			if ($a_date == $tdate) {	
+				$vb=$invoice->get_sum_amount();
+			}
+		}
+		if (!$vb) {		
+			array_push($yvalue,NULL);
+		} else {
+			
+			array_push($yvalue,$vb);
+		}
+		
+	}
+
+} else {
+	// list all 
+
+	$invoices=$tinvoicelib->list_invoices($id_emitter, 'tiki', $id_receiver, 'tiki');
+	foreach($invoices as $invoice) {
+	
+		$amount=$invoice->get_amount();
+		$a_date=$invoice->get_date_as_timestamp();
+		array_push($yvalue,$amount);
+		array_push($xvalue,strftime("%d-%m-%Y",$a_date));
+	}
+}
+
+// Create the graph. These two calls are always required
+$graph = new Graph(600,330,"auto"); 
+$graph->SetScale("textlin");
+$bplot = new BarPlot($yvalue);
+if ($graphPeriod == "week") {
+$bplot->value->Show();
+$bplot->value->SetFormat('%01.2f');
+$bplot->value->SetFont(FF_FONT1,FS_BOLD);
+// Center the values in the bar
+$bplot->SetValuePos('bottom');
+$bplot->SetShadow("#e6ae5c",10,10);
+$bplot->SetFillGradient("#b36b00","#FF9900",GRAD_RIGHT_REFLECTION);
+} else if  ($graphPeriod == "month") {
+$bplot->SetFillGradient("#b36b00","#FF9900",GRAD_RIGHT_REFLECTION);
+$bplot->SetShadow("steelblue",3,3);
+} else {
+$bplot->SetShadow("steelblue",2,2);
+}
+$bplot->SetColor("#FF9900");
+$bplot->SetLegend("Invoices");
+$bplot->SetWidth(1.0);
+$graph->Add($bplot);
+
+// Create and add Text stats
+$label="Total : ".array_sum($yvalue);
+$graph->tabtitle->Set($label);
+$graph->tabtitle->SetTabAlign('right'); 
+$graph->tabtitle->SetCorner(2);
+$graph->tabtitle->SetFillColor('white');
+$graph->tabtitle->SetColor('black','white','orange');
+
+$graph->yaxis->title->Set('Incomes');
+$graph->yaxis->title->SetColor('black');
+$graph->yaxis->title->SetMargin(10,0,0,0);
+$graph->xaxis->SetTickLabels($xvalue);
+if ($xtype == "day") {
+	$graph->xaxis->SetTextLabelInterval(14);
+}
+$graph->xaxis->SetTitle('Values for','middle'); 
+//$graph->xaxis->scale->ticks->SetLabelDateFormat("d.m.y");
+//$graph->xaxis->SetLabelAngle(90);
+$graph->title->Set($graphPeriod."ly Combined Invoices");
+$graph->img->SetMargin(50,10,30,40);
+$graph->legend->Pos(0.03,0.1,"right","top");
+$graph->legend->SetShadow();
+// Add a drop shadow
+$graph->SetShadow();
+
+//if ($graphPeriod != "week" && $graphPeriod != "month") {
+//	$datax = $gDateLocale->GetShortMonth();
+//	$graph->xaxis->SetTickLabels($datax)
+//}
+
+// ...and add it to the graPH
+
+
+// Display the graph
+$graph->Stroke();
+
+?>
