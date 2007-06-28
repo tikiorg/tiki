7984abc6d80387edae5e15110778e011997f041f
sylvieg <sylvieg@users.sourceforge.net>
2007-06-28 22:26:05 +0000

    [MOD]trackeritemfiled: some optimisation to lower nb of queries if the plugin is used a lot of time on the same page
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@8723 b456876b-0849-0410-b77d-98878d47e9d5

--- _mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php
+++ _mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php,v 1.4 2007-06-28 19:27:52 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php,v 1.5 2007-06-28 22:26:05 sylvieg Exp $
 function wikiplugin_trackeritemfield_help() {
 	$help = tra("Displays the value of a tracker item field or the wiki text if the value of the field is set (if itemId not specified, use the itemId of the url or the user tracker).").":\n";
 	$help .= "~np~{TRACKERITEMFIELD(itemId=>1, fieldId=>1, test=>1|0)}".tra('Wiki text')."{TRACKERITEMFIELD}~/np~";
@@ -8,25 +8,37 @@ function wikiplugin_trackeritemfield_help() {
 function wikiplugin_trackeritemfield($data, $params) {
 	global $userTracker, $group, $user, $userlib, $tiki_p_admin_trackers;
 	global $trklib; include_once('lib/trackers/trackerlib.php');
+	static $memoItemId = 0;
+	static $memoTrackerId = 0;
 
 	extract ($params, EXTR_SKIP);
-	if (empty($itemId) && !empty($_REQUEST['itemId'])) {
-		$itemId = $_REQUEST['itemId'];
-	}
-	if (empty($itemId) && $userTracker == 'y' && !empty($group) && ($utid = $userlib->get_usertrackerid($group)) && $utid['usersTrackerId']) {
-		$trackerId = $utid['usersTrackerId'];
-		$itemId = $trklib->get_item_id($trackerId, $utid['usersFieldId'], $user);
-		$is_user_tracker = true;
-	} else if (empty($trackerId) && !empty($itemId)) {
-		$item = $trklib->get_tracker_item($itemId);
-		$trackerId = $item['trackerId'];
-	}
 
-	if (empty($itemId) || empty($trackerId) || empty($fieldId)) {
-		return false;
-	}
-	if ($tiki_p_admin_trackers != 'y' && !$userlib->user_has_perm_on_object($user, $trackerId, 'tracker','tiki_p_view_trackers') && empty($is_user_tracker)) {
-		return false;
+	if ((!empty($itemId) && $memoItemId == $itemId) || (empty($itemId) && !empty($memoItemId))) {
+		$itemId = $memoItemId;
+		if (empty($memoTrackerId)) {
+			return false;
+		}
+		$trackerId = $memoTrackerId;
+	} else {
+		if (empty($itemId) && !empty($_REQUEST['itemId'])) {
+			$itemId = $_REQUEST['itemId'];
+		}
+		if (empty($itemId) && $userTracker == 'y' && !empty($group) && ($utid = $userlib->get_usertrackerid($group)) && $utid['usersTrackerId']) {
+			$trackerId = $utid['usersTrackerId'];
+			$itemId = $trklib->get_item_id($trackerId, $utid['usersFieldId'], $user);
+			$is_user_tracker = true;
+		} else if (empty($trackerId) && !empty($itemId)) {
+			$item = $trklib->get_tracker_item($itemId);
+			$trackerId = $item['trackerId'];
+		}
+		$memoItemId = $itemId;
+		if (empty($itemId) || empty($trackerId) || empty($fieldId)) {
+			return false;
+		}
+		if ($tiki_p_admin_trackers != 'y' && !$userlib->user_has_perm_on_object($user, $trackerId, 'tracker','tiki_p_view_trackers') && empty($is_user_tracker)) {
+			return false;
+		}
+		$memoTrackerId = $trackerId;
 	}
 
 	if (!($field = $trklib->get_tracker_field($fieldId))) {
