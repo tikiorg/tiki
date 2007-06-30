38b253d89614d108652ef363c99c0c03c079f9a4
sylvieg <sylvieg@users.sourceforge.net>
2007-06-30 00:44:09 +0000

    Instant-Auto-Merge from BRANCH-1-9 to HEAD
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@8736 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-pagehistory.php
+++ tiki/tiki-pagehistory.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-pagehistory.php,v 1.41 2007-06-06 13:27:04 nkoth Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-pagehistory.php,v 1.42 2007-06-30 00:44:09 sylvieg Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
