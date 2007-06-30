f36a0ebb31cc4f43c8628fae359148f52d53bd23
sylvieg <sylvieg@users.sourceforge.net>
2007-06-30 00:52:05 +0000

    Instant-Auto-Merge from BRANCH-1-9 to HEAD
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@8737 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-rename_page.php
+++ tiki/tiki-rename_page.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-rename_page.php,v 1.19 2007-03-06 19:29:51 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-rename_page.php,v 1.20 2007-06-30 00:52:05 sylvieg Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
