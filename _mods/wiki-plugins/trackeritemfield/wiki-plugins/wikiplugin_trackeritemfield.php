43475a8eb1ba167945e66e5d4445b151739f359f
sylvieg <sylvieg@users.sourceforge.net>
2007-07-17 13:03:52 +0000

    notice
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@8879 b456876b-0849-0410-b77d-98878d47e9d5

--- _mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php
+++ _mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php,v 1.5 2007-06-28 22:26:05 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/_mods/wiki-plugins/trackeritemfield/wiki-plugins/wikiplugin_trackeritemfield.php,v 1.6 2007-07-17 13:03:52 sylvieg Exp $
 function wikiplugin_trackeritemfield_help() {
 	$help = tra("Displays the value of a tracker item field or the wiki text if the value of the field is set (if itemId not specified, use the itemId of the url or the user tracker).").":\n";
 	$help .= "~np~{TRACKERITEMFIELD(itemId=>1, fieldId=>1, test=>1|0)}".tra('Wiki text')."{TRACKERITEMFIELD}~/np~";
@@ -31,10 +31,10 @@ function wikiplugin_trackeritemfield($data, $params) {
 			$item = $trklib->get_tracker_item($itemId);
 			$trackerId = $item['trackerId'];
 		}
-		$memoItemId = $itemId;
 		if (empty($itemId) || empty($trackerId) || empty($fieldId)) {
 			return false;
 		}
+		$memoItemId = $itemId;
 		if ($tiki_p_admin_trackers != 'y' && !$userlib->user_has_perm_on_object($user, $trackerId, 'tracker','tiki_p_view_trackers') && empty($is_user_tracker)) {
 			return false;
 		}
