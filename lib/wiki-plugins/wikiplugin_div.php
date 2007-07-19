967b2c78ddc1e03a072a20e02761741367da10be
ricks99 <ricks99@users.sourceforge.net>
2007-07-19 21:02:35 +0000

    added support for the id attribute on the div element
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@8926 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_div.php
+++ lib/wiki-plugins/wikiplugin_div.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_div.php,v 1.9 2006-08-29 20:19:11 sylvieg Exp $
+ * $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_div.php,v 1.10 2007-07-19 21:02:35 ricks99 Exp $
  *
  * DIV plugin. Creates a division block for the content. Forces the content 
  * to be aligned (left by default).
@@ -27,8 +27,9 @@ function wikiplugin_div($data, $params) {
 	$al   = (isset($align) && ($align == 'right' || $align == "center" || $align == "justify")) ? " text-align: $align;" : " text-align: left;";
 	$fl   = (isset($float) && ($float == 'left' || $float == 'right')) ? " float: $float;"  : " float: none;";
 	$cl   = (isset($clear) && ($clear == 'left' || $clear == 'right' || $clear == 'both')) ? " clear: $clear;"  : " clear: none;";
+	$id    = (isset($id)) ? "id='$id'"  : "";
 
-	$begin  = "<$t style=\"$bg$al$w$fl$cl\"$c>";
+	$begin  = "<$t style=\"$bg$al$w$fl$cl\"$c $id>";
 	$end = "</$t>";
 	return $begin . $data . $end;
 }
