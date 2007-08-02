4a325fbdef0315da1275969c87f1da2b6a2d574b
sylvieg <sylvieg@users.sourceforge.net>
2007-08-02 13:06:48 +0000

    Instant-Auto-Merge from BRANCH-1-9 to HEAD
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@9008 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-list_articles.php
+++ tiki-list_articles.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-list_articles.php,v 1.33 2007-04-02 17:21:18 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-list_articles.php,v 1.34 2007-08-02 13:06:48 sylvieg Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -154,6 +154,15 @@ if ($feature_multilingual == 'y') {
 	$smarty->assign_by_ref('languages', $languages);
 	$avls = unserialize($tikilib->get_preference("available_languages"));
 	$smarty->assign_by_ref('available_languages', $avls);
+}
+
+if ($tiki_p_edit_article != 'y' && $tiki_p_remove_article != 'y') { //check one editable
+	foreach ($listpages['data'] as $page) {
+		if ($page['author'] == $user && $page['creator_edit'] == 'y') {
+			$smarty->assign('oneEditPage', 'y');
+			break;
+		}
+	}
 }
 
 include_once ('tiki-section_options.php');
