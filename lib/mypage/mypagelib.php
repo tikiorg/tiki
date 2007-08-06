1ff01204711adce00b687708ba51ed130ee5c895
niclone <niclone@users.sourceforge.net>
2007-08-06 19:25:30 +0000

    [FIX] mypage: make emacs users happy
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@9045 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/mypage/mypagelib.php
+++ lib/mypage/mypagelib.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/lib/mypage/mypagelib.php,v 1.5 2007-08-06 19:16:14 niclone Exp $
+// $Header: /cvsroot/tikiwiki/tiki/lib/mypage/mypagelib.php,v 1.6 2007-08-06 19:25:30 niclone Exp $
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -330,5 +330,10 @@ class MyPageWindow {
     }
 }
 
+/* For the emacs weenies in the crowd.
+Local Variables:
+   c-basic-offset: 4
+End:
+*/
 
 ?>
