6faf8d3d1860175408d6a9469f244fd87f147072
niclone <niclone@users.sourceforge.net>
2007-08-08 14:15:06 +0000

    [FIX] mypage: reindent the code with tab-width = 4 instead of 8
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@9087 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/mootools/make-js.php
+++ lib/mootools/make-js.php
@@ -4,88 +4,94 @@
 require_once("packer/class.JavaScriptPacker.php");
 
 function compact_files($header, $srcdir, $files, $destination) {
+	
+	$t1 = microtime(true);
+	
+	$content='';
+	foreach($files as $src) {
+		$src="$srcdir/$src";
+		$content.=file_get_contents($src);
+	}
 
-    $t1 = microtime(true);
+	$packer = new JavaScriptPacker($content, 'Normal', true, false);
+	$packed = $header.$packer->pack();
 
-    $content='';
-    foreach($files as $src) {
-	$src="$srcdir/$src";
-	$content.=file_get_contents($src);
-    }
-
-    $packer = new JavaScriptPacker($content, 'Normal', true, false);
-    $packed = $header.$packer->pack();
-
-    $t2 = microtime(true);
-    $time = sprintf('%.4f', ($t2 - $t1) );
-    echo 'script ', $destination, ' packed in ' , $out, ', in ', $time, ' s.', "\n";
-    
-    file_put_contents($destination, $packed);
+	$t2 = microtime(true);
+	$time = sprintf('%.4f', ($t2 - $t1) );
+	echo 'script ', $destination, ' packed in ' , $out, ', in ', $time, ' s.', "\n";
+	
+	file_put_contents($destination, $packed);
 }
 
 
 compact_files("//MooTools, My Object Oriented Javascript Tools. Copyright (c) 2006 Valerio Proietti, <http://mad4milk.net>, MIT Style License.\n\r\n\r",
-	      "src",
-	      array(
-		    "Core/Core.js",
-		    "Class/Class.js",
-		    "Class/Class.Extras.js",
-		    "Native/Array.js",
-		    "Native/String.js",
-		    "Native/Function.js",
-		    "Native/Number.js",
-		    "Native/Element.js",
-		    "Element/Element.Event.js",
-		    "Element/Element.Filters.js",
-		    "Element/Element.Selectors.js",
-		    "Element/Element.Form.js",
-		    "Element/Element.Dimensions.js",
-		    "Window/Window.DomReady.js",
-		    "Window/Window.Size.js",
-		    "Effects/Fx.Base.js",
-		    "Effects/Fx.CSS.js",
-		    "Effects/Fx.Style.js",
-		    "Effects/Fx.Styles.js",
-		    "Effects/Fx.Elements.js",
-		    "Effects/Fx.Scroll.js",
-		    "Effects/Fx.Slide.js",
-		    "Effects/Fx.Transitions.js",
-		    "Drag/Drag.Base.js",
-		    "Drag/Drag.Move.js",
-		    "Remote/XHR.js",
-		    "Remote/Ajax.js",
-		    "Remote/Cookie.js",
-		    "Remote/Json.js",
-		    "Remote/Json.Remote.js",
-		    "Remote/Assets.js",
-		    "Plugins/Hash.js",
-		    "Plugins/Hash.Cookie.js",
-		    "Plugins/Color.js",
-		    "Plugins/Scroller.js",
-		    "Plugins/Slider.js",
-		    "Plugins/SmoothScroll.js",
-		    "Plugins/Sortables.js",
-		    "Plugins/Tips.js",
-		    "Plugins/Group.js",
-		    "Plugins/Accordion.js"),
-	      "mootools.js");
+			  "src",
+			  array(
+					"Core/Core.js",
+					"Class/Class.js",
+					"Class/Class.Extras.js",
+					"Native/Array.js",
+					"Native/String.js",
+					"Native/Function.js",
+					"Native/Number.js",
+					"Native/Element.js",
+					"Element/Element.Event.js",
+					"Element/Element.Filters.js",
+					"Element/Element.Selectors.js",
+					"Element/Element.Form.js",
+					"Element/Element.Dimensions.js",
+					"Window/Window.DomReady.js",
+					"Window/Window.Size.js",
+					"Effects/Fx.Base.js",
+					"Effects/Fx.CSS.js",
+					"Effects/Fx.Style.js",
+					"Effects/Fx.Styles.js",
+					"Effects/Fx.Elements.js",
+					"Effects/Fx.Scroll.js",
+					"Effects/Fx.Slide.js",
+					"Effects/Fx.Transitions.js",
+					"Drag/Drag.Base.js",
+					"Drag/Drag.Move.js",
+					"Remote/XHR.js",
+					"Remote/Ajax.js",
+					"Remote/Cookie.js",
+					"Remote/Json.js",
+					"Remote/Json.Remote.js",
+					"Remote/Assets.js",
+					"Plugins/Hash.js",
+					"Plugins/Hash.Cookie.js",
+					"Plugins/Color.js",
+					"Plugins/Scroller.js",
+					"Plugins/Slider.js",
+					"Plugins/SmoothScroll.js",
+					"Plugins/Sortables.js",
+					"Plugins/Tips.js",
+					"Plugins/Group.js",
+					"Plugins/Accordion.js"),
+			  "mootools.js");
 
 
 compact_files("//Windoo: Mootools window class <http://code.google.com/p/windoo>. Copyright (c) 2007 Yevgen Gorshkov, MIT Style License.\n\r\n\r",
-	      "extensions/windoo/src",
-	      array(
-		    "Effects/Fx.Overlay.js",
-		    "Windoo/Windoo.Core.js",
-		    "Windoo/Windoo.Manager.js",
-		    "Windoo/Windoo.Drag.js",
-		    "Windoo/Windoo.Dialog.js",
-		    "Windoo/Windoo.Panel.js",
-		    "Windoo/Windoo.Ajax.js",
-		    "Windoo/Windoo.Themes.js",
-		    "Drag/Drag.Multi.js",
-		    "Drag/Drag.Resize.js",
-		    "Drag/Drag.ResizeImage.js"),
-	      "extensions/windoo/windoo.js");
+			  "extensions/windoo/src",
+			  array(
+					"Effects/Fx.Overlay.js",
+					"Windoo/Windoo.Core.js",
+					"Windoo/Windoo.Manager.js",
+					"Windoo/Windoo.Drag.js",
+					"Windoo/Windoo.Dialog.js",
+					"Windoo/Windoo.Panel.js",
+					"Windoo/Windoo.Ajax.js",
+					"Windoo/Windoo.Themes.js",
+					"Drag/Drag.Multi.js",
+					"Drag/Drag.Resize.js",
+					"Drag/Drag.ResizeImage.js"),
+			  "extensions/windoo/windoo.js");
 
+/* For the emacs weenies in the crowd.
+Local Variables:
+   tab-width: 4
+   c-basic-offset: 4
+End:
+*/
 
 ?>
--- tiki-mypage.php
+++ tiki-mypage.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-mypage.php,v 1.5 2007-08-07 17:31:19 niclone Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-mypage.php,v 1.6 2007-08-08 14:15:05 niclone Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -11,59 +11,66 @@ require_once ('lib/mypage/mypagelib.php');
 require_once ('lib/ajax/ajaxlib.php');
 
 if (strlen($user) <= 0) {
-    $id_users=0;
+	$id_users=0;
 } else {
-    $id_users=$userlib->get_user_id($user);
+	$id_users=$userlib->get_user_id($user);
 }
 
 function mypage_ajax_init() {
-    global $ajaxlib;
+	global $ajaxlib;
 
-    //$ajaxlib->debugOn();
-    $ajaxlib->setRequestURI("tiki-mypage_ajax.php");
-    $ajaxlib->registerFunction("mypage_win_setrect");
-    $ajaxlib->registerFunction("mypage_win_destroy");
-    $ajaxlib->registerFunction("mypage_win_create");
-    $ajaxlib->processRequests();
+	//$ajaxlib->debugOn();
+	$ajaxlib->setRequestURI("tiki-mypage_ajax.php");
+	$ajaxlib->registerFunction("mypage_win_setrect");
+	$ajaxlib->registerFunction("mypage_win_destroy");
+	$ajaxlib->registerFunction("mypage_win_create");
+	$ajaxlib->processRequests();
 }
 
 function mypage_init() {
-    global $smarty, $headerlib, $id_users;
+	global $smarty, $headerlib, $id_users;
 
-    $headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.css");
-    $headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.aero.css");
-    $headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.alphacube.css");
-    $headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.aqua.css");
-    $headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.nada.css");
-
-
-    $id_mypage=isset($_REQUEST['id_mypage']) ? (int)$_REQUEST['id_mypage'] : 0;
-
-    $mypage=new MyPage($id_mypage, $id_users);
-    $smarty->assign('mypagejswindows', $mypage->getJSCode());
-    $smarty->assign('id_mypage', $id_mypage);
-
-    $width=$mypage->getParam('width');
-    $height=$mypage->getParam('height');
-    $width=($width == 0 ? '100%' : $width.'px');
-    $height=($height == 0 ? '100%' : $height.'px');
-
-    $smarty->assign('mypage_width', $width);
-    $smarty->assign('mypage_height', $height);
-
-    // deactivate left and right columns 
-    $smarty->assign('feature_right_column', 'n');
-    $smarty->assign('feature_left_column', 'n');
+	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.css");
+	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.aero.css");
+	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.alphacube.css");
+	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.aqua.css");
+	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.nada.css");
 
 
-    mypage_ajax_init();
+	$id_mypage=isset($_REQUEST['id_mypage']) ? (int)$_REQUEST['id_mypage'] : 0;
 
-    $smarty->assign("mid", "tiki-mypage.tpl");
-    $smarty->display("tiki.tpl");
+	$mypage=new MyPage($id_mypage, $id_users);
+	$smarty->assign('mypagejswindows', $mypage->getJSCode());
+	$smarty->assign('id_mypage', $id_mypage);
+
+	$width=$mypage->getParam('width');
+	$height=$mypage->getParam('height');
+	$width=($width == 0 ? '100%' : $width.'px');
+	$height=($height == 0 ? '100%' : $height.'px');
+
+	$smarty->assign('mypage_width', $width);
+	$smarty->assign('mypage_height', $height);
+
+	// deactivate left and right columns 
+	$smarty->assign('feature_right_column', 'n');
+	$smarty->assign('feature_left_column', 'n');
+
+
+	mypage_ajax_init();
+
+	$smarty->assign("mid", "tiki-mypage.tpl");
+	$smarty->display("tiki.tpl");
 
 }
 
 
 mypage_init();
+
+/* For the emacs weenies in the crowd.
+Local Variables:
+   tab-width: 4
+   c-basic-offset: 4
+End:
+*/
 
 ?>
