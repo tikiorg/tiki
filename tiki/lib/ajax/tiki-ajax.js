3739353dd1b63a4a8444a9d54f0a4d2e07e93016
sylvieg <sylvieg@users.sourceforge.net>
2007-08-08 13:46:05 +0000

    [FIX]avoid js error if xajax is not defeined
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@9085 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/ajax/tiki-ajax.js
+++ tiki/lib/ajax/tiki-ajax.js
@@ -2,14 +2,15 @@ function loadComponent(url, template, htmlelement) {
     xajaxRequestUri = url;
     xajax_loadComponent(template, htmlelement);      
 }
+if (typeof xajax != "undefined") {
+	xajax.loadingFunction = function() {
+		show('ajaxLoading');
+	};
 
-xajax.loadingFunction = function() {
-    show('ajaxLoading');
-};
-
-xajax.doneLoadingFunction = function() {
-    hide('ajaxLoading');
-};
+	xajax.doneLoadingFunction = function() {
+		hide('ajaxLoading');
+	};
+}
 
 
 
