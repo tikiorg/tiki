ea1190388ebe4ffe6055886ca4129df7b50499f1
sylvieg <sylvieg@users.sourceforge.net>
2007-08-23 20:54:39 +0000

    [MOD]last-articles module : new params showImg=width&showDate=y&showHeading=chars
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@9260 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/modules/mod-last_articles.php
+++ tiki/modules/mod-last_articles.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/tikiwiki/tiki/modules/mod-last_articles.php,v 1.16 2007-08-04 10:29:21 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/modules/mod-last_articles.php,v 1.17 2007-08-23 20:54:39 sylvieg Exp $
 
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
@@ -9,7 +9,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 
 if (!function_exists('mod_last_articles_help')) {
 	function mod_last_articles_help() {
-		return "type=Article|Event|...&topicId=1&topic=xx&categId=1&lang=en";
+		return "type=Article|Event|...&topicId=1&topic=xx&categId=1&lang=en&showImg=width&showDate=y&showHeading=chars";
 	}
 }
 // Parameter absurl set if the last_article url is absolute or not [y|n].
@@ -24,8 +24,22 @@ $smarty->assign('type',$mod_type);
 $smarty->assign('topicId',$mod_topicId);
 $categId = isset($module_params['categId']) ? $module_params['categId'] : '';
 $l = isset($module_params['lang']) ? $module_params['lang'] : '';
+if (isset($module_params['showImg'])) {
+	$smarty->assign('showImg', $module_params['showImg']);
+}
+if (isset($module_params['showDate']) && $module_params['showDate'] == 'y') {
+	$smarty->assign('showDate','y');
+}
 
 $ranking = $tikilib->list_articles(0,$module_rows,'publishDate_desc', '', date("U"), '', $mod_type, $mod_topicId, 'y', $mod_topic, $categId, '', '', $l);
+if (isset($module_params['showHeading']) && $module_params['showHeading'] != 'n') {
+	if ($module_params['showHeading'] == 'y')
+		$module_params['showHeading'] = -1;
+	$smarty->assign('showHeading',$module_params['showHeading']);
+	foreach ($ranking['data'] as $key=>$article) {
+		$ranking['data'][$key]['parsedHeading'] = $tikilib->parse_data($article['heading']);
+	}
+}
 $smarty->assign('modLastArticles',$ranking["data"]);
 $smarty->assign('nonums', isset($module_params["nonums"]) ? $module_params["nonums"] : 'n');
 $smarty->assign('absurl', isset($module_params["absurl"]) ? $module_params["absurl"] : 'n');
