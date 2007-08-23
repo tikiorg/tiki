911a8423459fd49025e195e33be81e4f3c4e6210
niclone <niclone@users.sourceforge.net>
2007-08-23 17:27:12 +0000

    [DEV] mypage: add preliminary support of plugin by mypage type
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@9252 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-mypage_ajax.php
+++ tiki-mypage_ajax.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-mypage_ajax.php,v 1.22 2007-08-21 22:04:40 niclone Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-mypage_ajax.php,v 1.23 2007-08-23 17:27:12 niclone Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -261,6 +261,14 @@ function mypage_fillinfos($id_mypage) {
     $objResponse->addAssign('mypageedit_height', 'value', $mypage->getParam('height'));
     $objResponse->addAssign('mypageedit_type', 'value', $mypage->getParam('id_types'));
     $objResponse->addScript('mypageTypeChange('.(int)$mypage->getParam('id_types').');');
+	
+	$conf=NULL;
+	$type=MyPage::getMypageType((int)$mypage->getParam('id_types'));
+	if (is_array($type))
+		$conf=MyPage::getTypeHTMLConfig($type['name']);
+	if ($conf === NULL) $conf='';
+	
+	$objResponse->addAssign('mypageedit_typeconf', 'innerHTML', $conf);
 
     return $objResponse;
 }
