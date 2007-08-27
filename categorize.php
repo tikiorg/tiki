0fe75af8b6ba59f940239b8becb505e29f02b94b
sylvieg <sylvieg@users.sourceforge.net>
2007-08-27 20:06:51 +0000

    [MOD]categ: extract the function update_object_categories from the smarty/REQUEST context
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@9289 b456876b-0849-0410-b77d-98878d47e9d5

--- categorize.php
+++ categorize.php
@@ -1,5 +1,5 @@
 <?php 
-// $Header: /cvsroot/tikiwiki/tiki/categorize.php,v 1.22 2007-08-10 13:42:39 guidoscherp Exp $
+// $Header: /cvsroot/tikiwiki/tiki/categorize.php,v 1.23 2007-08-27 20:06:51 sylvieg Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -13,10 +13,8 @@ $access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__));
 global $feature_categories;
 
 if ($feature_categories == 'y') {
-	global $categlib;
-	if (!is_object($categlib)) {
-		include_once('lib/categories/categlib.php');
-	}
+	global $categlib; include_once('lib/categories/categlib.php');
+
 	$smarty->assign('cat_categorize', 'n');
 
 	if (isset($_REQUEST['import']) and isset($_REQUEST['categories'])) {
@@ -24,36 +22,13 @@ if ($feature_categories == 'y') {
 		$_REQUEST["cat_categorize"] = 'on';
 	}
 
-	$old_categories = $categlib->get_object_categories($cat_type, $cat_objid);
-	$new_categories = array();
-	$removed_categories = $old_categories;
-
 	if (isset($_REQUEST["cat_categorize"]) && $_REQUEST["cat_categorize"] == 'on') {
 		$smarty->assign('cat_categorize', 'y');
-		$categlib->uncategorize_object($cat_type, $cat_objid);
-
-		if (isset($_REQUEST["cat_categories"])) {
-			$new_categories=array_diff($_REQUEST["cat_categories"], $old_categories);
-			$removed_categories=array_diff($old_categories, $_REQUEST["cat_categories"]);						
-			foreach ($_REQUEST["cat_categories"] as $cat_acat) {
-				if ($cat_acat) {
-					$catObjectId = $categlib->is_categorized($cat_type, $cat_objid);
-
-					if (!$catObjectId) {
-						// The object is not cateorized  
-						$catObjectId = $categlib->add_categorized_object($cat_type, $cat_objid, $cat_desc, $cat_name, $cat_href);
-					}
-
-					$categlib->categorize($catObjectId, $cat_acat);
-				}
-			}
-		}
 	} else {
-		$categlib->uncategorize_object($cat_type, $cat_objid);
+		$_REQUEST['cat_categories'] = NULL;
 	}
-	
-	$categorizedObject=$categlib->get_categorized_object($cat_type, $cat_objid);
-	
+	$categlib->update_object_categories($_REQUEST['cat_categories'], $cat_objid, $cat_type, $cat_desc, $cat_name, $cat_href);
+
 	$cats = $categlib->get_object_categories($cat_type, $cat_objid);
 	if (isset($section) && $section == 'wiki' && $feature_wiki_mandatory_category > 0)
 		$categories = $categlib->list_categs($feature_wiki_mandatory_category);
@@ -69,24 +44,6 @@ if ($feature_categories == 'y') {
 	}
 	$smarty->assign_by_ref('categories', $categories["data"]);
 
-	foreach ( $new_categories as $cat) {			
-		$category=$categlib->get_category($cat);
-		$values= array("categoryId"=>$cat, "categoryName"=>$category['name'], "categoryPath"=>$categlib->get_category_path_string_with_root($cat),
-			"description"=>$category['description'], "parentId" => $category['parentId'], "parentName" => $categlib->get_category_name($category['parentId']),
-			"action"=>"object entered category", "objectName"=>$categorizedObject['name'],
-			"objectType"=>$categorizedObject['type'], "objectUrl"=>$categorizedObject['href']);		
-		$categlib->notify($values);								
-	}
-		
-	foreach ( $removed_categories as $cat) {
-		$category=$categlib->get_category($cat);	
-		$values= array("categoryId"=>$cat, "categoryName"=>$category['name'], "categoryPath"=>$categlib->get_category_path_string_with_root($cat),
-			"description"=>$category['description'], "parentId" => $category['parentId'], "parentName" => $categlib->get_category_name($category['parentId']),
-			"action"=>"object leaved category", "objectName"=>$categorizedObject['name'],
-			"objectType"=>$categorizedObject['type'], "objectUrl"=>$categorizedObject['href']);		
-		$categlib->notify($values);								
-	}		
-	
 }
 
 ?>
--- lib/categories/categlib.php
+++ lib/categories/categlib.php
@@ -1,6 +1,6 @@
 <?php
 /** \file
- * $Header: /cvsroot/tikiwiki/tiki/lib/categories/categlib.php,v 1.108 2007-08-10 13:34:07 guidoscherp Exp $
+ * $Header: /cvsroot/tikiwiki/tiki/lib/categories/categlib.php,v 1.109 2007-08-27 20:06:51 sylvieg Exp $
  *
  * \brief Categories support class
  *
@@ -1365,6 +1365,48 @@ class CategLib extends ObjectLib {
 			}			
 		}
 		return $result;
+	}
+	function update_object_categories($categories, $objId, $objType, $desc='', $name='', $href='') {
+		global $feature_user_watches;
+		if ($feature_user_watches == 'y') { //todo: optimize by doing this only if categ watched
+			$old_categories = $this->get_object_categories($objType, $objId);
+			$new_categories = array();
+			$removed_categories = $old_categories;
+		}
+
+		if (!empty($old_categories)) {
+			$this->uncategorize_object($objType, $objId);
+		}
+		if (!empty($categories))  {
+			$new_categories = array_diff($categories, $old_categories);
+			$removed_categories = array_diff($old_categories, $categories);						
+			foreach ($categories as $category) {
+				if ($category) {
+					if (!($catObjectId = $this->is_categorized($objType, $objId))) {
+						$catObjectId = $this->add_categorized_object($objType, $objId, $desc, $name, $href);
+					}
+					$this->categorize($catObjectId, $category);
+				}
+			}
+		}
+
+		if ($feature_user_watches == 'y') {
+			$categorizedObject = $this->get_categorized_object($objType, $objId);
+			foreach ($new_categories as $categId) {			
+		   		$category = $this->get_category($categId);
+				$values = array('categoryId'=>$categId, 'categoryName'=>$category['name'], 'categoryPath'=>$this->get_category_path_string_with_root($categId),
+					'description'=>$category['description'], 'parentId'=>$category['parentId'], 'parentName'=>$this->get_category_name($category['parentId']),
+					'action'=>'object entered category', 'objectName'=>$name, 'objectType'=>$objType, 'objectUrl'=>$href);		
+				$this->notify($values);								
+			}
+			foreach ($removed_categories as $categId) {
+				$category = $this->get_category($categhId);	
+				$values= array('categoryId'=>$categId, 'categoryName'=>$category['name'], 'categoryPath'=>$this->get_category_path_string_with_root($cat),
+					'description'=>$category['description'], 'parentId'=>$category['parentId'], 'parentName'=>$this->get_category_name($category['parentId']),
+				 	'action'=>'object leaved category', 'objectName'=>$name, 'objectType'=>$objType, 'objectUrl'=>$href);
+				$this->notify($values);								
+			}
+		}
 	}
 	
 }
