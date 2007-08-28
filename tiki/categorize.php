e5eef6a4e81381c49cbd7494f775a74f4705532c
sylvieg <sylvieg@users.sourceforge.net>
2007-08-28 11:52:00 +0000

    continue to remove the access library
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@9292 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/categorize.php
+++ tiki/categorize.php
@@ -1,5 +1,5 @@
 <?php 
-// $Header: /cvsroot/tikiwiki/tiki/categorize.php,v 1.23 2007-08-27 20:06:51 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/categorize.php,v 1.24 2007-08-28 11:52:00 sylvieg Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -7,8 +7,11 @@
 
 //this script may only be included - so its better to err & die if called directly.
 //smarty is not there - we need setup
+if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
+  header("location: index.php");
+  exit;
+}
 require_once('tiki-setup.php');  
-$access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__));
 
 global $feature_categories;
 
