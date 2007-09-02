861542debbb0a1266ba0dcc213ab84c998e7aeb1
parkercroft <parkercroft@users.sourceforge.net>
2007-09-02 20:22:11 +0000

    Remove a bogus key
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@9335 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki-1.10-mysqli.sql
+++ db/tiki-1.10-mysqli.sql
@@ -1,6 +1,6 @@
 # $Rev$
-# $Date: 2007-09-02 12:18:20 $
-# $Author: sylvieg $
+# $Date: 2007-09-02 20:22:11 $
+# $Author: parkercroft $
 # $Name: not supported by cvs2svn $
 # phpMyAdmin MySQL-Dump
 # version 2.5.1
@@ -4917,8 +4917,7 @@ CREATE TABLE `tiki_mypage` (
   PRIMARY KEY  (`id`),
   KEY `id_users` (`id_users`),
   KEY `name` (`name`)
-  KEY `id_types` (`id_types`)
-) ENGINE=MyISAM;
+) ENGINE = MyISAM;
 
 CREATE TABLE `tiki_mypagewin` (
   `id` int(11) NOT NULL auto_increment,
