3b9dfe1dc33aeaf42b11a885463e69a47e5e98d0
tombombadilom <tombombadilom@users.sourceforge.net>
2007-09-08 10:41:31 +0000

    [MOD]smarty: duration an option to have a short duration expression + tr
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@9373 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/smarty_tiki/modifier.duration.php
+++ tiki/lib/smarty_tiki/modifier.duration.php
@@ -14,26 +14,66 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
  * Purpose:  formats a duration from seconds
  * -------------------------------------------------------------
  */
-function smarty_modifier_duration($string)
+function smarty_modifier_duration($string, $long=true)
 {
   $result=Array();
   if($string > 60*60*24) {
     $days = floor($string/(60*60*24));
-    $result[]="$days days";
+    if ($days > 1) {
+	$s = tra('days');
+    } else {
+	$s = tra('day');
+    }
+    if ($long) {
+	$s = " $s";
+    } else {
+	$s = substr($s, 0, 1);
+    }
+    $result[]="$days$s";
     $string = $string % (60*60*24);
   }
   if($string > 60*60) {
     $hours = floor($string/(60*60));
-    $result[]="$hours hours";
+    if ($hours > 1) {
+	$s = tra('hours');
+    } else {
+	$s = tra('hour');
+    }
+    if ($long) {
+	$s = " $s";
+    } else {
+	$s = substr($s, 0, 1);
+    }
+    $result[]="$hours$s";
     $string = $string % (60*60);
   }
-  if($string > 60) {
+  if($string > 60 && ($long || (!$long && empty($days)))) {
     $mins = floor($string/(60));
-    $result[]="$mins minutes";
+    if ($mins > 1) {
+	$s = tra('mins');
+    } else {
+	$s = tra('min');
+    }
+    if ($long) {
+	$s = " $s";
+    } else {
+	$s = substr($s, 0, 1);
+    }
+    $result[]="$mins$s";
     $string = $string % (60);
   }
-  if($string > 0) {
-    $result[]="$string seconds";
+  if($string > 0 && ($long || (!$long && empty($days) && empty($hours)))) {
+    if ($string > 1) {
+	$s = tra('secs');
+    } else {
+	$s = tra('sec');
+    }
+    if ($long) {
+	$s = " $s";
+    } else {
+	$s = substr($s, 0, 1);
+    }
+    $result[]="$string$s";
   }
   
   return implode(' ',$result);
