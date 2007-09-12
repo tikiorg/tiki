c45f935e5b1db45edb34e59e309b58643af21327
sylvieg <sylvieg@users.sourceforge.net>
2007-09-12 10:12:34 +0000

    [MOD]mypage: partial implementation of feature_mypage_category_mandatory
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@9425 b456876b-0849-0410-b77d-98878d47e9d5

--- categorize_list.php
+++ categorize_list.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/categorize_list.php,v 1.28 2007-08-27 16:41:33 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/categorize_list.php,v 1.29 2007-09-12 10:12:34 sylvieg Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -25,9 +25,10 @@ if ($feature_categories == 'y') {
 	}
 
 	$cats = $categlib->get_object_categories($cat_type, $cat_objid);
-	if ($cat_type == 'wiki page' || $cat_type == 'blog' || $cat_type == 'image gallery') {
+	if ($cat_type == 'wiki page' || $cat_type == 'blog' || $cat_type == 'image gallery' || $cat_type == 'mypage') {
 		$ext = ($cat_type == 'wiki page')? 'wiki':str_replace(' ', '_', $cat_type);
 		$pref = 'feature_'.$ext.'_mandatory_category';
+		global $$pref;
 		if ($$pref > 0)
 			$all_categories = $categlib->list_categs($$pref);
 		else
