df7ab21a515e18270ec2e4c1af7462344d569606
niclone <niclone@users.sourceforge.net>
2007-09-14 23:42:20 +0000

    [FIX] mypage: also set mypage theme for mypages listing
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@9451 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-mypages.php
+++ tiki/tiki-mypages.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-mypages.php,v 1.21 2007-09-10 17:41:51 niclone Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-mypages.php,v 1.22 2007-09-14 23:42:20 niclone Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -148,9 +148,10 @@ function mypageedit_init() {
 
 	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.css");
 	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.aero.css");
-	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.alphacube.css");
-	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.aqua.css");
-	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.nada.css");
+// 	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.alphacube.css");
+// 	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.aqua.css");
+// 	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.nada.css");
+	$headerlib->add_cssfile("lib/mootools/extensions/windoo/themes/windoo.mypage.css");
 
 	$smarty->assign("mid", "tiki-mypages.tpl");
 	$smarty->display("tiki.tpl");
