0ebe24ce5caef0f63b4365a971a0a34247ef6cc8
sylvieg <sylvieg@users.sourceforge.net>
2007-10-03 21:17:15 +0000

    notice
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk/tiki@9567 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/block.tr.php
+++ lib/smarty_tiki/block.tr.php
@@ -20,6 +20,8 @@ if (strpos($_SERVER['SCRIPT_NAME'],basename(__FILE__)) !== false) {
 include_once('lib/init/tra.php');
 
 function smarty_block_tr($params, $content, &$smarty) {
+	if (empty($content) || empty($params['lang']))
+		return;
 	$lang = $params['lang'];
 	if ( !isset($lang) or $lang == '') $lang = $language;
 	echo tra($content,$lang);
