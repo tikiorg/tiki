e15a1d2de0ad603201392da58bcc79622dd865f0
niclone <niclone@users.sourceforge.net>
2007-10-16 10:46:03 +0000

    [FIX] mootools: updated the make-js script for making a packed and a not packed versions of each scripts.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@9725 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/mootools/make-js.php
+++ tiki/lib/mootools/make-js.php
@@ -3,7 +3,7 @@
 
 require_once("packer/class.JavaScriptPacker.php");
 
-function compact_files($header, $srcdir, $files, $destination) {
+function compact_files($header, $srcdir, $files, $destination_packed, $destination) {
 	
 	$t1 = microtime(true);
 	
@@ -18,9 +18,10 @@ function compact_files($header, $srcdir, $files, $destination) {
 
 	$t2 = microtime(true);
 	$time = sprintf('%.4f', ($t2 - $t1) );
-	echo 'script ', $destination, ' packed in ' , $out, ', in ', $time, ' s.', "\n";
+	echo 'script ', $destination_packed, ' packed in ' , $out, ', in ', $time, ' s.', "\n";
 	
-	file_put_contents($destination, $packed);
+	file_put_contents($destination_packed, $packed);
+	file_put_contents($destination, $header.$content);
 }
 
 function compact_module($modulename) {
@@ -33,69 +34,72 @@ function compact_module($modulename) {
 							"Class/Class.js",
 							"Class/Class.Extras.js",
 							"Native/Array.js",
-					"Native/String.js",
-					"Native/Function.js",
-					"Native/Number.js",
-					"Native/Element.js",
-					"Element/Element.Event.js",
-					"Element/Element.Filters.js",
-					"Element/Element.Selectors.js",
-					"Element/Element.Form.js",
-					"Element/Element.Dimensions.js",
-					"Window/Window.DomReady.js",
-					"Window/Window.Size.js",
-					"Effects/Fx.Base.js",
-					"Effects/Fx.CSS.js",
-					"Effects/Fx.Style.js",
-					"Effects/Fx.Styles.js",
-					"Effects/Fx.Elements.js",
-					"Effects/Fx.Scroll.js",
-					"Effects/Fx.Slide.js",
-					"Effects/Fx.Transitions.js",
-					"Drag/Drag.Base.js",
-					"Drag/Drag.Move.js",
-					"Remote/XHR.js",
-					"Remote/Ajax.js",
-					"Remote/Cookie.js",
-					"Remote/Json.js",
-					"Remote/Json.Remote.js",
-					"Remote/Assets.js",
-					"Plugins/Hash.js",
-					"Plugins/Hash.Cookie.js",
-					"Plugins/Color.js",
-					"Plugins/Scroller.js",
-					"Plugins/Slider.js",
-					"Plugins/SmoothScroll.js",
-					"Plugins/Sortables.js",
-					"Plugins/Tips.js",
-					"Plugins/Group.js",
-					"Plugins/Accordion.js"),
-			  "mootools.js");
+							"Native/String.js",
+							"Native/Function.js",
+							"Native/Number.js",
+							"Native/Element.js",
+							"Element/Element.Event.js",
+							"Element/Element.Filters.js",
+							"Element/Element.Selectors.js",
+							"Element/Element.Form.js",
+							"Element/Element.Dimensions.js",
+							"Window/Window.DomReady.js",
+							"Window/Window.Size.js",
+							"Effects/Fx.Base.js",
+							"Effects/Fx.CSS.js",
+							"Effects/Fx.Style.js",
+							"Effects/Fx.Styles.js",
+							"Effects/Fx.Elements.js",
+							"Effects/Fx.Scroll.js",
+							"Effects/Fx.Slide.js",
+							"Effects/Fx.Transitions.js",
+							"Drag/Drag.Base.js",
+							"Drag/Drag.Move.js",
+							"Remote/XHR.js",
+							"Remote/Ajax.js",
+							"Remote/Cookie.js",
+							"Remote/Json.js",
+							"Remote/Json.Remote.js",
+							"Remote/Assets.js",
+							"Plugins/Hash.js",
+							"Plugins/Hash.Cookie.js",
+							"Plugins/Color.js",
+							"Plugins/Scroller.js",
+							"Plugins/Slider.js",
+							"Plugins/SmoothScroll.js",
+							"Plugins/Sortables.js",
+							"Plugins/Tips.js",
+							"Plugins/Group.js",
+							"Plugins/Accordion.js"),
+					  "mootools_packed.js",
+					  "mootools.js");
 		break;
-
+		
 	case 'windoo':
 		compact_files("//Windoo: Mootools window class <http://code.google.com/p/windoo>. Copyright (c) 2007 Yevgen Gorshkov, MIT Style License.\n\r\n\r",
 					  "extensions/windoo/src",
-			  array(
-					"Effects/Fx.Overlay.js",
-					"Windoo/Windoo.Core.js",
-					"Windoo/Windoo.Manager.js",
-					"Windoo/Windoo.Drag.js",
-					"Windoo/Windoo.Dialog.js",
-					"Windoo/Windoo.Panel.js",
-					"Windoo/Windoo.Ajax.js",
-					"Windoo/Windoo.Themes.js",
-					"Drag/Drag.Multi.js",
-					"Drag/Drag.Resize.js",
-					"Drag/Drag.ResizeImage.js"),
-			  "extensions/windoo/windoo.js");
+					  array(
+							"Effects/Fx.Overlay.js",
+							"Windoo/Windoo.Core.js",
+							"Windoo/Windoo.Manager.js",
+							"Windoo/Windoo.Drag.js",
+							"Windoo/Windoo.Dialog.js",
+							"Windoo/Windoo.Panel.js",
+							"Windoo/Windoo.Ajax.js",
+							"Windoo/Windoo.Themes.js",
+							"Drag/Drag.Multi.js",
+							"Drag/Drag.Resize.js",
+							"Drag/Drag.ResizeImage.js"),
+					  "extensions/windoo/windoo_packed.js",
+					  "extensions/windoo/windoo.js");
 		break;
-
+		
 	case 'moorainbow':		
 		compact_files("//mooRainbow: Mootools color picker, from: http://w00fz.altervista.org/mooRainbow/\n\r\n\r",
 					  "extensions/mooRainbow",
 					  array("mooRainbow.js"),
-					  "extensions/mooRainbow/mooRainbow_compressed.js");
+					  "extensions/mooRainbow/mooRainbow_packed.js",
+					  "extensions/mooRainbow/mooRainbow.js");
 		break;
 	}
 }
@@ -113,6 +117,10 @@ function show_help() {
 $modules=array('mootools', 'windoo', 'moorainbow');
 
 unset($argv[0]);
+if (count($argv) == 0) {
+	show_help();
+	exit(0);
+}
 foreach($argv as $arg) {
 	switch($arg) {
 	case '-h':
