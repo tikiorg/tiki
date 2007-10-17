5f88f0c4292e33cc7fd346968442c3a40256f4db
niclone <niclone@users.sourceforge.net>
2007-10-17 20:01:04 +0000

    [FIX] removed "!isset($_SESSION)" because it break auth on php5. Seem that a $_SESSION is always set.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10/tiki@9766 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup_base.php
+++ tiki-setup_base.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-setup_base.php,v 1.142.2.1 2007-10-17 19:26:15 jonnybradley Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-setup_base.php,v 1.142.2.2 2007-10-17 20:01:04 niclone Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -84,7 +84,7 @@ if ($prefs['session_db'] == 'y') {
 	include('session/adodb-session.php');
 }
 
-if ( ($sessions_silent == 'disabled' or !empty($_COOKIE)) && !isset($_SESSION) ) {
+if ($sessions_silent == 'disabled' or !empty($_COOKIE)) {
 	// enabing silent sessions mean a session is only started when a cookie is presented
 	session_start();
 }
