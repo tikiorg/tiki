162267bd82fba68b85a5f60c5d2cd3b860935d94
sylvieg <sylvieg@users.sourceforge.net>
2007-11-14 22:47:27 +0000

    [MOD]plugin: a plugin subscribe_group http://doc.tikiwiki.org/tiki-index.php?page=PluginSubscribeGroup
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@10327 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki/lib/wiki-plugins/wikiplugin_subscribegroup.php
@@ -0,0 +1,57 @@
+<?php
+// $Header: /cvsroot/tikiwiki/tiki/lib/wiki-plugins/wikiplugin_subscribegroup.php,v 1.1.2.1 2007-11-14 22:47:24 sylvieg Exp $
+// Display wiki text if user is in one of listed groups
+// Usage:
+// {GROUP(groups=>Admins|Developers)}wiki text{GROUP}
+
+function wikiplugin_subscribegroup_help() {
+	$help = tra('Subscribe or unsubscribe to a group').":\n";
+	$help.= "~np~<br />{SUBSCRIBEGROUP(group=, subscribe=text, unsubscribe=text) /}<br />~/np~";
+	return $help;
+}
+function wikiplugin_subscribegroup($data, $params) {
+	global $tiki_p_subscribe_groups, $userlib, $user, $smarty;
+	if ($tiki_p_subscribe_groups != 'y' || empty($user)) {
+		return '';
+	}
+	extract ($params, EXTR_SKIP);
+	if (empty($group)) {
+		if (!empty($_REQUEST['group'])) {
+			$group = $_REQUEST['group'];
+		} else {
+			return tra('Missing parameter');
+		}
+	}
+	if ($group == 'Anonymous' || $group == 'Registered') {
+		return tra('Incorrect param');
+	}
+	if (!($info = $userlib->get_group_info($group))) {
+		return tra('Incorrect param');
+	}
+	if ($info['userChoice'] != 'y') {
+		return tra('Permission denied');
+	}
+
+	$groups = $userlib->get_user_groups_inclusion($user);
+
+	if (!empty($_REQUEST['subscribeGroup'])) {
+		if (isset($groups[$group])) {
+			$userlib->remove_user_from_group($user, $group);
+		} else {
+			$userlib->assign_user_to_group($user, $group);
+		}
+	}
+
+	if (isset($groups[$group])) {
+		if ($groups[$group] == 'included') {
+			return tra('Incorrect param');
+		}
+		$text = empty($unsubscribe)? 'Unsubscribe %s': $unsubscribe;
+	} else {
+		$text = empty($subscribe)? 'Subscribe to %s': $subscribe;
+	}
+	$smarty->assign('text', sprintf(tra($text), $group));
+	$smarty->assign('subscribeGroup', $group);
+	$data = $smarty->fetch('wiki-plugins/wikiplugin_subscribegroup.tpl');
+	return $data;
+}
