6f4907374e4c5d7443d9b2d08443874b94feffb5
sylvieg <sylvieg@users.sourceforge.net>
2007-11-21 15:08:12 +0000

    temporary modif
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@10377 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/menubuilder/menu.js
+++ tiki/lib/menubuilder/menu.js
@@ -1,5 +1,23 @@
 sfHover = function() {
+	var sfEls = document.getElementById("cssmenu0").getElementsByTagName("LI");
+	for (var i=0; i<sfEls.length; i++) {
+		sfEls[i].onmouseover=function() {
+			this.className+=" sfhover";
+		}
+		sfEls[i].onmouseout=function() {
+			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+		}
+	}
 	var sfEls = document.getElementById("cssmenu1").getElementsByTagName("LI");
+	for (var i=0; i<sfEls.length; i++) {
+		sfEls[i].onmouseover=function() {
+			this.className+=" sfhover";
+		}
+		sfEls[i].onmouseout=function() {
+			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+		}
+	}
+	var sfEls = document.getElementById("cssmenu2").getElementsByTagName("LI");
 	for (var i=0; i<sfEls.length; i++) {
 		sfEls[i].onmouseover=function() {
 			this.className+=" sfhover";
