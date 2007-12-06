1ded9969955bc52d9894311227c3b110ba0a320d
nkoth <nkoth@users.sourceforge.net>
2007-12-06 16:38:27 +0000

    [MOD] add maxRecords to sanitization as integer
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10/tiki@10676 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup_base.php
+++ tiki-setup_base.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-setup_base.php,v 1.142.2.3 2007-12-05 09:28:04 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-setup_base.php,v 1.142.2.4 2007-12-06 16:38:27 nkoth Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -202,6 +202,7 @@ $vartype['name'] = 'string';
 $vartype['reqId'] = '+hash';
 $vartype['days'] = '+int';
 $vartype['max'] = '+int';
+$vartype['maxRecords'] = '+int';
 $vartype['numrows'] = '+int';
 $vartype['rows'] = '+int';
 $vartype['cols'] = '+int';
