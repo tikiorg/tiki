e2e27ac89942cf2138c84664e54938d904afffbe
nkoth <nkoth@users.sourceforge.net>
2007-12-11 15:01:43 +0000

    [MOD] do not show category for approved pages in category list if this is a staging page
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@10772 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/categorize_list.php
+++ tiki/categorize_list.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/categorize_list.php,v 1.30.2.3 2007-12-08 16:48:06 nkoth Exp $
+// $Header: /cvsroot/tikiwiki/tiki/categorize_list.php,v 1.30.2.4 2007-12-11 15:01:43 nkoth Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -50,6 +50,11 @@ if ($prefs['feature_categories'] == 'y') {
 	for ($i = 0; $i < count($all_categories); $i++) {
 		if($userlib->user_has_perm_on_object($user,$all_categories[$i]['categId'],'category','tiki_p_edit_categorized')) {
 			$categories[] = $all_categories[$i];
+			// remove category for approved pages from category list if this is a staging page
+			if ($prefs["feature_wikiapproval"] == 'y' && $cat_type == 'wiki page' && substr($cat_objid, 0, strlen($prefs['wikiapproval_prefix'])) == $prefs['wikiapproval_prefix']
+			 && $prefs['wikiapproval_approved_category'] > 0 && $prefs['wikiapproval_approved_category'] == $all_categories[$i]['categId']) {			
+				array_pop($categories);
+			}	
 		}
 	}
 
