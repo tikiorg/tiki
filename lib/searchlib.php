a6258de561fa90d7fd1ec5c7dbbee51b2745d99b
nkoth <nkoth@users.sourceforge.net>
2007-12-15 23:29:43 +0000

    [FIX] query error due to bracket closing problem
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10/tiki@10837 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/searchlib.php
+++ lib/searchlib.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/tikiwiki/tiki/lib/searchlib.php,v 1.48.2.4 2007-12-07 14:20:47 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/lib/searchlib.php,v 1.48.2.5 2007-12-15 23:29:43 nkoth Exp $
 //test
 
 //this script may only be included - so its better to die if called directly.
@@ -175,7 +175,7 @@ class SearchLib extends TikiLib {
 		    $sqlGroup = " GROUP BY $objKeyGroup ";
 
 		    if ($chkObjPerm) {
-			$sqlHaving = " HAVING (perms=? AND (NOT categorized OR NOT forbidden OR forbidden IS NULL)))";
+			$sqlHaving = " HAVING (perms=? AND (NOT categorized OR NOT forbidden OR forbidden IS NULL))";
 			if ($globalPerm == 'y') {
 				$sqlHaving .= " or ";
 			} else {
