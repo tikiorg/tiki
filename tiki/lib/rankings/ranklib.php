3687e626bf2bfebc4a20ef4a7fbb7084ed76241b
nkoth <nkoth@users.sourceforge.net>
2007-12-15 17:59:40 +0000

    [MOD] add forum rankings of "last replied to topics" vs. just "last topics"
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@10832 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/rankings/ranklib.php
+++ tiki/lib/rankings/ranklib.php
@@ -145,32 +145,52 @@ class RankLib extends TikiLib {
 		return $retval;
 	}
 
-	function forums_ranking_last_topics($limit, $forumId='') {
+	function forums_ranking_last_replied_topics($limit, $forumId='', $last_replied=true) {
+		$retval = $this->forums_ranking_last_topics($limit, $forumId, $last_replied);
+		return $retval;
+	}
+	
+	function forums_ranking_last_topics($limit, $forumId='', $last_replied=false) {
+		// $last_replied == true, means that topics shown will be based on last replied, not last created.
 		global $user;
 		if (is_array($forumId)) {
 			$bindvars = $forumId;
-			$mid = ' and tf.`forumId` in ('.implode(',',array_fill(0,count($forumId),'?')).')';
+			$mid = ' and a.`object` in ('.implode(',',array_fill(0,count($forumId),'?')).')';
 		} elseif (!empty($forumId)) {
 			$bindvars=array((int) $forumId);
-		    $mid = ' and tf.`forumId`=?';
+		    $mid = ' and a.`object`=?';
 		} else {
 			$bindvars = array();
 			$mid = '';
 		}
+		if ($last_replied == false)
+		{		
 		$query = "select * from
-		`tiki_comments`,`tiki_forums` tf where
+		`tiki_comments` a,`tiki_forums` tf where
 		`object`=".$this->sql_cast("`forumId`","string")." and `objectType` = 'forum' and
 		`parentId`=0 $mid order by `commentDate` desc";
+		} else {		
+		$query = "select a.*, tf.*, max(b.`commentDate`) as `lastPost` from
+		`tiki_comments` a left join `tiki_comments` b on b.`parentId`=a.`threadId` right join `tiki_forums` tf on "
+		.$this->sql_cast("tf.`forumId`","string").
+		" where	a.`objectType` = 'forum' and a.`parentId`=0 $mid group by a.`threadId` order by `lastPost` desc";
+		}
 
 		$result = $this->query($query, $bindvars);
 		$ret = array();
 		$count = 0;
 		while (($res = $result->fetchRow()) && $count < $limit) {
-                  if ($this->user_has_perm_on_object($user, $res['forumId'], 'forum', 'tiki_p_forum_read')) {
+                  if ($this->user_has_perm_on_object($user, $res['object'], 'forum', 'tiki_p_forum_read')) {
 				$aux['name'] = $res['name'] . ': ' . $res['title'];
-				$aux['hits'] = $res['commentDate'];
-				$aux['href'] = 'tiki-view_forum_thread.php?forumId=' . $res['forumId'] . '&amp;comments_parentId=' . $res['threadId'];
-				$aux['date'] = $res['commentDate'];
+				$aux['href'] = 'tiki-view_forum_thread.php?forumId=' . $res['object'] . '&amp;comments_parentId=' . $res['threadId'];
+				if ($last_replied == false) {
+					$aux['date'] = $res['commentDate'];
+					// the following line is correct, the second column named hits shows date				
+					$aux['hits'] = $res['commentDate']; 				
+				} else {
+					$aux['date'] = $res['lastPost'];
+					$aux['hits'] = $res['lastPost']; 
+				}
 				$aux['user'] = $res['userName'];
 				$ret[] = $aux;
 				++$count;
