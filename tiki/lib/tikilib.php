4e60416f92d8583f49d5cf45373756fb0d9f7615
sylvieg <sylvieg@users.sourceforge.net>
2008-01-03 23:37:28 +0000

    [FIX]wiki: in the doc, minor does not send notification->so do it
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@10948 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/tikilib.php
+++ tiki/lib/tikilib.php
@@ -1,5 +1,5 @@
 <?php
-// CVS: $Id: tikilib.php,v 1.801.2.63 2008-01-01 00:14:01 nkoth Exp $
+// CVS: $Id: tikilib.php,v 1.801.2.64 2008-01-03 23:37:28 sylvieg Exp $
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   header("location: index.php");
@@ -6768,7 +6768,7 @@ if (!$simple_wiki) {
 		   }
 		}
 
-		if ($prefs['feature_user_watches'] == 'y') {
+		if ($prefs['feature_user_watches'] == 'y' && !$minor) {
 			//  Deal with mail notifications.
 			include_once('lib/notifications/notificationemaillib.php');
 			global $histlib; include_once ("lib/wiki/histlib.php");
