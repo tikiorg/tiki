f03dc79dc1f3fdaf828283a9e5ccd21586cb9cdf
sept_7 <sept_7@users.sourceforge.net>
2008-01-21 09:13:01 +0000

    [MOD] : adding a smarty function to display an icon to show the sort order of a list.
    Exemple of code :
    <a href="{$smarty.server.PHP_SELF}?{query sort="date_pub_asc,date_pub_desc}">{tr}Beginning of Publication{/tr}{show_sort sort=sort var=date_pub}</a>
    
    show_sort takes two arguments :
      sort : the variable name on wich the sort if done
    	var  : the prefix of the value of the sort
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10/tiki@11102 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/smarty_tiki/function.show_sort.php
@@ -0,0 +1,26 @@
+<?php
+
+//this script may only be included - so its better to die if called directly.
+if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
+  header("location: index.php");
+  exit;
+}
+
+function smarty_function_show_sort($params, &$smarty) {
+	global $url_path;
+  if (isset($params['sort']) and isset($params['var']) and isset($_REQUEST[$params['sort']])) {
+	  $prop = substr($_REQUEST[$params['sort']],0,strrpos($_REQUEST[$params['sort']],'_'));
+		$order = substr($_REQUEST[$params['sort']],strrpos($_REQUEST[$params['sort']],'_')+1);
+		if (strtolower($prop) == strtolower(trim($params['var']))) {
+		  switch($order) {
+			  case 'asc':
+				  echo "<img style='border:none;vertical-align:middle' src='".$url_path."pics/icons/resultset_up.png'/>";
+					break;
+			  case 'desc':
+				  echo "<img style='border:none;vertical-align:middle' src='".$url_path."pics/icons/resultset_down.png'/>";
+					break;
+			}
+		}
+	}
+}
+?>
