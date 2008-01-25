0972cdbc008b891d444da521577971cd20f1ad7a
nyloth <nyloth@users.sourceforge.net>
2008-01-25 15:26:02 +0000

    [MOD] freetags normalization: allow all chars - including accents -  by default on new installations (does not affect upgrades)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10/tiki@11169 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/prefs.php
+++ lib/setup/prefs.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/lib/setup/prefs.php,v 1.16.2.37 2008-01-24 20:00:21 nyloth Exp $
+// $Header: /cvsroot/tikiwiki/tiki/lib/setup/prefs.php,v 1.16.2.38 2008-01-25 15:26:02 nyloth Exp $
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for
@@ -454,7 +454,7 @@ if ( ! $_SESSION['need_reload_prefs'] ) {
 	$prefs['freetags_cloud_colors'] = '';
 	$prefs['freetags_preload_random_search'] = 'y';
 	$prefs['freetags_browse_amount_tags_in_cloud'] = '100';
-	$prefs['freetags_normalized_valid_chars'] = 'a-zA-Z0-9';
+	$prefs['freetags_normalized_valid_chars'] = '';
 	$prefs['freetags_lowercase_only'] = 'y';
 	$prefs['freetags_feature_3d'] = 'n';
 	$prefs['freetags_3d_width'] = 500;
