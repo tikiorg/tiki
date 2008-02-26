b2c09af5745a83813f2020e4ee15ad3aa38e6eb2
lphuberdeau <lphuberdeau@users.sourceforge.net>
2008-02-26 16:33:43 +0000

    [FIX] Appropriate permission checks.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@11477 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/tiki-freetag_translate.php
+++ tiki/tiki-freetag_translate.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-freetag_translate.php,v 1.1.2.4 2008-02-26 16:24:26 lphuberdeau Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-freetag_translate.php,v 1.1.2.5 2008-02-26 16:33:43 lphuberdeau Exp $
 
 // Based on tiki-galleries.php
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
@@ -31,7 +31,7 @@ if ($prefs['feature_multilingual'] != 'y') {
 	die;
 }
 
-if (false &&$tiki_p_admin_freetags != 'y') {
+if ($tiki_p_freetags_tag != 'y') {
 	$smarty->assign('msg', tra("You do not have permission to use this feature"));
 
 	$smarty->display("error.tpl");
@@ -56,7 +56,15 @@ if ($cat_type != 'wiki page') {
 include_once "lib/freetag/freetaglib.php";
 include_once "lib/multilingual/multilinguallib.php";
 
-if ( $cat_objId ) $info = $tikilib->get_page_info( $cat_objId );
+if ( $cat_objId ) {
+	$info = $tikilib->get_page_info( $cat_objId );
+} elseif ($tiki_p_admin_freetags != 'y') {
+	// Global tag edit only available to admins
+	$smarty->assign('msg', tra("You do not have permission to use this feature"));
+
+	$smarty->display("error.tpl");
+	die;
+}
 
 $smarty->assign( 'type', $cat_type );
 $smarty->assign( 'objId', $cat_objId );
