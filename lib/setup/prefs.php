66ea6ddd927ee52383bd83910dddee803f09dc2a
nyloth <nyloth@users.sourceforge.net>
2008-02-29 08:12:07 +0000

    [ENH] New pref 'javascript_enabled' that will be automatically filled to 'y' or 'n' depending on weither JavaScript has been detected on client browser. This allows to remove NOSCRIPT tags and only return the correct code (with javascript or not) to the user's browser). Note that if javascript is detected, it will consider it is enabled during the whole session (e.g. until the user log off).
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10/tiki@11548 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/prefs.php
+++ lib/setup/prefs.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/lib/setup/prefs.php,v 1.16.2.46 2008-02-27 15:18:47 nyloth Exp $
+// $Header: /cvsroot/tikiwiki/tiki/lib/setup/prefs.php,v 1.16.2.47 2008-02-29 08:12:07 nyloth Exp $
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for
@@ -1043,6 +1043,13 @@ if ( ! $_SESSION['need_reload_prefs'] ) {
 
 	// Assign prefs to the session
 	$_SESSION['s_prefs'] = $prefs;
+}
+
+// Javascript auto-detection
+if ( isset($_SESSION['tiki_cookie_jar']) && isset($_SESSION['tiki_cookie_jar']['javascript_enabled']) ) {
+	$prefs['javascript_enabled'] = $_SESSION['tiki_cookie_jar']['javascript_enabled'];
+} else {
+	$prefs['javascript_enabled'] = 'n';
 }
 
 // Assign the prefs array in smarty, by reference
