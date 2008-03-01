75265d19904e5ff5ffb11d3d66765338c113de68
lphuberdeau <lphuberdeau@users.sourceforge.net>
2008-03-01 22:06:06 +0000

    [MOD] Removing unused feature
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@11585 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki/lib/setup/prefs.php
+++ tiki/lib/setup/prefs.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/lib/setup/prefs.php,v 1.16.2.48 2008-03-01 17:13:01 lphuberdeau Exp $
+// $Header: /cvsroot/tikiwiki/tiki/lib/setup/prefs.php,v 1.16.2.49 2008-03-01 22:06:06 lphuberdeau Exp $
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for
@@ -530,7 +530,6 @@ if ( ! $_SESSION['need_reload_prefs'] ) {
 	$prefs['calendar_view_tab'] = 'n';
 	$prefs['calendar_firstDayofWeek'] = 'user';
 	$prefs['calendar_timespan'] = '5';
-	$prefs['feature_cal_manual_time'] = '0';
 	$prefs['feature_jscalendar'] = 'n';
 	$prefs['feature_action_calendar'] = 'n';
 	$prefs['calendar_start_year'] = '+0';
