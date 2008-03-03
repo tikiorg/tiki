f444a550f093e2bcfaaf21f28836724a3b21cc66
nyloth <nyloth@users.sourceforge.net>
2008-03-03 20:16:13 +0000

    [FIX] filegals: Fix files archives panel that was not displaying columns anymore since fgal revamp + keep only archives table to have a lighter interface / code
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10/tiki@11688 b456876b-0849-0410-b77d-98878d47e9d5

--- templates/tiki-file_archives.tpl
+++ templates/tiki-file_archives.tpl
@@ -1,15 +1,16 @@
-{* $Header: /cvsroot/tikiwiki/tiki/templates/tiki-file_archives.tpl,v 1.11 2007-10-04 22:17:40 nyloth Exp $ *}
+{* $Header: /cvsroot/tikiwiki/tiki/templates/tiki-file_archives.tpl,v 1.11.2.1 2008-03-03 20:16:13 nyloth Exp $ *}
+{popup_init src="lib/overlib.js"}
 
 <h1><a class="pagetitle" href="tiki-file_archives.php?fileId={$file_info.fileId}">{tr}File Archive{/tr}: {if empty($file_info.name)}{$file_info.filename|escape}{else}{$file_info.name}{/if}</a></h1>
 
-<table><tr>
-<td style="vertical-align:top;">
+{if $tiki_p_list_file_galleries eq 'y' or ( ! isset($tiki_p_list_file_galleries) and $tiki_p_view_file_gallery eq 'y' )}
+	<a href="tiki-list_file_gallery.php" class="linkbut" title="{tr}List Galleries{/tr}">{tr}List Galleries{/tr}</a>
+{/if}
 
-{if $tiki_p_list_file_galleries eq 'y' or (!isset($tiki_p_list_file_galleries) and $tiki_p_view_file_gallery eq 'y')}<a href="tiki-file_galleries.php" class="linkbut" title="{tr}List Galleries{/tr}">{tr}List Galleries{/tr}</a>{/if}
 <a href="tiki-list_file_gallery.php?galleryId={$gal_info.galleryId}" class="linkbut" title="{tr}List Gallery{/tr}">{tr}List Gallery{/tr}</a>
 
 {if $tiki_p_admin_file_galleries eq 'y' or $user eq $gal_info.user}
-  <a href="tiki-file_galleries.php?edit_mode=1&amp;galleryId={$gal_info.galleryId}" class="linkbut" title="{tr}Edit Gallery{/tr}">{tr}Edit Gallery{/tr}</a>
+  <a href="tiki-list_file_gallery.php?edit_mode=1&amp;galleryId={$gal_info.galleryId}" class="linkbut" title="{tr}Edit Gallery{/tr}">{tr}Edit Gallery{/tr}</a>
 {/if}
 
 {if $tiki_p_admin_file_galleries eq 'y' or $user eq $gal_info.user or $gal_info.public eq 'y'}
@@ -20,10 +21,6 @@
     <a href="tiki-batch_upload_files.php?galleryId={$gal_info.galleryId}" class="linkbut">{tr}Directory batch{/tr}</a>
   {/if}
 {/if}
-</td></tr></table>
 
-<h2>{tr}File{/tr}</h2>
-{include file=list_file_gallery.tpl files=$file show_find='n' maxRecords=0 gal_info=$file_gal_info}
-
-<h2>{tr}Archives{/tr}</h2>
-{include file=list_file_gallery.tpl ext='file_' sort_mode=$file_sort_mode offset=$file_offset prev_offset=$file_prev_offset next_offset=$file_next_offset find=$file_find}
+<br /><br />
+{include file=list_file_gallery.tpl}
--- tiki-file_archives.php
+++ tiki-file_archives.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/tikiwiki/tiki/tiki-file_archives.php,v 1.9.2.1 2007-11-04 22:08:04 nyloth Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-file_archives.php,v 1.9.2.2 2008-03-03 20:16:13 nyloth Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -30,6 +30,8 @@ if (!($tiki_p_admin_file_galleries == 'y' || $tiki_p_view_file_gallery == 'y')) 
 	die;
 }
 
+$auto_query_args = array('fileId','offset','find','sort_mode','filegals_manager','maxRecords');
+
 if (!empty($_REQUEST['remove'])) {
 	check_ticket('list-archives');
 	if (!($removeInfo = $filegallib->get_file_info($_REQUEST['remove']))) {
@@ -44,15 +46,15 @@ if (!empty($_REQUEST['remove'])) {
 	}
 	$area = 'delfile';
 	if ($prefs['feature_ticketlib2'] != 'y' or (isset($_POST['daconfirm']) and isset($_SESSION["ticket_$area"]))) {
-    	key_check($area);
+		key_check($area);
 		$filegallib->remove_file($removeInfo, $user, $gal_info);
 	} else {
-	  key_get($area, ($removeInfo['archiveId']? tra('Remove archive: '): tra('Remove file gallery: ')).(!empty($removeInfo['name'])?$removeInfo['name'].' - ':'').$removeInfo['filename']);
+		key_get($area, ($removeInfo['archiveId']? tra('Remove archive: '): tra('Remove file gallery: ')).(!empty($removeInfo['name'])?$removeInfo['name'].' - ':'').$removeInfo['filename']);
 	}
 }
 if (isset($_REQUEST['delsel_x']) && !empty($_REQUEST['file'])) {
 	check_ticket('list-archives');
-	foreach (array_values($_REQUEST['file'])as $fileId) {
+	foreach (array_values($_REQUEST['file']) as $fileId) {
 		if (!($removeInfo = $filegallib->get_file_info($fileId))) {
 			$smarty->assign('msg', tra("Incorrect param"));
 			$smarty->display('error.tpl');
@@ -62,48 +64,49 @@ if (isset($_REQUEST['delsel_x']) && !empty($_REQUEST['file'])) {
 	}
 }
 
-if (!isset($_REQUEST['file_sort_mode']))
-	$_REQUEST['file_sort_mode'] = 'created_desc';
-$smarty->assign_by_ref('file_sort_mode', $_REQUEST['file_sort_mode']);
-if (!isset($_REQUEST['file_find']))
-	$_REQUEST['file_find'] = '';
-$smarty->assign('file_find', $_REQUEST['file_find']);
-if (!isset($_REQUEST['file_offset']))
-	$_REQUEST['file_offset'] = 0;
-$smarty->assign_by_ref('file_offset', $_REQUEST['file_offset']);
+// Set display config
+if ( ! isset($_REQUEST['maxRecords']) || $_REQUEST['maxRecords'] <= 0 ) {
+	$_REQUEST['maxRecords'] = $prefs['maxRecords'];
+}
+$smarty->assign_by_ref('maxRecords', $_REQUEST['maxRecords']);
 
-$files = $filegallib->get_archives($_REQUEST['fileId'], $_REQUEST['file_offset'], $maxRecords, $_REQUEST['file_sort_mode'], $_REQUEST['file_find']);
+
+if ( ! isset($_REQUEST['offset'])) $_REQUEST['offset'] = 0;
+$smarty->assign_by_ref('offset', $_REQUEST['offset']);
+
+if ( ! isset($_REQUEST['sort_mode']) ) $_REQUEST['sort_mode'] = 'lastmodif_desc';
+$smarty->assign_by_ref('sort_mode', $_REQUEST['sort_mode']);
+
+if ( ! isset($_REQUEST['find']) ) $_REQUEST['find'] = '';
+$smarty->assign('find', $_REQUEST['find']);
+
+$files = $filegallib->get_archives($_REQUEST['fileId'], $_REQUEST['offset'], $_REQUEST['maxRecords'], $_REQUEST['sort_mode'], $_REQUEST['find']);
+
+$file = array($fileInfo);
 $smarty->assign_by_ref('files', $files['data']);
-$file[] = $fileInfo;
-$smarty->assign_by_ref('file',$file); 
-
-$cant_pages = ceil($files['cant'] / $maxRecords);
-$smarty->assign_by_ref('cant_pages', $cant_pages);
-$smarty->assign_by_ref('maxRecords', $maxRecords);
-$smarty->assign('actual_page', 1 + ($file_offset / $maxRecords));
-if ($files["cant"] > ($file_offset + $maxRecords)) {
-	$smarty->assign('file_next_offset', $file_offset + $maxRecords);
-} else {
-	$smarty->assign('file_next_offset', -1);
-}
-if ($file_offset > 0) {
-	$smarty->assign('file_prev_offset', $file_offset - $maxRecords);
-} else {
-	$smarty->assign('file_prev_offset', -1);
-}
-
+$smarty->assign_by_ref('file', $file); 
+$smarty->assign_by_ref('cant', $files['cant']);
 $smarty->assign_by_ref('file_info', $fileInfo);
-$gal_info['show_checked'] = 'n';
-$gal_info['show_created'] = 'n';
-$gal_info['show_modified'] = 'y';
-$gal_info['show_comment'] = 'y';
-$smarty->assign('file_gal_info', $gal_info);
-unset($gal_info['show_checked']);
-$gal_info['show_comment'] = 'y';
-$gal_info['show_description'] = 'n';
-$gal_info['show_created'] = 'n';
-$gal_info['show_modified'] = 'y';
-$gal_info['show_lockedby'] = 'n';
+
+$gal_info = array_merge($gal_info, array(
+	'show_id' => 'n',
+	'show_icon' => 'y',
+	'show_name' => 'f',
+	'show_description' => 'o',
+	'show_size' => 'o',
+	'show_created' => 'o',
+	'show_modified' => 'y',
+	'show_creator' => 'o',
+	'show_author' => 'o',
+	'show_last_user' => 'o',
+	'show_comment' => 'y',
+	'show_files' => 'n',
+	'show_hits' => 'n',
+	'show_lockedby' => 'n',
+	'show_checked' => 'y',
+	'show_userlink' => 'y',
+//	'show_checked' = 'n'
+));
 $smarty->assign_by_ref('gal_info', $gal_info);
 
 $section = 'file_galleries';
@@ -115,6 +118,9 @@ if ($prefs['feature_theme_control'] == 'y') {
 	include ('tiki-tc.php');
 }
 ask_ticket('list-archives');
+
+// Get listing display config
+include_once('fgal_listing_conf.php');
 
 $smarty->assign('mid', 'tiki-file_archives.tpl');
 $smarty->display("tiki.tpl");
