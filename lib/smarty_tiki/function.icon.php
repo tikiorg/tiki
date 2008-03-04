aa4977974bea28d618449fb09adde6312be6e608
nyloth <nyloth@users.sourceforge.net>
2008-03-04 22:31:20 +0000

    [ENH] smarty icon function is now able to return input tags of type image, instead of img tags only, by using _tag='input_image' + new _confirm param to specify a text to use in a popup requesting the user to confirm it's action (yet only available with javascript)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10/tiki@11739 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.icon.php
+++ lib/smarty_tiki/function.icon.php
@@ -9,24 +9,28 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 /*
  * smarty_function_icon: Display a Tikiwiki icon, using theme icons if they exists
  *
- * params will be used as params for the IMG tag (e.g. border, class, ...), except special params starting with '_' :
+ * params will be used as params for the HTML tag (e.g. border, class, ...), except special params starting with '_' :
  *  - _id: short name (i.e. 'page_edit') or relative file path (i.e. 'pics/icons/page_edit.png'). [required]
  *  - _type: type of URL to use (e.g. 'absolute_uri', 'absolute_path'). Defaults to a relative URL.
+ *  - _tag: type of HTML tag to use (e.g. 'img', 'input_image'). Defaults to 'img' tag.
  *  - _notag: if set to 'y', will only return the URL (which also handles theme icons).
  *  - _menu_text: if set to 'y', will use the 'title' argument as text after the icon and place the whole content between div tags with a 'icon_menu' class (not compatible with '_notag' param set to 'y').
  *  - _menu_icon: if set to 'n', will not show icon image when _menu_text is 'y'.
+ *  - _confirm: text to use in a popup requesting the user to confirm it's action (yet only available with javascript)
  */
 function smarty_function_icon($params, &$smarty) {
   if ( ! is_array($params) || ! isset($params['_id']) ) return;
 
   $basedirs = array('pics/icons', 'images', 'img/icons', 'pics/icons/mime');
   $icons_extension = '.png';
+  $tag = 'img';
   $notag = false;
   $default_class = 'icon';
   $default_width = 16;
   $default_height = 16;
   $menu_text = false;
   $menu_icon = true;
+  $confirm = '';
   $html = '';
 
   // Handle _ids that contains the real filename and path
@@ -41,7 +45,7 @@ function smarty_function_icon($params, &$smarty) {
 
   if ( ! eregi('^[a-z0-9_]+$', $params['_id']) ) return;
 
-  global $smarty, $style_base, $tikidomain, $tikipath, $url_path, $base_url;
+  global $smarty, $prefs, $style_base, $tikidomain, $tikipath, $url_path, $base_url;
 
   // Include smarty functions used below
   require_once $smarty->_get_plugin_filepath('function', 'html_image');
@@ -87,6 +91,14 @@ function smarty_function_icon($params, &$smarty) {
           $menu_text = ($v == 'y');
           $menu_icon = ( ! isset($params['_menu_icon']) || $params['_menu_icon'] == 'y' );
           break;
+        case '_tag':
+          $tag = $v;
+          break;
+        case '_confirm':
+          if ( $prefs['javascript_enabled'] == 'y' ) {
+            $params['onclick'] = "return confirm('".str_replace("'", "\'", $v)."');";
+          }
+          break;
       }
       unset($params[$k]);
     }
@@ -98,8 +110,10 @@ function smarty_function_icon($params, &$smarty) {
     $params['basedir'] = $tikipath;
     $params['file'] = '/'.$params['file'];
   }
-  if ( ! isset($params['width']) ) $params['width'] = $default_width;
-  if ( ! isset($params['height']) ) $params['height'] = $default_height;
+  if ( $tag == 'img' ) {
+    if ( ! isset($params['width']) ) $params['width'] = $default_width;
+    if ( ! isset($params['height']) ) $params['height'] = $default_height;
+  }
 
   if ( $notag ) {
     $html = $params['path_prefix'].$params['file'];
@@ -121,7 +135,23 @@ function smarty_function_icon($params, &$smarty) {
       unset($params['title']);
     }
 
-    if ( $menu_icon ) $html = smarty_function_html_image($params, $smarty);
+    if ( $menu_icon ) {
+      if ( $tag != 'img' ) {
+        $params['src'] = $params['file'];
+        unset($params['file']);
+        foreach ( $params as $k => $v ) {
+          $html .= ' '.htmlspecialchars($k, ENT_QUOTES, 'UTF-8').'="'.htmlspecialchars($v, ENT_QUOTES, 'UTF-8').'"';
+        }
+      }
+      switch ( $tag ) {
+        case 'input_image': $html = '<input type="image"'.$html.' />'; break;
+        case 'img': default: $html = smarty_function_html_image($params, $smarty);
+      }
+      if ( $tag != 'img' ) {
+        // Add a span tag to be able to apply a CSS style on hover for the icon
+        $html = "<span>$html</span>";
+      }
+    }
     if ( $menu_text ) $html = '<div class="iconmenu">'.$html.'<span class="iconmenutext"> '.$menu_text_val.'</span></div>';
 
   }
