59d5e455b0996a714acab266941f35ad2ac6258b
sylvieg <sylvieg@users.sourceforge.net>
2008-03-10 16:26:29 +0000

    [MOD]perm: new perm tiki_p_clean_cache to allow editor to clean cache
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10/tiki@11814 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki_1.9to1.10.sql
+++ db/tiki_1.9to1.10.sql
@@ -1,4 +1,4 @@
-# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.221.2.40 2008-03-06 19:45:42 sampaioprimo Exp $
+# $Header: /cvsroot/tikiwiki/tiki/db/tiki_1.9to1.10.sql,v 1.221.2.41 2008-03-10 16:26:29 sylvieg Exp $
 
 # The following script will update a tiki database from version 1.9 to 1.10
 # 
@@ -1741,3 +1741,8 @@ INSERT INTO users_permissions (permName, permDesc, level, type) SELECT 'tiki_p_s
 INSERT INTO `users_grouppermissions` (groupName,permName) SELECT 'Anonymous','tiki_p_search' FROM `users_grouppermissions` WHERE @fgcant = 0;
 INSERT INTO `users_grouppermissions` (groupName,permName) SELECT 'Registered','tiki_p_search' FROM `users_grouppermissions` WHERE @fgcant = 0;
 
+#2008-03-10
+INSERT INTO users_permissions (permName, permDesc, level, type) VALUES('tiki_p_clean_cache', 'Can clean cache', 'editors', 'tiki');
+UPDATE tiki_menu_options set perm='tiki_p_clean_cache' WHERE url='tiki-admin_system.php';
+DELETE FROM `tiki_menu_options` WHERE menuId='42' and type='r' and name='Admin' and url='tiki-admin_system.php' and position='1050' and perm='tiki_p_admin' and groupname='' ;
+INSERT INTO tiki_menu_options (menuId,type,name,url,position,section,perm,groupname) VALUES (42,'r','Admin','tiki-admin.php',1050,'','tiki_p_clean_cache','');
