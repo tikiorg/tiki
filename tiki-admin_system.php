b5755cae6163e9696b4100dcfcefcfb9a7483267
sylvieg <sylvieg@users.sourceforge.net>
2008-03-24 14:53:23 +0000

    [MOD] list in admin system the list of directories to save to help backup procedure: some directories could be still missing
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10/tiki@12159 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_system.php
+++ tiki-admin_system.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_system.php,v 1.28.2.4 2008-03-19 17:59:07 sylvieg Exp $
+// $Header: /cvsroot/tikiwiki/tiki/tiki-admin_system.php,v 1.28.2.5 2008/03/24 14:51:10 sylvieg Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
@@ -87,6 +87,37 @@ if(isset($_REQUEST['utf8ft'])) {
    $smarty->assign('utf8ft',$_REQUEST['utf8ft']);
    $smarty->assign('utf8ff',$_REQUEST['utf8ff']);
 }
+
+if ($prefs['feature_forums'] == 'y') {
+	include_once('lib/commentslib.php');$commentslib = new Comments($dbTiki);
+	$dirs = $commentslib->list_directories_to_save();
+} else {
+	$dirs = array();
+}
+if ($prefs['feature_galleries'] == 'y' && !empty($prefs['gal_use_dir'])) {
+	$dirs[] = $prefs['gal_use_dir'];
+}
+if ($prefs['feature_file_galleries'] == 'y' && !empty($prefs['fgal_use_dir'])) {
+	$dirs[] = $prefs['fgal_use_dir'];
+}
+if ($prefs['feature_trackers'] == 'y') {
+	if (!empty($prefs['t_use_dir']))
+		$dirs[] = $prefs['t_use_dir'];
+	$dirs[] = 'img/trackers';
+}
+if ($prefs['feature_wiki'] == 'y') {
+	if (!empty($prefs['w_use_dir']))
+		$dirs[] = $prefs['w_use_dir'];
+	if ($prefs['feature_create_webhelp'] == 'y') 
+		$dirs[] = 'whelp';
+	$dirs[] = 'img/wiki';
+	$dirs[] = 'img/wiki_up';
+}
+if ($prefs['feature_maps'] && !empty($prefs['map_path'])) {
+	$dirs[] = $prefs['map_path'];
+}
+$dirs = array_unique($dirs);
+$smarty->assign_by_ref('dirs', $dirs);
 
 // disallow robots to index page:
 $smarty->assign('metatag_robots', 'NOINDEX, NOFOLLOW');
