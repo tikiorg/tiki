9acb506f1b540a5f88f0510e57ce2f03d24823ba
luciash <luciash@users.sourceforge.net>
2008-03-28 00:42:18 +0000

    will Header work as keyword too ?
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@12212 b456876b-0849-0410-b77d-98878d47e9d5

--- tests/index.php
+++ tests/index.php
@@ -2,7 +2,8 @@
 
 // $Header: /cvsroot/tikiwiki/tiki/tests/index.php,v 1.6 2007-03-06 19:30:45 sylvieg Exp $
 
-// $Id$
+// $Id:index.php 12211 2008-03-28 00:40:39Z luciash $
+// $URL$
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
