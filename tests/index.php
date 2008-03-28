f189d7002b7ffb2064cfef9426cb9de2ec174c4c
luciash <luciash@users.sourceforge.net>
2008-03-28 02:08:43 +0000

    ok, enough testing...
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@12214 b456876b-0849-0410-b77d-98878d47e9d5

--- tests/index.php
+++ tests/index.php
@@ -1,12 +1,10 @@
 <?php
-// $Id$
+/** This is TikiWiki CMS/Groupware by the Tiki Community -- LGPL licensed Free / Open Source Software.
+ * Copyright © 2002–2008 Luis Argerich, Garland Foster, Eduardo Polidor and many others.
+ * See the README file for details... */
 
-// This is a part of TikiWiki CMS/Groupware -- LGPL licensed Free / Open Source Software
-// Copyright © 2002-2008 Luis Argerich, Garland Foster, Eduardo Polidor and many others
-// See the README file for details...
-
-/// just redirect to the site's root to prevent directory browsing
-
+// File $Id$
+// just redirects to the site's root to prevent directory browsing
 header ("location: ../tiki-index.php");
 die;
 ?>
