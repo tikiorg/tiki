560f2d900ed756ab95148d60083e9036038157aa
luciash <luciash@users.sourceforge.net>
2008-03-28 00:36:43 +0000

    test commit with Id property set via Subclipse (should be same as command svn propset svn:keywords "Id" tests/index.php)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@12210 b456876b-0849-0410-b77d-98878d47e9d5

--- tests/index.php
+++ tests/index.php
@@ -2,6 +2,8 @@
 
 // $Header: /cvsroot/tikiwiki/tiki/tests/index.php,v 1.6 2007-03-06 19:30:45 sylvieg Exp $
 
+// $Id$
+
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
