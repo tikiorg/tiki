4595df54ea5f69ba2439ba62a661db14ac7531bb
luciash <luciash@users.sourceforge.net>
2008-03-30 15:03:01 +0000

    [MOD] $Header: $ CVS keyword is deprecated, use $Id: $ instead
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@12260 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/tiki-installer.php
+++ installer/tiki-installer.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/installer/tiki-installer.php,v 1.22.2.13 2008/03/24 20:53:40 sylvieg Exp $
+// $Id$
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
