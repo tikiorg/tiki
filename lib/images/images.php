a42ada2bc9b7a7b0e04dadfe5801af6f214516c7
nyloth <nyloth@users.sourceforge.net>
2008-04-03 12:38:36 +0000

    [ENH] fgal images lib: auto-detect best installed lib instead of looking at imagegals prefs
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@12336 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/images/images.php
+++ lib/images/images.php
@@ -1,34 +1,25 @@
 <?php
-/* Select the right library for images manipulation */
-/* we choose from :
-/*  o gd
-/*  o imagemagick
-*/
+/* Select the right library for images manipulation
+ * We handle gd and imagemagick 1.x and 2.x
+ */
 require_once('tiki-setup.php');
 
-$extensions = get_loaded_extensions();
-$prefered_lib = $prefs['gal_use_lib'];
-
-switch ( $prefered_lib ) {
-  case 'imagick':
-    if ( in_array('imagick', $extensions ) ) {
-     $funcs = get_extension_funcs('imagick');
-       if ( in_array('imagick_blob2image', $funcs) ) {
-         require_once('lib/images/imagick_old.php');
-         break;
-       } else {
-         require_once('lib/images/imagick_new.php');
-         break;
-       }
-    };
-  case 'gd':
-    if ( in_array('gd', $extensions ) ) {
-       require_once('lib/images/gd.php');
-       break;
-    };
-  default:
-    $smarty->assign('msg', tra('No graphic library found.'));
-    $smarty->display('error.tpl');
-    die();
+// Auto-detect php extension to use as image lib
+//   This assumes imagick is better than gd (which does not handle transparency in gray PNG), so try to find it first
+//
+if ( class_exists('Imagick') ) {
+	$detected_lib = 'imagick_new'; // Imagick 2.x
+} elseif ( function_exists('imagick_rotate') ) {
+	$detected_lib = 'imagick_old'; // Imagick 1.x
+} elseif ( function_exists('gd_info') ) {
+	$detected_lib = 'gd'; // Imagick 1.x
+} else {
+	$smarty->assign('msg', tra('No graphic library found.'));
+	$smarty->display('error.tpl');
+	die();
 }
+
+// Load the detected lib
+require_once('lib/images/'.$detected_lib.'.php');
+
 ?>
