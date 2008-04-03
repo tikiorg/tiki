2bdd44e18dbd261446628ab31510e2ee973566ae
nyloth <nyloth@users.sourceforge.net>
2008-04-03 13:32:47 +0000

    [FIX] fix svn Id
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@12342 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-list_file_gallery.php
+++ tiki-list_file_gallery.php
@@ -1,6 +1,6 @@
 <?php
 
-// $Header: /cvsroot/tikiwiki/tiki/tiki-list_file_gallery.php,v 1.50.2.14 2008-03-16 00:06:53 nyloth Exp $
+// $Id: /cvsroot/tikiwiki/tiki/tiki-list_file_gallery.php,v 1.50.2.14 2008-03-16 00:06:53 nyloth Exp $
 
 // Copyright (c) 2002-2007, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
