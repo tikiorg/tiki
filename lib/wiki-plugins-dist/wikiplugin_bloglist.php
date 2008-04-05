54232b3b7d9c4ee947518f7d02d76b284aab8ff2
gregmartin <gregmartin@users.sourceforge.net>
2008-04-05 03:40:37 +0000

    Added option to limit the number of blog entries displayed.  Updated help to reflect changes.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@12380 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins-dist/wikiplugin_bloglist.php
+++ lib/wiki-plugins-dist/wikiplugin_bloglist.php
@@ -5,7 +5,7 @@
 // {BLOGLIST(Id=>blogId)}{BLOGLIST}
 // FieldName can be any field in the tiki_articles table, but title,heading, or body are probably the most useful.
 function wikiplugin_bloglist_help() {
-	return tra("Use BLOGLIST to include all posts from a blog. Syntax is").":<br />~np~{BLOGLIST(Id=n)}{BLOGLIST}~/np~";
+	return tra("Use BLOGLIST to include posts from a blog. Syntax is").":<br />~np~{BLOGLIST(Id=n, Items=n)}{BLOGLIST}~/np~<br />" . tra(" where Id is the blog Id and Items is the max number of posts to display"). "<br />" . tra("Ex: ~np~{BLOGLIST(Id=2, Items=15)}{BLOGLIST}~/np~");
 }
 function wikiplugin_bloglist($data, $params) {
 	global $tikilib;
@@ -23,10 +23,12 @@ function wikiplugin_bloglist($data, $params) {
 	$text="<div class=\"blogtools\"><table><tr><th>" . tra("Date") . "</th><th>" . tra("Title") . "</th><th>" . tra("Author") . "</th></tr>\n";
 	$query = "select `postId`, `title`, `user`, `created`  from `tiki_blog_posts` where `blogId`=? order by `created` desc";
 	$result = $tikilib->query($query, array($Id));
-    	while ($res = $result->fetchRow()) {
+	$i=0;
+    	while (($res = $result->fetchRow()) && ($i < $Items)) {
         	$text.="<tr><td>" . TikiLib::date_format("%d/%M/%Y %H:%M", $res["created"]) . "</td>";
 		$text.="<td><a href=\"tiki-view_blog_post.php?blogId=" . $Id . "&postId=" . $res["postId"] . "\">" . $res["title"] . "</a></td>";
         	$text.= "<td>" . $res["user"] . "</td></tr>\n";
+		$i++;
   	}
 	$text.="</table></div>\n";
 
