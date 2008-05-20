6b8041a55727e35fe7c8f6bc052f4bbadc0b636d
sylvieg <sylvieg@users.sourceforge.net>
2008-05-20 20:05:12 +0000

    notice
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@12903 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/prefs.php
+++ lib/setup/prefs.php
@@ -157,6 +157,7 @@ if ( ! $_SESSION['need_reload_prefs'] ) {
 	$prefs['wikiapproval_delete_staging'] = 'n';
 	$prefs['wikiapproval_master_group'] = '';
 	$prefs['wiki-edit_section'] = 'n';
+	$prefs['wiki_edit_section'] = 'n';
 
 	# wysiwyg
 	$prefs['feature_wysiwyg'] = 'n';
