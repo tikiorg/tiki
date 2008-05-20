b28aa938718579bb07fb7c8d9de88199aff34315
sylvieg <sylvieg@users.sourceforge.net>
2008-05-20 20:09:21 +0000

    [MOD]articles: to be able to call the module in tpl
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@12904 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-articles.php
+++ modules/mod-articles.php
@@ -1,10 +1,10 @@
 <?php
-
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   header("location: index.php");
   exit;
 }
+global $smarty, $tikilib, $user, $module_rows;
 
 $smarty->assign('module_title', isset($module_params["title"]) ? $module_params["title"] : tra("Articles"));
 
