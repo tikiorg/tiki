2d94dd9e745fd866cbbe0a13d4aa04827e402be3
pkdille <pkdille@users.sourceforge.net>
2008-05-30 20:00:51 +0000

    typo: change the help code in order to have something readable and translatable. Keep the same help.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@13067 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_versions.php
+++ lib/wiki-plugins/wikiplugin_versions.php
@@ -11,7 +11,15 @@
  * http://doc.tikiwiki.org/PluginVersions
  */
 function wikiplugin_versions_help() {
-	return tra("Split the text in parts visible only under some conditions").":<br />~np~{VERSIONS(nav=>y|n,title=>y|n,default=>)}".tra("This is the default text<br />---(version 3)-----------------------------<br />This is version 3 info<br />---(version 2)-----------------------------<br />This is version 2 info<br />---(version 1)-----------------------------<br />This is version 1 info")."{VERSIONS}~/np~";
+	return tra("Split the text in parts visible only under some conditions") . ":<br />"
+            . "~np~{VERSIONS(nav=>y|n,title=>y|n,default=>)}"
+            . tra("This is the default text") . "<br />"
+            . "---" . tra("(version 3)") . "-----------------------------" . "<br />" 
+            . tra("This is version 3 info") . "<br />"
+            . "---" . tra("(version 2)") . "-----------------------------" . "<br />"
+            . tra("This is version 2 info") . "<br />"
+            . "---" . tra("(version 1)") . "-----------------------------" . "<br />"
+            . tra("This is version 1 info") ."{VERSIONS}~/np~";
 }
 
 function wikiplugin_versions($data, $params) {
