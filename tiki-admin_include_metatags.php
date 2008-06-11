50c79b1121abc9732b641ec27c67b9ba4532c666
yonixxx <yonixxx@users.sourceforge.net>
2008-06-11 13:30:30 +0000

    [MOD] admin can add freetags in meta keywords
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@13189 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_include_metatags.php
+++ tiki-admin_include_metatags.php
@@ -16,6 +16,7 @@ if (isset($_REQUEST["metatags"])) {
 	simple_set_value('metatag_keywords');
 	simple_set_value('metatag_threadtitle');
 	simple_set_value('metatag_imagetitle');
+	simple_set_value('metatag_freetags');
 	simple_set_value('metatag_description');
 	simple_set_value('metatag_author');
 	simple_set_value('metatag_geoposition');
