6210ade7b92efdf7b82c70a93c097ced0cd4c80d
pkdille <pkdille@users.sourceforge.net>
2008-06-12 07:15:39 +0000

    [FIX] most popular freetags module: fix this modules which was broken
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@13214 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-freetags_most_popular.php
+++ modules/mod-freetags_most_popular.php
@@ -7,8 +7,8 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 }
 
 global $dbTiki;
-global $freetaglib;
-if (!isset($freetaglib) or !is_object($freetaglib)) { include_once 'lib/freetag/freetaglib.php'; }
+global $freetag;
+if (!isset($freetag) or !is_object($freetag)) { include_once 'lib/freetag/freetaglib.php'; }
 $most_popular_tags = $freetaglib->get_most_popular_tags('', 0, $module_rows);
 $smarty->assign('most_popular_tags', $most_popular_tags);
 $smarty->assign('type', isset($module_params["type"]) ? $module_params["type"] : 'list');
