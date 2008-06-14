bb0e8c5250ab5ec427b502a8e8070ec683c48001
rlpowell <rlpowell@users.sourceforge.net>
2008-06-14 19:34:19 +0000

    Extra carriage return removed.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@13255 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikilib.php
+++ lib/tikilib.php
@@ -5273,8 +5273,7 @@ function add_pageview() {
 		$attrib .= 'target="_blank" ';
 	if ($prefs['feature_wiki_ext_icon'] == 'y') {
 		$attrib .= 'class="wiki external" ';
-		$ext_icon = "<img border=\"0\" class=\"externallink\" src=\"img/icons/external_link.gif\" alt=\" 
-external link\" />";
+		$ext_icon = "<img border=\"0\" class=\"externallink\" src=\"img/icons/external_link.gif\" alt=\"external link\" />";
 	} else {
 		$attrib .= 'class="wiki" ';
 		$ext_icon = "";
