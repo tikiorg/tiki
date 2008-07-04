9c3353c5171d4aad966dbbdda37fa23ae4536af7
pkdille <pkdille@users.sourceforge.net>
2008-07-04 20:09:13 +0000

    [MOD] csscheck: add a comment to this file and add the stylesheet to analyze as a parameter
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/1.10@13438 b456876b-0849-0410-b77d-98878d47e9d5

--- doc/devtools/csscheck.sh
+++ doc/devtools/csscheck.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# grep -r '{$helpurl}' templates | sed -e "s/^.*{$helpurl}\([^\"']*\)[\"'].*$/\1/" | sort | uniq | awk '{print "(("$1"))~|~ ~|~"}' | tr "+" " "
-
-# grep -r '^[A-Za-z\.\#0-9].*{' ../styles/mose.css | sed -e "s/^\([A-Za-z0-9]+\).*{$/\1/" | grep -v "{" | sort | uniq
+# Provide a report on CSS classes used in a stylesheet 
+# Usage: ./csscheck.sh tikineat
 
 # remove { anything }
 # remove comments
@@ -11,7 +10,11 @@
 # remove whiltespace after
 # remove blank lines
 
-./stripbraces.pl ../styles/mose.css \
+# grep -r '{$helpurl}' templates | sed -e "s/^.*{$helpurl}\([^\"']*\)[\"'].*$/\1/" | sort | uniq | awk '{print "(("$1"))~|~ ~|~"}' | tr "+" " "
+
+# grep -r '^[A-Za-z\.\#0-9].*{' ../styles/mose.css | sed -e "s/^\([A-Za-z0-9]+\).*{$/\1/" | grep -v "{" | sort | uniq
+
+./stripbraces.pl ../../styles/$1.css \
 | ./stripcomments.pl \
 | sed -e "s/,[ 	]*/\n/g;" \
 | sed -e "s/^[ 	]*//g;" \
@@ -19,7 +22,6 @@
 | egrep -v "^[ 	]*$" \
 | sort \
 | uniq
-
 
 # grep -v "^[   ]*{.*}[         ]*$" \
 # ^.*;+[   ]*$ | ^[  ]*[{}][         ]*$
