a6bcfb614dbd6f23014744d93afe3796aa0f825b
sylvieg <sylvieg@users.sourceforge.net>
2008-07-15 21:02:11 +0000

    [FIX]wiki: when removing a page go to the base_url and not tiki-index othervise the default wikipage can never be deleted
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@13683 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-removepage.php
+++ tiki-removepage.php
@@ -66,7 +66,7 @@ if (isset($_REQUEST["remove"])) {
 	if (isset($_REQUEST["all"]) && $_REQUEST["all"] == 'on') {
 		$tikilib->remove_all_versions($_REQUEST["page"]);
 
-		header ("location: tiki-index.php");
+		header ("location: $base_url");
 		die;
 	} else {
 		if ($version == "last") {
