f02a4f307ed8c39fa2d71ba409701fe060c06ca6
sylvieg <sylvieg@users.sourceforge.net>
2008-07-17 21:43:41 +0000

    [FIX]calendar: can change title and desc in rss
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@13703 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-calendars_rss.php
+++ tiki-calendars_rss.php
@@ -21,7 +21,7 @@ if ($tiki_p_view_calendar != 'y') {
 	require_once ('tiki-rss_error.php');
 }
 
-$feed = "calendars";
+$feed = "calendar";
 $calendarIds=array();
 if (isset($_REQUEST["calendarIds"])) {
     $calendarIds = $_REQUEST["calendarIds"];
@@ -36,10 +36,8 @@ if (isset($_REQUEST["calendarIds"])) {
 $output = $rsslib->get_from_cache($uniqueid);
 
 if ($output["data"]=="EMPTY") {
-	$tmp = tra("Tiki RSS feed for calendars");
-	$title = (!empty($title_rss_calendars)) ? $title_rss_calendars : $tmp;
-	$tmp = tra("Upcoming events.");
-	$desc = (!empty($desc_rss_calendars)) ? $desc_rss_calendars : $tmp;
+	$title = tra("Tiki RSS feed for calendars");
+	$desc = tra("Upcoming events.");
 	$id = "calitemId";
 	$titleId = "name";
 	$descId = "body";
