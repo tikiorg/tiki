3a708cf2e040f06476172bf2ef7142d5a6765871
lphuberdeau <lphuberdeau@users.sourceforge.net>
2008-07-31 20:50:29 +0000

    [MOD] Adding priorDays parameter to allow viewing past/recent events
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@13994 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/calendar/calendarlib.php
+++ lib/calendar/calendarlib.php
@@ -586,7 +586,7 @@ class CalendarLib extends TikiLib {
 		return true;
 	}
 
-	function upcoming_events($maxrows = -1, $calendarId = 0, $maxDays = -1, $order = 'start_asc') {
+	function upcoming_events($maxrows = -1, $calendarId = 0, $maxDays = -1, $order = 'start_asc', $priorDays = 0) {
 		$cond = '';
 		$bindvars = array();
 		if(is_array($calendarId) && count($calendarId) > 0) {
@@ -595,12 +595,14 @@ class CalendarLib extends TikiLib {
 				$cond = $cond." or i.`calendarId` = ? ";
 			}
 			$cond = $cond.")";
-			$bindvars += $calendarId;
+			$bindvars = array_merge( $bindvars, $calendarId );
 		} elseif (!is_array($calendarId) and $calendarId > 0) {
 			$cond = $cond." and i.`calendarId` = ? ";
-			$bindvars += array($calendarId);
+			$bindvars[] = $calendarId;
 		}
-		$cond .= " and `end` >= (unix_timestamp(now()))";
+		$cond .= " and `end` >= (unix_timestamp(now()) - ?*3600*34)";
+		$bindvars[] = $priorDays;
+
 		if($maxDays > 0)
 		{
 			$maxSeconds = ($maxDays * 24 * 60 * 60);
