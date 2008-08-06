c865367826f8e9201cd1a260fec0bb1b9bcaf826
lphuberdeau <lphuberdeau@users.sourceforge.net>
2008-08-06 20:07:23 +0000

    [NEW] Allow creation of menus from profiles
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@14102 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/profilelib/installlib.php
+++ lib/profilelib/installlib.php
@@ -11,10 +11,12 @@ class Tiki_Profile_Installer
 		'category' => 'Tiki_Profile_InstallHandler_Category',
 		'file_gallery' => 'Tiki_Profile_InstallHandler_FileGallery',
 		'module' => 'Tiki_Profile_InstallHandler_Module',
+		'menu' => 'Tiki_Profile_InstallHandler_Menu',
 	);
 
 	private static $typeMap = array(
 		'wiki_page' => 'wiki page',
+		'file_gallery' => 'fgal',
 	);
 
 	public static function convertType( $type ) // {{{
@@ -810,6 +812,151 @@ class Tiki_Profile_InstallHandler_Module extends Tiki_Profile_InstallHandler // 
 		$this->obj->replaceReferences( $data );
 		
 		return $modlib->assign_module( 0, $data['name'], null, $data['position'], $data['order'], $data['cache'], $data['rows'], $data['groups'], $data['params'] );
+	}
+} // }}}
+
+class Tiki_Profile_InstallHandler_Menu extends Tiki_Profile_InstallHandler // {{{
+{
+	function getData()
+	{
+		if( $this->data )
+			return $this->data;
+
+		$defaults = array(
+			'description' => '',
+			'collapse' => 'collapsed',
+			'icon' => '',
+			'groups' => array(),
+			'items' => array(),
+			'cache' => 0,
+		);
+
+		$data = array_merge(
+			$defaults,
+			$this->obj->getData()
+		);
+
+		$data['groups'] = serialize( $data['groups'] );
+
+		$position = 0;
+		foreach( $data['items'] as &$item )
+			$this->fixItem( $item, $position );
+
+		$items = array();
+		$this->flatten( $data['items'], $items );
+		$data['items'] = $items;
+
+		return $this->data = $data;
+	}
+
+	function flatten( $entries, &$list ) // {{{
+	{
+		foreach( $entries as $item )
+		{
+			$children = $item['items'];
+			unset( $item['items'] );
+
+			$list[] = $item;
+			$this->flatten( $children, $list );
+		}
+	} // }}}
+
+	private function fixItem( &$item, &$position, $parent = null ) // {{{
+	{
+		$position += 10;
+
+		if( !isset( $item['name'] ) )
+			$item['name'] = 'Unspecified';
+		if( !isset( $item['url'] ) )
+			$item['url'] = 'tiki-index.php';
+		if( !isset( $item['section'] ) )
+			$item['section'] = null;
+		if( !isset( $item['level'] ) )
+			$item['level'] = 0;
+		if( ! isset( $item['permissions'] ) )
+			$item['permissions'] = array();
+		if( ! isset( $item['groups'] ) )
+			$item['groups'] = array();
+		if( ! isset( $item['items'] ) )
+			$item['items'] = array();
+
+		$item['position'] = $position;
+
+		if( $parent )
+		{
+			if( $parent['type'] == 's' )
+				$item['type'] == 1;
+			else
+				$item['type'] = $parent['type'] + 1;
+
+			$item['permissions'] = array_unique( 
+				array_merge( $parent['permissions'], $item['permissions'] ) );
+			$item['groups'] = array_unique( 
+				array_merge( $parent['groups'], $item['groups'] ) );
+		}
+		else
+			$item['type'] = 's';
+
+		foreach( $item['items'] as &$child )
+			$this->fixItem( $child, $position, $item );
+
+		foreach( $item['permissions'] as &$perm )
+			if( strpos( $perm, 'tiki_p_' ) !== 0 )
+				$perm = 'tiki_p_' . $perm;
+
+		if( count( $item['items'] ) == 0 )
+			$item['type'] = 'o';
+	} // }}}
+
+	function canInstall()
+	{
+		$data = $this->getData();
+		if( ! isset( $data['name'] ) )
+			return false;
+		if( count( $data['items'] ) == 0 )
+			return false;
+
+		return true;
+	}
+
+	function _install()
+	{
+		global $modlib, $menulib, $tikilib;
+		if( ! $modlib ) require_once 'lib/modules/modlib.php';
+		if( ! $menulib ) require_once 'lib/menubuilder/menulib.php';
+
+		$data = $this->getData();
+
+		$this->obj->replaceReferences( $data );
+		
+		$type = 'f';
+		if( $data['collapse'] == 'collapsed' )
+			$type = 'd';
+		elseif( $data['collapse'] == 'expanded' )
+			$type = 'e';
+
+		$menulib->replace_menu( 0, $data['name'], $data['description'], $type, $data['icon'] );
+		$result = $tikilib->query( "SELECT MAX(menuId) FROM tiki_menus" );
+		$menuId = reset( $result->fetchRow() );
+
+		foreach( $data['items'] as $item )
+			$menulib->replace_menu_option( $menuId, 0, $item['name'], $item['url'], $item['type'], $item['position'], $item['section'], implode( ',', $item['permissions'] ), implode( ',', $item['groups'] ), $item['level'] );
+
+
+		// Set as side menu if position and order are specified
+		if( isset( $data['position'], $data['order'] ) )
+		{
+			if( $data['position'] == 'left' )
+				$column = 'l';
+			else
+				$column = 'r';
+
+			$modlib->replace_user_module( "menu_$menuId", $data['name'], "{menu id=$menuId}" );
+			$modlib->assign_module( 0, "menu_$menuId", null, $column, $data['order'], $data['cache'], 10, $data['groups'], '' );
+		}
+
+		return $menuId;
+
 	}
 } // }}}
 
