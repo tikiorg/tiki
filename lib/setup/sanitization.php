268bac13dbdea7b8ef10dac9c1528d24b16e1dd0
nyloth <nyloth@users.sourceforge.net>
2008-08-09 14:54:33 +0000

    [SEC] XSS sanitization fixes + tuning to be less aggressive for inline CSS styles:   * don't disable 'style' HTML attribute because it was considered as too aggressive. Now it is only catched as HTML tag but alert() and url() are catched,   * move straight replacements handling into the while loop,   * fix XSS vulnerabilities using html comments, CDATA, link tags, ?xml tags, ?import tags, spaces in javascript functions names
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/2.0@14133 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/sanitization.php
+++ lib/setup/sanitization.php
@@ -13,56 +13,76 @@ $access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__));
  * http://quickwired.com/smallprojects/php_xss_filter_function.php
  */
 function RemoveXSS($val) {
+	static $ra_as_tag_only = NULL;
+	static $ra_as_attribute = NULL;
+	static $ra_as_content = NULL;
+	static $ra_javascript = NULL;
+
+	// now the only remaining whitespace attacks are \t, \n, and \r
+	if ( $ra_as_tag_only == NULL ) {
+		$ra_as_tag_only = array('style', 'script', 'embed', 'object', 'applet', 'meta', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'base', 'xml', 'import', 'link');
+		$ra_as_attribute = array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload','ondragdrop', 'dynsrc');
+		$ra_as_content = array('vbscript', 'expression', 'blink', 'mocha', 'livescript', 'url', 'alert');
+		$ra_javascript = array('javascript');
+///		$ra_style = array('style'); // Commented as it has been considered as a bit too aggressive
+	}
+
+	// keep replacing as long as the previous round replaced something
+	while ( RemoveXSSchars($val)
+		|| RemoveXSSregexp($ra_as_tag_only, $val, '(\<|\[\\\\xC0\]\[\\\\xBC\])\??')
+		|| RemoveXSSregexp($ra_as_attribute, $val)
+		|| RemoveXSSregexp($ra_as_content, $val, '[\.\\\\+\*\?\[\^\]\$\(\)\{\}\=\!\<\|\:;\-\/`#"\']', '(?!\s*[a-z0-9])', true)
+		|| RemoveXSSregexp($ra_javascript, $val, '', ':', true)
+///		|| RemoveXSSregexp($ra_style, $val, '[^a-z0-9]', '=') // Commented as it has been considered as a bit too aggressive
+	);
+
+	return $val;
+}
+
+function RemoveXSSchars(&$val) {
 	static $patterns = NULL;
 	static $replacements = NULL;
-	// remove all non-printable characters. CR(0a) and LF(0b) and TAB(9) are
-	// allowed this prevents some character re-spacing such as <java\0script>
-	// note that you have to handle splits with \n, \r, and \t later since they
-	// *are* allowed in some inputs
-	$val = preg_replace('/([\x00-\x08\x0b-\x0c\x0e-\x19])/', '', $val);
+	$val_before = $val;
+	$found = true;
 
-	// straight replacements, the user should never need these since they're
-	// normal characters this prevents like 
-	// <IMG SRC=&#X40&#X61&#X76&#X61&#X73&#X63&#X72&#X69&#X70&#X74&#X3A&#X61&#X6C&#X65&#X72&#X74&#X28&#X27&#X58&#X53&#X53&#X27&#X29>
-	// Calculate the search and replace patterns only once
-	if ($patterns == NULL or $replacements == NULL) {
+	if ( $patterns == NULL ) {
+		$patterns = array();
+		$replacements = array();
+
+		// remove all non-printable characters. CR(0a) and LF(0b) and TAB(9) are
+		// allowed this prevents some character re-spacing such as <java\0script>
+		// note that you have to handle splits with \n, \r, and \t later since they
+		// *are* allowed in some inputs
+		$patterns[] = '/([\x00-\x08\x0b-\x0c\x0e-\x19])/';
+		$replacements[] = '';
+
+		// straight replacements, the user should never need these since they're
+		// normal characters this prevents like 
+		// <IMG SRC=&#X40&#X61&#X76&#X61&#X73&#X63&#X72&#X69&#X70&#X74&#X3A&#X61&#X6C&#X65&#X72&#X74&#X28&#X27&#X58&#X53&#X53&#X27&#X29>
+		// Calculate the search and replace patterns only once
 		$search = 'abcdefghijklmnopqrstuvwxyz';
 		$search .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 		$search .= '1234567890!@#$%^&*()';
 		$search .= '~`";:?+/={}[]-_|\'\\';
-		$patterns = array();
-		$replacements = array();
 		for ($i = 0; $i < strlen($search); $i++) {
 			// ;? matches the ;, which is optional
-			// 0{0,7} matches any padded zeros,
+			// 0{0,8} matches any padded zeros,
 			// which are optional and go up to 8 chars
 			// &#x0040 @ search for the hex values
 			$patterns[] = '/(&#[xX]0{0,8}'.dechex(ord($search[$i])).';?)/i';
 			$replacements[] = $search[$i];
-			// &#00064 @ 0{0,7} matches '0' zero to seven times
+			// &#00064 @ 0{0,8} matches '0' zero to eight times
 			// with a ;
 			$patterns[] = '/(&#0{0,8}'.ord($search[$i]).';?)/';
 			$replacements[] = $search[$i];
 		}
 	}
 	$val = preg_replace($patterns, $replacements, $val);
-
-	// now the only remaining whitespace attacks are \t, \n, and \r
-	$ra_as_tag_only = array('style', 'script', 'embed', 'object', 'applet', 'meta', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'base');
-	$ra_as_attribute = array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload','ondragdrop', 'dynsrc');
-	$ra_as_content = array('vbscript', 'expression', 'xml', 'blink', 'link', 'mocha', 'livescript');
-	$ra_javascript = array('javascript');
-	$ra_style = array('style');
-
-	// keep replacing as long as the previous round replaced something
-	while ( RemoveXSSregexp($ra_as_tag_only, $val, '(\<|\[\\\\xC0\]\[\\\\xBC\])')
-		|| RemoveXSSregexp($ra_as_attribute, $val)
-		|| RemoveXSSregexp($ra_as_content, $val, '[\.\\\+\*\?\[\^\]\$\(\)\{\}\=\!\<\|\:;\-\/`#"\']', '(?![a-z0-9])')
-		|| RemoveXSSregexp($ra_javascript, $val, '', ':', true)
-		|| RemoveXSSregexp($ra_style, $val, '[^a-z0-9]', '=')
-	);
-
-	return $val;
+	if ($val_before == $val) {
+		// no replacements were made, so exit the loop
+		$found = false;
+	}	
+	return $found;
 }
 
 function RemoveXSSregexp(&$ra, &$val, $prefix = '', $suffix = '', $allow_spaces = false) {
@@ -72,16 +92,13 @@ function RemoveXSSregexp(&$ra, &$val, $prefix = '', $suffix = '', $allow_spaces 
 	$replacements = array();
 
 	$pattern_sep = '('
-		. '(&#[xX]0{0,8}([9ab]);)'
-		. '|'
-		. '|(&#0{0,8}([9|10|13]);)'
-		. ')*'
-		. '(?ms)(\/\*.*?\*\/)*'
-		. '\\\\?';
-
-	if ( $allow_spaces ) {
-		$pattern_sep = '\s*' . $pattern_sep . '\s*';
-	}
+		.'&#[xX]0{0,8}([9ab]);?'
+		.'|&#0{0,8}([9|10|13]);?'
+		.'|(?ms)(\/\*.*?\*\/|\<\!\-\-.*?\-\-\>)'
+		.'|(\<\!\[CDATA\[|\]\]\>)'
+		.'|\\\\?'
+		.( $allow_spaces ? '|\s' : '' )
+	.')*';
 
 	$pattern_start = '/';
 	if ( $prefix != '' ) {
@@ -121,6 +138,7 @@ function RemoveXSSregexp(&$ra, &$val, $prefix = '', $suffix = '', $allow_spaces 
 	if ($val_before == $val) {
 		// no replacements were made, so exit the loop
 		$found = false;
-	}	
+	}
+
 	return $found;
 }
