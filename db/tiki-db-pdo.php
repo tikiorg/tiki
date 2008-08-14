4bc6ff7d8d8ce98a6be6948bdcc75594e8339b9b
sept_7 <sept_7@users.sourceforge.net>
2008-08-14 15:19:07 +0000

    [FIX] fix PDO for MySQL and UTF-8
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@14253 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki-db-pdo.php
+++ db/tiki-db-pdo.php
@@ -28,9 +28,9 @@ $dbTiki->setAttribute(PDO::ATTR_CASE,PDO::CASE_NATURAL);
 $dbTiki->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
 $dbTiki->setAttribute(PDO::ATTR_ORACLE_NULLS,PDO::NULL_EMPTY_STRING);
 //$dbTiki->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY,false);
-//if ($dbTiki->getAttribute(PDO::ATTR_DRIVER_NAME) == 'mysql') {
-//	$dbTiki->setAttribute(array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-//}
+if ($dbTiki->getAttribute(PDO::ATTR_DRIVER_NAME) == 'mysql') {
+	$dbTiki->exec("SET CHARACTER SET utf8");
+}
 
 
 $pq = $dbTiki->prepare('select `login` from `users_users` limit 1');
