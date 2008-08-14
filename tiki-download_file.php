0ae14f47cfda7df5e136203d0d8d32b3e6b522cf
sept_7 <sept_7@users.sourceforge.net>
2008-08-14 14:08:43 +0000

    [FIX] pb with file galleries on the filesystem
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@14250 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-download_file.php
+++ tiki-download_file.php
@@ -7,38 +7,38 @@ require_once('tiki-setup.php');
 include_once('lib/filegals/filegallib.php');
 
 if ( $prefs['feature_file_galleries'] != 'y' ) {
-  $smarty->assign('msg', tra('This feature is disabled'));
-  $smarty->display('error.tpl');
-  die;
+	$smarty->assign('msg', tra('This feature is disabled'));
+	$smarty->display('error.tpl');
+	die;
 }
 @set_time_limit(0);
 
 /*
-Borrowed from http://php.net/manual/en/function.readfile.php#54295
-to come over the 2MB readfile() limitation
-*/
+	 Borrowed from http://php.net/manual/en/function.readfile.php#54295 to come
+	 over the 2MB readfile() limitation
+ */
 function readfile_chunked($filename,$retbytes=true) {
-   $chunksize = 1*(1024*1024); // how many bytes per chunk
-   $buffer = '';
-   $cnt =0;
-   $handle = fopen($filename, 'rb');
-   if ($handle === false) {
-       return false;
-   }
-   while (!feof($handle)) {
-       $buffer = fread($handle, $chunksize);
-       echo $buffer;
-       @ob_flush();
-       flush();
-       if ($retbytes) {
-           $cnt += strlen($buffer);
-       }
-   }
-   $status = fclose($handle);
-   if ($retbytes && $status) {
-       return $cnt; // return num. bytes delivered like readfile() does.
-   }
-   return $status;
+	$chunksize = 1*(1024*1024); // how many bytes per chunk
+	$buffer = '';
+	$cnt =0;
+	$handle = fopen($filename, 'rb');
+	if ($handle === false) {
+		return false;
+	}
+	while (!feof($handle)) {
+		$buffer = fread($handle, $chunksize);
+		echo $buffer;
+		@ob_flush();
+		flush();
+		if ($retbytes) {
+			$cnt += strlen($buffer);
+		}
+	}
+	$status = fclose($handle);
+	if ($retbytes && $status) {
+		return $cnt; // return num. bytes delivered like readfile() does.
+	}
+	return $status;
 }
 
 if ( isset($_REQUEST['fileId']) ) {
@@ -61,31 +61,31 @@ $_REQUEST['galleryId'] = $info['galleryId'];
 
 $smarty->assign('individual', 'n');
 if ( $userlib->object_has_one_permission($_REQUEST['galleryId'], 'file gallery') ) {
-  $smarty->assign('individual', 'y');
-  if ( $tiki_p_admin != 'y' ) {
-    // Now get all the permissions that are set for this type of permissions 'file gallery'
-    $perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'file galleries');
-    foreach ( $perms['data'] as $perm ) {
-      $permName = $perm['permName'];
-      if ( $userlib->object_has_permission($user, $_REQUEST['galleryId'], 'file gallery', $permName) ) {
-        $$permName = 'y';
-        $smarty->assign("$permName", 'y');
-      } else {
-        $$permName = 'n';
-        $smarty->assign("$permName", 'n');
-      }
-    }
-  }
+	$smarty->assign('individual', 'y');
+	if ( $tiki_p_admin != 'y' ) {
+		// Now get all the permissions that are set for this type of permissions 'file gallery'
+		$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'file galleries');
+		foreach ( $perms['data'] as $perm ) {
+			$permName = $perm['permName'];
+			if ( $userlib->object_has_permission($user, $_REQUEST['galleryId'], 'file gallery', $permName) ) {
+				$$permName = 'y';
+				$smarty->assign("$permName", 'y');
+			} else {
+				$$permName = 'n';
+				$smarty->assign("$permName", 'n');
+			}
+		}
+	}
 }
 
 if ( $tiki_p_admin_file_galleries == 'y' ) {
-  $tiki_p_download_files = 'y';
+	$tiki_p_download_files = 'y';
 }
 
 if ( $tiki_p_download_files != 'y' ) {
-  $smarty->assign('msg', tra('You can not download files'));
-  $smarty->display('error.tpl');
-  die;
+	$smarty->assign('msg', tra('You can not download files'));
+	$smarty->display('error.tpl');
+	die;
 }
 
 // Add hits ( if download or display only )
@@ -111,6 +111,7 @@ if ( ! isset($_GET['thumbnail']) && ! isset($_GET['icon']) ) {
 session_write_close();
 error_reporting(E_ALL);
 
+$content_changed = false;
 $content = &$info['data'];
 
 // Handle images display, files thumbnails and icons
@@ -119,8 +120,13 @@ if ( isset($_GET['thumbnail']) || isset($_GET['display']) || isset($_GET['icon']
 	// Modify the original image if needed
 	if ( ! isset($_GET['display']) || isset($_GET['x']) || isset($_GET['y']) || isset($_GET['scale']) || isset($_GET['max']) || isset($_GET['format']) ) {
 
+		$content_changed = true;
+		if ( $info['path'] ) {
+			$content=file_get_contents(($prefs['fgal_use_dir'].$info['path']));
+		}
+
 		require_once('lib/images/images.php');
-    if (!class_exists('Image')) die();
+		if (!class_exists('Image')) die();
 
 		$format = substr($info['filename'], strrpos($info['filename'], '.') + 1);
 
@@ -129,7 +135,7 @@ if ( isset($_GET['thumbnail']) || isset($_GET['display']) || isset($_GET['icon']
 			$_GET['icon'] = 'y';
 			$_GET['max'] = 32;
 		}
-		
+
 		if ( isset($_GET['icon']) ) {
 			unset($content);
 			if ( isset($_GET['max']) ) {
@@ -147,7 +153,7 @@ if ( isset($_GET['thumbnail']) || isset($_GET['display']) || isset($_GET['icon']
 
 		if ( ! isset($_GET['icon']) || ( isset($_GET['format']) && $_GET['format'] != $format ) ) {
 			$image = new Image($content);
-		
+
 			$resize = false;
 			// We resize if needed
 			if ( isset($_GET['x']) || isset($_GET['y']) ) {
@@ -168,7 +174,7 @@ if ( isset($_GET['thumbnail']) || isset($_GET['display']) || isset($_GET['icon']
 			elseif ( isset($_GET['thumbnail']) ) {
 				$image->resizethumb();
 			}
-		
+
 			// We change the image format if needed
 			if ( isset($_GET['format']) && Image::is_supported($_GET['format']) ) {
 				$image->convert($_GET['format']);
@@ -177,22 +183,11 @@ if ( isset($_GET['thumbnail']) || isset($_GET['display']) || isset($_GET['icon']
 			elseif ( isset($_GET['thumbnail']) ) {
 				$image->convert('jpeg');
 			}
-		
+
 			$content =& $image->display();
 			$info['filetype'] = $image->get_mimetype();
 		}
-
 	}
-
-	if ( function_exists('mb_strlen') ) {
-		header('Content-Length: '.mb_strlen($content, '8bit'));
-	} else {
-		header('Content-Length: '.strlen($content));
-	}
-	header('Content-type: '.$info['filetype']);
-	echo "$content";
-
-	die();
 }
 
 // Lock while downloading
@@ -213,17 +208,23 @@ $file = preg_replace('/.*([^\/]*)$/U', '$1', $info['filename']);
 // File galleries should always be attachments (files) not inline (textual)
 
 header('Content-type: '.$info['filetype']);
-header("Content-Disposition: attachment; filename=\"$file\"");
+if (!$content_change and !isset($_GET['display'])) {
+	header("Content-Disposition: attachment; filename=\"$file\"");
+}
 
 header('Expires: 0');
 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 header('Pragma: public');
 
-if ( $info['path'] ) {
+if ( $info['path'] and !$content_changed ) {
 	header('Content-Length: '.filesize($prefs['fgal_use_dir'].$info['path']) );
 	readfile_chunked($prefs['fgal_use_dir'].$info['path']);
 } else {
-	header('Content-Length: '.$info['filesize']);
+	if ( function_exists('mb_strlen') ) {
+		header('Content-Length: '.mb_strlen($content, '8bit'));
+	} else {
+		header('Content-Length: '.strlen($content));
+	}
 	echo "$content";
 }
 ?>
