6b9bd32db98ece059187087e6295974c858c4fd7
sylvieg <sylvieg@users.sourceforge.net>
2008-08-31 13:47:43 +0000

    [FIX]googleanalytics - seems the javascript normalization bother google
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@14561 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_googleanalytics.php
+++ lib/wiki-plugins/wikiplugin_googleanalytics.php
@@ -23,13 +23,10 @@ function wikiplugin_googleanalytics($data, $params) {
 	$ret = '<script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
-//--><!]]>
 </script>
 <script type="text/javascript">
-<!--//--><![CDATA[//><!--
 var pageTracker = _gat._getTracker("UA-'.$account.'");
 pageTracker._trackPageview();
-//--><!]]>
 </script>';
 	return $ret;
 }
