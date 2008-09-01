d52e0d6e5e793c4d40629a279c3834188ce3b9be
lphuberdeau <lphuberdeau@users.sourceforge.net>
2008-09-01 17:38:44 +0000

    [MOD] Create proper patches for the latest updates
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@14573 b456876b-0849-0410-b77d-98878d47e9d5

--- upgrade/db/tiki_2.0to3.0.sql
+++ upgrade/db/tiki_2.0to3.0.sql
@@ -39,7 +39,6 @@ UPDATE `tiki_menu_options` set `url`='tiki-list_file_gallery.php' where `url`='t
 UPDATE `tiki_menu_options` SET `url`='tiki-admin_include_score.php' where `url`='tiki-admin_score.php';
 
 #2008-08-16 princessxine
-#2008-08-27 bitey [embiggens the feature_type column]
 DROP TABLE IF EXISTS `tiki_feature`;
 CREATE TABLE `tiki_feature` (
   `feature_id` mediumint(9) NOT NULL auto_increment,
@@ -67,23 +66,3 @@ CREATE TABLE tiki_schema (
 	install_date TIMESTAMP
 ) ENGINE=MyISAM;
 
-#2008-08-22 lphuberdeau
-ALTER TABLE tiki_links ADD COLUMN reltype VARCHAR(50);
-CREATE TABLE tiki_semantic_tokens (
-	token VARCHAR(15) PRIMARY KEY,
-	label VARCHAR(25) NOT NULL,
-	invert_token VARCHAR(15)
-) ENGINE=MyISAM ;
-
-#2008-08-29 lphuberdeau
-INSERT INTO tiki_semantic_tokens (token, label) VALUES('alias', 'Page Alias');
-
-#2008-08-29 lphuberdeau
-INSERT INTO users_permissions (permName, permDesc, level, type) VALUES ('tiki_p_plugin_viewdetail', 'Can view unapproved plugin details', 'editors', 'wiki');
-INSERT INTO users_permissions (permName, permDesc, level, type) VALUES ('tiki_p_plugin_preview', 'Can execute unapproved plugin', 'editors', 'wiki');
-INSERT INTO users_permissions (permName, permDesc, level, type) VALUES ('tiki_p_plugin_approve', 'Can approve plugin execution', 'editors', 'wiki');
-
-#2008-09-01 lphuberdeau
-DELETE FROM users_permissions WHERE permName IN('tiki_p_plugin_viewdetail', 'tiki_p_plugin_preview');
-INSERT INTO users_permissions (permName, permDesc, level, type) VALUES ('tiki_p_plugin_viewdetail', 'Can view unapproved plugin details', 'registered', 'wiki');
-INSERT INTO users_permissions (permName, permDesc, level, type) VALUES ('tiki_p_plugin_preview', 'Can execute unapproved plugin', 'registered', 'wiki');
