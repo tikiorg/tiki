73f99d311e883cdc7d27323d20af8b6099ea1b76
sylvieg <sylvieg@users.sourceforge.net>
2008-09-11 22:09:18 +0000

    [FIX]freetag: feature_perm check
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@14681 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-freetags_most_popular.php
+++ modules/mod-freetags_most_popular.php
@@ -6,10 +6,15 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   exit;
 }
 
-global $dbTiki;
-global $freetaglib;
-if (!isset($freetaglib) or !is_object($freetaglib)) { require_once 'lib/freetag/freetaglib.php'; }
-$most_popular_tags = $freetaglib->get_most_popular_tags('', 0, $module_rows);
-$smarty->assign('most_popular_tags', $most_popular_tags);
-$smarty->assign('type', isset($module_params["type"]) ? $module_params["type"] : 'list');
-?>
+global $prefs, $tiki_p_view_freetags;
+if ($prefs['feature_freetags'] != 'y') {
+	$smarty->assign('module_error', tra("This feature is disabled").": feature_freetags");
+} elseif ($tiki_p_view_freetags != 'y') {
+	$smarty->assign('module_error', tra("You do not have permission to use this feature"));
+} else {
+	global $freetaglib; require_once 'lib/freetag/freetaglib.php';
+	$most_popular_tags = $freetaglib->get_most_popular_tags('', 0, empty($module_params['max'])?$module_rows: $module_params['max']);
+	$smarty->assign_by_ref('most_popular_tags', $most_popular_tags);
+	$smarty->assign('type', (isset($module_params['type']) && $module_params['type'] =='cloud') ? 'cloud' : 'list');
+	print_r($module_params);
+}
