6cd52eb87c0455064bf8aeadab9faad8bd171476
matwho <matwho@users.sourceforge.net>
2008-09-20 08:51:03 +0000

    [FIX] Made tiki-psplugin.php work and added some comments
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@14781 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-jsplugin.php
+++ tiki-jsplugin.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This is included in the html generated for each wiki page. It is included for each plugin used on a wiki page.
+ * The include is of the form <script type="text/javascript" src="tiki-jsplugin.php?plugin=googledoc"></script>
+ * If no plugin name is given, a list of all the plugins is used instead
+ * The java script generated defines tiki_plugins["pluginname"] with meta data for the parameters of the plugin.
+ * This is then used to allow a nice way for the editor of the page to use a form to edit the plug-in when they
+ * click the little edit icon next to the plug-ins generated html.
+ *
+ * /
 
 $all = !isset( $_GET['plugin'] );
 
@@ -21,6 +30,7 @@ if( $all )
 else
 {
 	$plugin = basename( $_GET['plugin'] );
+
 	$cache = "temp/cache/wikiplugin_$plugin";
 
 	if( file_exists( $cache ) )
@@ -29,9 +39,9 @@ else
 		exit;
 	}
 
+    $saveP=$plugin;
 	include 'tiki-setup.php';
-
-	$plugins = array( $plugin );
+ 	$plugins = array( $saveP );
 }
 
 ob_start();
