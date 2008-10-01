a8dfe74756ee15bfeacb0717b6f6182f50094b21
sylvieg <sylvieg@users.sourceforge.net>
2008-10-01 14:58:38 +0000

    [FIX]fgal: tiki-upload_file can use galleryId[]
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@14943 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/categories.php
+++ lib/setup/categories.php
@@ -14,9 +14,16 @@ if ($prefs['feature_categories'] == 'y' && $prefs['categories_used_in_tpl'] == '
 	// pick up the objectType from cat_type is set or from section
     if (!empty($section) && !empty($sections) && !empty($sections[$section])) {
 		$here = $sections[$section];
+		if (isset($_REQUEST[$here['key']])) {
+			if (isset($_REQUEST[$here['key']][0])) { // tiki-upload_file uses galleryId[]
+				$key = $_REQUEST[$here['key']][0];
+			} else {
+				$key = $_REQUEST[$here['key']];
+			}
+		}
 		if (isset($here['itemkey']) && isset($_REQUEST[$here['itemkey']]) && isset($here['itemObjectType'])) {
 			if (strstr($here['itemObjectType'], '%') && isset($_REQUEST[$here['key']])) {
-				$objectType = sprintf($here['itemObjectType'], $_REQUEST[$here['key']]);
+				$objectType = sprintf($here['itemObjectType'], $key);
 			} else {
 				$objectType = $here['itemObjectType'];
 			}
@@ -29,7 +36,7 @@ if ($prefs['feature_categories'] == 'y' && $prefs['categories_used_in_tpl'] == '
 		if (isset($here['itemkey']) && isset($_REQUEST[$here['itemkey']]) && isset($here['itemObjectType'])) {
 			$objectCategoryIds = $categlib->get_object_categories($objectType, $_REQUEST[$here['itemkey']]);
 		} elseif (isset($here['key']) && isset($_REQUEST[$here['key']])) {
-			$objectCategoryIds = $categlib->get_object_categories($objectType, $_REQUEST[$here['key']]);
+			$objectCategoryIds = $categlib->get_object_categories($objectType, $key);
 		}
 	}
     $smarty->assign_by_ref('objectCategoryIds', $objectCategoryIds);
