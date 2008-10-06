7a12a42ad029c8d54f86198b8c12d33e978c8e9b
sylvieg <sylvieg@users.sourceforge.net>
2008-10-06 12:27:24 +0000

    backport the error message when feature is not set: the feature should be changed manually in the database
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/2.0@15002 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-minichat.php
+++ modules/mod-minichat.php
@@ -6,25 +6,27 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   exit;
 }
 
+if ($prefs['feature_minichat'] != 'y') {
+	$smarty->assign('module_error', tra("This feature is disabled").": feature_minichat");
+} else {
+	if (isset($module_params["channels"])) {
+		$channels=explode(',', $module_params["channels"]);
+	} else
+		$channels=array('default');
 
-if (isset($module_params["channels"])) {
-    $channels=explode(',', $module_params["channels"]);
-} else $channels=array('default');
+	if (isset($_SESSION['minichat_channels'])) {
+		$channels=$_SESSION['minichat_channels'];
+	}
 
-if (isset($_SESSION['minichat_channels'])) {
-	$channels=$_SESSION['minichat_channels'];
+	$jscode='';
+	foreach($channels as $k => $channel) {
+		$channel='#'.preg_replace('/[^a-zA-Z0-9\-\_]/i','',$channel);
+		$channel=substr($channel, 0, 30);
+		$channels[$k]=$channel;
+
+		$jscode.="minichat_addchannel('".$channel."');\n";
+	}
+
+	$smarty->assign('jscode', $jscode);
+	$smarty->assign('module_rows', $module_rows*10);
 }
-
-$jscode='';
-foreach($channels as $k => $channel) {
-	$channel='#'.preg_replace('/[^a-zA-Z0-9\-\_]/i','',$channel);
-	$channel=substr($channel, 0, 30);
-	$channels[$k]=$channel;
-
-	$jscode.="minichat_addchannel('".$channel."');\n";
-}
-
-$smarty->assign('jscode', $jscode);
-$smarty->assign('module_rows', $module_rows*10);
-
-?>
