fe105a432492656b2241cd8ab4ccea534d520c9c
sylvieg <sylvieg@users.sourceforge.net>
2008-10-06 15:14:42 +0000

    [MOD]trackerstat: additional option to link to tiki-view_tracker with the associated filter
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@15005 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_trackerstat.php
+++ lib/wiki-plugins/wikiplugin_trackerstat.php
@@ -11,7 +11,7 @@
  */
 function wikiplugin_trackerstat_help() {
 	$help = tra("Displays some stat of a tracker content, fields are indicated with numeric ids.").":\n";
-	$help.= "~np~{TRACKERSTAT(trackerId=>1,fields=>2:4:5,show_percent=>y,show_bar=>n,status=>o|c|p|op|oc|pc|opc)}Title{TRACKERSTAT}~/np~";
+	$help.= "~np~{TRACKERSTAT(trackerId=>1,fields=>2:4:5,show_percent=>y,show_bar=>n,status=>o|c|p|op|oc|pc|opc,show_link=n)}Title{TRACKERSTAT}~/np~";
 	return $help;
 }
 
@@ -48,6 +48,11 @@ function wikiplugin_trackerstat_info() {
 				'name' => tra('Status Filter'),
 				'description' => 'o|p|c|op|oc|pc|opc'.' '.tra('Which item status to list. o = open, p = pending, c = closed.'),
 			),
+			'show_link' => array(
+				'required' => false,
+				'name' => tra('Sow link to tiki-view_tracker'),
+				'description' => 'y|n',
+			),
 		),
 	);
 }
@@ -77,6 +82,11 @@ function wikiplugin_trackerstat($data, $params) {
 		$smarty->assign('show_bar', 'y');
 	} else {
 		$smarty->assign('show_bar', 'n');
+	}
+	if (isset($show_link) && $show_link == 'y') {
+		$smarty->assign('show_link', 'y');
+	} else {
+		$smarty->assign('show_link', 'n');
 	}
 	
 	$allFields = $trklib->list_tracker_fields($trackerId, 0, -1, 'position_asc', '');
@@ -144,8 +154,9 @@ function wikiplugin_trackerstat($data, $params) {
 						break;
 					}
 				}
+				$v[$j]['href'] = "trackerId=$trackerId&amp;filterfield=$fieldId&amp;filtervalue[$fieldId][]=".$listCategs[$j]['categId'];
 			}
-		} else	if ($allFields["data"][$i]['type'] == 'h') {
+		} else	if ($allFields["data"][$i]['type'] == 'h') {//header
 			$stat['name'] = $allFields["data"][$i]['name'];
 			$stat['values'] = array();
 			$stats[] = $stat;
@@ -168,6 +179,7 @@ function wikiplugin_trackerstat($data, $params) {
 					if (isset($userValues) && in_array($value, $userValues)) {
 						$v[$j]['me'] = 'y';
 					}
+					$v[$j]['href'] = "trackerId=$trackerId&amp;filterfield=$fieldId&amp;filtervalue[$fieldId]=".urlencode($value);
 				} else {
 					++$v[$j]['count'];
 				}
@@ -179,6 +191,9 @@ function wikiplugin_trackerstat($data, $params) {
 				$v[$j]['average'] = 100*$v[$j]['count']/$total;
 			}
 		}
+		if ($tracker_info['showStatus'] == 'y') {
+			$v[$j]['href'] .= "&amp;status=$status";
+		}
 		if (!empty($v)) {
 			$stat['name'] = $allFields["data"][$i]['name'];
 			$stat['values'] = $v;
@@ -189,5 +204,3 @@ function wikiplugin_trackerstat($data, $params) {
 	$smarty->assign_by_ref('stats', $stats);
 	return "~np~".$smarty->fetch('wiki-plugins/wikiplugin_trackerstat.tpl')."~/np~";
 }
-
-?>
