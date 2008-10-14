1fe650bf631c70e0083ed6a220cc63289ab4b355
Jyhem <Jyhem@users.sourceforge.net>
2008-10-14 09:18:13 +0000

    [SEC] Limit size of user-provided input.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/2.0@15134 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-error_simple.php
+++ tiki-error_simple.php
@@ -2,17 +2,24 @@
 
 // $Id:
 
-echo '<html><body><pre><p>';
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+
+echo '<html><title>maintenance</title><body><pre><p>';
 if (isset($_REQUEST['error']) and !is_null($_REQUEST['error'])) {
-	echo htmlentities(strip_tags($_REQUEST["error"]), ENT_QUOTES, "UTF-8");	
+
+	$_REQUEST["error"] = substr($_REQUEST["error"],0,256);
+	echo htmlentities(strip_tags($_REQUEST["error"]), ENT_QUOTES );	
+
 } else {
 	echo 'There was an unspecified error.  Please go back and try again.';
 }
 
 echo '</p>
 <form name="loginbox" action="tiki-login.php?page=tikiIndex" method="post">
-user: <input type="text" name="user"  size="20" /><br />
-pass: <input type="password" name="pass" size="20" /><br />
+User: <input type="text" name="user"  size="20" /><br />
+Pass: <input type="password" name="pass" size="20" /><br />
 <input type="submit" name="login" value="login" /></form>';
 
 echo '<p><a href="javascript:history.back()">Go back</a></p></pre></body></html>';
