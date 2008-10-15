9534f2dcf467d573031b3068b9b04dce95d3b195
sylvieg <sylvieg@users.sourceforge.net>
2008-10-15 15:57:24 +0000

    [MOD]menupage: new param page (=pagemenu) and title
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@15157 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-menupage.php
+++ modules/mod-menupage.php
@@ -3,13 +3,18 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   header("location: index.php");
   exit;
 }
-
 if (isset($module_params['pagemenu'])) {
-	if ($tikilib->page_exists($module_params['pagemenu'])) {
-		$info = $tikilib->get_page_info($module_params['pagemenu']);
+	$pagemenu = $module_params['pagemenu'];
+} elseif (isset($module_params['page'])) {
+	$pagemenu = $module_params['page'];
+}
+
+if (!empty($pagemenu)) {
+	if ($tikilib->page_exists($pagemenu)) {
+		$info = $tikilib->get_page_info($pagemenu);
 		$content = $tikilib->parse_data($info['data'], array('is_html' => $info['is_html']));
-		$smarty->assign('tpl_module_title',$module_params['pagemenu']);
-		$smarty->assign('contentmenu',$content);
+		$smarty->assign('module_title', isset($module_params['title']) ? $module_params['title'] : $pagemenu);
+		$smarty->assign_by_ref('contentmenu',$content);
 	}
 }
 
