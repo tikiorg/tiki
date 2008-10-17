1dc1127aa0d06ec08247a49ab85779c4836aabb8
lphuberdeau <lphuberdeau@users.sourceforge.net>
2008-10-17 12:56:49 +0000

    [FIX] Broken with PDO
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@15191 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/trackers/trackerlib.php
+++ lib/trackers/trackerlib.php
@@ -1882,7 +1882,7 @@ class TrackerLib extends TikiLib {
 				$this->query($query,array($name,$description,$descriptionIsParsed,(int)$this->now,(int) $trackerId));
 			}
 		} else {
-			$this->getOne("delete from `tiki_trackers` where `name`=?",array($name),false);
+			$this->query("delete from `tiki_trackers` where `name`=?",array($name),-1,-1,false);
 			$query = "insert into `tiki_trackers`(`name`,`description`,`descriptionIsParsed`,`created`,`lastModif`) values(?,?,?,?,?)";
 			$this->query($query,array($name,$description,$descriptionIsParsed,(int) $this->now,(int) $this->now));
 			$trackerId = $this->getOne("select max(`trackerId`) from `tiki_trackers` where `name`=? and `created`=?",array($name,(int) $this->now));
