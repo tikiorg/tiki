bdc3e4d1e49734a3702ac7b17c14c2cf98f9ad79
sylvieg <sylvieg@users.sourceforge.net>
2008-10-24 15:52:48 +0000

    notice
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@15289 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/search/refresh-functions.php
+++ lib/search/refresh-functions.php
@@ -170,7 +170,7 @@ function refresh_index($object_type, $object_id = null) {
 			foreach ( $f_id as $k_id => $v_id ) $id .= (($id!='')?'#':'').$res[(is_string($k_id)?$k_id:$v_id)];
 			foreach ( $f_content as $k_content => $v_content ) $content .= ' '.$res[(is_string($k_content)?$k_content:$v_content)];
 			if ( isset($f_index_type) && $f_index_type != '' ) $index_type = $res[$f_index_type];
-			if ( is_array($filtering_expr) ) foreach ( $filtering_expr as $expr ) eval($expr);
+			if ( isset($filtering_expr) && is_array($filtering_expr) ) foreach ( $filtering_expr as $expr ) eval($expr);
 
 			if ( $content != '' && $index_type != '' && $id != '' ) {
 				insert_index(search_index($content), $index_type, $id);
