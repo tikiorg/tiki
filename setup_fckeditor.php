e22b7f20c704be4599e1cdb92a0d0886c828df78
lphuberdeau <lphuberdeau@users.sourceforge.net>
2008-10-28 14:28:50 +0000

    [FIX] Normal should be P (as default) and not DIV. Also remove slow create_function()
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@15357 b456876b-0849-0410-b77d-98878d47e9d5

--- setup_fckeditor.php
+++ setup_fckeditor.php
@@ -11,14 +11,13 @@ $smarty->assign('fckstyle',$fckstyle);
 
 $tools = split("\r\n|\n",$prefs['wysiwyg_toolbar']);
 $line = 0;
-$trim = create_function('&$x', '$x=trim($x);');
 foreach ($tools as $t) {
 	$t = trim($t);
 	if ($t == '/') {
 		$line++;
 	} else {
 		$els = split(',',$t);
-		array_walk($els,$trim);
+		$els = array_map('trim',$els);
 		$toolbar[$line][] = $els;
 	}
 }
