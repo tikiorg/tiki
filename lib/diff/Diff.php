13998962d826cc1c412151f8a03f8f980f77e7a1
c_schmitz <c_schmitz@users.sourceforge.net>
2008-11-06 15:41:22 +0000

    [MOD] Commented out 'dead' assertion. Calling assert() without a callback functions seems to give the dreaded blank page with some servers on HTML diffs.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@15540 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/diff/Diff.php
+++ lib/diff/Diff.php
@@ -222,8 +222,8 @@ class Text_MappedDiff extends Text_Diff {
     function Text_MappedDiff($from_lines, $to_lines,
                              $mapped_from_lines, $mapped_to_lines)
     {
-        assert(count($from_lines) == count($mapped_from_lines));
-        assert(count($to_lines) == count($mapped_to_lines));
+//        assert(count($from_lines) == count($mapped_from_lines));
+//        assert(count($to_lines) == count($mapped_to_lines));
 
         parent::Text_Diff($mapped_from_lines, $mapped_to_lines);
 
@@ -383,8 +383,8 @@ class Text_Diff_Engine_native {
         $edits = array();
         $xi = $yi = 0;
         while ($xi < $n_from || $yi < $n_to) {
-            assert($yi < $n_to || $this->xchanged[$xi]);
-            assert($xi < $n_from || $this->ychanged[$yi]);
+//            assert($yi < $n_to || $this->xchanged[$xi]);
+//            assert($xi < $n_from || $this->ychanged[$yi]);
 
             // Skip matching "snake".
             $copy = array();
@@ -482,7 +482,7 @@ class Text_Diff_Engine_native {
                 foreach ($matches as $y) {
                     if (empty($this->in_seq[$y])) {
                         $k = $this->_lcsPos($y);
-                        assert($k > 0);
+//                        assert($k > 0);
                         $ymids[$k] = $ymids[$k-1];
                         break;
                     }
@@ -490,7 +490,7 @@ class Text_Diff_Engine_native {
 
                 while (list($junk, $y) = each($matches)) {
                     if ($y > $this->seq[$k - 1]) {
-                        assert($y < $this->seq[$k]);
+//                        assert($y < $this->seq[$k]);
                         /* Optimization: this is a common case: next match is
                          * just replacing previous match. */
                         $this->in_seq[$this->seq[$k]] = false;
@@ -498,7 +498,7 @@ class Text_Diff_Engine_native {
                         $this->in_seq[$y] = 1;
                     } elseif (empty($this->in_seq[$y])) {
                         $k = $this->_lcsPos($y);
-                        assert($k > 0);
+//                        assert($k > 0);
                         $ymids[$k] = $ymids[$k-1];
                     }
                 }
@@ -536,7 +536,7 @@ class Text_Diff_Engine_native {
             }
         }
 
-        assert($ypos != $this->seq[$end]);
+//        assert($ypos != $this->seq[$end]);
 
         $this->in_seq[$this->seq[$end]] = false;
         $this->seq[$end] = $ypos;
@@ -620,7 +620,7 @@ class Text_Diff_Engine_native {
         $i = 0;
         $j = 0;
 
-        assert('count($lines) == count($changed)');
+//        assert('count($lines) == count($changed)');
         $len = count($lines);
         $other_len = count($other_changed);
 
@@ -641,7 +641,7 @@ class Text_Diff_Engine_native {
             }
 
             while ($i < $len && ! $changed[$i]) {
-                assert('$j < $other_len && ! $other_changed[$j]');
+//                assert('$j < $other_len && ! $other_changed[$j]');
                 $i++; $j++;
                 while ($j < $other_len && $other_changed[$j]) {
                     $j++;
@@ -673,11 +673,11 @@ class Text_Diff_Engine_native {
                     while ($start > 0 && $changed[$start - 1]) {
                         $start--;
                     }
-                    assert('$j > 0');
+//                    assert('$j > 0');
                     while ($other_changed[--$j]) {
                         continue;
                     }
-                    assert('$j >= 0 && !$other_changed[$j]');
+//                    assert('$j >= 0 && !$other_changed[$j]');
                 }
 
                 /* Set CORRESPONDING to the end of the changed run, at the
@@ -698,7 +698,7 @@ class Text_Diff_Engine_native {
                         $i++;
                     }
 
-                    assert('$j < $other_len && ! $other_changed[$j]');
+//                    assert('$j < $other_len && ! $other_changed[$j]');
                     $j++;
                     if ($j < $other_len && $other_changed[$j]) {
                         $corresponding = $i;
@@ -714,11 +714,11 @@ class Text_Diff_Engine_native {
             while ($corresponding < $i) {
                 $changed[--$start] = 1;
                 $changed[--$i] = 0;
-                assert('$j > 0');
+//                assert('$j > 0');
                 while ($other_changed[--$j]) {
                     continue;
                 }
-                assert('$j >= 0 && !$other_changed[$j]');
+//                assert('$j >= 0 && !$other_changed[$j]');
             }
         }
     }
