6f01ddb79af9e1693e6692f665d3ee1f5ca2f5be
obnoxxx <obnoxxx@users.sourceforge.net>
2008-11-08 22:58:23 +0000

    [MOD] reformat error page for connect failed in tiki-db-adodb.
    This makes the html code better readable (proper indentation/line break).
    It also adds one missing paragrapgh closing.
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@15573 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki-db-adodb.php
+++ db/tiki-db-adodb.php
@@ -52,13 +52,24 @@ if (!@$dbTiki->Connect($host_tiki, $user_tiki, $pass_tiki, $dbs_tiki)) {
 	<div id="tiki-main">
 		<div id="tiki-mid">
 			<div style="margin:10px 30px;">
-				<h1><font color="red">Tikiwiki is unable to connect to the database.</font> <a title="help" href="http://doc.tikiwiki.org/Installation" target="help"><img border="0" src="img/icons/help.gif" alt="Help" /></a></h1>
-';
-	print '<p>The following error message was returned:<div class="simplebox">';
+				<h1>
+					<font color="red">Tikiwiki is unable to connect to the database.</font>
+					<a title="help" href="http://doc.tikiwiki.org/Installation" target="help"><img border="0" src="img/icons/help.gif" alt="Help" /></a>
+				</h1>
+				<p>The following error message was returned:
+					<div class="simplebox">'
 	print $dbTiki->ErrorMsg();
-	print '</div></p><p>Things to check:<ul><li>Is your database up and running?</li><li>Are your database login credentials correct?</li><li>Did you complete the Tiki Installer?</li></ul>
-<p>Please see <a href="http://doc.tikiwiki.org/">the documentation</a> for more information.</p>
-</div>
+	print '</div>
+				</p>
+				<p>Things to check:
+					<ul>
+						<li>Is your database up and running?</li>
+						<li>Are your database login credentials correct?</li>
+						<li>Did you complete the Tiki Installer?</li>
+						</ul>
+				</p>
+				<p>Please see <a href="http://doc.tikiwiki.org/">the documentation</a> for more information.</p>
+			</div>
 		</div>
 		<hr>
 		<p align="center">
