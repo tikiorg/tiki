231196d21f47e3a301cf6bd750edf070d1eba6ba
sylvieg <sylvieg@users.sourceforge.net>
2008-11-19 21:12:39 +0000

    rename
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@15696 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-export_tracker.php
+++ tiki-export_tracker.php
@@ -182,7 +182,7 @@ while (($items = $trklib->list_items($_REQUEST['trackerId'], $offset, $maxRecord
 		}
 	$smarty->assign_by_ref('items', $items["data"]);
 
-	$data = $smarty->fetch('tiki-export_tracker.tpl');
+	$data = $smarty->fetch('tiki-export_tracker_item.tpl');
 	if (empty($_REQUEST['encoding']) || $_REQUEST['encoding'] == 'ISO-8859-1') {
 		$data = utf8_decode($data);
 	}
