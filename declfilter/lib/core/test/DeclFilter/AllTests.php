37e81c94ac613f58d198b2cbb38f40ec33a38fac
lphuberdeau <lphuberdeau@users.sourceforge.net>
2008-11-28 22:40:54 +0000

    [NEW] Pattern matching filtering rules
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@15811 b456876b-0849-0410-b77d-98878d47e9d5

--- declfilter/lib/core/test/DeclFilter/AllTests.php
+++ declfilter/lib/core/test/DeclFilter/AllTests.php
@@ -11,11 +11,16 @@ class DeclFilter_AllTests
     {
         $suite = new PHPUnit_Framework_TestSuite('DeclFilterSuite');
 
-        $suite->addTestSuite('DeclFilter_BaseTest');
         $suite->addTestSuite('DeclFilter_StaticKeyFilterTest');
         $suite->addTestSuite('DeclFilter_StaticKeyUnsetTest');
+
+        $suite->addTestSuite('DeclFilter_KeyPatternFilterTest');
+        $suite->addTestSuite('DeclFilter_KeyPatternUnsetTest');
+
         $suite->addTestSuite('DeclFilter_CatchAllFilterTest');
         $suite->addTestSuite('DeclFilter_CatchAllUnsetTest');
+
+        $suite->addTestSuite('DeclFilter_BaseTest');
         $suite->addTestSuite('DeclFilter_ConfigureTest');
 
         return $suite;
