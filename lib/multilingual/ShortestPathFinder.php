080a03edb5d9426be36f53f269075671aaa705d6
alain_desilets <alain_desilets@users.sourceforge.net>
2008-12-01 18:15:20 +0000

    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@15852 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/multilingual/ShortestPathFinder.php
+++ lib/multilingual/ShortestPathFinder.php
@@ -10,6 +10,7 @@ require_once 'ShortestPathFinder.php';
 //
 // For details on this algorithm, see:
 //   http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm 
+//
  
 class  ShortestPathFinder {
 	var $visited = array();
@@ -179,73 +180,6 @@ class  ShortestPathFinder {
 	}
 } // end class 
 
-//
-// this function illustrates how you can run the shortest path algorithm
-function run_path_finder_example() {
- 
-   // I is the infinite distance.
-   define('I',1000);
- 
-   // Size of the matrix
-   $matrixWidth = 20;
- 
-   // $points is an array in the following format: (router1,router2,distance-between-them)
-   $points = array(
-   	   array(0,1,4),
-   	   array(0,2,I),
-	   array(1,2,5),
- 	   array(1,3,5),
-	   array(2,3,5),
-	   array(3,4,5),
-	   array(4,5,5),
-	   array(4,5,5),
-	   array(2,10,30),
-	   array(2,11,40),
-	   array(5,19,20),
-	   array(10,11,20),
-	   array(12,13,20),
-   );
- 
-   $ourMap = array();
- 
- 
-   // Read in the points and push them into the map
- 
-   for ($i=0,$m=count($points); $i<$m; $i++) {
-	   $x = $points[$i][0];
-	   $y = $points[$i][1];
-	   $c = $points[$i][2];
-	   $ourMap[$x][$y] = $c;
-	   $ourMap[$y][$x] = $c;
-   }
- 
-   // ensure that the distance from a node to itself is always zero
-   // Purists may want to edit this bit out.
- 
-   for ($i=0; $i < $matrixWidth; $i++) {
-       for ($k=0; $k < $matrixWidth; $k++) {
-           if ($i == $k) $ourMap[$i][$k] = 0;
-       }
-   }
- 
- 
-   // initialize the algorithm class
-   $path_finder = new ShortestPathFinder($ourMap, I,$matrixWidth);
- 
-   // $path_finder->computeShortestPathes(0,13); to find only path from field 0 to field 13...
-   $path_finder->computeShortestPathes(0); 
- 
-   // Display the results
- 
-   echo '<pre>';
-   echo "the map looks like:\n\n";
-   echo $path_finder -> printMap($ourMap);
-   echo "\n\nthe shortest paths from point 0:\n";
-   echo $path_finder -> getResults();
-   echo '</pre>';
-}
-
-//run_path_finder_example();
 
 
 ?>
