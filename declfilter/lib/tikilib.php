52e095fa2f3a82df64a82703981b880a6ec2dbc2
lphuberdeau <lphuberdeau@users.sourceforge.net>
2008-12-07 19:54:05 +0000

    [MOD] Enforcing validation at the plugin-execution level
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@15934 b456876b-0849-0410-b77d-98878d47e9d5

--- declfilter/lib/tikilib.php
+++ declfilter/lib/tikilib.php
@@ -5602,7 +5602,25 @@ class TikiLib extends TikiDB {
 		$func_name = 'wikiplugin_' . $name;
 
 		if( function_exists( $func_name ) ) {
+			$info = $this->plugin_info( $name );
+			$default = TikiFilter::get('xss');
+
+			// Apply filters on the body
+			$filter = isset($info['filter']) ? TikiFilter::get($info['filter']) : $default;
+			$data = $filter->filter($data);
+
+			// Make sure all arguments are declared
+			$params = $info['params'];
+			$args = array_intersect_key( $args, $params );
+
+			// Apply filters on values individually
+			foreach( $args as $key => &$value ) {
+				$filter = isset($params[$key]['filter']) ? TikiFilter::get($params[$key]['filter']) : $default;
+				$value = $filter->filter($value);
+			}
+
 			return $func_name( $data, $args, $offset );
+
 		} elseif( $info = $this->plugin_alias_info( $name ) ) {
 			$name = $info['implementation'];
 
