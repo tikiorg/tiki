705ce72f54a81dc5f6bfdbc636f2a410f3d7427e
jonnybradley <jonnybradley@users.sourceforge.net>
2008-12-11 17:38:37 +0000

    [FIX] Observe $prefs['feature_ajax_autosave'] to prevent JS error
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@16005 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.editform.php
+++ lib/smarty_tiki/function.editform.php
@@ -15,7 +15,9 @@ function smarty_function_editform($params, &$smarty) {
 	if (isset($params['Meat']))       $fcked->Meat = $params['Meat'];
 	if (isset($params['Width']))      $fcked->Width = $params['Width'];
 	if (isset($params['Height']))     $fcked->Height = $params['Height'];
-	$fcked->Config['autoSaveSelf'] = htmlentities($_SERVER['REQUEST_URI']);
+	if ($prefs['feature_ajax'] == 'y' && $prefs['feature_ajax_autosave'] == 'y') {
+		$fcked->Config['autoSaveSelf'] = htmlentities($_SERVER['REQUEST_URI']);
+	}
 	if (isset($params['ToolbarSet'])) {
 		$fcked->ToolbarSet = $params['ToolbarSet'];
 	} else {
