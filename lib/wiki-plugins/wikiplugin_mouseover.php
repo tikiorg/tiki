067f185c8c3baec3d04e2da4c9dc61dd3754a87e
sylvieg <sylvieg@users.sourceforge.net>
2009-01-12 16:02:58 +0000

    [FIX]mouseover: reintroduce the overlib params: if none are set, do not use the overlib lib+ for previous compatibility introduce param label that use data as the text (the param text is still using the data as label) + use a param for background color
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@16243 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_mouseover.php
+++ lib/wiki-plugins/wikiplugin_mouseover.php
@@ -12,7 +12,7 @@ function wikiplugin_mouseover_info() {
 		'name' => tra('Mouse Over'),
 		'description' => tra('Create a mouseover feature on some text'),
 		'prefs' => array( 'wikiplugin_mouseover' ),
-		'body' => tra('Text displayed in the mouse over box.'),
+		'body' => tra('Mouseover text is param label exists. Page text is text param exists'),
 		'params' => array(
 			'url' => array(
 				'required' => false,
@@ -21,6 +21,18 @@ function wikiplugin_mouseover_info() {
 				'filter' => 'url',
 			),
 			'text' => array(
+				'required' => true,
+				'name' => tra('Text'),
+				'description' => tra('Text displayed on the page. The body is the mouseover content'),
+				'filter' => 'striptags',
+			),
+			'text' => array(
+				'required' => true,
+				'name' => tra('Text'),
+				'description' => tra('Text displayed on the popup.'),
+				'filter' => 'striptags',
+			),
+			'label' => array(
 				'required' => true,
 				'name' => tra('Text'),
 				'description' => tra('Text displayed on the page.'),
@@ -56,6 +68,21 @@ function wikiplugin_mouseover_info() {
 				'description' => tra('y|n, parse the body of the plugin as wiki content. (Default to y)'),
 				'filter' => 'alpha',
 			),
+			'fgcolor' => array(
+				'required' => false,
+				'name' => tra('Color of the inside popup'),
+				'description' => tra('#000000'),
+			),
+			'textcolor' => array(
+				'required' => false,
+				'name' => tra('Text popup color'),
+				'description' => tra('#FFFFFF'),
+			),
+			'sticky' => array(
+				'required' => false,
+				'name' => tra('Sticky'),
+				'description' => 'y|n',
+			),
 		),
 	);
 }
@@ -75,13 +102,16 @@ function wikiplugin_mouseover( $data, $params ) {
 	$offsety = isset( $params['offsety'] ) ? (int) $params['offsety'] : 0;
 	$parse = ! isset($params['parse']) || $params['parse'] != 'n';
 
-	$text = isset( $params['text'] ) ? $params['text'] : 'No label specified';
+	$text = isset( $params['text'] ) ? $params['text'] : $data;
+	$label = isset( $params['label'] ) ? $params['label'] : $data;
 
-	$data = trim($data);
+	$text = trim($text);
 
-	if( $parse ) {
-		// Default output of the plugin is in ~np~, so escape it if content has to be parsed.
-		$data = "~/np~$data~np~";
+	if( $parse == 'y' or $parse == '1') {
+		$text = $tikilib->parse_data($text);
+		$text = substr($text, 0, -1);// do not ask me why - but the parsing adds a CR
+	} else {		// Default output of the plugin is in ~np~, so escape it if content has to be parsed.
+		$text = "~/np~$text~np~";
 	}
 
 	static $lastval = 0;
@@ -89,9 +119,32 @@ function wikiplugin_mouseover( $data, $params ) {
 
 	$url = htmlentities( $url, ENT_QUOTES, 'UTF-8' );
 
-	global $headerlib;
+	if (!empty($params['sticky']) || !empty($param['center']) || !empty($param['left']) || !empty($param['right']) || !empty($param['above']) || !empty($param['bellow']) || !empty($param['offsetx']) || !empty($param['offsety'])) {
+		if (!$smarty->get_template_vars('overlib_loaded')) {
+			$html .= '<div id="'.$id.'" style="position:absolute; visibility:hidden; z-index:1000;"></div>';
+			$html .= '<script type="text/javascript" src="lib/overlib.js"></script>';
+			$smarty->assign('overlib_loaded',1);
+		}
+		$text = preg_replace('/\r\n/', '<br />', $text);
+		$html .= "<a href='$url'";
+		$html .= " onmouseover=\"return overlib('".str_replace("'", "\'", htmlspecialchars($text))."'";
+		foreach ($params as $param=>$value) {
+			$p = strtoupper($param);
+			if ($p != 'URL' && $p != 'TEXT' && $p != 'PARSE' && $p != 'LABEL') {
+				if ((!empty($value) || $value != 'n') && ($p == 'STICKY' || $p == 'LEFT' || $p == 'RIGHT' || $p == 'CENTER' || $p == 'ABOVE'  || $p == 'BELOW'  || $p == 'AUTOSTATUS' || $p == 'AUTOSTATUSCAP' || $p == 'HAUTO' || $p == 'VAUTO' || $p == 'CLOSECLICK' || $p == 'FULLHTML' || $p == 'CSSOFF' || $p == 'CSSSTYLE' || $p == 'CSSCLASS' || $p == 'NOCLOSE')) {
+					$html .= ','.$p;
+				} else {
+					$html .= ','.$p;
+					$html .= ",'$value'";	
+				}
+			}
+		}
+		$html .= ");\" onmouseout='nd();' >";
+		$html .= "$label</a>";
+	} else {
+		global $headerlib;
 
-	$headerlib->add_js( "
+		$headerlib->add_js( "
 window.addEvent('domready', function() {
 	$('$id-link').addEvent( 'mouseover', function(event) {
 		$('$id').setStyle('left', (event.page.x + $offsetx) + 'px');
@@ -103,7 +156,11 @@ window.addEvent('domready', function() {
 	} );
 } );
 " );
+		$fg = (isset($params['fgcolor']))?"background-color:".$params['fgcolor'].';':'';
+		$tc = (isset($params['textcolor']))?"color:".$params['textcolor'].';':'';
 
-	return "~np~<a id=\"$id-link\" href=\"$url\">$text</a><div id=\"$id\" style=\"width: {$width}px; height: {$height}px; display:none; position: absolute; z-index: 500; background: white;\">$data</div>~/np~";
+		$html = "~np~<a id=\"$id-link\" href=\"$url\">$label</a><div id=\"$id\" style=\"width: {$width}px; height: {$height}px; {$fg}{$tc}display:none; position: absolute; z-index: 500; background: white;\">$text</div>~/np~";
+	}
+	return $html;
 }
 ?>
