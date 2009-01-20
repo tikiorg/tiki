2600bd0fec06bf48d46cb3d6fbe322db2beb0e6f
lphuberdeau <lphuberdeau@users.sourceforge.net>
2009-01-20 19:47:34 +0000

    [NEW] Adding wikiplugin enable controls in normal admin panel
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@16339 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_include_textarea.php
+++ tiki-admin_include_textarea.php
@@ -6,45 +6,56 @@
 
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
-  header("location: index.php");
-  exit;
+	header("location: index.php");
+	exit;
 }
 
+$plugins = array();
+foreach( $tikilib->plugin_get_list() as $name ) {
+	$info = $tikilib->plugin_info( $name );
+	if( isset( $info['prefs'] ) && is_array( $info['prefs'] ) && count( $info['prefs'] ) > 0 )
+		$plugins[$name] = $info;
+}
 
+$smarty->assign( 'plugins', $plugins );
 
 if (isset($_REQUEST["textareasetup"])) {
-ask_ticket('admin-inc-textarea');
+	ask_ticket('admin-inc-textarea');
 
-    $pref_toggles = array(
-	"feature_hotwords",
-	"feature_hotwords_nw",
-	"feature_dynamic_content",
-	"feature_filegals_manager",
-	"feature_use_quoteplugin",
-	"feature_comments_post_as_anonymous",
-	"feature_smileys",
-	"popupLinks",
-	"feature_autolinks",
-        "quicktags_over_textarea",
-    );
+	$pref_toggles = array(
+		"feature_hotwords",
+		"feature_hotwords_nw",
+		"feature_dynamic_content",
+		"feature_filegals_manager",
+		"feature_use_quoteplugin",
+		"feature_comments_post_as_anonymous",
+		"feature_smileys",
+		"popupLinks",
+		"feature_autolinks",
+		"quicktags_over_textarea",
+	);
 
+	foreach ($pref_toggles as $toggle) {
+		simple_set_toggle ($toggle);
+	}
 
-    foreach ($pref_toggles as $toggle) {
-        simple_set_toggle ($toggle);
-    }
+	foreach( $plugins as $key => $info ) {
+		$key = 'wikiplugin_' . $key;
+		if( in_array( $key, $info['prefs'] ) ) {
+			simple_set_toggle( $key );
+		}
+	}
 
-    $pref_simple_values = array(
-        "default_rows_textarea_wiki",
-        "default_rows_textarea_comment",
-        "default_rows_textarea_forum",
-        "default_rows_textarea_forumthread",
-    );
+	$pref_simple_values = array(
+		"default_rows_textarea_wiki",
+		"default_rows_textarea_comment",
+		"default_rows_textarea_forum",
+		"default_rows_textarea_forumthread",
+	);
 
-    foreach ($pref_simple_values as $svitem) {
-        simple_set_value ($svitem);
-    }
-
+	foreach ($pref_simple_values as $svitem) {
+		simple_set_value ($svitem);
+	}
 }
-
 
 ?>
