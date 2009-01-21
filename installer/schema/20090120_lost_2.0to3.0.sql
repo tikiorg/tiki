0b06632f7c50dd81fd1134663ecf8705f9535757
sylvieg <sylvieg@users.sourceforge.net>
2009-01-21 11:49:16 +0000

    typo
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@16352 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/20090120_lost_2.0to3.0.sql
+++ installer/schema/20090120_lost_2.0to3.0.sql
@@ -2,5 +2,4 @@
 SET @fgcant=0;
 SELECT (@fgcant:=count(*)) FROM users_permissions WHERE permName = 'tiki_p_search_categorized';
 INSERT INTO users_permissions (permName, permDesc, level, type) VALUES ('tiki_p_search_categorized', 'Can search on objects of this category', 'basic', 'category');
-INSERT INTO `users_objectpermissions` (groupName, permName, objectType, objectId) SELECT  groupName, 'tiki_p_search_categorized', objectType , objectId FROM `users_objectpe
-rmissions` WHERE permName = 'tiki_p_view_categorized' AND @fgcant = 0;
+INSERT INTO `users_objectpermissions` (groupName, permName, objectType, objectId) SELECT  groupName, 'tiki_p_search_categorized', objectType , objectId FROM `users_objectpermissions` WHERE permName = 'tiki_p_view_categorized' AND @fgcant = 0;
