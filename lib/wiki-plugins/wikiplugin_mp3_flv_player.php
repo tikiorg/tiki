4187bc5b33a020f83567932d3de5d6892383beca
sylvieg <sylvieg@users.sourceforge.net>
2009-01-22 14:05:27 +0000

    [MOD]mp3/flv player: add support for flv file + rename the plugin
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@16373 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_mp3_flv_player.php
+++ lib/wiki-plugins/wikiplugin_mp3_flv_player.php
@@ -1,14 +1,19 @@
 <?php
-function wikiplugin_audioplayer_info() {
+function wikiplugin_mp3_flv_player_info() {
 	return array(
 				 'name' => tra('Flash Mp3 Player'),
 				 'documentation' => 'See http://flash-mp3-player.net/ http://code.google.com/p/mp3player for additional parameters',
 				 'description' => 'Simple MP3 Player',
 				 'params' => array(
 								   'mp3' => array(
-												  'required' => true,
+												  'required' => false,
 												  'name'=> tra('MP3 URL'),
-												  'description' => tra('Complete URL to the MP3 to include.'),
+												  'description' => tra('Complete URL to the mp3 to include.'),
+												  ),
+								   'flv' => array(
+												  'required' => false,
+												  'name'=> tra('FLV URL'),
+												  'description' => tra('Complete URL to the flv to include.'),
 												  ),
 								   'style' => array(
 													'required' => false,
@@ -19,23 +24,33 @@ function wikiplugin_audioplayer_info() {
 								   ),
 				 );
 }
-function wikiplugin_audioplayer($data, $params) {
-	if (empty($params['mp3']))
+function wikiplugin_mp3_flv_player($data, $params) {
+	if (empty($params['mp3']) && empty($params['flv'])) {
 		return;
-	$defaults = array(
+	}
+	$defaults_mp3 = array(
 		'width' => 200,
 		'height' => 20,
 		'player' => 'player_mp3.swf',
 		'where' => 'http://flash-mp3-player.net/medias/',
 	);
-	$params = array_merge( $defaults, $params );
+	$defaults_flv = array(
+		'width' => 320,
+		'height' => 240,
+		'player' => 'player_flv.swf',
+		'where' => 'http://flv-player.net/medias/',
+	);
+	if (!empty($params['flv'])) {
+		$params = array_merge($defaults_flv, $params );
+	} else {
+		$params = array_merge($defaults_mp3, $params );
+	}
 	$styles = array('normal', 'mini', 'maxi', 'multi');
 	if (empty($params['style']) || $params['style'] == 'normal' || !in_array($params['style'], $styles)) {
 		$player = $params['player'];
 	} else {
 		$player = str_replace('.swf', '_'.$params['style'].'.swf', $params['player']);
 	}
-	echo $player;
 	$code = '<object type="application/x-shockwave-flash" data="'.$params['where'].$player.'" width="'.$params['width'].'" height="'.$params['height'].'">';
 	$code .= '<param name="movie" value="'.$params['where'].$player.'" />';
 	$code .= '<param name="FlashVars" value="mp3='.$params['mp3'].'" />';
