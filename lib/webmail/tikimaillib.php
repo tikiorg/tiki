bf7fb133f90adc3a8f848910f4122fdf1371e92a
Jyhem <Jyhem@users.sourceforge.net>
2009-01-25 22:29:25 +0000

    [FIX] The previous syntax caused Reply-To issues with valid return emails such as 'TW Notification <do-not-reply@example.com>'
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@16393 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/webmail/tikimaillib.php
+++ lib/webmail/tikimaillib.php
@@ -8,7 +8,7 @@ include_once("lib/webmail/htmlMimeMail.php");
 
 class TikiMail extends HtmlMimeMail {
 		var $charset;
-	
+
 	/* $user = user you send the mail
 	   $from = email you send from*/
 	function TikiMail($user = null, $from=null) {
@@ -26,8 +26,8 @@ class TikiMail extends HtmlMimeMail {
 		$this->setFrom($from);
 		if (!@ini_get('safe_mode'))
 			$this->setReturnPath($from); // in safe-mode, return-path must then be configured at the server level
-		$this->setHeader("Return-Path", "<".$from.">"); // just in case, mainly will not work as usually the server rewrites the envelop
-		$this->setHeader("Reply-To",  "<".$from.">");
+		$this->setHeader("Return-Path", $from); // just in case, mainly will not work as usually the server rewrites the envelop
+		$this->setHeader("Reply-To",  $from);
 	}
 
 	function setUser($user) {
