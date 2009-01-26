1878219fb0ee429836f72285a14ee96f84f18770
lphuberdeau <lphuberdeau@users.sourceforge.net>
2009-01-26 22:19:54 +0000

    [MOD] Adding basic support to fetch plugin info and repair broken tests by path changes in files
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@16419 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/test/AllTests.php
+++ lib/core/test/AllTests.php
@@ -2,7 +2,11 @@
 ini_set( 'display_errors', 'on' );
 error_reporting( E_ALL );
 
-ini_set( 'include_path', ini_get('include_path') . ':.:../lib' );
+ini_set( 'include_path', ini_get('include_path') . ':.:../lib:../../..' );
+
+function tra( $string ) {
+	return $string;
+}
 
 function __autoload( $name ) {
 	$path = str_replace( '_', '/', $name );
