07da9bd1ba66ebdeb63ac9f5bc8a2b9dd6f69b08
alain_desilets <alain_desilets@users.sourceforge.net>
2009-01-27 20:32:39 +0000

    - Use ; instead  of : to separate include path directories in lib/core/test/AllTests.php.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@16439 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/test/AllTests.php
+++ lib/core/test/AllTests.php
@@ -2,7 +2,7 @@
 ini_set( 'display_errors', 'on' );
 error_reporting( E_ALL );
 
-ini_set( 'include_path', ini_get('include_path') . ':.:../lib:../../..' );
+ini_set( 'include_path', ini_get('include_path') . ';.;../lib;../../..' );
 
 function tra( $string ) {
 	return $string;
