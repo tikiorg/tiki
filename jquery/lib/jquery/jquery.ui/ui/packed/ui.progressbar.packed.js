e399889c6fa5e81bdbddae3ab5d11e5c14fcd613
jonnybradley <jonnybradley@users.sourceforge.net>
2009-02-07 02:13:38 +0000

    [ENH] New RC of jquery.ui
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@16604 b456876b-0849-0410-b77d-98878d47e9d5

--- jquery/lib/jquery/jquery.ui/ui/packed/ui.progressbar.packed.js
+++
@@ -1,12 +0,0 @@
-/*
- * jQuery UI Progressbar 1.6rc4
- *
- * Copyright (c) 2008 AUTHORS.txt (http://ui.jquery.com/about)
- * Dual licensed under the MIT (MIT-LICENSE.txt)
- * and GPL (GPL-LICENSE.txt) licenses.
- *
- * http://docs.jquery.com/UI/Progressbar
- *
- * Depends:
- *   ui.core.js
- */eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(3(A){A.5("2.4",{z:3(){8 B=1,C=1.k;1.a.v("2-4 2-5 2-5-u 2-e-t").x({o:"4","7-p":1.c(),"7-s":1.9(),"7-l":1.h()});1.b=A(\'<w G="2-4-6 2-5-I 2-e-H"></w>\').J(1.a);1.i()},q:3(){1.a.r("2-4 2-5 2-5-u 2-e-t").d("o").d("7-p").d("7-s").d("7-l").K("4").F(".4");1.b.D();A.5.m.q.n(1,j)},6:3(B){j.E&&1.g("6",B);f 1.h()},g:3(B,C){S(B){V"6":1.k.6=C;1.i();1.L("U",X,{});Y}A.5.m.g.n(1,j)},h:3(){8 B=1.k.6;y(B<1.c()){B=1.c()}y(B>1.9()){B=1.9()}f B},c:3(){8 B=0;f B},9:3(){8 B=P;f B},i:3(){8 B=1.6();1.b[B==1.9()?"v":"r"]("2-e-Q");1.b.R(B+"%");1.a.x("7-l",B)}});A.O(A.2.4,{N:"@T",W:{6:0}})})(M)',61,61,'|this|ui|function|progressbar|widget|value|aria|var|_valueMax|element|valueDiv|_valueMin|removeAttr|corner|return|_setData|_value|_refreshValue|arguments|options|valuenow|prototype|apply|role|valuemin|destroy|removeClass|valuemax|all|content|addClass|div|attr|if|_init||||remove|length|unbind|class|left|header|appendTo|removeData|_trigger|jQuery|version|extend|100|right|width|switch|VERSION|change|case|defaults|null|break'.split('|'),0,{}))
