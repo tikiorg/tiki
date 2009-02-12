d8842bfe50fab02776568392e466e07559a02159
nyloth <nyloth@users.sourceforge.net>
2009-02-12 19:59:12 +0000

    [MOD] setup: remove useless PHP5 check, since Tiki 3.0 is not PHP4 compatible
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@16735 b456876b-0849-0410-b77d-98878d47e9d5

--- coe/tiki-setup.php
+++ coe/tiki-setup.php
@@ -34,12 +34,8 @@ $tiki_timer->start();
 
 require_once('tiki-setup_base.php');
 
-// TikiTests are PHP5 only
-if ($prefs['feature_tikitests'] == 'y' and version_compare(PHP_VERSION, '5.0.0', '>='))  {
-	require_once('tiki_tests/tikitestslib.php');
-}
+if ( $prefs['feature_tikitests'] == 'y' ) require_once('tiki_tests/tikitestslib.php');
 $crumbs[] = new Breadcrumb($prefs['siteTitle'], '', $prefs['tikiIndex']);
-
 if ( $prefs['site_closed'] == 'y' ) require_once('lib/setup/site_closed.php');
 require_once('lib/setup/error_reporting.php');
 if ( $prefs['feature_bot_bar_debug'] == 'y' || $prefs['use_load_threshold'] == 'y' ) require_once('lib/setup/load_threshold.php');
