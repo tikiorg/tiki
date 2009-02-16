0034a54d84a5aeb9afe869d072ecaad389d3bdd7
obnoxxx <obnoxxx@users.sourceforge.net>
2009-02-16 00:43:54 +0000

    [FIX] fix intentation/tabbing of confirm_user()
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@16787 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/userslib.php
+++ lib/userslib.php
@@ -1950,31 +1950,31 @@ function get_included_groups($group, $recur=true) {
 	}
     }
 
-    function confirm_user($user) {
-	global $prefs,$cachelib;
+	function confirm_user($user) {
+		global $prefs,$cachelib;
 
-	$query = "select `provpass`, `login` from `users_users` where `login`=?";
-	$result = $this->query($query, array($user));
-	$res = $result->fetchRow();
-	$hash = $this->hash_pass($res['provpass']);
-	$provpass = $res["provpass"];
+		$query = "select `provpass`, `login` from `users_users` where `login`=?";
+		$result = $this->query($query, array($user));
+		$res = $result->fetchRow();
+		$hash = $this->hash_pass($res['provpass']);
+		$provpass = $res["provpass"];
 
-	if ($prefs['feature_clear_passwords'] == 'n') {
-	    $provpass = '';
+		if ($prefs['feature_clear_passwords'] == 'n') {
+			$provpass = '';
+		}
+
+		$query = "update `users_users` set `password`=? ,`hash`=? ,`provpass`=?, valid=?, `email_confirm`=?, `waiting`=? where `login`=?";
+		$result = $this->query($query, array(
+				$provpass,
+				$hash,
+				'',
+				NULL,
+				$this->now,
+				NULL,
+				$user
+				));
+		$cachelib->invalidate('userslist');
 	}
-
-	$query = "update `users_users` set `password`=? ,`hash`=? ,`provpass`=?, valid=?, `email_confirm`=?, `waiting`=? where `login`=?";
-	$result = $this->query($query, array(
-		    $provpass,
-		    $hash,
-		    '',
-			NULL,
-			$this->now,
-			NULL,
-		    $user
-		    ));
-	$cachelib->invalidate('userslist');
-    }
 
 	function change_user_waiting($user, $who) {
 		$query = 'update `users_users` set `waiting`=?, `currentLogin`=?, `lastLogin`=? where `login`=?';
