d469f80b90775f6dded6ac3ba65fda5df40e5fba
sylvieg <sylvieg@users.sourceforge.net>
2009-03-04 17:08:18 +0000

    remove 3.0 update scripts
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@17168 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/20081027_trackers_tiki.sql
+++
@@ -1,5 +0,0 @@
-#gillesm 2008-10-27
-#2008_10_26 GillesM
-ALTER TABLE tiki_trackers ADD groupforAlert varchar(255) default NULL ;
-ALTER TABLE tiki_trackers DROP groupforAlert;
-
--- installer/schema/20090211_mailnotification_tiki.sql
+++
@@ -1,8 +0,0 @@
-
-#2009-02-12 pkdille
-#Add new perm tiki_p_admin_notifications which gives the perm to manage the mail notifications (without being admin)
-
-INSERT INTO users_permissions (permName, permDesc, level, type) VALUES ('tiki_p_admin_notifications', 'Can admin mail notifications', 'editors', 'mail notifications');
-
-UPDATE `tiki_menu_options` SET `perm` = 'tiki_p_admin_notifications' WHERE menuId = '42' AND name = 'Mail Notifications' AND url = 'tiki-admin_notifications.php';
-
