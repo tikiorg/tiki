00810bac7f8e9e0379ebef15d9de232990a1302b
amette <amette@users.sourceforge.net>
2009-03-15 23:33:49 +0000

    Modules must obey max_rows
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@17347 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-blog_last_comments.php
+++ modules/mod-blog_last_comments.php
@@ -12,7 +12,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 
 global $bloglib;
 include_once ('lib/blogs/bloglib.php');
-$comments = $bloglib->list_blog_post_comments();
+$comments = $bloglib->list_blog_post_comments('y', $module_rows);
 
 $smarty->assign('comments', $comments['data']);
 $smarty->assign('nonums', isset($module_params['nonums']) ? $module_params['nonums'] : 'n');
