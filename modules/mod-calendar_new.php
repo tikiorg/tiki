04656306af4f53109a1f8872c59946a7c9309984
sylvieg <sylvieg@users.sourceforge.net>
2009-03-16 17:02:28 +0000

    [FIX]calendar_new module: use the session calendar or def calendars if not param calIds specified->TODO: fix the display if mulitple calendars are selected
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@17377 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-calendar_new.php
+++ modules/mod-calendar_new.php
@@ -6,20 +6,30 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 	exit;
 }
 
-global $prefs, $tiki_p_view_calendar;
-if ( $prefs['feature_calendar'] == 'y' && $tiki_p_view_calendar == 'y' ) {
+global $prefs, $tiki_p_view_calendar, $tiki_p_admin_calendars;
+if ( $prefs['feature_calendar'] == 'y' ) {
 
 	global $calendarlib; include_once('lib/calendar/calendarlib.php');
+	global $userlib; include_once('lib/userslib.php');
 	global $headerlib; $headerlib->add_cssfile('css/calendar.css',20);
 	global $calendarViewMode;
 
 	$calendarViewMode = 'month';
 	$group_by = 'day';
-
 	if (empty($module_params['calIds'])) {
-		$module_params['calIds'] = array();
+		if (!empty($_SESSION['CalendarViewGroups'])) {
+			$module_params['calIds'] = $_SESSION['CalendarViewGroups'];
+		} elseif (!empty($prefs['default_calendars'])) {
+			$module_params['calIds'] = $_SESSION['CalendarViewGroups'] = is_array($prefs['default_calendars']) ? $prefs['default_calendars'] : unserialize($prefs['default_calendars']);
+		} else {
+			$module_params['calIds'] = array();
+		}
 	}
-
+	foreach ($module_params['calIds'] as $i=>$cal_id) {
+		if ($tiki_p_admin_calendars != 'y' && !$userlib->user_has_perm_on_object($user, $cal_id, 'calendar', 'tiki_p_view_calendar')) {
+			unset($module_params['calIds'][$i]);
+		}
+	}
 	include('tiki-calendar_setup.php');
 
 	$tc_infos = $calendarlib->getCalendar($module_params['calIds'], $viewstart, $viewend, $group_by);
