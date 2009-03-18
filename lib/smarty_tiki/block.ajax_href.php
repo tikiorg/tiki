99fb4b33cd4672fcb588fc9999610bfe12213a0e
luciash <luciash@users.sourceforge.net>
2009-03-18 15:18:53 +0000

    [FIX] tikitabs: same max value as in header.tpl
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@17439 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/block.ajax_href.php
+++ lib/smarty_tiki/block.ajax_href.php
@@ -18,7 +18,7 @@ function smarty_block_ajax_href($params, $content, &$smarty, $repeat) {
     if ( $prefs['feature_ajax'] != 'y' || $prefs['javascript_enabled'] == 'n' ) {
 	return " href=\"$url\" ";
     } else {
-	$max_tikitabs = 5; // Same value as in header.tpl, <body> tag onload's param
+	$max_tikitabs = 50; // Same value as in header.tpl, <body> tag onload's param
 	return " href=\"#main\" onclick=\"window.scrollTo(0,0);loadComponent('$url','$template','$htmlelement',$max_tikitabs,'$last_user');return false;\" ";
     }
 }
