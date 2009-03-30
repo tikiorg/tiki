242709874a4a73e1638518163735a6833834b35f
jonnybradley <jonnybradley@users.sourceforge.net>
2009-03-30 22:42:08 +0000

    [KIL] Remove stray copy of function.js_maxlength.php Corrected Id svn:keyword & removed ancient comments on "real" file
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@17713 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.js_maxlength.php
+++ lib/smarty_tiki/function.js_maxlength.php
@@ -6,7 +6,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   exit;
 }
 
-// $Id: /cvsroot/tikiwiki/tiki/lib/smarty_tiki/function.js_maxlength.php,v 1.5 2005-05-18 11:01:44 mose Exp $
+// $Id$
 /**
  * \brief Smarty {js_maxlength} function handler
  *
@@ -21,26 +21,12 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 function smarty_function_js_maxlength($params, &$smarty) {
 	extract($params); // textarea=string maxlength=num
 
-    echo "\n<script type=\"text/javascript\" language=\"Javascript1.2\">\n";
+    echo "\n<script type=\"text/javascript\"\n";
     echo "<!--\n";
-    //
-    // TODO this is the best I can get a countdown to work but it is still not
-    // good enough. Improve or Remove permanently
-    // function textCounter(f, countf, maxl) {
-    //  if ( f.value.length > maxl-1 ) {
-    //    f.value = f.value.substring(0, maxl-1);
-    //    alert( 'This text can only be ' + maxl + ' characters long.' );
-    //    return false;
-    //  } else {
-    //    countf.value = maxl - 1 - f.value.length;
-    //  }
-    //}
+
     echo "function verifyForm(f){\n";
     echo " var rtn=true;\n";
     echo "  if ( f.$textarea.value.length > $maxlength ) {\n";
-    // save the next two lines for implementing multiple checks via array arguments
-    //    echo "    f.$textarea.style.color='red';\n";
-    //    echo "    alert('" . tra("The text in RED is") . " ' + (f.$textarea.value.length - $maxlength) + ' " . tra("character(s) too long - please edit it.") . "');\n";
     echo "    alert('" . tra("The text is") . " ' + (f.$textarea.value.length - $maxlength) + ' " . tra("character(s) too long - please edit it.") . "');\n";
     echo "    rtn = false;\n";
     echo "  }\n";
