7bf9b6f180cc4bc112c79f0e1a7defef77f8ee3e
jonnybradley <jonnybradley@users.sourceforge.net>
2009-03-30 15:15:56 +0000

    [MOD] More exmaples and help on {jq} syntax
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@17684 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/block.jq.php
+++ lib/smarty_tiki/block.jq.php
@@ -9,10 +9,24 @@ global $access; $access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__)
  *
  * Creates JQuery javascript if enabled
  * Defaults to execute on DOM ready
+ * The content script is automatically escaped with {literal}{/literal} unless the tag is already in there.
+ * To "unescape" back to smarty synax use {{ to start, and }} to stop. See examples below.
  * 
  * Usage:
- *    {jq [notonready=false|true], [nojquery='Optional markup for when feature_jquery is off']}$jq("#exampleId").hide(){/jq}
- *
+ *    {jq [notonready=false|true], [nojquery='Optional markup for when feature_jquery is off']}
+ *        $jq("#exampleId").hide()
+ *    {/jq}
+ * 
+ * Examples:
+ * 
+ *  Simple, no escaping - result wrapped in {literal}{/literal}
+ *    {jq}$jq(#exampleId").click(function() { alert("Clicked!"); });{/jq}
+ * 
+ *  Smarty markup between {{ and }} - result parsed and wrapped in literals
+ *    {jq}$jq(#exampleId").show({{if $animation_fast eq 'y'}"fast"{else}"slow"{/if}}){/jq}
+ * 
+ *  Escaped already - not re-parsed, not wrapped in literals
+ *    {jq}{literal}$jq(#exampleId").show({/literal}{if $animation_fast eq 'y'}"fast"{else}"slow"{/if}){/jq}
  */
 
 function smarty_block_jq($params, $content, &$smarty) {
