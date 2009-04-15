572b72b96253c709e3d4c023f5907a0913439395
sylvieg <sylvieg@users.sourceforge.net>
2009-04-15 19:25:47 +0000

    notice
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@18011 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-edit_submission.php
+++ tiki-edit_submission.php
@@ -53,7 +53,7 @@ if (isset($_REQUEST["previewId"])) {
 $smarty->assign('subId', $subId);
 $smarty->assign('articleId', $subId);
 $smarty->assign('previewId', $previewId);
-$smarty->assign('imageIsChanged', ($_REQUEST["imageIsChanged"]=='y')?'y':'n');
+$smarty->assign('imageIsChanged', (isset($_REQUEST['imageIsChanged']) && $_REQUEST['imageIsChanged']=='y')?'y':'n');
 
 $smarty->assign('allowhtml', 'y');
 $publishDate = $tikilib->now;
@@ -431,7 +431,7 @@ include_once ("categorize_list.php");
 
 if ($prefs['feature_freetags'] == 'y') {
     include_once ("freetag_list.php");
-    if ($_REQUEST["preview"]) {
+    if (isset($_REQUEST['preview'])) {
 	$smarty->assign('taglist',$_REQUEST["freetag_string"]);
     }
 }
