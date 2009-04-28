57eb2dc200baf05491d3331bea0f5d915757191a
jonnybradley <jonnybradley@users.sourceforge.net>
2009-04-28 15:21:20 +0000

    [FIX] Now works as advertised with the simple example on http://doc.tikiwiki.org/PluginVersions (tested locally - needs more real world testing) Not changed nav=n mode as i don't understand it (the code or the intention) Needs a proper overhaul IMHO
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@18269 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_versions.php
+++ lib/wiki-plugins/wikiplugin_versions.php
@@ -96,7 +96,11 @@ function wikiplugin_versions($data, $params) {
 			$data = substr($data,strpos($data,'---('.$v[1][$p-1]));
 			$data = preg_replace('/\)---*[\r\n]*/',"</b>\n","<b class='versiontitle'>". substr($data,4));
 		} else {
-			$data = preg_replace('/(?:.*---\('.$v[1][$p-1].'\)-{3,}?[\r\n]*?)(.*?)(?:---\(.*)/mis',"$1", $data);
+			// can't get it to work as a single preg_match_all, so...
+			preg_match_all("/(^|---\([^\(]*\)---*\s)/",$data,$t, PREG_OFFSET_CAPTURE);
+			$start = $t[0][$p][1] + strlen($t[0][$p][0]);
+			$end   = $p + 1 < count($t[0]) ? $t[0][$p+1][1] : strlen($data);
+			$data = substr($data, $start, $end);
 		}
 		if (strpos($data,'---(') !== false) {
 			$data = substr($data,0,strpos($data,'---('));
