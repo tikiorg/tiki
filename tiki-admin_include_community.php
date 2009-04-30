3231af3667e675b30f8812426a85d6c038d463b2
ricks99 <ricks99@users.sourceforge.net>
2009-04-30 14:05:16 +0000

    [FIX]re-added the missing user preferences. moved from old admin: login to admin: community.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@18323 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_include_community.php
+++ tiki-admin_include_community.php
@@ -32,4 +32,59 @@ if (isset($_REQUEST["userfeatures"])) {
 	simple_set_value("user_list_order");
 }
 
+
+// Users Defaults
+if (isset($_REQUEST['users_defaults'])) {
+	check_ticket('admin-inc-login');
+
+	// numerical and text values
+	$_prefs = array(
+		'users_prefs_userbreadCrumb',
+		'users_prefs_language',
+		'users_prefs_display_timezone',
+		'users_prefs_user_information',
+		'users_prefs_mailCharset',
+		'users_prefs_mess_maxRecords',
+		'users_prefs_minPrio',
+		'users_prefs_user_dbl',
+		'users_prefs_diff_versions',
+		'users_prefs_mess_archiveAfter',
+		'users_prefs_tasks_maxRecords'
+	);
+	foreach($_prefs as $pref) {
+		simple_set_value($pref);
+	}
+
+	// boolean values
+	$_prefs = array(
+		'users_prefs_show_mouseover_user_info',
+		'users_prefs_allowMsgs',
+		'users_prefs_mytiki_pages',
+		'users_prefs_mytiki_blogs',
+		'users_prefs_mytiki_articles',		
+		'users_prefs_mytiki_gals',
+		'users_prefs_mytiki_msgs',
+		'users_prefs_mytiki_tasks',
+		'users_prefs_mytiki_items',
+		'users_prefs_mytiki_workflow',
+		'users_prefs_mytiki_forum_topics',
+		'users_prefs_mytiki_forum_replies',
+		'users_prefs_mess_sendReadStatus'
+	);
+	foreach($_prefs as $pref) {
+		simple_set_toggle($pref);
+	}
+}
+
+
+// Users Defaults
+$mailCharsets = array('utf-8', 'iso-8859-1');
+$smarty->assign_by_ref('mailCharsets', $mailCharsets);
+
+// Get list of available languages
+$languages = array();
+$languages = $tikilib->list_languages(false,null,true);
+$smarty->assign_by_ref('languages', $languages);
+
+
 ask_ticket('admin-inc-community');
--- tiki-admin_include_login.php
+++ tiki-admin_include_login.php
@@ -136,48 +136,6 @@ if (isset($_REQUEST['auth_shib'])) {
   simple_set_value('shib_group');
 }
 
-// Users Defaults
-if (isset($_REQUEST['users_defaults'])) {
-	check_ticket('admin-inc-login');
-
-	// numerical and text values
-	$_prefs = array(
-		'users_prefs_userbreadCrumb',
-		'users_prefs_language',
-		'users_prefs_display_timezone',
-		'users_prefs_user_information',
-		'users_prefs_mailCharset',
-		'users_prefs_mess_maxRecords',
-		'users_prefs_minPrio',
-		'users_prefs_user_dbl',
-		'users_prefs_diff_versions',
-		'users_prefs_mess_archiveAfter',
-		'users_prefs_tasks_maxRecords'
-	);
-	foreach($_prefs as $pref) {
-		simple_set_value($pref);
-	}
-
-	// boolean values
-	$_prefs = array(
-		'users_prefs_show_mouseover_user_info',
-		'users_prefs_allowMsgs',
-		'users_prefs_mytiki_pages',
-		'users_prefs_mytiki_blogs',
-		'users_prefs_mytiki_articles',		
-		'users_prefs_mytiki_gals',
-		'users_prefs_mytiki_msgs',
-		'users_prefs_mytiki_tasks',
-		'users_prefs_mytiki_items',
-		'users_prefs_mytiki_workflow',
-		'users_prefs_mytiki_forum_topics',
-		'users_prefs_mytiki_forum_replies',
-		'users_prefs_mess_sendReadStatus'
-	);
-	foreach($_prefs as $pref) {
-		simple_set_toggle($pref);
-	}
-}
 
 $smarty->assign("phpcas_enabled", $phpcas_enabled);
 $smarty->assign('gd_lib_found', function_exists('gd_info') ? 'y' : 'n' );
@@ -195,9 +153,6 @@ $smarty->assign("listTrackers",$listTrackers['list']);
 $listgroups = $userlib->get_groups(0, -1, 'groupName_desc', '', '', 'n');
 $smarty->assign("listgroups", $listgroups['data']);
 
-// Users Defaults
-$mailCharsets = array('utf-8', 'iso-8859-1');
-$smarty->assign_by_ref('mailCharsets', $mailCharsets);
 
 ask_ticket('admin-inc-login');
 ?>
