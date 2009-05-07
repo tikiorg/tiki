8bb60c3ce953260f95608c90ae5651f8e4a888d3
mangapower <mangapower@users.sourceforge.net>
2009-05-07 19:00:19 +0000

    [ADD] Added the Workspace folder and wslib.php
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@18554 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ ws/lib/workspaces/wslib.php
@@ -0,0 +1,17 @@
+<?php
+/** \file
+ * $Id: /cvsroot/tikiwiki/tiki/lib/workspaces/wslib.php
+ *
+ * \brief Workspaces support class
+ *
+ */
+
+//this script may only be included - so its better to die if called directly.
+if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
+  header("location: index.php");
+  exit;
+}
+
+class Workspacelib {
+
+}
