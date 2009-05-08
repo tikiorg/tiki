2237e6a055b922d2e5f8c5d40ee1d55dc42131ba
sylvieg <sylvieg@users.sourceforge.net>
2009-05-08 12:57:00 +0000

    [FIX]tracker: tiki_p_use_as_template is not really useful as each field is tested individually
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@18563 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/resource.wiki.php
+++ lib/smarty_tiki/resource.wiki.php
@@ -14,10 +14,8 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
  * -------------------------------------------------------------
  */
 function smarty_resource_wiki_source($page, &$tpl_source, &$smarty) {
-	global $tikilib, $user;
-	if (!$tikilib->user_has_perm_on_object($user, $page, 'wiki page', 'tiki_p_use_as_template')) {
-		return false;
-	}
+	global $tikilib;
+
 	$info = $tikilib->get_page_info($page);
 	if (empty($info)) {
 		return false;
