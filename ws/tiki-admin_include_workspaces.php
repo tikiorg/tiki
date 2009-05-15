4b90a3247b6eb41d85f86769aa50270b7e48d2a6
mangapower <mangapower@users.sourceforge.net>
2009-05-15 13:33:22 +0000

    [NEW] Added a proto interface about configuring workspaces
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@18830 b456876b-0849-0410-b77d-98878d47e9d5

--- ws/tiki-admin_include_workspaces.php
+++ ws/tiki-admin_include_workspaces.php
@@ -5,7 +5,10 @@
 require_once ('tiki-setup.php');
 $access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__));
 
-$smarty->assign('warning', tra('In order to get Workspaces working you must to configure first how do you want '));
+//if $prefs['feature_workspaces_init'] != 'n'
+//{
+	$smarty->assign('welcome', tra('Please select one option bellow. Remember this step is very important, so be careful when you select the workspace category container, because you can not step back!'));
+//}
 
 ask_ticket('admin-inc-workspaces');
 ?>
