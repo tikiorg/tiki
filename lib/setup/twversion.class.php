9c49ec24ace8fad578f601c2ae0671b1896ba523
nyloth <nyloth@users.sourceforge.net>
2009-05-15 09:41:36 +0000

    [REL] Preparing 3.0rc2 release
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@18815 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -30,7 +30,7 @@ class TWVersion {
 		$this->branch 	= 'unstable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '3.0rc1';
+		$this->version 	= '3.0rc2';
 		$this->star	= 'Betelgeuse';
 		$this->release 	= $this->version;
 		$this->releases	= array();
@@ -96,7 +96,8 @@ class TWVersion {
 				21=>'3.0beta2',
 				22=>'3.0beta3',
 				23=>'3.0beta4',
-				24=>'3.0rc1'
+				24=>'3.0rc1',
+				25=>'3.0rc2'
 				);
 	}
 
