92b180639b7155d81eab056050f48bc0117d740f
nyloth <nyloth@users.sourceforge.net>
2009-05-28 13:11:27 +0000

    [FIX] smarty: fix SMARTY_DIR when setup_smarty.php is included in a script without calling tiki-setup.php before
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@19166 b456876b-0849-0410-b77d-98878d47e9d5

--- setup_smarty.php
+++ setup_smarty.php
@@ -12,7 +12,8 @@ if (strpos($_SERVER['SCRIPT_NAME'],basename(__FILE__)) !== FALSE) {
   exit;
 }
 
-require_once(SMARTY_DIR.'Smarty.class.php');
+require_once 'lib/setup/third_party.php';
+require_once SMARTY_DIR.'Smarty.class.php';
 
 class Smarty_Tikiwiki extends Smarty {
 	
