cec31edf76289d4b60fab7ad998b8953384ef9eb
axold <axold@users.sourceforge.net>
2009-06-23 13:49:37 +0000

    [MOD] Added one panel of Workspaces in the admin panel
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@19746 b456876b-0849-0410-b77d-98878d47e9d5

--- ws/tiki-admin_include_workspaces.php
+++ ws/tiki-admin_include_workspaces.php
@@ -7,7 +7,7 @@ $access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__));
 
 //if $prefs['feature_workspaces_init'] == 'y'
 //{
-	$smarty->assign('welcome', tra('Please select one option bellow. Remember this step is very important, so be careful when you select the workspace category container, because you can not step back!'));
+	$smarty->assign('welcome', tra('Please select one option bellow. Remember this step is very important, so be careful when you select the workspace category container, because this will destroy any category o whatever inside it!'));
 //}
 
 ask_ticket('admin-inc-workspaces');
