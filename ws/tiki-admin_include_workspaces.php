cd27358ff455775d696391931717809c80b95b0b
axold <axold@users.sourceforge.net>
2009-06-23 14:03:25 +0000

    [FIX] Fixing more things in the manual merge
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@19747 b456876b-0849-0410-b77d-98878d47e9d5

--- ws/tiki-admin_include_workspaces.php
+++ ws/tiki-admin_include_workspaces.php
@@ -9,6 +9,9 @@ $access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__));
 //{
 	$smarty->assign('welcome', tra('Please select one option bellow. Remember this step is very important, so be careful when you select the workspace category container, because this will destroy any category o whatever inside it!'));
 //}
+//else {
+	//$smarty->assign('welcome', tra();
+//}
 
 ask_ticket('admin-inc-workspaces');
 ?>
