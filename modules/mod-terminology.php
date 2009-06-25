841d964b9320ef96cdcbebef1e1edbb2f96d4d49
alain_desilets <alain_desilets@users.sourceforge.net>
2009-06-25 17:40:38 +0000

    - If MultilingualCollaborativeTerminology profile has not been installed, then the Terminology module will display a warning saying that this module needs the profile.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@19794 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-terminology.php
+++ modules/mod-terminology.php
@@ -7,6 +7,11 @@ global $multilinguallib, $smarty, $tikilib;
 
 include_once('lib/multilingual/multilinguallib.php');
 include_once('lib/tikilib.php');
+require_once 'lib/profilelib/profilelib.php';
+require_once 'lib/profilelib/installlib.php';
+require_once 'lib/profilelib/listlib.php';
+
+make_sure_terminology_profil_was_installed();
 
 $search_terms_in_lang = $multilinguallib->currentSearchLanguage(true);
 $smarty->assign('search_terms_in_lang', $search_terms_in_lang);
@@ -17,3 +22,14 @@ $smarty->assign('user_languages', $userLanguagesInfo);
 $smarty->assign('create_new_pages_using_template_name', 'Term Template');
 
 
+function make_sure_terminology_profil_was_installed() {
+	global $smarty;
+	print "<pre>-- mod-terminology.make_sure_terminology_profil_was_installed: invoked</pre>\n";
+	$profile_installer = new Tiki_Profile_Installer;
+	$profile = Tiki_Profile::fromNames('profiles.tikiwiki.org', 'Collaborative_Multilingual_Terminology');
+	if ($profile_installer->isInstalled($profile)) {
+		$smarty->assign('terminology_profile_was_installed', 'y');
+	} else  {
+		$smarty->assign('terminology_profile_was_installed', 'n');
+	}
+}
