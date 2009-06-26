b00fe9f8ffd2f4481d4f2a40a15dd67a60a7c235
mangapower <mangapower@users.sourceforge.net>
2009-06-26 14:07:12 +0000

    [MOD] Added the function add_ws and the headers for remove_ws and add_ws_object
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@19816 b456876b-0849-0410-b77d-98878d47e9d5

--- ws/lib/workspaces/wslib.php
+++ ws/lib/workspaces/wslib.php
@@ -1,6 +1,6 @@
 <?php
 /** \file
- * $Id: /cvsroot/tikiwiki/tiki/lib/workspaces/wslib.php by MangaPowerX
+ * $Id: /cvsroot/tikiwiki/tiki/lib/workspaces/wslib.php by mangapower
  *
  * \brief Workspaces support class
  *
@@ -12,18 +12,48 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   exit;
 }
 
-include_once ('lib/categories/categlib.php');
-include_once ('tiki_admin.php');
-
-class Workspacelib 
+class wslib 
 {
+	
+	private $wsContainerId = $prefs['ws_container_id'];
+	
+	function __autoload() 
+	{
+	     require_once ('lib/categories/categlib.php');
+	     require_once ('tiki-setup.php')
+	}
+
+	// This function will set a container for WS in the category table and return its ID
 	function init_ws ($newName)
 	{
 		if (empty($categlib->get_category_id($newName)))
-		{
 			return $wsContainer = $categlib->add_category(0,$newName,'Workspaces Container Category');
+		else
+			return -1; //Error - a category with the same name already exists (REALLY HARD TO HAPPEN)
+	}
+	
+	// Get the name and its parent WS and create a new WS (NOTE: parentID = WSContainerID)
+	function add_ws ($newName, $parent)
+	{
+		$query = "select `categId` from `tiki_categories` where `name`=? and `parentId`=?";
+		if (empty($id = $this->getOne($query,array($newName,(int) $wsContainerId))))
+		{
+			return $wsId = $categlib->add_category($wsContainerId,$newName,$parent);
 		}
 		else
-			return -1; //Error
+			return -1; //Error - A WS with the same name already exists
 	}
+	
+	// Remove a WS
+	function remove_ws ($ws_id)
+	{
+	}
+	
+	// Add a object to a WS
+	function add_ws_object ($ws_id,$object_id)
+	{
+		
+	} 
 }
+
+
