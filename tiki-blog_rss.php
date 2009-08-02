378f26301590dee9bdd0660060e9c5367fb20d76
lphuberdeau <lphuberdeau@users.sourceforge.net>
2009-08-02 11:53:52 +0000

    [FIX] Some more legacy checking
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@20479 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-blog_rss.php
+++ tiki-blog_rss.php
@@ -17,27 +17,8 @@ if (!isset($_REQUEST["blogId"])) {
 	require_once ('tiki-rss_error.php');
 }
 $smarty->assign('individual', 'n');
-if ($userlib->object_has_one_permission($_REQUEST["blogId"], 'blog')) {
-	$smarty->assign('individual', 'y');
-	if ($tiki_p_admin != 'y') {
-		// Now get all the permissions that are set for this type of permissions 'image gallery'
-		$perms = $userlib->get_permissions(0, -1, 'permName_desc', '', 'blogs');
-		foreach($perms["data"] as $perm) {
-			$permName = $perm["permName"];
-			if ($userlib->object_has_permission($user, $_REQUEST["blogId"], 'blog', $permName)) {
-				$$permName = 'y';
-				$smarty->assign("$permName", 'y');
-			} else {
-				$$permName = 'n';
-				$smarty->assign("$permName", 'n');
-			}
-		}
-	}
-}
-if ($tiki_p_blog_admin == 'y') {
-	$tiki_p_read_blog = 'y';
-	$smarty->assign('tiki_p_read_blog', 'y');
-}
+$tikilib->get_perm_object($_REQUEST["blogId"], 'blog');
+
 if ($tiki_p_read_blog != 'y') {
 	$smarty->assign('errortype', 401);
 	$errmsg = tra("Permission denied you cannot view this section");
