14a4e2d0d19a5e328df149dfbef2c058c630785d
jonnybradley <jonnybradley@users.sourceforge.net>
2009-08-07 18:54:57 +0000

    [FIX] Stop feature_jquery being turned off if javascript_enabled
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@20681 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/js_detect.php
+++ lib/setup/js_detect.php
@@ -34,6 +34,8 @@ if ( $prefs['javascript_enabled'] != 'y' ) {
 	
 } else {	// we have JavaScript
 
+	$prefs['feature_jquery'] = 'y';	// just in case
+	
 	/** Use custom.js in styles or options dir if there **/
 	$custom_js = $tikilib->get_style_path($prefs['style'], $prefs['style_option'], 'custom.js');
 	if (!empty($custom_js)) {
