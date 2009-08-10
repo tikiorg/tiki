87cddfddee9d669b12334bb73588bd3302aaca80
jonnybradley <jonnybradley@users.sourceforge.net>
2009-08-10 16:52:18 +0000

    [FIX] Batch upload zip had gone bad. Updating pclzip lib (and moved to third_party) and changed references. Small fix for call to process_batch_file_upload() with no description.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@20762 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-upload_file.php
+++ tiki-upload_file.php
@@ -183,7 +183,7 @@ if (isset($_REQUEST["upload"])) {
 			$name = $_FILES["userfile"]["name"][$key];
 			if (isset($_REQUEST["isbatch"][$key]) && $_REQUEST["isbatch"][$key] == 'on' && strtolower(substr($name, strlen($name) - 3)) == 'zip') {
 				if ($tiki_p_batch_upload_files == 'y') {
-					$filegallib->process_batch_file_upload($_REQUEST["galleryId"][$key], $_FILES["userfile"]['tmp_name'][$key], $user, $_REQUEST["description"][$key]);
+					$filegallib->process_batch_file_upload($_REQUEST["galleryId"][$key], $_FILES["userfile"]['tmp_name'][$key], $user, isset($_REQUEST["description"][$key]) ? $_REQUEST["description"][$key] : '');
 					$batch_job = true;
 					$batch_job_galleryId = $_REQUEST["galleryId"][$key];
 					print_msg(tra('Batch file processed') . " $name", $formId);
