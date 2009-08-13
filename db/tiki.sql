cc6d047df1b1a29deb6e8fdc16c7ce88f39037a2
jonnybradley <jonnybradley@users.sourceforge.net>
2009-08-13 11:59:50 +0000

    [QT][bp/r20412][FIX] remove UNIQUE KEY on tiki_sefurl_regex_out The important part of r20415 - fixes wish2559 (Error installing into UTF-8 MySQL database)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposed@20822 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki.sql
+++ db/tiki.sql
@@ -3762,7 +3762,6 @@ CREATE TABLE `tiki_sefurl_regex_out` (
   `comment` varchar(256),
   `order` int(11) NULL default 0,
   PRIMARY KEY(`id`),
-  UNIQUE KEY `left` (`left`(256)),
   INDEX `idx1` (silent, type, feature(30))
 );
 INSERT INTO `tiki_sefurl_regex_out` (`left`, `right`, `type`, `feature`) VALUES('tiki-index.php\\?page=(.+)', '$1', 'wiki', 'feature_wiki');
