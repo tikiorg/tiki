8f7f3ca13ed7b01c02246d0213eda1d1ebf336ea
sylvieg <sylvieg@users.sourceforge.net>
2009-08-26 15:10:29 +0000

    rollback 20869: htmpurifier interfers in html pretty tracker page
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@21095 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikilib.php
+++ lib/tikilib.php
@@ -4694,7 +4694,7 @@ class TikiLib extends TikiDb_Bridge {
 			return false;
 
 		$html=$is_html?1:0;
-		if ($html) {
+		if ($html && $prefs['feature_purifier'] != 'n') {
 			require_once('lib/htmlpurifier_tiki/HTMLPurifier.tiki.php');
 			$edit_data = HTMLPurifier($edit_data);
 		}
@@ -7380,7 +7380,7 @@ class TikiLib extends TikiDb_Bridge {
 		}
 
 		$html=$is_html?1:0;
-		if ($html) {
+		if ($html&& $prefs['feature_purifier'] != 'n') {
 			require_once('lib/htmlpurifier_tiki/HTMLPurifier.tiki.php');
 			$edit_data = HTMLPurifier($edit_data);
 		}
