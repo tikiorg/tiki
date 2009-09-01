0e34048b737622ab8d7b0040085d678c3a68f280
chibaguy <chibaguy@users.sourceforge.net>
2009-09-01 04:49:44 +0000

    [FIX] Enable CSS id to be specified for div. (This works, but please check code.)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@21201 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_div.php
+++ lib/wiki-plugins/wikiplugin_div.php
@@ -13,7 +13,7 @@
  * 
  */
 function wikiplugin_div_help() {
-	return tra("Insert a division block on wiki page").":<br />~np~{DIV(class=>class, type=>div|span|pre|b|i|tt|p|blockquote, align=>left|right|center|justify, bg=>color, width=>num[%], float=>left|right])}".tra("text")."{DIV}~/np~";
+	return tra("Insert a division block on wiki page").":<br />~np~{DIV(class=>class, id=>id, type=>div|span|pre|i|b|tt|blockquote, align=>left|right|center|justify, bg=>color, width=>num[%], float=>left|right])}".tra("text")."{DIV}~/np~";
 }
 
 function wikiplugin_div_info() {
@@ -55,7 +55,12 @@ function wikiplugin_div_info() {
 			'class' => array(
 				'required' => false,
 				'name' => tra('CSS Class'),
-				'description' => tra('Apply custom CSS class to the box.'),
+				'description' => tra('Apply custom CSS class to the div.'),
+			),
+			'id' => array(
+				'required' => false,
+				'name' => tra('CSS Id'),
+				'description' => tra('Apply custom CSS id to the div.'),
 			),
 		),
 	);
@@ -67,12 +72,13 @@ function wikiplugin_div($data, $params) {
 	$possibletypes = array('div','span','pre','b','i','tt','p','blockquote');
 	$t    = (isset($type) and in_array($type,$possibletypes)) ? "$type"  : "div";
 	$c    = (isset($class)) ? " class='$class'"  : "";
+	$id    = (isset($id)) ? " id='$id'"  : "";
 	$w    = (isset($width)) ? " width: $width;"  : "";
 	$bg   = (isset($bg))    ? " background: $bg;" : "";
 	$al   = (isset($align) && ($align == 'right' || $align == "center" || $align == "justify")) ? " text-align: $align;" : " text-align: left;";
 	$fl   = (isset($float) && ($float == 'left' || $float == 'right')) ? " float: $float;"  : " float: none;";
 	$cl   = (isset($clear) && ($clear == 'left' || $clear == 'right' || $clear == 'both')) ? " clear: $clear;"  : " clear: none;";
-	$id    = (isset($id)) ? "id='$id'"  : "";
+//	$id    = (isset($id)) ? "id='$id'"  : "";
 
 	$begin  = "<$t style=\"$bg$al$w$fl$cl\"$c $id>";
 	$end = "</$t>";
