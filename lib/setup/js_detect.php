7f85844c2a172425ad9d89d1b29aa760307b3127
nyloth <nyloth@users.sourceforge.net>
2009-09-14 08:01:07 +0000

    [QT] Quality Team Backport
    r21404 | pkdille | 2009-09-11 16:13:47 +0200 (ven. 11 sept. 2009) | 1 line
    [bp/r21403][FIX] javascript detextion: set one year lifetime for the javascript_enabled cookie in order to keep the right value when opening a new session (we can assume that this value should not change for the same pc/browser between two sessions)
    
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@21467 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/js_detect.php
+++ lib/setup/js_detect.php
@@ -19,12 +19,11 @@ if ( isset($_COOKIE['javascript_enabled']) ) {
 } else {
 	// Set the cookie to 'n', through PHP / HTTP headers
 	$prefs['javascript_enabled'] = 'n';
-	setcookie('javascript_enabled', 'n');
 }
 
 if ( $prefs['javascript_enabled'] != 'y' ) {
-	// Set the cookie to 'y', through javascript (will override the above cookie set to 'n' and sent by PHP / HTTP headers)
-	$headerlib->add_js("setCookieBrowser('javascript_enabled','y');");
+	// Set the cookie to 'y', through javascript (will override the above cookie set to 'n' and sent by PHP / HTTP headers) - duration: approx. 1 year
+	$headerlib->add_js("var jsedate = new Date();\njsedate.setTime(" . ( 1000 * ( $tikilib->now + 365 * 24 * 3600 ) ) . ");\nsetCookieBrowser('javascript_enabled', 'y', null, jsedate);");
 
 	$prefs['feature_tabs'] = 'n';
 	$prefs['feature_jquery'] = 'n';
