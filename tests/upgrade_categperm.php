087fd1a3b922f05facc75975083404c9ab081eec
sylvieg <sylvieg@users.sourceforge.net>
2009-09-15 17:28:55 +0000

    [MOD]cat: more comments in the upgrade script + rename the tiki_p_view_categories (thx chealer)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@21516 b456876b-0849-0410-b77d-98878d47e9d5

--- tests/upgrade_categperm.php
+++ tests/upgrade_categperm.php
@@ -6,6 +6,9 @@ if (!($tiki_p_admin == 'y' || $tiki_p_admin_users == 'y')) { // temporary patch:
 	$smarty->display("error.tpl");
 	die;
 }
+// $view describes what was supposed to be given by tiki_p_view_categorized
+// $edit describes what was supposed to be given by tiki_p_edit_categorized
+// these lists are probably incomplete
 $view[] = 'tiki_p_view_trackers';
 $edit[] = 'tiki_p_modify_tracker_items';
 $edit[] = 'tiki_p_create_tracker_items';
@@ -63,6 +66,8 @@ $view[] = 'tiki_p_view_category';
 $query = 'SELECT * FROM `users_objectpermissions` WHERE `permName` = ?';
 $insert = 'INSERT into `users_objectpermissions` (`groupName`, `permName`, `objectType`, `objectId`) values (?,?,?,?)';
 $test = 'SELECT COUNT(*) FROM `users_objectpermissions` WHERE `permName` = ? AND `groupName`=? AND `objectType`=? AND `objectId`=?';
+
+// replace the perm tiki_p_view_categorized with the adequate set of perms for the objects
 $result = $tikilib->query($query, array('tiki_p_view_categorized'));
 while ($res = $result->fetchRow() ) {
 	foreach ($view as $perm) {
@@ -71,6 +76,8 @@ while ($res = $result->fetchRow() ) {
 		}
 	}
 }
+
+// replace the perm tiki_p_edit_categorized with the adequate set of perms for the objects 
 $result = $tikilib->query($query, array('tiki_p_edit_categorized'));
 while ($res = $result->fetchRow() ) {
 	foreach ($edit as $perm) {
@@ -79,3 +86,12 @@ while ($res = $result->fetchRow() ) {
 		}
 	}
 }
+
+//rename tii_p_view_categories to tiki_p_view_category
+$query = 'UPDATE  `users_objectpermissions` SET `permName`=? WHERE `permName`=?';
+$tikilib->query($query, array('tiki_p_view_category', 'tiki_p_view_categories'));
+
+
+// FINALLY: remove tiki_p_view_categorized and tiki_p_edit_categorized
+// Not done yet - before we are sure with have all the mapping
+
