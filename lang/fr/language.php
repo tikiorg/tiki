tikiorg/tiki

commit f5ca691eff7d103edd8f5d049deec574ba59c78f

git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@21571 b456876b-0849-0410-b77d-98878d47e9d5

lang/fr/language.php
@@ -477,7 +477,7 @@ $lang=Array(
 "File Title" => "Titre du fichier",
 "File with names appended by -{\$user} are modifiable, others are only duplicable and be used as model." => "Seuls les fichiers avec un nom se terminant par -{\$user} sont modifiables. Les autres sont seulement duplicables et peuvent être utilisés comme modèle.",
 "Filter the list of wanted pages according to page_regex or custom filter. The default value is the site's __current__ page_regex.<br />" => "Filtrer la liste des pages voulues selon l'expression régulière ou le filtre local. La valeur par defaut est __current__ page_regex.<br />",
-"Find Term" => "Chercher un terme",
+"Find term" => "Chercher un terme",
 "First year in the dropdown, either year number, or relative to current year (+/- N)" => "Première année dans les listes déroulantes, sinon le nombre d'années, ou relativement à l'année courante (+/- N)",
 "flagged" => "avec drapeau",
 "Flash player not available" => "Le lecteur Flash n'est pas disponible",
@@ -546,7 +546,7 @@ $lang=Array(
 "I18n setup" => "Configuration I18n",
 "if no jscalendar" => "Si jscalendar non activé",
 "I forgot my pass" => "J'ai oublié mon mot de passe",
-"If not found, you will be given a chance to create it" => "Si le terme n'est pas trouvé, on vous donnera l'occasion de le créer",
+"If not found, you will be given a chance to create it" => "Si le terme n'est pas trouvé, on vous donnera l'occasion de le créer" 
 "If this is a first time installation, go to <strong>tiki-admin.php</strong> after login to start configuring your new Tiki installation" => "Si ceci est votre première installation, ouvrez la page <strong>tiki-admin.php</strong> après connexion pour démarrer la configuration de votre Tiki",
 "If your host requires SMTP authentication, additional configuration may be necessary" => "Si votre serveur nécessite une authentification SMTP, une configuration additionnelle peut s'avérer nécessaire",
 "I have canceled my friendship request" => "J'ai annulé la demande de mise en relation",
@@ -5979,7 +5979,7 @@ $lang=Array(
 "Create watch for author on page creation" => "Activer la surveillance de la page pour l'auteur automatiquement à la création de la page",
 "Enable watch events when I am the editor" => "Envoyer une notification de changement même si je suis l'éditeur",
 "Enable watches on comments" => "La surveillance d'une page tient compte des commentaires",
-"Watch minor edits" => "Surveiller les modifications mineures",
+"Watch minor edits" => "Surveiller les modifications mineur",
 "Hide page name prefix" => "Cacher le préfixe des noms de page",
 "Approved" => "Approuvé",
 "Out-of-sync" => "Désynchronisé",

commit 00810a15be9b68af39ba4221ced44556a1d7d072

[FIX] french language.php: fix (again) this string which was broken in r21571 and previously in r21540. Please take care before commiting. This simple missing comma breaks tiki
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@21586 b456876b-0849-0410-b77d-98878d47e9d5

lang/fr/language.php
@@ -546,7 +546,7 @@ $lang=Array(
 "I18n setup" => "Configuration I18n",
 "if no jscalendar" => "Si jscalendar non activé",
 "I forgot my pass" => "J'ai oublié mon mot de passe",
-"If not found, you will be given a chance to create it" => "Si le terme n'est pas trouvé, on vous donnera l'occasion de le créer" 
+"If not found, you will be given a chance to create it" => "Si le terme n'est pas trouvé, on vous donnera l'occasion de le créer",
 "If this is a first time installation, go to <strong>tiki-admin.php</strong> after login to start configuring your new Tiki installation" => "Si ceci est votre première installation, ouvrez la page <strong>tiki-admin.php</strong> après connexion pour démarrer la configuration de votre Tiki",
 "If your host requires SMTP authentication, additional configuration may be necessary" => "Si votre serveur nécessite une authentification SMTP, une configuration additionnelle peut s'avérer nécessaire",
 "I have canceled my friendship request" => "J'ai annulé la demande de mise en relation",