21e1e39789fe1dd74c0ae2db3e8331f0ee00445c
chealer <chealer@users.sourceforge.net>
2009-10-02 03:03:04 +0000

    [FIX] missing galleryId param declaration oops
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@21942 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-func-last_images.php
+++ modules/mod-func-last_images.php
@@ -12,6 +12,11 @@ function module_last_images_info() {
 		'description' => tra('List the specified number of images, starting from the most recently added.'),
 		'prefs' => array("feature_galleries"),
 		'params' => array(
+			'galleryId' => array(
+				'name' => tra('Gallery identifier'),
+				'description' => tra('If set to an image gallery identifier, restricts the images to those in the identified gallery.') . " " . tra('Example value: 13.') . " " . tra('Not set by default.'),
+				'filter' => 'int'
+			),
 			'content' => array(
 				'name' => tra('Link content'),
 				'description' => tra('Display the links as image names or thumbnails.') . " " . tra('Possible values: "names" or "thumbnails". Default value: "names"')
