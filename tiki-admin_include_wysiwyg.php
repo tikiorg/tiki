9dc21ad5dfd18a0683557ee0e658077fd5dbb72e
pkdille <pkdille@users.sourceforge.net>
2009-10-04 18:56:31 +0000

    [MOD] lesser magic deployment to wysiwyg admin template
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@21980 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_include_wysiwyg.php
+++ tiki-admin_include_wysiwyg.php
@@ -3,7 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: /cvsroot/tikiwiki/tiki/tiki-admin_include_wysiwyg.php,v 1.1.2.3 2008-03-19 14:00:42 sylvieg Exp $
+
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 	header("location: index.php");
@@ -11,16 +11,5 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 }
 if (isset($_REQUEST["wysiwygfeatures"])) {
 	check_ticket('admin-inc-wysiwyg');
-	$pref_toggles = array(
-		"wysiwyg_optional",
-		"wysiwyg_default",
-		'wysiwyg_memo',
-		"wysiwyg_wiki_parsed",
-		"wysiwyg_wiki_semi_parsed",
-	);
-	foreach($pref_toggles as $toggle) {
-		simple_set_toggle($toggle);
-	}
-	simple_set_value('wysiwyg_toolbar_skin');
 }
 ask_ticket('admin-inc-wysiwyg');
