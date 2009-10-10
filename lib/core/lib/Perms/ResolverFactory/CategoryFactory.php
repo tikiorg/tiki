dc191440a74abbe0aefd818b3f1a1ccd7b8ca214
chealer <chealer@users.sourceforge.net>
2009-10-10 01:03:56 +0000

    [FIX] hopefully last shot at identifier quoting here
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@22151 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/lib/Perms/ResolverFactory/CategoryFactory.php
+++ lib/core/lib/Perms/ResolverFactory/CategoryFactory.php
@@ -101,7 +101,7 @@ class Perms_ResolverFactory_CategoryFactory implements Perms_ResolverFactory
 
 		$db = TikiDb::get();
 		$bindvars = array( $baseContext['type'] );
-		$result = $db->query( 'SELECT categId, itemId FROM tiki_category_objects INNER JOIN tiki_objects ON catObjectId = objectId WHERE type = ? AND ' . $db->in( 'itemId', array_keys( $objects ), $bindvars ) . ' ORDER BY catObjectId, categId', $bindvars );
+		$result = $db->query( 'SELECT `categId`, `itemId` FROM `tiki_category_objects` INNER JOIN `tiki_objects` ON `catObjectId` = `objectId` WHERE `type` = ? AND ' . $db->in( 'itemId', array_keys( $objects ), $bindvars ) . ' ORDER BY `catObjectId`, `categId`', $bindvars );
 
 		$categories = array();
 
@@ -131,7 +131,7 @@ class Perms_ResolverFactory_CategoryFactory implements Perms_ResolverFactory
 		$db = TikiDb::get();
 
 		$bindvars = array();
-		$result = $db->query( 'SELECT objectId, groupName, permName FROM users_objectpermissions WHERE objectType = \'category\' AND ' . $db->in( 'objectId', array_keys( $objects ), $bindvars ), $bindvars );
+		$result = $db->query( 'SELECT `objectId`, `groupName`, `permName` FROM `users_objectpermissions` WHERE `objectType` = \'category\' AND ' . $db->in( 'objectId', array_keys( $objects ), $bindvars ), $bindvars );
 
 		while( $row = $result->fetchRow() ) {
 			$object = $row['objectId'];
