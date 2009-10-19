17b24bb4215963d465d74ec04f36f8399d296fb5
nyloth <nyloth@users.sourceforge.net>
2009-10-19 15:09:50 +0000

    [bp/r22416][FIX] wiki: make colors syntax working when used on multiple lines
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposed@22419 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikilib.php
+++ lib/tikilib.php
@@ -6461,7 +6461,7 @@ class TikiLib extends TikiDB {
 		if (!$simple_wiki) {
 			// Replace colors ~~foreground[,background]:text~~
 			// must be done before []as the description may contain color change
-			$data = preg_replace("/\~\~([^\:\,]+)(,([^\:]+))?:(.*)\~\~/U", "<span style=\"color:$1; background:$3\">$4</span>", $data);
+			$data = preg_replace("/\~\~([^\:\,]+)(,([^\:]+))?:(.*)\~\~/Ums", "<span style=\"color:$1; background:$3\">$4</span>", $data);
 		}
 		// This section matches [...].
 		// Added handling for [[foo] sections.  -rlpowell
