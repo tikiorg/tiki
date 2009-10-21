7821f069dce70316f9638e3433d8fec053e0f0af
sylvieg <sylvieg@users.sourceforge.net>
2009-10-21 17:59:40 +0000

    [FIX] at least 5 .php redefine $group -> give up the & - sorry for the multiple commits
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@22532 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/user_prefs.php
+++ lib/setup/user_prefs.php
@@ -28,7 +28,7 @@ if ( $user ) {
 	$user_preferences = array(); // Used for cache
 	$user_preferences[$user] =& $_SESSION['u_info']['prefs'];
 
-	$group =& $_SESSION['u_info']['group'];
+	$group = $_SESSION['u_info']['group'];
 	$smarty->assign('group', $group); // do not use by_ref as $group can be changed in the .php
 	$smarty->assign_by_ref('user', $user);
 	$smarty->assign('default_group', $group);
