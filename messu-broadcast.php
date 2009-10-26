acafc84f7e39717fe22346440e7025ca3705c560
sylvieg <sylvieg@users.sourceforge.net>
2009-10-26 14:30:31 +0000

    [FIX]broadcast: confirmation step with list of user before sending a broadcast + check tiki_p_messages perms + check allowMsgs preference
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@22648 b456876b-0849-0410-b77d-98878d47e9d5

--- messu-broadcast.php
+++ messu-broadcast.php
@@ -33,6 +33,7 @@ if ($tiki_p_broadcast != 'y') {
 	$smarty->display("error.tpl");
 	die;
 }
+$auto_query_args = array('to', 'cc', 'bcc', 'subject', 'body', 'priority', 'replyto_hash', 'groupbr');
 if (!isset($_REQUEST['to'])) $_REQUEST['to'] = '';
 if (!isset($_REQUEST['cc'])) $_REQUEST['cc'] = '';
 if (!isset($_REQUEST['bcc'])) $_REQUEST['bcc'] = '';
@@ -49,18 +50,22 @@ $smarty->assign('priority', $_REQUEST['priority']);
 $smarty->assign('replyto_hash', $_REQUEST['replyto_hash']);
 $smarty->assign('mid', 'messu-broadcast.tpl');
 $smarty->assign('sent', 0);
-if (isset($_REQUEST['group'])) {
-	if ($_REQUEST['group'] == 'all' && $tiki_p_broadcast_all == 'y') {
+if (isset($_REQUEST['groupbr'])) {
+	if ($_REQUEST['groupbr'] == 'all' && $tiki_p_broadcast_all == 'y') {
 		$a_all_users = $userlib->get_users(0, -1, 'login_desc', '');
 		$all_users = array();
 		foreach($a_all_users['data'] as $a_user) {
 			$all_users[] = $a_user['user'];
 		}
 	} else {
-		$all_users = $userlib->get_group_users($_REQUEST['group']);
+		$all_users = $userlib->get_group_users($_REQUEST['groupbr']);
 	}
+	$smarty->assign_by_ref('groupbr', $_REQUEST['groupbr']);
 }
-if (isset($_REQUEST['send'])) {
+$groups = $userlib->list_all_groups();
+$smarty->assign_by_ref('groups', $groups);
+
+if (isset($_REQUEST['send']) || isset($_REQUEST['preview'])) {
 	check_ticket('messu-broadcast');
 	$message = '';
 	// Validation:
@@ -75,10 +80,12 @@ if (isset($_REQUEST['send'])) {
 	foreach($all_users as $a_user) {
 		if (!empty($a_user)) {
 			if ($userlib->user_exists($a_user)) {
-				if ($tikilib->get_user_preference($a_user, 'allowMsgs', 'y')) {
+				if (!$userlib->user_has_permission($a_user, 'tiki_p_messages')) {
+					$message .= sprintf(tra('User %s does not have the permission'), htmlspecialchars($a_user)). "<br />" ;
+				} elseif ($tikilib->get_user_preference($a_user, 'allowMsgs', 'y') == 'y') {
 					$users[] = $a_user;
 				} else {
-					$message.= sprintf(tra("User %s can not receive messages", htmlspecialchars($a_user))). "<br />" ;
+					$message.= sprintf(tra("User %s does not want to receive messages"), htmlspecialchars($a_user)). "<br />" ;
 				}
 			} else {
 				$message.= tra("Invalid user") . "$a_user<br />";
@@ -91,32 +98,40 @@ if (isset($_REQUEST['send'])) {
 		$users_formatted = array();
 		foreach ($users as $rawuser)
 			$users_formatted[] = htmlspecialchars($rawuser);
-		$message.= tra("Message sent to") . ': ' . implode(',', $users_formatted) . "<br />";
+		if (isset($_REQUEST['send'])) {
+			$message .= tra('The message has been sent to:').' ';
+		} else {
+			$message .= tra('The message will be sent to:').' ';
+		}
+		$message .= implode(',', $users_formatted) . "<br />";
 	} else {
-		$message = tra('ERROR: No valid users to send the message');
+		$message .= tra('ERROR: No valid users to send the message');
 		$smarty->assign('message', $message);
 		$smarty->display("tiki.tpl");
 		die;
 	}
-	$smarty->assign('sent', 1);
-	// Insert the message in the inboxes of each user
-	foreach($users as $a_user) {
-		$messulib->post_message($a_user, $user, $a_user, '', $_REQUEST['subject'], $_REQUEST['body'], $_REQUEST['priority']);
-		// if this is a reply flag the original messages replied to
-		if ($_REQUEST['replyto_hash'] <> '') {
-			$messulib->mark_replied($a_user, $_REQUEST['replyto_hash']);
+	if (isset($_REQUEST['send'])) {
+		$smarty->assign('sent', 1);
+		// Insert the message in the inboxes of each user
+		foreach($users as $a_user) {
+			$messulib->post_message($a_user, $user, $a_user, '', $_REQUEST['subject'], $_REQUEST['body'], $_REQUEST['priority']);
+			// if this is a reply flag the original messages replied to
+			if ($_REQUEST['replyto_hash'] <> '') {
+				$messulib->mark_replied($a_user, $_REQUEST['replyto_hash']);
+			}
 		}
-	}
-	// Insert a copy of the message in the sent box of the sender
-	$messulib->save_sent_message($user, $user, $_REQUEST['to'], $_REQUEST['cc'], $_REQUEST['subject'], $_REQUEST['body'], $_REQUEST['priority'], $_REQUEST['replyto_hash']);
-	$smarty->assign('message', $message);
-	if ($prefs['feature_actionlog'] == 'y') {
-		$logslib->add_action('Posted', '', 'message', 'add=' . strlen($_REQUEST['body']));
+		// Insert a copy of the message in the sent box of the sender
+		$messulib->save_sent_message($user, $user, $_REQUEST['to'], $_REQUEST['cc'], $_REQUEST['subject'], $_REQUEST['body'], $_REQUEST['priority'], $_REQUEST['replyto_hash']);
+		$smarty->assign('message', $message);
+		if ($prefs['feature_actionlog'] == 'y') {
+			$logslib->add_action('Posted', '', 'message', 'add=' . strlen($_REQUEST['body']));
+		}
+	} else {
+		$smarty->assign_by_ref('message', $message);
+		$smarty->assign('preview', 1);
 	}
 }
 ask_ticket('messu-broadcast');
-$groups = $userlib->list_all_groups();
-$smarty->assign_by_ref('groups', $groups);
 include_once ('tiki-section_options.php');
 include_once ('tiki-mytiki_shared.php');
 $smarty->display("tiki.tpl");
