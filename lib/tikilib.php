971e9b0abc96d5053bb7c96326024cb585efc6fe
sylvieg <sylvieg@users.sourceforge.net>
2009-10-29 22:35:25 +0000

    notice
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@22747 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikilib.php
+++ lib/tikilib.php
@@ -4130,7 +4130,7 @@ class TikiLib extends TikiDb_Bridge {
 	// $categperm is the category permission that should grant $perm. if none, pass 0
 	function user_has_perm_on_object($user,$object,$objtype,$perm,$categperm='tiki_p_view_categorized') {
 		global $userlib;
-		$groups = $userlib->get_user_groups( $toCheck );
+		$groups = $userlib->get_user_groups( $user );
 
 		$context = array( 'type' => $objtype, 'object' => $object );
 
