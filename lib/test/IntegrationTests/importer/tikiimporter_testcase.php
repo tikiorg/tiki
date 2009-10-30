0cfc93309e6f85fab7e469cd2e3012f4c3f586c3
alain_desilets <alain_desilets@users.sourceforge.net>
2009-10-30 20:51:06 +0000

    - Trying to put some order into automated tests.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@22772 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/test/IntegrationTests/importer/tikiimporter_testcase.php
@@ -0,0 +1,7 @@
+<?php
+ 
+require_once('PHPUnit/Extensions/OutputTestCase.php'); 
+ 
+class TikiImporter_TestCase extends PHPUnit_Extensions_OutputTestCase {
+ 	protected $backupGlobals = FALSE;	
+}
