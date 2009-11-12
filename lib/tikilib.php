c5007ce128849c2ad770f3fb2da5af9f890e597b
chealer <chealer@users.sourceforge.net>
2009-11-12 13:47:17 +0000

    [FIX] strike syntax ("--") would match HTML comment ends to HTML comment starts from invocations of plugins using Smarty, such as wikiplugin_calendar, when log_tpl is enabled, resulting in content between a comment and the next one being commented. This solution does not consider "-->" as a strike begin thanks Jonny
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/4.x@23121 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikilib.php
+++ lib/tikilib.php
@@ -6191,7 +6191,7 @@ class TikiLib extends TikiDb_Bridge {
 
 		// Strike-deleted text --text-- (but not in the context <!--[if IE]><--!> or <!--//--<!CDATA[//><!--
 		if (!$simple_wiki) {
-			$data = preg_replace("#(?<!<!|//)--([^\s].+?)--#", "<del>$1</del>", $data);
+			$data = preg_replace("#(?<!<!|//)--([^\s>].+?)--#", "<del>$1</del>", $data);
 		}
 
 		// Handle comment sections
