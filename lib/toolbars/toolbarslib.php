75323fb2ce0d354bddcd40f40b106fe73e661da7
jonnybradley <jonnybradley@users.sourceforge.net>
2009-11-12 23:57:51 +0000

    [FIX] urlencode was wrong for url encoding - using htmlentities instead
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/4.x@23171 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/toolbars/toolbarslib.php
+++ lib/toolbars/toolbarslib.php
@@ -1343,7 +1343,7 @@ class ToolbarFileGallery extends Toolbar
 		global $smarty;
 		
 		require_once $smarty->_get_plugin_filepath('function','filegal_manager_url');
-		return $this->getSelfLink('openFgalsWindow(\''.urlencode(smarty_function_filegal_manager_url(array('area_name'=>$areaName), $smarty)).'\');',
+		return $this->getSelfLink('openFgalsWindow(\''.htmlentities(smarty_function_filegal_manager_url(array('area_name'=>$areaName), $smarty)).'\');',
 							htmlentities($this->label, ENT_QUOTES, 'UTF-8'), 'qt-filegal');
 	} // }}}
 
