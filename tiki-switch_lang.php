5af7c6d25bbf8c6556e81ae6f4672c4ad83223f0
sylvieg <sylvieg@users.sourceforge.net>
2009-11-18 21:56:12 +0000

    [FIX]article: to have switch_lang working on article if sefurl on: (horrible: but you can you get in php the url after rewritting)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/4.x@23368 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-switch_lang.php
+++ tiki-switch_lang.php
@@ -13,9 +13,15 @@ if ($prefs['feature_multilingual'] != 'y') {
 if (isset($_GET['from'])) $orig_url = $_GET['from'];
 elseif (isset($_SERVER['HTTP_REFERER'])) $orig_url = $_SERVER['HTTP_REFERER'];
 else $orig_url = $prefs['tikiIndex'];
-if ($prefs['feature_sefurl'] == 'y' && !strstr($orig_url, '.php') && !preg_match('/article[0-9]+$/', $orig_url)) {
-	$orig_url = preg_replace('#\/([^\/]+)$#', '/tiki-index.php?page=$1', $orig_url);
+
+if ($prefs['feature_sefurl'] == 'y' && !strstr($orig_url, '.php')) { 
+        if (!preg_match('/article[0-9]+$/', $orig_url)) {
+                $orig_url = preg_replace('#\/([^\/]+)$#', '/tiki-index.php?page=$1', $orig_url);
+        } else {
+                $orig_url = preg_replace('#\/article([0-9]+)$#', '/tiki-read_article.php?articleId=$1', $orig_url);
+        }
 }
+
 if (strstr($orig_url, 'tiki-index.php') || strstr($orig_url, 'tiki-read_article.php')) {
 	global $multilinguallib;
 	include_once ("lib/multilingual/multilinguallib.php");
