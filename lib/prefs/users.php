7742c024378acf494df6d1c6ba255c24fe162b14
lphuberdeau <lphuberdeau@users.sourceforge.net>
2009-11-20 19:41:12 +0000

    [FIX] Missing file
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@23410 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/prefs/users.php
@@ -0,0 +1,13 @@
+<?php
+
+function prefs_users_list() {
+	return array(
+		'users_serve_avatar_static' => array(
+			'name' => tra('Serve avatar images statically'),
+			'description' => tra('When enabled, feature checks and permission checks will be skipped.'),
+			'type' => 'flag',
+		),
+	);
+}
+
+?>
