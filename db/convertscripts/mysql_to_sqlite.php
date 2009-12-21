94ac67e9265b0a57d63a58581334170c7be432df
Kissaki <Kissaki@users.sourceforge.net>
2009-12-21 20:41:08 +0000

    work on sqlite
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@23978 b456876b-0849-0410-b77d-98878d47e9d5

--- db/convertscripts/mysql_to_sqlite.php
+++ db/convertscripts/mysql_to_sqlite.php
@@ -21,10 +21,27 @@ $data = preg_replace('/ENGINE=[a-zA-Z0-9]*/', '', $data);
 $data = preg_replace('/AUTO_INCREMENT=[0-9]+/', '', $data);
 
 // remove column auto_increment
-$data = preg_replace('/\n(.*)(PRIMARY KEY )auto_increment(,)?\n/', "\n$1$2 AUTOINCREMENT$3\n", $data);
-$data = preg_replace('/\n(.*)(!PRIMARY KEY )auto_increment(,)?\n/', "\n$1$3\n", $data);
+$data = preg_replace('/(,)?\n(.*)auto_increment(,)?\n/', "\n$1$2$3\n", $data);
+//$data = preg_replace('/\n(.*)(PRIMARY KEY )auto_increment(,)?\n/', "\n$1$2 AUTOINCREMENT$3\n", $data);
+//$data = preg_replace('/\n(.*)(?!PRIMARY KEY )auto_increment(,)?\n/', "\n$1$3\n", $data);
 
-$data = preg_replace('/\n([ \t]*)KEY [a-zA-Z0-9`]* \([a-zA-Z0-9`]*, [a-zA-Z0-9`]*\)(,)?\n/', "\n$1$2\n", $data);
+// remove table KEYs
+$data = preg_replace('/(,)?\n([ \t]*)(FULLTEXT )?KEY [a-zA-Z0-9`]* \([a-zA-Z0-9`\(\), ]*\),?/', "\n$2\n", $data);
+
+// remove column types unsigned
+$data = preg_replace('/(,)?\n(.*)unsigned(.)*(,)?\n/', "$1\n$2$3$4\n", $data);
+
+$data = preg_replace('/,([\n \t]*)\)(.*);/', '$1)$2;', $data);
+
+
+
+
+//$data = preg_replace('/,([\n \t]*)\),/', '', $data);
+//$data = preg_replace('/\n([ \t]*)\),([ \t]*)\)\n/', '', $data);
+
+//$data = preg_replace('/\n[ \t]*\n/', '', $data);
+//$data = preg_replace('/\n/', '', $data);
+//$data = preg_replace('/(CREATE TABLE [`a-zA-Z0-9]* )\((.*)\)(.*);/', '$1$2$3', $data);
 
 // save file
 file_put_contents($tikiversion.'.to_sqlite.sql', $data);
