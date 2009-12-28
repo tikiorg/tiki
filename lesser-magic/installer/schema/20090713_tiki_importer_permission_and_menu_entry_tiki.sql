957ea488597e93b4cdceaddc7cc56fb996e01014
marclaporte <marclaporte@users.sourceforge.net>
2009-12-28 18:10:44 +0000

    [KIL] Lesser magic was merged long ago
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@24072 b456876b-0849-0410-b77d-98878d47e9d5

--- lesser-magic/installer/schema/20090713_tiki_importer_permission_and_menu_entry_tiki.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `users_permissions` (`permName`, `permDesc`, `level`, `type`, `admin`) VALUES ('tiki_p_admin_importer', 'Can use the Tiki Importer', 'admin', 'tiki', 'y');
-INSERT INTO `tiki_menu_options` (`menuId`, `type`, `name`, `url`, `position`, `section`, `perm`, `groupname`, `userlevel`) VALUES (42,'o','Tiki Importer','tiki-importer.php',1240,'','tiki_p_admin_importer','',0);
--- lesser-magic/templates/print/print-object_wiki.tpl
+++
@@ -1,2 +0,0 @@
-{if $prefs.feature_page_title ne 'n'}<h1>{$info.pageName}</h1>{/if}
-<div class="wikitext">{$info.parsed}</div>
