33ab1635a83e6c00db50c22e4a80e265e306b83a
marclaporte <marclaporte@users.sourceforge.net>
2010-01-06 12:59:06 +0000

    [MOD] As soon as 4.1 is released, the next revision number is in reality Tiki 4.2 pre-version. This should be reflected in http://dev.tikiwiki.org/What+revision+is+be+used 1- How should this be reflected in the same file in trunk?
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@24158 b456876b-0849-0410-b77d-98878d47e9d5

--- 4.x/lib/setup/twversion.class.php
+++ 4.x/lib/setup/twversion.class.php
@@ -26,7 +26,7 @@ class TWVersion {
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '4.1';
+		$this->version 	= '4.2';
 		$this->star	= 'Aldebaran';
 		$this->releases	= array();
 
@@ -109,7 +109,8 @@ class TWVersion {
 				'4.0beta1',
 				'4.0RC1',
 				'4.0',
-				'4.1'
+				'4.1',
+				'4.2'
 				);
 	}
 
