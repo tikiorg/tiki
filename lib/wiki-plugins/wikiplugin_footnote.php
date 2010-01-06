3b6461a97d0759ef4d590b7418478e29d96b5a93
campbe13 <campbe13@users.sourceforge.net>
2010-01-06 21:25:19 +0000

    [MOD] new params sameas/checkDuplicates to allow for multiple ref to  footnotes
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@24179 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_footnote.php
+++ lib/wiki-plugins/wikiplugin_footnote.php
@@ -18,15 +18,39 @@ function wikiplugin_footnote_info() {
 		'description' => tra( 'Inserts a superscripted footnote number next to text and takes in footnote as parameter.' ),
 		'prefs' => array('wikiplugin_footnote'),
 		'body' => tra('The footnote'),
-		'params' => array(),
+		'params' => array(
+                    'sameas' => array(
+                          'required' => false,
+                           'name' => tra('Sameas'),
+                           'description' => tra('Tag to existing footnote' )),
+                    'checkDuplicate' => array(
+                          'required' => false,
+                           'name' => tra('CheckDuplicate'),
+                           'description' => tra('check for duplcate footnotes'))
+			   )
 	);
 }
 
 function wikiplugin_footnote($data, $params) {
-
-	$GLOBALS["footnoteCount"]++;
-	$footnoteCount = $GLOBALS["footnoteCount"];
-	$GLOBALS["footnotesData"][] = $data;
+        if (empty($params)) {
+		$GLOBALS["footnoteCount"]++;
+		$footnoteCount = $GLOBALS["footnoteCount"];
+		$GLOBALS["footnotesData"][] = trim($data);
+        } else {
+	        extract($params, EXTR_SKIP);
+                if (!empty($sameas)) { 
+        	   $footnoteCount = $sameas;
+                } else {
+                if (ucfirst($checkDuplicate) == "Y") {
+	        foreach($GLOBALS["footnotesData"] as $key => $value){
+	           if ( strcmp(trim($data), $value) == 0 )   {
+                       $footnoteCount = $key + 1;
+                       break;
+                       }
+                  }
+	        }
+	     }   // else for if (!empty($sameas
+	}    // else for if (empty($params
 
 	$html = '{SUP()}'
 				. "<a id=\"ref_footnote$footnoteCount\" href=\"#footnote$footnoteCount\">$footnoteCount</a>"
