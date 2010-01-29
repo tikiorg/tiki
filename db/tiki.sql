7bd6fae95b70f1508cabfb996dc022b9f0c623b0
chealer <chealer@users.sourceforge.net>
2010-01-29 05:34:35 +0000

    [KIL] unused events: evlib, tiki_events and now unused functions from notificationlib and registrationlib Ref: http://sourceforge.net/mailarchive/forum.php?thread_name=4d9048fc1001280740o7325e83fq3b9e821b9f1f39cd%40mail.gmail.com&forum_name=tikiwiki-devel
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@24732 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki.sql
+++ db/tiki.sql
@@ -3072,23 +3072,6 @@ CREATE TABLE `tiki_stats` (
   PRIMARY KEY (`object`(200),`type`,`day`)
 ) ENGINE=MyISAM;
 
-DROP TABLE IF EXISTS `tiki_events`;
-CREATE TABLE `tiki_events` (
-  `callback_type` int(1) NOT NULL default '3',
-  `order` int(2) NOT NULL default '50',
-  `event` varchar(200) NOT NULL default '',
-  `file` varchar(200) NOT NULL default '',
-  `object` varchar(200) NOT NULL default '',
-  `method` varchar(200) NOT NULL default '',
-  PRIMARY KEY (`callback_type`,`order`)
-) ENGINE=MyISAM;
-
-INSERT IGNORE INTO tiki_events(callback_type,`order`,event,file,object,method) VALUES ('1', '20', 'user_registers', 'lib/registration/registrationlib.php', 'registrationlib', 'callback_tikiwiki_setup_custom_fields');
-INSERT IGNORE INTO tiki_events(event,file,object,method) VALUES ('user_registers', 'lib/registration/registrationlib.php', 'registrationlib', 'callback_tikiwiki_save_registration');
-INSERT IGNORE INTO tiki_events(callback_type,`order`,event,file,object,method) VALUES ('5', '20', 'user_registers', 'lib/registration/registrationlib.php', 'registrationlib', 'callback_logslib_user_registers');
-INSERT IGNORE INTO tiki_events(callback_type,`order`,event,file,object,method) VALUES ('5', '25', 'user_registers', 'lib/registration/registrationlib.php', 'registrationlib', 'callback_tikiwiki_send_email');
-INSERT IGNORE INTO tiki_events(callback_type,`order`,event,file,object,method) VALUES ('5', '30', 'user_registers', 'lib/registration/registrationlib.php', 'registrationlib', 'callback_tikimail_user_registers');
-
 DROP TABLE IF EXISTS `tiki_registration_fields`;
 CREATE TABLE `tiki_registration_fields` (
   `id` int(11) NOT NULL auto_increment,
