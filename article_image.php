6e0d5371dafa7debb6b0c05951acbcddea2def61
changi67 <changi67@users.sourceforge.net>
2010-02-11 15:05:08 +0000

    [MOD] header change : copyright + Id
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@25070 b456876b-0849-0410-b77d-98878d47e9d5

--- article_image.php
+++ article_image.php
@@ -3,7 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id
+// $Id$
 
 // application to display an image from the database with 
 // option to resize the image dynamically creating a thumbnail on the fly.
