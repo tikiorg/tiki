39c9d8d166cb03172e6d4a364943b8bda72086a1
changi67 <changi67@users.sourceforge.net>
2010-02-13 21:59:27 +0000

    [MOD] header change : copyright + Id
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@25192 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/trackers/trackerlib.php
+++ lib/trackers/trackerlib.php
@@ -1,8 +1,13 @@
 <?php
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
 /**
  * Tracker Library
  *
- * $Id$
  * \brief Functions to support accessing and processing of the Trackers.
  *
  * @package		Tiki
