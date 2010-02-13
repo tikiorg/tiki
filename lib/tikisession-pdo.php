c6785cc1b8669e3f6129e31ad8858f0052af3a2f
changi67 <changi67@users.sourceforge.net>
2010-02-13 09:11:55 +0000

    [MOD] header change : copyright + Id
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@25175 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikisession-pdo.php
+++ lib/tikisession-pdo.php
@@ -1,4 +1,9 @@
 <?php
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
 
 /* from 
    http://www.spiration.co.uk/post/1333/PHP 5 sessions in mysql database with PDO db objects
