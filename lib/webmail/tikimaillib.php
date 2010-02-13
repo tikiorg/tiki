c94cc990a010bcc73dd17057dfd0176e57c20bbb
changi67 <changi67@users.sourceforge.net>
2010-02-13 21:52:33 +0000

    [MOD] header change : copyright + Id
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@25190 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/webmail/tikimaillib.php
+++ lib/webmail/tikimaillib.php
@@ -1,4 +1,10 @@
 <?php
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
 /** Extension htmlMimeMail
   * set some default params (mainly utf8 as titi is utf8) + use the mailCharset pref from a user
   */
