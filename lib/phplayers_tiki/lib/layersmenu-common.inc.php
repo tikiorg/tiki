acb0f0f641a05728b3e20bd0741a30e435616417
changi67 <changi67@users.sourceforge.net>
2010-02-14 20:57:27 +0000

    [MOD] header change : copyright + Id
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@25211 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/phplayers_tiki/lib/layersmenu-common.inc.php
+++ lib/phplayers_tiki/lib/layersmenu-common.inc.php
@@ -1,7 +1,12 @@
 <?php
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
 // PHP Layers Menu 3.2.0-rc (C) 2001-2004 Marco Pratesi - http://www.marcopratesi.it/
 // (copy of original, included in lib/smarty_tiki/function.phplayers.php, modified for Tiki purposes, changes marked using "// [MOD] by <sfnick>")
-// CVS: $Id$
 
 /**
 * This file contains the code of the LayersMenuCommon class.
