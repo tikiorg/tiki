29096887c99930cc11cb6f0ca9d8b311fc5cfcc6
changi67 <changi67@users.sourceforge.net>
2010-02-14 20:52:22 +0000

    [MOD] header change : copyright + Id
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@25210 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/score/scorelib.php
+++ lib/score/scorelib.php
@@ -1,4 +1,10 @@
 <?php
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
 // This is for users to earn points in the community
 // It's been implemented before and now it's being coded in v1.9.
 // This code is provided here for you to check this implementation
