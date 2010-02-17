3663bcc8bfb636008a6aa633eea8ead68b17950c
chealer <chealer@users.sourceforge.net>
2010-02-17 08:12:01 +0000

    [bp/r25300][FIX] OpenID on machines running httpd as several users.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@25302 b456876b-0849-0410-b77d-98878d47e9d5

--- 3.x/tiki-login_openid.php
+++ 3.x/tiki-login_openid.php
@@ -138,7 +138,7 @@ function &getStore() { // {{{
      * created elsewhere.  After you're done playing with the example
      * script, you'll have to remove this directory manually.
      */
-    $store_path = "/tmp/_php_consumer_test";
+    $store_path = "temp/openid_consumer";
 
     if (!file_exists($store_path) &&
         !mkdir($store_path)) {
