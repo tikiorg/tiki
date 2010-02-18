1899ff6daed55b381dc199c0618630eeba212005
pascalstjean <pascalstjean@users.sourceforge.net>
2010-02-18 19:10:25 +0000

    [MOD] - Added Default value to published Date variable & assign current Date if not set.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@25363 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/blogs/bloglib.php
+++ lib/blogs/bloglib.php
@@ -389,17 +389,21 @@ class BlogLib extends TikiLib
 	 * @access public
 	 * @return int postId
 	 */
-	function blog_post($blogId, $data, $user, $title = '', $contributions = '', $priv = 'n', $created) {
+	function blog_post($blogId, $data, $user, $title = '', $contributions = '', $priv = 'n', $created = 0) {
 		// update tiki_blogs and call activity functions
 		global $smarty, $tikilib, $prefs, $reportslib;
-
+		
+		if(!$created) {
+			$created = $this->now;	
+		}
+		
 		$data = strip_tags($data, '<a><b><i><h1><h2><h3><h4><h5><h6><ul><li><ol><br><p><table><tr><td><img><pre>');
 		$query = "insert into `tiki_blog_posts`(`blogId`,`data`,`created`,`user`,`title`,`priv`) values(?,?,?,?,?,?)";
 		$result = $this->query($query, array((int) $blogId, $data, (int) $created, $user, $title, $priv));
 		$query = "select max(`postId`) from `tiki_blog_posts` where `created`=? and `user`=?";
 		$id = $this->getOne($query, array((int) $created, $user));
 		$query = "update `tiki_blogs` set `lastModif`=?,`posts`=`posts`+1 where `blogId`=?";
-		$result = $this->query($query, array((int) $this->now, (int) $blogId));
+		$result = $this->query($query, array((int) $created, (int) $blogId));
 		$this->add_blog_activity($blogId);
 
 		if ($prefs['feature_user_watches'] == 'y') {
