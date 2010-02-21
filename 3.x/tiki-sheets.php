55cac092b683adaf1a79dcc122817bcc7b359227
xavidp <xavidp@users.sourceforge.net>
2010-02-21 10:43:24 +0000

    [FIX][bp/r25461]Fix bug for users with local perm to admin a sheet but not full admin (re: bug1920 ). Thanks space_egd for reporting the fix.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@25463 b456876b-0849-0410-b77d-98878d47e9d5

--- 3.x/tiki-sheets.php
+++ 3.x/tiki-sheets.php
@@ -77,7 +77,7 @@ if (isset($_REQUEST["edit_mode"]) && $_REQUEST["edit_mode"]) {
 	// Get information about this galleryID and fill smarty variables
 	$smarty->assign('edit_mode', 'y');
 
-	if ($tiki_p_admin == 'y') {
+	if ($tiki_p_admin == 'y' || $tiki_p_admin_sheet == 'y') {
 		$users = $tikilib->list_users(0, -1, 'login_asc', '', false);
 		$smarty->assign_by_ref('users', $users['data']);
 	}
