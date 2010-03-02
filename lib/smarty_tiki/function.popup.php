b203e828bab8244ec0f084b6b70b4f7b64c53f51
pkdille <pkdille@users.sourceforge.net>
2010-03-02 19:59:42 +0000

    [QT] Quality Team Backport
    r25736 | sept_7 | 2010-02-26 15:20:26 +0100 (Fri, 26 Feb 2010) | 5 lines
    [FIX/bp25652] popup smarty plugin : better escaping of the string popup
    (urlencode) Suppress the size limit in the popup, previuously 2000 characters
    by Jonny
    
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/4.x@25819 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.popup.php
+++ lib/smarty_tiki/function.popup.php
@@ -116,7 +116,7 @@ function smarty_function_popup($params, &$smarty)
 		$append .= ',\'' . $trigger . '\'';
 	}
 
-    $retval = $trigger . '="return convertOverlib(this,\''.preg_replace(array("!'!","![\r\n]!"),array("\'",'\r'),$text).'\'';
+	$retval = $trigger . '="return convertOverlib(this,\''.urlencode(TikiLib::htmldecode($text)).'\'';
 	$append = trim($append, ',');
     $retval .= ',[' . $append . ']);"';
 //    if ($trigger == 'onmouseover')
