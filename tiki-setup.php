4f23dd09b20515e07e53cbd8e8f56ec9a0f7ff10
jonnybradley <jonnybradley@users.sourceforge.net>
2010-03-05 18:47:26 +0000

    [FIX] Restore sheet menu and help
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@25941 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup.php
+++ tiki-setup.php
@@ -192,6 +192,11 @@ if ($prefs['javascript_enabled'] == 'y') {
 		$headerlib->add_cssfile( 'lib/jquery/jquery.sheet/jquery.sheet.css' );
 		$headerlib->add_jsfile( 'lib/jquery/jquery.sheet/jquery.sheet.js' );
 		$headerlib->add_jsfile( 'lib/jquery/jquery.json-2.2.js' );
+		// plugins
+		$headerlib->add_cssfile( 'lib/jquery/jquery.sheet/plugins/menu.css' );
+		$headerlib->add_jsfile( 'lib/jquery/jquery.sheet/plugins/mbMenu.min.js' );
+		$headerlib->add_jsfile( 'lib/jquery/jquery.sheet/plugins/jquery.scrollTo-min.js' );
+		$headerlib->add_jsfile( 'lib/jquery/jquery.sheet/plugins/jgcharts.pack.js' );
 	}
 	if( $prefs['feature_jquery_jqs5'] == 'y' ) {
 		if (strpos($_SERVER['PHP_SELF'], 'tiki-index_raw.php') !== false && isset($_REQUEST['format']) && $_REQUEST['format'] == 'jqs5') {
