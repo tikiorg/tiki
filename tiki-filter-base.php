b4757828761a595a83d33c48e9ae91f2eec5e92c
chealer <chealer@users.sourceforge.net>
2010-03-09 15:47:26 +0000

    [FIX] directly called scripts outside of root directory (r25039 regression) oops
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@26018 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-filter-base.php
+++ tiki-filter-base.php
@@ -67,7 +67,7 @@ $tikiroot_relative = '';
 if ($dir_level > 0) {
 	$tikiroot = preg_replace('#(/[^/]+){'.$dir_level.'}$#','',$tikiroot);
 	$tikipath = preg_replace('#(/[^/]+){'.$dir_level.'}$#','',$tikipath);
-	$tikiroot_relative = str_repeat('../',$dir_level+1);
+	$tikiroot_relative = str_repeat('../',$dir_level);
 	chdir($tikiroot_relative);
 }
 
