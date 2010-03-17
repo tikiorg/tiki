56eb77db512b1df612f2ce2d80700ae221d36149
nkoth <nkoth@users.sourceforge.net>
2010-03-17 17:47:41 +0000

    [FIX] even better email obsfucation
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/5.x@26168 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikilib.php
+++ lib/tikilib.php
@@ -5780,7 +5780,10 @@ class TikiLib extends TikiDb_Bridge
 	}
 
 	function protect_email($name, $domain, $sep = '@') {
-		return "<script language=\"Javascript\" type=\"text/javascript\">var sep='$sep';var atsign='@';document.write('<a class=\"wiki\" href=\"mailto:$name'+atsign+'$domain\">$name'+sep+'$domain</a>');</script><noscript>$name ".tra("at","",true)." $domain</noscript>";
+        for ($x=0; $x < strlen($sep); $x++) {
+            $sep_encode = '%' . bin2hex($sep[$x]);
+        }
+		return "<script language=\"Javascript\" type=\"text/javascript\">document.write('<a class=\"wiki\" href=\"mailto:$name'+unescape('$sep_encode')+'$domain\">$name'+unescape('$sep_encode')+'$domain</a>');</script><noscript>$name ".tra("at","",true)." $domain</noscript>";
 	}
 	
 	//Updates a dynamic variable found in some object
