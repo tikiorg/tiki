2d7c088c4f818a10f32467d1dd5fd5707b1b79cf
pkdille <pkdille@users.sourceforge.net>
2010-03-21 15:26:10 +0000

    [MOD] no changes - indenting
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/5.x@26241 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_include_intertiki.php
+++ tiki-admin_include_intertiki.php
@@ -5,9 +5,9 @@
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
 
-//this script may only be included - so its better to die if called directly.
-if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
-	header("location: index.php");
+// This script may only be included - so its better to die if called directly.
+if (strpos($_SERVER['SCRIPT_NAME'], basename(__FILE__)) !== false) {
+	header('location: index.php');
 	exit;
 }
 if (!isset($_REQUEST['interlist'])) $_REQUEST['interlist'] = array();
