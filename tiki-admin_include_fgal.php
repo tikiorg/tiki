0f77346913481f77527d3f70cae35e3760d75302
pkdille <pkdille@users.sourceforge.net>
2010-03-21 14:19:04 +0000

    [MOD] no changes - indenting
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/5.x@26239 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_include_fgal.php
+++ tiki-admin_include_fgal.php
@@ -5,9 +5,9 @@
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
 
-//this script may only be included - so its better to die if called directly.
-if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
-	header("location: index.php");
+// This script may only be included - so its better to die if called directly.
+if (strpos($_SERVER['SCRIPT_NAME'], basename(__FILE__)) !== false) {
+	header('location: index.php');
 	exit;
 }
 include_once ('lib/filegals/filegallib.php');
@@ -53,7 +53,8 @@ if (!empty($_REQUEST['move'])) {
 	}
 	if (!empty($feedbacks)) {
 		$smarty->assign_by_ref('feedbacks', $feedbacks);
-	}}
+	}
+}
 
 if (isset($_REQUEST["filegallistprefs"])) {
 	check_ticket('admin-inc-fgal');
