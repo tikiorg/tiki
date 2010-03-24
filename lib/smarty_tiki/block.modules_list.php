34b13176b9d7843d25dd248c166eb98a62d72cf1
changi67 <changi67@users.sourceforge.net>
2010-03-24 07:52:15 +0000

    [MOD] Just add svn keywords
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/5.x@26284 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/block.modules_list.php
+++ lib/smarty_tiki/block.modules_list.php
@@ -3,7 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: block.tabset.php 25992 2010-03-08 16:46:30Z jonnybradley $
+// $Id$
 
 // this script may only be included - so it's better to die if called directly
 if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
