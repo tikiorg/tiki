545318dbd0350ccdb5f467d9307d337c1e95f22d
chealer <chealer@users.sourceforge.net>
2010-04-03 06:28:20 +0000

    [bp/r26426] Smarty popup function: Simplify treatment of $text param. Newlines in $text would cause an issue with JavaScript but were removed in convertOverlib(), so remove them in smarty_function_popup() instead and get rid of URL encoding and decoding. This get rids of JavaScript unescape(), which is deprecated. [FIX] Corrupted non-ASCII characters in {popup} (r25652 regression)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@26444 b456876b-0849-0410-b77d-98878d47e9d5

--- 4.x/lib/smarty_tiki/function.popup.php
+++ 4.x/lib/smarty_tiki/function.popup.php
@@ -116,7 +116,8 @@ function smarty_function_popup($params, &$smarty)
 		$append .= ',\'' . $trigger . '\'';
 	}
 
-	$retval = $trigger . '="return convertOverlib(this,\''.urlencode(TikiLib::htmldecode($text)).'\'';
+	$text = preg_replace(array('/\\\\r\n/','/\\\\n/','/\\\\r/'), "", $text);	// Remove newlines to avoid JavaScript statement over several lines
+	$retval = $trigger . '="return convertOverlib(this,\''.$text.'\'';
 	$append = trim($append, ',');
     $retval .= ',[' . $append . ']);"';
 //    if ($trigger == 'onmouseover')
