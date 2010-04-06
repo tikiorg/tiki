52b2e5110e1214d15f6af1252b2b3a58490c8b10
sylvieg <sylvieg@users.sourceforge.net>
2010-04-06 12:40:12 +0000

    [FIX]newsletter: fix error db for a newsletter = 2 newsletters where an email is in both but in different case
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/5.x@26476 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/newsletters/nllib.php
+++ lib/newsletters/nllib.php
@@ -145,7 +145,7 @@ class NlLib extends TikiLib
 				}
 				$all_users[$res['email']] = array(
 					'nlId' => (int)$nlId,
-					'email' => $res['email'],
+					'email' => strtolower($res['email']),
 					'code' => $this->genRandomString($res['login']),
 					'valid' => 'y',
 					'subscribed' => $this->now,
@@ -190,6 +190,7 @@ class NlLib extends TikiLib
 		$query = "select * from `tiki_newsletter_subscriptions` where `nlId`=?";
 		$result = $this->query($query, array((int)$nlId));
 		while ( $res = $result->fetchRow() ) {
+			$res['email'] = strtolower($res['email']);
 			if ( ( $res['included'] != 'y' || $res['valid'] == 'x' ) && ((
 					$res['valid'] != 'n' && ( $res['isUser'] != 'g' || $res['valid'] == 'x' ) )
 					|| ( $res['isUser'] == 'g' && in_array($res['email'], $group_users) )
