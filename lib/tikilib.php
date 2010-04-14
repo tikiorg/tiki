6327ebdf2f2f8c348c808aebdc37b07cb8a49b5f
jonnybradley <jonnybradley@users.sourceforge.net>
2010-04-14 15:14:28 +0000

    [MOD] Change calls to convertTZByID over to use setTZbyID (as these functions have been identical since before php 5)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/5.x@26621 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikilib.php
+++ lib/tikilib.php
@@ -7633,7 +7633,7 @@ class TikiLib extends TikiDb_Bridge
 
 		// If user timezone is not also in UTC, convert the date
 		if ( $tz != 'UTC' ) {
-			$tikidate->convertTZByID($tz);
+			$tikidate->setTZbyID($tz);
 		}
 
 		return $tikidate->format($format, $is_strftime_format);
