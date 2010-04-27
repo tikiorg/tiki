dd8ede3a3d95ec7fce5947c8f2d3cb6796f7845f
jonnybradley <jonnybradley@users.sourceforge.net>
2010-04-27 18:11:03 +0000

    [FIX] IP check: explode doesn't use regexp.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/5.x@26796 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikilib.php
+++ lib/tikilib.php
@@ -8415,7 +8415,7 @@ JS;
 	function isValidIP($ip, $ver = 4) {
 		$result = false;
 	
-		$octets = explode("\.", $ip);
+		$octets = explode('.', $ip);
 		if (count($octets) == 4) {
 			for ($c = 0; $c < 4; $c++) {
 				if ($octets[$c] < 0 || $octets[$c] > 255) {
