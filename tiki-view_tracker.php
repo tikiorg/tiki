dc3722c9616586fb9a080d8ff0cbf67bc336e5cc
eromneg <eromneg@users.sourceforge.net>
2010-04-30 15:28:14 +0000

    [FIX] item link issue which wrongly eliminates duplicates when [displayedFieldsList] is used to provide multiple fields separated with a | to display instead of [fieldId]
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@26834 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-view_tracker.php
+++ tiki-view_tracker.php
@@ -379,7 +379,12 @@ for ($i = 0; $i < $temp_max; $i++) {
 				$fields["data"][$i]["value"] = '';
 			}
 			if ($fields["data"][$i]["type"] == 'r') { // item link
-				$fields["data"][$i]["list"] = array_unique($trklib->get_all_items($fields["data"][$i]["options_array"][0], $fields["data"][$i]["options_array"][1], isset($fields['data'][$i]['options_array'][4])?$fields['data'][$i]['options_array'][4]:'poc'));
+				if (!isset($fields["data"][$i]["options_array"][3])) {
+					$fields["data"][$i]["list"] = array_unique($trklib->get_all_items($fields["data"][$i]["options_array"][0], $fields["data"][$i]["options_array"][1], isset($fields['data'][$i]['options_array'][4])?$fields['data'][$i]['options_array'][4]:'poc'));
+				} 
+				else {	
+					$fields["data"][$i]["list"] = $trklib->get_all_items($fields["data"][$i]["options_array"][0], $fields["data"][$i]["options_array"][1]);	
+				}
 				if (isset($fields["data"][$i]["options_array"][3])) $fields["data"][$i]["listdisplay"] = array_unique($trklib->concat_all_items_from_fieldslist($fields["data"][$i]["options_array"][0], $fields["data"][$i]["options_array"][3], isset($fields['data'][$i]['options_array'][4])?$fields['data'][$i]['options_array'][4]:'poc'));
 			} elseif (($fields["data"][$i]["type"] == 'M') && ($fields["data"][$i]["options_array"][0] >= '3')) {
 				if (isset($_FILES["$ins_id"]) && is_uploaded_file($_FILES["$ins_id"]['tmp_name'])) {
