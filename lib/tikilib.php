f3bb6bf08252e2e4d26390d6c02e835661a2c9c0
lphuberdeau <lphuberdeau@users.sourceforge.net>
2010-05-05 17:03:41 +0000

    [ENH] Minor refactoring to help reuse
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/5.x@26910 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikilib.php
+++ lib/tikilib.php
@@ -118,6 +118,25 @@ class TikiLib extends TikiDb_Bridge
 		return $this->getOne("select `dsn`  from `tiki_dsn` where `name`='$name'");
 	}
 
+	function get_dsn_info($name) {
+		$info = array();
+
+		$dsnsqlplugin = $this->get_dsn_by_name($name);
+
+		$parsedsn = $dsnsqlplugin;
+		$info['driver'] = strtok( $parsedsn, ":" );
+		$parsedsn = substr( $parsedsn, strlen($info['driver']) + 3 );
+		$info['user'] = strtok( $parsedsn, ":" );
+		$parsedsn = substr( $parsedsn, strlen($info['user']) + 1 );
+		$info['password'] = strtok( $parsedsn, "@" );
+		$parsedsn = substr( $parsedsn, strlen($info['password']) + 1 );
+		$info['host'] = strtok( $parsedsn, "/" );
+		$parsedsn = substr( $parsedsn, strlen($info['host']) + 1 );
+		$info['database'] = $parsedsn;
+
+		return $info;
+	}
+
 	function get_db_by_name( $name ) {
 		include_once ('tiki-setup.php');
 		if( $name == 'local' || empty($name) ) {
@@ -128,18 +147,12 @@ class TikiLib extends TikiDb_Bridge
 			if( ! isset( $connectionMap[$name] ) ) {
 				$connectionMap[$name] = false;
 
-				$dsnsqlplugin = $this->get_dsn_by_name($name);
-
-				$parsedsn = $dsnsqlplugin;
-				$dbdriver = strtok( $parsedsn, ":" );
-				$parsedsn = substr( $parsedsn, strlen($dbdriver) + 3 );
-				$dbuserid = strtok( $parsedsn, ":" );
-				$parsedsn = substr( $parsedsn, strlen($dbuserid) + 1 );
-				$dbpassword = strtok( $parsedsn, "@" );
-				$parsedsn = substr( $parsedsn, strlen($dbpassword) + 1 );
-				$dbhost = strtok( $parsedsn, "/" );
-				$parsedsn = substr( $parsedsn, strlen($dbhost) + 1 );
-				$database = $parsedsn;
+				$info = $this->get_dsn_info( $name );
+				$dbdriver = $info['driver'];
+				$dbuserid = $info['user'];
+				$dbpassword = $info['password'];
+				$dbhost = $info['host'];
+				$database = $info['database'];
 				
 				$api_tiki = null;
 				require 'db/local.php';				
