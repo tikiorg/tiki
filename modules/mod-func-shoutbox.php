c1310c31d54de8e201891653c483060d6af5a723
cdrwhite <cdrwhite@users.sourceforge.net>
2010-05-10 16:20:11 +0000

    Twitter integration/minor fix in comments
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@27060 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-func-shoutbox.php
+++ modules/mod-func-shoutbox.php
@@ -13,6 +13,14 @@
  * Enable Admin/Wiki/Wiki Features/feature_antibot to prevent spam ("Anonymous editors must input anti-bot code")
  * 
  */
+ 
+/*
+ * Added twitter support (joernott for poiesipedia.com May 2010)
+ *
+ * Requires a site registration with twitter to receive site consumer key/secret.
+ * The user must authorize the site by requesting an oauth token via tiki-socialnetworks.php.
+ *
+ */
 
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER['SCRIPT_NAME'],basename(__FILE__)) !== false) {
@@ -43,6 +51,11 @@ function module_shoutbox_info() {
 				'name' => tra('Maximum messages shown'),
 				'description' => tra('Number of messages to display.') . ' ' . tra('Default:') . ' 5.',
 				'filter' => 'int'
+			),
+			'tweet' => array(
+				'name'=> tra('Tweet'),
+				'description' => tra('If set to "1" and the user has authorized us to tweet messages with Twitter, the user can decide, if he wants to shout via twitter.'),
+				'filter' => 'word'
 			)
 		)
 	);
@@ -56,14 +69,15 @@ function doProcessShout($inFormValues) {
 			$smarty->assign('shout_error', tra('You have mistyped the anti-bot verification code; please try again.'));
 			$smarty->assign_by_ref('shout_msg', $inFormValues['shout_msg']);
 		} else {
-			$shoutboxlib->replace_shoutbox(0, $user, $inFormValues['shout_msg']);
+			
+			$shoutboxlib->replace_shoutbox(0, $user, $inFormValues['shout_msg'],($inFormValues['tweet']==1));
 		}
 	}
 }
 
 function module_shoutbox( $mod_reference, $module_params ) {
 	global $tikilib; require_once ('lib/tikilib.php');
-	global $shoutboxlib, $prefs, $user, $tiki_p_view_shoutbox, $tiki_p_admin_shoutbox, $tiki_p_post_shoutbox, $base_url, $smarty;
+	global $shoutboxlib, $prefs, $user, $tiki_p_view_shoutbox, $tiki_p_admin_shoutbox, $tiki_p_post_shoutbox, $base_url, $smarty, $access;
 	include_once ('lib/shoutbox/shoutboxlib.php');
 
 	if ($tiki_p_view_shoutbox == 'y') {
@@ -122,7 +136,7 @@ function module_shoutbox( $mod_reference, $module_params ) {
 		$smarty->assign('tooltip', isset($module_params['tooltip']) ? $module_params['tooltip'] : 0);
 		$smarty->assign('buttontext', isset($module_params['buttontext']) ? $module_params['buttontext'] : tra('Post'));
 		$smarty->assign('waittext', isset($module_params['waittext']) ? $module_params['waittext'] : tra('Please wait...'));
-		
+		$smarty->assign('tweet', isset($module_params['tweet']) &&($tikilib->get_user_preference($user,'twitter_token')!='') ? $module_params['tweet'] : 0);
 		if ($prefs['feature_ajax'] == 'y') {
 			if (!isset($_REQUEST['xajax'])) {
 				$ajaxlib->registerTemplate('mod-shoutbox.tpl');
