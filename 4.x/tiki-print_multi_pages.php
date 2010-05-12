e7eddee1eb15fa01d4ed4c8bcea815fe4fe290f7
sylvieg <sylvieg@users.sourceforge.net>
2010-05-12 20:31:49 +0000

    [bp/r 27135][FIX]muti print: must setup option page too for TRACKERLIST(filter={{page}})
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@27136 b456876b-0849-0410-b77d-98878d47e9d5

--- 4.x/tiki-print_multi_pages.php
+++ 4.x/tiki-print_multi_pages.php
@@ -46,7 +46,7 @@ if (isset($_REQUEST["print"]) || isset($_REQUEST["display"])) {
 			die;
 		}
 		$page_info = $tikilib->get_page_info($page);
-		$page_info['parsed'] = $tikilib->parse_data($page_info['data'], array('is_html' => $page_info['is_html'], 'print'=>'y'));
+		$page_info['parsed'] = $tikilib->parse_data($page_info['data'], array('is_html' => $page_info['is_html'], 'print'=>'y', 'page'=>$page_info['pageName']));
 		$pages[] = $page_info;
 	}
 	foreach($printstructures as $structureId) {
@@ -56,7 +56,7 @@ if (isset($_REQUEST["print"]) || isset($_REQUEST["display"])) {
 			$page_ref_id = $struct_page['page_ref_id']; //to interpret {toc}
 			if ($struct_page['pos'] != '' && $struct_page['last'] == 1) continue;
 			$page_info = $tikilib->get_page_info($struct_page['pageName']);
-			$page_info['parsed'] = $tikilib->parse_data($page_info['data'], array('is_html' => $page_info['is_html'], 'print' => 'y'));
+			$page_info['parsed'] = $tikilib->parse_data($page_info['data'], array('is_html' => $page_info['is_html'], 'print' => 'y', 'page'=>$page_info['pageName']));
 			$page_info['pos'] = $struct_page['pos'];
 			$page_info['h'] = empty($struct_page['pos']) ? 0 : count(explode('.', $struct_page['pos']));
 			$h = $page_info['h'] + 5;
