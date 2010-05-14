9f22eee121283ce1203d7d9a650793ff19fc71d2
chealer <chealer@users.sourceforge.net>
2010-05-14 23:06:46 +0000

    minor code simplification
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@27187 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/block.self_link.php
+++ lib/smarty_tiki/block.self_link.php
@@ -76,7 +76,7 @@ function smarty_block_self_link($params, $content, &$smarty, $repeat = false) {
 			}
 			// Complete _script path if needed (not empty, not an anchor, ...)
 			if ( !empty($params['_script']) && $params['_script'][0] != '#' && $params['_script'] != 'javascript:void(0)' ) {
-				if ( $params['_script'] != '' && $_SERVER['PHP_SELF'][0] == '/' && strpos($params['_script'], '/') === false ) {
+				if ( $_SERVER['PHP_SELF'][0] == '/' && strpos($params['_script'], '/') === false ) {
 					$self_dir = dirname($_SERVER['PHP_SELF']);
 					$params['_script'] = ( $self_dir == '/' ? '' : $self_dir ).'/'.$params['_script'];
 				}
