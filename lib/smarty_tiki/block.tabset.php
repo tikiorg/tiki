e231875e9157df962c286b258c4da7597b11ef8d
jonnybradley <jonnybradley@users.sourceforge.net>
2010-05-26 18:58:23 +0000

    [FIX] Tabs: Don't return the tabs if there's no content to show in them
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@27347 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/block.tabset.php
+++ lib/smarty_tiki/block.tabset.php
@@ -45,6 +45,10 @@ function smarty_block_tabset($params, $content, &$smarty, &$repeat) {
 		global $smarty_tabset_name, $smarty_tabset;
 		return;
 	} else {
+		$content = trim($content);
+		if (empty($content)) {
+			return '';
+		}
 		$ret = '';
 		//closing
 		if ( $prefs['feature_tabs'] == 'y') {
