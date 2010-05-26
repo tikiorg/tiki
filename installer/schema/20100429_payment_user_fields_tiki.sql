ae69245f3715b1eb7f4d1220eb85dd2d91b5430a
jonnybradley <jonnybradley@users.sourceforge.net>
2010-05-26 18:10:12 +0000

    [FIX] Payment: Rollback r27218 (Payments by non admins should only be visible only to the user that created them) Payments shouldn't be associated (or associatable) with users (apparently)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@27343 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/20100429_payment_user_fields_tiki.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `tiki_payment_requests` ADD `user` VARCHAR( 200 ) NOT NULL DEFAULT '',
-ADD INDEX ( `user` );
-ALTER TABLE `tiki_payment_received` ADD `user` VARCHAR( 200 ) NOT NULL DEFAULT '',
-ADD INDEX ( `user` ) ;
