715c8a466e129bb875b7cc1be6f52b29f68fb43f
jacmoe2 <jacmoe2@users.sourceforge.net>
2010-06-11 09:19:07 +0000

    Oops - accidentally overwrote ereg -> preg quality improvements
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@27589 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_code.php
+++ lib/wiki-plugins/wikiplugin_code.php
@@ -109,7 +109,7 @@ function wikiplugin_code($data, $params) {
 
 		// Remove first <pre> tag
 		if ( $out != '' ) {
-			$out = ereg_replace('^<pre[^>]*>(.*)</pre>$', '\\1', $out);
+			$out = preg_replace('/^<pre[^>]*>(.*)</pre>$/', '\\1', $out);
 			$out = trim($out);
 		}
 
@@ -121,13 +121,13 @@ function wikiplugin_code($data, $params) {
 
 		// Convert &nbsp; into spaces and <br /> tags into real line breaks, since it will be displayed in a <pre> tag
 		$out = str_replace('&nbsp;', ' ', $out);
-		$out = eregi_replace('<br[^>]+>', "\n", $out);
+		$out = preg_replace('/<br[^>]+>/i', "\n", $out);
 
 		// Remove first <code> tag
-		$out = eregi_replace('^\s*<code[^>]*>(.*)</code>$', '\\1', $out);
+		$out = preg_replace("#^\s*<code[^>]*>(.*)</code>$#i", '\\1', $out);
 
 		// Remove spaces after the first tag and before the start of the code
-		$out = ereg_replace("^\s*(<[^>]+>)\n", '\\1', $out);
+		$out = preg_replace("/^\s*(<[^>]+>)\n/", '\\1', $out);
 		$out = trim($out);
 
 		if ( ! $escape_html ) $out = TikiLib::htmldecode($out);
