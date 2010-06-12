266d1e1daee54a152947d2202fa1c0efc5276d12
niclone <niclone@users.sourceforge.net>
2010-06-12 13:35:30 +0000

    [MOD] moved tiki-register controller part (in terms of model/view/controller) to lib, so that it could be reused for alternative registering methods. (my personal goal is to implement registering via intertiki, so more to come...)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@27609 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-register.php
+++ tiki-register.php
@@ -6,8 +6,8 @@
 // $Id$
 
 require_once ('tiki-setup.php');
-include_once ('lib/registration/registrationlib.php');
-include_once ('lib/notifications/notificationlib.php');
+require_once ('lib/registration/registrationlib.php');
+
 $smarty->assign('headtitle', tra('Register'));
 // Permission: needs p_register and not to be a slave
 if ($prefs['allowRegister'] != 'y' || ($prefs['feature_intertiki'] == 'y' && !empty($prefs['feature_intertiki_mymaster']))) {
@@ -34,7 +34,6 @@ if (!isset($_REQUEST['novalidation'])) {
 	$novalidation = $_REQUEST['novalidation'];
 }
 //get custom fields
-$customfields = array();
 $customfields = $registrationlib->get_customfields();
 $smarty->assign_by_ref('customfields', $customfields);
 $listgroups = $userlib->get_groups(0, -1, 'groupName_asc', '', '', 'n');
@@ -58,173 +57,23 @@ if (isset($_REQUEST['register'])) {
 	check_ticket('register');
 	$cookie_name = $prefs['session_cookie_name'];
 
-	if( ini_get('session.use_cookie') && ! isset( $_COOKIE[$cookie_name] ) ) {
-		$smarty->assign('msg',tra("You have to enable cookies to be able to login to this site"));
-		$smarty->display("error.tpl");
-		exit;
-	}
+	if( ini_get('session.use_cookie') && ! isset( $_COOKIE[$cookie_name] ) )
+	    register_error(tra("You have to enable cookies to be able to login to this site"));
 
 	$smarty->assign('errortype', 'no_redirect_login');
-	
-	if (empty($_REQUEST['name'])) {
-		$smarty->assign('msg', tra("Username is required"));
-		$smarty->display("error.tpl");
-		die;	
+
+	$email_valid='y';
+	$result=$registrationlib->register_new_user($_REQUEST);
+	if (is_a($result, 'RegistrationError')) {
+		if ($result->field == 'email' && $result->field == 'email_not_valid') // i'm not sure why email is a special case..
+			$email_valid='n';
+		else
+			register_error($result->msg);
+	} else if (is_string($result)) {
+		$smarty->assign('msg', $result);
+		$smarty->assign('showmsg', 'y');
 	}
-	if (empty($_REQUEST['pass']) && !isset($_SESSION['openid_url'])) {
-		$smarty->assign('msg', tra("Password is required"));
-		$smarty->display("error.tpl");
-		die;	
-	}
-	if ($novalidation != 'yes' and ($_REQUEST["pass"] != $_REQUEST["passAgain"]) and !isset($_SESSION['openid_url'])) {
-		$smarty->assign('msg', tra("The passwords don't match"));
-		$smarty->display("error.tpl");
-		die;
-	}
-	if ($userlib->user_exists($_REQUEST["name"])) {
-		$smarty->assign('msg', tra("User already exists"));
-		$smarty->display("error.tpl");
-		die;
-	}
-	if (!$user && $prefs['rnd_num_reg'] == 'y') {
-		if (!isset($_SESSION['random_number']) || $_SESSION['random_number'] != $_REQUEST['antibotcode']) {
-			$smarty->assign('msg', tra("You have mistyped the anti-bot verification code; please try again."));
-			$smarty->display("error.tpl");
-			die;
-		}
-	}
-	// VALIDATE NAME HERE
-	$n = strtolower($_REQUEST['name']);
-	if ($n == 'admin' || $n == 'anonymous' || $n == 'registered' || $n == strtolower(tra('Anonymous')) || $n == strtolower(tra('Registered'))) {
-		$smarty->assign('msg', tra("Invalid username"));
-		$smarty->display("error.tpl");
-		die;
-	}
-	if (strlen($_REQUEST["name"]) > 200) {
-		$smarty->assign('msg', tra("Username is too long"));
-		$smarty->display("error.tpl");
-		die;
-	}
-	if ($prefs['lowercase_username'] == 'y') {
-		if (preg_match('/[[:upper:]]/', $_REQUEST["name"])) {
-			$smarty->assign('msg', tra("Username cannot contain uppercase letters"));
-			$smarty->display("error.tpl");
-			die;
-		}
-	}
-	//FALTA DEFINIR VALORES PADRÕES PARA AS DUAS VARIÁVEIS!!!
-	if (strlen($_REQUEST["name"]) < $prefs['min_username_length']) {
-		$smarty->assign('msg', tra("Username must be at least") . ' ' . $prefs['min_username_length'] . ' ' . tra("characters long"));
-		$smarty->display("error.tpl");
-		die;
-	}
-	if (strlen($_REQUEST["name"]) > $prefs['max_username_length']) {
-		$smarty->assign('msg', tra("Username cannot contain more than") . ' ' . $prefs['max_username_length'] . ' ' . tra("characters"));
-		$smarty->display("error.tpl");
-		die;
-	}
-	$newPass = $_REQUEST["pass"] ? $_REQUEST["pass"] : $_REQUEST["genepass"];
-	$polerr = $userlib->check_password_policy($newPass);
-	if (!isset($_SESSION['openid_url']) && (strlen($polerr) > 0)) {
-		$smarty->assign('msg', $polerr);
-		$smarty->display("error.tpl");
-		die;
-	}
-	if (!empty($prefs['username_pattern']) && !preg_match($prefs['username_pattern'], $_REQUEST["name"])) {
-		$smarty->assign('msg', tra("Invalid username"));
-		$smarty->display("error.tpl");
-		die;
-	}
-	// Check the mode
-	if ($prefs['useRegisterPasscode'] == 'y') {
-		if ($_REQUEST['passcode'] != $prefs['registerPasscode']) {
-			$smarty->assign('msg', tra("Wrong passcode. You need to know the passcode to register at this site"));
-			$smarty->display("error.tpl");
-			die;
-		}
-	}
-	if ($nbChoiceGroups > 0 && $mandatoryChoiceGroups && empty($_REQUEST['chosenGroup'])) {
-		$smarty->assign('msg', tra('You must choose a group'));
-		$smarty->display("error.tpl");
-		die;
-	}
-	if ($prefs['login_is_email'] == 'y') {
-		if (empty($_REQUEST['novalidation']) || $_REQUEST['novalidation'] != 'yes') {
-			$_POST['email'] = $_REQUEST['email'] = $_REQUEST['name'];
-		} else {
-			$_POST['name'] = $_REQUEST['name'] = $_REQUEST['email'];
-		}
-	}
-	$email_valid = 'y';
-	if (!validate_email($_REQUEST["email"], $prefs['validateEmail'])) {
-		$email_valid = 'n';
-	}
-	if ($email_valid == 'y') {
-		if (isset($_SESSION['openid_url'])) {
-			$openid_url = $_SESSION['openid_url'];
-		} else {
-			$openid_url = '';
-		}
-		if ($prefs['validateUsers'] == 'y' || (isset($prefs['validateRegistration']) && $prefs['validateRegistration'] == 'y')) {
-			$apass = addslashes(md5($tikilib->genPass()));
-			$userlib->send_validation_email($_REQUEST['name'], $apass, $_REQUEST['email'], '', '', isset($_REQUEST['chosenGroup']) ? $_REQUEST['chosenGroup'] : '');
-			$userlib->add_user($_REQUEST["name"], $newPass, $_REQUEST["email"], '', false, $apass, $openid_url , $prefs['validateRegistration'] == 'y'?'a':'u');
-			$logslib->add_log('register', 'created account ' . $_REQUEST["name"]);
-			$smarty->assign('showmsg', 'y');
-		} else {
-			$userlib->add_user($_REQUEST["name"], $newPass, $_REQUEST["email"], '', false, NULL, $openid_url);
-			$logslib->add_log('register', 'created account ' . $_REQUEST["name"]);
-			$smarty->assign('msg', $smarty->fetch('mail/user_welcome_msg.tpl'));
-			$smarty->assign('showmsg', 'y');
-		}
-		if (isset($_REQUEST['chosenGroup']) && $userlib->get_registrationChoice($_REQUEST['chosenGroup']) == 'y') {
-			$userlib->set_default_group($_REQUEST['name'], $_REQUEST['chosenGroup']);
-		} elseif (empty($_REQUEST['chosenGroup'])) {
-			$userlib->set_default_group($_REQUEST['name'], 'Registered'); // to have tiki-user_preferences links par default to the registration tracker
-		}
-		$userlib->set_email_group($_REQUEST['name'], $_REQUEST['email']);
-		// save default user preferences
-		$tikilib->set_user_preference($_REQUEST['name'], 'theme', $prefs['style']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'userbreadCrumb', $prefs['users_prefs_userbreadCrumb']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'language', $prefs['users_prefs_language']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'display_timezone', $prefs['users_prefs_display_timezone']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'user_information', $prefs['users_prefs_user_information']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'user_dbl', $prefs['users_prefs_user_dbl']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'diff_versions', $prefs['users_prefs_diff_versions']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'show_mouseover_user_info', $prefs['users_prefs_show_mouseover_user_info']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'email is public', $prefs['users_prefs_email_is_public']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'mailCharset', $prefs['users_prefs_mailCharset']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'realName', '');
-		$tikilib->set_user_preference($_REQUEST['name'], 'homePage', '');
-		$tikilib->set_user_preference($_REQUEST['name'], 'lat', floatval(0));
-		$tikilib->set_user_preference($_REQUEST['name'], 'lon', floatval(0));
-		$tikilib->set_user_preference($_REQUEST['name'], 'country', '');
-		$tikilib->set_user_preference($_REQUEST['name'], 'mess_maxRecords', $prefs['users_prefs_mess_maxRecords']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'mess_archiveAfter', $prefs['users_prefs_mess_archiveAfter']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'mess_sendReadStatus', $prefs['users_prefs_mess_sendReadStatus']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'minPrio', $prefs['users_prefs_minPrio']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'allowMsgs', $prefs['users_prefs_allowMsgs']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'mytiki_pages', $prefs['users_prefs_mytiki_pages']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'mytiki_blogs', $prefs['users_prefs_mytiki_blogs']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'mytiki_articles', $prefs['users_prefs_mytiki_articles']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'mytiki_gals', $prefs['users_prefs_mytiki_gals']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'mytiki_msgs', $prefs['users_prefs_mytiki_msgs']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'mytiki_tasks', $prefs['users_prefs_mytiki_tasks']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'mytiki_items', $prefs['users_prefs_mytiki_items']);
-		$tikilib->set_user_preference($_REQUEST['name'], 'tasks_maxRecords', $prefs['users_prefs_tasks_maxRecords']);
-		// Custom fields
-		foreach($customfields as $custpref => $prefvalue) {
-			if (isset($_REQUEST[$customfields[$custpref]['prefName']])) $tikilib->set_user_preference($_REQUEST["name"], $customfields[$custpref]['prefName'], $_REQUEST[$customfields[$custpref]['prefName']]);
-		}
-		$emails = $notificationlib->get_mail_events('user_registers', '*');
-		if (count($emails)) {
-			include_once ("lib/notifications/notificationemaillib.php");
-			$smarty->assign('mail_user', $_REQUEST["name"]);
-			$smarty->assign('mail_date', $tikilib->now);
-			$smarty->assign('mail_site', $_SERVER["SERVER_NAME"]);
-			sendEmailNotification($emails, "email", "new_user_notification_subject.tpl", null, "new_user_notification.tpl");
-		}
-	}
+
 }
 
 if ($prefs['userTracker'] == 'y') {
@@ -297,6 +146,13 @@ function chkRegEmail($mail) {
 		$objResponse->assign("ajax_msg_mail", "innerHTML", $pre_yes.tra("Valid Email"));
 	}
 	return $objResponse;
+}
+
+function register_error($msg) {
+	global $smarty;
+	$smarty->assign('msg', $msg);
+	$smarty->display("error.tpl");
+	die;
 }
 
 if (empty($module) || !$module) {
