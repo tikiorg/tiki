72d543f944614ef186f27aaf4c3ff138dc8b55bb
sampaioprimo <sampaioprimo@users.sourceforge.net>
2010-06-17 13:27:48 +0000

    [NEW] replacing old Tiki captcha with Zend_Captcha_Image (commiting the files I forgot to commit yesterday - missing svn add :-))
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@27670 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/prefs/recaptcha.php
@@ -0,0 +1,27 @@
+<?php
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: contact.php 25210 2010-02-14 20:52:22Z changi67 $
+
+function prefs_recaptcha_list() {
+    return array (
+		'recaptcha_enabled' => array(
+            'name' => tra('Use ReCaptcha'),
+            'description' => tra('Use ReCaptcha instead of default CAPTCHA'),
+			'help' => 'Spam+protection',
+			'type' => 'flag',
+        ),
+		'recaptcha_pubkey' => array(
+			'name' => tra('Pubkey'),
+            'type' => 'text',
+            'size' => 40,
+		),
+		'recaptcha_privkey' => array(
+			'name' => tra('Privkey'),
+			'type' => 'text',
+            'size' => 40,
+		),
+	);
+}
