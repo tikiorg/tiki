fb2573587ab3081a4140c2e6faf81bc3662add49
pkdille <pkdille@users.sourceforge.net>
2010-06-20 07:11:20 +0000

    [bp/r27600][FIX] external RSS: An url with more than 256 chars can happen
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/5.x@27709 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki.sql
+++ db/tiki.sql
@@ -3587,17 +3587,17 @@ CREATE TABLE IF NOT EXISTS `tiki_translations_in_progress` (
 
 DROP TABLE IF EXISTS `tiki_rss_items`;
 CREATE TABLE `tiki_rss_items` (
-	`rssItemId` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`rssId` INT NOT NULL,
-	`guid` VARCHAR(255) NOT NULL,
-	`url` VARCHAR(255) NOT NULL,
-	`publication_date` INT UNSIGNED NOT NULL,
-	`title` VARCHAR(255) NOT NULL,
-	`author` VARCHAR(255),
-	`description` TEXT,
-	`content` TEXT,
-	KEY `tiki_rss_items_rss` (`rssId`),
-	UNIQUE `tiki_rss_items_item` (`rssId`, `guid`)
+    `rssItemId` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `rssId` INT NOT NULL,
+    `guid` VARCHAR(255) NOT NULL,
+    `url` TEXT NOT NULL,
+    `publication_date` INT UNSIGNED NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `author` VARCHAR(255),
+    `description` TEXT,
+    `content` TEXT,
+    KEY `tiki_rss_items_rss` (`rssId`),
+    UNIQUE `tiki_rss_items_item` (`rssId`, `guid`)
 );
 
 DROP TABLE IF EXISTS `tiki_object_attributes`;
