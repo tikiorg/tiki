1291c554b51fea1c2489840129d02c0e7f5a331b
chealer <chealer@users.sourceforge.net>
2010-07-07 06:58:10 +0000

    Web PI post-install: set dbversion_tiki
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/5.x@27879 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-finish_install.php
+++ tiki-finish_install.php
@@ -7,10 +7,14 @@
 
 // This is not part of the traditional installer. At the moment this is only used to finish the install when using the Microsoft Web Platform Installer.
 
-// For now, this just hashes the admin password.
 require_once('tiki-setup.php');
 if ($password = $tikilib->getOne("SELECT `password` FROM `users_users` WHERE `userId`=1 AND `hash`='DummyHashForInstallation'")) {
+	// Hashes the admin password
 	$query = "UPDATE `users_users` SET `hash`=?, `password`=NULL WHERE `userId`=1 AND `hash`='DummyHashForInstallation'";
 	$tikilib->query($query, array($userlib->hash_pass($password)));
+
+	// Sets dbversion_tiki
+	$db = fopen('db/local.php', 'a');
+	fwrite($db, "\n\$dbversion_tiki='" . $TWV->getVersion() . "';\n");
 }
 header("location: tiki-index.php");
