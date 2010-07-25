8fad664292a2c86bfe7df12d4eb4c2e8f268b279
jonnybradley <jonnybradley@users.sourceforge.net>
2010-07-25 18:25:11 +0000

    [DEV] Fixes for convert_tpl_ajax.php - better, but still not reliable
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@28128 b456876b-0849-0410-b77d-98878d47e9d5

--- doc/devtools/convert_tpl_ajax.php
+++ doc/devtools/convert_tpl_ajax.php
@@ -4,19 +4,22 @@
  * simple script to convert html anchors in smarty templates to self_links for ajax (tiki 6)
  * couldn't work out a clever enough regexp so trying in php
  *
- * ***** N.B. Move into root of tiki and run in a browser ******
+ * ***** N.B. Move into root of tiki and run in a browser  ******
  * 
+ * ***** N.B. 2: {if} statements inside anchor attributes  ******
+ * ***** will be commented out for later manual correction ******
  */
 
 // just in case
 require_once ('tiki-setup.php');
 $access->check_permission('tiki_p_admin');
+$access->check_feature('javascript_enabled');
 
 $tpl = $_GET['tpl'];
 
-if (empty($tpl)) {
-	$access->display_error('', 'no tpl');
-}
+//if (empty($tpl)) {
+//	$access->display_error('', 'no tpl');
+//}
 
 function replace_with_self_links($original, $template_base) {
 
@@ -27,40 +30,70 @@ function replace_with_self_links($original, $template_base) {
 		$ahref = $phplinks[0][$j];
 		preg_match_all('/([^=\s]*?)="([^"]*?)"/i', $ahref, $attrs);
 		
+		$str = '{self_link ';
 		for($i = 0; $i < count($attrs[1]); $i++) {
 			
-			$str = '{self_link ';
 			if (strtolower($attrs[1][$i]) == 'href') {
 				$query = parse_url(urldecode(str_replace('&amp;', '&', $attrs[2][$i])));
 				if ($query['path'] != $template_base . '.php') {
 					$str .= ' _script="' . $query['path'] . '" ';
 				}
-				$str .= implode(' ', explode('&', $query['query'])) . ' ';
+				$vars = explode('&', $query['query']);
+				foreach($vars as &$var) {
+					$ar = explode( '=', $var);
+					$var = $ar[0] . '=' . process_value( $ar[1] );
+				}
+				$str .= implode(' ', $vars) . ' ';
 			} else {
-
-				$str .= '_' . $attrs[1][$i] . '=' . $attrs[2][$i] . ' ';
+				$str .= '_' . $attrs[1][$i] . '=' . process_value( $attrs[2][$i] ) . ' ';
 			}
-			$str = trim($str) . '}' . $phplinks[1][$j] . '{/self_link}';
-				
-			$replacements[] = $str;
 		}
-
-		$replaced = str_replace($phplinks[0], $replacements, $original);
+		$str = trim($str) . '}' . $phplinks[1][$j] . '{/self_link}';	
+		$replacements[] = $str;
+		
 	}
+	$replaced = str_replace($phplinks[0], $replacements, $original);
 	return $replaced;
 }
 
+function process_value ($var) {
+	if (strpos( $var, '{$' ) === 0) {
+		$var =  trim( $var, '{}');
+		$q = '';
+	} else {
+		$q = '"';
+	}
+	// comment out if's inside attributes for manual processing
+	$var = preg_replace(array('/\{if\s([^\}]*)\}/i', '/\{else\}/i', '/\{\/if\}/i', '/\{elseif\s([^\}]*)\}/i'),
+						array('{*if $1*}',           '{*else*}',    '{*/if*}',     '/{*elseif $1*}/'), $var);
+	$var = "$q" . $var . "$q";
+	return $var;
+}
+
 $markup = file_get_contents('templates/' . $tpl . '.tpl');
-$checked = isset($_REQUEST['toggle']) ? ' checked=\'checked\'' : '';
+$checked = !empty($_REQUEST['toggle']) ? ' checked=\'checked\'' : '';
 if (!empty($checked)) {
 	$markup = replace_with_self_links($markup, $tpl);
 }
+
+ $fp = opendir('templates/');
+
+$tpl_sel = '<select name=\'tpl\' id=\'tpl\' onclick=\'this.form.submit();\'><option>Select tpl</option>';
+while(false !== ($f = readdir($fp))) {
+	preg_match('/^(.*)\.tpl$/', $f, $m);
+	if (count($m) > 0) {
+		$tpl_sel .= '<option value=\'' . $m[1] . '\'';
+		$tpl_sel .= $m[1] == $tpl ? ' selected=\"selected\">' : '>';
+		$tpl_sel .= $m[1] . '.tpl</option>';
+	}
+}
+$tpl_sel .= '</select>';
 
 // cheating - lazy ;)
 $form = str_replace("\n", '', "<form action='#'>
 <label for='toggle'>Show replacements:</label>
 <input type='checkbox' id='toggle' name='toggle' onclick='this.form.submit();'$checked />
-<input type='hidden' name='tpl' value='$tpl' />
+$tpl_sel
 </form>");
 
 $headerlib->add_jq_onready(<<<JS
