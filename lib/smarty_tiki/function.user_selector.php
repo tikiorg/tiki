b4745c2dac330585f4ab17434e59bff74d7a0c72
jonnybradley <jonnybradley@users.sourceforge.net>
2010-07-28 16:31:09 +0000

    [FIX] User selector smarty function: Use more efficient count of users to test (not just listing them all!) and make "editable" param y/n to match perms. Also, don't render out the other user options if not editable.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@28183 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.user_selector.php
+++ lib/smarty_tiki/function.user_selector.php
@@ -25,36 +25,45 @@ function smarty_function_user_selector($params, &$smarty) {
 	static $iUserSelector = 0;
 	$iUserSelector++;
 	
-	$defaults = array( 'user' => $user, 'group' => 'all', name => 'user', id => 'user_selector_' . $iUserSelector, 'editable' => $tiki_p_admin == 'y');
+	$defaults = array( 'user' => $user, 'group' => 'all', name => 'user', id => 'user_selector_' . $iUserSelector, 'editable' => $tiki_p_admin);
 	$params = array_merge($defaults, $params);
 	if (isset($params['size'])) {
 		$sz = ' size="' . $params['size'] . '"';
 	} else {
 		$sz = '';
 	}
-	if (!$params['editable']) {
+	if ($params['editable'] != 'y') {
 		$ed = ' disabled="disabled"';
 	} else {
 		$ed = '';
 	}
+	
 	if ($params['group'] == 'all') {
-		$usrs = $tikilib->list_users(0, -1, 'login_asc');
-		$users = array();
-		foreach ($usrs['data'] as $usr) {
-			$users[] = $usr['login'];
-		}
+		$ucant = $tikilib->list_users(0, 0, 'login_asc');
+		$ucant = $ucant['cant'];
 	} else {
-		$users = $userlib->get_group_users($params['group']);
+		$ucant = $userlib->count_users($params['group']);
 	}
 	$ret = '';
 	
-	if ($prefs['feature_jquery_autocomplete'] == 'y' && count($users) > $prefs['user_selector_threshold']) {
+	if ($prefs['feature_jquery_autocomplete'] == 'y' && $ucant > $prefs['user_selector_threshold']) {
 		$ret .= '<input id="' . $params['id'] . '" type="text" name="' . $params['name'] . '" value="' . $params['user'] . '"' . $sz . $ed . ' />';
 		$headerlib->add_jq_onready('$jq("#' . $params['id'] . '").tiki("autocomplete", "username", {mustMatch: true});');
 	} else {
+		if ($params['group'] == 'all') {
+			$usrs = $tikilib->list_users(0, -1, 'login_asc');
+			$users = array();
+			foreach ($usrs['data'] as $usr) {
+				$users[] = $usr['login'];
+			}
+		} else {
+			$users = $userlib->get_group_users($params['group']);
+		}
 		$ret .= '<select name="' . $params['name'] . '" id="' . $params['id'] . '"' . $sz . $ed . '>';
 		foreach($users as $usr) {
-			$ret .= '<option value="' . $usr . '"' . ($usr == $params['user'] ? ' selected="selected"' : '') . ' >' . $usr .'</option>';
+			if ($params['editable'] == 'y' || $usr == $params['user']) {
+				$ret .= '<option value="' . $usr . '"' . ($usr == $params['user'] ? ' selected="selected"' : '') . ' >' . $usr .'</option>';
+			}
 		}
 		$ret .= '</select>';
 	}
