178cb04de11c881a9d3723c18e7b936c89459661
jonnybradley <jonnybradley@users.sourceforge.net>
2010-08-11 18:17:14 +0000

    [MOD] autosave: fade preview page while loading for some feedback
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@28457 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-auto_save.php
+++ tiki-auto_save.php
@@ -35,6 +35,7 @@ if (isset($_REQUEST['editor_id']) && isset($_REQUEST['data'])) {
 	if (!empty($user)) {
 		$headerlib->add_js('
 function get_new_preview() {
+	$("body").css("opacity", 0.6);
 	location.replace("' . $tikiroot . 'tiki-auto_save.php?editor_id=' . $_REQUEST['editor_id'] . '&autoSaveId=' . $_REQUEST['autoSaveId'] . '");
 }
 $(window).load(function(){
