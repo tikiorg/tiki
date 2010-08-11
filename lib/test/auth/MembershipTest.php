e9c90ad16de8818b2fe8dc2eed193df2c3ec6d92
sampaioprimo <sampaioprimo@users.sourceforge.net>
2010-08-11 20:02:50 +0000

    using new instance of userlib for test class instead of global userlib to avoid time conflict ($userlib->now) when running all tests at once
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@28461 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/test/auth/MembershipTest.php
+++ lib/test/auth/MembershipTest.php
@@ -4,71 +4,72 @@
  */
 class MembershipTest extends TikiTestCase
 {
+
+	protected $userlib;
+
 	function setUp() {
 		parent::setUp();
-		global $userlib;
 		global $cachelib;
 
-		$cachelib->invalidate( 'grouplist' );
-
-		$userlib->remove_user( 'membershiptest_a' );
-		$userlib->remove_user( 'membershiptest_b' );
-		$userlib->remove_user( 'membershiptest_c' );
-		$userlib->remove_group( 'MembershipTest' );
+		$this->userlib = new UsersLib();
 
 		$cachelib->invalidate( 'grouplist' );
 
-		$userlib->add_group( 'MembershipTest', '', '', 0, 0, '', '', 0, '', 0, 0, 'n', 45 );
+		$this->userlib->remove_user( 'membershiptest_a' );
+		$this->userlib->remove_user( 'membershiptest_b' );
+		$this->userlib->remove_user( 'membershiptest_c' );
+		$this->userlib->remove_group( 'MembershipTest' );
 
-		$userlib->add_user( 'membershiptest_a', 'abc', 'a@example.com' );
-		$userlib->add_user( 'membershiptest_b', 'abc', 'a@example.com' );
-		$userlib->add_user( 'membershiptest_c', 'abc', 'a@example.com' );
+		$cachelib->invalidate( 'grouplist' );
 
-		$userlib->assign_user_to_group( 'membershiptest_a', 'MembershipTest' );
-		$userlib->assign_user_to_group( 'membershiptest_b', 'MembershipTest' );
+		$this->userlib->add_group( 'MembershipTest', '', '', 0, 0, '', '', 0, '', 0, 0, 'n', 45 );
+
+		$this->userlib->add_user( 'membershiptest_a', 'abc', 'a@example.com' );
+		$this->userlib->add_user( 'membershiptest_b', 'abc', 'a@example.com' );
+		$this->userlib->add_user( 'membershiptest_c', 'abc', 'a@example.com' );
+
+		$this->userlib->assign_user_to_group( 'membershiptest_a', 'MembershipTest' );
+		$this->userlib->assign_user_to_group( 'membershiptest_b', 'MembershipTest' );
 	}
 
 	function tearDown() {
 		parent::tearDown();
-		global $userlib;
-		$userlib->remove_user( 'membershiptest_a' );
-		$userlib->remove_user( 'membershiptest_b' );
-		$userlib->remove_user( 'membershiptest_c' );
-		$userlib->remove_group( 'MembershipTest' );
+		$this->userlib->remove_user( 'membershiptest_a' );
+		$this->userlib->remove_user( 'membershiptest_b' );
+		$this->userlib->remove_user( 'membershiptest_c' );
+		$this->userlib->remove_group( 'MembershipTest' );
 	}
 
 	function testExtendMembership() {
-		global $userlib;
 		
-		$userlib->extend_membership( 'membershiptest_a', 'MembershipTest', 3 );
+		$this->userlib->extend_membership( 'membershiptest_a', 'MembershipTest', 3 );
 
-		$expect = $userlib->now + 45 * 2 * ( 3600 * 24 );
+		$expect = $this->userlib->now + 45 * 2 * ( 3600 * 24 );
 
 		$this->assertEquals( $expect, 
-			$userlib->getOne( 'SELECT `created` FROM `users_usergroups` WHERE `userId` = ? AND `groupName` = "MembershipTest"',
-				array( $userlib->get_user_id( 'membershiptest_a' ) )
+			$this->userlib->getOne( 'SELECT `created` FROM `users_usergroups` WHERE `userId` = ? AND `groupName` = "MembershipTest"',
+				array( $this->userlib->get_user_id( 'membershiptest_a' ) )
 			)
 		);
-		$this->assertEquals( $userlib->now, 
-			$userlib->getOne( 'SELECT `created` FROM `users_usergroups` WHERE `userId` = ? AND `groupName` = "MembershipTest"',
-				array( $userlib->get_user_id( 'membershiptest_b' ) )
+		$this->assertEquals( $this->userlib->now, 
+			$this->userlib->getOne( 'SELECT `created` FROM `users_usergroups` WHERE `userId` = ? AND `groupName` = "MembershipTest"',
+				array( $this->userlib->get_user_id( 'membershiptest_b' ) )
 			)
 		);
 	}
 
 	function testExtendExpiredMembership() {
-		global $userlib;
-		$id = $userlib->get_user_id( 'membershiptest_b' );
+		$id = $this->userlib->get_user_id( 'membershiptest_b' );
 
-		$userlib->query( 'UPDATE `users_usergroups` SET `created` = `created` - 12*3600 - 45*24*3600 WHERE `userId` = ?', array( $id ) );
+		$this->userlib->query( 'UPDATE `users_usergroups` SET `created` = `created` - 12*3600 - 45*24*3600 WHERE `userId` = ?', array( $id ) );
 
-		$userlib->extend_membership( 'membershiptest_b', 'MembershipTest', 2 );
+		$this->userlib->extend_membership( 'membershiptest_b', 'MembershipTest', 2 );
 
-		$expect = $userlib->now + 45 * ( 3600 * 24 );
+		$expect = $this->userlib->now + 45 * ( 3600 * 24 );
 
 		$this->assertEquals( $expect, 
-			$userlib->getOne( 'SELECT `created` FROM `users_usergroups` WHERE `userId` = ? AND `groupName` = "MembershipTest"',
-				array( $userlib->get_user_id( 'membershiptest_b' ) )
+			$this->userlib->getOne( 'SELECT `created` FROM `users_usergroups` WHERE `userId` = ? AND `groupName` = "MembershipTest"',
+				array( $this->userlib->get_user_id( 'membershiptest_b' ) )
 			)
 		);
 	}
