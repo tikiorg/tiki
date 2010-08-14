e267ee3af33746e44ae04e38db10c14f0ce37497
jonnybradley <jonnybradley@users.sourceforge.net>
2010-08-14 17:59:52 +0000

    [MOD] ckeditor: Significant refactoring and simplification of autosave plugin. Also added standard tiki copyright.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@28508 b456876b-0849-0410-b77d-98878d47e9d5

--- wysiwyg_ckeditor_update/lib/ckeditor_tiki/plugins/autosave/plugin.js
+++ wysiwyg_ckeditor_update/lib/ckeditor_tiki/plugins/autosave/plugin.js
@@ -1,140 +1,127 @@
-/*
- * Licensed under the terms of the GNU Lesser General Public License:
- *   http://www.opensource.org/licenses/lgpl-license.php
- * 
- * File Name: plugin.js
- *   Plugin to post the editor's content to the server through AJAX 
- * 
- * File Authors:
- *   Mike Tonks (http://greenmap.sourceforge.net/fck_autosave.html)
- *              (adapted from ajaxPost by)
- *              Paul Moers (http://www.saulmade.nl, http://www.saulmade.nl/FCKeditor/FCKPlugins.php)
- *   
- *   sept_7 for tiki 4?/fckeditor ????
- *   
- *   jonnyb for Tiki 6/ckeditor aug 2010
- *   added (modified) contents of ajaxAutoSave.js to this file
- *  
- * */
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id:  $
 
 CKEDITOR.plugins.add( 'autosave',
 {
-	init : function( editor )
-	{
-		this.button = {
-				label : 'Auto Save',
-				command : 'autosave',
+	// declare property to hold the state
+	ajaxAutoSaveIsDirty: false,
+	// declare a counter to give us a few keystrokes leeway
+	ajaxAutoSaveCounter: 0,
+	// declare a status flag so we know if a draft has been saved
+	ajaxAutoSaveDraftSaved: false,
+	// shortcut to my button
+	button: null,
+	editor: null,
+
+	init: function( editor ) {
+		
+		asplugin = this;	// for closure references later
+		this.editor = editor;
+	
+		editor.ui.addButton( 'autosave', {
+				label: 'Auto Save',
+				command: 'autosave',
 				icon: this.path + 'images/ajaxAutoSaveClean.gif'
 
-			};
-		editor.ui.addButton( 'autosave', this.button );
+			} );
 		
-		this.command = editor.addCommand( 'autosave', new CKEDITOR.command( editor ,
+		command = editor.addCommand( 'autosave', new CKEDITOR.command( editor ,
 			{
 				modes: { wysiwyg:1, source:1 },
 				canUndo: false,
+				// button clicked or timer
 				exec: function(elem, editor, data) {
-					//if (!confirm("cancel=debug")){debugger;}
-					
-					editor.plugins.autosave.autoSaveManager.exec();
+					return asplugin.doAjaxSave(editor);
 				}
 			} ));
-
-		this.autoSaveManager = new AutoSaveManager( editor, this );
-	}
-});
-
-// AutoSaveManager constructor
-var AutoSaveManager = function(an_editor, a_plugin) {
-	// references
-	this.editor = an_editor;
-	this.plugin = a_plugin;
 		
-	// declare a global variable to hold the state
-	this.ajaxAutoSaveIsDirty = false;
-	
-	// declare a counter to give us a few keystrokes leeway
-	this.ajaxAutoSaveCounter = 0;
-	
-	// declare a status flag so we know if a draft has been saved
-	this.ajaxAutoSaveDraftSaved = false;
-	
-	// preload toolbar loading image
-	var tempNode = new Image();
-	tempNode.src = this.plugin.path + "images/loadingSmall.gif";
-	
-	// instantiate autosave Object
-	this.ajaxAutoSaveObject = new AxpObject(this.editor);
-	
-	this.editor.element.$.form.onsubmit = function() {
-		a_plugin.autoSaveManager.onSave( an_editor );
-	};
-	
-	//var plugs = this.plugin;
-	this.editor.on( 'contentDom', function() {
-		this.document.on('keydown', function(event) {
-			// Do not capture CTRL hotkeys.
-			if (!event.data.$.ctrlKey && !event.data.$.metaKey) {
-				a_plugin.autoSaveManager.onSelectionChange(this);
-			}
+		// preload toolbar loading image
+		var tempNode = new Image();
+		tempNode.src = this.path + "images/loadingSmall.gif";
+		
+		editor.element.$.form.onsubmit = function() {
+			asplugin.onSave( editor );
+		};
+		
+		//var plugs = this.plugin;
+		editor.on( 'contentDom', function() {
+			this.document.on('keydown', function(event) {
+				// Do not capture CTRL hotkeys.
+				if (!event.data.$.ctrlKey && !event.data.$.metaKey) {
+					asplugin.onSelectionChange(editor);
+				}
+			});
 		});
-	});
+	
+		editor.on('blur', function(event) {
+			asplugin.doAjaxSave(editor);
+		});
+	},	// end init
 
-	this.editor.on('blur', function(event) {
-		this.plugins.autosave.autoSaveManager.exec();
-	});
-};
-
-AutoSaveManager.prototype = {
-
-	init: function () {
-		this.plugin.button = this.editor.getCommand("autosave").uiItems[0];
-	},
-	// what do we do when the button is clicked
-	exec: function() {
-		
-		this.init();
-		this.changeIcon( "loadingSmall.gif" );
-		
-		// save
-		this.ajaxAutoSaveObject.post();
-		
-		// reset state
-		this.ajaxAutoSaveIsDirty = false;
-		this.ajaxAutoSaveCounter = 0;
-		this.ajaxAutoSaveDraftSaved = true;
+	doAjaxSave: function (editor) {
+		if (this.ajaxAutoSaveIsDirty) {
+			this.changeIcon("loadingSmall.gif");
+			
+			var asplugin = this;
+			jQuery.ajax({
+				url: CKEDITOR.config.ajaxAutoSaveTargetUrl,
+				data: 'script=' + editor.config.autoSaveSelf + '&editor_id=' + editor.name + '&data=' + encodeURIComponent(editor.getData()),
+				// good callback
+				success: function(data) {
+				
+					// update AJAX preview if there
+					if (parent.window.ajaxPreviewWindow && typeof parent.window.ajaxPreviewWindow.get_new_preview === 'function') {
+						parent.window.ajaxPreviewWindow.get_new_preview();
+					}
+					// reset state
+					asplugin.ajaxAutoSaveIsDirty = false;
+					asplugin.ajaxAutoSaveCounter = 0;
+					asplugin.ajaxAutoSaveDraftSaved = true;
+					// show
+					asplugin.changeIcon("tick_animated.gif");
+					// clear anim
+					setTimeout(function() {
+						asplugin.resetAjaxAutoSaveTool();
+					}, 2000);
+					
+				},
+				// bad callback - no good info in the params :(
+				error: function(req, status, error) {
+					asplugin.changeIcon("cross_animated.gif"); // just leave a cross there
+				}
+			});
+		}
 		return true;
 	},
 	
 	onSave: function( editor ) {
 		this.ajaxAutoSaveIsDirty = false;
-		// added by jonnyb for tiki 4 - remove draft when page saved
-		//var n = ajaxAutoSaveObject.editorInstance.Name;
+		// remove draft when page saved
 		if (parent && typeof parent.xajax_remove_save === 'function') {
 			parent.xajax_remove_save(editor.name, editor.config.autoSaveSelf);
 		}
 		return true;
-		
 	},
 	
 	// what to do when the fckeditor content is changed
-	onSelectionChange: function() {
+	onSelectionChange: function( editor ) {
 		
-		var asm;
-		this.init();
+		var asplugin;
 		
 		if (!this.ajaxAutoSaveIsDirty) {
 			this.changeIcon("ajaxAutoSaveDirty.gif");
-			//this.plugin.button.label = FCKLang.ajaxAutoSaveButtonTitle;
 			
 			this.ajaxAutoSaveCounter++;
 			
 			if (this.ajaxAutoSaveCounter > CKEDITOR.config.ajaxAutoSaveSensitivity) {
-				if (!asm) {
-					asm = this;
+				if (!asplugin) {
+					asplugin = this;
 					setTimeout(function() {
-						asm.exec();
-						asm = null;
+						asplugin.doAjaxSave( editor );
+						asplugin = null;
 					}, CKEDITOR.config.ajaxAutoSaveRefreshTime * 1000);
 					
 					this.ajaxAutoSaveIsDirty = true;
@@ -144,191 +131,31 @@ AutoSaveManager.prototype = {
 		return true;
 	},
 	
+	getButton: function () {
+		if (!this.button) {
+			this.button = this.editor.getCommand("autosave").uiItems[0];
+		}
+	},
+
 	changeIcon: function( fileName ) {
-		// use of jquery - must be a better ck-way of doing this
-		var $img = jQuery("#" + this.plugin.button._.id + " span:first");
-		$img.css( "background-image", $img.css( "background-image" ).replace(/[^\/\)]*\)$/, fileName + ")"));
-	}
+		this.getButton();
+		// use of jquery - must be a better "ck-way" of doing this
+		var $img = jQuery("#" + this.button._.id + " span:first");
+		$img.css( "background-image", $img.css( "background-image" ).replace(/[^\/]*\.gif/i, fileName));
+	},
 	
-};
-
-//
-//// manage the plugins' button behavior
-//AutoSaveManager.prototype.getState = function ()
-//{
-//	return FCK_TRISTATE_OFF;
-//};
-
-
-// previously was ajaxAutoSave.js separate file - now combined
-
-/*
- * Licensed under the terms of the GNU Lesser General Public License:
- * http://www.opensource.org/licenses/lgpl-license.php
- * 
- * File Name: ajaxAutoSave.js
- * ajaxAutoSave Object.
- * 
- * File Authors:
- *  Mike Tonks (http://greenmap.sourceforge.net/fck_autosave.html)
- * 	(adapted from ajaxPost by)
- * 	Paul Moers (http://www.saulmade.nl, http://www.saulmade.nl/FCKeditor/FCKPlugins.php)
- * 
- *  Updated for ckeditor/tiki6 jonnyb 2010
-*/
-
-
-// AxpObject constructor
-var AxpObject = function (editorInstance) {
+	setMessage: function(errorMessage, errorData) {
+		var message;
+		
+		message = errorMessage + (errorData ? ' ' + errorData : '');
+		this.button.label = message;	// doesn't seem to...
+	},
 	
-	this.editorInstance	= editorInstance;
-	this.isDirty = false;
+	resetAjaxAutoSaveTool: function() {
+		this.changeIcon( "ajaxAutoSaveClean.gif" );
+		this.button.label = "Auto Save";
+	}
 
-//	if (editorInstance.EditorWindow) {
-//		this.FCKLang	= editorInstance.EditorWindow.parent.FCKLang;
-//	} else {
-//		this.FCKLang	= "";
-//	}
-
-};
-
-
-// initialize
-AxpObject.prototype.initialize = function () {
 	
-	this.asManager = this.editorInstance.plugins.autosave.autoSaveManager;
 
-	// create requestObject
-	if (window.XMLHttpRequest) // Mozilla, Safari, IE7, ...
-	{
-		requestObject = new XMLHttpRequest();
-	}
-	else if (window.ActiveXObject) // IE
-	{
-		requestObject = new ActiveXObject('MsXml2.XmlHttp');
-	}
-	this.requestObject = requestObject;
-
-	// set function to do on completion of the request
-	this.requestObject.onreadystatechange = this.onReadyStateChange;
-	this.requestObject.parentObject = this;
-};
-
-
-// post
-AxpObject.prototype.post = function () {
-	// set up the requestObject
-	this.initialize();
-
-	// make request
-	this.requestObject.open('POST', CKEDITOR.config.ajaxAutoSaveTargetUrl, true);
-	this.requestObject.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	this.requestObject.send('script=' + this.editorInstance.config.autoSaveSelf + '&editor_id=' + this.editorInstance.name + '&data=' + encodeURIComponent(this.editorInstance.getData()));
-
-	// update AJAX preview if there
-
-	if (parent.window.ajaxPreviewWindow && typeof parent.window.ajaxPreviewWindow.get_new_preview === 'function') {
-		parent.window.ajaxPreviewWindow.get_new_preview();
-	}
-};
-
-
-// the readystatechange event
-AxpObject.prototype.onReadyStateChange = function( event ) {
-	var requestObject = event.target;
-	
-	if (requestObject.readyState === 4) {
-		if (requestObject.status === 200 && requestObject.responseXML) {
-			// error node available?
-			var errorNode = requestObject.responseXML.getElementsByTagName('error')[0];
-			var resultNode = requestObject.responseXML.getElementsByTagName('result')[0];
-			if (errorNode) {
-				this.parentObject.feedback(errorNode.attributes.getNamedItem('errorNumber').value, errorNode.attributes.getNamedItem('errorData').value);
-			}   // success
-			else if (resultNode) {
-				this.parentObject.feedback(0);
-			} else {
-				this.parentObject.feedback(104, 'ajaxAutoSaveRequestedURL: ' + CKEDITOR.config.ajaxAutoSaveTargetUrl + '<br />' + 'ajaxAutoSaveResponseText:<br />' + this.requestObject.responseText);
-			}
-		} else {
-			this.parentObject.feedback(105, this.requestObject.statusText + ' (' + this.requestObject.status + ')');
-		}
-	}
-};
-
-// feedback
-AxpObject.prototype.feedback = function (errorNumber, errorData)
-{
-// set toolbar icon and re-enable editor
-	if (this.asManager)
-	{
-		if (parseInt(errorNumber, 10) > 0)
-		{
-			this.asManager.changeIcon( "cross_animated.gif" );
-		}
-		else
-		{
-			this.asManager.changeIcon( "tick_animated.gif" );
-		}
-	}
-
-	switch (parseInt(errorNumber, 10))
-	{
-		case 0:
-			var now = new Date();
-			var hours = now.getHours();
-			var mins = now.getMinutes();
-			if (hours < 11) {
-				hours = "0" + hours;
-			}
-			if (mins < 11) {
-				mins = "0" + mins;
-			}
-			this.setMessage("ajaxAutoSaveSaveCompleted  " + hours + ":" + mins);
-			break;
-		case 101:
-			this.setMessage("ajaxAutoSaveNoContentReceived", errorData);
-			break;
-		case 102:
-			this.setMessage("ajaxAutoSaveDBConnectError", errorData);
-			break;
-		case 103:
-			this.setMessage("ajaxAutoSaveQueryError", errorData);
-			break;
-		case 104:
-			this.setMessage("ajaxAutoSaveErrMssgBadXMLResponse", errorData);
-			break;
-		case 105:
-			this.setMessage("ajaxAutoSaveErrMssgXMLRequestError", errorData);
-			break;
-		default: 
-			this.setMessage("ajaxAutoSaveErrMssgDefault" + ' ' + errorNumber, errorData);
-			break;
-	}
-
-	var asm = this.asManager;
-	var delay = (parseInt(errorNumber, 10) > 0) ? 8000 : 2000;
-	setTimeout( function() {
-		if (asm) {
-			asm.ajaxAutoSaveObject.resetAjaxAutoSaveTool();
-		}
-	}, delay);
-};
-
-// set message
-AxpObject.prototype.setMessage = function(errorMessage, errorData) {
-	var message;
-	
-	message = errorMessage + (errorData ? ' ' + errorData : '');
-	if (this.asManager) {
-		this.asManager.plugin.button.label = message;
-	} else {
-		//alert(message);
-		window.status = message;
-	}
-};
-
-AxpObject.prototype.resetAjaxAutoSaveTool = function() {
-	this.asManager.changeIcon( "ajaxAutoSaveClean.gif" );
-	this.asManager.plugin.button.label = "Auto Save";
-};
+});
