35497f0fbb2c44efd3fe73c51c1ed52886347521
minger0 <minger0@users.sourceforge.net>
2010-08-16 14:44:34 +0000

    [FIX] 3159+++ Trackers: Modifying dynamic list item type does not work when the reference field is not in fields - additional fix: to allow for linking via not item link fields; simplification to the previous commit and it is also more generic; added even more simplification
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@28537 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_tracker.php
+++ lib/wiki-plugins/wikiplugin_tracker.php
@@ -1027,13 +1027,7 @@ function wikiplugin_tracker($data, $params)
 						if ($flds['data'][$i]['type'] == 'r') {
 							// we prentended it was an item link
 							$bindingValue = $trklib->get_item_value($trackerId, $itemId, $f['options_array'][2]);
-							$bindingItemLinkField = $trklib->get_tracker_field($f['options_array'][2]);
-							if ($bindingItemLinkField['type'] != 'r') {
-								$flds['data'][$i]['list'] = $trklib->get_filtered_item_values($f['options_array'][1], $bindingValue, $f['options_array'][3]);
-							} else {
-								$bindingValueIndex = $trklib->get_item_id($bindingItemLinkField['options_array'][0], $bindingItemLinkField['options_array'][3], $bindingValue);
-								$flds['data'][$i]['list'] = $trklib->get_filtered_item_values($f['options_array'][1], $bindingValueIndex, $f['options_array'][3]);
-							}
+							$flds['data'][$i]['list'] = $trklib->get_filtered_item_values($f['options_array'][1], $bindingValue, $f['options_array'][3]);
 						}
 					} elseif ($f['type'] == 'f' && empty($itemId) && empty($f['options_array'][3])) {
 						$flds['data'][$i]['value'] = $tikilib->now;
