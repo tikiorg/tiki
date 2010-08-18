25c1f050f5ab97c285c3971450e2966edbf889a5
nkoth <nkoth@users.sourceforge.net>
2010-08-18 19:30:08 +0000

    [FIX] Previous commit wrong (aim is to have zero values for credits that user does not have)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@28598 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/credits/creditslib.php
+++ lib/credits/creditslib.php
@@ -99,11 +99,11 @@ class CreditsLib extends TikiLib
 		}
 		
 		// set zero for creditTypes that user does not have
-		foreach($creditTypes as $c) {
-			if (!array_key_exists($c, $credits)) {
-				$credits[$type]['used'] = 0;
-				$credits[$type]['remain'] = 0;
-				$credits[$type]['total'] = 0;
+		foreach($creditTypes as $k => $c) {
+			if (!array_key_exists($k, $credits)) {
+				$credits[$k]['used'] = 0;
+				$credits[$k]['remain'] = 0;
+				$credits[$k]['total'] = 0;
 			}			
 		}
 		return $credits;
