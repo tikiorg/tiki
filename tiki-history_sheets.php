19d4518f72a0faaee167dee1ae82a442bff6159b
robertplummer <robertplummer@users.sourceforge.net>
2010-09-11 18:40:12 +0000

    [MOD] Changed sheet history viewing page to be tiki-history_sheets.php [MOD] Changed sheets so that they scroll together and tab click changed them together.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@29131 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-history_sheets.php
+++ tiki-history_sheets.php
@@ -6,6 +6,7 @@
 // $Id$
 
 $section = 'sheet';
+$tiki_sheet_div_style = 'height: 400px ! important;';
 require_once ('tiki-setup.php');
 require_once ('lib/sheet/grid.php');
 $access->check_feature('feature_sheet');
@@ -36,15 +37,65 @@ $smarty->assign('description', $info['description']);
 
 $smarty->assign('page_mode', 'view' );
 
-$result = $tikilib->query( "SELECT DISTINCT `begin`, `user` FROM `tiki_sheet_values` WHERE `sheetId` = ? ORDER BY begin DESC", array( $_REQUEST['sheetId'] ) );
-$data = array();
-while( $row = $result->fetchRow() )
-	$data[] = array( "stamp" =>$row['begin'], "string" => $tikilib->date_format( "%Y-%m-%d %H:%M:%S", $row['begin'] ), "user" => $row['user'] );
-
-$smarty->assign_by_ref( 'history', $data );
-
-include_once ('tiki-section_options.php');
-ask_ticket('sheet');
+//display the history picker if no sheets are defined
+if (empty($_REQUEST['readdate'])) {
+	$result = $tikilib->query( "SELECT DISTINCT `begin`, `user` FROM `tiki_sheet_values` WHERE `sheetId` = ? ORDER BY begin DESC", array( $_REQUEST['sheetId'] ) );
+	$data = array();
+	while( $row = $result->fetchRow() )
+		$data[] = array( "stamp" =>$row['begin'], "string" => $tikilib->date_format( "%Y-%m-%d %H:%M:%S", $row['begin'] ), "user" => $row['user'] );
+	
+	$smarty->assign_by_ref( 'history', $data );
+	
+	include_once ('tiki-section_options.php');
+	ask_ticket('sheet');
+} else {
+	// Process the insertion or modification of a gallery here
+	$grid = new TikiSheet($_REQUEST["sheetId"]);
+	$handler = new TikiSheetDatabaseHandler($_REQUEST["sheetId"]);
+	$dates = array(time());
+	
+	$i = 0;
+	$readdates = explode("|", $_REQUEST['readdate'] );
+	foreach ( $readdates as $dateStr ) {
+		if ( $dateStr ) {
+			$dates[$i] = $dateStr;
+			if (!is_numeric($dates[$i])) $dates[$i] = strtotime($dates[$i]);
+			if ($dates[$i] == - 1) $dates[$i] = time();
+		}
+		$i++;
+	}
+	
+	$tableHtml = array();
+	$i = 0;
+	
+	foreach ( $dates as $date ) {
+		$smarty->assign('read_date', $date);
+		$handler->setReadDate($date);
+		$grid->import($handler);
+		$tableHtml[$i] = $grid->getTableHtml( true, $date );
+		$i++;
+	}
+	
+	$smarty->assign('grid_content', $tableHtml);
+	$handler = new TikiSheetDatabaseHandler($_REQUEST["sheetId"]);
+	$grid->import($handler);
+	
+	$headerlib->add_jq_onready(
+		'if (typeof ajaxLoadingShow == "function") {
+			ajaxLoadingShow("role_main");
+		}
+		setTimeout (function () { $("div.tiki_sheet").tiki("sheet", "",{editable:false});}, 500);
+		instanceCount = ' . count($readdates) . ';
+		$("#tiki_sheet_container").one("mousedown", function() {
+			lockSheetTogether();		
+		});
+		'
+	, 500);
+	
+	if ( $tiki_sheet_div_style) {
+		$smarty->assign('tiki_sheet_div_style',  $tiki_sheet_div_style);
+	}
+}
 
 // Display the template
 $smarty->assign('mid', 'tiki-history-sheets.tpl');
