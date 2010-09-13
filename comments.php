46cad9e83666e76b86f2d60d1b2efb3ac6b476a4
sampaioprimo <sampaioprimo@users.sourceforge.net>
2010-09-13 14:43:58 +0000

    removing unused block of code
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@29165 b456876b-0849-0410-b77d-98878d47e9d5

--- comments.php
+++ comments.php
@@ -281,13 +281,6 @@ if ( isset($_REQUEST['comments_objectId']) && $_REQUEST['comments_objectId'] == 
 	}
 }
 
-if (($tiki_p_vote_comments == 'y' && (!isset($forum_mode) || $forum_mode == 'n')) || (isset($forum_mode) && $forum_mode == 'y' && $tiki_p_forum_vote == 'y')) {
-	// Process a vote here
-	$smarty->assign( 'rating_is_enabled', 'y' );
-} else {
-	$smarty->assign( 'rating_is_enabled', 'n' );
-}
-
 // Comments Moderation
 global $tiki_p_admin_comments;
 if ( (!isset($forum_mode) || $forum_mode == 'n') && $tiki_p_admin_comments == 'y' && isset($_REQUEST["comments_threadId"]) && !empty($_REQUEST['comments_approve']) ) {
