81f33cbdd4cd4bcf4c08c42aa9f0d07786c472cb
sylvieg <sylvieg@users.sourceforge.net>
2010-09-14 17:12:35 +0000

    [FIX]sefurl: urlencode the page to replace space into +
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@29216 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-switch_lang.php
+++ tiki-switch_lang.php
@@ -57,7 +57,7 @@ if (strstr($orig_url, 'tiki-index.php') || strstr($orig_url, 'tiki-read_article.
 		} elseif (!empty($param['articleId'])) {
 			$orig_url = preg_replace('/(.*[&?]articleId=)' . $pageId . '(.*)/', '${1}' . $bestLangPageId . '$2', $orig_url);
 		} else {
-			$newPage = $tikilib->get_page_name_from_id($bestLangPageId);
+			$newPage = urlencode($tikilib->get_page_name_from_id($bestLangPageId));
 			$orig_url = preg_replace('/(.*[&?]page=)'.$page.'(.*)/', '${1}'."${newPage}".'$2', $orig_url);
 			$orig_url = preg_replace('/(.*)(tiki-index.php)$/', "$1$2?page=$newPage", $orig_url);
 		}
