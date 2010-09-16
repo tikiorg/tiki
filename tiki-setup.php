320606bb557243bd926c71d904bfb046c2108cc8
sept_7 <sept_7@users.sourceforge.net>
2010-09-16 19:09:17 +0000

    [FIX] correct dependencies of feature_wiki_save_draft and ajax_xajax
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@29312 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup.php
+++ tiki-setup.php
@@ -216,9 +216,9 @@ if ($prefs['javascript_enabled'] != 'n') {
 	if ( $prefs['feature_ajax'] === 'y' ) {
 		if ($prefs['ajax_xajax'] === 'y') {
 			$headerlib->add_jsfile('lib/ajax/tiki-ajax.js');
-		}
-		if ($prefs['feature_wiki_save_draft'] === 'y') {
-			$headerlib->add_jsfile('lib/wiki/wiki-ajax.js');
+			if ($prefs['feature_wiki_save_draft'] === 'y') {
+				$headerlib->add_jsfile('lib/wiki/wiki-ajax.js');
+			}
 		}
 		if ( $prefs['ajax_autosave'] === 'y' ) {
 			$headerlib->add_jsfile('lib/ajax/autosave.js');
