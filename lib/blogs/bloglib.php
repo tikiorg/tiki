b7f1f11ebc515785fda40963a430a8c6fc1b8a3c
pkdille <pkdille@users.sourceforge.net>
2010-09-20 19:56:06 +0000

    [QT] Quality Team Backport
        r28808 | sdoh | 2010-08-31 16:53:04 +0200 (Tue, 31 Aug 2010) | 1 line
        [bp/r28807] - [FIX] Corrige les freetags orphelins après suppression du post parent.
        
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/5.x@29425 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/blogs/bloglib.php
+++ lib/blogs/bloglib.php
@@ -512,6 +512,11 @@ class BlogLib extends TikiLib
 			$result = $this->query($query, array((int) $blogId));
 		}
 
+		// When a post is deleted, all freetags asociated must also be deleted
+		$objectId = $objectlib->get_object_id('blog post', $postId);
+		$query = "DELETE FROM `tiki_freetagged_objects` WHERE `objectId` = ?";
+		$this->query($query,array((int) $objectId));
+                
 		$query = "delete from `tiki_blog_posts_images` where `postId`=?";
 		$this->query($query, array((int) $postId));
 
