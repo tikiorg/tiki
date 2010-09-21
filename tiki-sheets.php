113f3962ec4c544203dd893ba55da369f03fe0cd
xavidp <xavidp@users.sourceforge.net>
2010-09-21 10:59:18 +0000

    [FIX][bp--] Adding r28752 in proposals/3x from ohertel (thanks Oliver). "Temporary fix for tiki sheet editing. Sheets are broken for admins of sites with a large user base, because the complete user base is loaded and assigned to a variable, causing a memory error most often. Needs to get improvement." There is no reference to backporting from trunk since tiki-sheets.php in trunk-r29121 seems not affected by this issue (please confirm; my php skills are very limited).
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/3.0@29481 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-sheets.php
+++ tiki-sheets.php
@@ -78,7 +78,8 @@ if (isset($_REQUEST["edit_mode"]) && $_REQUEST["edit_mode"]) {
 	$smarty->assign('edit_mode', 'y');
 
 	if ($tiki_p_admin == 'y' || $tiki_p_admin_sheet == 'y') {
-		$users = $tikilib->list_users(0, -1, 'login_asc', '', false);
+		// TODO: fetch only data from user base that are really required, till then we only get 300 latest entries, cause -1 kills sheets for all site with a big user base
+		$users = $tikilib->list_users(0, 300, 'login_asc', '', false);
 		$smarty->assign_by_ref('users', $users['data']);
 	}
 	if ($_REQUEST["sheetId"] > 0) {
