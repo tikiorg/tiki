6e8941fcfdf2313d22e81c0d131d077440a22696
sept_7 <sept_7@users.sourceforge.net>
2010-09-27 19:07:23 +0000

    [FIX/bp 29667] PHP notice and absolute include path
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@29668 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-jsplugin.php
+++ tiki-jsplugin.php
@@ -19,7 +19,7 @@
 header('content-type: application/x-javascript');
 
 require_once('lib/init/initlib.php');
-TikiInit::appendIncludePath('lib/core');
+TikiInit::appendIncludePath(dirname(__FILE__) . 'lib/core');
 
 // Apply filters on the body
 include 'lib/core/TikiFilter.php';
