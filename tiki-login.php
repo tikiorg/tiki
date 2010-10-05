aa39bdf6c93c6152c84164d5a3a2892d382a95ff
marclaporte <marclaporte@users.sourceforge.net>
2010-10-05 02:02:57 +0000

    typo
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@29790 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-login.php
+++ tiki-login.php
@@ -41,7 +41,7 @@ if( $prefs['session_silent'] == 'y' ) {
 }
 
 // Remember where user is logging in from and send them back later; using session variable for those of us who use WebISO services
-// Note that loginfrom will always be a complete URL (http://...)
+// Note that login from will always be a complete URL (http://...)
 if (!isset($_SESSION['loginfrom'])) {
 	$_SESSION['loginfrom'] = (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $prefs['tikiIndex']);
 	if (!preg_match('/^http/', $_SESSION['loginfrom'])) {
