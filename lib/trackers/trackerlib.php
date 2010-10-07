d6a7e01d6c24160d1ccf14a8873eedcf148703f5
nkoth <nkoth@users.sourceforge.net>
2010-10-07 22:59:39 +0000

    [FIX] When user_selector_realnames_tracker is in use, to allow selecting of new users in special cases as would be required during registration.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@29896 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/trackers/trackerlib.php
+++ lib/trackers/trackerlib.php
@@ -1328,7 +1328,10 @@ class TrackerLib extends TikiLib
 			if ($prefs['user_selector_realnames_tracker'] == 'y' && $ins_fields['data'][$i]['type'] == 'u') {
 				if (!$userlib->user_exists($ins_fields['data'][$i]['value'])) {
 					$finalusers = $userlib->find_best_user(array($ins_fields['data'][$i]['value']), '' , 'login');
-					$ins_fields['data'][$i]['value'] = $finalusers[0];
+					if (!empty($finalusers[0]) && !(isset($_REQUEST['register']) && isset($_REQUEST['name']) && $_REQUEST['name'] == $u)) {
+						// It could be in fact that a new user is required (when no match is found or during registration even if match is found)
+						$ins_fields['data'][$i]['value'] = $finalusers[0];
+					}
 				}
 			}
 			if ($ins_fields['data'][$i]['type'] == 'G' && isset($ins_fields['data'][$i]['options_array'][0]) && $ins_fields['data'][$i]['options_array'][0] == 'y') {
