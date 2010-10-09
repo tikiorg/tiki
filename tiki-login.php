tikiorg/tiki

commit 32f1de31c9568f73640b1d66cd99bc2248b7a42c

[FIX]login: instead of the error page - display the login box with an error - that is more friendly than go back to the page..
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@28103 b456876b-0849-0410-b77d-98878d47e9d5

tiki-login.php
@@ -321,16 +321,15 @@ if ($isvalid) {
 			}
 			$userlib->set_unsuccessful_logins($user, $nb_bad_logins + 1);
 		}
-		unset($user);
 		unset($isvalid);
 		switch ($error) {
 			case PASSWORD_INCORRECT:
-				$error = 'Invalid password';
-				break;
-
 			case USER_NOT_FOUND:
-				$error = 'Invalid username';
-				break;
+				$smarty->assign('error_login', $error);
+				$smarty->assign('mid', 'tiki-login.tpl');
+				$smarty->assign('error_user', $user);
+				$smarty->display('tiki.tpl');
+				exit;
 
 			case ACCOUNT_DISABLED:
 				$error = 'Account disabled';
@@ -352,7 +351,6 @@ if ($isvalid) {
 			default:
 				$error = 'Invalid username or password';
 		}
-		if (isset($user) and $prefs['feature_score'] == 'y') $tikilib->score_event($user, 'login');
 		if (isset($extraButton)) $smarty->assign_by_ref('extraButton', $extraButton);
 		$smarty->assign('msg', tra($error));
 		$smarty->display('error.tpl');

lib/userslib.php
@@ -355,7 +355,7 @@ class UsersLib extends TikiLib
 		$shib_skip_admin = ($prefs['shib_skip_admin'] == 'y');
 
 		if ( strlen($pass) < $prefs['min_pass_length'] and ($user === 'admin' or (!$auth_cas and !$auth_shib )) ) {
-			return false;
+			return array(false, $user, $this->user_exists($user)?PASSWORD_INCORRECT:USER_NOT_FOUND);
 		}
 
 		// first attempt a login via the standard Tiki system

templates/modules/mod-login_box.tpl
@@ -89,9 +89,16 @@ function capLock(e){
 	 {if !empty($urllogin)}<input type="hidden" name="url" value="{$urllogin|escape}" />{/if}
         <fieldset>
           <legend>{tr}Log in as{/tr}&hellip;</legend>
+		  {if !empty($error_login)}
+			{remarksbox type='errors' title='{tr}Error{/tr}'}
+				{if $error_login == -5 {*USER_NOT_FOUND (define does not work on old php)*}}{tr}Invalid username{/tr}
+				{elseif $error_login == -3 {*PASSWORD_INCORRECT*}}{tr}Invalid password{/tr}
+				{else}{$error_login|escape}{/if}
+			{/remarksbox}
+		  {/if}
             <div><label for="login-user">{if $prefs.login_is_email eq 'y'}{tr}Email{/tr}{else}{tr}Username{/tr}{/if}:</label><br />
 		{if $loginuser eq ''}
-              <input type="text" name="user" id="login-user" size="{if empty($module_params.input_size)}15{else}{$module_params.input_size}{/if}" />
+              <input type="text" name="user" id="login-user" size="{if empty($module_params.input_size)}15{else}{$module_params.input_size}{/if}" {if !empty($error_login)} value="{$error_user|escape}"{/if} />
 	  <script type="text/javascript">document.getElementById('login-user').focus();</script>
 		{else}
 		      <input type="hidden" name="user" id="login-user" value="{$loginuser}" /><b>{$loginuser}</b>

commit 0d520b71829d21e41ee3ed0f05187c27ee4b1306

[FIX] Prevent modules from showing stuff that anonymous should not see (bug introduced in r28103)
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@29915 b456876b-0849-0410-b77d-98878d47e9d5

tiki-login.php
@@ -346,6 +346,7 @@ if ($isvalid) {
 			}
 			$userlib->set_unsuccessful_logins($user, $nb_bad_logins + 1);
 		}
+		unset($user); // Important so that modules are showing based on anonymous
 		unset($isvalid);
 		switch ($error) {
 			case PASSWORD_INCORRECT: