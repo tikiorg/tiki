0021850a80904625e4f7970929bf7dddf1df1ff0
jonnybradley <jonnybradley@users.sourceforge.net>
2010-10-11 19:19:13 +0000

    userprefs: php warning
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@29947 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-user_preferences.php
+++ tiki-user_preferences.php
@@ -72,7 +72,7 @@ $smarty->assign('show_mouseover_user_info', isset($prefs['show_mouseover_user_in
 if ($prefs['feature_userPreferences'] == 'y' && isset($_REQUEST["new_prefs"])) {
 	check_ticket('user-prefs');
 	// setting preferences
-	if ($prefs['change_theme'] == 'y' && $group_style == '') {
+	if ($prefs['change_theme'] == 'y' && empty($group_style)) {
 		if (isset($_REQUEST["mystyle"])) {
 			if ($user == $userwatch) {
 				$t = $tikidomain ? $tikidomain . '/' : '';
