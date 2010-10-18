4ffb57bf3c4b9cdfa8e0fe71ae96ca064a4036ee
sylvieg <sylvieg@users.sourceforge.net>
2010-10-18 13:55:33 +0000

    [NEW]img: url param thumbnail=fileId to download a different thumbnail for an image
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@30090 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-download_file.php
+++ tiki-download_file.php
@@ -112,6 +112,15 @@ if (!$skip) {
 		$smarty->display('error.tpl');
 		die;
 	}
+	if ( isset($_GET['thumbnail']) && is_numeric($_GET['thumbnail'])) { //check also perms on thumb 
+		$info_thumb = $tikilib->get_file($_GET['thumbnail']);
+		if ( !$zip && $tiki_p_admin_file_galleries != 'y' && !$userlib->user_has_perm_on_object($user, $info_thumb['galleryId'], 'file gallery', 'tiki_p_download_files') && !($info['backlinkPerms'] == 'y' && !$filegallib->hasOnlyPrivateBacklinks($info_thumb['fileId']))) {
+			$smarty->assign('errortype', 401);
+			$smarty->assign('msg', tra('Permission denied'));
+			$smarty->display('error.tpl');
+			die;
+		}
+	}
 }
 
 // Add hits ( if download or display only ) + lock if set
@@ -240,7 +249,7 @@ if ( isset($_GET['preview']) || isset($_GET['thumbnail']) || isset($_GET['displa
 	if ($build_content) {
 
 		// Modify the original image if needed
-		if ( ! isset($_GET['display']) || isset($_GET['x']) || isset($_GET['y']) || isset($_GET['scale']) || isset($_GET['max']) || isset($_GET['format']) ) {
+		if ( ! isset($_GET['display']) || isset($_GET['x']) || isset($_GET['y']) || isset($_GET['scale']) || isset($_GET['max']) || isset($_GET['format']) || isset($_GET['thumbnail']) ) {
 	
 			require_once('lib/images/images.php');
 			if (!class_exists('Image')) die();
@@ -299,6 +308,18 @@ if ( isset($_GET['preview']) || isset($_GET['thumbnail']) || isset($_GET['displa
 				}
 				// We resize to a thumbnail size if needed
 				elseif ( isset($_GET['thumbnail']) ) {
+					if (is_numeric($_GET['thumbnail'])) {
+						if (empty($info_thumb)) {
+							$info_thumb = $tikilib->get_file($_GET['thumbnail']);
+						}
+						if ( ! empty($info_thumb['path']) ) {
+							$image = new Image($prefs['fgal_use_dir'].$info_thumb['path'], true);
+						} else {
+							$image = new Image($info_thumb['data']);
+							$content = null; // Explicitely free memory before getting cache
+						}
+						if ( $image->is_empty() ) die;
+					}
 					$image->resizethumb();
 				}
 				// We resize to a preview size if needed
