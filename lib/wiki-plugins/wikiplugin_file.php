d6acd4f45a95fc125769e55987f92f581683e172
chealer <chealer@users.sourceforge.net>
2010-10-27 14:58:55 +0000

    Plugin FILE: clarify how to use [FIX] remove absolute dependency on feature_wiki_attachments, since FILE now also supports files from galleries I'm not sure about the way it now complains when feature_wiki_attachments is actually missing.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@30350 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_file.php
+++ lib/wiki-plugins/wikiplugin_file.php
@@ -10,8 +10,8 @@ function wikiplugin_file_info()
 	return array(
 		'name' => tra( 'File' ),
 		'documentation' => 'PluginFile',
-		'description' => tra("Displays a link to an attachment to a wiki page and can display an image attachment. "),
-		'prefs' => array( 'feature_wiki_attachments', 'wikiplugin_file' ),
+		'description' => tra("Displays a link to a file (either from the file gallery or an attachment to a wiki page) and can display an image attachment."),
+		'prefs' => array( 'wikiplugin_file' ),
 		'inline' => true,
 		'params' => array(
     		'name' => array(
@@ -22,12 +22,12 @@ function wikiplugin_file_info()
  			'desc' => array(
 				'required' => false,
 				'name' => tra('Description'),
-				'description' => tra('Comment'),
+				'description' => tra('Wiki attachment:') . ' ' . tra('Comment'),
 			),
     		'page' => array(
 				'required' => false,
 				'name' => tra('Page'),
-				'description' => tra("Gives the name of another page the attached file is on. The file on that page is linked to instead. Only works with wiki pages"),
+				'description' => tra('Wiki attachment:') . ' ' . tra("Name of the wiki page the file is attached to. If left empty when the plugin is used on a wiki page, this defaults to that wiki page."),
 			),
     		'showdesc' => array(
 				'required' => false,
@@ -37,20 +37,20 @@ function wikiplugin_file_info()
     		'image' =>array(
 				'required' => false,
 				'name' => tra('Image'),
-				'description' => tra("Says that this file is an image, and should be displayed inline using the img tag"),
+				'description' => tra('Wiki attachment:') . ' ' . tra("Says that this file is an image, and should be displayed inline using the img tag"),
 			),
 			'fileId' => array(
 				'required' => false,
-				'name' => tra('File ID'),
-				'description' => tra('Numeric ID of a file in the file galleries'),
+				'name' => tra('File identifier'),
+				'description' => tra('File from gallery:') . ' ' . tra('Identifier of a file in the file galleries.') . ' ' . tra('Example value:') . ' 42',
 				'filter' => 'digits',
 			),
 			'date' => array(
 				'required' => false,
 				'name' => tra('Date'),
-				'description' => tra('Pick the archive if exists created just before the date of the fileId'),
+				'description' => tra('File from gallery:') . ' ' . tra('Pick the archive if exists created just before the date of the fileId'),
 			),
-		),
+		)
 	);
 }
 
@@ -93,6 +93,9 @@ function wikiplugin_file( $data, $params )
 		return "[tiki-download_file.php?fileId=$fileId|$data]";
 	}
 
+	if ($prefs['feature_wiki_attachments'] != 'y') {
+		return "<span class='warn'>" . tra("Wiki attachments are disabled."). "</span>";
+	}	
 	$filedata = array();
 	$filedata["name"] = '';
 	$filedata["desc"] = '';
