7ac9c36e3ae199856522a3792a0016fb113ae14d
xavidp <xavidp@users.sourceforge.net>
2010-10-27 13:09:06 +0000

    [FIX]Usability issue: no js file was found under the en folder, so that it was not easy to guess that this file exists, and which strings in js files can be translated (at least, the ones in here :-) )
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@30344 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lang/en/language.js
@@ -0,0 +1,21 @@
+/* 
+ * Javascript translation definition
+ * Example to help translation of some English strings in some js files
+ * 
+ * $Id: language.js 30343 2010-10-27 12:50:51Z xavidp $
+ */
+
+lang = {
+
+"Very Secure" : "Very Secure",
+"Secure" : "Secure",
+"Very Strong" : "Very Strong",
+"Strong" : "Strong",
+"Average" : "Average",
+"Weak" : "Weak",
+"Very Weak" : "Very Weak",
+"Strength" : "Strength",
+"Valid User Name" : "Valid User Name",
+"Passwords match" : "Passwords match",
+"Valid Email" : "Valid Email"
+};
