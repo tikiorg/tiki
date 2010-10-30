4525ea363fcd78f9b6923fba1ad0a56b6b5fb34a
robertplummer <robertplummer@users.sourceforge.net>
2010-10-30 16:41:35 +0000

    [ADD] Added profile handler for sheet for further development
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@30422 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/profilelib/installlib.php
+++ lib/profilelib/installlib.php
@@ -40,6 +40,7 @@ class Tiki_Profile_Installer
 		'extwiki' => 'Tiki_Profile_InstallHandler_ExtWiki',
 		'webmail_account' => 'Tiki_Profile_InstallHandler_WebmailAccount',
 		'webmail' => 'Tiki_Profile_InstallHandler_Webmail',
+		'sheet' => 'Tiki_Profile_InstallHandler_Sheet',
 	);
 
 	private static $typeMap = array(
@@ -2793,4 +2794,80 @@ class Tiki_Profile_ValueMapConverter // {{{
 		return $tab[$key];
 	}
 
+} // }}}
+
+class Tiki_Profile_InstallHandler_Sheet extends Tiki_Profile_InstallHandler // {{{
+{
+	function getData()
+	{
+		if( $this->data )
+			return $this->data;
+		$data = $this->obj->getData();
+		$this->replaceReferences($data);
+
+		return $this->data = $data;
+	}
+	
+	function canInstall()
+	{
+		$data = $this->getData();
+		
+		if (isset($data)) return true;
+		else return false;
+	}
+	
+	function _install()
+	{
+		if ($this->canInstall())
+		{
+			global $user;
+			require_once ('lib/sheet/grid.php');
+			
+			//here we convert the array to that of what is acceptable to the sheet lib
+			$parentSheetId;
+			$sheets = array();
+			
+			for ($sheetI = 0; $sheetI < count($this->data); $sheetI++)
+			{
+				for ($r = 0; $r < count($this->data[$sheetI]); $r++)
+				{
+					for ($c = 0; $c < count($this->data[$sheetI][$r]); $c++)
+					{
+						$value;
+						$formula;
+						$rawValue = $this->data[$sheetI][$r][$c];
+						 
+						if (substr($rawValue, 0, 1) == "=") {
+							$formula = $rawValue;
+						} else {
+							$value = $rawValue;
+						}
+						
+						$ri = 'r'.$r;
+						$ci = 'c'.$c;
+						
+						$sheets[$sheetI]->data->$ri->$ci->formula = $formula;
+						$sheets[$sheetI]->data->$ri->$ci->value = $value;
+						
+						$sheets[$sheetI]->data->$ri->$ci->width = 1;
+						$sheets[$sheetI]->data->$ri->$ci->height = 1;
+					}
+				}
+				
+				$sheets[$sheetI]->metadata->rows = (count($this->data[$sheetI]) - 1);
+				$sheets[$sheetI]->metadata->columns = (count($this->data[$sheetI][0]) - 1);
+				
+				$id = $sheetlib->replace_sheet(0, "Profile Test", "", $user, $parentSheetId);
+				$parentSheetId = ($parentSheetId ? $parentSheetId : $id);
+				
+				$grid = new TikiSheet($id);
+				$handler = new TikiSheetHTMLTableHandler($sheets[$sheetI]);
+				$res = $grid->import($handler);
+				$handler = new TikiSheetDatabaseHandler($id);
+				$grid->export($handler);
+			}
+			
+			return $parentSheetId;
+		}
+	}
 } // }}}
