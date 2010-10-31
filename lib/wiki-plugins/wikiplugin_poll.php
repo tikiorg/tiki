e6aef42ec0c9f641b529a1c3961e4296d77f2bd9
sylvieg <sylvieg@users.sourceforge.net>
2010-10-31 15:31:09 +0000

    [NEW]POLL: option showresult to show result as soon as the suer has voted or always
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@30429 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_poll.php
+++ lib/wiki-plugins/wikiplugin_poll.php
@@ -34,13 +34,20 @@ function wikiplugin_poll_info() {
 				'description' => 'y|n',
 				'default' => 'y',
 			),
+			'showresult' => array(
+				'required' => false,
+				'name' => tra('Show result'),
+				'description' => 'link|voted|always',
+				'filter' => 'alpha',
+				'default' => 'link',
+			),
 		),
 	);
 }
 
 function wikiplugin_poll($data, $params) {
 	global $smarty, $polllib, $trklib, $tikilib, $dbTiki, $userlib, $tiki_p_admin, $prefs, $_REQUEST, $user;
-	$default = array('showtitle' => 'y');
+	$default = array('showtitle' => 'y', 'showresult' => 'link');
 	$params = array_merge($default, $params);
 
 	extract ($params,EXTR_SKIP);
@@ -48,22 +55,34 @@ function wikiplugin_poll($data, $params) {
 	if (!isset($pollId)) {
 	    $smarty->assign('msg', tra("missing poll ID for plugin POLL"));
 	    return $smarty->fetch("error_simple.tpl");
-	} else {
-	    include_once ('lib/polls/polllib.php');
-
-
-	    $poll_info = $polllib->get_poll($pollId);
-	    $options = $polllib->list_poll_options($pollId);
-
-	    $smarty->assign_by_ref('menu_info', $poll_info);
-	    $smarty->assign_by_ref('channels', $options);
-	    $smarty->assign_by_ref('poll_title', $data);
-		$smarty->assign_by_ref('showtitle', $showtitle);
-	    $smarty->assign('ownurl', $tikilib->httpPrefix(). $_SERVER["REQUEST_URI"]);
-
-	    ask_ticket('poll-form');
-
-	    // Display the template
-	    return '~np~'.$smarty->fetch("tiki-plugin_poll.tpl").'~/np~';
 	}
+	global $polllib;include_once ('lib/polls/polllib.php');
+
+
+    $poll_info = $polllib->get_poll($pollId);
+    $options = $polllib->list_poll_options($pollId);
+
+	$hasVoted = $tikilib->user_has_voted($user, 'poll' . $pollId);
+	$ret = '';
+	$smarty->assign_by_ref('showresult', $showresult);
+	$smarty->assign_by_ref('hasVoted', $hasVoted);
+    if (!$hasVoted || $prefs['feature_poll_revote'] == 'y') {
+		$smarty->assign_by_ref('menu_info', $poll_info);
+		$smarty->assign_by_ref('channels', $options);
+		$smarty->assign_by_ref('poll_title', $data);
+		$smarty->assign_by_ref('showtitle', $showtitle);
+		$smarty->assign('ownurl', $tikilib->httpPrefix(). $_SERVER['REQUEST_URI']);
+
+		ask_ticket('poll-form');
+
+		$ret = $smarty->fetch('tiki-plugin_poll.tpl');
+	}
+	if (($showresult == 'voted' && $hasVoted) || $showresult == 'always') {
+		$total = $polllib->options_percent($poll_info, $options);
+		$poll_info['options'] = $options;
+		$smarty->assign_by_ref('poll_info', $poll_info);
+		print_r($poll_info); print_r($options);
+		$ret .= $smarty->fetch('tiki-poll_results_bar.tpl');
+	}
+	return '~np~'.$ret.'~/np~';
 }
