461909a1f975f1816e62533bacb66631c4ff2108
sylvieg <sylvieg@users.sourceforge.net>
2010-11-02 19:33:12 +0000

    [FIX]browser title: description and no breadcrumb must show description in title
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@30482 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/breadcrumblib.php
+++ lib/breadcrumblib.php
@@ -37,6 +37,7 @@ class Breadcrumb
 
 /* static */
 function breadcrumb_buildHeadTitle($crumbs) {
+	global $prefs;
     if( _is_assoc($crumbs) ) {
         return false;
     }
@@ -46,6 +47,8 @@ function breadcrumb_buildHeadTitle($crumbs) {
             $ret[] = breadcrumb_buildHeadTitle($crumb);
         }
         return implode(" : ", $ret);
+	} elseif ($prefs['site_title_breadcrumb'] == 'desc') {
+		return $crumbs->description;
     } else {
         return htmlspecialchars($crumbs->title);
     }
