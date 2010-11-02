cff10dde3fb54418430536f5a0e044f401ebd9a6
jonnybradley <jonnybradley@users.sourceforge.net>
2010-11-02 19:13:22 +0000

    [FIX] javascript_cdn: Sync jquery versions from CDN with bundled libs
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@30480 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup.php
+++ tiki-setup.php
@@ -202,7 +202,7 @@ if ($prefs['javascript_enabled'] != 'n') {
 	$headerlib->add_jsfile( 'lib/swfobject/swfobject.js' );
 	
 	if( isset($prefs['javascript_cdn']) && $prefs['javascript_cdn'] == 'google' ) {
-		$headerlib->add_jsfile( 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'external' );
+		$headerlib->add_jsfile( 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js', 'external' );
 	} else {
 		if ( $prefs['tiki_minify_javascript'] === 'y' ) {
 			$headerlib->add_jsfile( 'lib/jquery/jquery.min.js' );
@@ -227,7 +227,7 @@ if ($prefs['javascript_enabled'] != 'n') {
 	
 	if( $prefs['feature_jquery_ui'] == 'y' ) {
 		if( isset($prefs['javascript_cdn']) && $prefs['javascript_cdn'] == 'google' ) {
-			$headerlib->add_jsfile( 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js', 'external' );
+			$headerlib->add_jsfile( 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js', 'external' );
 		} else {
 			if ( $prefs['tiki_minify_javascript'] === 'y' ) {
 				$headerlib->add_jsfile( 'lib/jquery/jquery-ui/ui/minified/jquery-ui.min.js' );
