d64412a98acc73de29dd712e3597f0d409239f2f
lphuberdeau <lphuberdeau@users.sourceforge.net>
2010-11-05 18:39:21 +0000

    [ENH] Adding odd/even class on li items to add some contrasts between rows
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@30547 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tree/categ_browse_tree.php
+++ lib/tree/categ_browse_tree.php
@@ -64,7 +64,9 @@ class CatBrowseTreeMaker extends TreeMaker
 	}
 	
 	function node_start_code($nodeinfo) {
-		return "\t" . '<li class="treenode">';
+		static $count = 0;
+		++$count;
+		return "\t" . '<li class="treenode ' . (($count % 2) ? 'odd' : 'even') . '">';
 	}
 
 	//
