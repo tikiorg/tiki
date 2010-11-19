b96330b897acff6636cc64e8d242b71503cd87d0
xavidp <xavidp@users.sourceforge.net>
2010-11-19 18:25:47 +0000

    [bp/r30776 r30778 r30779][ENH] extending flash wikiplugin with basic support for youtube, vimeo and blip.tv videos.  flash wikiplugin display error if unable to recognize url for youtube, vimeo and blip.tv,  flash wikiplugin: use blip.tv embed URL since it is not possible to extract it from the video URL. Thanks rodrigo_sampaio
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@30796 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_flash.php
+++ lib/wiki-plugins/wikiplugin_flash.php
@@ -23,20 +23,41 @@ function wikiplugin_flash_info() {
 				'description' => tra('Whether you want to insert a Flash from a URL, a fileId from a podcast file gallery or a link to a specific service like Youtube or Vimeo'),
 				'options' => array(
 					array('text' => tra('Movie URL'), 'value' => 'url'),
+					array('text' => tra('Blip.tv'), 'value' => 'bliptv'), 
 					array('text' => tra('FileId from a podcast file gallery'), 'value' => 'fileId'),
+					array('text' => tra('Vimeo'), 'value' => 'vimeo'),
+					array('text' => tra('Youtube'), 'value' => 'youtube'),
 				),
 			),
 			'movie' => array(
-				'required' => false,
+				'required' => true,
 				'name' => tra('Movie URL'),
 				'description' => tra('Complete URL to the movie to include. e.g. files/test.swf'),
 				'parent' => array('name' => 'type', 'value' => 'url'),
 			),
 			'fileId' => array(
-				'required' => false,
+				'required' => true,
 				'name' => tra('FileId from a podcast file gallery'),
 				'description' => tra('Id of a file from a podcast gallery - will work only with podcast gallery'),
 				'parent' => array('name' => 'type', 'value' => 'fileId'),
+			),
+			'youtube' => array(
+				'required' => true,
+				'name' => tra('Youtube URL'),
+				'description' => tra('Entire URL to the YouTube video. Example: http://www.youtube.com/watch?v=1i2ZnU4iR24'),
+				'parent' => array('name' => 'type', 'value' => 'youtube'),
+			),
+			'vimeo' => array(
+				'required' => true,
+				'name' => tra('Vimeo URL'),
+				'description' => tra('Entire URL to the Vimeo video. Example: http://vimeo.com/3319966'),
+				'parent' => array('name' => 'type', 'value' => 'youtube'),
+			),
+			'bliptv' => array(
+				'required' => true,
+				'name' => tra('Blip.tv URL'),
+				'description' => tra('Blip.tv embed URL. Example: http://blip.tv/play/AYGd_GAC'),
+				'parent' => array('name' => 'type', 'value' => 'bliptv'),
 			),
 			'width' => array(
 				'required' => false,
@@ -62,6 +83,8 @@ function wikiplugin_flash_info() {
 
 function wikiplugin_flash($data, $params) {
 	global $tikilib, $prefs, $userlib, $user;
+	
+	// Handle file from a podcast file gallery
 	if (isset($params['fileId']) && !isset($params['movie'])) {
 		global $filegallib; include_once ('lib/filegals/filegallib.php');
 		$file_info = $filegallib->get_file_info($params['fileId']);
@@ -69,6 +92,26 @@ function wikiplugin_flash($data, $params) {
 			return tra('Permission denied');
 		}
 		$params['movie'] = $prefs['fgal_podcast_dir'].$file_info['path'];
+	}
+
+	// Handle Youtube video
+	if (isset($params['youtube']) && preg_match('|http(s)?://(\w+\.)?youtube\.com/watch\?v=([\w-]+)|', $params['youtube'], $matches)) {
+		$params['movie'] = "http://www.youtube.com/v/" . $matches[3];
+	}
+
+	// Handle Vimeo video
+	if (isset($params['vimeo']) && preg_match('|http(s)?://(www\.)?vimeo\.com/(clip:)?(\d+)|', $params['vimeo'], $matches)) {
+		$params['movie'] = 'http://vimeo.com/moogaloop.swf?clip_id=' . $matches[4];
+	}
+	
+	// Handle Blip.tv video
+	// We need the embed URL because there is tno relation between the video URL and the embed URL
+	if (isset($params['bliptv']) && preg_match('|http://blip.tv/play/\w+|', $params['bliptv'], $matches)) {
+		$params['movie'] = $params['bliptv'];
+	}	
+
+	if ((isset($params['youtube']) || isset($params['vimeo']) || isset($params['bliptv'])) && !isset($params['movie'])) {		
+		return tra('Invalid URL');
 	}
 		
 	$code = $tikilib->embed_flash($params);
