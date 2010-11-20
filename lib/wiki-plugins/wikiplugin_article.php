998ab1bf1906c85b52eeea5e42f75709f0f7f97c
jonnybradley <jonnybradley@users.sourceforge.net>
2010-11-20 18:27:02 +0000

    [FIX] wikiplugin_article: Missing global $tiki_p_admin_cms
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@30815 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_article.php
+++ lib/wiki-plugins/wikiplugin_article.php
@@ -44,7 +44,7 @@ function wikiplugin_article_info() {
 }
 
 function wikiplugin_article($data, $params) {
-	global $tikilib,$user,$userlib;
+	global $tikilib,$user,$userlib,$tiki_p_admin_cms;
 	global $statslib; include_once('lib/stats/statslib.php');
 
 	extract ($params,EXTR_SKIP);
