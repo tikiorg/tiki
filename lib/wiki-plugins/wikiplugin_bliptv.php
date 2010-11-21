7d7e030cde4ef169849c00ca19941095c3d3308e
xavidp <xavidp@users.sourceforge.net>
2010-11-21 17:33:24 +0000

    [bp/r30804][ENH] new wikiplugins vimeo and bliptv as alias for flash wikiplugin. Thanks rodrigo_sampaio
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@30831 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_bliptv.php
@@ -0,0 +1,48 @@
+<?php
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+require_once('lib/wiki-plugins/wikiplugin_flash.php');
+
+function wikiplugin_bliptv_info() {
+	return array(
+		'name' => tra('Bliptv'),
+		'documentation' => 'PluginBliptv',
+		'description' => tra('Display a Blip.tv video'),
+		'prefs' => array( 'wikiplugin_bliptv' ),
+		'icon' => 'pics/icons/bliptv.png',
+		'params' => array(
+			'url' => array(
+				'required' => true,
+				'name' => tra('URL'),
+				'description' => tra('Blip.tv embed URL. Example: http://blip.tv/play/AYGd_GAC'),
+				'filter' => 'url',
+				'default' => '',
+			),
+			'width' => array(
+				'required' => false,
+				'name' => tra('Width'),
+				'description' => tra('Width in pixels'),
+				'filter' => 'digits',
+			),
+			'height' => array(
+				'required' => false,
+				'name' => tra('Height'),
+				'description' => tra('Height in pixels'),
+				'filter' => 'digits',
+			),
+		),
+	);
+}
+
+function wikiplugin_bliptv($data, $params) {
+	if (isset($params['url'])) {
+		$params['bliptv'] = $params['url'];
+		unset($params['movie']);
+	}
+	
+	return wikiplugin_flash($data, $params);
+}
