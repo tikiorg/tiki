fddffcf75d93afcf8578740a9012bb42cf249b1f
chealer <chealer@users.sourceforge.net>
2010-12-02 20:24:32 +0000

    remove convertTZbyID() and clarify behavior of TimezoneIsValidId()
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@31000 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikidate-php5.php
+++ lib/tikidate-php5.php
@@ -186,11 +186,6 @@ class TikiDate
 		}
 		$this->date->setTimezone($dtz);
 	}
-
-	// TODO delete in Tiki 6 (if really unused)
-	function convertTZbyID($tz_id) {
-		$this->setTZbyID($tz_id);
-	}
 	
 	function convertMissingTimezone($tz_id) {
 		switch ($tz_id) {		// Convert timezones not in PHP 5
@@ -278,9 +273,10 @@ class TikiDate
 		return $this->date->format("e");
 	}
 
+	// Checks that the string is either a timezone identifier or an abbreviation. display_timezone can be manually set to an identifier in preferences but will be an [uppercase] abbreviation if auto-detected by JavaScript.
 	static function TimezoneIsValidId($id) {
-		return array_key_exists( strtolower($id), timezone_abbreviations_list() ) ||
-			in_array($id, timezone_identifiers_list());
+		return array_key_exists( strtolower($id), DateTimeZone::listAbbreviations() ) ||
+			in_array($id, DateTimeZone::listAbbreviations());
 	}
 	
 }
