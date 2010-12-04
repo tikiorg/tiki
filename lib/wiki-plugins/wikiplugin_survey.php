642ed90e77c698589ac672237ecd8c04208074f6
lindonb <lindonb@users.sourceforge.net>
2010-12-04 04:56:02 +0000

    Add defaults and other cleanup of plugin info
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@31051 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_survey.php
+++ lib/wiki-plugins/wikiplugin_survey.php
@@ -21,8 +21,8 @@ function wikiplugin_survey_help() {
 function wikiplugin_survey_info() {
 	return array(
 		'name' => tra('Survey'),
-		'documentation' => 'PluginSurvey',		
-		'description' => tra("Displays a survey"),
+		'documentation' => tra('PluginSurvey'),		
+		'description' => tra('Displays a survey'),
 		'prefs' => array( 'feature_surveys', 'wikiplugin_survey' ),
 		'body' => '',
 		'params' => array(
@@ -30,16 +30,21 @@ function wikiplugin_survey_info() {
 				'required' => true,
 				'name' => tra('Id'),
 				'description' => tra('Id'),
+				'filter' => 'digits',
+				'default' => '',
 			),
 			'page' => array(
 				'required' => false,
 				'name' => tra('Page'),
 				'description' => tra('Wiki Page to redirect the user after his vote'),
+				'default' => 'tiki-list_surveys.php',
 			),
 			'lang' => array(
 				'required' => false,
 				'name' => tra('Language'),
 				'description' => tra('Language'),
+				'filter' => 'alpha',
+				'default' => '',
 			),
 		),
 	);
