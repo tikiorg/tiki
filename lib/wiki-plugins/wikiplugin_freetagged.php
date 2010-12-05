7ba23cab73949213ee9b40e43d026d9cdfa57537
lindonb <lindonb@users.sourceforge.net>
2010-12-05 05:37:54 +0000

    Add options, default fields and other cleanup to plugin info for use in plugin edit popup and plugin manager
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@31128 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_freetagged.php
+++ lib/wiki-plugins/wikiplugin_freetagged.php
@@ -17,21 +17,23 @@ function wikiplugin_freetagged_help() {
 function wikiplugin_freetagged_info() {
 	return array(
     	'name' => tra('Freetagged'),
-    	'documentation' => 'PluginFreetagged',
+    	'documentation' => tra('PluginFreetagged'),
     	'description' => tra('Find similarly tagged objects'),
     	'prefs' => array( 'wikiplugin_freetagged' ),
     	'params' => array(
     		'tags' => array(
     			'required' => false,
     			'name' => tra('Tags to find similar to'),
-    			'description' => tra("Leave blank to use the object's own tags."),
+    			'description' => tra('Leave blank to use the object\'s own tags.'),
 				'filter' => 'text',
+				'default' => ''
 			),
 			'type' => array(
 				'required' => false,
 				'name' => tra('Type'),
 				'description' => tra('Type of objects to extract. Set to All to find all types.'),
 				'filter' => 'text',
+				'default' => null,
 				'options' => array (
 					array('text' => tra('Same'), 'value' => 'all'),
 					array('text' => tra('All'), 'value' => 'all'),
@@ -53,50 +55,89 @@ function wikiplugin_freetagged_info() {
 				'name' => tra('Offset'),
 				'description' => tra('Start record.'),
 				'filter' => 'text',
+				'default' => 0
 			),
 			'maxRecords' => array(
 				'required' => false,
 				'name' => tra('Max Records'),
 				'description' => tra('Default -1 (all)'),
 				'filter' => 'text',
+				'default' => -1
 			),
 			'sort_mode' => array(
 				'required' => false,
 				'name' => tra('Sort Order'),
 				'description' => tra('Choose from:  objectId, type, itemId, description, created, name, href, hits, comments_locked (Default: created_desc)'),
 				'filter' => 'text',
+				'default' => 'created_desc',
+				'options' => array (
+					array('text' => tra(''), 'value' => ''),
+					array('text' => tra('Comments Locked Ascending'), 'value' => 'comments_locked_asc'),
+					array('text' => tra('Comments Locked Descending'), 'value' => 'comments_locked_desc'),
+					array('text' => tra('Created Ascending'), 'value' => 'created_asc'),
+					array('text' => tra('Created Descending'), 'value' => 'created_desc'),
+					array('text' => tra('Description Ascending'), 'value' => 'description_asc'),
+					array('text' => tra('Description Descending'), 'value' => 'description_desc'),
+					array('text' => tra('Hits Ascending'), 'value' => 'hits_asc'),
+					array('text' => tra('Hits Descending'), 'value' => 'hits_desc'),
+					array('text' => tra('Href Ascending'), 'value' => 'href_asc'),
+					array('text' => tra('Href Descending'), 'value' => 'href_desc'),
+					array('text' => tra('Item ID Ascending'), 'value' => 'itemid_asc'),
+					array('text' => tra('Item ID Descending'), 'value' => 'itemid_desc'),
+					array('text' => tra('Name Ascending'), 'value' => 'name_asc'),
+					array('text' => tra('Name Descending'), 'value' => 'name_desc'),
+					array('text' => tra('Object ID Ascending'), 'value' => 'objectid_asc'),
+					array('text' => tra('Object ID Descending'), 'value' => 'objectid_desc'),
+					array('text' => tra('Type Ascending'), 'value' => 'type_asc'),
+					array('text' => tra('Type Descending'), 'value' => 'type_desc'),
+				),
 			),
 			'find' => array(
 				'required' => false,
 				'name' => tra('Find'),
-				'description' => tra(''),
+				'description' => tra('Show objects with names or descriptions similar to the text entered here'),
 				'filter' => 'text',
+				'default' => ''
 			),
 			'broaden' => array(
 				'required' => false,
 				'name' => tra('Broaden'),
 				'description' => tra('n|y'),
 				'filter' => 'text',
+				'default' => 'n',
+				'options' => array(
+					array('text' => '', 'value' => ''), 
+					array('text' => tra('Yes'), 'value' => 'y'), 
+					array('text' => tra('No'), 'value' => 'n')
+				)
 			),
 			'h_level' => array(
 				'required' => false,
-				'name' => tra('Header level'),
-				'description' => tra('Default: 3'),
+				'name' => tra('Header Level'),
+				'description' => tra('Choos the header level for formatting. Default is 3 (for header level h3)'),
 				'filter' => 'text',
+				'default' => '3'
 			),
 			'titles_only' => array(
 				'required' => false,
-				'name' => tra('Show titles only'),
-				'description' => tra('Default: n'),
+				'name' => tra('Show Titles Only'),
+				'description' => tra('Choose whether to show titles only (not set by default)'),
 				'filter' => 'text',
+				'default' => 'n',
+				'options' => array(
+					array('text' => '', 'value' => ''), 
+					array('text' => tra('Yes'), 'value' => 'y'), 
+					array('text' => tra('No'), 'value' => 'n')
+				)
 			),
 			'max_image_size' => array(
 				'required' => false,
 				'name' => tra('Maximum Image Size'),
 				'description' => tra('Height or width in pixels. Default = 0 (no maximum)'),
 				'filter' => 'text',
-			),
-		),
+				'default' => 0
+			)
+		)
 	);
 }
 
