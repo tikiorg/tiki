eb6a50000d1659263ed2cca04c845acbb667e2d3
jonnybradley <jonnybradley@users.sourceforge.net>
2010-12-11 13:11:07 +0000

    [MRG] Automatic merge, trunk 31348 to 31365
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@31366 b456876b-0849-0410-b77d-98878d47e9d5

--- coe_fgal_relook2/lib/prefs/menus.php
+++ coe_fgal_relook2/lib/prefs/menus.php
@@ -1,5 +1,5 @@
 <?php
-// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki CMS Groupware project
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -15,6 +15,12 @@ function prefs_menus_list() {
 		'menus_items_icons_path' => array(
 			'name' => tra('Default path for the icons'),
 			'type' => 'text',
+		),
+		'menus_item_names_raw' => array(
+			'name' => tra('Allow HTML in menu items (Please note: this might be insecure if you allow more people to edit menus)'),
+			'description' => tra('If enabled, treat menu item names as HTML item content and do not escape them (do not replace HTML special characters), allowing to use HTML in menu items to put images for example. Code must be valid. This allows menu item editors to put arbitrary HTML; only enable if you know what you are doing.'),
+			'help' => 'Menus',
+			'type' => 'flag',
 		),
 	);
 }
