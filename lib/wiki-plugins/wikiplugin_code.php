064660dd382733e39ade290a372465d6d0ac41d8
robertplummer <robertplummer@users.sourceforge.net>
2010-12-17 22:32:53 +0000

    [ADD] Initial add of codemirror being used in tiki we will need to add a way to turn it on and off, right now it is always on but only in plugin "Code" with the editor popup.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@31473 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_code.php
+++ lib/wiki-plugins/wikiplugin_code.php
@@ -5,6 +5,8 @@
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
 
+global $headerlib;
+
 // Displays a snippet of code
 
 function wikiplugin_code_info() {
@@ -207,3 +209,29 @@ function wikiplugin_code($data, $params) {
 	$code_count++;
 	return $out;
 }
+
+$headerlib->add_cssfile( 'lib/codemirror_tiki/docs.css' );
+$headerlib->add_jsfile( 'lib/codemirror_tiki/js/codemirror.js' );
+
+$headerlib->add_js("
+	$(document)
+		.bind('plugin_code_ready', function(args) {
+			var code = args.container.find('textarea:first')
+				.attr('id', 'code');
+			
+			//ensure that codemirror is running, if so run
+			if (CodeMirror) {
+				var editor = CodeMirror.fromTextArea('code', {
+					height: '350px',
+					parserfile: ['parsexml.js', 'parsecss.js', 'tokenizejavascript.js', 'parsejavascript.js', 'parsehtmlmixed.js'],
+					stylesheet: ['lib/codemirror_tiki/css/xmlcolors.css', 'lib/codemirror_tiki/css/jscolors.css', 'lib/codemirror_tiki/css/csscolors.css'],
+					path: 'lib/codemirror_tiki/js/',
+					onChange: function() {
+						//Setup codemirror to send the text back to the textarea
+						code.val(editor.getCode());
+					},
+					lineNumbers: true
+				});
+			}
+		});
+");
