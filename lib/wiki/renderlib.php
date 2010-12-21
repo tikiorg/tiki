eba9ecf513e8df284da0fc66e1ca33040b33589d
nkoth <nkoth@users.sourceforge.net>
2010-12-21 21:15:18 +0000

    [FIX] Usability: Show approving/staging interface on page history page as well (not just view page interface)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@31588 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki/renderlib.php
+++ lib/wiki/renderlib.php
@@ -517,7 +517,7 @@ class WikiRenderer
 		$this->smartyassign('trail', $crumbs);
 	} // }}}
 
-	private function setupStaging() // {{{
+	function setupStaging() // {{{
 	{
 		global $prefs, $tikilib, $categlib, $histlib, $tiki_p_edit;
 
