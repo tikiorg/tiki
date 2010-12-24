d9f8c1ea91327e896796e7ef2761fe6379e12001
robertplummer <robertplummer@users.sourceforge.net>
2010-12-24 13:35:24 +0000

    [ADD] Added R support to codemirror_tiki.php [MOD] Moved support for code to codemirror_tiki.php for a cleaner integration
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@31641 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_code.php
+++ lib/wiki-plugins/wikiplugin_code.php
@@ -208,32 +208,5 @@ function wikiplugin_code($data, $params) {
 	return $out;
 }
 
-require_once 'tiki-setup.php';
-global $headerlib, $prefs;
-if ( $prefs['feature_syntax_highlighter'] == 'y' ) {
-	$headerlib->add_cssfile( 'lib/codemirror_tiki/docs.css' );
-	$headerlib->add_jsfile( 'lib/codemirror_tiki/js/codemirror.js' );
-	
-	$headerlib->add_js("
-		$(document)
-			.bind('plugin_code_ready', function(args) {
-				var code = args.container.find('textarea:first')
-					.attr('id', 'code');
-				
-				//ensure that codemirror is running, if so run
-				if (CodeMirror) {
-					var editor = CodeMirror.fromTextArea('code', {
-						height: '350px',
-						parserfile: ['parsexml.js', 'parsecss.js', 'tokenizejavascript.js', 'parsejavascript.js', 'parsehtmlmixed.js'],
-						stylesheet: ['lib/codemirror_tiki/css/xmlcolors.css', 'lib/codemirror_tiki/css/jscolors.css', 'lib/codemirror_tiki/css/csscolors.css'],
-						path: 'lib/codemirror_tiki/js/',
-						onChange: function() {
-							//Setup codemirror to send the text back to the textarea
-							code.val(editor.getCode());
-						},
-						lineNumbers: true
-					});
-				}
-			});
-	");
-}
+require_once('lib/codemirror_tiki/codemirror_tiki.php');
+tiki_syntax_highlighter_code();
