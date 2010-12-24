9594e3955a446c661460c2e7885377c08b349942
robertplummer <robertplummer@users.sourceforge.net>
2010-12-24 21:23:26 +0000

    [FIX] css, added CKEditor detection so not to conflict.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@31650 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/codemirror_tiki/codemirror_tiki.php
+++ lib/codemirror_tiki/codemirror_tiki.php
@@ -8,28 +8,31 @@ function tiki_syntax_highlighter_base() {
 		$headerlib->add_jsfile( 'lib/codemirror/js/codemirror.js' );		
 
 		$headerlib->add_js("
-			var editwiki = $('#editwiki');
+			var startCodeMirror = function() {
+				var editwiki = $('#editwiki');
 				
-			//ensure that codemirror is running, if so run
-			if (CodeMirror) {			
-				if (editwiki[0]) {
-					var editor = CodeMirror.fromTextArea(editwiki[0], {
-						height: '350px',
-						path: 'lib/codemirror/js/',
-						parserfile: ['../../codemirror_tiki/js/parsetikisyntax.js'],
-						stylesheet: ['lib/codemirror_tiki/css/tikiwikisyntaxcolors.css'],
-						onChange: function() {
-							//Setup codemirror to send the text back to the textarea
-							editwiki.val(editor.getCode());
-						}
+				//ensure that codemirror is running and CKEditor isn't, if so run
+				if (window.CKEDITOR) return false;
+				if (!editwiki[0]) return false;
+				if (!CodeMirror) return false;
+
+				var editor = CodeMirror.fromTextArea(editwiki[0], {
+					height: '350px',
+					path: 'lib/codemirror/js/',
+					parserfile: ['../../codemirror_tiki/js/parsetikisyntax.js'],
+					stylesheet: ['lib/codemirror_tiki/css/tikiwikisyntaxcolors.css'],
+					onChange: function() {
+						//Setup codemirror to send the text back to the textarea
+						editwiki.val(editor.getCode());
+					}
+				});
+
+				editwiki
+					.change(function() {
+						editor.setCode(editwiki.val());
 					});
-				
-					editwiki
-						.change(function() {
-							editor.setCode(editwiki.val());
-						});
-				}
-			}
+			};
+			startCodeMirror();
 		");
 	}
 }
@@ -44,20 +47,21 @@ function tiki_syntax_highlighter_code() {
 			$(document)
 				.bind('plugin_code_ready', function(args) {
 					var code = args.container.find('textarea:first');
-					
-					//ensure that codemirror is running, if so run
-					if (CodeMirror && code[0]) {
-						var editor = CodeMirror.fromTextArea(code[0], {
-							height: '350px',
-							parserfile: ['parsexml.js', 'parsecss.js', 'tokenizejavascript.js', 'parsejavascript.js', 'parsehtmlmixed.js'],
-							stylesheet: ['lib/codemirror/css/xmlcolors.css', 'lib/codemirror/css/jscolors.css', 'lib/codemirror/css/csscolors.css'],
-							path: 'lib/codemirror/js/',
-							onChange: function() {
-								//Setup codemirror to send the text back to the textarea
-								code.val(editor.getCode());
-							}
-						});
-					}
+					//ensure that codemirror is running and CKEditor isn't, if so run
+					if (window.CKEDITOR) return false;
+					if (!code[0]) return false;
+					if (!CodeMirror) return false;
+
+					var editor = CodeMirror.fromTextArea(code[0], {
+						height: '350px',
+						parserfile: ['parsexml.js', 'parsecss.js', 'tokenizejavascript.js', 'parsejavascript.js', 'parsehtmlmixed.js'],
+						stylesheet: ['lib/codemirror/css/xmlcolors.css', 'lib/codemirror/css/jscolors.css', 'lib/codemirror/css/csscolors.css'],
+						path: 'lib/codemirror/js/',
+						onChange: function() {
+							//Setup codemirror to send the text back to the textarea
+							code.val(editor.getCode());
+						}
+					});
 				});
 		");
 	}
@@ -74,20 +78,22 @@ function tiki_syntax_highlighter_r() {
 				.bind('plugin_r_ready', function(args) {
 					var r = args.container.find('textarea:first');
 				
-					//ensure that codemirror is running, if so run
-					if (CodeMirror && r[0]) {
-						var editor = CodeMirror.fromTextArea(r[0], {
-							height: '350px',
-							parserfile: ['../../codemirror_tiki/js/parsersplus.js'],
-							stylesheet: ['lib/codemirror_tiki/css/rspluscolors.css'],
-							path: 'lib/codemirror/js/',
-							onChange: function() {
-								//Setup codemirror to send the text back to the textarea
-								r.val(editor.getCode());
-							},
-							lineNumbers: true
-						});
-					}
+					//ensure that codemirror is running and CKEditor isn't, if so run
+					if (window.CKEDITOR) return false;
+					if (!r[0]) return false;
+					if (!CodeMirror) return false;
+
+					var editor = CodeMirror.fromTextArea(r[0], {
+						height: '350px',
+						parserfile: ['../../codemirror_tiki/js/parsersplus.js'],
+						stylesheet: ['lib/codemirror_tiki/css/rspluscolors.css'],
+						path: 'lib/codemirror/js/',
+						onChange: function() {
+							//Setup codemirror to send the text back to the textarea
+							r.val(editor.getCode());
+						},
+						lineNumbers: true
+					});
 				});
 		");
 	}
@@ -97,28 +103,29 @@ function tiki_syntax_highlighter_rr() {
 	global $headerlib, $prefs;
 	if ( $prefs['feature_syntax_highlighter'] == 'y' ) {
 		$headerlib->add_cssfile( 'lib/codemirror_tiki/docs.css' );
-		$headerlib->add_jsfile( 'lib/codemirror_tiki/js/codemirror.js' );
+		$headerlib->add_jsfile( 'lib/codemirror/js/codemirror.js' );
 	
 		$headerlib->add_js("
 			$(document)
 				.bind('plugin_rr_ready', function(args) {
-					var rr = args.container.find('textarea:first')
-						.attr('id', 'rr');
+					var rr = args.container.find('textarea:first');
 				
-					//ensure that codemirror is running, if so run
-					if (CodeMirror) {
-						var editor = CodeMirror.fromTextArea('rr', {
-							height: '350px',
-							parserfile: ['../../codemirror_tiki/js/parsersplus.js'],
-							stylesheet: ['lib/codemirror_tiki/css/rspluscolors.css'],
-							path: 'lib/codemirror/js/',
-							onChange: function() {
-								//Setup codemirror to send the text back to the textarea
-								rr.val(editor.getCode());
-							},
-							lineNumbers: true
-						});
-					}
+					//ensure that codemirror is running and CKEditor isn't, if so run
+					if (window.CKEDITOR) return false;
+					if (!rr[0]) return false;
+					if (!CodeMirror) return false;
+
+					var editor = CodeMirror.fromTextArea(rr[0], {
+						height: '350px',
+						parserfile: ['../../codemirror_tiki/js/parsersplus.js'],
+						stylesheet: ['lib/codemirror_tiki/css/rspluscolors.css'],
+						path: 'lib/codemirror/js/',
+						onChange: function() {
+							//Setup codemirror to send the text back to the textarea
+							rr.val(editor.getCode());
+						},
+						lineNumbers: true
+					});
 				});
 		");
 	}
