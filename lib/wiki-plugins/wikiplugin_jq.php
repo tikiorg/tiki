ce74ba8f9164e67542350a74334a311747025786
chealer <chealer@users.sourceforge.net>
2011-01-03 06:21:11 +0000

    JQ plugin doesn't depend on feature_jquery anymore
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@31726 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_jq.php
+++ lib/wiki-plugins/wikiplugin_jq.php
@@ -7,9 +7,6 @@
 /* 
  * Params
  *     notonready = bool (default: false) - set to true if you want it _not_ to execute on document ready
- *     nojquery = string (default: "<!-- jq smarty plugin inactive: feature_jquery off -->") - Optional markup for when feature_jquery is off
- * 
- * data is the JQuery javascript code
  */
 function wikiplugin_jq_help() {
 	return tra("Insert JQuery javascript code.")."<br />~np~{JQ(nojquery='<p>You need JQuery for this!</p>')}".tra("jquery code e.g. \$(\"img\").click(function() {\n  \$(this).hide(\"slow\").show(\"fast\");\n});")."{JQ}~/np~";
@@ -46,7 +43,7 @@ function wikiplugin_jq($data, $params) {
 	global $headerlib, $prefs;
 	extract($params, EXTR_SKIP);
 	
-	$nojquery = isset($nojquery) ? $nojquery : tr('<!-- jq smarty plugin inactive: feature_jquery off -->');
+	$nojquery = isset($nojquery) ? $nojquery : tr('<!-- jq plugin inactive: JavaScript off -->');
 	if ($prefs['javascript_enabled'] != 'y') { return $nojquery; }
 	$notonready = isset($notonready) ? $notonready : false;
 	
