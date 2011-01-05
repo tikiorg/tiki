92b3d3815b557691b3a0de65f52c33077c9a8576
sylvieg <sylvieg@users.sourceforge.net>
2011-01-05 17:35:19 +0000

    [bp/r31773][FIX]sefurl: missing image sefurl (used in CATEGORY)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@31774 b456876b-0849-0410-b77d-98878d47e9d5

--- 6.x/lib/smarty_tiki/modifier.sefurl.php
+++ 6.x/lib/smarty_tiki/modifier.sefurl.php
@@ -69,6 +69,9 @@ function smarty_modifier_sefurl($source, $type='wiki', $with_next = '', $all_lan
 	case 'file gallery':
 		$href = 'tiki-list_file_gallery.php?galleryId='.$source;
 		break;
+	case 'image':
+		$href = 'tiki-browse_image.php?imageId='.$source;
+		break;
 	default:
 		$href = $source;
 		break;
