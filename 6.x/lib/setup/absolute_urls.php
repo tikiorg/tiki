015de7e5dd34bfe19f598cea208ad2c335fedb77
jonnybradley <jonnybradley@users.sourceforge.net>
2011-01-18 14:49:12 +0000

    [bp/r31975][FIX] header: Missing base_uri smarty var
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@31976 b456876b-0849-0410-b77d-98878d47e9d5

--- 6.x/lib/setup/absolute_urls.php
+++ 6.x/lib/setup/absolute_urls.php
@@ -47,6 +47,8 @@ $base_url_http = 'http://'.$url_host.(($prefs['http_port']!='')?':'.$prefs['http
 $base_url_https = 'https://'.$url_host.(($prefs['https_port']!='')?':'.$prefs['https_port']:'').$url_path;
 // for <base> tag, which needs the " absolute URI that acts as the base URI for resolving relative URIs", not just the root of the site
 $base_uri = !empty($_SERVER['REDIRECT_SCRIPT_URI']) ? $_SERVER['REDIRECT_SCRIPT_URI'] : isset($_SERVER['SCRIPT_URI']) ? $_SERVER['SCRIPT_URI'] : $base_url;
+global $smarty;
+$smarty->assign('base_uri', $base_uri);
 
 // SSL options
 
