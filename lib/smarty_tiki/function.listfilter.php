34e233625efc619d967f8811e2a07510d4666fd9
jonnybradley <jonnybradley@users.sourceforge.net>
2011-02-07 20:23:40 +0000

    [FIX] listfilter: Change key event so it picks up backspace & delete keys
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@32490 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.listfilter.php
+++ lib/smarty_tiki/function.listfilter.php
@@ -66,7 +66,7 @@ function smarty_function_listfilter($params, &$smarty) {
 		if (!isset($selectors)) $selectors = ".$id table tr";
 			
 		$content = "
-\$('#$id').keypress( function() {
+\$('#$id').keyup( function() {
 	var criterias = this.value.toLowerCase().split( /\s+/ );
 	
 	\$('$selectors').each( function() {
