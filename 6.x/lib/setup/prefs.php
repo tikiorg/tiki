35787bcde2f05214a4a603ef38537995ffeecebe
lindonb <lindonb@users.sourceforge.net>
2011-02-13 06:16:54 +0000

    [bp/r32623][FIX]Keep payment_cclite_hashing_algorithm preference from setting when user sets other preferences in the payment admin panel
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@32630 b456876b-0849-0410-b77d-98878d47e9d5

--- 6.x/lib/setup/prefs.php
+++ 6.x/lib/setup/prefs.php
@@ -1779,7 +1779,7 @@ function get_default_prefs() {
 		'payment_cclite_merchant_user' => 'manager',
 		'payment_cclite_mode' => 'test',
 		'payment_cclite_notify' => 'y',
-		'payment_cclite_hashing_algorithm' => '',
+		'payment_cclite_hashing_algorithm' => 'sha1',
 		'payment_manual' => '',
 		'payment_invoice_prefix' => '',
 
