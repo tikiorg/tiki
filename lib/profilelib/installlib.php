847812653bd9b2eb690cffd52d670ccf50b05c6c
pkdille <pkdille@users.sourceforge.net>
2011-02-15 21:02:57 +0000

    [QT] Quality Team Backport
    r31624 | sampaioprimo | 2010-12-22 20:05:13 +0100 (Wed, 22 Dec 2010) | 2 lines
    [FIX][bp/31623] profilelib/installlib.php and xmlrpc.php were using wrong param order when calling bloglib::blog_post()
    
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@32722 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/profilelib/installlib.php
+++ lib/profilelib/installlib.php
@@ -1485,7 +1485,7 @@ class Tiki_Profile_InstallHandler_BlogPost extends Tiki_Profile_InstallHandler /
 			}
 		}
 
-		$entryId = $bloglib->blog_post( $data['blog'], $data['content'], $data['user'], $data['title'], '', $data['private'] );
+		$entryId = $bloglib->blog_post( $data['blog'], $data['content'], $data['excerpt'], $data['user'], $data['title'], '', $data['private'] );
 
 		return $entryId;
 	}
