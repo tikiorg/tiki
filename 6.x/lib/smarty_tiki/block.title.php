6361a401197c06fd883da2adc2da32f98850c14a
nyloth <nyloth@users.sourceforge.net>
2011-02-21 17:24:06 +0000

    [QT] Rollback rejected proposition r32304
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@33002 b456876b-0849-0410-b77d-98878d47e9d5

--- 6.x/lib/smarty_tiki/block.title.php
+++ 6.x/lib/smarty_tiki/block.title.php
@@ -24,7 +24,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
  */
 
 function smarty_block_title($params, $content, &$smarty, $repeat) {
-  global $prefs, $tiki_p_view_templates, $tiki_p_edit_templates, $tiki_p_admin, $smarty;
+  global $prefs, $tiki_p_view_templates, $tiki_p_edit_templates, $tiki_p_admin;
 
   if ( $repeat || $content == '' ) return;
   include_once('lib/smarty_tiki/function.icon.php');
@@ -39,7 +39,6 @@ function smarty_block_title($params, $content, &$smarty, $repeat) {
   $html = '<h1>';
   $html .= '<a class="pagetitle" href="' . $params['url'] . '">' . $content . "</a>\n";
   
-  if ($smarty->get_template_vars('print_page') != 'y') {
   if ( $prefs['feature_help'] == 'y' && $prefs['helpurl'] != '' && $params['help'] != '' ) {
     $html .= '<a href="' . $prefs['helpurl'] . $params['help'] . '" class="titletips" title="' . tra('Help page:') . ' ' . $content . '">'
           . smarty_function_icon(array('_id' => 'help') , $smarty)
@@ -56,7 +55,6 @@ function smarty_block_title($params, $content, &$smarty, $repeat) {
     $html .= '<a class="titletips" href="tiki-admin.php?page=' . $params['admpage'] . '" title="' . tra('Admin page:') . ' ' . $content . '">'
           . smarty_function_icon(array('_id' => 'wrench', 'alt' => tra('Admin Feature')), $smarty)
           . "</a>\n";
-  }
   }
   
   $html .= '</h1>';
