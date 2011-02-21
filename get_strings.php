27bac6e0887ffd8896aed7f4b4a7fa345bf23337
sept_7 <sept_7@users.sourceforge.net>
2011-02-21 20:08:01 +0000

    [FIX] Fix for the release process
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@33020 b456876b-0849-0410-b77d-98878d47e9d5

--- get_strings.php
+++ get_strings.php
@@ -247,6 +247,11 @@ function formatted_print($string) {
 ////////////////////////////////////////////////////////////////////////////
 
 if ( $script_mode ) {
+	
+	// Needs a fake tra() when called from command line
+	function tra($msg) {
+		return $msg;
+	}
 
 	$_REQUEST = array();
 	for ( $k = 1 ; $k < $_SERVER['argc'] ; $k++ ) {
