3049906edcae041a65f80bff4c81426b41397d8d
nyloth <nyloth@users.sourceforge.net>
2011-02-21 15:30:33 +0000

    [QT] Quality Team Backport
    r32604 | nkoth | 2011-02-12 00:54:18 +0100 (sam. 12 févr. 2011) | 1 ligne
    [bp/r32603][FIX] database error caused by trying to set attribute with missing itemId (before item is created) when first creating tracker item
    
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@32974 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/trackers/trackerlib.php
+++ lib/trackers/trackerlib.php
@@ -1362,15 +1362,13 @@ class TrackerLib extends TikiLib
 				}
 				$geo = explode(',', $ins_fields['data'][$i]['value']);
 				if (!empty($geo[0]) && !empty($geo[1])) {
-					$attributelib->set_attribute('trackeritem', $itemId, 'tiki.geo.lon', $geo[0]);
-					$attributelib->set_attribute('trackeritem', $itemId, 'tiki.geo.lat', $geo[1]);
+					$geoattribute = $geo;
 					if ($trackersync && $prefs["user_trackersync_geo"] == 'y') {
 						$trackersync_lon = $geo[0];
 						$trackersync_lat = $geo[1];
 					}
 				}
 				if (!empty($geo[2])) {
-					$attributelib->set_attribute('trackeritem', $itemId, 'tiki.geo.google.zoom', $geo[2]);
 					if ($trackersync && $prefs["user_trackersync_geo"] == 'y') {
 						$trackersync_zoom = $geo[2];
 					}
@@ -1989,6 +1987,13 @@ class TrackerLib extends TikiLib
 			$this->object_post_save( array('type'=>'trackeritem', 'object'=>$itemId, 'name' => "Tracker Item $itemId", 'href'=>"tiki-view_tracker_item.php?itemId=$itemId"), array( 'content' => $parsed ));
 		}
 
+		if (!empty($geoattribute) && $itemId) {
+			$attributelib->set_attribute('trackeritem', $itemId, 'tiki.geo.lon', $geoattribute[0]);
+			$attributelib->set_attribute('trackeritem', $itemId, 'tiki.geo.lat', $geoattribute[1]);
+			if (!empty($geoattribute[2])) {
+				$attributelib->set_attribute('trackeritem', $itemId, 'tiki.geo.google.zoom', $geoattribute[2]);
+			}
+		}
 		return $itemId;
 	}
 
