b69b921c9d5cb0adbdd398e20205d009d76bde16
sylvieg <sylvieg@users.sourceforge.net>
2011-03-01 16:42:56 +0000

    [FIX]categories: ksort does not respect accents. the sort is already done correctly on the name
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@33178 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/categories/categlib.php
+++ lib/categories/categlib.php
@@ -1042,7 +1042,6 @@ class CategLib extends ObjectLib
 			$res["objects"] = $this->getOne($query,array($id));
 			$ret[$categpathforsort] = $res;
 		}
-		ksort($ret);
 		$ret = array_values($ret);
 		if ($showWS)
 			$cachelib->cacheItem("allws",serialize($ret));
