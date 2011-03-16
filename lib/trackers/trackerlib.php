0079fb903f6d6d5577cb83611c0054d878457f90
sylvieg <sylvieg@users.sourceforge.net>
2011-03-16 19:36:26 +0000

    [FIX]tracker: image tracker field display for tiki_view_tracker_item
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@33438 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/trackers/trackerlib.php
+++ lib/trackers/trackerlib.php
@@ -5722,6 +5722,55 @@ class Tracker_field_Image extends Tracker_Field_File
 
 		return parent::getInsertValues($requestData);
 	}
+	function getInnerValue()
+	{
+		global $prefs;
+		$smarty = TikiLib::lib('smarty');
+
+		$val = $this->getValue();
+		$list_mode = $smarty->get_template_vars('list_mode'); // to be fixed
+		if ($list_mode == 'csv') {
+			return $val; // return the filename
+		}
+		$pre = '';
+		if (!empty($val)) {
+			$params['file'] = $val;
+			if ($list_mode != 'n') {
+				$shadowtype = $this->getOption(5);
+				if ($prefs['feature_shadowbox'] == 'y' && !empty($shadowtype)) {
+					switch ($shadowtype) {
+					case 'item':
+						$rel = '['.$this->getItemId().']';
+						break;
+					case 'individual':
+						$rel = '';
+						break;
+					default:
+						$rel = '['.$this->getConfiguration('fieldId').']';
+						break;
+					}
+					$pre = "<a href=\"$val\" rel=\"shadowbox$rel;type=img\">"; 
+				}
+				if ($this->getOption(0))
+					$params['width'] = $this->getOption(0);
+				if ($this->getOption(1))
+					$params['height'] = $this->getOption(1);
+			} else {
+				if ($this->getOption(2))
+					$params['width'] = $this->getOption(2);
+				if ($this->getOption(3))
+					$params['height'] = $this->getOption(3);
+			}
+		} else {
+			$params['file'] = 'img/icons/na_pict.gif';
+			$params['alt'] = 'n/a';
+		}
+		require_once $smarty->_get_plugin_filepath('function', 'html_image');
+		$ret = smarty_function_html_image($params, $smarty);
+		if (!empty($pre))
+			$ret = $pre.$ret.'</a>';
+		return $ret;
+	}
 }
 
 /**
