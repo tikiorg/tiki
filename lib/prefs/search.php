5b38cd6b02253524ecef9c78856bef4f73b6fd8d
lphuberdeau <lphuberdeau@users.sourceforge.net>
2011-04-19 12:39:06 +0000

    [ENH] Cleaning up admin panel
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/7.x@34031 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/prefs/search.php
+++ lib/prefs/search.php
@@ -7,52 +7,6 @@
 
 function prefs_search_list() {
 	return array (
-		'search_refresh_index_mode' => array(
-			'name' => tra('Refresh mode'),
-			'type' => 'list',
-			'perspective' => false,
-			'options' => array(
-				'normal' => tra('Normal'),
-				'random' => tra('Random'),
-			),
-		),
-		'search_refresh_rate' => array(
-			'name' => tra('Refresh rate'),
-			'hint' => tra('Use 0 for no refresh'),
-			'type' => 'text',
-			'size' => '4',
-			'perspective' => false,
-		),
-		'search_min_wordlength' => array(
-			'name' => tra('Minimum length of search word'),
-			'type' => 'text',
-			'size' => '4',
-			'perspective' => false,
-		),
-		'search_max_syllwords' => array(
-			'name' => tra('Max. number of words containing a syllable'),
-			'type' => 'text',
-			'size' => '4',
-			'perspective' => false,
-		),
-		'search_syll_age' => array(
-			'name' => tra('Max. age in hours of syllable search cache'),
-			'type' => 'text',
-			'size' => '4',
-			'perspective' => false,
-		),
-		'search_lru_purge_rate' => array(
-			'name' => tra('LRU list purging rate'),
-			'type' => 'text',
-			'size' => '4',
-			'perspective' => false,
-		),
-		'search_lru_length' => array(
-			'name' => tra('LRU list length'),
-			'type' => 'text',
-			'size' => '4',
-			'perspective' => false,
-		),
 		'search_parsed_snippet' => array(
 			'name' => tra('Parse the results'),
 			'hint' => tra('May impact performance'),
