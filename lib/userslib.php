193f148286f98e41838606433e0a0d9e3ef6a92e
sampaioprimo <sampaioprimo@users.sourceforge.net>
2011-04-25 13:54:41 +0000

    [FIX] check for both tiki_p_list_users and tiki_p_admin instead of checking only for tiki_p_list_users
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/7.x@34110 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/userslib.php
+++ lib/userslib.php
@@ -1502,9 +1502,11 @@ class UsersLib extends TikiLib
 	
 	function get_users_light($offset = 0, $maxRecords = -1, $sort_mode = 'login_asc', $find = '', $group = '') {
 		// This is a lighter version of get_users_names designed for ajax checking of userrealnames
-		global $prefs, $tiki_p_list_users;
+		global $prefs, $tiki_p_list_users, $tiki_p_admin;
 
-		if ( $tiki_p_list_users	!== 'y') return array();
+		if ($tiki_p_list_users	!== 'y' && $tiki_p_admin != 'y') {
+			return array();
+		}
 
 		$mid = '';
 		$bindvars = array();
@@ -1547,8 +1549,11 @@ class UsersLib extends TikiLib
 	
 	function get_users_names($offset = 0, $maxRecords = -1, $sort_mode = 'login_asc', $find = '') {
 
-		global $tiki_p_list_users;
-		if ( $tiki_p_list_users	!== 'y') return array();
+		global $tiki_p_list_users, $tiki_p_admin;
+		
+		if ($tiki_p_list_users	!== 'y' && $tiki_p_admin != 'y') {
+			return array();
+		}
 
 		// This function gets an array of user login names.
 		if ( !empty($find) ) {
@@ -1573,8 +1578,11 @@ class UsersLib extends TikiLib
 
 	function get_users($offset = 0, $maxRecords = -1, $sort_mode = 'login_asc', $find = '', $initial = '', $inclusion=false, $group='', $email='') {
 
-		global $tiki_p_list_users;
-		if ( $tiki_p_list_users	!== 'y') return array();
+		global $tiki_p_list_users, $tiki_p_admin;
+		
+		if ($tiki_p_list_users	!== 'y' && $tiki_p_admin != 'y') { 
+			return array();
+		}
 
 		$mid = '';
 		$bindvars = array();
