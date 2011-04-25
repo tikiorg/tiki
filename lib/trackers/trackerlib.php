f5455a3bb65dd90efdc3776f31162d174997e5f2
jonnybradley <jonnybradley@users.sourceforge.net>
2011-04-25 17:28:13 +0000

    [FIX] trackers: Various - missing param, global and library, also remove undefined var in get_item_value() and correct logic
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/7.x@34118 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/trackers/trackerlib.php
+++ lib/trackers/trackerlib.php
@@ -500,13 +500,11 @@ class TrackerLib extends TikiLib
 				if ($row['lang'] == $prefs['language']) {
 					return $row['value'];
 				}
-
-				$ret = $res['value'];
 			}
-		} else {
-			if ($res = reset($result)) {
-				$ret = $res['value'];
-			}
+		}
+		
+		if ($res = reset($result)) {
+			$ret = $res['value'];
 		}
 		return $ret;
 	}
@@ -3096,7 +3094,7 @@ class TrackerLib extends TikiLib
 			$categlib->remove_category($currentCategId);
 		}
 
-		foreach ($this->get_all_tracker_items() as $itemId) {
+		foreach ($this->get_all_tracker_items($trackerId) as $itemId) {
 			$this->remove_tracker_item($itemId);
 		}
 
@@ -3918,6 +3916,7 @@ class TrackerLib extends TikiLib
 	function get_user_item(&$trackerId, $trackerOptions, $userparam=null, $user= null, $status='') {
 		global $prefs;
 		$tikilib = TikiLib::lib('tiki');
+		$userlib = TikiLib::lib('user');
 		if (empty($user)) {
 			$user = $GLOBALS['user'];
 		}
@@ -4314,7 +4313,7 @@ class TrackerLib extends TikiLib
 		return false;
 	}
 	function fieldId_is_editable($field, $item) {
-		global $tiki_p_admin_trackers;
+		global $tiki_p_admin_trackers, $user;
 		if ($tiki_p_admin_trackers == 'y') {
 			return true;
 		}
