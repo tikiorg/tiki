478bd0814779127e15b5f4289bec1f0b2ed50da3
sylvieg <sylvieg@users.sourceforge.net>
2011-05-15 21:56:52 +0000

    [FIX]tracker: change also the CR and quote for any field (especially textarea)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@34435 b456876b-0849-0410-b77d-98878d47e9d5

--- 6.x/tiki-export_tracker_ajax.php
+++ 6.x/tiki-export_tracker_ajax.php
@@ -316,12 +316,12 @@ while (($items = $trklib->list_items($_REQUEST['trackerId'], $offset, $chunkSize
 							if (is_array($data)) {			// TODO handle other types of field better here (preferably in a function in $trklib)
 								$data = implode('%%%', $data);
 							}
-							$data = str_replace(array("\r\n", "\n", '<br />', $delimitorL, $delimitorR), array($CR, $CR, $CR, $delimitorL.$delimitorL, $delimitorR.$delimitorR), $data);
+L.$delimitorL, $delimitorR.$delimitorR), $data);
 							break;
 					default: 
 						$data = $smarty->fetch('tracker_item_field_value.tpl');
 					}
-
+				$data = str_replace(array("\r\n", "\n", '<br />', $delimitorL, $delimitorR), array($CR, $CR, $CR, $delimitor
 				}
 				$str .= needs_separator($str) ? '' : $separator;
 				$str .= $delimitorL.$data.$delimitorR;
