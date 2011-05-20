dee28ce087fe620c9bdff28a3f2bf70f60565369
chealer <chealer@users.sourceforge.net>
2011-05-20 03:49:03 +0000

    Fix SVN properties
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@34545 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/20110519_quick_edit_categ_params_merge_tiki.php
+++ installer/schema/20110519_quick_edit_categ_params_merge_tiki.php
@@ -3,7 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: 20091004_last_tracker_items_modules_merge_tiki.php 33195 2011-03-02 17:43:40Z changi67 $
+// $Id$
 
 if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
   header("location: index.php");
