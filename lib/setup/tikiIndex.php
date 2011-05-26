26733d58bb9f5e5acba62592b235be3357934174
sylvieg <sylvieg@users.sourceforge.net>
2011-05-26 16:35:01 +0000

    typo
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/7.x@34627 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/tikiIndex.php
+++ lib/setup/tikiIndex.php
@@ -3,7 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: $
+// $Id:$
 
 //this script may only be included - so its better to die if called directly.
 $access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__));
