3a6c40ea941cad4fb05a8f944c976266006958dc
chealer <chealer@users.sourceforge.net>
2011-05-30 00:15:57 +0000

    remove max parameter to the max
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/7.x@34697 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-func-trackerhelp.php
+++ modules/mod-func-trackerhelp.php
@@ -33,7 +33,7 @@ function module_trackerhelp_info() {
 
 function module_trackerhelp( $mod_reference, &$module_params ) {
 	global $smarty;
-	$default = array('height' => 4, 'cols' => 23, 'max'=> 100);
+	$default = array('height' => 4, 'cols' => 23);
 	$module_params = array_merge($default, $module_params);
 	if (!empty($_REQUEST['trackerhelp'])) {
 		global $trklib; include_once('lib/trackers/trackerlib.php');
