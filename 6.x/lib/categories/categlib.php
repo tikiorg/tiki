53e2d49321d00ca594b20445df4fbc41d85c2c98
jonnybradley <jonnybradley@users.sourceforge.net>
2011-06-04 15:31:52 +0000

    [bp/r34759+60][FIX] categories: Fix getting multiple categories in "and" mode and stop ignoring maxRecords in get_categoryobjects (previous workaround) Also partial bp/r32018 to handle an array of types
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@34761 b456876b-0849-0410-b77d-98878d47e9d5

--- 6.x/lib/categories/categlib.php
+++ 6.x/lib/categories/categlib.php
@@ -459,14 +459,15 @@ class CategLib extends ObjectLib
 	    $join = '';
 	    if (is_array($categId) && $and) {
 			$categId = $this->get_jailed( $categId );
-			$i = count($categId);
-			$bindWhere = $categId;
+			$i = count($categId)+1;
+			$bindWhere = array();
 			foreach ($categId as $c) {
-				if (--$i)
-					$join .= " INNER JOIN tiki_category_objects tco$i on (tco$i.`catObjectId`=o.`catObjectId` and tco$i.`categId`=?) ";
+				if (--$i) {
+					$join .= " INNER JOIN tiki_category_objects tco$i on tco$i.`catObjectId`=o.`objectId` and tco$i.`categId`=? ";
+					$bindWhere[] = $c;
+				}
 			}
-			$where = ' AND c.`categId`=? ';
-	   } elseif (is_array($categId)) {
+		} elseif (is_array($categId)) {
 			$bindWhere = $categId;
 			if ($deep) {
 				foreach ($categId as $c) {
@@ -499,8 +500,13 @@ class CategLib extends ObjectLib
 			$where .= " AND (`name` LIKE ? OR `description` LIKE ?)";
 		} 
 		if (!empty($type)) {
-			$where .= ' AND `type` =? ';
-			$bindWhere[] = $type;
+			if (is_array($type)) {
+				$where .= ' AND `type` in ('.implode(',',array_fill(0,count($type),'?')).')';
+				$bindWhere = array_merge($bindWhere, $type);
+			} else {
+				$where .= ' AND `type` =? ';
+				$bindWhere[] = $type;
+			}
 		}
 
 		global $user;
@@ -517,7 +523,7 @@ class CategLib extends ObjectLib
 		}
 
 		// Fetch all results as was done before, but only do it once
-		$query_cant = "SELECT DISTINCT c.*, o.* FROM `tiki_category_objects` c, `tiki_categorized_objects` co, `tiki_objects` o WHERE c.`catObjectId`=o.`objectId` AND o.`objectId`=co.`catObjectId` $where";
+		$query_cant = "SELECT DISTINCT c.*, o.* FROM `tiki_category_objects` c, `tiki_categorized_objects` co, `tiki_objects` o $join WHERE c.`catObjectId`=o.`objectId` AND o.`objectId`=co.`catObjectId` $where";
 		$query = $query_cant . $orderBy;
 		$result = $this->fetchAll($query,$bindVars);
 		$cant = count($result);
@@ -1220,7 +1226,7 @@ class CategLib extends ObjectLib
 		foreach ($catids as $id) {
 			$titles["$id"] = $this->get_category_name($id);
 			$objectcat = array();
-			$objectcat = $this->list_category_objects($id, $offset, $maxRecords, $sort, '', $find, $sub);
+			$objectcat = $this->list_category_objects($id, $offset, $maxRecords, $sort, $types == '*'? '': $typesallowed, $find, $sub, $and);
 
 			$acats = $andcat = array();
 			foreach ($objectcat["data"] as $obj) {
