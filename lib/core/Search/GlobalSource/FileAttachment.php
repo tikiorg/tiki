d3a42808998a132197e6423ffe10d40536cb7b90
lphuberdeau <lphuberdeau@users.sourceforge.net>
2011-06-06 18:59:32 +0000

    [FIX] Missing file
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@34794 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/core/Search/GlobalSource/FileAttachment.php
@@ -0,0 +1,55 @@
+<?php
+// (c) Copyright 2002-2011 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: FreeTagSource.php 33195 2011-03-02 17:43:40Z changi67 $
+
+class Search_GlobalSource_FileAttachment implements Search_GlobalSource_Interface
+{
+	private $relationlib;
+	private $fileSource;
+
+	function __construct()
+	{
+		$this->relationlib = TikiLib::lib('relation');
+		$this->fileSource = new Search_ContentSource_FileSource;
+	}
+
+	function getProvidedFields()
+	{
+		return array('attachment_contents', 'attachments');
+	}
+
+	function getGlobalFields()
+	{
+		return array(
+			'attachment_contents' => false,
+		);
+	}
+
+	function getData($objectType, $objectId, Search_Type_Factory_Interface $typeFactory, array $data = array())
+	{
+		$relations = $this->relationlib->get_relations_from($objectType, $objectId, 'tiki.file.attach');
+
+		$textual = array();
+		$files = array();
+
+		foreach ($relations as $rel) {
+			if ($rel['type'] == 'file') {
+				$files[] = $rel['itemId'];
+				$data = $this->fileSource->getDocument($rel['itemId'], $typeFactory);
+
+				foreach ($this->fileSource->getGlobalFields() as $name => $keep) {
+					$textual[] = $data[$name]->getValue();
+				}
+			}
+		}
+
+		return array(
+			'attachments' => $typeFactory->multivalue($files),
+			'attachment_contents' => $typeFactory->plaintext(implode(' ', $textual)),
+		);
+	}
+}
+
