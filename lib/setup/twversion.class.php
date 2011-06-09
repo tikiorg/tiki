1bdc13d6593743ba4138715e351ac47bef16bcfd
changi67 <changi67@users.sourceforge.net>
2011-06-09 20:27:23 +0000

    [REL] Preparing 7.0 release
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/7.x@34872 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -23,10 +23,10 @@ class TWVersion
 		//   stable   : Represents stable releases.
 		//   unstable : Represents candidate and test/development releases.
 		//   trunk     : Represents next generation development version.
-		$this->branch 	= 'unstable';
+		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '7.0RC1';	// needs to have no spaces for releases
+		$this->version 	= '7.0';	// needs to have no spaces for releases
 		$this->star	= '';
 		$this->releases	= array();
 
