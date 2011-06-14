e057dfd4067c20015e81de0e10216f7a83e78b96
robertplummer <robertplummer@users.sourceforge.net>
2011-06-14 13:36:05 +0000

    [NEW] Creation of new lib to be used with trackers, tracker query lib.  This lib is simply for reporting tracker data
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@34920 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/trackers/trackerquerylib.php
@@ -0,0 +1,467 @@
+<?php
+// (c) Copyright 2002-2011 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+/**
+ * Tracker Query Library
+ *
+ * \brief Functions to support reporting of the Trackers.
+ *
+ * @package		Tiki
+ * @subpackage		Trackers
+ * @author		Robert Plummer
+ * @copyright		Copyright (c) 2002-2009, All Rights Reserved.
+ * 			See copyright.txt for details and a complete list of authors.
+ * @license		LGPL - See license.txt for details.
+ * @version		SVN $Rev$
+ * @filesource
+ * @link		http://dev.tiki.org/Trackers
+ * @since		TIki 8
+ */
+/**
+ * This script may only be included, so it is better to die if called directly.
+ */
+if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
+  header("location: index.php");
+  exit;
+}
+
+class TrackerQueryLib extends TikiLib {
+	function __construct() {
+		global $tikilib, $tikilib;
+		$tikilib->query("
+		 	DROP TABLE IF EXISTS temp_tracker_field_options;
+			CREATE TEMPORARY TABLE temp_tracker_field_options (
+				trackerIdHere INT,
+				trackerIdThere INT,
+				fieldIdThere INT,
+				fieldIdHere INT,
+				displayFieldIdThere INT,
+				displayFieldIdHere INT,
+				linkToItems INT,
+				type VARCHAR(1),
+				options VARCHAR(50)
+			);
+			
+			INSERT INTO temp_tracker_field_options
+			SELECT
+				tiki_tracker_fields.trackerId,
+				REPLACE(SUBSTRING(
+					SUBSTRING_INDEX(tiki_tracker_fields.options, ',', 1),
+					LENGTH(SUBSTRING_INDEX(tiki_tracker_fields.options, ',', 1 -1)) + 1
+					),
+				',', ''),
+				REPLACE(SUBSTRING(
+					SUBSTRING_INDEX(tiki_tracker_fields.options, ',', 2),
+					LENGTH(SUBSTRING_INDEX(tiki_tracker_fields.options, ',', 2 -1)) + 1
+					),
+				',', ''),
+				REPLACE(SUBSTRING(
+					SUBSTRING_INDEX(tiki_tracker_fields.options, ',', 3),
+					LENGTH(SUBSTRING_INDEX(tiki_tracker_fields.options, ',', 3 -1)) + 1
+					),
+				',', ''),
+				REPLACE(SUBSTRING(
+					SUBSTRING_INDEX(tiki_tracker_fields.options, ',', 4),
+					LENGTH(SUBSTRING_INDEX(tiki_tracker_fields.options, ',', 4 -1)) + 1
+					),
+				',', ''),
+				tiki_tracker_fields.fieldId,
+				REPLACE(SUBSTRING(
+					SUBSTRING_INDEX(tiki_tracker_fields.options, ',', 5),
+					LENGTH(SUBSTRING_INDEX(tiki_tracker_fields.options, ',', 5 -1)) + 1
+					),
+				',', ''),
+				tiki_tracker_fields.type,
+				tiki_tracker_fields.options
+			FROM tiki_tracker_fields
+			WHERE tiki_tracker_fields.type = 'l';
+			
+			SET group_concat_max_len = 4294967295;
+		");
+	
+		foreach($tikilib->fetchAll("SELECT * FROM temp_tracker_field_options WHERE options LIKE '%|%'") as $row) {
+			$option = explode(",", $row["options"]);
+			$displayFieldIdsThere = explode("|", $option["3"]);
+			foreach($displayFieldIdsThere as $key => $displayFieldIdThere) {
+				if ($key > 0) {
+				$tikilib->query("
+						INSERT INTO temp_tracker_field_options
+						VALUES (?,?,?,?,?,?,?,?,?)
+					", array(
+						    $row["trackerIdHere"],
+						    $row["trackerIdThere"],
+						    $row["fieldIdThere"],
+						    $row["fieldIdHere"],
+						    $displayFieldIdThere,
+						    $row["displayFieldIdHere"],
+						    $row["linkToItems"],
+						    $row["type"],
+						    $row["options"]
+					));
+				}
+			}
+		}
+	}
+
+	function prepend_field_header(&$trackerPrimary = array(), $nameOrder = array()) {
+		global $tikilib;
+		//You will see a few 'LIMIT 1' here, this is just to help with memory, only 1 should be returned
+		$result = $tikilib->fetchAll("
+			SELECT fieldId, trackerId, name FROM tiki_tracker_fields
+		");
+		
+		$header = array();
+		
+		foreach($result as $row) {
+			$header[$row['fieldId']] = $row['name'];
+		}
+		
+		$joinedTrackerHeader = array();
+	
+		foreach($trackerPrimary as $item) {
+			foreach($item as $key => $field) {
+				$joinedTrackerHeader[$key] = $header[$key];
+			}
+		}
+		
+		if (!empty($nameOrder)) {
+			$sortedHeader = array();
+			$unsortedHeader = array();
+			foreach($nameOrder as $name) {
+				foreach($joinedTrackerHeader as $key => $field) {
+					if ($field == $name) {
+						$sortedHeader[$key] = $field;
+					} else {
+						$unsortedHeader[$key] = $field;
+					}
+				}
+			}
+			$joinedTrackerHeader = $sortedHeader + $unsortedHeader;
+		}
+		
+		$joinedTrackerHeader = array("HEADER" => $joinedTrackerHeader);
+		
+		return $joinedTrackerHeader + $trackerPrimary;
+	}
+
+	private function sort_direction($dir) {
+		switch( $dir ) {
+			case "asc":
+				$dir = SORT_ASC;
+				break;
+			case "desc":
+				$dir = SORT_DESC;
+				break;
+			case "regular":
+				$dir = SORT_REGULAR;
+				break;
+			case "numeric":
+				$dir = SORT_NUMERIC;
+				break;
+			case "string":
+				$dir = SORT_STRING;
+				break;
+			default:
+				$dir = SORT_ASC;
+		}
+		
+		return $dir;
+	}
+
+	function arfsort( &$array, $fieldList ){
+	    if (!is_array($fieldList)) {
+	    	$fieldList = explode('|', $fieldList);
+	        $fieldList = array(array($fieldList[0], $this->sort_direction($fieldList[1])));
+	    } else {
+	        for ($i = 0; $i < count($fieldList); ++$i) {
+	            $fieldList[$i] = explode('|', $fieldList[$i]);
+				$fieldList[$i] = array($fieldList[$i][0], $this->sort_direction($fieldList[$i][1]));
+	        }
+	    }
+	    
+	    $GLOBALS['__ARFSORT_LIST__'] = $fieldList;
+	    usort( $array, 'arfsort_func' );
+	}
+
+	function arfsort_func( $a, $b ){
+	    foreach( $GLOBALS['__ARFSORT_LIST__'] as $f ) {
+	    	switch($f[1]) {
+	    		case SORT_NUMERIC:
+	    			$strc = ((float)$b[$f[0]] > (float)$a[$f[0]] ? -1 : 1);
+	    			return $strc;
+	    			break;
+	    		default:
+	    			$strc = strcasecmp($b[$f[0]], $a[$f[0]]);
+			    	if ( $strc != 0 ){
+			            return $strc * (!empty($f[1]) && $f[1] == SORT_DESC ? 1 : -1);
+			        }
+	    	}
+	    }
+	    return 0;
+	}
+
+	function tracker_query($trackerId, $start, $end, $itemId, $equals, $search, $fields, $status = "opc", $sort, $limit, $offset, $delimiter = "[{|!|}]") {
+		global $tikilib;
+		$debug = false;
+		$params = array();
+		$fields_safe = "";
+		$status_safe = "";
+		$isSearch = false;
+		
+		$params[] = $trackerId;
+		
+		if ($start && !$search) $params[] = $start;
+		if ($end && !$search) $params[] = $end;
+		if ($itemId && !$search) $params[] = $itemId;
+		
+		if (count($fields) > 0 && (count($equals) > 0 || count($search) > 0)) {
+			for($i = 0; $i < count($fields); $i++) {
+				if (strlen($fields[$i]) > 0) {
+					$fields_safe .= " ( search_item_fields.fieldId = ? ";
+					$params[] = $fields[$i];
+					
+					if (strlen($equals[$i]) > 0) {
+						$fields_safe .= " AND search_item_fields.value = ? ";
+						$params[] = $equals[$i];
+					} elseif (strlen($search[$i]) > 0) {
+						$fields_safe .= " AND search_item_fields.value LIKE ? ";
+						$params[] = '%' . $equals[$i] . '%';
+					}
+					
+					$fields_safe .= " ) ";
+					
+					
+					if ($i + 1 < count($fields) && count($fields) > 1) $fields_safe .= " OR ";
+				}
+			}
+			
+			if (strlen($fields_safe) > 0) {
+				$fields_safe = " AND ( $fields_safe ) ";
+				$isSearch = true; 
+			}
+		}
+		
+		if (strlen($status) > 0) {
+			for($i=0; $i < strlen($status); $i++) {
+				if (strlen($status[$i]) > 0) {
+					$status_safe .= " tiki_tracker_items.status = ? ";
+					if ($i + 1 < strlen($status) && strlen($status) > 1) $status_safe .= " OR ";
+					$params[] = $status[$i];
+				}
+			}
+			
+			if (strlen($status_safe) > 0) {
+				$status_safe = " AND ( $status_safe ) ";
+			}
+		}
+		
+		if ( isset($limit) == true && is_numeric($limit) == false) {
+			unset($limit);
+		}
+		
+		if ( isset($offset) == true && is_numeric($offset) == false) {
+			unset($offset);
+		}
+		
+		$query = "
+			SELECT
+				tiki_tracker_items.status,
+				tiki_tracker_item_fields.itemId,
+				tiki_tracker_fields.trackerId,
+				GROUP_CONCAT(tiki_tracker_item_fields.fieldId 								SEPARATOR '$delimiter') AS item_fieldIds,
+				GROUP_CONCAT(IFNULL(items_right.value, tiki_tracker_item_fields.value) 	SEPARATOR '$delimiter') AS item_values
+						
+			FROM tiki_tracker_item_fields ".($isSearch == true ? " AS search_item_fields " : "")."
+			
+			".($isSearch == true ? "
+			LEFT JOIN tiki_tracker_item_fields ON 
+				search_item_fields.itemId = tiki_tracker_item_fields.itemId
+			" : "" )."
+			LEFT JOIN tiki_tracker_fields ON 
+				tiki_tracker_fields.fieldId = tiki_tracker_item_fields.fieldId
+			LEFT JOIN tiki_trackers ON 
+				tiki_trackers.trackerId = tiki_tracker_fields.trackerId
+			LEFT JOIN tiki_tracker_items ON tiki_tracker_items.itemId = tiki_tracker_item_fields.itemId
+			
+			
+			
+			LEFT JOIN temp_tracker_field_options items_left_display ON
+				items_left_display.displayFieldIdHere = tiki_tracker_item_fields.fieldId
+			
+			LEFT JOIN tiki_tracker_item_fields items_left ON (
+				items_left.fieldId = items_left_display.fieldIdHere AND
+				items_left.itemId = tiki_tracker_item_fields.itemId
+			)
+			
+			LEFT JOIN tiki_tracker_item_fields items_middle ON (
+				items_middle.value = items_left.value AND
+				items_left_display.fieldIdThere = items_middle.fieldId
+			)
+			
+			LEFT JOIN tiki_tracker_item_fields items_right ON (
+				items_right.itemId = items_middle.itemId AND
+				items_right.fieldId = items_left_display.displayFieldIdThere
+			)
+			 
+			
+			WHERE tiki_trackers.trackerId = ?
+			
+			".(isset($start) && !$search ? 								" AND tiki_tracker_items.lastModif > ? " : "")."
+			".(isset($end) && !$search ? 								" AND tiki_tracker_items.lastModif < ? " : "")."
+			".(isset($itemId) && !$search ? 							" AND tiki_tracker_item_fields.itemId = ? " : "")."
+			".(isset($fields_safe) ? $fields_safe : "")."
+			".(isset($status_safe) ? $status_safe : "")."
+			
+			GROUP BY
+				tiki_tracker_item_fields.itemId
+			ORDER BY 
+				tiki_tracker_items.lastModif
+			".(isset($limit) ? 
+				" LIMIT ".(isset($offset) ? "$offset, " : "")." $limit"
+				: ""
+			);
+		
+		if ($debug == true) {
+			$result = array($query, $params);
+			print_r( $result );
+			die;
+		} else {
+			$result = $tikilib->fetchAll($query, $params);
+		}
+		
+		$newResult = array();
+		foreach($result as $key => $row) {
+			$newRow = array();
+			$fieldIds = explode($delimiter, $row['item_fieldIds']); 
+			$itemValues = explode($delimiter, $row['item_values']);
+			
+			foreach($fieldIds as $key => $fieldId) {
+				if (isset($newRow[$fieldId])) {
+					if (is_array($newRow[$fieldId]) == false) {
+						$newRow[$fieldId] = array($newRow[$fieldId]);
+					}
+					
+					$newRow[$fieldId][] = $itemValues[$key];
+				} else {
+					$newRow[$fieldId] = $itemValues[$key];
+				}
+			}
+			
+			$newRow['status'.$trackerId] = $row['status']; 
+			$newRow['trackerId'] = $row['trackerId'];
+			$newRow['itemId'] = $row['itemId'];
+			$newResult[$row['itemId']] = $newRow;
+		}
+		unset($result);
+		return $newResult;
+	}
+
+	function filter_fields_from_tracker_query($tracker, $fieldIdsToRemove = array(), $fieldIdsToShow = array()) {
+		if (empty($fieldIdsToShow) == false) {
+			$newTracker = array();
+			foreach($tracker as $key => $item) {
+				$newTracker[$key] = array();
+				foreach($fieldIdsToShow as $fieldIdToShow) {
+					$newTracker[$key][$fieldIdToShow] = $tracker[$key][$fieldIdToShow];
+				}
+			}
+			
+			return $newTracker;
+		}
+	
+		if (empty($fieldIdsToRemove) == false) {
+			foreach($tracker as $key => $item) {
+				foreach($fieldIdsToRemove as $fieldIdToRemove) {
+					unset($tracker[$key][$fieldIdToRemove]);
+				}
+			}
+		}
+		
+		return $tracker;
+	}
+
+	function join_trackers($trackerLeft, $trackerRight, $fieldLeftId, $joinType) {
+		$joinedTracker = array();
+		switch ($joinType) {
+			case "outer":
+				foreach($trackerRight as $key => $itemRight) {
+					$match = false;
+					foreach($trackerLeft as $itemLeft) {
+						if ($key == $itemLeft[$fieldLeftId]) {
+							$match = true;
+							$joinedTracker[] = $itemLeft + $itemRight;
+						}
+					}
+					
+					if ($match == false) {
+						$joinedTracker[] = $itemRight;
+					}
+				}
+				break;
+			default:
+				foreach($trackerLeft as $itemLeft) {
+					if (isset($trackerRight[$itemLeft[$fieldLeftId]]) == true) {
+						$joinedTracker[] = $itemLeft + $trackerRight[$itemLeft[$fieldLeftId]];
+					}
+				}
+		}
+			
+		return $joinedTracker;
+	}
+
+
+	function to_csv($array, $header = false, $col_sep = ",", $row_sep = "\n", $qut = '"')
+	{
+		header("Content-type: application/csv");
+		header("Content-Disposition: attachment; filename=".$_REQUEST['csvFileName']);
+		header("Pragma: no-cache");
+		header("Expires: 0");
+		
+		if (!is_array($array) or !is_array($array[0])) return false;
+		
+		//Header row.
+		if ($header == true) {
+			foreach ($array[0] as $key => $val)
+			{
+				//Escaping quotes.
+				$key = str_replace($qut, "$qut$qut", $key);
+				$output .= "$col_sep$qut$key$qut";
+			}
+			$output = substr($output, 1)."\n";
+		}
+		
+		$cellKeys = array();
+		$cellKeysSet = false;
+		foreach ($array as $key => $val) {
+			$tmp = '';
+			
+			if ($cellKeysSet == false) {
+				foreach ($val as $cell_key => $cell_val) {
+					$cellKeys[] = $cell_key;
+				}
+				$cellKeysSet = true;
+			}
+			
+			
+			foreach ($cellKeys as $cellKey) {
+				//Escaping quotes.
+				if (is_array($val[$cellKey]) == true) $val[$cellKey] = implode(" ", $val[$cellKey]);
+				
+				$cell_val = str_replace("\n", " ", $val[$cellKey]);
+				$cell_val = str_replace($qut, "$qut$qut", $cell_val);
+				$tmp .= "$col_sep$qut$cell_val$qut";
+			}
+			
+			$output .= substr($tmp, 1).$row_sep;
+		}
+		
+		return $output;
+	}
+}
+
+global $trkqrylib;
+$trkqrylib = new TrackerQueryLib;
