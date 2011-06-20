d671b05f9a4a1423bc6d4678d0dcc55f0b75867f
chealer <chealer@users.sourceforge.net>
2011-06-20 17:55:01 +0000

    we apparently moved from CVS
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@34988 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/last_update.php
+++ lib/setup/last_update.php
@@ -8,7 +8,7 @@
 //this script may only be included - so its better to die if called directly.
 $access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__));
 
-/* trick for use with doc/devtools/cvsup.sh */
+/* trick for use with doc/devtools/svnup.sh */
 if ( is_file('.lastup') and is_readable('.lastup') ) {
 	$lastup = file('.lastup');
 	$smarty->assign('lastup', trim($lastup[0]));
