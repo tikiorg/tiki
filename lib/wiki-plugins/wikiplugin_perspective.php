3c8801e79b09b15aa933fa5e64098c77ae8e3191
xen216 <xen216@users.sourceforge.net>
2011-06-29 23:59:54 +0000

    [NEW] backport of perspective wiki plugin: displays content based on the user's current perspective.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/7.x@35134 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_perspective.php
@@ -0,0 +1,69 @@
+<?php
+// (c) Copyright 2002-2011 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+function wikiplugin_perspective_info() {
+	return array(
+		'name' => tra('Perspective'),
+		'documentation' => 'PluginPerspective',
+		'description' => tra('Display content based on the user\'s current perspective'),
+		'prefs' => array( 'feature_perspective', 'wikiplugin_perspective' ),
+		'body' => tra('Wiki text to display if conditions are met. The body may contain {ELSE}. Text after the marker will be displayed to users not matching the condition.'),
+		'filter' => 'wikicontent',
+		'params' => array(
+			'perspectives' => array(
+				'required' => false,
+				'name' => tra('Allowed Perspectives'),
+				'description' => tra('Pipe separated list of Ids for perspectives in which the block is shown. ex: 2|3|5'),
+				'filter' => 'text',
+				'default' => ''
+			),
+			'notperspectives' => array(
+				'required' => false,
+				'name' => tra('Denied Perspectives'),
+				'description' => tra('Pipe separated list of Ids for perspectives in which the block is not shown. ex: 3|5|8'),
+				'filter' => 'text',
+				'default' => ''
+			),
+		),
+	);
+}
+
+function wikiplugin_perspective($data, $params)
+{
+	global $prefs, $perspectivelib;
+
+	$dataelse = '';
+	if (strpos($data,'{ELSE}')) {
+		$dataelse = substr($data,strpos($data,'{ELSE}')+6);
+		$data = substr($data,0,strpos($data,'{ELSE}'));
+	}
+
+	if (!empty($params['perspectives'])) {
+		$perspectives = explode('|', $params['perspectives']);
+	}
+	if (!empty($params['notperspectives'])) {
+		$notperspectives = explode('|', $params['notperspectives']);
+	}
+	if (empty($perspectives) && empty($notperspectives)) {
+		return '';
+	}
+
+	require_once 'lib/perspectivelib.php';
+	$currentPerspective = $perspectivelib->get_current_perspective( $prefs );
+
+	// if the current perspective is not an allowed perspective, return the content after the "{ELSE}"
+	if (!empty($perspectives) && !in_array($currentPerspective, $perspectives)) {
+		return $dataelse;
+	}
+
+	// if the current perspective is a denied perspective, return the content after the "{ELSE}"
+	if (!empty($notperspectives) && in_array($currentPerspective, $notperspectives)) {
+		return $dataelse;
+	}
+
+	return $data;
+}
