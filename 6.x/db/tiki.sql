e42f7146f11042886960ea7f76ecd164b317c8f6
chealer <chealer@users.sourceforge.net>
2011-07-09 21:27:02 +0000

    [bp/r32346]MySQL 5.5: don't use removed TIMESTAMP(N), just TIMESTAMP. sorry, Aptana messing with indentation a bit
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@35293 b456876b-0849-0410-b77d-98878d47e9d5

--- 6.x/db/tiki.sql
+++ 6.x/db/tiki.sql
@@ -229,8 +229,8 @@ CREATE TABLE `tiki_banning` (
   `ip3` char(3) default NULL,
   `ip4` char(3) default NULL,
   `user` varchar(200) default '',
-  `date_from` timestamp(14) NOT NULL,
-  `date_to` timestamp(14) NOT NULL,
+  `date_from` timestamp NOT NULL,
+  `date_to` timestamp NOT NULL,
   `use_dates` char(1) default NULL,
   `created` int(14) default NULL,
   `message` text,
@@ -1658,7 +1658,7 @@ CREATE TABLE `tiki_polls` (
   `active` char(1) default NULL,
   `publishDate` int(14) default NULL,
   `voteConsiderationSpan` int(4) default 0,
-	`anonym` ENUM( 'a', 'u', 'i', 'c' ) NOT NULL DEFAULT 'u',
+    `anonym` ENUM( 'a', 'u', 'i', 'c' ) NOT NULL DEFAULT 'u',
   PRIMARY KEY (`pollId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 ;
 ALTER TABLE tiki_polls ADD INDEX tiki_poll_lookup ( active , title );
@@ -3080,7 +3080,7 @@ DROP TABLE IF EXISTS `tiki_friendship_requests`;
 CREATE TABLE `tiki_friendship_requests` (
   `userFrom` varchar(200) NOT NULL default '',
   `userTo` varchar(200) NOT NULL default '',
-  `tstamp` timestamp(14) NOT NULL,
+  `tstamp` timestamp NOT NULL,
   PRIMARY KEY (`userFrom`(120),`userTo`(120))
 ) ENGINE=MyISAM;
 
@@ -3128,7 +3128,7 @@ CREATE TABLE `tiki_users_score` (
   `user` char(200) NOT NULL default '',
   `event_id` char(200) NOT NULL default '',
   `expire` int(14) NOT NULL default '0',
-  `tstamp` timestamp(14) NOT NULL,
+  `tstamp` timestamp NOT NULL,
   PRIMARY KEY (`user`(110),`event_id`(110)),
   KEY `user` (user(110),event_id(110),expire)
 ) ENGINE=MyISAM;
