63aa3d9608526d54464f784bb35139a9bb6b6c03
jonnybradley <jonnybradley@users.sourceforge.net>
2011-07-14 09:22:31 +0000

    [FIX] wysiwyg: Restore tikiimage tool for wysiwyg (was a mistake to disable it i see now, far too useful!)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/7.x@35330 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/toolbars/toolbarslib.php
+++ lib/toolbars/toolbarslib.php
@@ -1261,7 +1261,7 @@ class ToolbarFileGallery extends Toolbar
 	{
 		$this->setLabel( tra('Choose or upload images') )
 			->setIcon( tra('pics/icons/pictures.png') )
-				->setWysiwygToken( '' )	/* disabling this by default for wysiwyg - make a custom tool with tikiimage as the wysiwyg token to override */
+				->setWysiwygToken( 'tikiimage' )
 					->setType('FileGallery')
 						->addRequiredPreference('feature_filegals_manager');
 	} // }}}
