401968431d077503200ed395bed8f8d09702e1a3
gta74 <gta74@users.sourceforge.net>
2011-07-16 14:14:15 +0000

    [TRA] adding missing strings/full tranlation
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@35424 b456876b-0849-0410-b77d-98878d47e9d5

--- lang/de/language.js
+++ lang/de/language.js
@@ -2,7 +2,7 @@
  * Javascript translation definition
  * Example to help translation of some English strings in some js files
  * 
- * $Id: language.js 30343 2010-10-27 12:50:51Z xavidp $
+ * $Id$
  */
 
 lang = {
@@ -17,12 +17,19 @@ lang = {
 "Valid User Name" : "gültiger Benutzername",
 "Passwords match" : "Paßwörter stimmen überein",
 "Valid Email" : "gültige Email",
-"Close" : "schließen",
-"Submit" : "übermitteln",
-"Insert" : "einfügen",
-"Replace" : "ersetzen",
-"Match" : "stimmen überein",
-"Do not match" : "stimmen nicht überein",
+"Close" : "Schließen",
+"Submit" : "Übermitteln",
+"Insert" : "Einfügen",
+"Replace" : "Ersetzen",
+"Match" : "Stimmen überein",
+"Do not match" : "Stimmen nicht überein",
 "Advanced options" : "Erweiterte Optionen",
 "Pick a file." : "Wähle eine Datei.",
+"URL to open this file with WebDAV" : "URL um diese Datei mit WebDAV zu öffnen",
+"To My Location" : "Zu meinem Ort",
+"What address are you looking for?" : "Nach welcher Adresse suchst Du?",
+"Search Location" : "Suche Ort",
+"Add Field" : "Füge Feld hinzu",
+"Edit Field" : "Bearbeite Feld",
+"Removing the field will result in data loss. Are you sure?" : "Wenn das Feld entfernt wird kommt es zu einem Datenverlust. Bist Du sicher?"
 };
