2d96ea5a97602e0216da841a69e04e3c0fc22f94
changi67 <changi67@users.sourceforge.net>
2011-07-16 10:18:28 +0000

    [FIX] Quick fix to avoid displaying &amp;nbsp; in title
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@35418 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/modifier.escape.php
+++ lib/smarty_tiki/modifier.escape.php
@@ -37,6 +37,8 @@ function smarty_modifier_escape($string, $esc_type = 'html', $char_set = 'UTF-8'
             $return = htmlspecialchars($string, ENT_QUOTES, $char_set);
 	    // Convert back sanitization tags into real tags to avoid them to be displayed
             $return = str_replace('&lt;x&gt;', '<x>', $return);
+            // Convert back sanitization tags into real tags for no wrap space
+            $return = str_replace('&amp;nbsp;', '&nbsp;', $return);
 	    return $return;
 
         case 'htmlall':
