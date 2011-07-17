076b6f929d921b49e0bb08b46728ff354139dc25
lindonb <lindonb@users.sourceforge.net>
2011-07-17 04:36:41 +0000

    [FIX] Use singular form for 1 day, hour, minute, or second
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@35434 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/modifier.duration_short.php
+++ lib/smarty_tiki/modifier.duration_short.php
@@ -20,18 +20,22 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
  * -------------------------------------------------------------
  */
 function smarty_modifier_duration_short($string) {
-  $result='';
-  if ($string > 60*60*24) {
-    $days = floor($string/(60*60*24));
-    $result = "$days ".tra('days');
-  } elseif ($string > 60*60) {
-    $hours = floor($string/(60*60));
-    $result = "$hours ".tra('hours');
-  } elseif ($string > 60) {
-    $mins = floor($string/(60));
-    $result = "$mins ".tra('minutes');
-  } else {
-	  $result = "$string ".tra('seconds');
-  }
-  return $result;
+	$result='';
+	if ($string > 60*60*24) {
+		$days = floor($string/(60*60*24));
+		$day_label = $days == 1 ? tra('day') : tra('days');
+		$result = "$days " . $day_label;
+	} elseif ($string > 60*60) {
+		$hours = floor($string/(60*60));
+		$hr_label = $hours == 1 ? tra('hour') : tra('hours');
+		$result = "$hours " . $hr_label;
+	} elseif ($string > 60) {
+		$mins = floor($string/(60));
+		$min_label = $mins == 1 ? tra('minute') : tra('minutes');
+		$result = "$mins " . $min_label;
+	} else {
+		$sec_label = $string == 1 ? tra('second') : tra('seconds');
+		$result = "$string " . $sec_label;
+	}
+	return $result;
 }
