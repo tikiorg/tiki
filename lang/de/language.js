9a6bb668c9e134e28841812f31ea0df18c72dd0c
gta74 <gta74@users.sourceforge.net>
2011-07-17 07:05:20 +0000

    [TRA] adding missing strings/full tranlation
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@35438 b456876b-0849-0410-b77d-98878d47e9d5

--- lang/de/language.js
+++ lang/de/language.js
@@ -22,7 +22,9 @@ lang = {
 "Insert" : "Einfügen",
 "Replace" : "Ersetzen",
 "Match" : "Stimmen überein",
+"Passwords match" : "Passwörter stimmen überein",
 "Do not match" : "Stimmen nicht überein",
+"Passwords don't match" : "Passwörter stimmen nicht überein",
 "Advanced options" : "Erweiterte Optionen",
 "Pick a file." : "Wähle eine Datei.",
 "URL to open this file with WebDAV" : "URL um diese Datei mit WebDAV zu öffnen",
@@ -31,5 +33,12 @@ lang = {
 "Search Location" : "Suche Ort",
 "Add Field" : "Füge Feld hinzu",
 "Edit Field" : "Bearbeite Feld",
-"Removing the field will result in data loss. Are you sure?" : "Wenn das Feld entfernt wird kommt es zu einem Datenverlust. Bist Du sicher?"
+"Removing the field will result in data loss. Are you sure?" : "Wenn das Feld entfernt wird kommt es zu einem Datenverlust. Bist Du sicher?",
+"You have unsaved changes to your modules, are you sure you want to leave the page without saving?" : "Du hast ungespeicherte Änderungen in den Modulen. Bist Du sicher, dass Du die Seite ohne Speichern verlassen willst?",
+"AJAX Error" : "AJAX Fehler",
+"It may not be possible to edit this page reliably in WYSIWYG mode in this browser." : "Es kann ggf. unmöglich sein, mit diesem Browser die Seite im WYSIWYG-Modus sicher zu bearbeiten",
+"The text you have selected is quite short. Select a longer piece to ensure the note is associated with the correct text." : "Der ausgewählte Text ist recht kurz. Wähle ein längeres Stück um sicherzustellen, dass der Hinweis mit dem richtigen Text verknüpft wird.",
+"Tip: Leave the first line as it is, starting with \";note:\". This is required" : "Tip: Lasse die erste Zeile, beginnend mit \";note:\" unverändert. Dies ist erforderlich",
+"Theme Generator:" : "Theme-Generator:",
+"Edit module:" : "Bearbeite Modul:",
 };
