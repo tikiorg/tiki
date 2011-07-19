fb5fa4078cea67aa9b46d06fa34a690195f10d57
jonnybradley <jonnybradley@users.sourceforge.net>
2011-07-19 08:50:29 +0000

    [FIX] userlist: Missing global $user
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@35475 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_userlist.php
+++ lib/wiki-plugins/wikiplugin_userlist.php
@@ -90,7 +90,7 @@ function wikiplugin_userlist_info() {
 }
 
 function wikiplugin_userlist($data, $params) {
-    global $tikilib, $userlib, $prefs, $tiki_p_admin, $tiki_p_admin_users;
+    global $tikilib, $userlib, $prefs, $tiki_p_admin, $tiki_p_admin_users, $user;
 
     extract ($params,EXTR_SKIP);
 
