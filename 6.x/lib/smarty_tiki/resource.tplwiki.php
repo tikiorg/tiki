5f1e4010912670bebae22ca7019561f6fbf793ea
xavidp <xavidp@users.sourceforge.net>
2011-07-25 08:31:39 +0000

    [bp/r35644][FIX]oups: Adding svn Id keyword
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@35645 b456876b-0849-0410-b77d-98878d47e9d5

--- 6.x/lib/smarty_tiki/resource.tplwiki.php
+++ 6.x/lib/smarty_tiki/resource.tplwiki.php
@@ -3,7 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: resource.wiki.php 26974 2010-05-07 18:35:19Z jonnybradley $
+// $Id$
 
 if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 	header("location: index.php");
