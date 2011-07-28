828f3875d498291abf84aa3561a87d267a30df74
changi67 <changi67@users.sourceforge.net>
2011-07-28 09:06:32 +0000

    Check script
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@35706 b456876b-0849-0410-b77d-98878d47e9d5

--- categorize.php
+++ categorize.php
@@ -5,13 +5,8 @@
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
 
-//this script may only be included - so its better to err & die if called directly.
-//smarty is not there - we need setup
-if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
-  header("location: index.php");
-  exit;
-}
-require_once('tiki-setup.php');  
+require_once('tiki-setup.php');
+$access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__));
 
 global $prefs;
 $catobjperms = Perms::get( array( 'type' => $cat_type, 'object' => $cat_objid ) );
