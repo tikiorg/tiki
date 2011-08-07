1af05fdb16d3cc13561be547d658209331845cc1
mlustenberg <mlustenberg@users.sourceforge.net>
2011-08-07 17:55:31 +0000

    [ADD] Tests for EditLib::parseToWysiwyg()
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@35886 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/test/editlib/ParseToWysiwyg_CharacterTest.php
@@ -0,0 +1,162 @@
+<?php
+// (c) Copyright 2002-2011 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: ParserTest.php 33195 2011-03-02 17:43:40Z changi67 $
+
+/**
+ * @group unit
+ *
+ */
+
+require_once 'lib/wiki/editlib.php';
+
+class EditLib_ParseToWysiwyg_CharacterTest extends TikiTestCase {
+	
+	private $dir = '';  // the unmodifed directory
+	
+	function __construct() {
+		$this->dir = getcwd();
+	}
+		
+	
+	function setUp() {
+		
+		$_SERVER['HTTP_HOST'] = ''; // editlib expects that HTTP_HOST is defined
+				
+		chdir($this->dir);
+		chdir('../../'); // the tiki installation directory
+	}
+	
+		
+	function tearDown() {
+		chdir($this->dir);
+	}	
+
+
+	function testFontFamily() {
+		$el = new Editlib();
+		
+		$inData = '{FONT(type="span", font-family="tahoma")}text{FONT}';
+		$exp = '<span style="font-family:tahoma;">text<span>';
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);
+	}
+
+	
+	function testFontSize() {
+		$el = new Editlib();
+		
+		$inData = '{FONT(type="span", font-size="12px")}text{FONT}';
+		$exp = '<span style="font-size:12px;">text<span>';
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);		
+	}
+	
+	
+	function testBold() {
+		$el = new Editlib();
+		
+		$inData = '__bold__';
+		$exp = '<strong>bold</strong>'; // like CKE
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);
+	}
+	
+	
+	function testItalic() {
+		$el = new EditLib();
+		
+		$inData = '\'\'italic\'\'';
+		$exp = '<em>italic</em>'; // like CKE
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);		
+	}
+	
+	
+	function testUnderlined() {
+		$el = new EditLib();
+		
+		$inData = '===underlined===';
+		$exp = '<u>underlined</u>'; // like CKE
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);
+	}
+	
+	
+	function testStrike() {
+		$el = new EditLib();
+		
+		$inData = '--strike through--';
+		$exp = '<strike>strike through</strike>'; // like CKE
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);		
+	}
+	
+	
+	function testSubscript() {
+		$el = new EditLib();
+		
+		$inData = '{SUB()}subscript{SUB}';
+		$exp = '<sub>subscript</sub>';
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);		
+	}	
+
+	
+	function testSuperscript() {
+		$el = new EditLib();
+		
+		$inData = '{SUP()}superscript{SUP}';
+		$exp = '<sup>superscript</sup>';
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);		
+	}		
+	
+	
+	function testMonospaced() {
+		$el = new EditLib();
+		
+		$inData = '<code>monospaced</code>';
+		$exp = '-+monospaced+-';
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);			
+	}
+
+	
+	function testTeletype() {
+		$el = new EditLib();
+		
+		$inData = '{DIV(type="tt")}teletype{DIV}';
+		$exp = '<tt>teletype</tt>';
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);			
+	}
+	
+	
+	function testColor() {
+		$el = new EditLib();
+		
+		$inData = '~~#112233:text~~';
+		$exp = '<span style="color:#112233;">text</span>';
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);			
+				
+		$inData = '~~ ,#112233:text~~';
+		$exp = '<span style="background-color:#112233;">text</span>';
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);			
+				
+		$inData = '~~#AABBCC,#112233:text~~';
+		$exp = '<span style="color:#AABBCC; background-color=#112233;">text</span>';
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);			
+				
+		$inData = '~~ #AABBCC , #112233 :text~~';
+		$exp = '<span style="color:#AABBCC; background-color=#112233;">text</span>';
+		$out = $el->parseToWysiwyg($inData, false);
+		$this->assertEquals($exp, $out);			
+	}
+	
+}
