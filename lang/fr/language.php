3015589adff7321935fd505f9631b65c9aee0ad4
jyhem <jyhem@users.sourceforge.net>
2011-08-08 17:27:48 +0000

    [FIX br/35917]i18n: use utf8 non-breaking space instead of html &nbsp;
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@35918 b456876b-0849-0410-b77d-98878d47e9d5

--- lang/fr/language.php
+++ lang/fr/language.php
@@ -64,8 +64,11 @@
 // ### If a string ending with colon needs translating (like "{tr}Login:{/tr}")
 // ### then Tiki tries to translate 'Login' and ':' separately.
 // ### This allows to have only one translation for "{tr}Login{/tr}" and "{tr}Login:{/tr}"
-// ### and it still allows to translate ":" as "&nbsp;:" for languages that
-// ### need it (like french)
+// ### and it still allows to translate ":" as " :" for languages that
+// ### need it (like French)
+// ### Note: the difference is invisible but " :" has an utf-8
+// ### non-breaking-space, not a regular space. This allows displaying correct emails
+// ### as well as web pages.
 
 $lang=Array(
 // ### Start of unused words
@@ -11831,8 +11834,7 @@ $lang=Array(
 "any language" => "n'importe quelle langue",
 "any category" => "toutes catégories",
 "Multiple select" => "Sélection multiple",
-"in:" => "dans :",
-":" => "&nbsp;:",
+":" => " :",
 "Number of displayed rows" => "Nombre de lignes affichées",
 "List View" => "Vue en liste",
 "Show php error messages" => "Afficher les messages d'erreurs PHP",
