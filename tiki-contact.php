fd82e7289b410c5d569cc13b12b8c144a1d6b407
chealer <chealer@users.sourceforge.net>
2011-08-11 05:58:59 +0000

    Contact Us: cleanup code duplication from anonymous vs non-anonymous and generally update to current standards
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@36045 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-contact.php
+++ tiki-contact.php
@@ -15,73 +15,55 @@ $access->check_feature(array('feature_contact', 'feature_messages'));
 
 $auto_query_args = array();
 
-$smarty->assign('mid', 'tiki-contact.tpl');
-
-if ($user == '') {
-	$access->check_feature('contact_anon');
-	
-	$smarty->assign('sent', 0);
-	if (isset($_REQUEST['send'])) {
-		check_ticket('contact');
-
-		$message = '';
-		// Validation:
-		// must have a subject or body non-empty (or both)
-		if (empty($_REQUEST['subject']) && empty($_REQUEST['body']) || empty($_REQUEST['from'])) {
-			$smarty->assign('message', tra('ERROR: you must include a subject or a message. You must also make sure to have a valid e-mail in the FROM field'));
-			$smarty->assign('priority', $_REQUEST['priority']);
-
-			if (!empty($_REQUEST['from'])) $smarty->assign_by_ref('from', $_REQUEST['from']);
-			if (!empty($_REQUEST['subject'])) $smarty->assign_by_ref('subject', $_REQUEST['subject']);
-			if (!empty($_REQUEST['body'])) $smarty->assign_by_ref('body', $_REQUEST['body']);
-			if (!empty($_REQUEST['priority'])) $smarty->assign_by_ref('priority', $_REQUEST['priority']);
-
-			$smarty->display("tiki.tpl");
-			die;
-		}
-		if ($prefs['feature_antibot'] == 'y') {
-			if(!$captchalib->validate()) {
-				$smarty->assign('message', $captchalib->getErrors());
-				$smarty->assign('errortype', 'no_redirect_login');
-				if (!empty($_REQUEST['from'])) $smarty->assign_by_ref('from', $_REQUEST['from']);
-				if (!empty($_REQUEST['subject'])) $smarty->assign_by_ref('subject', $_REQUEST['subject']);
-				if (!empty($_REQUEST['body'])) $smarty->assign_by_ref('body', $_REQUEST['body']);
-				if (!empty($_REQUEST['priority'])) $smarty->assign_by_ref('priority', $_REQUEST['priority']);
-				$smarty->display("tiki.tpl");
-				die;
-			}
-		}
-		$smarty->assign('sent', 1);
-		$body = tr("%0 sent you a message:", $_REQUEST['from']) . "\n" . $_REQUEST['body'];
-		$messulib->post_message($prefs['contact_user'], $_REQUEST['from'], $_REQUEST['to'],
-			'', $_REQUEST['subject'], $body, $_REQUEST['priority']);
-		$message = tra('Message sent to'). ': ' . $prefs['contact_user'] . '<br />';
-		$smarty->assign('message', $message);
-	}
-} else {
+if ($user) {
 	$access->check_permission('tiki_p_messages');
-	$smarty->assign('sent', 0);
+} else {
+	$access->check_feature('contact_anon');
+}
 
-	if (isset($_REQUEST['send'])) {
-		check_ticket('contact');
+$smarty->assign('sent', 0);
 
+$priority = 3;
+$from = $user ? $user : '';
+$subject = '';
+$body = '';
+if (isset($_REQUEST['send'])) {
+	if (isset($_REQUEST['priority'])) {
+		$priority = $_REQUEST['priority']; 
+	}
+	if (!$user && isset($_REQUEST['from'])) {
+		$from =  $_REQUEST['from'];
+	}
+	if (isset($_REQUEST['subject'])) {
+		$subject =  $_REQUEST['subject'];
+	}
+	if (isset($_REQUEST['body'])) {
+		$body =  $_REQUEST['body'];
+	}
+}
 
-		$message = '';
-
-		// Validation:
-		// must have a subject or body non-empty (or both)
-		if (empty($_REQUEST['subject']) && empty($_REQUEST['body'])) {
-			$smarty->assign('message', tra('ERROR: Either the subject or body must be non-empty'));
-
-			$smarty->display("tiki.tpl");
-			die;
+if (isset($_REQUEST['send'])) {
+	// Validation:
+	// must have a subject or body non-empty (or both)
+	$hasContent = !empty($_REQUEST['subject']) || !empty($_REQUEST['body']);
+	
+	$failsCaptcha = !$user && $prefs['feature_antibot'] == 'y' && !$captchalib->validate();
+	if (!$hasContent || empty($from) || $failsCaptcha) {
+		if (!$hasContent) {
+			$message = tra("You must include a subject or a message.");
+		} elseif (empty($from)) {
+			$message = tra("You must make sure to have a valid e-mail address in the From field.");
+		} else {
+			$message = $captchalib->getErrors();
 		}
-		$smarty->assign('sent', 1);
-		$message = tra('Message sent to'). ': ' . $prefs['contact_user'] . '<br />';
-		$body = tra("{$user} sent you a message:") . "\n" . $_REQUEST['body'];
-		$messulib->post_message($prefs['contact_user'], $user, $_REQUEST['to'],
+		$smarty->assign('errorMessage', $message);
+	} else {
+		$access->check_ticket();
+		$body = tr("%0 sent you a message:", $from) . "\n" . $_REQUEST['body'];
+		$messulib->post_message($prefs['contact_user'], $from, $_REQUEST['to'],
 			'', $_REQUEST['subject'], $body, $_REQUEST['priority']);
-
+		$message = tra('Message sent to'). ': ' . $prefs['contact_user'] . '<br />';
+		$smarty->assign('sent', 1);
 		$smarty->assign('message', $message);
 	}
 }
@@ -92,7 +74,10 @@ $smarty->assign('email0', $email);
 $email = scrambleEmail($email, $tikilib->get_user_preference('admin', "email is public"));
 $smarty->assign('email', $email);
 
-$smarty->assign('priority', 3);
-ask_ticket('contact');
+$smarty->assign('priority', $priority);
+$smarty->assign('from', $from);
+$smarty->assign('subject', $subject);
+$smarty->assign('body', $body);
 
+$smarty->assign('mid', 'tiki-contact.tpl');
 $smarty->display("tiki.tpl");
