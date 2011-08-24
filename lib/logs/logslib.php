fa646ac54958ec2de106a6b2632a35040e8af449
chealer <chealer@users.sourceforge.net>
2011-08-24 18:52:02 +0000

    add_action(): simplify conditions
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@36461 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/logs/logslib.php
+++ lib/logs/logslib.php
@@ -176,22 +176,16 @@ class LogsLib extends TikiLib
 		$actions = array();
 		if ( $logObject ) {
 			$param = substr( $param, 0, '200' );
-			if (!$logCateg) {
+			if ($logCateg && count($categs) > 0) {
+				foreach ($categs as $categ) {
+					$query = "insert into `tiki_actionlog` (`action`, `object`, `lastModif`, `user`, `ip`, `comment`, `objectType`, `categId`, `client`) values(?,?,?,?,?,?,?,?,?)";
+					$this->query($query, array($action, $object, (int)$date, $who, $ip, $param, $objectType, $categ, $client));
+					$actions[] = $this->lastInsertId();
+				}
+			} else {
 				$query = "insert into `tiki_actionlog` (`action`, `object`, `lastModif`, `user`, `ip`, `comment`, `objectType`, `client`) values(?,?,?,?,?,?,?,?)";
 				$this->query($query, array($action, $object, (int)$date, $who, $ip, $param, $objectType, $client));
 				$actions[] = $this->lastInsertId();
-			} else {
-				if (count($categs) > 0) {
-					foreach ($categs as $categ) {
-						$query = "insert into `tiki_actionlog` (`action`, `object`, `lastModif`, `user`, `ip`, `comment`, `objectType`, `categId`, `client`) values(?,?,?,?,?,?,?,?,?)";
-						$this->query($query, array($action, $object, (int)$date, $who, $ip, $param, $objectType, $categ, $client));
-						$actions[] = $this->lastInsertId();
-					}
-				} else {
-					$query = "insert into `tiki_actionlog` (`action`, `object`, `lastModif`, `user`, `ip`, `comment`, `objectType`, `client`) values(?,?,?,?,?,?,?,?)";
-					$this->query($query, array($action, $object, (int)$date, $who, $ip, $param, $objectType, $client));
-					$actions[] = $this->lastInsertId();
-				}
 			}
 		}
 		if (!empty($contributions)) {
