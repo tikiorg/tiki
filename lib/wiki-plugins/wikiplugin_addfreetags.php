81d27b728e59294f4751886a0ee773ef7606ed12
nkoth <nkoth@users.sourceforge.net>
2011-08-24 03:06:55 +0000

    [NEW] New way to add freetags to object in wiki page, useful to add tags to pretty tracker objects within trackerlist
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@36431 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_addfreetags.php
@@ -0,0 +1,77 @@
+<?php
+// (c) Copyright 2002-2011 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: wikiplugin_relations.php 36072 2011-08-11 15:31:53Z changi67 $
+
+function wikiplugin_addfreetags_info()
+{
+	return array(
+		'name' => tra('Add Freetags'),
+		'description' => tra('Add freetags to specified object'),
+		'format' => 'html',
+		'prefs' => array('feature_freetags', 'wikiplugin_addfreetags'),
+		'introduced' => 8,
+		'documentation' => 'PluginAddFreetags',
+		'params' => array(
+			'object' => array(
+				'required' => false,
+				'name' => tra('Object'),
+				'description' => tra('Object type and id, as in type:id, if unset, will use current object.'),
+				'filter' => 'text',
+				'default' => null,
+				'since' => '8.0',
+			),
+		),
+	);
+}
+
+function wikiplugin_addfreetags($data, $params)
+{
+	global $user;
+	$object = current_object();
+
+	if (isset($params['object']) && false !== strpos($params['object'], ':')) {
+		list($object['type'], $object['object']) = explode(':', $params['object'], 2);
+	}
+
+	$identifier = 'wp_addfreetag_' . str_replace(array(':',' '), array('_',''), $params['object']);
+
+	if (!empty($_POST[$identifier])) {
+		if ($object['type'] == 'trackeritem') {
+			$permobject = TikiLib::lib('trk')->get_tracker_for_item($object['object']); 
+			$permobjecttype = 'tracker';
+		} else {
+			$permobject = $object['object'];
+			$permobjecttype = $object['type']; 
+		}
+		if (TikiLib::lib('tiki')->user_has_perm_on_object($user, $permobject, $permobjecttype, 'tiki_p_view_freetags')) { 
+			TikiLib::lib('freetag')->tag_object( $user, $object['object'], $object['type'], $_POST[$identifier]);
+		} 
+		if ($object['type'] == 'trackeritem') {
+			// need to update tracker field as well
+			$definition = Tracker_Definition::get($permobject);
+			if ($field = $definition->getFreetagField()) {
+				$currenttags = TikiLib::lib('freetag')->get_tags_on_object( $object['object'], 'trackeritem' );
+				$taglist = '';	
+				foreach ($currenttags['data'] as $tag) {
+					if (strstr($tag['tag'], ' ')) {
+						$taglist .= '"'.$tag['tag'] . '" ';
+					} else {
+						$taglist .= $tag['tag'] . ' ';
+					}
+      				} 
+				TikiLib::lib('trk')->modify_field($object['object'], $field, $taglist);
+			}
+		} 
+		$url = $_SERVER['REQUEST_URI'];
+		header("Location: $url");
+		die;
+	}
+
+	$smarty = TikiLib::lib('smarty');
+	$smarty->assign('wp_addfreetag', $identifier); 
+	return $smarty->fetch('wiki-plugins/wikiplugin_addfreetags.tpl');
+}
+
