6a46e5054d8be38bb6401341a7b86726319eb200
chealer <chealer@users.sourceforge.net>
2011-08-24 01:58:50 +0000

    [FIX] upcoming_events(): calendar_description_is_html not honored (from r36401)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@36423 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/calendar/calendarlib.php
+++ lib/calendar/calendarlib.php
@@ -631,6 +631,7 @@ class CalendarLib extends TikiLib
 	// Returns an array of a maximum of $maxrows upcoming (but possibly past) events in the given $order. If $calendarId is set, events not in the specified calendars are filtered. $calendarId can be a calendar identifier or an array of calendar identifiers. If $maxDaysEnd is a natural, events ending after $maxDaysEnd days are filtered. If $maxDaysStart is a natural, events starting after $maxDaysStart days are filtered. Events ending more than $priorDays in the past are filtered.
 	// Each event is represented by a string-indexed array with indices start, end, name, description, calitemId, calendarId, user, lastModif, url, allday in the same format as tiki_calendar_items fields, as well as location for the event's locations, parsed for the parsed description and category for the event's calendar category.
 	function upcoming_events($maxrows = -1, $calendarId = null, $maxDaysEnd = -1, $order = 'start_asc', $priorDays = 0, $maxDaysStart = -1) {
+		global $prefs;
 		$cond = '';
 		$bindvars = array();
 		if (isset($calendarId)) {
