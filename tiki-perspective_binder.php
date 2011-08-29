ecfc0f07fbcbdb4f033bdddaaa3f53aff29542d6
changi67 <changi67@users.sourceforge.net>
2011-08-29 11:55:25 +0000

    Dos2unix + SVN Id
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@36591 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-perspective_binder.php
+++ tiki-perspective_binder.php
@@ -1,4 +1,10 @@
 <?php
+// (c) Copyright 2002-2011 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
 // For documentation how to use this file please see the comment at the end of this file
 $sagsSideCatId = 1; // ID of the parent category that indicates the current page is assigned to an "Area"
 global $prefs;
