ba705cb5780f3240b93f313b63f3c6c4982795d9
pkdille <pkdille@users.sourceforge.net>
2011-08-30 19:53:32 +0000

    Remove empty statements
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@36693 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/imagegals/imagegallib.php
+++ lib/imagegals/imagegallib.php
@@ -233,8 +233,7 @@ class ImageGalsLib extends TikiLib
 	{
 		$this->filesize = filesize($fname);
 
-		if ($this->uselib == "imagick") {
-		} else if ($this->uselib == "gd") {
+		if ($this->uselib == "gd") {
 			unset ($this->filetype);
 
 			$imageinfo = getimagesize($fname);
@@ -562,9 +561,7 @@ class ImageGalsLib extends TikiLib
 		$size = strlen($this->image);
 		$fhash = "";
 
-		if ($prefs['gal_use_db'] == 'y') {
-			// Prepare to store data in database
-		} else {
+		if ($prefs['gal_use_db'] != 'y') {
 			// Store data in directory
 			switch ($this->type) {
 			case 't':
@@ -829,9 +826,7 @@ class ImageGalsLib extends TikiLib
 		}
 		$path = '';
 
-		if ($prefs['gal_use_db'] == 'y') {
-			// Prepare to store data in database
-		} else {
+		if ($prefs['gal_use_db'] != 'y') {
 			// Store data in directory
 			$fhash = md5(uniqid($filename));
 			if (!($fw = fopen($prefs['gal_use_dir'] . $fhash, "wb"))) {
