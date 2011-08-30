a24e1e869551378eed0f6f59be2d449683cf50d0
robertplummer <robertplummer@users.sourceforge.net>
2011-08-30 18:19:47 +0000

    [FIX] Removed slideshow call from tiki-setup.php to make it less invasive [ENH] Backported slideshow fixes from 7
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/proposals@36676 b456876b-0849-0410-b77d-98878d47e9d5

--- 6.x/tiki-setup.php
+++ 6.x/tiki-setup.php
@@ -268,21 +268,6 @@ if ($prefs['javascript_enabled'] != 'n') {
 	if( $prefs['feature_jquery_media'] == 'y' ) {
 		$headerlib->add_jsfile( 'lib/jquery/jquery.media.js');
 	}
-	if( $prefs['feature_jquery_jqs5'] == 'y' ) {
-		if (strpos($_SERVER['PHP_SELF'], 'tiki-index_raw.php') !== false && isset($_REQUEST['format']) && $_REQUEST['format'] == 'jqs5') {
-			$headerlib->add_cssfile( 'lib/jquery/jquery.s5/jquery.s5.css' );
-			//$headerlib->add_cssfile( 'lib/jquery/jqs5/theme/staticfree/style.css' );
-			$headerlib->add_jsfile( 'lib/jquery/jquery.s5/jquery.s5.js' );
-			$headerlib->add_jq_onready( '$("h1,h2,h3,h5,h6").first().parent().tiki("s5", "", {});', 20 );	// late, and tell jqs5 where the page is in tiki
-			$prefs['feature_wiki_description'] = 'n';
-			$prefs['wiki_authors_style'] = 'none';
-			$prefs['feature_page_title'] = 'n';
-			$prefs['wiki_topline_position'] = 'none';
-			$prefs['page_bar_position'] = 'none';
-			$prefs['wiki_edit_section'] = 'n';
-			$prefs['wiki_edit_plugin'] = 'n';
-		}
-	}
 	if( $prefs['feature_jquery_tablesorter'] == 'y' ) {
 		$headerlib->add_cssfile( 'lib/jquery_tiki/tablesorter/themes/tiki/style.css' );
 		$headerlib->add_jsfile( 'lib/jquery/tablesorter/jquery.tablesorter.js' );
