1f63f0335e1c877a2b9d41b53b7640b934ac63dd
chealer <chealer@users.sourceforge.net>
2011-09-02 22:40:22 +0000

    [REF] to_id modifier: fix/update documentation and advise to avoid
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@36841 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/modifier.to_id.php
+++ lib/smarty_tiki/modifier.to_id.php
@@ -16,9 +16,10 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
  *
  * Type:     modifier
  * Name:     to_id
- * Purpose:  Convert any string to a string that can be used as an HTML id attribute
+ * Purpose:  Convert all whitespace from a string, turning any non-empty string to a string that can be used as an HTML id attribute.
+ * ids still need to be non-empty and unique. This modifier should be avoided, use OIDs instead.
  * @param string The string from which an id should be built
- * @return string A string to use as an id
+ * @return string A string to use as an id, encoded for an HTML element attribute context
  */
 function smarty_modifier_to_id($string)
 {
