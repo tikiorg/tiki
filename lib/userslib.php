60fa58c57edaa8a9781573c551f90eb3fdf6f3fe
nkoth <nkoth@users.sourceforge.net>
2011-09-03 01:42:51 +0000

    [MRG] Manual merge r36793 [FIX] Changing oneself default group not effecting due to cache
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@36846 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/userslib.php
+++ lib/userslib.php
@@ -2284,6 +2284,9 @@ class UsersLib extends TikiLib
 		$query = "update `users_users` set `default_group` = ?
 			where `login` = ?";
 		$this->query($query, array($group, $user));
+		if ($user == $_SESSION['u_info']['login']) {
+			$_SESSION['u_info']['group'] = $group;
+		}
 	}
 	function set_email_group($user, $email) {
 		$query = 'select `id`, `groupName`, `emailPattern` from `users_groups` where `emailPattern`!=?';
