ea82b969892e9156c1c3064bd587b7b52a1b4f2d
sylvieg <sylvieg@users.sourceforge.net>
2011-09-03 11:15:06 +0000

    [merge 36277][FIX]SUBSCRIBENEWSLETTER: since 34625: user and email are in lower case since around 34625
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@36856 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_subscribenewsletter.php
+++ lib/wiki-plugins/wikiplugin_subscribenewsletter.php
@@ -73,7 +73,7 @@ function wikiplugin_subscribenewsletter($data, $params) {
 	if ($user) {
 		$alls = $nllib->get_all_subscribers($nlId, false);
 		foreach ($alls as $all) {
-			if ($all['db_email'] == $user)
+			if (strtolower($all['db_email']) == strtolower($user))
 				return;
 		}
 	}
