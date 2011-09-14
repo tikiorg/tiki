abb5d48147f8b640a199300847d2d44b0acfe58b
arildb <arildb@users.sourceforge.net>
2011-09-14 21:13:44 +0000

    [FIX] The tiki installer doesn't like c-style comments. Removed commented sections, including the table tiki_user_bookmarks_folders which causes the error: "Incorrect table definition; there can be only one auto column and it must be defined as a key". Should be fixed.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@37265 b456876b-0849-0410-b77d-98878d47e9d5

--- innodb/db/tiki.sql
+++ innodb/db/tiki.sql
@@ -2309,19 +2309,6 @@ CREATE TABLE `tiki_user_assigned_modules` (
   PRIMARY KEY (`name`(30),`user`,`position`, `ord`)
 ) ENGINE=MyISAM;
 
-/*
-Incorrect table definition; there can be only one auto column and it must be defined as a key
-
-DROP TABLE IF EXISTS `tiki_user_bookmarks_folders`;
-CREATE TABLE `tiki_user_bookmarks_folders` (
-  `folderId` int(12) NOT NULL auto_increment,
-  `parentId` int(12) default NULL,
-  `user` varchar(200) NOT NULL default '',
-  `name` varchar(30) default NULL,
-  PRIMARY KEY (`user`,`folderId`)
-) ENGINE=MyISAM AUTO_INCREMENT=1 ;
-*/
-
 DROP TABLE IF EXISTS `tiki_user_bookmarks_urls`;
 CREATE TABLE `tiki_user_bookmarks_urls` (
   `urlId` int(12) NOT NULL auto_increment,
@@ -3673,20 +3660,3 @@ CREATE TABLE `tiki_connect` (
     PRIMARY KEY (`id`),
     KEY `server` (`server`)
 ) ENGINE=MyISAM;
-
-/* 
-MyISAM FULLTEXT indexing 
-CREATE FULLTEXT INDEX ft_articles ON tiki_articles(`title`, `heading`, `body`);
-CREATE FULLTEXT INDEX ft_blog_posts ON tiki_blog_posts(`data`, `title`);
-CREATE FULLTEXT INDEX ft_blogs ON tiki_blogs(`title`, `description`);
-CREATE FULLTEXT INDEX ft_calendar_items ON tiki_calendar_items(`name`,`description`);
-CREATE FULLTEXT INDEX ft_comments ON tiki_comments(title,data);
-CREATE FULLTEXT INDEX ftidx_directory_sites ON tiki_directory_sites(name,description);
-CREATE FULLTEXT INDEX ftidx_faq_questions ON tiki_faq_questions(question,answer);
-CREATE FULLTEXT INDEX ftidx_faqs ON tiki_faqs(title,description);
-CREATE FULLTEXT INDEX ftidx_files ON tiki_files(name,description,search_data,filename);
-CREATE FULLTEXT INDEX ftidx_galleries ON tiki_galleries(name,description);
-CREATE FULLTEXT INDEX ftidx_images ON tiki_images(name,description);
-CREATE FULLTEXT INDEX ftidx_pages ON tiki_pages(`pageName`,`description`,`data`);
-CREATE FULLTEXT INDEX ftidx_tracker_item_fields ON tiki_tracker_item_fields(value);
-*/
