4b65af1106736d242649a953257e5385f549b13e
jonnybradley <jonnybradley@users.sourceforge.net>
2011-09-16 21:20:22 +0000

    [SVN] missing prop
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@37349 b456876b-0849-0410-b77d-98878d47e9d5

--- doc/devtools/rebuild_search_index.php
+++ doc/devtools/rebuild_search_index.php
@@ -3,7 +3,7 @@
 //
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: release.php 36090 2011-08-11 20:31:15Z sept_7 $
+// $Id$
 //
 // from http://dev.tiki.org/Unified+Search#Cron_job
 
