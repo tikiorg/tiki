3e1903bb7959f4c2c919811389afc8068c153124
nkoth <nkoth@users.sourceforge.net>
2011-09-21 22:38:14 +0000

    [FIX] Expecting string so must be expecting text of Zend_Search_Lucene_Index_Term object, not object itself
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@37538 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Search/Index/Lucene.php
+++ lib/core/Search/Index/Lucene.php
@@ -248,7 +248,7 @@ class Search_Index_Lucene implements Search_Index_Interface
 			$term = $this->buildTerm($node);
 		}
 
-		if ($term && method_exists($term, 'getTerm') && (string) $term->getTerm()) {
+		if ($term && method_exists($term, 'getTerm') && (string) $term->getTerm()->text) {
 			$term->setBoost($node->getWeight());
 		}
 
