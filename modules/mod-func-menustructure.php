d71ab81171d781db6b50d1051ef2cfd9584163f0
chealer <chealer@users.sourceforge.net>
2011-09-21 14:19:17 +0000

    avoid referring to Tiki
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@37504 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-func-menustructure.php
+++ modules/mod-func-menustructure.php
@@ -13,7 +13,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 function module_menustructure_info() {
 	return array(
 		'name' => tra('Wiki Structure Menu'),
-		'description' => tra('Displays a structure.') . ' ' . tra('N.B. Deprecated in Tiki 8+, use Menu instead'),
+		'description' => tra('Displays a structure.') . ' ' . tra('N.B. Deprecated, use the Menu module instead'),
 		'prefs' => array( 'feature_wiki_structure' ),
 		'params' => array(
 			'structure' => array(
