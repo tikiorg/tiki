ab5f09d84d03f433b92040d5fa818451b2bfa71b
chealer <chealer@users.sourceforge.net>
2011-09-22 01:39:34 +0000

    Reintroduce support for switchLang URL parameter. switchLang can now be disabled in code. Note: Support was removed in r37478, which should effectively be reverted by this. Thanks Nelson Ko and Sylvie Greverend.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@37541 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/language.php
+++ lib/setup/language.php
@@ -10,7 +10,7 @@ $access->check_script($_SERVER["SCRIPT_NAME"],basename(__FILE__));
 
 // Indicates whether a locale identifier is valid
 // $localeIdentifier: A locale identifier, such as "en"
-// Returns a boolean, true if and only if the given locale identifier is valid. 
+// Returns a boolean, true if and only if the given locale identifier is valid and allowed. 
 function isValidLocale($localeIdentifier = '') {
 	global $prefs;
 	return preg_match("/[a-zA-Z-_]+$/", $localeIdentifier) && file_exists('lang/'. $localeIdentifier .'/language.php')
@@ -19,7 +19,7 @@ function isValidLocale($localeIdentifier = '') {
 
 // Sets the language
 // $localeIdentifier: the identifier of the locale to set
-// Returns true on success, false on failure (if $localeIdentifier is not a valid locale identifier) 
+// Returns true on success, false on failure (if $localeIdentifier is not a valid and allowed locale identifier) 
 function setLanguage($localeIdentifier = '') {
 	global $prefs, $tikilib, $user;
 	if (isValidLocale($localeIdentifier)) {
@@ -34,8 +34,13 @@ if ($prefs['feature_multilingual'] != 'y') { // change_language depends on featu
 }
 
 if ( $prefs['change_language'] == 'y') {
-	// Detect browser language
-	if ($prefs['feature_detect_language'] == 'y' and !$tikilib->userHasPreference('language')) {
+	// $noSwitchLang = true; // Uncomment to disabled switchLang
+	if (isset($_GET['switchLang']) && !isset($noSwitchLang)) {
+		// Special feature to allow creating Tiki links that also permanently switch the language of the user following the link.
+		 // Tiki does not create such links. See http://doc.tiki.org/i18n+Admin#Goodies 
+		setLanguage($_GET['switchLang']);
+	} elseif ($prefs['feature_detect_language'] == 'y' and !$tikilib->userHasPreference('language')) {
+		// Detect browser language
 		$browser_language = detect_browser_language();
 		if ( isValidLocale($browser_language) ) {
 			$prefs['language'] = $browser_language;
