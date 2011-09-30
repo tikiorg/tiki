c6c25271505f23a6adf171f20f05872825a9c603
sampaioprimo <sampaioprimo@users.sourceforge.net>
2011-09-30 17:13:37 +0000

    wraper file to make it possible to debug the test suite using Xdebug/Aptana
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@37821 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/test/phpunit.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+* A simple wrapper around /usr/bin/phpunit to enable debugging
+* tests with Xdebug from withing Aptana or Eclipse.
+* 
+* Linux only (it should be simple to add support to other OSs).
+*/
+
+require_once('/usr/bin/phpunit');
