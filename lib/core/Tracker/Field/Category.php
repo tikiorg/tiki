6f91a18944d8b51aa45079b5e6fdbae163212596
nkoth <nkoth@users.sourceforge.net>
2011-10-01 17:11:06 +0000

    [FIX] // only show existing category of not receiving request, otherwise might be uncategorization in progress which will not take effect
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@37846 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Tracker/Field/Category.php
+++ lib/core/Tracker/Field/Category.php
@@ -80,7 +80,8 @@ class Tracker_Field_Category extends Tracker_Field_Abstract implements Tracker_F
 
 		if (isset($requestData[$key]) && is_array($requestData[$key])) {
 			$selected = $requestData[$key];
-		} elseif ($this->getItemId()) {
+		} elseif ($this->getItemId() && empty($requestData)) {
+			// only show existing category of not receiving request, otherwise might be uncategorization in progress
 			$selected = $this->getCategories();
 		} else {
 			$selected = TikiLib::lib('categ')->get_default_categories();
