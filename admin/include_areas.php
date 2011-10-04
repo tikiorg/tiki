1b14ed2c5729d43ce3195cf24bb6a6ad0612e980
chealer <chealer@users.sourceforge.net>
2011-10-04 19:42:44 +0000

    [FIX] svn:keywords Id
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@37971 b456876b-0849-0410-b77d-98878d47e9d5

--- admin/include_areas.php
+++ admin/include_areas.php
@@ -3,7 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: include_webmail.php 35618 2011-07-23 13:59:19Z lphuberdeau $
+// $Id$
 
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
