6392a6ae38c568ff4e5a303f5f31c61105912c7c
chibaguy <chibaguy@users.sourceforge.net>
2011-10-11 13:30:46 +0000

    [FIX] Enable gauge background color even when theme specifies a wikitext td background color (also some table attributes not supprted in HTML5 removed). (Xavi reported the problem with Mollio theme.)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@38151 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_gauge.php
+++ lib/wiki-plugins/wikiplugin_gauge.php
@@ -163,19 +163,19 @@ function wikiplugin_gauge($data, $params) {
     $h_size_rest = 100-$h_size;
 
     if ($h_size == 100) {
-        $h_td = '<td style="background-color:' . $color . ';">&nbsp;</td>';
+        $h_td = '<td style="background:' . $color . ';">&nbsp;</td>';
         } else {
             if ($h_size_rest == 100) {
-                $h_td = '<td style="background-color:' . $bgcolor . ';">&nbsp;</td>';
+                $h_td = '<td style="background:' . $bgcolor . ';">&nbsp;</td>';
             } else {
-                $h_td = '<td style="background-color:' . $color . ';" width="' . $h_size . '%' .'">&nbsp;</td>';
-                $h_td .= '<td style="background-color:' . $bgcolor . ';" width="' . $h_size_rest .  '%' . '">&nbsp;</td>';
+                $h_td = '<td style="background:' . $color . ';" width="' . $h_size . '%' .'">&nbsp;</td>';
+                $h_td .= '<td style="background:' . $bgcolor . ';" width="' . $h_size_rest .  '%' . '">&nbsp;</td>';
             }
         }
 
 
-	$html  ='<table border="0" cellpadding="0" cellspacing="0"><tr>' . $label_td . '<td width="' . $size . '" height="' . $height . '">';
-	$html .='<table border="0" cellpadding="0" cellspacing="0" width="100%"><tr>' . $h_td . '</tr></table>';
+	$html  ='<table class="plugin_gauge" border="0" width="100%"><tr>' . $label_td . '<td width="' . $size . '" height="' . $height . '">';
+	$html .='<table class="plugin_gauge-bar" border="0" width="100%"><tr>' . $h_td . '</tr></table>';
 	$html .='</td>' . ($showvalue ? $perc_td : '') . '<td>&nbsp;</td></tr>';
 
 	if (!empty($data)) {
