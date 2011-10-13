104ff12cc178ead73ad5c232c2537a48274ef7c0
robertplummer <robertplummer@users.sourceforge.net>
2011-10-13 14:10:22 +0000

    [ENH] Made spreadsheet update when things are initiated in jtrack
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@38230 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-timesheet.php
+++ tiki-timesheet.php
@@ -14,30 +14,40 @@ $headerlib->add_jsfile("lib/jquery/jtrack/js/jtrack.js");
 $headerlib->add_jq_onready("
 	jTask.init();
 	
-	var table = $('<table title=/>').attr('title', tr('Overview'));
-	table.append('<tr><td>Task</td><td>Estimate</td><td>Time Spent</td></tr>');
-	
-	var rowI = 1;
-	for (var item in $.DOMCached.storage) {
-		var row = $('<tr />').appendTo(table);
+	$.timesheetSpreadsheet = function() {
+		var table = $('<table title=/>').attr('title', tr('Overview'));
+		table.append('<tr><td>Task</td><td>Estimate</td><td>Time Spent (seconds)</td></tr>');
 		
-		row.append('<td>' + item + '</td>');
-		row.append('<td>' + $.DOMCached.storage[item].estimate.value + '</td>');
-		row.append('<td>' + $.DOMCached.storage[item].timer.value + '</td>');
-		rowI++;
-	}
-	var row = $('<tr />').appendTo(table);		
-	row.append('<td>Totals</td>');
-	row.append($('<td></td>').attr('formula', '=(SUM(B2:B' + rowI +  '))'));
-	row.append($('<td></td>').attr('formula', '=(SUM(C2:C' + rowI +  ') / 60) + \'Minutes\''));
+		var rowI = 1;
+		for (var item in $.DOMCached.storage) {
+			var row = $('<tr />').appendTo(table);
+			
+			row.append('<td>' + item + '</td>');
+			row.append('<td>' + $.DOMCached.storage[item].estimate.value + '</td>');
+			row.append('<td>' + $.DOMCached.storage[item].timer.value + '</td>');
+			rowI++;
+		}
+		var row = $('<tr />').appendTo(table);		
+		row.append('<td>Totals</td>');
+		row.append($('<td></td>').attr('formula', '=(SUM(B2:B' + rowI +  '))'));
+		row.append($('<td></td>').attr('formula', '=(SUM(C2:C' + rowI +  ') / 60) + \'Minutes\''));
+		
+		$('#timesheetSpreadsheet').siblings().remove();
+		
+		$('#timesheetSpreadsheet')
+			.html(table)
+			.sheet({
+				buildSheet: true,
+				editable: false,
+				height: $('#jtrack-holder').height()
+			});
+	};
 	
-	$('#timesheetSpreadsheet')
-		.html(table)
-		.sheet({
-			buildSheet: true,
-			editable: false,
-			height: $('#jtrack-holder').height()
-		});
+	$('.jtrack-create,.jtrack-update,.jtrack-remove,.jtrack-remove-all,.jtrack-cancel,.jtrack-power,#jtrack-button-remove,#jtrack-button-remove-all,#jtrack-button-create,#jtrack-button-update').live('click', function() {
+		$.timesheetSpreadsheet();
+	});
+	
+	$.timesheetSpreadsheet();
 ");
 $smarty->assign('mid', 'tiki-timesheet.tpl');
 // use tiki_full to include include CSS and JavaScript
