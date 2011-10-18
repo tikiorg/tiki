c8743e26f90ef00f7084b65657b57b88a102d82e
robertplummer <robertplummer@users.sourceforge.net>
2011-10-18 15:41:39 +0000

    [ENH] Made slider titles parse for wiki syntax
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@38363 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_slider.php
+++ lib/wiki-plugins/wikiplugin_slider.php
@@ -394,6 +394,7 @@ function wikiplugin_slider($data, $params) {
 	
 	$titles = array();
 	if (!empty($titles)) {
+		$titles = $tikilib->parse_data($titles, array('suppress_icons' => true));
 		$titles = explode('|', $titles);
 	}
 	
