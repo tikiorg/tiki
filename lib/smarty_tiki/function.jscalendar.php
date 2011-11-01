00aaf1651cce3c6ce3cb49ff9618fc423da5d424
pkdille <pkdille@users.sourceforge.net>
2011-11-01 11:11:02 +0000

    No changes - code styling only - split long lines (up to 658 chars)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@38606 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.jscalendar.php
+++ lib/smarty_tiki/function.jscalendar.php
@@ -46,16 +46,61 @@ function smarty_function_jscalendar($params, $smarty)
 			}
 			
 			$datepicker_options_common .= ', firstDay: '.$first;
-			$datepicker_options_common .= ", closeText: '".smarty_function_jscalendar_tra('Done')."'";
-			$datepicker_options_common .= ", prevText: '".smarty_function_jscalendar_tra('Prev')."'";
-			$datepicker_options_common .= ", nextText: '".smarty_function_jscalendar_tra('Next')."'";
-			$datepicker_options_common .= ", currentText: '".smarty_function_jscalendar_tra('Today')."'";
-			$datepicker_options_common .= ", weekHeader: '".smarty_function_jscalendar_tra('Wk')."'";
-			$datepicker_options_common .= ", dayNames: ['".smarty_function_jscalendar_tra('Sunday')."','".smarty_function_jscalendar_tra('Monday')."','".smarty_function_jscalendar_tra('Tuesday')."','".smarty_function_jscalendar_tra('Wednesday')."','".smarty_function_jscalendar_tra('Thursday')."','".smarty_function_jscalendar_tra('Friday')."','".smarty_function_jscalendar_tra('Saturday')."']";
-			$datepicker_options_common .= ", dayNamesMin: ['".smarty_function_jscalendar_tra('Su')."','".smarty_function_jscalendar_tra('Mo')."','".smarty_function_jscalendar_tra('Tu')."','".smarty_function_jscalendar_tra('We')."','".smarty_function_jscalendar_tra('Th')."','".smarty_function_jscalendar_tra('Fr')."','".smarty_function_jscalendar_tra('Sa')."']";
-			$datepicker_options_common .= ", dayNamesShort: ['".smarty_function_jscalendar_tra('Sun')."','".smarty_function_jscalendar_tra('Mon')."','".smarty_function_jscalendar_tra('Tue')."','".smarty_function_jscalendar_tra('Wed')."','".smarty_function_jscalendar_tra('Thu')."','".smarty_function_jscalendar_tra('Fri')."','".smarty_function_jscalendar_tra('Sat')."']";
-			$datepicker_options_common .= ", monthNames: ['".smarty_function_jscalendar_tra('January')."','".smarty_function_jscalendar_tra('February')."','".smarty_function_jscalendar_tra('March')."','".smarty_function_jscalendar_tra('April')."','".smarty_function_jscalendar_tra('May')."','".smarty_function_jscalendar_tra('June')."','".smarty_function_jscalendar_tra('July')."','".smarty_function_jscalendar_tra('August')."','".smarty_function_jscalendar_tra('September')."','".smarty_function_jscalendar_tra('October')."','".smarty_function_jscalendar_tra('November')."','".smarty_function_jscalendar_tra('December')."']";
-			$datepicker_options_common .= ", monthNamesShort: ['".smarty_function_jscalendar_tra('Jan')."','".smarty_function_jscalendar_tra('Feb')."','".smarty_function_jscalendar_tra('Mar')."','".smarty_function_jscalendar_tra('Apr')."','".smarty_function_jscalendar_tra('May')."','".smarty_function_jscalendar_tra('Jun')."','".smarty_function_jscalendar_tra('Jul')."','".smarty_function_jscalendar_tra('Aug')."','".smarty_function_jscalendar_tra('Sep')."','".smarty_function_jscalendar_tra('Oct')."','".smarty_function_jscalendar_tra('Nov')."','".smarty_function_jscalendar_tra('Dec')."']";
+			$datepicker_options_common .= ", closeText: '" . smarty_function_jscalendar_tra('Done') . "'";
+			$datepicker_options_common .= ", prevText: '" . smarty_function_jscalendar_tra('Prev') . "'";
+			$datepicker_options_common .= ", nextText: '" . smarty_function_jscalendar_tra('Next') . "'";
+			$datepicker_options_common .= ", currentText: '" . smarty_function_jscalendar_tra('Today') . "'";
+			$datepicker_options_common .= ", weekHeader: '" . smarty_function_jscalendar_tra('Wk') . "'";
+
+			$datepicker_options_common .= ", dayNames: ['" . smarty_function_jscalendar_tra('Sunday') . "','" .
+																											 smarty_function_jscalendar_tra('Monday') . "','" .
+																											 smarty_function_jscalendar_tra('Tuesday') . "','" .
+																											 smarty_function_jscalendar_tra('Wednesday') . "','" .
+																											 smarty_function_jscalendar_tra('Thursday') . "','" .
+																											 smarty_function_jscalendar_tra('Friday') . "','" .
+																											 smarty_function_jscalendar_tra('Saturday') . "']";
+
+			$datepicker_options_common .= ", dayNamesMin: ['" . smarty_function_jscalendar_tra('Su') . "','" .
+																													smarty_function_jscalendar_tra('Mo') . "','" .
+																													smarty_function_jscalendar_tra('Tu') . "','" .
+																													smarty_function_jscalendar_tra('We') . "','" .
+																													smarty_function_jscalendar_tra('Th') . "','" .
+																													smarty_function_jscalendar_tra('Fr') . "','" .
+																													smarty_function_jscalendar_tra('Sa') . "']";
+
+			$datepicker_options_common .= ", dayNamesShort: ['" . smarty_function_jscalendar_tra('Sun') . "','" .
+																														smarty_function_jscalendar_tra('Mon') . "','" .
+																														smarty_function_jscalendar_tra('Tue') . "','" .
+																														smarty_function_jscalendar_tra('Wed') . "','" .
+																														smarty_function_jscalendar_tra('Thu') . "','" .
+																														smarty_function_jscalendar_tra('Fri') . "','" .
+																														smarty_function_jscalendar_tra('Sat') . "']";
+
+			$datepicker_options_common .= ", monthNames: ['" . smarty_function_jscalendar_tra('January') . "','" .
+																												 smarty_function_jscalendar_tra('February') . "','" .
+																												 smarty_function_jscalendar_tra('March') . "','" .
+																												 smarty_function_jscalendar_tra('April') . "','" .
+																												 smarty_function_jscalendar_tra('May') . "','" .
+																												 smarty_function_jscalendar_tra('June') . "','" .
+																												 smarty_function_jscalendar_tra('July') . "','" .
+																												 smarty_function_jscalendar_tra('August') . "','" .
+																												 smarty_function_jscalendar_tra('September') . "','" .
+																												 smarty_function_jscalendar_tra('October') . "','" .
+																												 smarty_function_jscalendar_tra('November') . "','" .
+																												 smarty_function_jscalendar_tra('December')."']";
+
+			$datepicker_options_common .= ", monthNamesShort: ['" . smarty_function_jscalendar_tra('Jan') . "','" .
+																															smarty_function_jscalendar_tra('Feb') . "','" .
+																															smarty_function_jscalendar_tra('Mar') . "','" .
+																															smarty_function_jscalendar_tra('Apr') . "','" .
+																															smarty_function_jscalendar_tra('May') . "','" .
+																															smarty_function_jscalendar_tra('Jun') . "','" .
+																															smarty_function_jscalendar_tra('Jul') . "','" .
+																															smarty_function_jscalendar_tra('Aug') . "','" .
+																															smarty_function_jscalendar_tra('Sep') . "','" .
+																															smarty_function_jscalendar_tra('Oct') . "','" .
+																															smarty_function_jscalendar_tra('Nov') . "','" .
+																															smarty_function_jscalendar_tra('Dec')."']";
 			$datepicker_options_common .= '}';
 		}
 
