4804e31823f646e5b169442b135f599e5d4be637
changi67 <changi67@users.sourceforge.net>
2011-11-03 19:29:44 +0000

    [REL] Preparing 7.3
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/7.x@38662 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -26,7 +26,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '7.3 (SVN)';	// needs to have no spaces for releases
+		$this->version 	= '7.3';	// needs to have no spaces for releases
 		$this->star	= '';
 		$this->releases	= array();
 
@@ -148,6 +148,7 @@ class TWVersion
 				'7.1RC2',
 				'7.1',
 				'7.2',
+				'7.3',
 			);
 	}
 
