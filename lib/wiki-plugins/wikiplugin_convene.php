e5655f6d0a40736c4a906628b880c124e2e3651c
robertplummer <robertplummer@users.sourceforge.net>
2011-11-03 15:01:46 +0000

    [ENH] Removal of existing date & time [ENH] Adding of new date & time (thanks to jquery datetimepicker) [ENH] Added auto sorting of dates [ENH] Cleaned up plugin layout [ENH] Migrated to jQuery UI styling for approved dates (ui highlight) and non approved (ui error)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@38653 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_convene.php
+++ lib/wiki-plugins/wikiplugin_convene.php
@@ -32,8 +32,8 @@ function wikiplugin_convene_info() {
 function wikiplugin_convene($data, $params) {
 	global $tikilib, $headerlib, $page, $caching;
 	static $htmlFeedLinkI = 0;
-	++$htmlFeedLinkI;
-	$i = $htmlFeedLinkI;
+	++$conveneI;
+	$i = $conveneI;
 	
 	$params = array_merge(array(
 		"type" => "replace",
@@ -42,7 +42,11 @@ function wikiplugin_convene($data, $params) {
 	extract ($params,EXTR_SKIP);
 	$dates = array();
 	
-	foreach(explode("\n", trim($data)) as $line) {
+	
+	//start flat static text to prepared array
+	$lines = explode("\n", trim($data));
+	sort($lines);
+	foreach($lines as $line) {
 		$line = trim($line);
 		
 		$parts = explode(':', $line);
@@ -50,24 +54,36 @@ function wikiplugin_convene($data, $params) {
 	}
 	
 	$dates = TikiFilter_PrepareInput::delimiter('_')->prepare($dates);
+	//end flat static text to prepared array
 	
+	
+	//start get users from array
 	$users = array();
 	foreach(end($dates['dates']) as $user => $vote) {
 		$users[] = $user;
 	}
+	//end get users from array
 	
+	
+	//start votes summed together
 	$votes = array();
 	foreach($dates['dates'] as $stamp => $date) {
 		foreach($date as $vote) {
 			$votes[$stamp] += $vote;
 		}
 	}
+	//end votes summed together
 	
+	
+	//start find top vote stamp
 	$topVoteStamp = 0;
 	foreach($votes as $stamp => $vote) {
 		$topVoteStamp = ($vote > $votes[$topVoteStamp] ? $stamp : $topVoteStamp);
 	}
-
+	//end find top vote stamp
+	
+	
+	//start reverse array for easy listing as table
 	$rows = array();
 	foreach($dates['dates'] as $stamp => $date) {
 		foreach($date as $user => $vote) {
@@ -76,38 +92,171 @@ function wikiplugin_convene($data, $params) {
 			$rows[$user][$stamp] = $vote;
 		}
 	}
+	//end reverse array for easy listing as table
 	
-	$result = "~np~<table>";
+	$result = "";
 	
-	$result .= "<tr><td />";
-	
+	//start date header
+	$dateHeader = "";
 	foreach($votes as $stamp => $totals) {
-		$result .= "<td>". $tikilib->get_short_datetime($stamp) ."</td>";
+		$dateHeader .= "<td>". $tikilib->get_short_datetime($stamp) ." <img src='pics/icons/delete.png' class='conveneDeleteDate$i' data-date='$stamp' /></td>";
 	}
+	$result .= <<<JQ
+		<tr>
+			<td />
+			$dateHeader
+			<td style='vertical-align: middle;'><img src='pics/icons/add.png' id='conveneAddDate$i' /></td>
+		</tr>
+JQ;
+	//end date header
 	
-	$result .= "</tr>";
 	
+	//start user list and votes 
+	$userList = "";
 	foreach($rows as $user => $row) {
-		$result .= "<tr>";
-		$result .= "<td>" . $user . "</td>";
+		$userList .= "<tr class='conveneUserList$i'>";
+		$userList .= "<td>" . $user . "</td>";
 		foreach($row as $stamp => $vote) {
-			$style = 	($vote == 1 ? 'color: green; background-color: #99FF66;' : 'color: red; background-color: #FFCCCC;');
+			$class = 	($vote == 1 ? 'ui-state-highlight' : 'ui-state-error');
 			$text = 	($vote  == 1 ? tr("OK") : "" );
 			
-			$result .= "<td style='$style'>". ($vote  == 1 ? tr("OK") : "" ) ."</td>";
+			$userList .= "<td class='$class'>". ($vote  == 1 ? tr("OK") : "" )
+				."<input type='hidden' name='dates_" . $stamp . "_" . $user . "' value='$vote' />"
+				."</td>";
 		}
-		$result .= "</tr>";
+		$userList .= "</tr>";
 	}
+	$result .= <<<JQ
+		$userList
+JQ;
+	//end user list and votes
 	
-	$result .= "<tr><td />";
-
-	foreach($votes as $total) {
-		$pic = ($total == $votes[$topVoteStamp] ? '<img src="pics/icons/tick.png" /><img src="pics/icons/calendar_add.png" />' : '');
-		$result .= "<td>". $total ."&nbsp;$pic</td>";
+	
+	//start add new user and votes
+	$newVotes .= "";
+	foreach(end($rows) as $stamp => $vote) {
+		$newVotes .= "<td><input type='checkbox' name='dates_" . $stamp . "_new' /></td>";
 	}
-	$result .= "</tr>";
+	$result .= <<<JQ
+		<tr>
+			<td><input name='user' type='text' style='width: 50px;' /></td>
+			$newVotes
+			<td><img src='pics/icons/accept.png' /></td>
+		</tr>
+JQ;
+	//end add new user and votes
 	
-	$result .= "</table>~/np~";
+	
+	//start last row with auto selected date(s)
+	$lastRow = "";
+	foreach($votes as $stamp => $total) {
+		$pic = ($total == $votes[$topVoteStamp] ? "<a href='tiki-calendar_edit_item.php?todate=$stamp&calendarId=1'><img src='pics/icons/tick.png' /><img src='pics/icons/calendar_add.png' /></a>" : '');
+		$lastRow .= "<td>". $total ."&nbsp;$pic</td>";
+	}
+	$result .= <<<JQ
+		<tr id='pluginConveneLastRow$i'>
+			<td />
+			$lastRow
+		</tr>
+JQ;
+	//end last row with auto selected date(s)
+	
+	
+	$result = <<<JQ
+		~np~
+			<form id='pluginConvene$i'>
+				<table cellpadding="0" cellspacing="0" border="0"> $result </table>
+			</form>
+		~/np~
+JQ;
+	;
+	
+	$conveneData = json_encode(array(
+		"dates" => $dates,
+		"users" => $users,
+		"votes" => $votes,
+		"topVote" => $votes[$topVoteStamp],
+		"rows" =>	$rows,
+		"data" => $data,
+	));
+	
+	$n = '\n\r';
+	
+	$headerlib->add_jsfile("lib/jquery/jquery-ui-timepicker-addon.js");
+	$headerlib->add_jq_onready(<<<JQ
+		var convene$i = $conveneData;
+		
+		convene$i.addDate = function(date) {
+			if (!date) return;
+			date = unixDate(date);
+			var addedData = '';
+			
+			for(user in convene$i.users) {
+				addedData += 'dates_' + date + '_' + convene$i.users[user] + ' : 0$n';
+			}
+			
+			convene$i.data = (convene$i.data + '$n' + addedData).split(/$n/).sort().join('$n');
+			
+			$.modal(tr("Loading..."));
+			$(document)
+				.unbind('plugin_convene_ready')
+				.one('plugin_convene_ready', function(e) {
+					var content = $(e.container).find('[name="content"]');
+					content.val(convene$i.data);
+					e.btns.Submit();
+				});
+			
+			$('#plugin-edit-convene1').click();
+		};
+		
+		convene$i.deleteDate = function(date) {
+			if (!date) return;
+			var addedData = '';
+			
+			for(user in convene$i.users) {
+				addedData += 'dates_' + date + '_' + convene$i.users[user] + ' : 0$n';
+			}
+			
+			var lines = convene$i.data.split(/$n/);
+			var newData = [];
+			for(line in lines) {
+				if (!(lines[line] + '' ).match(date)) {
+					 newData.push(lines[line]);
+				}
+			}
+			
+			convene$i.data = newData.join('$n');
+			
+			$.modal(tr("Loading..."));
+			$(document)
+				.unbind('plugin_convene_ready')
+				.one('plugin_convene_ready', function(e) {
+					var content = $(e.container).find('[name="content"]');
+					content.val(convene$i.data);
+					e.btns.Submit();
+				});
+			
+			$('#plugin-edit-convene1').click();
+		};
+			
+		$('#conveneAddDate$i')
+			.click(function() {
+				var picker = $('<input name="date" style="width: 50px;" />')
+					.datetimepicker({
+						onClose: function() {
+							convene$i.addDate($(this).val());
+						}
+					});
+					
+				$(this).replaceWith(picker);
+			});
+			
+		$('.conveneDeleteDate$i')
+			.click(function() {
+				convene$i.deleteDate($(this).data("date"));
+			});
+JQ
+);
 	
 	return $result;
 }
