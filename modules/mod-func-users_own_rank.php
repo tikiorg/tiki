1c25843a4d8ee94f54cfe71693565e22c21d7df2
jyhem <jyhem@users.sourceforge.net>
2011-11-06 01:31:15 +0000

    Typo (thanks chealer)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@38717 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-func-users_own_rank.php
+++ modules/mod-func-users_own_rank.php
@@ -9,7 +9,7 @@ if (strpos($_SERVER["SCRIPT_NAME"],basename(__FILE__)) !== false) {
 function module_users_own_rank_info() {
 	return array(
 		'name' => tra('My Score'),
-		'description' => tra('Displays the loggued user\'s rank and score.'),
+		'description' => tra('Displays the logged user\'s rank and score.'),
 		'prefs' => array( 'feature_score' ),
 		'params' => array()
 	);
