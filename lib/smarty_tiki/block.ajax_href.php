d396ae06dd412df7f0cae3d505323437ed35a876
chealer <chealer@users.sourceforge.net>
2011-11-07 19:04:18 +0000

    [FIX] Broken links from self_link (and button) with double-HTML-encoded ampersands when a script links to itself Note: r38710 / r38639 regression. Oops
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/8.x@38740 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/block.ajax_href.php
+++ lib/smarty_tiki/block.ajax_href.php
@@ -51,7 +51,7 @@ function smarty_block_ajax_href($params, $content, $smarty, $repeat)
 	$def_func = (isset($params['scrollTo']) ? 'window.scrollTo('.$params['scrollTo'].');' : '') . 'loadComponent';
 	$func = isset($params['function']) ? $params['function']: $def_func;	// preserve previous behaviour
 	$last_user = htmlspecialchars($user);
-	$attributes = " href=\"" . htmlspecialchars($url, ENT_QUOTES) . '" ';
+	$attributes = " href=\"" . $url . '" ';
 	if ($onclick) {
 		$attributes .= "onclick=\"$onclick\" ";
 	}
