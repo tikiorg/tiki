eb32e25d85b6b9f6da01b2f5e6d507889d92679a
xavidp <xavidp@users.sourceforge.net>
2011-11-13 19:21:11 +0000

    all missing js strings to en
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@38803 b456876b-0849-0410-b77d-98878d47e9d5

--- lang/en/language.js
+++ lang/en/language.js
@@ -25,4 +25,20 @@ lang = {
 //	"Do not match" : "Do not match",
 //	"Advanced options" : "Advanced options",
 //	"Pick a file." : "Pick a file.",
+//	"Add Field" : "Add Field",
+//	"Edit Field" : "Edit Field",
+//	"Removing the field will result in data loss. Are you sure?" : "Removing the field will result in data loss. Are you sure?",
+//	"Save" : "Save",
+//	"Cancel" : "Cancel",
+//	"Delete" : "Delete",	
+//	"Unassign module" : "Unassign module",
+//	"Edit module" : "Edit module",
+//	"Edit module:" : "Edit module:",
+//	"Favorite" : "Favorite",
+//	"What address are you looking for?" : "What address are you looking for?",
+//	"Filter:" : "Filter:",
+//	"Please enter a page name" : "Please enter a page name",
+//	"Change Highlighter" : "Change Highlighter",
+//	"Are you sure you want to unassign this module?" : "Are you sure you want to unassign this module?"
+	// remember the IE does not support ending comma on last item
 };
