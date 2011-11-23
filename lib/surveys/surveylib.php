6f70bf4d6ff5fdefd752950be786191096a44a9e
sampaioprimo <sampaioprimo@users.sourceforge.net>
2011-11-23 16:19:46 +0000

    indentation
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@38952 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/surveys/surveylib.php
+++ lib/surveys/surveylib.php
@@ -7,8 +7,8 @@
 
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
-  header("location: index.php");
-  exit;
+	header("location: index.php");
+	exit;
 }
 
 class SurveyLib extends TikiLib
@@ -16,12 +16,12 @@ class SurveyLib extends TikiLib
 	function list_surveys($offset, $maxRecords, $sort_mode, $find)
 	{
 		if ($find) {
-		  $findesc = '%' . $find . '%';
-		  $mid = " where (`name` like ? or `description` like ?)";
-		  $bindvars=array($findesc, $findesc);
+			$findesc = '%' . $find . '%';
+			$mid = " where (`name` like ? or `description` like ?)";
+			$bindvars=array($findesc, $findesc);
 		} else {
-		  $mid = '';
-		  $bindvars=array();
+			$mid = '';
+			$bindvars=array();
 		}
 
 		$query = "select `surveyId` from `tiki_surveys` $mid";
@@ -31,21 +31,21 @@ class SurveyLib extends TikiLib
 
 		//FIXME Perm:filter ?
 		foreach ( $result as $res ) {
-		  $objperm = $this->get_perm_object($res['surveyId'], 'survey', '', false);
-		  if ( $objperm['tiki_p_take_survey'] ) {
-			if ( ($maxRecords == -1) || (($n >= $offset) && ($n < ($offset + $maxRecords))) ) {
-			  $retids[] = $res['surveyId'];
+			$objperm = $this->get_perm_object($res['surveyId'], 'survey', '', false);
+			if ( $objperm['tiki_p_take_survey'] ) {
+				if ( ($maxRecords == -1) || (($n >= $offset) && ($n < ($offset + $maxRecords))) ) {
+					$retids[] = $res['surveyId'];
+				}
+				$n++;
 			}
-			$n++;
-		  }
 		}
 		if ( $n > 0 ) {
-		  $query = 'select * from `tiki_surveys` where `surveyId` in (' . implode(',', $retids) . ') order by ' . $this->convertSortMode($sort_mode);
-		  $result = $this->fetchAll($query);
-		  foreach ( $result as $res ) {
-			$res["questions"] = $this->getOne('select count(*) from `tiki_survey_questions` where `surveyId`=?', array( (int) $res['surveyId']));
-			$ret[] = $res;
-		  }
+			$query = 'select * from `tiki_surveys` where `surveyId` in (' . implode(',', $retids) . ') order by ' . $this->convertSortMode($sort_mode);
+			$result = $this->fetchAll($query);
+			foreach ( $result as $res ) {
+				$res["questions"] = $this->getOne('select count(*) from `tiki_survey_questions` where `surveyId`=?', array( (int) $res['surveyId']));
+				$ret[] = $res;
+			}
 		} 
 		
 		$retval = array();
