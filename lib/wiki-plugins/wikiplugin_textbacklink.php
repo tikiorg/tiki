290f4bc879bc36ee0c826578632b0b0f09c72572
robertplummer <robertplummer@users.sourceforge.net>
2011-12-02 22:05:38 +0000

    [NEW] TextBacklink and Textlink plugins
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@39079 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_textbacklink.php
@@ -0,0 +1,81 @@
+<?php
+// (c) Copyright 2002-2011 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: wikiplugin_htmlfeed.php 39021 2011-11-28 16:34:55Z robertplummer $
+
+function wikiplugin_textbacklink_info()
+{
+	return array(
+		'name' => tra('TextBacklink'),
+		'documentation' => 'PluginTextBacklink',
+		'description' => tra('Creates a linkable part of a page using textbacklink protocol'),
+		'prefs' => array( 'feature_wiki', 'wikiplugin_textbacklink' ),
+		'icon' => 'pics/icons/link.png',
+		'params' => array(			
+			'name' => array(
+				'required' => true,
+				'name' => tra('Name'),
+				'default' => false
+			),
+		),
+	);
+}
+
+function wikiplugin_textbacklink($data, $params)
+{
+    global $tikilib, $headerlib, $feedItem, $caching, $page;
+    static $textbacklinkI = 0;
+	++$textbacklinkI;
+	
+	$params = array_merge(array("name" => ""), $params);
+	
+	extract($params, EXTR_SKIP);
+	
+	if ($caching == true) {
+		//here we are building a list of items that can be linked to
+		$feed = new Feed_TextBacklink();
+		$data = TikiLib::lib("parser")->parse_data($data);
+		
+		$feedItem['description'] = $data;
+		$feedItem['name'] = (!empty($name) ? $name : $feedItem['name'] . ' ' . $textbacklinkI);;
+		
+		$feed->addItem($feedItem);
+	}
+	
+	$contributions = json_decode( Feed_TextBacklink_Contribution::textbacklink($name)->getCache() );
+	
+	foreach($contributions->entry as $key => $item) {
+    	$data = "~np~<a href='$item->href' title='$item->name' class='textlink$textbacklinkI' data-key='$key'>*</a>~/np~" . $data;
+	}
+	
+	$headerlib->add_jq_onready("
+		$('#textBacklink$textbacklinkI').click(function() {
+			alert('" . $tikilib->tikiUrl() . "tiki-feed?type=textbacklink" . "');
+		});
+		
+		
+		var textlinks = " . json_encode($contributions->entry) . ";
+		$('.textlink$textbacklinkI').click(function() {
+			var textlink = textlinks[$(this).data('key')];
+			var popup = $('<div>' +
+				'<table>' +
+					'<tr>' +
+						'<th>Href</th>' +
+						'<th>Name</th>' +
+					'</tr>' +
+					'<tr>' +
+						'<td><a>' + textlink['href'] + '</a></td>' +
+						'<td>' + textlink['name'] + '</td>' +
+					'</tr>' +
+				'</table>' +
+			'</div>').dialog({title: textlink['name']});
+			
+			popup.find('a').attr('href', textlink['href']);
+			
+			return false;
+		});
+	");
+	return $data . "~np~<br /><input id='textBacklink" . $textbacklinkI . "' type='button' value='" . tr("Create TextBacklink") .  "' />~/np~";
+}
---
+++ lib/wiki-plugins/wikiplugin_textlink.php
@@ -0,0 +1,62 @@
+<?php
+// (c) Copyright 2002-2011 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: wikiplugin_htmlfeed.php 39021 2011-11-28 16:34:55Z robertplummer $
+
+function wikiplugin_textlink_info()
+{
+	return array(
+		'name' => tra('TextLink'),
+		'documentation' => 'PluginTextlink',
+		'description' => tra('Links your article to a site using textbacklink protocol'),
+		'prefs' => array( 'feature_wiki', 'wikiplugin_textlink' ),
+		'icon' => 'pics/icons/link.png',
+		'params' => array(			
+			'name' => array(
+				'required' => true,
+				'name' => tra('Name'),
+				'default' => false
+			),
+			'href' => array(
+				'required' => true,
+				'name' => tra('Href'),
+				'default' => false
+			),
+		),
+	);
+}
+
+function wikiplugin_textlink($data, $params)
+{
+    global $tikilib, $headerlib, $feedItem, $caching, $page;
+    static $textlinkI = 0;
+	++$textlinkI;
+	
+	$params = array_merge(array("href" => ""), $params);
+	
+	extract($params, EXTR_SKIP);
+	
+	if (empty($href) || empty($name)) return tr("Ensure name and href are set.");
+	
+	$feed = Feed_Remote_TextBacklink::href($href);
+	
+	$href = parse_url($href);
+	
+	Feed_Remote_TextBacklink_Contribution::sendItem(array(
+		'href'=> $href['scheme'] . "://" . $href['host'] . $href['path'],
+		'pageName'=> $page,
+		'linkName'=> $page . $textlinkI,
+		'description'=> TikiLib::lib("parser")->parse_data($data),
+		'originName'=> $name
+	));
+	
+	$item = $feed->getItem($name);
+	
+	if (!empty($item->href)) {
+    	return "<a href='$item->href'>$data</a>";
+	} else {
+    	return $data;
+    }
+}
