ba9f68927fbde8ab5cc888cd7fdd4c284f8f1842
robertplummer <robertplummer@users.sourceforge.net>
2011-12-02 22:07:02 +0000

    [ENH] Removed bad ID
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@39080 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_textbacklink.php
+++ lib/wiki-plugins/wikiplugin_textbacklink.php
@@ -3,7 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: wikiplugin_htmlfeed.php 39021 2011-11-28 16:34:55Z robertplummer $
+// $Id$
 
 function wikiplugin_textbacklink_info()
 {
