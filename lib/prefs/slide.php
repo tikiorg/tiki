a95f1ad81ceaf5a95bac9d8481975d2d7a8d2244
marclaporte <marclaporte@users.sourceforge.net>
2011-12-19 04:10:22 +0000

    [FIX/REL] Since the preference(s) once contained in these files were removed from Tiki, restore empty (dummy) files to make sure users which upgrade via FTP overwrite the file and avoid a Fatal error: Call to undefined method
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/8.x@39222 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/prefs/slide.php
@@ -0,0 +1,10 @@
+<?php
+// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: slide.php 25210 2010-02-14 20:52:22Z changi67 $
+
+// Since the preference(s) once contained in this file were removed from Tiki, this file 
+// is purposely here and empty to make sure users which upgrade via FTP overwrite the file 
+// and avoid a Fatal error: Call to undefined method 
