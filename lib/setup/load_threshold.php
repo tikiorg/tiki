1c1cacaf370883f28398e4f4a6d351ac5754a183
arildb <arildb@users.sourceforge.net>
2012-01-24 08:15:24 +0000

    [FIX] Activating "Close site when server load is above the threshold (except for those with permission)" caused failures on Windows. The functionality uses the PHP5 function: sys_getloadavg(), which is not implemented for Windows.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/8.x@39635 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/load_threshold.php
+++ lib/setup/load_threshold.php
@@ -9,14 +9,16 @@
 $access->check_script($_SERVER["SCRIPT_NAME"], basename(__FILE__));
 
 // get average server load in the last minute
-$load = sys_getloadavg();
-$server_load = $load[0];
+if(function_exists('sys_getloadavg')) {
+	$load = sys_getloadavg();
+	$server_load = $load[0];
 
-if ( $prefs['use_load_threshold'] == 'y' and $tiki_p_access_closed_site != 'y' and !isset($bypass_siteclose_check) ) {
-	if ( $server_load > $prefs['load_threshold'] ) {
-		$url = 'tiki-error_simple.php?error=' . urlencode($prefs['site_busy_msg']);
-		header('location: ' . $url);
-		exit;
+	if ( $prefs['use_load_threshold'] == 'y' and $tiki_p_access_closed_site != 'y' and !isset($bypass_siteclose_check) ) {
+		if ( $server_load > $prefs['load_threshold'] ) {
+			$url = 'tiki-error_simple.php?error=' . urlencode($prefs['site_busy_msg']);
+			header('location: ' . $url);
+			exit;
+		}
 	}
+	$smarty->assign('server_load', $server_load == 0 ? '?' : $server_load);
 }
-$smarty->assign('server_load', $server_load == 0 ? '?' : $server_load);
