5f825b8f5965507ffb81599827606bb071343aa8
sylvieg <sylvieg@users.sourceforge.net>
2012-01-29 16:04:07 +0000

    [merge/r39697][FIX]search: unified_incremental_update prefs needs feature_search
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@39699 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/search/refresh-functions.php
+++ lib/search/refresh-functions.php
@@ -17,7 +17,7 @@ function refresh_index($object_type, $object_id = null, $process = true)
 
 	// First process unified search, then process the legacy indexing if required.
 
-	if ( $prefs['unified_incremental_update'] == 'y' && $object_id ) {
+	if ( $prefs['feature_search'] == 'y' && $prefs['unified_incremental_update'] == 'y' && $object_id ) {
 		$unified_type = refresh_index_convert_type($object_type);
 
 		try {
