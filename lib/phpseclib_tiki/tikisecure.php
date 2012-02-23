baae684f33dadfb0584f280cbb2b387a5f7f9936
robertplummer <robertplummer@users.sourceforge.net>
2012-02-23 19:24:05 +0000

    [NEW] Added of a security certificate generator (right now stores in file gal), encrypter and decrypter wrapper (phpseclib)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@39929 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/phpseclib_tiki/tikisecure.php
@@ -0,0 +1,81 @@
+<?php
+
+// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+class TikiSecure
+{
+	var $certName = "Tiki Secure Certificate";
+	
+	function __construct()
+	{
+	}
+	
+	function encrypt($data = "")
+	{
+		$keys = $this->getKeys();
+		
+		$path = get_include_path();
+		set_include_path("lib/phpseclib/");
+		require_once('Crypt/RSA.php');
+		$rsa = new Crypt_RSA();
+		
+		$rsa->loadKey($keys->privatekey);
+		
+		set_include_path($path);
+		
+		return $rsa->encrypt($data);
+	}
+	
+	function decrypt($cipher)
+	{
+		if ($this->hasKeys() == false) return "";
+		
+		$keys = $this->getKeys();
+
+		$rsa = new Crypt_RSA();
+		
+		$rsa->loadKey($keys->privatekey);
+		$rsa->loadKey($keys->publickey);
+		
+		echo $rsa->decrypt($cipher);
+	}
+	
+	function hasKeys()
+	{
+		return FileGallery_File::filename($this->certName)->exists();
+	}
+	
+	function getKeys()
+	{
+		//Get existing certificate if it exists
+		$keys = json_decode(FileGallery_File::filename($this->certName)->data());
+		
+		if (empty($keys)) {
+			$keys = $this->newKeys();
+		}
+		
+		return $keys;
+	}
+	
+	private function newKeys()
+	{
+		$path = get_include_path();
+		set_include_path("lib/phpseclib/");
+		require_once('Crypt/RSA.php');
+		
+		$rsa = new Crypt_RSA();
+		$keys = $rsa->createKey();
+		
+		set_include_path($path);
+		
+		FileGallery_File::filename($this->certName)
+			->setParam("description", $this->certName)
+			->replace(json_encode($keys));
+		
+		return $keys;
+	}
+}
