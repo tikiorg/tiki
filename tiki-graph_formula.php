5367838b0f22384c6fe357872cfc6b6996ec7949
pkdille <pkdille@users.sourceforge.net>
2012-03-01 21:25:56 +0000

    No changes - code styling only (seen on quality.tiki.org)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@39993 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-graph_formula.php
+++ tiki-graph_formula.php
@@ -1,6 +1,6 @@
 <?php
 // (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
-// 
+//
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
@@ -51,69 +51,67 @@ function convert_formula( $formula )
 
 	// Stripping all quotes
 	$chars = array( '`', "'", '"', '&', '[', ']', '$', '{', '}' );
-	$formula = str_replace( $chars, array_fill( 0, count($chars), '' ), $formula );
+	$formula = str_replace($chars, array_fill(0, count($chars), ''), $formula);
 
 	// Make sure only valid functions are used
-	preg_match_all( '/([a-z0-9_]+)/i', $formula, $out, PREG_PATTERN_ORDER );
+	preg_match_all('/([a-z0-9_]+)/i', $formula, $out, PREG_PATTERN_ORDER);
 	foreach ( $out[0] as $match )
-		if ( !is_numeric( $match ) && !in_array( strtolower( $match ), $valid ) && $match !== 'x' )
+		if ( !is_numeric($match) && !in_array(strtolower($match), $valid) && $match !== 'x' )
 			die( "Invalid function call {$match}" );
 	
 	// Replace spaces for commas
-	$formula = preg_replace( "/\s+/", ', ', $formula );
+	$formula = preg_replace('/\s+/', ', ', $formula);
 
-	$formula = str_replace( 'x', '$x', $formula );
+	$formula = str_replace('x', '$x', $formula);
 
 	return create_function('$x', "return $formula;");
 }
 
 $access->check_permission('feature_sheet');
 
-if ( !( is_numeric( $_GET['w'] )
-	&& is_numeric( $_GET['h'] )
-	&& is_numeric( $_GET['s'] )
+if ( !( is_numeric($_GET['w'])
+	&& is_numeric($_GET['h'])
+	&& is_numeric($_GET['s'])
 	&& $_GET['s'] <= 500 && $_GET['s'] > 0
-	&& is_numeric( $_GET['min'] )
-	&& is_numeric( $_GET['max'] )
-	&& is_array( $_GET['f'] )
-	&& $_GET['min'] < $_GET['max'] 
+	&& is_numeric($_GET['min'])
+	&& is_numeric($_GET['max'])
+	&& is_array($_GET['f'])
+	&& $_GET['min'] < $_GET['max']
 	&& $_GET['w'] >= 100
-	&& $_GET['h'] >= 100 ) )
+	&& $_GET['h'] >= 100 )
+)
 	die;
 
-switch( $_GET['t'] )
-{
-case 'png':
-	$renderer = new GD_GRenderer( $_GET['w'], $_GET['h'] );
-	break;
-case 'pdf':
-	$renderer = new PDFLib_GRenderer( $_GET['p'], $_GET['o'] );
-	break;
-default:
-	die;
+switch ( $_GET['t'] ) {
+	case 'png':
+		$renderer = new GD_GRenderer($_GET['w'], $_GET['h']);
+					break;
+	case 'pdf':
+		$renderer = new PDFLib_GRenderer($_GET['p'], $_GET['o']);
+					break;
+	default:
+		die;
 }
 
 $graph = new MultilineGraphic;
-$graph->setTitle( $_GET['title'] );
+$graph->setTitle($_GET['title']);
 
 $size = ($_GET['max'] - $_GET['min']) / $_GET['s'];
 
 $data = array();
-foreach ( array_values( $_GET['f'] ) as $key=>$formula )
-{
-	$formula = convert_formula( $formula );
+foreach ( array_values($_GET['f']) as $key=>$formula) {
+	$formula = convert_formula($formula);
 	
 	$data['x'] = array();
 	$data['y'.$key] = array();
 	
-	for( $x = $_GET['min']; $_GET['max'] > $x; $x += $size )
-	{
+	for ( $x = $_GET['min']; $_GET['max'] > $x; $x += $size ) {
 		$data['x'][] = $x;
 		$data['y'.$key][] = $formula($x);
 	}
 }
 
-$graph->setData( $data );
-$graph->draw( $renderer );
+$graph->setData($data);
+$graph->draw($renderer);
 
-$renderer->httpOutput( "graph.{$_GET['t']}" );
+$renderer->httpOutput("graph.{$_GET['t']}");
