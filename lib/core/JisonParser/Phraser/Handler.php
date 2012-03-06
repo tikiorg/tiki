02b0811af8c7323daa43538a319fa08a02e6aa99
pkdille <pkdille@users.sourceforge.net>
2012-03-06 20:48:39 +0000

    No changes - code styling only (seen on quality.tiki.org)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@40050 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/JisonParser/Phraser/Handler.php
+++ lib/core/JisonParser/Phraser/Handler.php
@@ -1,4 +1,10 @@
 <?php
+// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
 class JisonParser_Phraser_Handler extends JisonParser_Phraser
 {
 	var $chars = array();
@@ -6,60 +12,58 @@ class JisonParser_Phraser_Handler extends JisonParser_Phraser
 	var $currentWord = -1;
 	var $wordsChars = array();
 	var $indexes = array();
-	var $parsed = "";
+	var $parsed = '';
 	var $cache = array();
-	
+
 	function tagHandler($tag)
 	{
 		return $tag;
 	}
-	
+
 	function wordHandler($word)
 	{
 		$this->currentWord++;
 		$this->words[] = $word;
-		
-		foreach($this->indexes as $i => $index) {
-			if (
-				$this->currentWord >= $index['start'] && 
-				$this->currentWord <= $index['end']
+
+		foreach ($this->indexes as $i => $index) {
+			if ($this->currentWord >= $index['start'] 
+					&& $this->currentWord <= $index['end']
 			) {
 				$word = '<span class="phrase phrase' . $i . '" style="border: none;">' . $word . '</span>';
 			}
-			
+
 			if ($this->currentWord == $index['start']) {
 				$word = '<span class="phraseStart phraseStart' . $i . '" style="border: none; font-weight: cold;"></span>' . $word;
 			}
-			
+
 			if ($this->currentWord == $index['end']) {
 				if (empty($this->wordsChars[$this->currentWord])) {
 					$word = $word . '<span class="phraseEnd phraseEnd' . $i . '" style="border: none;"></span>';
 				} else {
 					$word = '<span class="phrase phrase' . $i . '" style="border: none;">' . $word . '</span>';
-				}	       						
+				}
 			}
 		}
-		
+
 		return $word;
 	}
-	
+
 	function charHandler($char)
 	{
 		if (empty($this->wordsChars[$this->currentWord])) $this->wordsChars[$this->currentWord] = "";
-		
+
 		//this line attempts to solve some character translation problems
-		$char = iconv("UTF-8", "ISO-8859-1", $char);
-		
+		$char = iconv('UTF-8', 'ISO-8859-1', $char);
+
 		$this->wordsChars[$this->currentWord] .= $char;
 		$this->chars[] = $char;
-		
-		foreach($this->indexes as $i => $index) {
-			if (
-				$this->currentWord >= $index['start'] && 
-				$this->currentWord <= $index['end']
+
+		foreach ($this->indexes as $i => $index) {
+			if ($this->currentWord >= $index['start'] 
+					&& $this->currentWord <= $index['end']
 			) {
 				$char = '<span class="phrases phrase' . $i . '" style="border: none;">' . $char . '</span>';
-				
+
 				if ($this->currentWord == $index['end']) {
 					if (!empty($this->wordsChars[$this->currentWord])) {
 						$char = $char . '<span class="phraseEnd phraseEnd' . $i . '" style="border: none;"></span>';
@@ -67,80 +71,80 @@ class JisonParser_Phraser_Handler extends JisonParser_Phraser
 				}
 			}
 		}
-		
-		
+
+
 		return $char;
 	}
-	
+
 	function isUnique($parent, $phrase)
 	{
 		$parentWords = $this->sanitizeToWords($parent);
-        $phraseWords = $this->sanitizeToWords($phrase);
-   		
+		$phraseWords = $this->sanitizeToWords($phrase);
+
 		$this->clearIndexes();
-		
-   		$this->addIndexes($parentWords, $phraseWords, true);
-        
-        if (count($this->indexes) > 1) {
-        	return false;
-        } else {
-        	return true;
-        }
+
+		$this->addIndexes($parentWords, $phraseWords, true);
+
+		if (count($this->indexes) > 1) {
+			return false;
+		} else {
+			return true;
+		}
 	}
-	
+
 	function findPhrases($parent, $phrases)
 	{
-       	$parentWords = $this->sanitizeToWords($parent);
-       	$phrasesWords = array();
-		
+		$parentWords = $this->sanitizeToWords($parent);
+		$phrasesWords = array();
+
 		$this->clearIndexes();
-		
-       	foreach($phrases as $phrase) {
-       		$phraseWords = $this->sanitizeToWords($phrase);
-			
+
+		foreach ($phrases as $phrase) {
+			$phraseWords = $this->sanitizeToWords($phrase);
+
 			$this->addIndexes($parentWords, $phraseWords);
 			$phrasesWords[] = $phraseWords;
-       	}
-		
+		}
+
 		if (!empty($this->indexes)) {
 			$parent = $this->parse($parent);
 		}
-		
+
 		return $parent;
 	}
-	
+
 	function clearIndexes()
 	{
 		$this->indexes = array();
 	}
-	
+
 	function addIndexes($parentWords, $phraseWords, $allMatches = false)
 	{
-        $phraseLength = count($phraseWords) - 1;
-		$phraseConcat = implode($phraseWords, "|");
-		$parentConcat = implode($parentWords, "|");
-        
+		$phraseLength = count($phraseWords) - 1;
+		$phraseConcat = implode($phraseWords, '|');
+		$parentConcat = implode($parentWords, '|');
+
 		$boundaries = explode($phraseConcat, $parentConcat);
-		
-		for($i = 0, $j = count($boundaries);$i < $j; $i++) {
-			$boundaryLength = substr_count($boundaries[$i], "|");
-			
+
+		for ($i = 0, $j = count($boundaries); $i < $j; $i++) {
+			$boundaryLength = substr_count($boundaries[$i], '|');
+
 			$this->indexes[] = array(
-				"start"=> $boundaryLength,
-				"end"=> $boundaryLength + $phraseLength
+					'start' => $boundaryLength,
+					'end' => $boundaryLength + $phraseLength
 			);
-			
+
 			$i++;
 		}
-    }
-	
+	}
+
 	function match($subject, $pattern)
 	{
 		preg_match('/' . $pattern . '/', $subject, $match);
-		
+
 		return (empty($match) ? false : true);
 	}
-	
+
 	static function hasPhrase($parent, $phrase)
 	{
 		$parent = self::sanitizeToWords($parent);
@@ -148,17 +152,17 @@ class JisonParser_Phraser_Handler extends JisonParser_Phraser
 
 		$parent = implode('|', $parent);
 		$phrase = implode('|', $phrase);
-		
+
 		return (strpos($parent, $phrase) !== false ? true : false);
 	}
-	
+
 	static function sanitizeToWords($html)
 	{
-		$sanitized = preg_replace('/<(.|\n)*?>/', " ", $html);
-		$sanitized = preg_replace('/\W/', " ", $sanitized);
+		$sanitized = preg_replace('/<(.|\n)*?>/', ' ', $html);
+		$sanitized = preg_replace('/\W/', ' ', $sanitized);
 		$sanitized = split(" ", $sanitized);
 		$sanitized = array_values(array_filter($sanitized, 'strlen'));
-		
+
 		return $sanitized;
 	}
 }
--- lib/core/JisonParser/Wiki/Handler.php
+++ lib/core/JisonParser/Wiki/Handler.php
@@ -1,6 +1,6 @@
 <?php
 // (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
-// 
+//
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
@@ -13,34 +13,32 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 	private $postedit_handlers = array();
 	var $npOn = false;
 	var $pluginStack = array();
-	
+
 	// state & plugin handlers
 	function plugin($pluginDetails)
 	{
 		//nested parsing!
 		$parserlib = new ParserLib;
 		$parser = new JisonParser_Wiki_Handler();
-		
+
 		return $parser->parse(
-			$parserlib->plugin_execute(
-				$pluginDetails['name'],
-				$pluginDetails['body'],
-				$parserlib->plugin_split_args(
-					$pluginDetails['args']
-				)
-			)
+						$parserlib->plugin_execute(
+										$pluginDetails['name'],
+										$pluginDetails['body'],
+										$parserlib->plugin_split_args($pluginDetails['args'])
+						)
 		);
 	}
-	
+
 	function stackPlugin($yytext)
 	{
 		$pluginName = $this->match('/^\{([A-Z]+)/', $yytext);
-		$pluginArgs =  $this->match('/[(].*?[)]/', $yytext);
-		
+		$pluginArgs = $this->match('/[(].*?[)]/', $yytext);
+
 		$this->pluginStack[] = array(
-			"name"=> $pluginName,
-			"args"=> $pluginArgs,
-			"body"=> ''
+			'name' => $pluginName,
+			'args' => $pluginArgs,
+			'body' => ''
 		);
 	}
 
@@ -49,14 +47,14 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 		$pluginName = $this->match('/^\{([a-z]+)/', $yytext);
 		$pluginArgs = $this->split(' ', $yytext);
 		$pluginArgs = $this->shift($pluginArgs);
-		
+
 		return array(
-			"name"=> $pluginName,
-			"args"=> implode(' ', $pluginArgs),
-			"body"=> ''
+			'name' => $pluginName,
+			'args' => implode(' ', $pluginArgs),
+			'body' => ''
 		);
 	}
-	
+
 	function npState($npState, $ifTrue, $ifFalse)
 	{
 		return ($npState == true ? $ifTrue : $ifFalse);
@@ -65,200 +63,217 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 	//Wiki Syntax Objects Parsing Start
 	function bold($content)
 	{
-		return "<strong>" . $content . "</strong>";
+		return '<strong>' . $content . '</strong>';
 	}
-	
+
 	function box($content)
 	{
-		return "<div style='border: solid 1px black;'>" . $content . "</div>";
+		return '<div style="border: solid 1px black;">' . $content . '</div>';
 	}
-	
+
 	function center($content)
 	{
-		return "<center>" . $content . "</center>";
+		return '<center>' . $content . '</center>';
 	}
-	
+
 	function colortext($content)
 	{
 		$text = $this->split(':', $content);
 		$color = $text[0];
 		$content = $text[1];
-		return "<span style='color: #" . $color . ";'>" . $content . "</span>";
+
+		return '<span style="color: #' . $color . ';">' . $content . '</span>';
 	}
-	
+
 	function italics($content)
 	{
-		return "<i>" . $content . "</i>";
+		return '<i>' . $content . '</i>';
 	}
-	
+
 	function header1($content)
 	{
-		return "<h1>" . $content . "</h1>";
+		return '<h1>' . $content . '</h1>';
 	}
-	
+
 	function header2($content)
 	{
-		return "<h2>" . $content . "</h2>";
+		return '<h2>' . $content . '</h2>';
 	}
-	
+
 	function header3($content)
 	{
-		return "<h3>" . $content . "</h3>";
+		return '<h3>' . $content . '</h3>';
 	}
-	
+
 	function header4($content)
 	{
-		return "<h4>" . $content . "</h4>";
+		return '<h4>' . $content . '</h4>';
 	}
-	
+
 	function header5($content)
 	{
-		return "<h5>" . $content . "</h5>";
+		return '<h5>' . $content . '</h5>';
 	}
-	
+
 	function header6($content)
 	{
-		return "<h6>" . $content . "</h6>";
+		return '<h6>' . $content . '</h6>';
 	}
-	
+
 	function hr()
 	{
-		return "<hr />";
+		return '<hr />';
 	}
-	
+
 	function link($content)
 	{
 		$link = $this->split(':', $content);
 		$href = $content;
-		
+
 		if ($this->match('/\|/', $content)) {
 			$href = $link[0];
 			$content = $link[1];
 		}
-		return "<a href='" . $href . "'>" . $content . "</a>";
+		return '<a href="' . $href . '">' . $content . '</a>';
 	}
-	
+
 	function smile($smile)
 	{ //this needs more tlc too
-		return "<img src='img/smiles/icon_" . $smile . ".gif' alt='" . $smile . "' />";
+		return '<img src="img/smiles/icon_' . $smile . '.gif" alt="' . $smile . '" />';
 	}
-	
+
 	function strikethrough($content)
 	{
-		return "<span style='text-decoration: line-through;'>" . $content . "</span>";
+		return '<span style="text-decoration: line-through;">' . $content . '</span>';
 	}
-	
+
 	function tableParser($content)
 	{
 		$tableContents = '';
 		$rows = $this->split('<br />', $content);
+
 		for ($i = 0, $count_rows = count($rows); $i < $count_rows; $i++) {
 			$row = '';
-			
+
 			$cells = $this->split('|', $rows[$i]);
 			for ($j = 0, $count_cells = count($cells); $j < $count_cells; $j++) {
 				$row .= $this->table_td($cells[$j]);
 			}
 			$tableContents .= $this->table_tr($row);
 		}
-		return "<table style='width: 100%;'>" . $tableContents . "</table>";
+
+		return '<table style="width: 100%;">' . $tableContents . '</table>';
 	}
-	
+
 	function table_tr($content)
 	{
-		return "<tr>" . $content . "</tr>";
+		return '<tr>' . $content . '</tr>';
 	}
-	
+
 	function table_td($content)
 	{
-		return "<td>" . $content . "</td>";
+		return '<td>' . $content . '</td>';
 	}
-	
+
 	function titlebar($content)
 	{
-		return "<div class='titlebar'>" . $content . "</div>";
+		return '<div class="titlebar">' . $content . '</div>';
 	}
-	
+
 	function underscore($content)
 	{
-		return "<u>" . $content . "</u>";
+		return '<u>' . $content . '</u>';
 	}
-	
+
 	function wikilink($content)
 	{
 		$wikilink = $this->split('|', $content);
 		$href = $content;
-		
+
 		if ($this->match('/\|/', $content)) {
 			$href = $wikilink[0];
 			$content = $wikilink[1];
 		}
-		return "<a href='" . $href . "'>" . $content . "</a>";
+
+		return '<a href="' . $href . '">' . $content . '</a>';
 	}
-	
+
 	function html($content)
 	{
 		return $content;
 	}
-	
+
 	function formatContent($content)
 	{
 		//return nl2br($content);
 		return $content;
 	}
-	
+
 	//unified functions used inside parser
 	function substring($val, $left, $right)
 	{
 		 return substr($val, $left, $right);
 	}
-	
+
 	function match($pattern, $subject)
 	{
 		preg_match($pattern, $subject, $match);
+
 		return (!empty($match[1]) ? $match[1] : false);
 	}
-	
+
 	function replace($search, $replace, $subject)
 	{
 		return str_replace($search, $replace, $subject);
 	}
-	
+
 	function split($delimiter, $string)
 	{
 		return explode($delimiter, $string);
 	}
-	
+
 	function join()
 	{
 		$array = func_get_args();
+
 		return implode($array, '');
 	}
-	
+
 	function size($array)
 	{
-		if (empty($array)) $array = array();
+		if (empty($array))
+			$array = array();
+
 		return count($array);
 	}
-	
+
 	function pop($array)
 	{
-		if (empty($array)) $array = array();
+		if (empty($array))
+			$array = array();
+
 		array_pop($array);
+
 		return $array;
 	}
-	
+
 	function push($array, $val)
 	{
-		if (empty($array)) $array = array();
+		if (empty($array))
+			$array = array();
+
 		array_push($array, $val);
+
 		return $array;
 	}
-	
+
 	function shift($array)
 	{
-		if (empty($array)) $array = array();
+		if (empty($array))
+			$array = array();
+
 		array_shift($array);
+
 		return $array;
 	}
 }
