c725e575404177287979e18bbcff3f8b8bb6e0e3
lindonb <lindonb@users.sourceforge.net>
2012-03-10 20:27:06 +0000

    [FIX] Broaden parameter wasn't working
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@40116 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-func-action_similarcontent.php
+++ modules/mod-func-action_similarcontent.php
@@ -46,7 +46,7 @@ function module_action_similarcontent($mod_reference, $module_params)
 	
 	$broaden = 'n';
 	if (isset($module_params['broaden'])) {
-		$filterType = $module_params['broaden'];
+		$broaden = $module_params['broaden'];
 	}
 		
 	$currentContentType = "article";
