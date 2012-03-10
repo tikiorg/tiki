a382d58867bac8d6f849958143698400c2ef3d05
pkdille <pkdille@users.sourceforge.net>
2012-03-10 13:47:21 +0000

    No changes - code styling only (seen on quality.tiki.org)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@40106 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Perms/Reflection/PermissionComparator.php
+++ lib/core/Perms/Reflection/PermissionComparator.php
@@ -10,30 +10,35 @@ class Perms_Reflection_PermissionComparator
 	private $additions;
 	private $removals;
 
-	function __construct( $left, $right ) {
-		$this->additions = $this->compare( $right, $left );
-		$this->removals = $this->compare( $left, $right );
+	function __construct($left, $right)
+	{
+		$this->additions = $this->compare($right, $left);
+		$this->removals = $this->compare($left, $right);
 	}
 
-	function equal() {
-		return empty( $this->additions ) && empty( $this->removals );
+	function equal()
+	{
+		return empty($this->additions) && empty($this->removals);
 	}
 
-	function getAdditions() {
+	function getAdditions()
+	{
 		return $this->additions;
 	}
 
-	function getRemovals() {
+	function getRemovals()
+	{
 		return $this->removals;
 	}
 
-	private function compare( $left, $right ) {
+	private function compare( $left, $right )
+	{
 		$out = array();
 
 		$all = $left->getPermissionArray();
 		foreach ( $all as $group => $permissions ) {
 			foreach ( $permissions as $perm ) {
-				if ( ! $right->has( $group, $perm ) ) {
+				if ( ! $right->has($group, $perm) ) {
 					$out[] = array( $group, $perm );
 				}
 			}
