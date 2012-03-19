e1f8d6daf5f460f9dd80501f99feaa6781da323b
robertplummer <robertplummer@users.sourceforge.net>
2012-03-19 17:13:24 +0000

    [FIX] Look for relationships on both sides, not one or the other
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@40247 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/20120319_remove_sheets_with_no_relation_tiki.sql
+++ installer/schema/20120319_remove_sheets_with_no_relation_tiki.sql
@@ -3,5 +3,8 @@ FROM `tiki_object_relations`
 	LEFT JOIN `tiki_sheets` `ps` on `ps`.`sheetId` = `tiki_object_relations`.`source_itemId`
 	LEFT JOIN `tiki_sheets` `cs` on `cs`.`sheetId` = `tiki_object_relations`.`target_itemId`
 WHERE
-	ISNULL(IFNULL(`ps`.`sheetId`, `cs`.`sheetId`))
+	(
+		ISNULL(`ps`.`sheetId`)
+		OR ISNULL(`cs`.`sheetId`)
+	)
 	AND `tiki_object_relations`.`source_type` = 'sheetId'
