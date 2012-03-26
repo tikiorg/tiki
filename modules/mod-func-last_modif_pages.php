dcf01c24394f50563d0cbf8fe70d41b80bc7c8b4
lindonb <lindonb@users.sourceforge.net>
2012-03-26 03:25:09 +0000

    Tidy up info a little for the PluginModule popup edit form
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@40382 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-func-last_modif_pages.php
+++ modules/mod-func-last_modif_pages.php
@@ -15,7 +15,7 @@ function module_last_modif_pages_info()
 {
 	return array(
 		'name' => tra('Latest Changes'),
-		'description' => tra('Lists the specified number of pages, starting from the most recently modified.'),
+		'description' => tra('List the specified number of pages, starting from the most recently modified.'),
 		'prefs' => array("feature_wiki"),
 		'params' => array(
 			'absurl' => array(
@@ -23,11 +23,11 @@ function module_last_modif_pages_info()
 				'description' => tra('If set to "y", some of the links use an absolute URL instead of a relative one. This can avoid broken links if the module is to be sent in a newsletter, for example.') . " " . tr('Default: "n".')
 			),
 			'url' => array(
-				'name' => tra('Link target'),
+				'name' => tra('Link Target'),
 				'description' => tra('Target URL of the "...more" link at the bottom of the module.') . " " . tr('Default:') . ' tiki-lastchanges.php'
 			),
 			'maxlen' => array(
-				'name' => tra('Maximum length'),
+				'name' => tra('Maximum Length'),
 				'description' => tra('Maximum number of characters in page names allowed before truncating.'),
 				'filter' => 'int'
 			)
