0c5688436d71ee6da3f937ab892bd350183b8608
lindonb <lindonb@users.sourceforge.net>
2012-03-26 04:19:20 +0000

    Tidy up info a little for the PluginModule popup edit form
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@40401 b456876b-0849-0410-b77d-98878d47e9d5

--- modules/mod-func-assistant.php
+++ modules/mod-func-assistant.php
@@ -15,8 +15,8 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 function module_assistant_info()
 {
 	return array(
-		'name' => tra('Tiki assistant'),
-		'description' => tra('Displays an assistant to guide new Tiki admins.'),
+		'name' => tra('Tiki Assistant'),
+		'description' => tra('Display an assistant to guide new Tiki admins.'),
 		'prefs' => array(),
 		'params' => array()
 	);
