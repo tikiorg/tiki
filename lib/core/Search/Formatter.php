4e347ceccf833174e5e89a9b08eb23129ca77c8c
sampaioprimo <sampaioprimo@users.sourceforge.net>
2012-03-27 20:13:30 +0000

    replace self with $this to avoid warning message (Search_Formatter::format() is only called as object method anyway)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@40437 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Search/Formatter.php
+++ lib/core/Search/Formatter.php
@@ -52,7 +52,7 @@ class Search_Formatter
 
 		foreach ($list as $row) {
 			// Clear blank values so the defaults prevail
-			$row = array_filter($row, array(self, 'is_empty_string'));
+			$row = array_filter($row, array($this, 'is_empty_string'));
 			$row = array_merge($defaultValues, $row);
 
 			$subEntries = array();
