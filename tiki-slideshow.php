74bbfaa7855b3a22b563b27ddf22c593e14fbbf8
pkdille <pkdille@users.sourceforge.net>
2012-03-29 19:26:24 +0000

    [QT] Quality Team Backport
    r36674 | robertplummer | 2011-08-30 19:41:43 +0200 (Tue, 30 Aug 2011) | 2 lines
    [FIX] Locations for sheet dependencies.
    [FIX] Brought the external jquery.sheet_tiki for mods from 7 model
    r36676 | robertplummer | 2011-08-30 20:19:47 +0200 (Tue, 30 Aug 2011) | 2 lines
    [FIX] Removed slideshow call from tiki-setup.php to make it less invasive
    [ENH] Backported slideshow fixes from 7
    r36683 | sylvieg | 2011-08-30 21:05:01 +0200 (Tue, 30 Aug 2011) | 1 line
    [bp/r36682][FIX]article: switch off need to confirm
    
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@40509 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-slideshow.php
+++ tiki-slideshow.php
@@ -1,5 +1,5 @@
 <?php
-// (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
+// (c) Copyright 2002-2011 by authors of the Tiki Wiki CMS Groupware Project
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -8,48 +8,78 @@
 $section = 'wiki page';
 require_once ('tiki-setup.php');
 
-$access->check_feature('feature_wiki');
+include_once ('lib/structures/structlib.php');
+include_once ('lib/wiki/wikilib.php');
+include_once ('lib/wiki-plugins/wikiplugin_slideshow.php');
+
+if ($prefs['feature_wiki'] != 'y') {
+	$smarty->assign('msg', tra("This feature is disabled").": feature_wiki");
+
+	$smarty->display("error_raw.tpl");
+	die;
+}
+
+//make the other things know we are loading a slideshow
+$tikilib->is_slideshow = true;
+$smarty->assign('is_slideshow' , 'y');
 
 // Create the HomePage if it doesn't exist
 if (!$tikilib->page_exists($prefs['wikiHomePage'])) {
-	$tikilib->create_page($prefs['wikiHomePage'], 0, '', $tikilib->now, 'Tiki initialization');
+	$tikilib->create_page($prefs['wikiHomePage'], 0, '', date("U"), 'Tiki initialization');
 }
+
+if (!isset($_SESSION["thedate"])) {
+	$thedate = date("U");
+} else {
+	$thedate = $_SESSION["thedate"];
+}
+
 // Get the page from the request var or default it to HomePage
 if (!isset($_REQUEST["page"])) {
-	$_REQUEST["page"] = $prefs['wikiHomePage'];
-	$page = $prefs['wikiHomePage'];
-	$smarty->assign('page', $prefs['wikiHomePage']);
-} else {
-	$page = $_REQUEST["page"];
-	$smarty->assign_by_ref('page', $_REQUEST["page"]);
+	$_REQUEST["page"] = $wikilib->get_default_wiki_page();
 }
+$page = $_REQUEST['page'];
+$smarty->assign('page', $page);
+
 // If the page doesn't exist then display an error
-if (!($info = $tikilib->get_page_info($page))) {
-	$smarty->assign('msg', tra('Page cannot be found'));
-	$smarty->display('error.tpl');
+if (!($info = $tikilib->page_exists($page))) {
+	include_once ('tiki-index.php');
 	die;
 }
-// Now check permissions to access this page
-$tikilib->get_perm_object($page, 'wiki page', $info);
 
-$access->check_permission('tiki_p_view');
+if (isset($_REQUEST['theme'])) {
+	print_r(getSlideshowTheme($_REQUEST['theme'], true));
+	die; 
+}
+
+// Now check permissions to access this page
+$tikilib->get_perm_object( $page, 'wiki page', $info);
+if ($tiki_p_view != 'y') {
+	$smarty->assign('errortype', 401);
+	$smarty->assign('msg', tra("Permission denied. You cannot view this page."));
+
+	$smarty->display("error_raw.tpl");
+	die;
+}
 
 // BreadCrumbNavigation here
-// Get the number of pages from the default or userPreferences
 // Remember to reverse the array when posting the array
-$anonpref = $prefs['userbreadCrumb'];
+
 if (!isset($_SESSION["breadCrumb"])) {
 	$_SESSION["breadCrumb"] = array();
 }
+
 if (!in_array($page, $_SESSION["breadCrumb"])) {
 	if (count($_SESSION["breadCrumb"]) > $prefs['userbreadCrumb']) {
-		array_shift($_SESSION["breadCrumb"]);
+		array_shift ($_SESSION["breadCrumb"]);
 	}
+
 	array_push($_SESSION["breadCrumb"], $page);
 } else {
 	// If the page is in the array move to the last position
 	$pos = array_search($page, $_SESSION["breadCrumb"]);
-	unset($_SESSION["breadCrumb"][$pos]);
+
+	unset ($_SESSION["breadCrumb"][$pos]);
 	array_push($_SESSION["breadCrumb"], $page);
 }
 
@@ -57,71 +87,95 @@ if (!in_array($page, $_SESSION["breadCrumb"])) {
 if ($prefs['count_admin_pvs'] == 'y' || $user != 'admin') {
 	$tikilib->add_hit($page);
 }
+
 // Get page data
 $info = $tikilib->get_page_info($page);
-$smarty->assign('page_info', $info);
-// Verify lock status
-if ($info["flag"] == 'L') {
-	$smarty->assign('lock', true);
-} else {
-	$smarty->assign('lock', false);
-}
-// If not locked and last version is user version then can undo
-$smarty->assign('canundo', 'n');
-if ($info["flag"] != 'L' && (($tiki_p_edit == 'y' && $info["user"] == $user) || ($tiki_p_remove == 'y'))) {
-	$smarty->assign('canundo', 'y');
-}
-if ($tiki_p_admin_wiki == 'y') {
-	$smarty->assign('canundo', 'y');
-}
-//Now process the pages
-preg_match_all("/-=([^=]+)=-/", $info["data"], $reqs);
-$slides = preg_split('/-=[^=]+=-/', $info["data"]);
-if (count($slides) < 2) {
-	$slides = explode("...page...", $info["data"]);
-	array_unshift($slides, '');
-}
-if (!isset($_REQUEST["slide"]) or $_REQUEST["slide"] < 0) {
-	$_REQUEST["slide"] = 0;
-}
-if (!isset($slides[$_REQUEST["slide"] + 1])) {
-	$_REQUEST["slide"] = count($slides) - 2;
-}
-$smarty->assign('prev_slide', $_REQUEST["slide"] - 1);
-$smarty->assign('next_slide', $_REQUEST["slide"] + 1);
-if (isset($reqs[1][$_REQUEST["slide"]])) {
-	$slide_title = $reqs[1][$_REQUEST["slide"]];
-} else {
-	$slide_title = '';
-}
-$slide_data = $tikilib->parse_data($slides[$_REQUEST["slide"] + 1]);
-if (isset($reqs[1][$_REQUEST["slide"] - 1])) {
-	$slide_prev_title = $reqs[1][$_REQUEST["slide"] - 1];
-} else {
-	$slide_prev_title = '';
-}
-if (isset($reqs[1][$_REQUEST["slide"] + 1])) {
-	$slide_next_title = $reqs[1][$_REQUEST["slide"] + 1];
-} else {
-	$slide_next_title = '';
-}
-$smarty->assign('slide_prev_title', $slide_prev_title);
-$smarty->assign('slide_next_title', $slide_next_title);
-$smarty->assign('slide_title', $slide_title);
-$smarty->assign('slide_data', $slide_data);
-$total_slides = count($slides) - 1;
-$current_slide = $_REQUEST["slide"] + 1;
-$smarty->assign('total_slides', $total_slides);
-$smarty->assign('current_slide', $current_slide);
+$pdata = $tikilib->parse_data_raw($info["data"]);
+
+if (!isset($_REQUEST['pagenum']))
+	$_REQUEST['pagenum'] = 1;
+
+$pages = $wikilib->get_number_of_pages($pdata);
+$pdata = $wikilib->get_page($pdata, $_REQUEST['pagenum']);
+$smarty->assign('pages', $pages);
+
+// Put ~pp~, ~np~ and <pre> back. --rlpowell, 24 May 2004
+$tikilib->replace_preparse( $info["data"], $preparsed, $noparsed );
+$tikilib->replace_preparse( $pdata, $preparsed, $noparsed );
+
+$smarty->assign_by_ref('parsed', $pdata);
+//$smarty->assign_by_ref('lastModif',date("l d of F, Y  [H:i:s]",$info["lastModif"]));
 $smarty->assign_by_ref('lastModif', $info["lastModif"]);
+
 if (empty($info["user"])) {
 	$info["user"] = 'anonymous';
 }
+
 $smarty->assign_by_ref('lastUser', $info["user"]);
+
 include_once ('tiki-section_options.php');
-$smarty->assign('wiki_extras', 'y');
-ask_ticket('slideshow');
+
+$headerlib->add_cssfile( 'lib/jquery.s5/jquery.s5.css' );
+$headerlib->add_jsfile( 'lib/jquery.s5/jquery.s5.js' );
+$headerlib->add_jq_onready( '
+	window.s5Settings = (window.s5Settings ? window.s5Settings : {});
+	
+	$.s5.start($.extend(window.s5Settings, {
+		menu: function() {
+			return $("#tiki_slideshow_buttons").show();
+		},
+		noteMenu: function() {
+			var menu =  $("#tiki_slideshowNote_buttons").clone().show();
+			
+			menu.find(".tiki-slideshow-theme")
+				.s5ThemeHandler()
+				.change(function() {
+					$(".tiki-slideshow-theme").val($(this).val());
+				});
+			
+			return menu;
+		},
+		themeName: "default"
+	}));
+	
+	$("#main").hide();
+	
+	$.fn.extend({
+		s5ThemeHandler: function(s) {
+			return this
+				.val(window.s5Settings.themeName)
+				.change(function() {
+					var theme = $(this).val();
+					theme = (theme ? theme : "default");
+					
+					window.s5Settings.themeName = theme;
+					$.get("tiki-slideshow.php", {theme: theme}, function(o) {
+						theme = $.parseJSON(o);
+						$.s5.makeTheme(theme);
+					}); 
+				});
+		}
+	});
+	
+	$(".tiki-slideshow-theme")
+		.s5ThemeHandler()
+		.change(function() {
+			if (!$.s5.note) return;
+			if (!$.s5.note.document) return;
+			
+			$($.s5.note.document).find(".tiki-slideshow-theme").val($(this).val());
+		});
+	
+	if (window.s5Settings.themeName == "default") {
+		$(".tiki-slideshow-theme").change();
+	}
+');
+
+ask_ticket('index-raw');
+
 // Display the Index Template
 $smarty->assign('dblclickedit', 'y');
-$smarty->assign('mid', 'tiki-show_page.tpl');
-$smarty->display("tiki-slideshow.tpl");
+$smarty->assign('mid','tiki-show_page_raw.tpl');
+
+// use tiki_full to include include CSS and JavaScript
+$smarty->display("tiki_full.tpl");
