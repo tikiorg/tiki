67114f9bf09e51776ba63dcdeaeb75f826bc7ce6
robertplummer <robertplummer@users.sourceforge.net>
2012-04-19 15:08:21 +0000

    [FIX] Refactor of decode pages so that it extends to history and pages (PART 1) next will be for plugin tracking
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@40992 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/999999991_decode_pages_sources_tiki.php
+++ installer/schema/999999991_decode_pages_sources_tiki.php
@@ -32,62 +32,60 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 
 function upgrade_999999991_decode_pages_sources_tiki($installer)
 {
+	set_time_limit(60 * 60 * 3); //3 hours
+
 	global $tikilib, $prefs, $tikiroot, $user_overrider_prefs, $tiki_p_trust_input, $smarty, $access, $local_php, $categlib, $headerlib;	// globals are required here for tiki-setup_base.php
+	$parserlib = TikiLib::lib('parser');
+
 	include_once('tiki-setup_base.php');
 	include_once ('lib/categories/categlib.php');	// needed for cat_jail fn in list_pages()
 
-	$maxRecords = 100;
-	// The outer loop attemps to limit memory usage by fetching pages gradually
-	for ($offset = 0; $pages = $tikilib->list_pages($offset, $maxRecords), !empty($pages['data']); $offset += $maxRecords) {
-		foreach ($pages['data'] as $page) {
-			if (!$page['is_html']) {
+	//we want to limit how much we have in memory, so here we count the pages that have plugins so we have can then offset threw them
+	$ids =
+		TikiLib::fetchAll('SELECT page_id FROM tiki_pages') +
+		TikiLib::fetchAll('SELECT historyId FROM tiki_history');
 
-				$data = $page['data'];
-				$parserlib = TikiLib::lib('parser');
+	foreach($ids as $id) {
+		if (isset($id['page_id'])) {
+			$data = TikiLib::getOne("SELECT data FROM tiki_pages WHERE page_id = ?", array($id['page_id']));
+		} else {
+			$data = TikiLib::getOne("SELECT data FROM tiki_history WHERE historyId = ?", array($id['historyId']));
+		}
 
-				$matches = WikiParser_PluginMatcher::match($data);			// find the plugins
+		//We know the page was single encoded, but plugin possibly double?
+		$data =  htmlspecialchars_decode($data);
 
-				$replaced = array();
+		$matches = WikiParser_PluginMatcher::match($data);			// find the plugins
 
-				foreach ($matches as $match) {								// each plugin
-					$plugin = (string) $match;
-					$key = '§'.md5($tikilib->genPass()).'§';				// by replace whole plugin with a guid
-					$data = preg_replace('/' . preg_quote($plugin, '/') . '/', $key, $data);
+		$replaced = array();
 
-					$body = $match->getBody();									// leave the bodies alone
-					$key2 = '§'.md5($tikilib->genPass()).'§';					// by replacing it with a guid
-					$plugin = preg_replace('/' . preg_quote($body, '/') . '/', $key2, $plugin);
-					$plugin = str_replace('&amp;', '&', $plugin);				// remove &amp; in case of double encoding as seen on info.t.o
-					$plugin = htmlspecialchars_decode($plugin);					// decode entities in the plugin args (usually &quot;)
+		foreach ($matches as $match) {								// each plugin
+			$plugin = (string) $match;
+			$key = '§'.md5($tikilib->genPass()).'§';				// by replace whole plugin with a guid
+			$data = preg_replace('/' . preg_quote($plugin, '/') . '/', $key, $data);
 
-					$plugin = str_replace($key2, $body, $plugin);				// finally put the body back
+			$body = $match->getBody();									// leave the bodies alone
+			$key2 = '§'.md5($tikilib->genPass()).'§';					// by replacing it with a guid
+			$plugin = preg_replace('/' . preg_quote($body, '/') . '/', $key2, $plugin);
 
-					$replaced['key'][] = $key;
-					$replaced['data'][] = $plugin;						// store the decoded-args plugin for replacement later
-
-				}
-
-				$parserlib->plugins_replace($data, $replaced);			// put the plugins back into the page
-
-
-				if ($data != $page['data']) {
-					$tikilib->update_page(
-									$page['pageName'],
-									$data,
-									'System upgrade: Converting HTML characters in plugin args (v3)',
-									'admin',
-									'0.0.0.0',
-									$page['description'],
-									1,
-									$page['lang'],
-									$page['is_html'],
-									null,
-									null,
-									'',
-									$page['wiki_authors_style']
-					);
-				}
+			//Here we detect if a plugin was double encoded and this is the second decode
+			if (preg_match("/&amp;&/i", $plugin) || preg_match("/&quot;/i", $plugin)) { //try to detect double encoding
+				$plugin = htmlspecialchars_decode($plugin);					// decode entities in the plugin args (usually &quot;)
 			}
+
+			$plugin = str_replace($key2, $body, $plugin);				// finally put the body back
+
+			$replaced['key'][] = $key;
+			$replaced['data'][] = $plugin;						// store the decoded-args plugin for replacement later
+
+		}
+
+		$parserlib->plugins_replace($data, $replaced);			// put the plugins back into the page
+
+		if (isset($id['page_id'])) {
+			TikiLib::query("UPDATE tiki_pages SET data = ? WHERE page_id = ?", array($id['page_id']));
+		} else {
+			TikiLib::query("UPDATE tiki_history SET data = ? WHERE historyId = ?", array($id['historyId']));
 		}
 	}
 }
