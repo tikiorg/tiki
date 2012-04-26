23cf4b379feb1ab396c246cab9ca55d7245d8100
pkdille <pkdille@users.sourceforge.net>
2012-04-26 08:07:43 +0000

    No changes - code styling only (seen on quality.tiki.org)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@41119 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-report_direct_object_perms.php
+++ tiki-report_direct_object_perms.php
@@ -3,6 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
 
 require_once ('tiki-setup.php');
 
@@ -13,21 +14,20 @@ $access->check_permission(array('tiki_p_admin_wiki'));
 
 global $tikilib;
 
-$query = "select distinct tp.pageName
-from tiki_pages tp,
-users_objectpermissions perm
-where md5(concat('wiki page', lower(tp.pageName))) = perm.objectId
-and perm.objectType = 'wiki page'
-order by tp.pageName";
+$query = 'select distinct tp.pageName' .
+					' from tiki_pages tp, users_objectpermissions perm' .
+					" where md5(concat('wiki page', lower(tp.pageName))) = perm.objectId" .
+					" and perm.objectType = 'wiki page'" .
+					' order by tp.pageName';
 
 $result = $tikilib->query($query);
 
 $pages = array();
-while( $row = $result->fetchRow() ) {
+while ($row = $result->fetchRow()) {
 	$pages[] = $row['pageName'];
 }
 
 $smarty->assign('pagesWithDirectPerms', $pages);
 
 $smarty->assign('mid', 'tiki-report_direct_object_perms.tpl');
-$smarty->display("tiki.tpl");
+$smarty->display('tiki.tpl');
