12e4f227d3ec81629d71bd3949c914b6b6b46569
lphuberdeau <lphuberdeau@users.sourceforge.net>
2012-04-29 20:22:56 +0000

    [NEW] Adding intrusion detection system (enabled by default)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@41188 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/prefs/ids.php
@@ -0,0 +1,31 @@
+<?php
+// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+function prefs_ids_list()
+{
+	return array(
+		'ids_enabled' => array(
+			'name' => tra('Intrusion Detection System'),
+			'description' => tra('Use PHPIDS to check the input to pages to discover malicious requests.'),
+			'type' => 'flag',
+			'default' => 'y',
+		),
+		'ids_single_threshold' => array(
+			'name' => tra('Per-request maximum impact'),
+			'type' => 'text',
+			'filter' => 'int',
+			'default' => 25,
+		),
+		'ids_session_threshold' => array(
+			'name' => tra('Per-session impact'),
+			'type' => 'text',
+			'filter' => 'int',
+			'default' => 150,
+		),
+	);
+}
+
--- templates/admin/include_security.tpl
+++ templates/admin/include_security.tpl
@@ -47,6 +47,12 @@
 			{preference name=feature_ticketlib2}
 		</fieldset>
 		{/tab}
+
+		{tab name="{tr}IDS{/tr}"}
+			{preference name=ids_enabled}
+			{preference name=ids_single_threshold}
+			{preference name=ids_session_threshold}
+		{/tab}
 	
 		{tab name="{tr}Spam protection{/tr}"}
 			<fieldset>
