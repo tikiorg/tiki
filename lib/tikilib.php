83e76cdd407d87a3be990d6f6ca51ec8d89fa592
sylvieg <sylvieg@users.sourceforge.net>
2012-04-30 11:15:20 +0000

    [FIX]newsletter: when sending to many users the user preferences cache can be hudge
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/8.x@41207 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikilib.php
+++ lib/tikilib.php
@@ -3610,7 +3610,11 @@ class TikiLib extends TikiDb_Bridge
 		return true;
 	}
 
-
+	function clear_cache_user_preferences()
+	{
+		global $user_preferences;
+		unset($user_preferences);
+	}
 	function get_user_preferences($my_user, $names = null)
 	{
 		global $user_preferences;
