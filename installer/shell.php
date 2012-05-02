3125e51ab707d7349b088ef16d9f2da444ffdf71
chealer <chealer@users.sourceforge.net>
2012-05-02 19:40:33 +0000

    [FIX] Fatal error: Cannot redeclare class Cachelib in [...]/lib/cache/cachelib.php on line 20 oops, r41281 regression
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@41288 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/shell.php
+++ installer/shell.php
@@ -82,6 +82,6 @@ else {
 	
 	# Clear caches, since patches often manipulate the database directly without using the functions normally available outside the installer.
 	# All caches, even though scripts and patches surely don't affect them all.
-	include 'lib/cache/cachelib.php';
+	require_once 'lib/cache/cachelib.php';
 	$cachelib->empty_cache();
 }
