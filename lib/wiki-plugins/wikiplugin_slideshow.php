14fb78180011cdd67613bbdefcf4dcebb402a649
robertplummer <robertplummer@users.sourceforge.net>
2012-05-04 19:53:22 +0000

    [FIX] forwardport of r41342
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@41344 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_slideshow.php
+++ lib/wiki-plugins/wikiplugin_slideshow.php
@@ -113,11 +113,20 @@ function wikiplugin_slideshow_info()
 			'slideseconds' => array(
 				'required' => false,
 				'name' => tra('Slide Seconds'),
-				'description' => tra('How many seconds a slide will be open while playing'),
+				'description' => tra('How many seconds a slide will be open while playing, overridden when slideduration is set.'),
 				'filter' => 'digits',
 				'accepted' => tra('Second count'),
 				'default' => '15',
 				'since' => '7.0'
+			),
+			'slideduration' => array(
+				'required' => false,
+				'name' => tra('Slide Milliseconds'),
+				'description' => tra('How many milliseconds a slide will be open while playing'),
+				'filter' => 'digits',
+				'accepted' => tra('Second count'),
+				'default' => '15000',
+				'since' => '9.0'
 			),
 			'textside' => array(
 				'required' => false,
@@ -150,7 +159,11 @@ function wikiplugin_slideshow($data, $params)
 	$slidefontcolor = (isset($slidefontcolor) ? $slidefontcolor : '');
 	$listitemhighlightcolor = (isset($listitemhighlightcolor) ? $listitemhighlightcolor : '');
 	$class = (isset($class) ? " $class"  : '');
-	$slideduration = (isset($slideseconds) ? $slideseconds : 15) * 1000;
+
+	if (!isset($slideduration)) {
+		$slideduration = (isset($slideseconds) ? $slideseconds : 15) * 1000;
+	}
+
 	$textside = (isset($textside) ? $textside : 'left');
 	
 	if ($theme) {
