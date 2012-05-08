d735c46f4afd5b41a2a557ac68a2396b437dbd1d
jonnybradley <jonnybradley@users.sourceforge.net>
2012-05-08 16:23:05 +0000

    Revert: r41138 Was breaking italics and no longer necessary following other html entity fixes
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@41390 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/parser/parserlib.php
+++ lib/parser/parserlib.php
@@ -1830,7 +1830,7 @@ if ( \$('#$id') ) {
 		// Replace bold text
 		$line = preg_replace("/__(.*?)__/", "<strong>$1</strong>", $line);
 		// Replace italic text
-		$line = preg_replace_callback("/(=?)\'\'(.*?)\'\'/", array($this, 'callback_parse_italics'), $line);
+		$line = preg_replace("/\'\'(.*?)\'\'/", "<em>$1</em>", $line);
 		
 		if (!$ck_editor) {
 			// Replace definition lists
@@ -1839,16 +1839,6 @@ if ( \$('#$id') ) {
 		}
 
 		return $line;
-	}
-
-	private function callback_parse_italics( $match )
-	{
-		//if italics is before a '=' it is probably in an html element as an attribute that is empty
-		if (isset($match[1]) && $match[1] == '=') {
-			return $match[0];
-		}
-
-		return "<em>".$match[0]."</em>";
 	}
 
 	//*
