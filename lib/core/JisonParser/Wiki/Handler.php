86fc657894ffdbbf3e2fb8bddc3bdf1662509b76
robertplummer <robertplummer@users.sourceforge.net>
2012-05-31 21:56:54 +0000

    [ENH] Integrated fingerprinting and option setting, fingerprinting not working all the way, but very close
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@41688 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/JisonParser/Wiki/Handler.php
+++ lib/core/JisonParser/Wiki/Handler.php
@@ -22,7 +22,33 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 	var $npEntries = array();
 	public static $npCount = 0;
 	var $pluginEntries = array();
-	var $options = array();
+
+	public static $option = array();
+	public function setOption($option = array())
+	{
+		$page = $_REQUEST['page'];
+
+		self::$option = array_merge(array(
+			'skipvalidation'=>  false,
+			'is_html'=> false,
+			'absolute_links'=> false,
+			'language' => '',
+			'noparseplugins' => false,
+			'stripplugins' => false,
+			'noheaderinc' => false,
+			'page' => $page,
+			'print' => false,
+			'parseimgonly' => false,
+			'preview_mode' => false,
+			'suppress_icons' => false,
+			'parsetoc' => true,
+			'inside_pretty' => false,
+			'process_wiki_paragraphs' => true,
+			'min_one_paragraph' => false,
+			'skipvalidation' => false,
+		), $option);
+	}
+
 	var $addLineBreaksTracking = array(
 		'inTable' => 0,
 		'inPre' => 0,
@@ -45,6 +71,8 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 			}
 		} else {
 			$this->parsing = true;
+
+			if (empty(self::$options)) $this->setOption();
 
 			$this->preParse($input);
 			$result = parent::parse($input);
@@ -80,18 +108,52 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 	// state & plugin handlers
 	function plugin($pluginDetails)
 	{
+		global $smarty;
+
 		$argParser = new WikiParser_PluginArgumentParser;
+		$args = $argParser->parse($pluginDetails['args']);
 
-		$key = '§' . md5('plugin:'.self::$pluginCount) . '§';
-		self::$pluginCount++;
+		if (!self::$option['skipvalidation']) {
+			$status = $this->pluginCanExecute(strtolower($pluginDetails['name']), $pluginDetails['body'], $args);
+		} else {
+			$status = true;
+		}
 
-		$this->pluginEntries[$key] = $this->parse( $this->pluginExecute(
-			$pluginDetails['name'],
-			$argParser->parse($pluginDetails['args']),
-			$pluginDetails['body']
-		));
+		if ($status == true) {
+			$key = '§' . md5('plugin:'.self::$pluginCount) . '§';
+			self::$pluginCount++;
 
-		return $key;
+			$this->pluginEntries[$key] = $this->parse( $this->pluginExecute(
+				$pluginDetails['name'],
+				$args,
+				$pluginDetails['body']
+			));
+
+			return $key;
+		} else {
+			$smarty->assign('plugin_name', $pluginDetails['name']);
+			$smarty->assign('plugin_index', 0);
+
+			$smarty->assign('plugin_status', $status);
+
+			global $tiki_p_plugin_viewdetail, $tiki_p_plugin_preview, $tiki_p_plugin_approve;
+			$details = $tiki_p_plugin_viewdetail == 'y' && $status != 'rejected';
+			$preview = $tiki_p_plugin_preview == 'y' && $details && ! self::$option['preview_mode'];
+			$approve = $tiki_p_plugin_approve == 'y' && $details && ! self::$option['preview_mode'];
+
+			if (!self::$option['inside_pretty']) {
+				$smarty->assign('plugin_details', $details);
+			} else {
+				$smarty->assign('plugin_details', '');
+			}
+			$smarty->assign('plugin_preview', $preview);
+			$smarty->assign('plugin_approve', $approve);
+
+			$smarty->assign('plugin_body', $pluginDetails['body']);
+			$smarty->assign('plugin_args', $args);
+
+			return $smarty->fetch('tiki-plugin_blocked.tpl');
+		}
 	}
 
 	function stackPlugin($yytext)
@@ -153,6 +215,217 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 		}
 
 		return false;
+	}
+
+	function pluginCanExecute( $name, $data = '', $args = array(), $dontModify = false )
+	{
+		global $prefs;
+
+		// If validation is disabled, anything can execute
+		if ( $prefs['wiki_validate_plugin'] != 'y' )
+			return true;
+
+		$meta = $this->pluginInfo($name);
+		if ( ! isset( $meta['validate'] ) )
+			return true;
+
+		$fingerprint = $this->pluginFingerprint($name, $meta, $data, $args);
+
+		$val = $this->pluginFingerprintCheck($fingerprint, $dontModify);
+
+		if ( strpos($val, 'accept') === 0 )
+			return true;
+		elseif ( strpos($val, 'reject') === 0 )
+			return 'rejected';
+		else {
+			global $tiki_p_plugin_approve, $tiki_p_plugin_preview, $user;
+			if (
+				isset($_SERVER['REQUEST_METHOD'])
+				&& $_SERVER['REQUEST_METHOD'] == 'POST'
+				&& isset( $_POST['plugin_fingerprint'] )
+				&& $_POST['plugin_fingerprint'] == $fingerprint
+			) {
+				if ( $tiki_p_plugin_approve == 'y' ) {
+					if ( isset( $_POST['plugin_accept'] ) ) {
+						global $page;
+						$tikilib = TikiLib::lib('tiki');
+						$this->pluginFingerprintStore($fingerprint, 'accept');
+						$tikilib->invalidate_cache($page);
+						return true;
+					} elseif ( isset( $_POST['plugin_reject'] ) ) {
+						global $page;
+						$tikilib = TikiLib::lib('tiki');
+						$this->pluginFingerprintStore($fingerprint, 'reject');
+						$tikilib->invalidate_cache($page);
+						return 'rejected';
+					}
+				}
+
+				if ( $tiki_p_plugin_preview == 'y'
+					&& isset( $_POST['plugin_preview'] ) ) {
+					return true;
+				}
+			}
+
+			return $fingerprint;
+		}
+	}
+
+	function pluginFingerprintStore( $fingerprint, $type )
+	{
+		global $tikilib, $user, $page;
+		if ( $page ) {
+			$objectType = 'wiki page';
+			$objectId = $page;
+		} else {
+			$objectType = '';
+			$objectId = '';
+		}
+
+		$pluginSecurity = $tikilib->table('tiki_plugin_security');
+		$pluginSecurity->delete(array('fingerprint' => $fingerprint));
+		$pluginSecurity->insert(array(
+			'fingerprint' => $fingerprint,
+			'status' => $type,
+			'added_by' => $user,
+			'last_objectType' => $objectType,
+			'last_objectId' => $objectId
+		));
+	}
+
+	function pluginInfo( $name )
+	{
+		static $known = array();
+
+		if ( isset( $known[$name] ) ) {
+			return $known[$name];
+		}
+
+		if ( ! $this->pluginExists($name, true) )
+			return $known[$name] = false;
+
+		$func_name_info = "wikiplugin_{$name}_info";
+
+		if ( ! function_exists($func_name_info) ) {
+			if ( $info = $this->pluginAliasInfo($name) )
+				return $known[$name] = $info['description'];
+			else
+				return $known[$name] = false;
+		}
+
+		return $known[$name] = $func_name_info();
+	}
+
+	function pluginAliasInfo( $name )
+	{
+		global $prefs;
+
+		if (empty($name)) return false;
+
+		$name = TikiLib::strtolower($name);
+		$prefName = "pluginalias_$name";
+
+		if ( ! isset( $prefs[$prefName] ) ) return false;
+
+		return @unserialize($prefs[$prefName]);
+	}
+
+	function pluginFingerprint( $name, $meta, $data, $args )
+	{
+		$validate = (isset($meta['validate']) ? $meta['validate'] : '');
+
+		//$data = $this->unprotectSpecialChars($data, true);
+
+		if ( $validate == 'all' || $validate == 'body' )
+			$validateBody = str_replace('<x>', '', $data);	// de-sanitize plugin body to make fingerprint consistant with 5.x
+		else
+			$validateBody = '';
+
+		if ( $validate == 'all' || $validate == 'arguments' ) {
+			$validateArgs = $args;
+
+			// Remove arguments marked as safe from the fingerprint
+			foreach ( $meta['params'] as $key => $info ) {
+				if ( isset( $validateArgs[$key] )
+					&& isset( $info['safe'] )
+					&& $info['safe']
+				) {
+					unset($validateArgs[$key]);
+				}
+			}
+			// Parameter order needs to be stable
+			ksort($validateArgs);
+
+			if (empty($validateArgs)) {
+				$validateArgs = array( '' => '' );	// maintain compatibility with pre-Tiki 7 fingerprints
+			}
+		} else
+			$validateArgs = array();
+
+		$bodyLen = str_pad(strlen($validateBody), 6, '0', STR_PAD_RIGHT);
+		$serialized = serialize($validateArgs);
+		$argsLen = str_pad(strlen($serialized), 6, '0', STR_PAD_RIGHT);
+
+		$bodyHash = md5($validateBody);
+		$argsHash = md5($serialized);
+
+		return "$name-$bodyHash-$argsHash-$bodyLen-$argsLen";
+	}
+
+	function pluginFingerprintCheck( $fingerprint, $dontModify = false )
+	{
+		global $tikilib, $user;
+
+		$limit = date('Y-m-d H:i:s', time() - 15*24*3600);
+		$result = $tikilib->query("
+			SELECT status, if(status='pending' AND last_update < ?, 'old', '') flag
+			FROM tiki_plugin_security
+			WHERE fingerprint = ?
+		", array( $limit, $fingerprint ));
+
+		$needUpdate = false;
+
+		if ( $row = $result->fetchRow() ) {
+			$status = $row['status'];
+			$flag = $row['flag'];
+
+			if ( $status == 'accept' || $status == 'reject' ) {
+				return $status;
+			}
+
+			if ( $flag == 'old' ) {
+				$needUpdate = true;
+			}
+		} else {
+			$needUpdate = true;
+		}
+
+		if ( $needUpdate && !$dontModify ) {
+			global $page;
+			if ( $page ) {
+				$objectType = 'wiki page';
+				$objectId = $page;
+			} else {
+				$objectType = '';
+				$objectId = '';
+			}
+
+			if (!$user) {
+				$user = tra('Anonymous');
+			}
+
+			$pluginSecurity = $tikilib->table('tiki_plugin_security');
+			$pluginSecurity->delete(array('fingerprint' => $fingerprint));
+			$pluginSecurity->insert(array(
+				'fingerprint' => $fingerprint,
+				'status' => 'pending',
+				'added_by' => $user,
+				'last_objectType' => $objectType,
+				'last_objectId' => $objectId
+			));
+		}
+
+		return '';
 	}
 
 	function removeNpEntities(&$matches)
@@ -251,10 +524,10 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 						$listate = substr($line, $listlevel, 1);
 						if (($listate == '+' || $listate == '-') && !($litype == '*' && !strstr(current($listbeg), '</ul>') || $litype == '#' && !strstr(current($listbeg), '</ol>'))) {
 							$thisid = 'id' . microtime() * 1000000;
-							if ( !$this->options['ck_editor'] ) {
+							if ( !self::$option['ck_editor'] ) {
 								$data .= '<br /><a id="flipper' . $thisid . '" class="link" href="javascript:flipWithSign(\'' . $thisid . '\')">[' . ($listate == '-' ? '+' : '-') . ']</a>';
 							}
-							$listyle = ' id="' . $thisid . '" style="display:' . ($listate == '+' || $this->options['ck_editor'] ? 'block' : 'none') . ';"';
+							$listyle = ' id="' . $thisid . '" style="display:' . ($listate == '+' || self::$option['ck_editor'] ? 'block' : 'none') . ';"';
 							$addremove = 1;
 						}
 					}
@@ -268,10 +541,10 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 				$listate = substr($line, $listlevel, 1);
 				if (($listate == '+' || $listate == '-')) {
 					$thisid = 'id' . microtime() * 1000000;
-					if ( !$this->options['ck_editor'] ) {
+					if ( !self::$option['ck_editor'] ) {
 						$data .= '<br /><a id="flipper' . $thisid . '" class="link" href="javascript:flipWithSign(\'' . $thisid . '\')">[' . ($listate == '-' ? '+' : '-') . ']</a>';
 					}
-					$listyle = ' id="' . $thisid . '" style="display:' . ($listate == '+' || $this->options['ck_editor'] ? 'block' : 'none') . ';"';
+					$listyle = ' id="' . $thisid . '" style="display:' . ($listate == '+' || self::$option['ck_editor'] ? 'block' : 'none') . ';"';
 					$addremove = 1;
 				}
 				$data .= ($litype == '*' ? "<ul$listyle>" : "<ol$listyle>");
