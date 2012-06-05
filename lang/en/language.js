c8c848f4605e56e8439d6565151e177c3c90cded
xavidp <xavidp@users.sourceforge.net>
2012-06-05 14:44:23 +0000

    Added a missing string
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@41785 b456876b-0849-0410-b77d-98878d47e9d5

--- lang/en/language.js
+++ lang/en/language.js
@@ -39,6 +39,7 @@ lang = {
 //	"Filter:" : "Filter:",
 //	"Please enter a page name" : "Please enter a page name",
 //	"Change Highlighter" : "Change Highlighter",
+//	"Toggle Highlighter" : "Toggle Highlighter",
 //	"Are you sure you want to unassign this module?" : "Are you sure you want to unassign this module?"
 	// remember the IE does not support ending comma on last item
 };
