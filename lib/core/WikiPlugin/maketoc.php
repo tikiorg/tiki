b14467cf87e24b78ae4968cf8ea67ecc376275f3
robertplummer <robertplummer@users.sourceforge.net>
2012-06-27 21:09:22 +0000

    [FIX] Use header manager
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@42145 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/WikiPlugin/maketoc.php
+++ lib/core/WikiPlugin/maketoc.php
@@ -8,18 +8,17 @@
 class WikiPlugin_maketoc extends WikiPlugin_HtmlBase
 {
 	var $type = 'toc';
-	var $validate = 'all';
-	var $prefs = array('wikiplugin_maketoc');
+	var $validate;
 	var $filter = 'rawhtml_unsafe';
 	var $icon = 'img/icons/text_list_numbers.png';
 	var $tags = array( 'basic' );
 	var $parserLevel = 1; //0 is standard, we put this after the others
 
-	function __constuct()
+	function __construct()
 	{
-		$this->name = tra('Table of contents for a page');
+		$this->name = tr('Table of contents for a page');
 		$this->documentation = 'PluginMaketoc';
-		$this->description = tra('Add a table of contents to a page');
+		$this->description = tr('Add a table of contents to a page');
 		$this->params = array(
 			'type',
 			'maxdepth',
@@ -33,36 +32,11 @@ class WikiPlugin_maketoc extends WikiPlugin_HtmlBase
 
 	function output($data, $params, $index, $parser)
 	{
-		if (!empty($parser->headerStack)) {
+		$result = '<div id="toctitle"><h3>' .
+			tr('Table of contents') .
+		'</h3></div>' .
+			$parser->headerManager->toHtmlList('toc');
 
-			$result = '<div id="toctitle"><h3>' .
-				tr('Table of contents') .
-			'</h3></div>' .
-			$this->toList($parser->headerStack, true);
-
-			return $result;
-		}
-	}
-
-	private function toList($array, $isBase = false) {
-		$result = '';
-
-		foreach($array as &$header){
-			if(empty($header['content']) == false){
-				$result .= '<li><a class="link" href="#' . $header['id'] . '">' . $header['content'] . '</a></li>';
-
-				if(empty($header['children']) == false) {
-					$result .= $this->toList($header['children']);
-				}
-
-				$result .= '</li>';
-
-			} elseif (empty($header['children']) == false) {
-
-				$result .= $this->toList($header['children']);
-			}
-		}
-
-		return '<ul' . ($isBase == true ? ' class = "toc" ' : '') . '>' . $result . '</ul>';
+		return $result;
 	}
 }
