e5ecc62dce7cbbe12c82fdc55d10dc90b08bff45
robertplummer <robertplummer@users.sourceforge.net>
2012-07-05 15:20:14 +0000

    [FIX] Give plugins layers to ckeditor
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@42227 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/WikiPlugin/CKEditorNegotiator.php
+++ lib/core/WikiPlugin/CKEditorNegotiator.php
@@ -11,35 +11,26 @@ class WikiPlugin_CKEditorNegotiator extends WikiPlugin_ParserNegotiator
 
 	public function execute()
 	{
-		$plugin_result = parent::execute();
+		$result = parent::execute();
+
+		//This gives us access to the plugin levels
+		if ($this->parserLevel > WikiPlugin_ParserNegotiator::$currentParserLevel) return $result;
+
 		$syntax = $this->toSyntax();
-
-		// work out if I'm a nested plugin and return empty if so
-		$stack = debug_backtrace();
-		$plugin_nest_level = 0;
-		foreach ($stack as $st) {
-			if ($st['function'] === 'execute') {
-				$plugin_nest_level ++;
-				if ($plugin_nest_level > 1) {
-					return '';
-				}
-			}
-		}
-
 
 		$icon = isset($this->info['icon']) ? $this->info['icon'] : 'img/icons/wiki_plugin_edit.png';
 
 		// some plugins are just too flakey to do wysiwyg, so show the "source" for them ;(
 		if (in_array($this->name, array('trackerlist', 'kaltura', 'toc', 'freetagged', 'draw', 'googlemap'))) {
-			$plugin_result = '&nbsp;&nbsp;&nbsp;&nbsp;' . $syntax;
+			$result = '&nbsp;&nbsp;&nbsp;&nbsp;' . $syntax;
 		} else {
 			// Tiki 7+ adds ~np~ to plugin output so remove them
-			$plugin_result = preg_replace('/~[\/]?np~/ms', '', $plugin_result);
+			$result = preg_replace('/~[\/]?np~/ms', '', $result);
 
 			// remove hrefs and onclicks
-			$plugin_result = preg_replace('/\shref\=/i', ' tiki_href=', $plugin_result);
-			$plugin_result = preg_replace('/\sonclick\=/i', ' tiki_onclick=', $plugin_result);
-			$plugin_result = preg_replace('/<script.*?<\/script>/mi', '', $plugin_result);
+			$result = preg_replace('/\shref\=/i', ' tiki_href=', $result);
+			$result = preg_replace('/\sonclick\=/i', ' tiki_onclick=', $result);
+			$result = preg_replace('/<script.*?<\/script>/mi', '', $result);
 		}
 
 		if (!in_array($this->name, array('html'))) {		// remove <p> and <br>s from non-html
@@ -47,33 +38,33 @@ class WikiPlugin_CKEditorNegotiator extends WikiPlugin_ParserNegotiator
 			$this->body = str_replace('<br />', "\n", $this->body);
 		}
 
-		if ($this->containsHtmlBlock($plugin_result)) {
+		if ($this->containsHtmlBlock($result)) {
 			$elem = 'div';
 		} else {
 			$elem = 'span';
 		}
 
 		$elem_style = 'position:relative;';
-		if (in_array($this->name, array('img', 'div')) && preg_match('/<'.$this->name.'[^>]*style="(.*?)"/i', $plugin_result, $m)) {
+		if (in_array($this->name, array('img', 'div')) && preg_match('/<'.$this->name.'[^>]*style="(.*?)"/i', $result, $m)) {
 			if (count($m)) {
 				$elem_style .= $m[1];
 			}
 		}
 
-		$ret = '<'.$elem.' class="tiki_plugin" plugin="' . $this->name . '" style="' . $elem_style . '"' .
+		$this->result = '<'.$elem.' class="tiki_plugin" plugin="' . $this->name . '" style="' . $elem_style . '"' .
 			' syntax="' . htmlentities( $syntax, ENT_QUOTES, 'UTF-8') . '"' .
 			' args="' . htmlentities($this->urlEncodeArgs(), ENT_QUOTES, 'UTF-8') . '"' .
 			' body="' . htmlentities($this->body, ENT_QUOTES, 'UTF-8') . '">'.	// not <!--{cke_protected}
-			'<img src="'.$icon.'" width="16" height="16" style="float:left;position:relative;z-index:10001" />' .
-			$plugin_result.'<!-- end tiki_plugin --></'.$elem.'>';
+			'<img src="'.$icon.'" width="16" height="16" style="float:left;position:absolute;z-index:10001" />' .
+			$result.'<!-- end tiki_plugin --></'.$elem.'>';
 
-		return 	$ret;
+		return $this->result;
 	}
 
-	function containsHtmlBlock($inHtml)
+	private function containsHtmlBlock(& $string)
 	{
 		// detect all block elements as defined on http://www.w3.org/2007/07/xhtml-basic-ref.html
 		$block_detect_regexp = '/<[\/]?(?:address|blockquote|div|dl|fieldset|h\d|hr|li|noscript|ol|p|pre|table|ul)/i';
-		return  (preg_match($block_detect_regexp, $inHtml) > 0);
+		return  (preg_match($block_detect_regexp, $string) > 0);
 	}
 }
