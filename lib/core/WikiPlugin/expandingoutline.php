69b257c7281682f43110f3feb6c37cba2716bd4d
robertplummer <robertplummer@users.sourceforge.net>
2012-07-06 18:56:26 +0000

    [NEW] Zend style wiki plugin expandingoutline turns lists into a dynamically expandable outline, COOL!
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@42246 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/core/WikiPlugin/expandingoutline.php
@@ -0,0 +1,120 @@
+<?php
+// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $$
+
+class WikiPlugin_outline extends WikiPlugin_HtmlBase
+{
+	var $type = 'outline';
+	var $validate = 'all';
+	var $prefs = array('wikiplugin_outline');
+	var $filter = 'rawhtml_unsafe';
+	var $icon = 'img/icons/mime/html.png';
+	var $tags = array( 'basic' );
+
+	function __construct()
+	{
+		$this->name = tra('Outline');
+		$this->documentation = 'PluginOutline';
+		$this->description = tra('Turns wiki syntax or html of list items into an expandable outline');
+		$this->body = tra('HTML code');
+		$this->params = array(
+
+		);
+	}
+
+	function output(&$data, &$params, &$index, &$parser)
+	{
+		global $headerlib;
+
+		if (get_class($parser) != 'JisonParser_Wiki_Handler') return '';
+
+		$parser->list->setOutput('outline');
+
+		$id = $this->id($index);
+
+		$headerlib->add_jq_onready(<<<JQ
+			var color = [
+				'rgb(255,37,6)',
+				'rgb(254,143,17)',
+				'rgb(249,245,41)',
+				'rgb(111,244,81)',
+				'rgb(83,252,243)',
+				'rgb(138,158,251)',
+				'rgb(206,127,250)',
+				'rgb(250,167,251)',
+				'rgb(255,214,188)',
+				'rgb(255,214,51)'
+			];
+        
+			var base = $('#$id');
+			base.find('table.tikiListTable:first table')
+				.parent().hide();
+
+			var tables = $('#$id').find('table.tikiListTable').each(function() {
+				var tier = $(this).data('tier');
+				$(this).find('td.tikiListTableLabel,td.tikiListTableBlank').each(function() {
+					$(this)
+						.css('background-color', color[tier]);
+				});
+			});
+
+			base.find('td.tikiListTableLabel')
+				.toggle(function(e) {
+				    if (e.shiftKey) {
+						tables.show();
+						return;
+				    }
+
+					var me = $(this).parent();
+					console.log(me.next().children('td'));
+					if (me.next().children('td').stop().fadeIn().length) {
+						me.find('img.listImg').attr('src', 'img/toggle-expand-dark.png');
+					}
+				}, function(e) {
+					if (e.shiftKey) {
+						tables.hide();
+						return;
+				    }
+
+					var me = $(this).parent();
+					if (me.next().children('td').stop().fadeOut().length) {
+						me.find('img.listImg').attr('src', 'img/toggle-collapse-dark.png');
+					}
+				})
+				.each(function() {
+					if ($(this).parent().next().html().match('table'))
+						$(this).prepend('<img class="listImg" src="img/toggle-collapse-dark.png" />');
+				});
+JQ
+);
+
+		$result = "<style>
+			#$id table {
+				width: 100%;
+				border-collapse:collapse;
+			}
+			#$id * {
+				border-width: 0px;
+			}
+			#$id .tikiListTable td, #$id .tikiListTable {
+				font-size: 14px;
+				background-color: white;
+				list-style-type: none;
+			}
+			#$id .tikiListTableLabel
+			{
+				width: 1px;
+				white-space: nowrap;
+
+			}
+		</style>" .
+			TikiLib::lib('tiki')->parse_data($data, array('is_html' => true));
+
+		$parser->list->setOutput('list');
+
+		return $result;
+	}
+}
