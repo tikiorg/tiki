9b41ce3317fd7e3a12567f61749d1ce428426d96
robertplummer <robertplummer@users.sourceforge.net>
2012-07-25 19:27:29 +0000

    [ROLLBACK] Accidental commit of some debug code
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@42475 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.payment.php
+++ lib/smarty_tiki/function.payment.php
@@ -24,33 +24,7 @@ function smarty_function_payment( $params, $smarty )
 	
 	// Unpaid payments can be seen by anyone as long as they know the number
 	// Just like your bank account, anyone can drop money in it.
-	print_r(array(
-		$invoice,
-		$user,
-		$userlib->get_user_id($user),
-		$info,
-		$objectperms->payment_view,
-		$info['state'] == 'outstanding',
-		$info['state'] == 'overdue',
-		$prefs['payment_user_only_his_own'] != 'y',
-		$info['state'] == 'past',
-		$prefs['payment_user_only_his_own_past'] != 'y',
-		$theguy,
-	));
-	if (
-		$info &&
-		$objectperms->payment_view &&
-		(
-			(
-				$info['state'] == 'outstanding' ||
-				$info['state'] == 'overdue'
-			) &&
-			$prefs['payment_user_only_his_own'] != 'y' ||
-			$info['state'] == 'past' &&
-			$prefs['payment_user_only_his_own_past'] != 'y' ||
-			$theguy
-		)
-	) {
+	if ( $info && $objectperms->payment_view && (($info['state'] == 'outstanding' || $info['state'] == 'overdue') && $prefs['payment_user_only_his_own'] != 'y' || $info['state'] == 'past' && $prefs['payment_user_only_his_own_past'] != 'y' || $theguy )) {
 		if ($prefs['payment_system'] == 'cclite' && isset($_POST['cclite_payment_amount']) && $_POST['cclite_payment_amount'] == $info['amount_remaining']) {
 			global $access, $cclitelib, $cartlib;
 			require_once 'lib/payment/cclitelib.php';
