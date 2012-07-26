3ff59a739c044c6953abe3403f215759964dadfd
robertplummer <robertplummer@users.sourceforge.net>
2012-07-26 20:05:49 +0000

    [FIX] More connectivity to the parent plugin that this one is leaching off of (memberpayment) [FIX] Allow for registered members to use plugin as well
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@42487 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_registermemberpayment.php
+++ lib/wiki-plugins/wikiplugin_registermemberpayment.php
@@ -1,4 +1,9 @@
 <?php
+// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
 
 function wikiplugin_registermemberpayment_info()
 {
@@ -10,7 +15,7 @@ function wikiplugin_registermemberpayment_info()
 		'documentation' => '',
 		'validate' => 'all',
 		'description' => tra('Allows user to register and make member payment at the same time'),
-		'prefs' => array( 'wikiplugin_registermemberpayment' ),
+		'prefs' => array( 'wikiplugin_registermemberpayment', 'payment_feature' ),
 		'body' => tra('NA'),
 		'icon' => 'img/icons/text_replace.png',
 		'params' => array_merge($infoFromParent['params'], array(
@@ -35,12 +40,10 @@ function wikiplugin_registermemberpayment($data, $params, $offset)
 		return tr('Insufficient Privileges');
 	}
 
-	$fakeUser = false;
-	if (empty($user)) {
-		$fakeUser = true;
+	if (empty($user) && isset($_REQUEST['wp_member_users']) == true) {
 		$user = end(explode('|', $_REQUEST['wp_member_users']));
 		$_SESSION['forceanon'] = 'y';
-		$_REQUEST['price'] = $_POST['price'] = $_GET['price'] = 30.00;
+		$_REQUEST['price'] = $_POST['price'] = $_GET['price'] = $params['price'];
 	}
 
 	$periodslabel = (isset($params['periodslabel']) ? tr($params['periodslabel']) : 'Number of periods:');
@@ -52,66 +55,113 @@ function wikiplugin_registermemberpayment($data, $params, $offset)
 			$fixedperiod = explode(':', $fixedperiod);
 			$name = $fixedperiod[0];
 			$value = (!empty($fixedperiod[1]) ? $fixedperiod[1] : $fixedperiod[0]);
-			$fixedperiodsDDL .= '<option value="' . trim($value) . '">' . trim($name) . '</option>\\';
+			$fixedperiodsDDL .= '<option value="' . trim($value) . '">' . trim($name) . '</option>';
 		}
 	}
 
-	$memberPayment = TikiLib::lib('parser')->parse_data(wikiplugin_memberpayment( $data, $params, $offset ), array('is_html' => true));
-	if ($fakeUser == true) unset($user);
-
-	include_once('lib/smarty_tiki/function.user_registration.php');
-	$register = smarty_function_user_registration(array(), $smarty);
-
-	if (isset($_POST['msg'])) {
-		$_POST['msg'] = addslashes(htmlspecialchars($_POST['msg']));
-		$headerlib->add_jq_onready("$.notify('" . $_POST['msg'] . "')");
+	if (empty($fixedperiodsDDL)) {
+		$periods = '<input id="memberDuration' . $i . '" name="duration" value="1" />';
+	} else {
+		$periods = '<select id="memberDuration' . $i . '" name="duration">' . $fixedperiodsDDL . '</select>';
 	}
 
+	$memberPayment = TikiLib::lib('parser')->parse_data(wikiplugin_memberpayment( $data, $params, $offset ), array('is_html' => true));
+	if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+		if (isset($_POST['msg'])) {
+			$_POST['msg'] = addslashes(htmlspecialchars($_POST['msg']));
+			$headerlib->add_jq_onready("$.notify('" . $_POST['msg'] . "')");
+		}
+
+		if (empty($user) ) {
+			include_once('lib/smarty_tiki/function.user_registration.php');
+			$register = smarty_function_user_registration(array(), $smarty);
+		} else {
+			$register = "<table>
+				<tr class='registerSubmitTr'>
+					<td colspan='2'>
+						<input type='hidden' id='name' value='$user' />
+						<input type='submit' value='" . tr('Submit') . "' class='registerSubmit'/>
+					</td>
+				</tr>
+			</table>";
+		}
+	}
+
+	$group = $params['group'];
+
 	$headerlib->add_jq_onready(<<<JQ
-		var reg = $('#memberRegister$i');
-		var submitBtn = reg.find('input.registerSubmit'),submitBtnTr = reg.find('tr.registerSubmitTr');
+		var reg = $('#memberRegister$i'),
+			pay = $('#memberPayment$i'),
+			user = "$user";
 
-		$('<tr>\
-			<td>Membership Type:</td>\
-			<td>\
-				<select id="memberType$i" name="group">\
-					<option value="Premium Registered">Premium</option>\
-					<option value="Registered">Free</option>\
-				</select>\
-			</td>\
-		</tr><tr>\
+		pay.find('.warning').insertAfter(pay); //just in case there are any warnings
+
+		var submitBtn = reg.find('input.registerSubmit'),
+			submitBtnTr = reg.find('tr.registerSubmitTr');
+
+		if (!user) {
+			$('<tr class="grpMmbChk">\
+				<td>$group Membership:</td>\
+				<td>\
+					<input type="checkbox" id="memberType$i" />\
+				</td>\
+			</tr>').insertBefore(submitBtnTr);
+		} else {
+			$('<tr>\
+				<td><b>$group Membership</b></td>\
+			</tr>').insertBefore(submitBtnTr);
+		}
+
+		$('<tr style="display: none;">\
 			<td>$periodslabel</td>\
-			<td>\
-				<select id="memberDuration$i" name="duration">\
-					$fixedperiodsDDL
-				</select>\
-			</td>\
-		</tr>').insertBefore(submitBtnTr);
+			<td>$periods</td>\
+		</tr>')
+			.insertBefore(submitBtnTr);
 
-		var frm = $('#memberRegister$i form');
-		frm
+		$('#memberType$i')
+			.click(function() {
+				$('tr.grpMmbChk').next()
+					.stop()
+					.fadeToggle();
+			})
+			.click();
+
+		reg
+			.bind('continueToPurchase', function() {
+				pay.find('input[name="wp_member_users"]').val($('#memberRegister$i #name').val());
+				pay.find('input[name="wp_member_periods"]').val($('#memberDuration$i').val());
+				pay.find('input:last').click();
+			})
 			.find('input:last').click(function() {
-				$.post($.service('user', 'register') + '&' + frm.serialize(), function(data) {
-					data = $.parseJSON(data);
-					if (typeof data == "string") {
-						var memberPayment = $('#memberPayment$i form');
-						if ($('#memberType$i').val() == 'Premium Registered') {
-							$('<input name="msg" />')
-								.val(data)
-								.prependTo(memberPayment);
+				var frmData = reg.find('form').serialize();
 
-							memberPayment.find('input[name="wp_member_users"]').val($('#memberRegister$i #name').val());
-							memberPayment.find('input[name="wp_member_periods"]').val($('#memberDuration$i').val());
-							memberPayment.find('input:last').click();
-						} else {
-							$.notify(data);
+				if (frmData) {
+					$.post($.service('user', 'register') + '&' + frmData, function(data) {
+						data = $.parseJSON(data);
+						if (typeof data == "string") {
+							if ($('#memberType$i').is(':checked')) {
+								$('<input name="msg" />')
+									.val(data)
+									.prependTo(pay.find('form'));
+
+								reg.trigger('continueToPurchase');
+							} else { //registered
+								$.notify(data);
+								$.notify(tr('You will be redirected in 5 seconds'));
+								setTimeout(function() {
+									document.location = 'tiki-index.php';
+								}, 5000);
+							}
+						} else { //errors
+							$.each(data, function(i) {
+								$.notify(data[i]);
+							});
 						}
-					} else {
-						$.each(data, function(i) {
-							$.notify(data[i]);
-						});
-					}
-				});
+					});
+				} else {
+					reg.trigger('continueToPurchase');
+				}
+
 				return false;
 			});
 JQ
@@ -119,7 +169,7 @@ JQ
 
 	$paymentStyle = '';
 	$registerStyle = '';
-	if (isset($_REQUEST['wp_member_users'])) {
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$registerStyle = 'display: none;';
 	} else {
 		$paymentStyle = 'display: none;';
