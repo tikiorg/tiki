1307a430a88a18e2503f8d6bb5bc2c91040b868f
lphuberdeau <lphuberdeau@users.sourceforge.net>
2012-08-09 14:56:13 +0000

    [NEW] {sign} plugin writes the current user and timestamp in the page (for discussion pages)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@42563 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_sign.php
@@ -0,0 +1,69 @@
+<?php
+// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+function wikiplugin_sign_info()
+{
+	return array(
+		'name' => tr('Signature'),
+		'description' => tr('By writing {sign} in a wiki page, it will be converted to your username with a timestamp.'),
+		'prefs' => array('wikiplugin_sign'),
+		'tags' => array('basic'),
+		'inline' => true,
+		'format' => 'html',
+		'params' => array(
+			'user' => array(
+				'required' => false,
+				'name' => tr('User'),
+				'description' => tr('Auto-generated, the username.'),
+				'default' => '',
+				'filter' => 'text',
+			),
+			'datetime' => array(
+				'required' => false,
+				'name' => tr('Date and time'),
+				'description' => tr('Auto-generated, the timestamp'),
+				'default' => '',
+				'filter' => 'text',
+			),
+		),
+	);
+}
+
+function wikiplugin_sign_rewrite($data, $params, $context)
+{
+	if (empty($params['user']) && empty($params['datetime'])) {
+		global $user;
+		$date = gmdate(DateTime::W3C);
+		return "{sign user=\"$user\" datetime=\"$date\"}";
+	}
+
+	return false;
+}
+
+function wikiplugin_sign($data, $params)
+{
+	if (empty($params['datetime'])) {
+		return false;
+	}
+
+	$user = isset($params['user']) ? $params['user'] : '';
+
+	$time = strtotime($params['datetime']);
+
+	if ($time === false) {
+		return false;
+	}
+
+	$smarty = TikiLib::lib('smarty');
+	$smarty->assign('sign', array(
+		'user' => $user,
+		'datetime' => $params['datetime'],
+		'time' => $time,
+	));
+	return $smarty->fetch('wiki-plugins/wikiplugin_sign.tpl');
+}
+
