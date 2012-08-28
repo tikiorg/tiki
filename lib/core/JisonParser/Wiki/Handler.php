5a38315dcd8ca335e5590346e6efc11e2254d4c6
robertplummer <robertplummer@users.sourceforge.net>
2012-08-28 21:14:40 +0000

    [ENH] Moved smiley handler to own parser
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@42789 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/JisonParser/Wiki/Handler.php
+++ lib/core/JisonParser/Wiki/Handler.php
@@ -37,6 +37,9 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 	/*hotWords parser */
 	public $hotWords;
 
+	/* smiley parser */
+	public $smileys;
+
 	//This var is used in both protectSpecialChars and unprotectSpecialChars to simplify the html ouput process
 	private $specialChars = array(
 		'≤REAL_LT≥' => array(
@@ -153,6 +156,10 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 			$this->Parser->hotWords = new JisonParser_Wiki_HotWords();
 		}
 
+		if (isset($this->Parser->smileys) == false) {
+			$this->Parser->smileys = new JisonParser_Wiki_Smileys();
+		}
+
 		parent::__construct();
 	}
 
@@ -264,7 +271,7 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 		}
 
 		if ($this->Parser->option['parseSmileys']) {
-			$this->parseSmileys($input);
+			$this->Parser->smileys->parse($input);
 		}
 
 		$this->restorePluginEntities($input);
@@ -350,13 +357,6 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 		return ($this->pluginStackCount > 0 || $this->npStack == true ? true : null);
 	}
 
-	static function getUnparsedPluginBodies($data)
-	{
-		$me = new self();
-		$me->parse($data);
-		return $me->plugins;
-	}
-
 	function getPluginNegotiator()
 	{
 		if (empty($this->pluginNegotiators[$this->wikiPluginParserNegotiatorClass])) {
@@ -370,7 +370,6 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 	{
 		$data = preg_replace('/§[a-z0-9]{32}§/','',$data);
 	}
-
 
 	function restorePluginEntities(&$input, $keep = false)
 	{
@@ -389,54 +388,6 @@ class JisonParser_Wiki_Handler extends JisonParser_Wiki
 
 		if (self::$parseDepth == 0) {
 			$this->getPluginNegotiator()->executeAwaiting($input);
-		}
-	}
-
-
-	function parseSmileys(&$input)
-	{
-		global $prefs;
-		static $patterns;
-
-		if ($prefs['feature_smileys'] == 'y') {
-			if (! $patterns) {
-				$patterns = array(
-					"/\(:([^:]+):\)/" => "<img alt=\"$1\" src=\"img/smiles/icon_$1.gif\" />",
-
-					// :) :-)
-					'/(\s|^):-?\)/' => "$1<img alt=\":-)\" title=\"".tra('smiling')."\" src=\"img/smiles/icon_smile.gif\" />",
-					// :( :-(
-					'/(\s|^):-?\(/' => "$1<img alt=\":-(\" title=\"".tra('sad')."\" src=\"img/smiles/icon_sad.gif\" />",
-					// :D :-D
-					'/(\s|^):-?D/' => "$1<img alt=\":-D\" title=\"".tra('grinning')."\" src=\"img/smiles/icon_biggrin.gif\" />",
-					// :S :-S :s :-s
-					'/(\s|^):-?S/i' => "$1<img alt=\":-S\" title=\"".tra('confused')."\" src=\"img/smiles/icon_confused.gif\" />",
-					// B) B-) 8-)
-					'/(\s|^)(B-?|8-)\)/' => "$1<img alt=\"B-)\" title=\"".tra('cool')."\" src=\"img/smiles/icon_cool.gif\" />",
-					// :'( :_(
-					'/(\s|^):[\'|_]\(/' => "$1<img alt=\":_(\" title=\"".tra('crying')."\" src=\"img/smiles/icon_cry.gif\" />",
-					// 8-o 8-O =-o =-O
-					'/(\s|^)[8=]-O/i' => "$1<img alt=\"8-O\" title=\"".tra('frightened')."\" src=\"img/smiles/icon_eek.gif\" />",
-					// }:( }:-(
-					'/(\s|^)\}:-?\(/' => "$1<img alt=\"}:(\" title=\"".tra('evil stuff')."\" src=\"img/smiles/icon_evil.gif\" />",
-					// !-) !)
-					'/(\s|^)\!-?\)/' => "$1<img alt=\"(!)\" title=\"".tra('exclamation mark !')."\" src=\"img/smiles/icon_exclaim.gif\" />",
-					// >:( >:-(
-					'/(\s|^)\>:-?\(/' => "$1<img alt=\"}:(\" title=\"".tra('frowning')."\" src=\"img/smiles/icon_frown.gif\" />",
-					// i-)
-					'/(\s|^)i-\)/' => "$1<img alt=\"(".tra('light bulb').")\" title=\"".tra('idea !')."\" src=\"img/smiles/icon_idea.gif\" />",
-					// LOL
-					'/(\s|^)LOL(\s|$)/' => "$1<img alt=\"(".tra('LOL').")\" title=\"".tra('laughing out loud !')."\" src=\"img/smiles/icon_lol.gif\" />$2",
-					// >X( >X[ >:[ >X-( >X-[ >:-[
-					'/(\s|^)\>[:X]-?\(/' => "$1<img alt=\">:[\" title=\"".tra('mad')."\" src=\"img/smiles/icon_mad.gif\" />",
-					// =D =-D
-					'/(\s|^)[=]-?D/' => "$1<img alt=\"=D\" title=\"".tra('Mr. Green laughing')."\" src=\"img/smiles/icon_mrgreen.gif\" />",
-				);
-			}
-
-			foreach ($patterns as $p => $r) {
-				$input = preg_replace($p, $r, $input);
-			}
 		}
 	}
 
