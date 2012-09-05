6b251df50fb8b0dbf3768f6a0c86aa1c7c8f8c7c
robertplummer <robertplummer@users.sourceforge.net>
2012-09-05 18:03:49 +0000

    [FIX] Just make sure that there is a header parameter to use
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@42839 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/WikiPlugin/maketoc.php
+++ lib/core/WikiPlugin/maketoc.php
@@ -35,12 +35,13 @@ class WikiPlugin_maketoc extends WikiPlugin_HtmlBase
 
 		if (isset($tikilib->is_slideshow) && $tikilib->is_slideshow == true) return '';
 		if (isset($killtoc) && $killtoc == true) return '';
+		if (isset($parser->header)) {
+			$result = '<div id="toctitle"><h3>' .
+				tr('Table of contents') .
+			'</h3></div>' .
+				$parser->header->toHtmlList('toc');
 
-		$result = '<div id="toctitle"><h3>' .
-			tr('Table of contents') .
-		'</h3></div>' .
-			$parser->header->toHtmlList('toc');
-
-		return $result;
+			return $result;
+		}
 	}
 }
