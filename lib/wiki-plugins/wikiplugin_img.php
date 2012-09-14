b7d7b66a3ffc0a636f9e926328598db9f70b40c6
robertplummer <robertplummer@users.sourceforge.net>
2012-09-14 14:13:48 +0000

    [FIX] src not taking
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@42960 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_img.php
+++ lib/wiki-plugins/wikiplugin_img.php
@@ -1372,9 +1372,9 @@ function wikiplugin_img( $data, $params )
 		} else {
 			$iconDisplayStyle = '';
 		}
-		$repl .= "<a href='tiki-edit_draw.php?fileId={$imgdata['fileId']}' onclick='return $(this).ajaxEditDraw();' title='".tr("Edit: Image") . " ".tr("(experimental)") . "'" .
+		$repl .= "<br /><a href='tiki-edit_draw.php?fileId={$imgdata['fileId']}' onclick='return $(this).ajaxEditDraw();' title='".tr("Edit: Image") . " ".tr("(experimental)") . "'" .
 					" class='editplugin pluginImgEdit{$imgdata['fileId']}' data-fileid='{$imgdata['fileId']}' data-galleryid='{$imgdata['galleryId']}'{$iconDisplayStyle} data-src='{$src}'>" .
-					"<br /><img width='16' height='16' class='icon' alt='Edit' src='img/icons/page_edit.png' /></a>";
+					"<img width='16' height='16' class='icon' alt='Edit' src='img/icons/page_edit.png' /></a>";
 	}
 	
 	return '~np~' . $repl. "\r" . '~/np~';
