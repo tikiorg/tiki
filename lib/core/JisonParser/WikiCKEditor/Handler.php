8f5b6e93d2ea2e9c6953850f53ce3f1b52638c18
robertplummer <robertplummer@users.sourceforge.net>
2012-09-25 14:16:53 +0000

    [FIX] Migrate to pluginNegotiator
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@43117 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/JisonParser/WikiCKEditor/Handler.php
+++ lib/core/JisonParser/WikiCKEditor/Handler.php
@@ -8,6 +8,8 @@ class JisonParser_WikiCKEditor_Handler extends JisonParser_Wiki_Handler
 			$this->Parser->htmlCharacter = new JisonParser_WikiCKEditor_HtmlCharacter($this->Parser);
 		}
 
+		$this->pluginNegotiator = new WikiPlugin_CKEditorNegotiator($this->Parser);
+
 		parent::__construct();
 	}
 
@@ -16,14 +18,5 @@ class JisonParser_WikiCKEditor_Handler extends JisonParser_Wiki_Handler
 		$this->Parser->option['is_html'] = true;
 
 		parent::setOption($option);
-	}
-
-	function getPluginNegotiator()
-	{
-		if (empty($this->pluginNegotiators['WikiPlugin_CKEditorNegotiator'])) {
-			$this->pluginNegotiators['WikiPlugin_CKEditorNegotiator'] = new WikiPlugin_CKEditorNegotiator($this->Parser);
-		}
-
-		return $this->pluginNegotiators['WikiPlugin_CKEditorNegotiator'];
 	}
 }
