9f1fa20634409b7f4aa605a046a393d4dca0b064
amette <amette@users.sourceforge.net>
2012-10-12 16:26:55 +0000

    [NEW] Very simple interface to view intrusion attempts detected by PHPIDS
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@43408 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ tiki-ids.php
@@ -0,0 +1,34 @@
+<?php
+// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+require_once ('tiki-setup.php');
+$access->check_permission('tiki_p_admin');
+
+$fp = fopen('temp/phpids_log.txt','r');
+$headers = array('ip', 'date', 'impact', 'tags', 'attackedParameters', 'request', 'serverIp');
+while (($line = fgets($fp)) !== false) {
+	$line = trim($line);
+	$intrusion = explode(',',$line);
+	$intrusions[] = array_combine($headers, $intrusion);
+}
+fclose($fp);
+
+foreach ($intrusions as &$value) {
+	$value['ip'] = trim($value['ip'], '"');
+	$value['tags'] = trim($value['tags'], '"');
+	$value['attackedParameters'] = trim($value['attackedParameters'], '"');
+	$value['attackedParameters'] = rawurldecode(urldecode($value['attackedParameters']));
+	$value['request'] = trim($value['request'], '"');
+	$value['request'] = rawurldecode(urldecode($value['request']));
+	$value['serverIp'] = trim($value['serverIp'], '"\n');
+}
+
+$intrusions = array_reverse($intrusions);
+
+$smarty->assign('intrusions', $intrusions);
+$smarty->assign('mid', 'tiki-ids.tpl');
+$smarty->display('tiki.tpl');
