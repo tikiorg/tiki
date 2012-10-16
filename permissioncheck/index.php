275fb6322a0c544ff3711c302f2afb3aef8a400a
fmg-sf <fmg-sf@users.sourceforge.net>
2012-10-16 18:27:49 +0000

    [NEW] detection of user, group and perms
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@43472 b456876b-0849-0410-b77d-98878d47e9d5

--- permissioncheck/index.php
+++ permissioncheck/index.php
@@ -5,7 +5,7 @@
 <meta name="robots" content="noindex, nofollow" />
 <title>Tiki Installation Permission Check</title>
 <style type="text/css">
-div.block {text-align: justify;}
+.block {text-align: justify;}
 </style>
 </head>
 <body>
@@ -19,6 +19,73 @@ div.block {text-align: justify;}
  </div>
  <p>PHP check: <?php
 		echo "PHP works";
+	?>
+ </p>
+ <p>permission check: <?php
+		$filename="index.php";
+		$user=(posix_getpwuid(fileowner($filename)));
+		$group=posix_getgrgid(filegroup($filename));
+		$perms_oct=substr(sprintf('%o', fileperms($filename)), -3);
+
+$perms = fileperms($filename);
+
+if (($perms & 0xC000) == 0xC000) {
+    // Socket
+    $info = 's';
+} elseif (($perms & 0xA000) == 0xA000) {
+    // Symbolic Link
+    $info = 'l';
+} elseif (($perms & 0x8000) == 0x8000) {
+    // Regular
+    $info = '-';
+} elseif (($perms & 0x6000) == 0x6000) {
+    // Block special
+    $info = 'b';
+} elseif (($perms & 0x4000) == 0x4000) {
+    // Directory
+    $info = 'd';
+} elseif (($perms & 0x2000) == 0x2000) {
+    // Character special
+    $info = 'c';
+} elseif (($perms & 0x1000) == 0x1000) {
+    // FIFO pipe
+    $info = 'p';
+} else {
+    // Unknown
+    $info = 'u';
+}
+
+// Owner
+$info .= (($perms & 0x0100) ? 'r' : '-');
+$info .= (($perms & 0x0080) ? 'w' : '-');
+$info .= (($perms & 0x0040) ?
+            (($perms & 0x0800) ? 's' : 'x' ) :
+            (($perms & 0x0800) ? 'S' : '-'));
+
+// Group
+$info .= (($perms & 0x0020) ? 'r' : '-');
+$info .= (($perms & 0x0010) ? 'w' : '-');
+$info .= (($perms & 0x0008) ?
+            (($perms & 0x0400) ? 's' : 'x' ) :
+            (($perms & 0x0400) ? 'S' : '-'));
+
+// World
+$info .= (($perms & 0x0004) ? 'r' : '-');
+$info .= (($perms & 0x0002) ? 'w' : '-');
+$info .= (($perms & 0x0001) ?
+            (($perms & 0x0200) ? 't' : 'x' ) :
+            (($perms & 0x0200) ? 'T' : '-'));
+
+//echo $info;
+
+		//$perms_asc=substr(sprintf(fileperms($filename)), -3);
+		$perms_asc=$info;
+		echo "this file "."<strong>".$filename."</strong>"." owned by ";
+		echo "user "."<strong>".$user["name"]."</strong>"." and group "."<strong>".$group["name"]."</strong>"." has got access permissions ";
+		echo "<strong>".$perms_asc."</strong>"." which is "."<strong>".$perms_oct."</strong>"." octal.";
+		echo "<br />\n";
+		//echo substr(sprintf('%o', fileperms($filename)), -3);
+		//echo "PHP works";
 	?>
  </p>
  <p class="block">
