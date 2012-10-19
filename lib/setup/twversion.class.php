c0a2da0f4bb2746cdc2975f31a16c971da73f9b6
yonixxx <yonixxx@users.sourceforge.net>
2012-10-19 19:38:10 +0000

    [Rel]Release 10 has a star name
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@43541 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -74,7 +74,8 @@ class TWVersion
 				17=>'Rigel',		// 6.x
 				18=>'Electra',		// 7.x
 				19=>'Acubens',		// 8.x
-				20=>'Herbig Haro'	// 9.x
+				20=>'Herbig Haro',	// 9.x
+				21=>'Sun'		// 10.x
 		);
 	}
 
