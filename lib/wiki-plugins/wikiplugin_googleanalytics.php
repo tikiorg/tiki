f126348b31614895da20665ad23d13a23c31ab4d
RobertPlummer <RobertPlummer@users.sourceforge.net>
2012-10-19 21:07:24 +0000

    [FIX] Google analytics needed updated, was failing
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@43544 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_googleanalytics.php
+++ lib/wiki-plugins/wikiplugin_googleanalytics.php
@@ -30,13 +30,18 @@ function wikiplugin_googleanalytics($data, $params)
 	if (empty($account)) {
 		return tra('Missing parameter');
 	}
-	$ret = '<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
-</script>
+	$ret = <<<JS
 <script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-'.$account.'");
-pageTracker._trackPageview();
-</script>';
-	return '~np~'.$ret.'~/np~';
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-$account']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+JS
+;
+	return "~np~" . $ret . "~/np~";
 }
