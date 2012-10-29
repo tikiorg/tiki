476788858c668666446361658975bfff72440876
jonnybradley <jonnybradley@users.sourceforge.net>
2012-10-29 19:09:06 +0000

    [bp/r43711][FIX] maketoc: Only match and process well formed maketocs
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@43712 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/parser/parserlib.php
+++ lib/parser/parserlib.php
@@ -2062,7 +2062,7 @@ if ( \$('#$id') ) {
 		if ( $options['ck_editor'] ) {
 			$need_maketoc = false ;
 		} else {
-			$need_maketoc = strpos($data, "{maketoc");
+			$need_maketoc = preg_match('/\{maketoc.*\}/', $data);
 		}
 
 		// Wysiwyg or allowhtml mode {maketoc} handling when not in editor mode (i.e. viewing)
@@ -2587,7 +2587,7 @@ if ( \$('#$id') ) {
 		 */
 		$new_data = '';
 		$search_start = 0;
-		if ( !$options['ck_editor']) {
+		if ( $need_maketoc && !$options['ck_editor']) {
 			while ( ($maketoc_start = strpos($data, "{maketoc", $search_start)) !== false ) {
 				$maketoc_length = strpos($data, "}", $maketoc_start) + 1 - $maketoc_start;
 				$maketoc_string = substr($data, $maketoc_start, $maketoc_length);
