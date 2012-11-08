5f70a176db67c37de1bedfd3c2c482a12a458a37
arildb <arildb@users.sourceforge.net>
2012-11-08 23:25:57 +0000

    [FIX] for installations using TortoiseSVN 1.7+. The SVN revision number is now displayed. Requires the "sqlite3" PHP extension. The last update time is still not set. Changed template to allow for display of revision only (thus last update time is unknown).
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@43846 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/last_update.php
+++ lib/setup/last_update.php
@@ -10,8 +10,37 @@ $access->check_script($_SERVER['SCRIPT_NAME'], basename(__FILE__));
 
 if (is_file('.svn/entries')) {
 	$svn = File('.svn/entries');
-	$smarty->assign('svnrev', isset($svn[3]) ? $svn[3] : '');
-	$smarty->assign('lastup', isset($svn[9]) ? strtotime($svn[9]) : '');
+	if(count($svn) > 2) {
+		// Standard SVN client
+		$smarty->assign('svnrev', $svn[3]);
+		$smarty->assign('lastup', strtotime($svn[9]));
+	} else {
+		// Check for Tortoise 1.7+ SVN client, if sqlite3 is present
+		if(extension_loaded('sqlite3')) {
+			$location = '.svn/wc.db';
+			if (is_file($location)) {
+				$handle = new SQLite3($location);
+
+				// Assign svnrev
+				$query = "select max(changed_revision) as svnrev from nodes";
+				$result = $handle->query($query); 
+				$resx = $result->fetchArray(SQLITE3_ASSOC);
+				$svnrev = $resx['svnrev'];
+				var_dump($svnrev);
+				$smarty->assign('svnrev', $svnrev);
+				
+				// Assign lastup
+				/* Must figure out how to convert changed_date(?) to a proper datetime
+				$query = "select max(changed_date) as lastup from nodes";
+				$result = $handle->query($query); 
+				$resx = $result->fetchArray(SQLITE3_ASSOC);
+				$lastupTime = $resx['lastup'];
+				var_dump($lastupTime);
+				$smarty->assign('lastup', $lastupTime);
+				*/
+			}
+		}
+	}
 }
 
 
