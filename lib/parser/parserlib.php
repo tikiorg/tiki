9c4f971b706f872583175cd55f8e80e7a7d88b35
robertplummer <robertplummer@users.sourceforge.net>
2012-11-12 15:04:22 +0000

    FIX] Added extra html special characters decode of plugin syntax, arg, and body to get legacy wysiwyg parsing of plugins
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/10.x@43905 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/parser/parserlib.php
+++ lib/parser/parserlib.php
@@ -1069,6 +1069,17 @@ if ( \$('#$id') ) {
 				$elem_style .= $m[1];
 			}
 		}
+
+		/*There was one extra html encode happening going to textarea, decoding it too early would lead to the plugin
+		not being detected, this fixes the over-encoded plugin body
+		*/
+		if (isset($_REQUEST['command']) && $_REQUEST['command'] == 'toHtmlFormat') {
+			$plugin_result = htmlspecialchars_decode($plugin_result);
+			$ck_editor_plugin = htmlspecialchars_decode($ck_editor_plugin);
+			$arg_str = htmlspecialchars_decode($arg_str);
+			$data = htmlspecialchars_decode($data);
+		}
+
 		$ret = '~np~<'.$elem.' class="tiki_plugin" plugin="' . $name . '" style="' . $elem_style . '"' .
 				' syntax="' . htmlentities($ck_editor_plugin, ENT_QUOTES, 'UTF-8') . '"' .
 				' args="' . htmlentities($arg_str, ENT_QUOTES, 'UTF-8') . '"' .
