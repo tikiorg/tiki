d979b3c346dd78c291e90747de932cfd5a8ea11b
arildb <arildb@users.sourceforge.net>
2012-11-18 21:54:19 +0000

    [MOD] Added check that script is run from a shell [ENH] Added command line arguments -x = exclude content reporting. Makes cleaner reports. [FIX] Time stat report formatting
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@44016 b456876b-0849-0410-b77d-98878d47e9d5

--- doc/devtools/htmlconvert.php
+++ doc/devtools/htmlconvert.php
@@ -1,8 +1,34 @@
 <?php
-
 require_once('tiki-setup.php');
 
+// Make sure script is run from a shell
+if (PHP_SAPI !== 'cli') {
+	die("Please run for a shell");
+}
+
+// Display command line options
+echo "Command line arguments\n";
+echo "  -x	eXclude the page contents from the dump\n";
+echo "\n";
+
+
+// Interpret command line
+
+// Help
+if (in_array('-?', $argv)) {
+	die;
+}
+
+// Hide content reporting
+$xcludeContent = false;
+if (in_array('-x', $argv)) {
+	echo "Hiding reported content.\n";
+	$xcludeContent = true;
+}
+
+// The test function
 function tf($wikiSyntax, &$startTime, &$endTime) {
+	global $xcludeContent;
 	//The new parser strips all \r and lets \n do all the line break work
 	$wikiSyntax = str_replace("\r", '', $wikiSyntax);
 
@@ -17,9 +43,10 @@ function tf($wikiSyntax, &$startTime, &$endTime) {
 	$endTime = getMicroTime();
 
 	$success =  $wikiSyntax == $wiki;
-
-	if($success == false) {
+	
+	if($success == false && !$xcludeContent) {
 		echo "\n";
+		echo "xcludeContent = ".$xcludeContent."\n";
 		echo '"' . $wikiSyntax . '"';
 		echo "\n---------------------" . mb_detect_encoding($wikiSyntax) . "-------------------------\n";
 		echo $html;
@@ -65,6 +92,7 @@ foreach($pages as &$page) {
 	$totalTime = $endTime - $startTime;
 	$totalElapsedTime += $totalTime;
 	echo " (Elapsed msec: ".$totalTime.")\n";
+
 }
 echo "\n------------- Statistics ---------------------------------\n";
 
@@ -73,9 +101,12 @@ echo "\nSUCCESS: ".$cntSUCCESS."\n";
 echo "FAILURE: ".$cntFAILURE."\n";
 
 // Time spent
-$avgTimePerPage = $totalElapsedTime / $pageCount;
-echo "\nTotal elapsed msec: ".$totalElapsedTime;
-echo "Avg msec/page: ".$totalElapsedTime."\n";
+$avgTimePerPage = "N/A";
+if($idx > 0) {
+	$avgTimePerPage = $totalElapsedTime / $idx;
+}
+echo "\nTotal elapsed msec: ".$totalElapsedTime."\n";
+echo "Avg msec/page: ".$avgTimePerPage."\n";
 
 
 // @return current micro time in milli-seconds
