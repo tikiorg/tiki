6423f1c5bba0ba79e14f45489c4ea70cc4a33d08
arildb <arildb@users.sourceforge.net>
2012-12-01 17:22:02 +0000

    [FIX] Catch invalid PDF
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@44154 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-print.php
+++ tiki-print.php
@@ -82,16 +82,19 @@ if (isset($_REQUEST['display']) && $_REQUEST['display'] == 'pdf') {
 	require_once 'lib/pdflib.php';
 	$generator = new PdfGenerator();
 	$pdf = $generator->getPdf('tiki-print.php', array('page' => $page));
-
-	header('Cache-Control: private, must-revalidate');
-	header('Pragma: private');
-	header("Content-Description: File Transfer");
-	header('Content-disposition: attachment; filename="'. $page. '.pdf"');
-	header("Content-Type: application/pdf");
-	header("Content-Transfer-Encoding: binary");
-	header('Content-Length: '. strlen($pdf));
-	echo $pdf;
-
+	if(empty($pdf)) {
+		echo "Unable to generate PDF";
+	} else {
+		$length = strlen($pdf);
+		header('Cache-Control: private, must-revalidate');
+		header('Pragma: private');
+		header("Content-Description: File Transfer");
+		header('Content-disposition: attachment; filename="'. $page. '.pdf"');
+		header("Content-Type: application/pdf");
+		header("Content-Transfer-Encoding: binary");
+		header('Content-Length: '. $length);
+		echo $pdf;
+	}
 } else {
 	$smarty->display('tiki-print.tpl');
 }
