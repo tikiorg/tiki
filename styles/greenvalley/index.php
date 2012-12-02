b411d31478e7ab3f3adee4837ce26d4d13b99433
yonixxx <yonixxx@users.sourceforge.net>
2012-12-02 14:38:21 +0000

    [NEW]New theme kind of nature working fine also with rtl languages
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@44168 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ styles/greenvalley/index.php
@@ -0,0 +1,10 @@
+<?php
+// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: index.php 40086 2012-03-08 15:47:37Z changi67 $
+
+// This redirects to the sites root to prevent directory browsing
+header("location: ../index.php");
+die;
