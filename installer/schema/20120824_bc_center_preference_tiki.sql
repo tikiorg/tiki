098bcff50a9272ce25c96549515440ece27931be
marclaporte <marclaporte@users.sourceforge.net>
2012-12-06 07:17:57 +0000

    Partial revert of r42708. Now that :: is no longer expropriated as a namespace separator (we are using :_: instead), there is no longer a need to change the several years old syntax for centered text. This was already done for the the Jison parser in r44111
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/10.x@44197 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/20120824_bc_center_preference_tiki.sql
+++
@@ -1,3 +0,0 @@
-
--- Upgrade only: Set the center tag character to :: unless it was manually set to ::: (which is the new default)
-INSERT IGNORE INTO tiki_preferences (name, value) VALUES ('feature_use_three_colon_centertag', 'n');
