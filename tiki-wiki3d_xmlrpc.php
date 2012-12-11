dd883f753bd112f65a1482160aa860629b907b69
changi67 <changi67@users.sourceforge.net>
2012-12-11 10:48:41 +0000

    Revision: 44247           http://tikiwiki.svn.sourceforge.net/tikiwiki/?rev=44247&view=rev 			 Author:   kstingel 			 Date:     2012-12-11 00:09:37 +0000 (Tue, 11 Dec 2012) 			 Log Message: 			 ----------- 			 [REF] added "@package tikiwiki" page level Docblock, working towards fixing PHPDoc inspection problems
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@44247 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-wiki3d_xmlrpc.php
+++ tiki-wiki3d_xmlrpc.php
@@ -1,4 +1,7 @@
-<?php 
+<?php
+/**
+ * @package tikiwiki
+ */
 // (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
