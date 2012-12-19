d18b3cecac37b1fc3bcc90db0f6f13d3c9fc4274
changi67 <changi67@users.sourceforge.net>
2012-12-19 20:46:35 +0000

    [FIX] Wrong url
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/10.x@44327 b456876b-0849-0410-b77d-98878d47e9d5

--- doc/devtools/svntools.php
+++ doc/devtools/svntools.php
@@ -1,12 +1,12 @@
 <?php
 // (c) Copyright 2002-2010 by authors of the Tiki Wiki/CMS/Groupware Project
-// 
+//
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
 
 define('SVN_MIN_VERSION', 1.3);
-define('TIKISVN', 'https://tikiwiki.svn.sourceforge.net/svnroot/tikiwiki');
+define('TIKISVN', 'https://svn.code.sf.net/p/tikiwiki/code');
 
 function full($relative)
 {
@@ -34,7 +34,7 @@ function color($string, $color)
 }
 
 function error($message)
-{ 
+{
 	die(color($message, 'red') . "\n");
 }
 
@@ -73,7 +73,7 @@ function is_valid_merge_source($destination, $source)
 
 	if (is_experimental($destination))
 		return is_trunk($source);
-	
+
 	return false;
 }
 
@@ -108,10 +108,10 @@ function update_working_copy($localPath, $ignore_externals = false)
 function has_uncommited_changes($localPath)
 {
 	$localPath = escapeshellarg($localPath);
-	
+
 	$dom = new DOMDocument;
 	$dom->loadXML(`svn status --xml $localPath`);
-	
+
 	$xp = new DOMXPath($dom);
 	$count = $xp->query("/status/target/entry/wc-status[@item = 'added' or @item = 'conflicted' or @item = 'deleted' or @item = 'modified' or @item = 'replaced']");
 
@@ -121,10 +121,10 @@ function has_uncommited_changes($localPath)
 function get_conflicts($localPath)
 {
 	$localPath = escapeshellarg($localPath);
-	
+
 	$dom = new DOMDocument;
 	$dom->loadXML(`svn status --xml $localPath`);
-	
+
 	$xp = new DOMXPath($dom);
 	$list = $xp->query("/status/target/entry/wc-status[@item = 'conflicted']");
 
@@ -135,7 +135,7 @@ function find_last_merge($path, $source)
 {
 	$short = preg_quote(short($source), '/');
 	$pattern = "/^\\[(MRG|BRANCH)\\].*$short'?\s+\d+\s+to\s+(\d+)/";
-		
+
 	$descriptorspec = array(
 		0 => array('pipe', 'r'),
 		1 => array('pipe', 'w'),
