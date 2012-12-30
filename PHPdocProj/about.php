47833fa6418613e17dc340628922ac202feee27a
kstingel <kstingel@users.sourceforge.net>
2012-12-30 19:16:12 +0000

    [REF] converted comment blocks to DocBlocks ready for PHPdocumentor parser
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/users/kstingel@44399 b456876b-0849-0410-b77d-98878d47e9d5

--- PHPdocProj/about.php
+++ PHPdocProj/about.php
@@ -1,69 +1,36 @@
 <?php
 /**
- * this page is used as doxygen output mainpage, content is pasted in from README.
+ * Tiki Wiki CMS Groupware is Free and Open Source Software (FOSS).
+ * It is a community-driven project which exists and improves thanks to the participation of
+ * people just like YOU.
  *
- * @package tikiwiki
- */
-// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
-// 
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+ * IMPORTANT LINKS
+ * ---------------
+ * - {@link http://info.tiki.org General information & overview}
+ * - {@link http://doc.tiki.org Official documentation}
+ * - {@link http://tiki.org/tiki-forums.php Forums & community}
+ * - {@link http://dev.tiki.org Developers}
+ * - {@link http://themes.tiki.org Themes & styling}
+ * - {@link http://security.tiki.org Security}
+ * - {@link http://irc.tiki.org/ IRC chat channel: #tikiwiki on irc.freenode.net}
+ *
+ * INSTALLATION
+ * ------------
+ * Please see the INSTALL file in this folder for basic installation instructions.
+ * For more details, please see: {@see http://doc.tiki.org/Installation}
+ *
+ * COPYRIGHT
+ * ---------
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ *
+ * ... Have fun!
+ *
+ *
+ * @package Tikiwiki
+ * @copyright Copyright (c) 2002-2008, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+ * @licence Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+*/
 // $Id$
-
-/*
-  this page is used as doxygen output mainpage,
-  content is pasted in from README.
-*/
-
-/**
-\mainpage
-
-
-Tiki Wiki CMS Groupware is Free and Open Source Software (FOSS). It is a community-driven project which exists and improves thanks to the participation of people just like YOU.
-
-
-IMPORTANT LINKS
----------------
-* General information & overview
-  http://info.tiki.org
-
-* Official documentation
-  http://doc.tiki.org
-
-* Forums & community
-  http://tiki.org/tiki-forums.php
-
-* Developers
-  http://dev.tiki.org
-
-* Themes & styling
-  http://themes.tiki.org
-
-* Security
-  http://security.tiki.org
-
-* IRC chat channel: #tikiwiki on irc.freenode.net
-  http://irc.tiki.org/
-
-
-
-INSTALLATION
-------------
-Please see the INSTALL file in this folder for basic installation instructions.
-For more details, please see:
-http://doc.tiki.org/Installation
-
-
-
-COPYRIGHT
----------
-Copyright (c) 2002-2008, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.All
-Rights Reserved. See copyright.txt for details and a complete list of authors.
-Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-
-
-... Have fun!
-*/
 
 // I call index.php because tiki may not be setup when people attempt to call this.
 header("location: index.php");
