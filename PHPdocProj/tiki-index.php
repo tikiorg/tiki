2f69358eed06b59d9490431c44d32c4f24e715de
kstingel <kstingel@users.sourceforge.net>
2013-01-03 19:42:59 +0000

    [REF] changes to comment blocks only .. addition of DocBlock comments
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/users/kstingel@44421 b456876b-0849-0410-b77d-98878d47e9d5

--- PHPdocProj/tiki-index.php
+++ PHPdocProj/tiki-index.php
@@ -1,8 +1,13 @@
 <?php
-// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
-//
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+/**
+ * Tiki Entry point.
+ *
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+ *
+ * @package Tikiwiki
+ * @copyright (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
+ */
 // $Id$
 
 $inputConfiguration = array(
