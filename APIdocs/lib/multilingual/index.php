2fac08de1ca9c570e25649e7c1a68c7349f8180c
kstingel <kstingel@users.sourceforge.net>
2013-01-04 17:22:49 +0000

    [REF] converted initial comment block (Id line left as comment) to PageLevel DocBlock ready for API Doc generation.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/users/kstingel@44434 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ APIdocs/lib/multilingual/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This redirects to the site's root to prevent directory browsing.
+ *
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ *
+ * @package \lib
+ * @subpackage multilingual
+ * @copyright (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
+ * @licence Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+ */
+// $Id: index.php 40086 2012-03-08 15:47:37Z changi67 $
+
+header("location: ../../index.php");
+die;
