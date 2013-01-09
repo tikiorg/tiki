cea969352917139e873fd4a560ce551ac3ad4232
kstingel <kstingel@users.sourceforge.net>
2013-01-09 10:18:12 +0000

    [REF] created page-level DocBlock
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/users/kstingel@44472 b456876b-0849-0410-b77d-98878d47e9d5

--- API-trunk/xmlrpc.php
+++ API-trunk/xmlrpc.php
@@ -1,8 +1,14 @@
 <?php
-// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
-//
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+/**
+ * provides XML functions for Tiki.
+ * 
+ * Used by Blog feature.
+ *
+ * @package TikiWiki
+ * @subpackage \
+ * @copyright (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project. All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * @licence Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+ */
 // $Id$
 
 include_once('tiki-setup.php');
