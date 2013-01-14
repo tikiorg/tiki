032811537c5fdc8833a2f59811003ebbc74ada7a
marclaporte <marclaporte@users.sourceforge.net>
2013-01-14 03:48:00 +0000

    Remove the note about experimental and make label clearer
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@44512 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_img.php
+++ lib/wiki-plugins/wikiplugin_img.php
@@ -1373,7 +1373,7 @@ function wikiplugin_img( $data, $params )
 			} else {
 				$iconDisplayStyle = '';
 			}
-			$repl .= "<br /><a href='tiki-edit_draw.php?fileId={$imgdata['fileId']}' onclick='return $(this).ajaxEditDraw();' title='".tr("Edit: Image") . " ".tr("(experimental)") . "'" .
+			$repl .= "<br /><a href='tiki-edit_draw.php?fileId={$imgdata['fileId']}' onclick='return $(this).ajaxEditDraw();' title='".tr("Draw on the Image") . " " . "'" .
 						" class='editplugin pluginImgEdit{$imgdata['fileId']}' data-fileid='{$imgdata['fileId']}' data-galleryid='{$dbinfo['galleryId']}'{$iconDisplayStyle}>" .
 						"<img width='16' height='16' class='icon' alt='Edit' src='img/icons/page_edit.png' /></a>";
 		}
