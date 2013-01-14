83ac37bc499331569237c6d482be5ac5cee124e2
jyhem <jyhem@users.sourceforge.net>
2013-01-14 16:13:00 +0000

    [ENH] Allow Lucene rebuilding with other permissions than Anonymous
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@44522 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/search/shell.php
+++ lib/search/shell.php
@@ -8,19 +8,24 @@
 if ( isset($_SERVER['REQUEST_METHOD']) ) die;
 
 if ( !isset( $_SERVER['argv'][1] ) || !in_array($_SERVER['argv'][1], array('rebuild','process','optimize')))
-	die( 'Usage: php lib/search/shell.php command [option]
+	die( 'Usage: [searchuser=<username>] php lib/search/shell.php command [option]
 	Where command [option] can be:
 		rebuild [log]
 		process [integer (default 10)]
 		optimize
 	Returns an error code (1) if search is already being rebuilt
 	N.B. Needs to be run as the "apache" user, e.g. > "sudo -u www-data php lib/search/shell.php process 20"
+	N.B. By default, executes with the Anonymous permissions but this can be overridden, e.g. > "sudo -u www-data searchuser=admin php lib/search/shell.php rebuild"
 ' );
 
 if ( ! file_exists('db/local.php') )
 	die( "Tiki is not installed yet.\n" );
 
 require_once('tiki-setup.php');
+
+if( $user = getenv('searchuser') ) {
+	echo "Execute with same permissions as user: " . $user . "\n";
+}
 
 include_once 'lib/core/Zend/Log/Writer/Syslog.php';
 $log_level = Zend_Log::INFO;
