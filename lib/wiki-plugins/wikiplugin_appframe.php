deaba57234ba90ea276255c6effdf39aaf9ccf7d
marclaporte <marclaporte@users.sourceforge.net>
2013-01-15 14:56:05 +0000

    [bp/r44526] [fix] variables shouldn't be inside translatable strings (see http://dev.tiki.org/Strings+Format+Convention)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/10.x@44529 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_appframe.php
+++ lib/wiki-plugins/wikiplugin_appframe.php
@@ -1,5 +1,5 @@
 <?php
-// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+// (c) Copyright 2002-2012 by authors of the Tiki Wiki CMS Groupware Project
 //
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -407,7 +407,7 @@ function wikiplugin_appframe_template($data, $params, $start)
 		$smarty->assign('input', $data);
 		return $smarty->fetch($file);
 	} catch (SmartyException $e) {
-		return tr('Template file not found: ' . $file);
+		return tr('Template file not found: %0', $file);
 	}
 }
 
