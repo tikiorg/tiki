322c31215c9ff69b7742a23d31fb958f7b24faca
luciash <luciash@users.sourceforge.net>
2013-01-21 20:59:18 +0000

    [NEW] WikiPlugin Pref: simple plugin to allow global preference check and display content depending on the condition
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@44578 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_pref.php
@@ -0,0 +1,40 @@
+<?php
+// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: wikiplugin_userpref.php 44444 2013-01-05 21:24:24Z changi67 $
+
+function wikiplugin_pref_info()
+{
+	return array(
+		'name' => tra('Preference'),
+		'documentation' => 'PluginPref',
+		'description' => tra('Display contents based on global preferences'),
+		'body' => tra('Wiki text to display if conditions are met. The body may contain {ELSE}. Text after the marker will be displayed if not matching the conditions.'),
+		'prefs' => array('wikiplugin_pref'),
+		'filter' => 'wikicontent',
+		'extraparams' => true,
+		'icon' => 'img/icons/wrench.png',
+		'params' => array(
+		),
+	);
+}
+
+function wikiplugin_pref($data, $params)
+{
+	global $prefs, $tikilib;
+	$dataelse = '';
+	if (strpos($data, '{ELSE}')) {
+		$dataelse = substr($data, strpos($data, '{ELSE}')+6);
+		$data = substr($data, 0, strpos($data, '{ELSE}'));
+	}
+
+	$else = false;
+	foreach ($params as $prefName=>$prefValue) {
+		if ($tikilib->get_preference($prefName) != $prefValue) {
+			return $dataelse;
+		}
+	}
+	return $data;
+}
