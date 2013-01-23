f5651e1e9f91410e3bddbc4f9a0b4e899306120b
marclaporte <marclaporte@users.sourceforge.net>
2013-01-23 20:15:47 +0000

    updating version number
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@44604 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '9.3';	// needs to have no spaces for releases
+		$this->version 	= '9.4svn';	// needs to have no spaces for releases
 		$this->star	= 'Herbig Haro';
 		$this->releases	= array();
 
