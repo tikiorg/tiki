db0b9617c55c17e062d9d9618067145536d33bb0
kstingel <kstingel@users.sourceforge.net>
2013-01-24 02:07:42 +0000

    [REF] added missing page level DocBlock
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/users/kstingel@44607 b456876b-0849-0410-b77d-98878d47e9d5

--- API-trunk/doc/devtools/index.php
+++ API-trunk/doc/devtools/index.php
@@ -1,10 +1,14 @@
 <?php
-// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
-// 
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+/**
+ * This redirects to the site's root to prevent directory browsing.
+ *
+ * @ignore
+ * @package    Tiki
+ * @copyright  (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+ * @license    LGPL. See licence.txt for more details
+ */
 // $Id$
 
-// This redirects to the sites root to prevent directory browsing
+// redirect
 header("location: ../../tiki-index.php");
 die;
--- API-trunk/lang/index.php
+++ API-trunk/lang/index.php
@@ -1,8 +1,12 @@
 <?php
-// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
-// 
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+/**
+ * This redirects to the site's root to prevent directory browsing.
+ *
+ * @ignore
+ * @package    Tiki
+ * @copyright  (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+ * @license    LGPL. See licence.txt for more details
+ */
 // $Id$
 
 header('location: ../tiki-index.php');
