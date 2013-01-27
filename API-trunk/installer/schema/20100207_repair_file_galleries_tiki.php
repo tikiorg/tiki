9da1037020cedba95cac3f822c2a57f7e3a51ebe
kstingel <kstingel@users.sourceforge.net>
2013-01-27 19:45:21 +0000

    [REF] added generic page-level DocBlock to php files (same comment for all files)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/users/kstingel@44643 b456876b-0849-0410-b77d-98878d47e9d5

--- API-trunk/installer/schema/20100207_repair_file_galleries_tiki.php
+++ API-trunk/installer/schema/20100207_repair_file_galleries_tiki.php
@@ -1,8 +1,12 @@
 <?php
-// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
-// 
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+/**
+ * used by Tiki's installer, you should not modify this file.
+ *
+ * @package   Tiki
+ * @subpackage Installer
+ * @copyright (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project. All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * @license   LGPLv2.1. See license.txt for more details
+ */
 // $Id$
 
 if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
