17bd4ea845ca1e453b9efbb69789dc9a9b87c96d
kstingel <kstingel@users.sourceforge.net>
2013-01-27 18:19:42 +0000

    [REF] added missing page level DocBlock
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/users/kstingel@44638 b456876b-0849-0410-b77d-98878d47e9d5

--- API-trunk/lib/videogals/index.php
+++ API-trunk/lib/videogals/index.php
@@ -1,10 +1,14 @@
 <?php
-// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
-// 
-// All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+/**
+ * This redirects to the site's root to prevent directory browsing.
+ *
+ * @ignore
+ * @package    Tiki
+ * @copyright  (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+ * @license    LGPLv2.1. See license.txt for more details
+ */
 // $Id$
 
-// This redirects to the sites root to prevent directory browsing
+// redirect
 header("location: ../../index.php");
 die;
