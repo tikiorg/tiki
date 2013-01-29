440117091732c0c01900cbb66261e47ac7611cd5
kstingel <kstingel@users.sourceforge.net>
2013-01-29 16:29:01 +0000

    [REF] replaced @package tikiwiki with @package Tiki
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/users/kstingel@44671 b456876b-0849-0410-b77d-98878d47e9d5

--- API-trunk/metrics-tab.php
+++ API-trunk/metrics-tab.php
@@ -1,6 +1,9 @@
 <?php
 /**
- * @package tikiwiki
+ *
+ *
+ * @package   Tiki
+ * @copyright (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project. All Rights Reserved. See copyright.txt for details and a complete list of authors.
  */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
