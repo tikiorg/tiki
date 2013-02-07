e8b7dd47dc9bbe88727ceabc2d25430929450dd5
pkdille <pkdille@users.sourceforge.net>
2013-02-07 20:46:30 +0000

    [REL] Preparing 6.10 release
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/10.x@44814 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -24,10 +24,10 @@ class TWVersion
 		//   stable   : Represents stable releases.
 		//   unstable : Represents candidate and test/development releases.
 		//   trunk     : Represents next generation development version.
-		$this->branch 	= 'unstable';
+		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '10.1svn';	// Never ever put any spaces in this value.  This will appear in tiki-admin.php?page=general
+		$this->version 	= '10.1';	// Never ever put any spaces in this value.  This will appear in tiki-admin.php?page=general
 		$this->star	= 'Sun';
 		$this->releases	= array();
 
@@ -154,6 +154,7 @@ class TWVersion
 				'6.7',
 				'6.8',
 				'6.9',
+				'6.10',
 				'7.0beta1',
 				'7.0beta2',
 				'7.0RC1',
@@ -182,6 +183,7 @@ class TWVersion
 				'10.0alpha',
 				'10.0beta',
 				'10.0',
+				'10.1',
 			);
 	}
 
