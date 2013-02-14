86401a4be846719ecd3da8497b02cac2b3da64de
jonnybradley <jonnybradley@users.sourceforge.net>
2013-02-14 16:52:10 +0000

    [ENH] trackertoggle: General revamp and fixes. Use ins_xxx field names, implement itemId param (was unused), use onkeyup event for inputs instead of onchange, cope with unquoted values and give better feedback on setup problems (thanks marclaporte)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@44872 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_trackertoggle.php
+++ lib/wiki-plugins/wikiplugin_trackertoggle.php
@@ -18,28 +18,32 @@ function wikiplugin_trackertoggle_info()
 				'name' => tra('Field ID'),
 				'description' => tra('Numeric value representing the field ID tested.'),
 				'filter' => 'digits',
-				'default' => '',
 			),
 			'value' => array(
 				'required' => true,
 				'name' => tra('Value'),
 				'description' => tra('Value to compare against.'),
+				'filter' => 'text',
 			),
 			'visible' => array(
 				'required' => false,
 				'name' => tra('Element visibility'),
 				'description' => 'y|n' . tra('If y, is visible when the field has the value.'),
+				'filter' => 'alpha',
 				'default' => 'n'
 			),
 			'id' => array(
 				'required' => true,
 				'name' => tra('ID'),
 				'description' => tra('Html ID of the element that is toggled'),
+				'filter' => 'text',
 			),
 			'itemId' => array(
 				'required' => false,
 				'name' => tra('Item ID'),
 				'description' => tra('Use the field of specific item. The URL param itemId is used if this parameter is not set.'),
+				'filter' => 'digits',
+				'default' => 0,
 			),
 		),
 	);
@@ -47,30 +51,59 @@ function wikiplugin_trackertoggle_info()
 
 function wikiplugin_trackertoggle($data, $params)
 {
-	global $trklib; include_once('lib/trackers/trackerlib.php');
-	global $headerlib; include_once('lib/headerlib.php');
+	global $trklib; TikiLib::lib('tracker');
+	global $headerlib; TikiLib::lib('header');
+
 	$default = array('visible' => 'n');
 	$params = array_merge($default, $params);
 	extract($params, EXTR_SKIP);
+
+	if (empty($fieldId)) {
+		TikiLib::lib('errorreport')->report(tr('trackertoggle: Param fieldId is required.'));
+	}
 	$field = $trklib->get_tracker_field($fieldId);
 	if (empty($field)) {
-		return tra('Incorrect param');
+		TikiLib::lib('errorreport')->report(tr('trackertoggle: Param fieldId field %0 does not exsist.', $fieldId));
 	}
+
+	if (!isset($value)) {
+		TikiLib::lib('errorreport')->report(tr('trackertoggle: Param value is required'));
+	}
+	if (empty($id)) {
+		TikiLib::lib('errorreport')->report(tr('trackertoggle: Param id is required'));
+	}
+
 	if (empty($itemId) && !empty($_REQUEST['itemId'])) {
 		$itemId = $_REQUEST['itemId'];
 	}
-	$htmlFieldId = "track_$fieldId";
+
 	$action = $visible == 'y'? 'show': 'hide';
 	$anti = $visible == 'y'? 'hide': 'show';
-	if ($field['type'] == 'c') {
-		$extension = ':checked';
-		$value = $value == 'y'? 'undefined': "'on'";
+	if (!empty($itemId)) {
+		$fieldValue = $trklib->get_item_value(0, $itemId, $fieldId);
+		if ($fieldValue === $value) {
+			$jq = "\$('#$id').$action();";
+		} else {
+			$jq = "\$('#$id').$anti();";
+		}
 	} else {
-		$extension = '';
+		$htmlFieldName = "ins_$fieldId";
+
+		if ($field['type'] == 'c') {
+			$extension = ':checked';
+			$value = $value == 'y'? 'undefined': "'on'";
+			$trigger = 'change';
+		} else {
+			$extension = '';
+			$trigger = 'keyup';
+			if (!is_numeric($value) && ! preg_match('/^[\'"].*[\'"]$/', $value)) {
+				$value = "'$value'";		// add quotes if not already quoted and not a number
+			}
+		}
+		$htmltype = $field['type'] == 'a'? 'textarea': 'input';
+		$jq = "if (\$('".$htmltype."[name=$htmlFieldName]$extension').val() == $value) {\$('#$id').$action();} else {\$('#$id').$anti();}";
+		$jq = "\$('".$htmltype."[name=$htmlFieldName]').$trigger(function () { $jq }).trigger('$trigger');\n";
 	}
-	$htmltype = $field['type'] == 'a'? 'textarea': 'input';
-	$jq = "if (\$('".$htmltype."[name=$htmlFieldId]$extension').val() == $value) {\$('#$id').$action();} else {\$('#$id').$anti();}";
-	$jq .= "\$('".$htmltype."[name=$htmlFieldId]').change(function () { $jq });";
 
 	$headerlib->add_jq_onready($jq);
 }
