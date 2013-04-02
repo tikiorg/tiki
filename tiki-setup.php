d9f272a2c6ca07ec94101501b1100a6091d8d25e
jonnybradley <jonnybradley@users.sourceforge.net>
2013-04-02 16:44:41 +0000

    [FIX] jquery: Remove include of r5 hoverIntent and replace with v7 (jq 1.9 compatible) version from new superfish plugin. Also make it independent of tooltips.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@45390 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup.php
+++ tiki-setup.php
@@ -423,7 +423,6 @@ if ($prefs['mobile_feature'] === 'y' && $prefs['mobile_mode'] === 'y') {
 	}
 
 	if ( $prefs['feature_jquery_tooltips'] == 'y' ) {
-		$headerlib->add_jsfile('lib/jquery/plugins/cluetip/lib/jquery.hoverIntent.js');
 		if ( $prefs['feature_jquery_ui'] !== 'y' ) {
 			$headerlib->add_jsfile('lib/jquery/plugins/cluetip/lib/jquery.bgiframe.min.js');
 		}
@@ -434,6 +433,9 @@ if ($prefs['mobile_feature'] === 'y' && $prefs['mobile_mode'] === 'y') {
 	if ( $prefs['feature_jquery_superfish'] == 'y' ) {
 		$headerlib->add_jsfile('lib/jquery/plugins/superfish/js/superfish.js');
 		$headerlib->add_jsfile('lib/jquery/plugins/superfish/js/supersubs.js');
+	}
+	if ( $prefs['feature_jquery_tooltips'] === 'y' || $prefs['feature_jquery_superfish'] === 'y' ) {
+		$headerlib->add_jsfile('lib/jquery/plugins/superfish/js/hoverIntent.js');
 	}
 	if ( $prefs['feature_jquery_reflection'] == 'y' ) {
 		$headerlib->add_jsfile('lib/jquery/plugins/reflection-jquery/js/reflection.js');
