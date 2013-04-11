7e6d39e379a3ad08a3a20228b15a2bf18e8fabae
lphuberdeau <lphuberdeau@users.sourceforge.net>
2013-04-11 20:54:04 +0000

    [MOD] Adding substraction and division, for convenience
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@45445 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Tracker/Field/Math.php
+++ lib/core/Tracker/Field/Math.php
@@ -3,7 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: GeographicFeature.php 44444 2013-01-05 21:24:24Z changi67 $
+// $Id$
 
 /**
  * Handler to perform a calculation for the tracker entry.
