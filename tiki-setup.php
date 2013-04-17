1773493dcaddf24690dd5683717510db1ff8cb1e
jonnybradley <jonnybradley@users.sourceforge.net>
2013-04-17 22:48:09 +0000

    [MOD] Moving colorbox to composer
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@45545 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup.php
+++ tiki-setup.php
@@ -462,8 +462,8 @@ if ($prefs['mobile_feature'] === 'y' && $prefs['mobile_mode'] === 'y') {
 		}
 	}
 	if ( $prefs['feature_shadowbox'] == 'y' ) {
-		$headerlib->add_jsfile('lib/jquery/plugins/colorbox/jquery.colorbox.js');
-		$headerlib->add_cssfile('lib/jquery/plugins/colorbox/css/colorbox.css');
+		$headerlib->add_jsfile('vendor/jquery/plugins/colorbox/jquery.colorbox.js');
+		$headerlib->add_cssfile('vendor/jquery/plugins/colorbox/example1/colorbox.css');
 	}
 	if ( $prefs['feature_jquery_carousel'] == 'y' ) {
 		$headerlib->add_jsfile('lib/jquery/plugins/infinitecarousel/jquery.infinitecarousel3.js');
