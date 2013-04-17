24efc8c1cc724a68f7cc15da0af520f5b54b50a7
nkoth <nkoth@users.sourceforge.net>
2013-04-17 05:27:34 +0000

    [FIX][ROLLBACK] Rollback r44785 because performance enhancement leads to unmpredictable weird results
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@45534 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Tracker/Field/ItemsList.php
+++ lib/core/Tracker/Field/ItemsList.php
@@ -119,7 +119,7 @@ class Tracker_Field_ItemsList extends Tracker_Field_Abstract
 	{
 		$items = $this->getItemIds();
 
-		$list = $this->getItemLabels($items, true);
+		$list = $this->getItemLabels($items);
 		$listtext = implode(' ', $list);
 
 		return array(
@@ -182,7 +182,7 @@ class Tracker_Field_ItemsList extends Tracker_Field_Abstract
 		return $items;
 	}
 
-	private function getItemLabels($items, $quick = false)
+	private function getItemLabels($items)
 	{
 		$displayFields = $this->getOption(3);
 		$trackerId = (int) $this->getOption(0);
@@ -196,7 +196,7 @@ class Tracker_Field_ItemsList extends Tracker_Field_Abstract
 		$list = array();
 		$trklib = TikiLib::lib('trk');
 		foreach ($items as $itemId) {
-			if ($displayFields && ! $quick) {
+			if ($displayFields) {
 				$list[$itemId] = $trklib->concat_item_from_fieldslist($trackerId, $itemId, $displayFields, $status, ' ');
 			} else {
 				$list[$itemId] = $trklib->get_isMain_value($trackerId, $itemId);
