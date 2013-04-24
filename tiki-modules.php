66aa96d88c160376d834a2939a214e7efb64b01b
lindonb <lindonb@users.sourceforge.net>
2013-04-24 01:09:02 +0000

    [FIX] Load CSS files needed for specific modules (so far just calendar modules) as loading from module php or template files doesn't work since r44022. By placing in tiki-modules, CSS won't be loaded unless module is assigned, although will still be loaded even if module doesn't show due to permissions.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@45709 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-modules.php
+++ tiki-modules.php
@@ -31,6 +31,7 @@ if (Perms::get()->admin) {
 
 $show_columns = array_fill_keys(array_keys($modules), 'n');
 
+$modnames = array();
 foreach ( $modules as $zone => & $moduleList ) {
 	if ($prefs['feature_fullscreen'] != 'y' || empty($_SESSION['fullscreen']) || $_SESSION['fullscreen'] != 'y' ||
 			strpos($zone, 'page') === 0) {	// pagetop and pagebottom zones appear in fullscreen
@@ -45,9 +46,20 @@ foreach ( $modules as $zone => & $moduleList ) {
 					return $modlib->execute_module($ref);
 				}
 			);
+			$modnames[$ref['name']] = '';
 		}
 
 		$smarty->assign($zone, $moduleList);
+	}
+}
+
+//add necessary css files to header as required for specific modules
+//TODO only add css when module will actually be showing
+$cssadd = array_intersect_key($modlib->cssfiles, $modnames);
+if (count($cssadd > 0)) {
+	$headerlib = TikiLib::lib('header');
+	foreach ($cssadd as $add) {
+		$headerlib->add_cssfile($add['csspath'], $add['rank']);
 	}
 }
 
