7ba3ac877ffda07a14f56c097aee4f028a9bdcad
marclaporte <marclaporte@users.sourceforge.net>
2013-05-01 20:15:46 +0000

    Adding fullcalendar-resourceviews 1.5.3.4 to vendor_extra/fullcalendar-resourceviews for now to avoid any package issues.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/11.x@45833 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_trackercalendar.php
+++ lib/wiki-plugins/wikiplugin_trackercalendar.php
@@ -52,8 +52,8 @@ function wikiplugin_trackercalendar($data, $params)
 {
 	static $id = 0;
 	$headerlib = TikiLib::lib('header');
-	$headerlib->add_cssfile('vendor/jarnokurlin/fullcalendar/fullcalendar/fullcalendar.css');
-	$headerlib->add_jsfile('vendor/jarnokurlin/fullcalendar/fullcalendar/fullcalendar.min.js');
+	$headerlib->add_cssfile('vendor_extra/fullcalendar-resourceviews/fullcalendar/fullcalendar.css');
+	$headerlib->add_jsfile('vendor_extra/fullcalendar-resourceviews/fullcalendar/fullcalendar.min.js');
 
 	$jit = new JitFilter($params);
 	$definition = Tracker_Definition::get($jit->trackerId->int());
