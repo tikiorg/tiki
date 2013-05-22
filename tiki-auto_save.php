0ae14de0c20107930b6a88925f328fb1eb987767
xavidp <xavidp@users.sourceforge.net>
2013-05-22 08:50:23 +0000

    [bp/r46017][FIX]auto_save: Check the user doing the save or preview has permission and is the one currently editing the page. Thanks Marcela Bénétrix Also modernise library use and fix some missing and/or unset vars. Thanks jonnyb
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@46024 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-auto_save.php
+++ tiki-auto_save.php
@@ -37,19 +37,29 @@ function send_ajax_response($command, $data )
 }
 
 if (isset($_REQUEST['editor_id'])) {
+	global $user;
+	$tikilib = TikiLib::lib('tiki');
+	$editlib = TikiLib::lib('edit');
+	$smarty = TikiLib::lib('smarty');
+
+
 	if (isset($_REQUEST['command']) && isset($_REQUEST['data']) && $_REQUEST['data'] != 'ajax error') {
-		if (!isset($_REQUEST['referer']))
+		if (!isset($_REQUEST['referer'])) {
 			$_REQUEST['referer'] =  '';
+		}
 		$referer = explode(':', $_REQUEST['referer']);	// user, section, object id
 		if ($referer && count($referer) === 3 && $referer[1] === 'wiki_page') {
 			$page = rawurldecode($referer[2]);	// plugins use global $page for approval
+
+			if (!Perms::get('wiki page', $page)->edit || $user != $tikilib->get_semaphore_user($page)) {
+				send_ajax_response($_REQUEST['command'], '');
+			}
 		}
-		
+		$res = '';
+
 		if ($_REQUEST['command'] == 'toWikiFormat') {
-			global $editlib; include_once 'lib/wiki/editlib.php';
 			$res = $editlib->parseToWiki(urldecode($_REQUEST['data']));
 		} else if ($_REQUEST['command'] == 'toHtmlFormat') {
-			global $editlib; include_once 'lib/wiki/editlib.php';
 			$res = $editlib->parseToWysiwyg(urldecode($_REQUEST['data']), false, !empty($_REQUEST['allowhtml']) ? $_REQUEST['allowhtml'] : false);
 		} else if ($_REQUEST['command'] == 'auto_save') {
 			include_once 'lib/ajax/autosave.php';
@@ -64,13 +74,18 @@ if (isset($_REQUEST['editor_id'])) {
 		}
 		send_ajax_response($_REQUEST['command'], $res);
 	} else if (isset($_REQUEST['autoSaveId'])) {	// wiki page previews
-		
+
 		$autoSaveIdParts = explode(':', $_REQUEST['autoSaveId']);	// user, section, object id
 		foreach ($autoSaveIdParts as & $part) {
 			$part = urldecode($part);
 		}
-		
+
 		$page = $autoSaveIdParts[2];	// plugins use global $page for approval
+
+		if (!Perms::get('wiki page', $page)->edit || $user != $tikilib->get_semaphore_user($page)) {
+			send_ajax_response($_REQUEST['command'], '');
+		}
+
 		$info = $tikilib->get_page_info($page, false);
 		if (isset($_REQUEST['allowHtml']) || empty($info)) {
 			$info['is_html'] = !empty($_REQUEST['allowHtml'])? 1 : 0;
@@ -81,15 +96,13 @@ if (isset($_REQUEST['editor_id'])) {
 		$options = array('is_html' => ($info['is_html'] == 1), 'preview_mode' => true, 'process_wiki_paragraphs' => ($prefs['wysiwyg_htmltowiki'] === 'y' || $info['wysiwyg'] == 'n'), 'page' => $autoSaveIdParts[2]);
 
 		if (count($autoSaveIdParts) === 3 && !empty($user) && $user === $autoSaveIdParts[0] && $autoSaveIdParts[1] === 'wiki_page') {
-			
-			$editlib; include_once 'lib/wiki/editlib.php';
+
 			if (isset($_REQUEST['inPage'])) {
 				if (!isset($_REQUEST['diff_style'])) {	// use previously set diff_style
 					$_REQUEST['diff_style'] = isset($_COOKIE['preview_diff_style']) ? $_COOKIE['preview_diff_style'] : '';
 				}
 				$data = $editlib->partialParseWysiwygToWiki(get_autosave($_REQUEST['editor_id'], $_REQUEST['autoSaveId']));
 				$smarty->assign('diff_style', $_REQUEST['diff_style']);
-				global $tikilib;
 				if (!empty($_REQUEST['diff_style'])) {
 					$info = $tikilib->get_page_info($autoSaveIdParts[2]);
 					if (!empty($info)) {
@@ -127,8 +140,8 @@ if (isset($_REQUEST['editor_id'])) {
 				echo $data;
 				
 			} else {					// popup window
-				$headerlib->add_js(
-								'function get_new_preview() {
+				TikiLib::lib('header')->add_js(
+					'function get_new_preview() {
 		$("body").css("opacity", 0.6);
 		location.replace("' . $tikiroot . 'tiki-auto_save.php?editor_id=' . $_REQUEST['editor_id'] . '&autoSaveId=' . $_REQUEST['autoSaveId'] . '");
 	}
