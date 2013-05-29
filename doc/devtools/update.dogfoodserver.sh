1efbf792d95fba5e72a42c47a5891672534e64a9
marclaporte <marclaporte@users.sourceforge.net>
2013-05-29 17:39:34 +0000

    typo and todo for dogfoodserver script
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@46087 b456876b-0849-0410-b77d-98878d47e9d5

--- doc/devtools/update.dogfoodserver.sh
+++ doc/devtools/update.dogfoodserver.sh
@@ -6,6 +6,7 @@
 # $Id: update.dogfoodserver.sh 41172 2012-04-29 15:25:16Z changi67 $
 
 # TODO: Handle local file gal and wiki_up
+# TODO: update to using new Console commands
 
 #Production 
 DOCROOTOLDVERSION="public_html/8x"
@@ -13,7 +14,7 @@ DOCROOTDOGFOODVERSION="publib_html/9x"
 
 OLDMYSQLDB="changi_8x"
 DOGFOODMYSQLDB="changi_9x"
-#Be carefull, this user need to have the right to drop the database.
+#Be careful, this user need to have the right to drop the database.
 MYSQLUSER="changi"
 MYSQLPASS="changi"
 MYSQLCOMMAND="mysql -u $MYSQLUSER -p $MYSQLPASS"
