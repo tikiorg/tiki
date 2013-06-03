99dd6aaf0c1d2b5f264903cce3e6f68e27a0fc92
jyhem <jyhem@users.sourceforge.net>
2013-06-03 12:12:14 +0000

    [FIX] When blog is configured to let other authors post, let them view unpublished and private posts
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@46142 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-view_blog_post.php
+++ tiki-view_blog_post.php
@@ -62,13 +62,18 @@ if ($user && $user == $blog_data["user"]) {
 	$ownsblog = 'y';
 }
 
-if ($ownsblog == 'n' && $tiki_p_blog_admin != 'y' && $post_info["priv"] == 'y') {
+$ownspost = 'n';
+if ($user && $user == $post_info["user"]) {
+	$ownspost = 'y';
+}
+
+if ($ownspost == 'n' && $ownsblog == 'n' && $tiki_p_blog_admin != 'y' && $post_info["priv"] == 'y') {
 	$smarty->assign('errortype', 401);
 	$smarty->assign('msg', tra("You do not have permission to view this blog post while it is marked as private"));
 	$smarty->display("error.tpl");
 	die;
 }
-if ($ownsblog == 'n' && $tiki_p_blog_admin != 'y' && $post_info['created'] > $tikilib->now) {
+if ($ownspost == 'n' && $ownsblog == 'n' && $tiki_p_blog_admin != 'y' && $post_info['created'] > $tikilib->now) {
 	$smarty->assign('errortype', 401);
 	$smarty->assign('msg', tra('Permission denied'));
 	$smarty->display("error.tpl");
