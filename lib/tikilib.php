tikiorg/tiki

commit a64be105defb6bd98818ee68cd1ebd1304644cb0

[FIX] get_style_path: Allow custom.css and js in styles or styles/tikidomain dir to be used
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/11.x@46108 b456876b-0849-0410-b77d-98878d47e9d5

lib/tikilib.php
@@ -5539,9 +5539,6 @@ class TikiLib extends TikiDb_Bridge
 		if (!empty($stl)) {
 			$sbase = $this->get_style_base($stl).'/';
 		}
-		if (!is_dir('styles/'.$dbase.$sbase)) {	// if the style dir doesn't exist in tikidomain, use root/styles
-			$dbase = '';
-		}
 
 		$obase = '';
 		if (!empty($opt)) {
@@ -5551,25 +5548,28 @@ class TikiLib extends TikiDb_Bridge
 			}
 		}
 
-		if (is_dir('styles/'.$dbase.$sbase)) {
-			if (empty($filename)) {
-				if (is_dir('styles/'.$dbase.$sbase.$obase)) {
-					$path = 'styles/'.$dbase.$sbase.$obase;
-				} else {
-					$path = 'styles/'.$dbase.$sbase;	// fall back to "parent" style dir if no option one
-				}
+		if (empty($filename)) {
+			if (is_dir('styles/'.$dbase.$sbase.$obase)) {
+				$path = 'styles/'.$dbase.$sbase.$obase;
 			} else {
-				if (is_file('styles/'.$dbase.$sbase.$obase.$filename)) {
-					$path = 'styles/'.$dbase.$sbase.$obase.$filename;
-				} else if (is_file('styles/'.$dbase.$sbase.$filename)) {	// try "parent" style dir if no option one
-					$path = 'styles/'.$dbase.$sbase.$filename;
-				} else if (is_file('styles/'.$sbase.$obase.$filename)) {	// try non-tikidomain dirs if not found
-					$path = 'styles/'.$sbase.$obase.$filename;
-				} else if (is_file('styles/'.$sbase.$filename)) {
-					$path = 'styles/'.$sbase.$filename;	// fall back to "parent" style dir if no option one
-				}
+				$path = 'styles/'.$dbase.$sbase;	// fall back to "parent" style dir if no option one
+			}
+		} else {
+			if (is_file('styles/'.$dbase.$sbase.$obase.$filename)) {
+				$path = 'styles/'.$dbase.$sbase.$obase.$filename;
+			} else if (is_file('styles/'.$dbase.$sbase.$filename)) {	// try "parent" style dir if no option one
+				$path = 'styles/'.$dbase.$sbase.$filename;
+			} else if (is_file('styles/'.$sbase.$obase.$filename)) {	// try non-tikidomain dirs if not found
+				$path = 'styles/'.$sbase.$obase.$filename;
+			} else if (is_file('styles/'.$sbase.$filename)) {
+				$path = 'styles/'.$sbase.$filename;				// fall back to "parent" style dir if no option
+			} else if (is_file('styles/'.$dbase.$filename)) {
+				$path = 'styles/'.$dbase.$filename;				// tikidomain root style dir?
+			} else if (is_file('styles/'.$dbase.$filename)) {
+				$path = 'styles/'.$filename;					// root style dir?
 			}
 		}
+
 		return $path;
 	}
 

commit 0087bdc394dc783828421db7a1e153fd3650c408

[FIX] get_style_path: Try non tikidomain "styles" dir if style or option dirs not found (regression from r46108)
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/11.x@46161 b456876b-0849-0410-b77d-98878d47e9d5

lib/tikilib.php
@@ -5551,8 +5551,12 @@ class TikiLib extends TikiDb_Bridge
 		if (empty($filename)) {
 			if (is_dir('styles/'.$dbase.$sbase.$obase)) {
 				$path = 'styles/'.$dbase.$sbase.$obase;
+			} else if (is_dir('styles/'.$dbase.$sbase)) {
+				$path = 'styles/'.$dbase.$sbase;	// try "parent" style dir if no option one
+			} else if (is_dir('styles/'.$sbase.$obase)) {
+				$path = 'styles/'.$sbase.$obase;	// try root style dir if no domain one
 			} else {
-				$path = 'styles/'.$dbase.$sbase;	// fall back to "parent" style dir if no option one
+				$path = 'styles/'.$sbase;			// fall back to "parent" style dir if no option one
 			}
 		} else {
 			if (is_file('styles/'.$dbase.$sbase.$obase.$filename)) {