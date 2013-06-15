da38a607b6e506412a24f5c0f8c9bda72b8aa29f
changi67 <changi67@users.sourceforge.net>
2013-06-15 19:46:27 +0000

    [FIX] This directory doesn't exist and cause a Uncaught exception
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/11.x@46338 b456876b-0849-0410-b77d-98878d47e9d5

--- get_strings.php
+++ get_strings.php
@@ -3,27 +3,27 @@
  * @package tikiwiki
  */
 // (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
-// 
+//
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
 
 /**
  * Update lang/xx/language.php files
- * 
+ *
  * Examples:
  * 		- http://localhost/pathToTiki/get_strings.php -> update all language.php files
  * 		- http://localhost/pathToTiki/get_strings.php?lang=fr -> update just lang/fr/language.php file
  * 		- http://localhost/pathToTiki/get_strings.php?lang[]=fr&lang[]=pt-br&outputFiles -> update both French
  * 		  and Brazilian Portuguese language.php files and for each string add a line with
  * 		  the file where it was found.
- * 
+ *
  * Command line examples:
  * 		- php get_strings.php
  * 		- php get_strings.php lang=pt-br outputFiles=true
  * 		- php get_strings.php baseDir=lib/ excludeDirs=lib/core/Zend,lib/captcha includeFiles=captchalib.php,index.php fileName=language_r.php
- * 
- * Note: baseDir and fileName parameters are available in command line mode only 
+ *
+ * Note: baseDir and fileName parameters are available in command line mode only
  *
  *
  * If you want to know the translation progression for your language, just visit : http://i18n.tiki.org/status
@@ -56,7 +56,7 @@ if ($request->hasProperty('outputFiles')) {
 
 $excludeDirs = array(
 	'dump' , 'img', 'lang',
-	 'lib/jquery', 'vendor', 'vendor_extra',
+	 'vendor', 'vendor_extra',
 	 'lib/test',	'temp',
 	'temp/cache',	'templates_c',
 );
@@ -69,20 +69,20 @@ $includeFiles = array(
 if (php_sapi_name() == 'cli') {
 	if ($request->hasProperty('baseDir')) {
 		$options['baseDir'] = $request->getProperty('baseDir');
-		
+
 		// when a custom base dir is set, default $includeFiles and $excludeDirs are not used
 		$includeFiles = array();
 		$excludeDirs = array();
 	}
-	
+
 	if ($request->hasProperty('excludeDirs')) {
 		$excludeDirs = explode(',', $request->getProperty('excludeDirs'));
 	}
-	
+
 	if ($request->hasProperty('includeFiles')) {
 		$includeFiles = explode(',', $request->getProperty('includeFiles'));
 	}
-	
+
 	if ($request->hasProperty('fileName')) {
 		$options['fileName'] = $request->getProperty('fileName');
 	}
@@ -93,7 +93,7 @@ $getStrings = new Language_GetStrings(new Language_CollectFiles, new Language_Wr
 $getStrings->addFileType(new Language_FileType_Php);
 $getStrings->addFileType(new Language_FileType_Tpl);
 
-// skip the following directories 
+// skip the following directories
 $getStrings->collectFiles->setExcludeDirs($excludeDirs);
 
 // manually add the following files from skipped directories
