f4541a444431279aa0b298d18de9458062b5bce7
changi67 <changi67@users.sourceforge.net>
2013-06-27 09:00:43 +0000

    [REL] Closing 6.11
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/6.x@46498 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -26,7 +26,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '6.11';
+		$this->version 	= '6.12svn';
 		$this->star	= 'Rigel';
 		$this->releases	= array();
 
