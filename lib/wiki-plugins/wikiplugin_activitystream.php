785b2ef68ddc0cd70493cd62f9cf5b42080bcba8
lphuberdeau <lphuberdeau@users.sourceforge.net>
2013-07-08 15:05:14 +0000

    [NEW] Adding basic activity stream plugin
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@46591 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_activitystream.php
@@ -0,0 +1,56 @@
+<?php
+// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+function wikiplugin_activitystream_info()
+{
+	return array(
+		'name' => tra('Activity Stream'),
+		'documentation' => 'PluginActivityStream',
+		'description' => tra('Generates a feed or activity stream based on the recorded events in the system.'),
+		'prefs' => array('wikiplugin_activitystream', 'activity_custom_events'),
+		'default' => 'y',
+		'body' => tra('List configuration information'),
+		'filter' => 'wikicontent',
+		'profile_reference' => 'search_plugin_content',
+		'icon' => 'img/icons/text_list_bullets.png',
+		'tags' => array('advanced'),
+		'params' => array(
+		),
+	);
+}
+
+function wikiplugin_activitystream($data, $params)
+{
+	$unifiedsearchlib = TikiLib::lib('unifiedsearch');
+
+	$alternate = null;
+	$output = null;
+
+	$query = new Search_Query;
+	$query->filterType('activity');
+
+	$matches = WikiParser_PluginMatcher::match($data);
+
+	$builder = new Search_Query_WikiBuilder($query);
+	$builder->apply($matches);
+
+	$query->setOrder('modification_date_desc');
+
+	if (! $index = $unifiedsearchlib->getIndex()) {
+		return '';
+	}
+
+	$result = $query->search($index);
+
+	$paginationArguments = $builder->getPaginationArguments();
+	$formatter = new Search_Formatter(new Search_Formatter_Plugin_SmartyTemplate('templates/activity/activitystream.tpl'));
+	$formatter->setDataSource($unifiedsearchlib->getDataSource());
+	$out = $formatter->format($result);
+
+	return $out;
+}
+
