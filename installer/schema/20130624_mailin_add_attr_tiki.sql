8516de74440faceeb6a41e7c580e1c47487e5d2f
arildb <arildb@users.sourceforge.net>
2013-07-16 12:24:17 +0000

    [MOD] Remove the AFTER column clauses from the update
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@46708 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/20130624_mailin_add_attr_tiki.sql
+++ installer/schema/20130624_mailin_add_attr_tiki.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `tiki_mailin_accounts` add COLUMN `routing` char(1) NULL AFTER `attachments`;
-ALTER TABLE `tiki_mailin_accounts` add COLUMN `leave_email` char(1) NULL AFTER `respond_email`;
+ALTER TABLE `tiki_mailin_accounts` add COLUMN `routing` char(1) NULL;
+ALTER TABLE `tiki_mailin_accounts` add COLUMN `leave_email` char(1) NULL;
