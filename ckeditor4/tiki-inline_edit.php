72de8b8670a479a394de964facd6a3cd9cd71358
arildb <arildb@users.sourceforge.net>
2013-07-20 19:59:02 +0000

    [ADD] missing file
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@46747 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ ckeditor4/tiki-inline_edit.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @package tikiwiki
+ */
+// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: tiki-accounting.php 44444 2013-01-05 21:24:24Z changi67 $
+
+
+// $section = 'accounting';
+require_once ('tiki-setup.php');
+
+/*
+global $prefs;
+
+// Activate inline editor	
+if ($prefs['feature_wysiwyg_inline'] == 'y') {
+	include_once('lib/ckeditor_tiki/wysiwyglib.php');
+	$wysiwyglib->setUpInlineEditor();		// init ckeditor if default editor
+}
+*/
+
+if ($_SESSION['edit_wysiwyg_inline'] === 'y') {
+	$_SESSION['edit_wysiwyg_inline'] = 'n';
+	include_once('lib/ckeditor_tiki/wysiwyglib.php');
+	$wysiwyglib->shutdownInlineEditor();
+} else {
+	$_SESSION['edit_wysiwyg_inline'] = 'y';
+}
+header('Location: '.$_SERVER['HTTP_REFERER']);
+
