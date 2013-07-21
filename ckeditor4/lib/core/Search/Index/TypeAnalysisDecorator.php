76e16b88e43e44fe7e3f23e5c0218de3fc88f350
arildb <arildb@users.sourceforge.net>
2013-07-21 19:15:55 +0000

    [MRG] Merged latest trunk from rev 45105-latest Add missing files
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@46753 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ ckeditor4/lib/core/Search/Index/TypeAnalysisDecorator.php
@@ -0,0 +1,69 @@
+<?php
+// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: TypeAnalysisDecorator.php 46397 2013-06-17 18:35:39Z lphuberdeau $
+
+class Search_Index_TypeAnalysisDecorator implements Search_Index_Interface
+{
+	private $parent;
+	private $identifierClass;
+	private $mapping = array();
+
+	function __construct(Search_Index_Interface $index)
+	{
+		$this->parent = $index;
+		$this->identifierClass = get_class($index->getTypeFactory()->identifier(1));
+	}
+
+	function addDocument(array $document)
+	{
+		$new = array_diff_key($document, $this->mapping);
+		foreach ($new as $key => $value) {
+			$this->mapping[$key] = $value instanceof $this->identifierClass;
+		}
+		return $this->parent->addDocument($document);
+	}
+
+	function invalidateMultiple(array $query)
+	{
+		return $this->parent->invalidateMultiple($query);
+	}
+
+	function endUpdate()
+	{
+		return $this->parent->endUpdate();
+	}
+
+	function find(Search_Query_Interface $query, $resultStart, $resultCount)
+	{
+		return $this->parent->find($query, $resultStart, $resultCount);
+	}
+
+	function getTypeFactory()
+	{
+		return $this->parent->getTypeFactory();
+	}
+
+	function optimize()
+	{
+		return $this->parent->optimize();
+	}
+
+	function destroy()
+	{
+		return $this->parent->destroy();
+	}
+
+	function exists()
+	{
+		return $this->parent->exists();
+	}
+
+	function getIdentifierFields()
+	{
+		return array_keys(array_filter($this->mapping));
+	}
+}
+
