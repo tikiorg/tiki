1875242826bb39f63dbae94cb2ecfb5fa4d2b490
jonnybradley <jonnybradley@users.sourceforge.net>
2013-07-30 14:41:30 +0000

    [FIX] services: Make js encoding correct and consistent (and non-deprecated)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@46912 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup.php
+++ tiki-setup.php
@@ -589,7 +589,7 @@ if ($prefs['feature_sefurl'] != 'y') {
 		}
 
 		return "tiki-ajax_services.php?" + $.map(query, function (v, k) {
-			return k + "=" + tiki_encodeURI(v);
+			return k + "=" + tiki_encodeURIComponent(v);
 		}).join("&");
 	};'
 	);
