4a9bfb0f9471b260940d69ecb715cdf29d123587
pascalstjean <pascalstjean@users.sourceforge.net>
2013-08-02 23:50:51 +0000

    [NEW] New plugin to allow over 20 file types to be viewed inline using Google Document Viewer
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@46978 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_webdocviewer.php
@@ -0,0 +1,71 @@
+<?php
+// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: wikiplugin_youtube.php 46682 2013-07-15 03:44:44Z pascalstjean $
+
+function wikiplugin_webdocviewer_info()
+{
+	return array(
+		'name' => tra('Web Document Viewer'),
+		'documentation' => 'PluginWebDocViewer',
+		'description' => tra('Display files found in a gallery or from a URL in an embedded document viewer'),
+		'prefs' => array( 'wikiplugin_webdocviewer' ),
+		'tags' => array( 'basic' ),		
+		'params' => array(
+			'fileId' => array(
+				'required' => false,
+				'name' => tra('File Id'),
+				'description' => tra('The FileId of a file in a File Gallery of the file you wish to embed in the viewer.'),
+			),
+			'url' => array(
+				'required' => false,
+				'name' => tra('URL'),
+				'description' => tra('The URL of the file you wish to embed in the viewer. If the file is stored in File Galleries, please use the fileId parameter'),
+			),
+			'width' => array(
+				'required' => false,
+				'name' => tra('Width'),
+				'description' => tra('Width in pixels'),
+				'filter' => 'digits',
+				'default' => 425,
+			),
+			'height' => array(
+				'required' => false,
+				'name' => tra('Height'),
+				'description' => tra('Height in pixels'),
+				'filter' => 'digits',
+				'default' => 350,
+			),
+		),
+	);
+}
+
+function wikiplugin_webdocviewer($data, $params)
+{
+	global $tikilib;
+	global $tikipath, $tikiroot;
+	
+ 	$plugininfo = wikiplugin_webdocviewer_info();
+ 	foreach ($plugininfo['params'] as $key => $param) {
+ 		$default["$key"] = $param['default'];
+ 	}
+	$params = array_merge($default, $params);
+	
+	
+	if(isset($params['fileId'])){
+		$url = $_SERVER['HTTP_HOST'].$tikiroot.'tiki-download_file.php?fileId='.$params['fileId'];
+	} else if(isset($params['url'])){
+		$url = $params['url'];
+	}
+	print($url);
+	if(isset($url)){
+		$iframe = ('<iframe src="http://docs.google.com/viewer?embedded=true&url='.$url.'" width="'.$params['width'].'" height="'.$params['height'].'" style="border: none;"></iframe>');
+		return '~np~' . $iframe . '~/np~';
+	} else { 
+		return '~np~'. tra('No FileId or URL has been set'). '~/np~';
+	}
+		
+	
+}
