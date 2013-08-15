d801ad38d5dc520322cea194cbe9702a4c8a3d5c
marclaporte <marclaporte@users.sourceforge.net>
2013-08-15 22:09:52 +0000

    https://dev.tiki.org/How+to+release#Update_lib_setup_twversion.class.php_after_the_release
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/11.x@47095 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '11.0';	// needs to have no spaces for releases
+		$this->version 	= 'SVN pre 11.1';	// needs to have no spaces for releases
 		$this->star	= 'TBA';
 		$this->releases	= array();
 
