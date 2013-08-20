e3251cea633949691401cf7042c804a7046658fa
pascalstjean <pascalstjean@users.sourceforge.net>
2013-08-20 19:25:58 +0000

    [MOD] Added blog post as example in parameter types
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@47167 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_category.php
+++ lib/wiki-plugins/wikiplugin_category.php
@@ -26,7 +26,7 @@ function wikiplugin_category_info()
 			'types' => array(
 				'required' => false,
 				'name' => tra('Types'),
-				'description' => tra('List of object types to include in the list separated by plus signs. ex: article+blog+faq+fgal<br />+forum+igal+newsletter<br />+event+poll+quiz+survey<br />+tracker+wiki+img'),
+				'description' => tra('List of object types to include in the list separated by plus signs. ex: article+blog+blog post+faq+fgal<br />+forum+igal+newsletter<br />+event+poll+quiz+survey<br />+tracker+wiki+img'),
 				'filter' => 'text',
 				'default' => '*',
 			),
