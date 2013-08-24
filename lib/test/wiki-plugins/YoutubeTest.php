31a7c8519ff145c34e85feba5a17c514d154f284
changi67 <changi67@users.sourceforge.net>
2013-08-24 12:39:16 +0000

    [ADD] More tests for youtube
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@47252 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/test/wiki-plugins/YoutubeTest.php
+++ lib/test/wiki-plugins/YoutubeTest.php
@@ -1,6 +1,6 @@
 <?php
 // (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
-// 
+//
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
@@ -23,6 +23,8 @@ class WikiPlugin_YoutubeTest extends PHPUnit_Framework_TestCase
 			array('', '^Plugin YouTube error: the movie parameter is empty.'),
 			array('', '~np~<iframe src="//www.youtube.com/embed/bPHuY7QL568?" frameborder="0" width="425" height="350"></iframe>~/np~', array('movie' => 'http://www.youtube.com/watch?v=bPHuY7QL568')),
 			array('', '~np~<iframe src="//www.youtube.com/embed/deby_Yb1-ac?" frameborder="0" width="425" height="350"></iframe>~/np~', array('movie' => 'https://www.youtube.com/watch?v=deby_Yb1-ac')),
+			array('', '~np~<iframe src="//www.youtube.com/embed/deby_Yb1-ac?" frameborder="0" width="425" height="350"></iframe>~/np~', array('movie' => 'https://youtu.be/deby_Yb1-ac')),
+			array('', '~np~<iframe src="//www.youtube-nocookie.com/embed/deby_Yb1-ac?" frameborder="0" width="425" height="350"></iframe>~/np~', array('movie' => 'https://youtu.be/deby_Yb1-ac'), 'privacyEnhanced' => 'y'),
 		);
 	}
 }
