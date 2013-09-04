74d1aafa2dba9a7803e81f8833b5c1ed848e38c7
nkoth <nkoth@users.sourceforge.net>
2013-09-04 04:25:09 +0000

    [FIX] Followers need to be array_merge as is numeric keys to avoid disappearing items
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@47340 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Search/GlobalSource/SocialSource.php
+++ lib/core/Search/GlobalSource/SocialSource.php
@@ -38,7 +38,7 @@ class Search_GlobalSource_SocialSource implements Search_GlobalSource_Interface
 			$groups = array_merge($groups, $this->userlib->get_user_groups_inclusion($user));
 			$userfollowers = $this->getFollowers($user);
 			if (is_array($userfollowers)) {
-				$followers += $this->getFollowers($user);
+				$followers = array_merge($followers, $userfollowers);	
 			}
 		}
 
