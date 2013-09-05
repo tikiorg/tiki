ad5f059315283eb161ce176cb5dc435b60701980
jonnybradley <jonnybradley@users.sourceforge.net>
2013-09-05 15:59:47 +0000

    [FIX] prefs: Rebuild the prefs search index when the main index is rebuilt (takes an extra 10 or so seconds)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@47354 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Search/Indexer.php
+++ lib/core/Search/Indexer.php
@@ -62,6 +62,8 @@ class Search_Indexer
 		$this->log('Starting optimization');
 		$this->searchIndex->optimize();
 		$this->log('Finished optimization');
+		$this->log('Rebuilding prefs index');
+		TikiLib::lib('prefs')->rebuildIndex();
 		$this->log('Finished rebuild');
 		return $stat;
 	}
