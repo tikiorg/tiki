a547da8311d1d9236268717ba4056079ea4b3230
arildb <arildb@users.sourceforge.net>
2013-09-17 17:09:47 +0000

    [FIX] The admin wizard onLogin function must only be called, when a user is actually logged in. This is not the case right after a console.php database:install command. User must change the password first, before the login is done and the admin wizard is run.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@47524 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-login.php
+++ tiki-login.php
@@ -443,9 +443,12 @@ $url.= ((strpos($url, '?') === false) ? '?' : '&') . SID;
 
 // Check if a wizard should be run.
 // If a wizard is run, it will return to the $url location when it has completed. Thus no code after $wizardlib->onLogin will be executed
-require_once('lib/wizard/wizardlib.php');
-$wizardlib = new WizardLib();
-$wizardlib->onLogin($user, $url);
+// The user must be actually logged in before onLogin is called. If $isdue is set, then: "Note that the user is not logged in he's just validated to change his password"
+if (!$isdue) {
+	require_once('lib/wizard/wizardlib.php');
+	$wizardlib = new WizardLib();
+	$wizardlib->onLogin($user, $url);
+}
 
 header('Location: ' . $url);
 exit;
