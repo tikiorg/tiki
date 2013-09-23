fbd2e5a7087aa088e38646a2f087525bfafbe7df
xavidp <xavidp@users.sourceforge.net>
2013-09-23 15:24:28 +0000

    [FIX]svn property (keywords Id)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@47677 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wizard/pages/admin_community.php
+++ lib/wizard/pages/admin_community.php
@@ -3,7 +3,7 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: admin_advanced.php 47636 2013-09-21 20:35:30Z arildb $
+// $Id$
 
 require_once('lib/wizard/wizard.php');
 
