9067b1269c64e7b5b0b91608ea669310a3cc52ef
alain_desilets <alain_desilets@users.sourceforge.net>
2013-10-04 21:25:48 +0000

    [Fix] Fixed some bad expectations in TranslationOfTest.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@47895 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/test/wiki-plugins/TranslationOfTest.php
+++ lib/test/wiki-plugins/TranslationOfTest.php
@@ -9,6 +9,39 @@ require_once('lib/wiki-plugins/wikiplugin_translationof.php');
 
 class WikiPlugin_TranslationOfTest extends PHPUnit_Framework_TestCase
 {
+//    public $orig_user;
+//
+//    protected function setUp()
+//    {
+//        global $tikilib, $_SERVER, $user, $prefs, $multilinguallib;
+//
+//        $this->orig_user = $user;
+//
+//        $prefs['site_language'] = 'en';
+//
+//
+//        /* Need to set those global vars to be able to create and delete pages */
+//        $_SERVER['HTTP_HOST'] = 'localhost';
+//        $_SERVER['REQUEST_URI'] = 'phpunit';
+//        $user = "user_that_can_edit";
+//
+//        $page_name = "APageContainingATranslationOfPlugin";
+//        $content = "{TranslationOf(orig_page=\"ChildPage\" translation_page=\"PageEnfant\" target_lang=\"fr\") /}";
+//        $lang = 'en';
+//        $tikilib->create_page($page_name, 0, $content, null, '', null, $user, '', $lang);
+//    }
+//
+//    protected function tearDown()
+//    {
+//        global $tikilib, $user;
+//
+//        $tikilib->remove_all_versions("APageContainingATranslationOfPlugin");
+//
+//        unset($_SERVER['HTTP_HOST']);
+//        unset($_SERVER['REQUEST_URI']);
+//        $user = $this->orig_user;
+//    }
+
 	/**
 	 * @dataProvider provider
 	 */
@@ -20,12 +53,18 @@ class WikiPlugin_TranslationOfTest extends PHPUnit_Framework_TestCase
 	public function provider()
 	{
 		return array(
-			array('', '<a href="tiki-index.php?page=SomePage"   class="tips"" data-cluetip-body=\'<a href="tiki-edit_translation.php?page=SomePage#new_translation">Translate this link</a>\' data-cluetip-options=\'{"activation":"mouseover","sticky":true,"mouseOutClose":false,"showTitle":false,"attribute":"data-cluetip-body"}\'>SomePage</a>',
-                  array('source_page' => 'SomePage'),
+			array('', '<a href="tiki-index.php?page=SomePage"   class="tips"" data-cluetip-body=\'<a href="tiki-edit_translation.php?page=SomePage&target_lang=fr#new_translation">Translate this link</a>\' data-cluetip-options=\'{"activation":"mouseover","sticky":true,"mouseOutClose":false,"showTitle":false,"attribute":"data-cluetip-body"}\'>SomePage</a>',
+                  array('orig_page' => 'SomePage', 'translation_lang' => 'fr'),
                   "Happy Path Case"),
-            array('', '<a href="tiki-index.php?page=SomePage"   class="tips"" data-cluetip-body=\'<a href="tiki-edit_translation.php?page=SomePage&translation_name=UnePage#new_translation">Translate this link</a>\' data-cluetip-options=\'{"activation":"mouseover","sticky":true,"mouseOutClose":false,"showTitle":false,"attribute":"data-cluetip-body"}\'>UnePage</a>',
-                  array('source_page' => 'SomePage', 'translated_anchor_text' => 'UnePage'),
-                  "Case with translated anchor text provided"),
+            array('', '<a href="tiki-index.php?page=SomePage"   class="tips"" data-cluetip-body=\'<a href="tiki-edit_translation.php?page=SomePage&target_lang=fr&translation_name=UnePage#new_translation">Translate this link</a>\' data-cluetip-options=\'{"activation":"mouseover","sticky":true,"mouseOutClose":false,"showTitle":false,"attribute":"data-cluetip-body"}\'>UnePage</a>',
+                  array('orig_page' => 'SomePage', 'translation_lang' => 'fr', 'translation_page' => 'UnePage'),
+                  "Case with name of translated page provided"),
 		);
 	}
+
+//    function test_create_page_that_contains_a_TranslationOf_plugin_creates_an_object_relation()
+//    {
+//        $this->fail('test incomplete');
+//    }
+
 }
