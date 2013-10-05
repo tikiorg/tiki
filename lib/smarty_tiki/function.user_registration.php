728213d74c091438b7427e9be2f951e9faeab4bb
jonnybradley <jonnybradley@users.sourceforge.net>
2013-10-05 14:20:40 +0000

    [FIX] user tracker: Tracker plugin no longer splits the fields string itself, so this need to be done prior to the tracker call (thanks fabricius)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@47902 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.user_registration.php
+++ lib/smarty_tiki/function.user_registration.php
@@ -119,6 +119,9 @@ function smarty_function_user_registration($params, $smarty)
 				$user = $_REQUEST['name'];	// so that one can set user preferences at registration time
 				$_REQUEST['iTRACKER'] = 1;	// only one tracker plugin on registration
 			}
+			if (!is_array($re['registrationUsersFieldIds'])) {
+				$re['registrationUsersFieldIds'] = explode(':', $re['registrationUsersFieldIds']);
+			}
 			if ($registrationlib->merged_prefs["user_register_prettytracker"] == 'y' && !empty($registrationlib->merged_prefs["user_register_prettytracker_tpl"])) {
 				if (substr($registrationlib->merged_prefs["user_register_prettytracker_tpl"], -4) == ".tpl") {
 					$userTrackerData = wikiplugin_tracker('', array('trackerId' => $re['usersTrackerId'], 'fields' => $re['registrationUsersFieldIds'], 'showdesc' => 'y', 'showmandatory' => 'y', 'embedded' => 'n', 'action' => tra('Register'), 'registration' => 'y', 'tpl' => $registrationlib->merged_prefs["user_register_prettytracker_tpl"], 'userField' => $re['usersFieldId'], 'outputwiki' => $outputwiki, 'outputtowiki' => $outputtowiki));
