a3d460a2339cf2d15d3a66802cbdaffa216a469e
xavidp <xavidp@users.sourceforge.net>
2013-10-07 17:50:15 +0000

    [FIX]Add default icons for the menu entries that had an icon missing. Improvable for sure, but better than current case (question mark for those menu items). Lgpl'd from the crystal clear icon set - http://commons.wikimedia.org/wiki/Crystal_Clear
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@47913 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ installer/schema/20131007_menus_items_missing_icons_tiki.sql
@@ -0,0 +1,11 @@
+UPDATE tiki_menu_options SET icon = 'home48x48' WHERE name = 'Home';
+UPDATE tiki_menu_options SET icon = 'categories48x48' WHERE name = 'Categories';
+UPDATE tiki_menu_options SET icon = 'accounting48x48' WHERE name = 'Accounting';
+UPDATE tiki_menu_options SET icon = 'directory48x48' WHERE name = 'Directory';
+UPDATE tiki_menu_options SET icon = 'invoice48x48' WHERE name = 'Invoice';
+UPDATE tiki_menu_options SET icon = 'quizzes48x48' WHERE name = 'Quizzes';
+UPDATE tiki_menu_options SET icon = 'reports48x48' WHERE name = 'Reports';
+UPDATE tiki_menu_options SET icon = 'stats48x48' WHERE name = 'Stats';
+UPDATE tiki_menu_options SET icon = 'surveys48x48' WHERE name = 'Surveys';
+UPDATE tiki_menu_options SET icon = 'spreadsheet48x48' WHERE name = 'Spreadsheets';
+UPDATE tiki_menu_options SET icon = 'timesheet48x48' WHERE name = 'Timesheet';
