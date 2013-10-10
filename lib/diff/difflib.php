f3e90da3588d9bd470ac7fb86240c617338d9b6d
alain_desilets <alain_desilets@users.sourceforge.net>
2013-10-10 17:47:07 +0000

    [FIX] Text_Diff_Renderer and its subclasses have many methods with incompatible signatures. These raise warnings that are reported as failures when running phpunit tests. To hide the problem from phpunit, temporarily disable E_WARNING when compiling a subclass of Text_Diff_Renderer.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@47963 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/diff/difflib.php
+++ lib/diff/difflib.php
@@ -151,8 +151,37 @@ function diffChar($orig, $final, $words=0, $function='character')
 	if ($z->isEmpty())
 		return array($orig[0], $final[0]);
 //echo "<pre>";print_r($z);echo "</pre>";
-	require_once("renderer_$function.php");
+
+    compileRendererClass($function);
       $new = "Text_Diff_Renderer_$function";
 	$renderer = new $new(count($line1));
 	return $renderer->render($z);
+}
+
+function compileRendererClass($function)
+{
+    /*
+     * The various subclasses of Text_Diff_Renderer have methods whose signatures are incompatible
+     * with those of their parents. This raises some warnings which don't matter in production settings.
+     *
+     * But when running phpunit tests, this causes some failures, because we have configured phpunit
+     * to report warnings as failures.
+     *
+     * Making the methods compatible with each other would be very involved, and might introduce some
+     * actual bugs. So instead, temporarily disable warning reporting, just for the compilation of
+     * this file.
+     */
+    $old_error_reporting_level;
+    if (defined('TIKI_IN_TEST'))
+    {
+        $old_error_reporting_level = error_reporting(E_ERROR | E_PARSE);
+    }
+
+    require_once("renderer_$function.php");
+
+    if (defined('TIKI_IN_TEST'))
+    {
+        error_reporting($old_error_reporting_level);
+    }
+
 }
