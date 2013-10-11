d2235a28e73b65c084ea32d49a6ebefc9589ccb7
alain_desilets <alain_desilets@users.sourceforge.net>
2013-10-11 18:19:05 +0000

    [Fix] Added missing require for Tracer.php in tikilib.php.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@47986 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/tikilib.php
+++ lib/tikilib.php
@@ -5,6 +5,8 @@
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id$
 
+require_once('lib/debug/Tracer.php');
+
 // this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 	header("location: index.php");
