408e10a5947ee132754166c4c5be4fbca9c85c57
jonnybradley <jonnybradley@users.sourceforge.net>
2013-10-15 14:54:38 +0000

    [bp/r48029 & r48030] [FIX] svn: In svn 1.8 the entries dir no longer exists, so check for .svn readable as that's all we need [FIX] svn: Check for .svn/entries before trying to open it. Logic could done be better but committing minimally now for backporting
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/11.x@48031 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/last_update.php
+++ lib/setup/last_update.php
@@ -8,13 +8,15 @@
 //this script may only be included - so its better to die if called directly.
 $access->check_script($_SERVER['SCRIPT_NAME'], basename(__FILE__));
 
-if (is_readable('.svn/entries')) {
+if (is_readable('.svn')) {
 	$svn = array();
-	$fp = fopen('.svn/entries', 'r');
-	for ($i = 0; 10 > $i && $line = fgets($fp, 80); ++$i) {
-		$svn[] = $line;
+	if (is_readable('.svn/entries')) {
+		$fp = fopen('.svn/entries', 'r');
+		for ($i = 0; 10 > $i && $line = fgets($fp, 80); ++$i) {
+			$svn[] = $line;
+		}
+		fclose($fp);
 	}
-	fclose($fp);
 
 	if (count($svn) > 2) {
 		// Standard SVN client
