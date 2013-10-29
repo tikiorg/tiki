3d2850beb8057399bbd34982fd0d7cccd7c10970
jonnybradley <jonnybradley@users.sourceforge.net>
2013-10-29 17:34:01 +0000

    [FIX] remove: Redirect to the same page if only removing the a version
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@48210 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-removepage.php
+++ tiki-removepage.php
@@ -52,16 +52,17 @@ if (isset($_REQUEST["remove"])) {
 	check_ticket('remove-page');
 	if (isset($_REQUEST["all"]) && $_REQUEST["all"] == 'on') {
 		$tikilib->remove_all_versions($_REQUEST["page"]);
-		header("location: tiki-index.php");
+		$access->redirect();
 		die;
 	} else {
+		$smarty->loadPlugin('smarty_modifier_sefurl');
 		if ($version == "last") {
 			$wikilib->remove_last_version($_REQUEST["page"]);
+			$access->redirect(smarty_modifier_sefurl($_REQUEST['page']));
 		} else {
 			$histlib->remove_version($_REQUEST['page'], $_REQUEST['version'], $_REQUEST['historyId']);
+			$access->redirect(smarty_modifier_sefurl($_REQUEST['page']));
 		}
-		header("location: tiki-index.php");
-		die;
 	}
 }
 ask_ticket('remove-page');
