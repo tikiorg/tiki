1644b7375da5b9c01474b038ecbb0ed998fc0e09
changi67 <changi67@users.sourceforge.net>
2013-10-29 21:42:38 +0000

    [REL] Closing 10.5
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/10.x@48235 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '10.5';	// Never ever put any spaces in this value.  This will appear in tiki-admin.php?page=general
+		$this->version 	= '10.6svn';	// Never ever put any spaces in this value.  This will appear in tiki-admin.php?page=general
 		$this->star	= 'Sun';
 		$this->releases	= array();
 
