9ef57948a2046087c93d0471a75c5ca1ddd7442b
jonnybradley <jonnybradley@users.sourceforge.net>
2013-11-17 19:37:06 +0000

    [FIX] bootstrap: Update conditional CSS applied when jquery_ui_chosen
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@48512 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup.php
+++ tiki-setup.php
@@ -428,8 +428,8 @@ if ($prefs['mobile_feature'] === 'y' && $prefs['mobile_mode'] === 'y') {
 			$headerlib->add_cssfile('vendor/jquery/plugins/chosen/chosen.css');
 			$headerlib->add_css(
 				'.chosen-container .chosen-drop, .chosen-results li { z-index: 10000; color: #444 }
-select { font-size: 13px; margin-right: 26px; padding: 0 0 0 8px; }
-select[multiple=multiple] { margin: 3px 0 3px 5px; padding: 3px 20px 3px 5px; }'
+select { font-size: 14px; padding: 6px 12px; }
+.chosen-container-multi .chosen-choices li.search-field input[type="text"] { height: inherit; }'
 			);
 		}
 		if ( $prefs['jquery_ui_selectmenu'] == 'y' ) {
