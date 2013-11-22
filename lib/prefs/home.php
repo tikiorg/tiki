38388d323f358cb16b958c8eed9846f11cd5bd53
jonnybradley <jonnybradley@users.sourceforge.net>
2013-11-22 15:01:03 +0000

    [FIX] filegals: More on "home" file gallery pref list - 'id' used by some routines, 'galleryId' by others? (just patched over here, but should be sorted for 13)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@48676 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/prefs/home.php
+++ lib/prefs/home.php
@@ -83,6 +83,7 @@ function listfgal_pref()
 	global $prefs;
 	$allfgals = $filegallib->getSubGalleries($prefs['fgal_root_id']);
 	array_unshift($allfgals['data'], $filegallib->get_file_gallery($prefs['fgal_root_id']));
+	$allfgals['data'][0]['id'] = $allfgals['data'][0]['galleryId'];	// sometimes galleries have a galleryId, sometimes it's in id :(
 
 	$listfgals = array();
 
