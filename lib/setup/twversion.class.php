51e9d41365a4c455012f9e219c81e06cb6602235
changi67 <changi67@users.sourceforge.net>
2013-11-30 10:14:32 +0000

    [REL] Preparing rerelease 12.0
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@48849 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '12.1svn';	// needs to have no spaces for releases
+		$this->version 	= '12.0';	// needs to have no spaces for releases
 		$this->star	= 'Altair';
 		$this->releases	= array();
 
