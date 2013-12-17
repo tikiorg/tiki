179bdeb528b4c02ef0e9660a6b35c4338a6c9da9
sept_7 <sept_7@users.sourceforge.net>
2013-12-17 12:42:20 +0000

    [FIX] Reorder includes to load mobile.php BEFORE javascript.php as the later depends on the former...
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@49118 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_system.php
+++ tiki-admin_system.php
@@ -22,8 +22,8 @@ if (isset($_GET['do'])) {
 		// seems combination of clearing prefs and public now messes up the page, so reload (tiki 11)
 		include_once('lib/setup/prefs.php');
 		initialize_prefs();
-		include('lib/setup/javascript.php');
 		include('lib/setup/mobile.php');
+		include('lib/setup/javascript.php');
 	}
 }
 if (isset($_GET['compiletemplates'])) {
