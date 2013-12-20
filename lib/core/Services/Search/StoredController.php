58fa3d7f493c8b1d8422f93a126da5e8236368ab
lphuberdeau <lphuberdeau@users.sourceforge.net>
2013-12-20 19:34:15 +0000

    [FIX] Missing file
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@49208 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/core/Services/Search/StoredController.php
@@ -0,0 +1,45 @@
+<?php
+// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+class Services_Search_StoredController
+{
+	function setUp()
+	{
+		Services_Exception_Disabled::check('feature_search');
+		Services_Exception_Disabled::check('storedsearch_enabled');
+		Services_Exception_Denied::checkAuth('feature_search');
+	}
+
+	function action_select($input)
+	{
+		$lib = TikiLib::lib('storedsearch');
+		$queryId = null;
+
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			$label = $input->label->text();
+			$priority = $input->priority->word();
+			$queryId = $input->queryId->int();
+
+			if (! $queryId) {
+				if ($label && $priority) {
+					$queryId = $lib->createBlank($label, $priority);
+				}
+			}
+
+			if (! $queryId) {
+				throw new Services_Exception(tr('Could not obtain the query.'), 406);
+			}
+		}
+
+		return array(
+			'title' => tr('Select Query'),
+			'priorities' => $lib->getPriorities(),
+			'queries' => $lib->getUserQueries(),
+			'queryId' => $queryId,
+		);
+	}
+}
