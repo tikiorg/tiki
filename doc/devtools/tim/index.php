b67e47452cbb7ee8a02cb34ee48a36c9cd3ac796
yonixxx <yonixxx@users.sourceforge.net>
2014-02-04 11:09:42 +0000

    [ADD]missing index.php to directories in which it has been forgotten
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@49693 b456876b-0849-0410-b77d-98878d47e9d5

--- doc/devtools/tim/index.php
+++ doc/devtools/tim/index.php
@@ -8,7 +8,7 @@
  * @copyright (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
  * @licence Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
  */
-// $Id$
+// $Id: index.php 44694 2013-01-30 20:55:37Z marclaporte $
 
 // This redirects to the sites root to prevent directory browsing
 header("location: ../tiki-index.php");
