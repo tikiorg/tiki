17265106a398877580b5ce78478858cc7b69f123
lindonb <lindonb@users.sourceforge.net>
2014-02-16 04:34:24 +0000

    [bp/r49923][SEC] [FIX] User reports: avoid invalid datetime system error when a new user registers with the daily reports for new users preference enabled.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/9.x@49924 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Reports/Users.php
+++ lib/core/Reports/Users.php
@@ -83,7 +83,7 @@ class Reports_Users
 								'view' => $view, 
 								'type' => $type,
 								'always_email' => $always_email, 
-								'last_report' => '',
+								'last_report' => '0000-00-00 00:00:00',
 							)
 			);
 		} else {
