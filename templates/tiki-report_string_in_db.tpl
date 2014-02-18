6590e429fd609d65dd396fa1e8105a31e273b05f
arildb <arildb@users.sourceforge.net>
2014-02-18 19:26:51 +0000

    [SEC] Better validation of input parameters.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@49962 b456876b-0849-0410-b77d-98878d47e9d5

--- templates/tiki-report_string_in_db.tpl
+++ templates/tiki-report_string_in_db.tpl
@@ -5,45 +5,48 @@
 <input type="text" name="string_in_db_search" size="60" /> <input type="submit" class="btn btn-default" value="Search" />
 </form>
 <hr/>
-{if isset($searchString)}
-	<p>
-	String: {$searchString}
-	</p>
-	<p>
-	<table class="string_in_db_search">
-	<tr>
-	<td>Table</td>
-	<td>Column</td>
-	<td>Occurrences</td>
-	<td>&nbsp;</td>
-	</tr>
-	{foreach from=$searchResult item=res}
+{if isset($errorMsg)}
+	<span id="error">{$errorMsg}</span>
+{else}
+	{if isset($searchString)}
+		<p>
+		String: {$searchString}
+		</p>
+		<p>
+		<table class="string_in_db_search">
 		<tr>
-		<td>{$res['table']}</td>
-		<td>{$res['column']}</td>
-		<td>{$res['occurrences']}</td>
-		<td><a href="tiki-report_string_in_db.php?query={$searchString}&table={$res['table']}&column={$res['column']}">View</a></td>
+		<td>Table</td>
+		<td>Column</td>
+		<td>Occurrences</td>
+		<td>&nbsp;</td>
 		</tr>
-	{/foreach}
-	</table>
-	</p>
-{/if}
+		{foreach from=$searchResult item=res}
+			<tr>
+			<td>{$res['table']}</td>
+			<td>{$res['column']}</td>
+			<td>{$res['occurrences']}</td>
+			<td><a href="tiki-report_string_in_db.php?query={$searchString}&table={$res['table']}&column={$res['column']}">View</a></td>
+			</tr>
+		{/foreach}
+		</table>
+		</p>
+	{/if}
 
-{if isset($tableHeaders)}
-<table>
-	<tr>
-	{foreach from=$tableHeaders item=hdr}
-		<td>{$hdr}</td>
-	{/foreach}
-	</tr>
-
-	{foreach from=$tableData item=row}
+	{if isset($tableHeaders)}
+	<table>
 		<tr>
-		{foreach from=$row item=val}
-			<td>{$val}</td>
+		{foreach from=$tableHeaders item=hdr}
+			<td>{$hdr}</td>
 		{/foreach}
 		</tr>
-	{/foreach}
-</table>
-{/if}
 
+		{foreach from=$tableData item=row}
+			<tr>
+			{foreach from=$row item=val}
+				<td>{$val}</td>
+			{/foreach}
+			</tr>
+		{/foreach}
+	</table>
+	{/if}
+{/if}
--- tiki-report_string_in_db.php
+++ tiki-report_string_in_db.php
@@ -12,37 +12,44 @@ require_once ('tiki-setup.php');
 $access->check_permission(array('tiki_p_admin_wiki'));
 
 global $tikilib;
+try {
+	if (!empty($_REQUEST['string_in_db_search'])) {
+		$searchString = $_REQUEST['string_in_db_search'];
+		$result = searchAllDB($searchString);
 
-if (!empty($_REQUEST['string_in_db_search'])) {
-	$searchString = $_REQUEST['string_in_db_search'];
-	$result = searchAllDB($searchString);
+		$smarty->assign('searchString', htmlentities($searchString));
+		$smarty->assign('searchResult', $result);
 
-	$smarty->assign('searchString', $searchString);
-	$smarty->assign('searchResult', $result);
+	} elseif (!empty($_REQUEST['query'])) {
+		$query = $_REQUEST['query'];
+		$table = $_REQUEST['table'];
+		sanitizeTableName($table);
+		$column = $_REQUEST['column'];
+		sanitizeColumnName($column);
 
-} elseif (!empty($_REQUEST['query'])) {
-	$query = $_REQUEST['query'];
-	$table = $_REQUEST['table'];
-	$column = $_REQUEST['column'];
+		$headers = array();
+		$sql2 = "SHOW COLUMNS FROM ".$table;
+		$rs2 = $tikilib->fetchAll($sql2);
+		foreach ($rs2 as $key2 => $val2) {
+			$vals2 = array_values($val2);
+			$colum = $vals2[0];
+			$type = $vals2[1];
+			$headers[] = $colum;
+		}
+		$smarty->assign('tableHeaders', $headers);
 
-	$headers = array();
-	$sql2 = "SHOW COLUMNS FROM ".$table;
-	$rs2 = $tikilib->fetchAll($sql2);
-	foreach ($rs2 as $key2 => $val2) {
-		$vals2 = array_values($val2);
-		$colum = $vals2[0];
-		$type = $vals2[1];
-		$headers[] = $colum;
+		$tableData = array();
+		$qrySearch = '%'.$query.'%';
+		$args = array($qrySearch);
+		$sql = "select * from `" . $table . "` where `" . $column . "` like ?";
+		$rs = $tikilib->fetchAll($sql, $args);
+		foreach ($rs as $row) {
+			$tableData[] = $row;
+		}
+		$smarty->assign('tableData', $tableData);
 	}
-	$smarty->assign('tableHeaders', $headers);
-	
-	$tableData = array();
-	$sql = "select * from `" . $table . "` where `" . $column . "` like '%" . $query . "%'";
-	$rs = $tikilib->fetchAll($sql);
-	foreach ($rs as $row) {
-		$tableData[] = $row;
-	}
-	$smarty->assign('tableData', $tableData);
+} catch (Exception $e) {
+	$smarty->assign('errorMsg', $e->getMessage());
 }
 $smarty->assign('mid', 'tiki-report_string_in_db.tpl');
 $smarty->display('tiki.tpl');
@@ -70,10 +77,12 @@ function searchAllDB($search)
 			$type = $vals2[1];
 			if (isTextType($type)) {
 				$sql_search_fields = Array();
-				$sql_search_fields[] = "`" . $colum . "` like '%" . str_replace("'", "''", $search) . "%'";
+				$qrySearch = '%'.$search.'%';
+				$args = array($qrySearch);
+				$sql_search_fields[] = "`" . $colum . "` like ?"; // '%" . str_replace("'", "''", $search) . "%'";
 				$sql_search = "select * from " . $table . " where ";
 				$sql_search .= implode(" OR ", $sql_search_fields);
-				$rs3 = $tikilib->fetchAll($sql_search);
+				$rs3 = $tikilib->fetchAll($sql_search, $args);
 				if (!empty($rs3)) {
 					$result[] = array('table' => $table, 'column' => $colum, 'occurrences' => count($rs3));
 				}
@@ -92,4 +101,20 @@ function isTextType($type)
 		return true;
 	}
 	return false;
+}
+
+function sanitizeTableName($table)
+{
+	$illigalChars = '/\\.<>%\'""';	// The character set should be checked more
+	if (!empty(strpbrk($table, $illigalChars))) {
+		throw new Exception("Invalid table name: ".htmlentities($table));
+	}
+}
+
+function sanitizeColumnName($column)
+{
+	$illigalChars = '/\\.<>%\'""';	// The character set should be checked more
+	if (!empty(strpbrk($column, $illigalChars))) {
+		throw new Exception("Invalid column name: ".htmlentities($column));
+	}
 }
