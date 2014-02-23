cef60be9971b61d76dc676a95426eab336de7bcb
robertplummer <robertplummer@users.sourceforge.net>
2014-02-23 19:45:47 +0000

    [ENH] Allow lines, and review syntax if it isn't acceptable
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/experimental@50031 b456876b-0849-0410-b77d-98878d47e9d5

--- wikiLingo/lib/wikiLingo_tiki/TikiWikiPluginBridge.php
+++ wikiLingo/lib/wikiLingo_tiki/TikiWikiPluginBridge.php
@@ -5,18 +5,27 @@ use WikiLingo\Expression\Plugin;
 
 class TikiWikiPluginBridge extends Base
 {
+	public function __construct()
+	{
+		$this->allowLines = true;
+	}
+
 	public function render(Plugin $plugin, $renderedChildren, $parser)
 	{
 		$name = strtolower($plugin->type);
-		require_once("lib/wiki-plugins/wikiplugin_" . $name . ".php");
-		$fn = "wikiplugin_" . $name;
+		$fileLocation = "lib/wiki-plugins/wikiplugin_" . $name . ".php";
+		if (file_exists($fileLocation)) {
+			require_once($fileLocation);
+			$fn = "wikiplugin_" . $name;
 
-		//$arguments = $this->argumentsParser->parse($plugin->parsed->arguments[0]->text);
+			//$arguments = $this->argumentsParser->parse($plugin->parsed->arguments[0]->text);
 
-		$output = $fn($renderedChildren, $plugin->parametersRaw);
+			$output = $fn($renderedChildren, $plugin->parametersRaw);
 
-		//$output = TikiLib::lib("parser")->parse_data($output);
+			//$output = TikiLib::lib("parser")->parse_data($output);
 
-		return $output;
+			return $output;
+		}
+		return $parser->syntax($plugin->parsed->loc);
 	}
 } 
--- wikiLingo/lib/wikiLingo_tiki/TikiWikiPluginStub.php
+++ wikiLingo/lib/wikiLingo_tiki/TikiWikiPluginStub.php
@@ -5,11 +5,15 @@ use WikiLingo\Expression\Plugin;
 
 class TikiWikiPluginStub extends Base
 {
+	public function __construct()
+	{
+		$this->allowLines = true;
+	}
+
 	public function render(Plugin $plugin, $renderedChildren, WikiLingoWYSIWYG\Parser $parser)
 	{
-		$name = strtolower($plugin->type);
 		$element = $parser->element('WikiLingo\\Expression\\Plugin', 'span');
-		$element->detailedAttributes['data-plugin-type'] = $name;
+		$element->detailedAttributes['data-plugin-type'] = $plugin->type;
 		$element->detailedAttributes['data-plugin-parameters'] = urlencode(json_encode($plugin->parametersRaw));
 		$element->useDetailedAttributes = true;
 		$element->staticChildren[] = $renderedChildren;
