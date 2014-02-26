46bc0f9be4f419f46b7974057ca9f797861443ff
manivannans <manivannans@users.sourceforge.net>
2014-02-26 18:17:41 +0000

    New showpref wiki plugin
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@50103 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/wiki-plugins/wikiplugin_showpref.php
@@ -0,0 +1,57 @@
+<?php
+// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+function wikiplugin_showpref_info()
+{
+	return array(
+		'name' => tra('Show Preference'),
+		'documentation' => 'PluginShowpref',
+		'description' => tra('Display contents of certain whitelisted global preference'),
+		'prefs' => array('wikiplugin_showpref'),
+		'filter' => 'wikicontent',
+		'icon' => 'img/icons/wrench.png',
+		'params' => array(
+			'pref' => array(
+				'required' => true,
+                                'name' => tra('Preference Name'),
+                                'description' => tra('Name of preference to be displayed.'),
+			),	
+		),
+	);
+}
+
+function wikiplugin_showpref($data, $params)
+{
+	global $prefs, $tikilib, $prefslib;
+                $tikilib->get_user_preference($user, 'pref_filters', 'basic');
+	// Security public prefs only, you would not want all prefs to be displayed via wiki syntax
+	
+	$name=$params['pref'];
+	if ( false !== $pos = strpos($name, '_') ) {
+			$file = substr($name, 0, $pos);
+		} else {
+			$file = 'global';
+		}
+
+	$inc_file = "lib/prefs/{$file}.php";
+		if (file_exists($inc_file)) {
+			require_once $inc_file;
+			$function = "prefs_{$file}_list";
+			if ( function_exists($function) ) {
+				$preffile = $function($partial);
+			} else {
+				$preffile = array();
+			}
+	}
+
+	if (empty($preffile[$name]['public'])) {
+		return '';
+	} else  {
+		return $tikilib->get_preference($name);
+	}	
+	return '';
+}
