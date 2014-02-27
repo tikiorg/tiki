tikiorg/tiki

commit f3c75e5a4449f84bc99b11b1ddf9397594d4d528

[FIX] articles: Use the html purifier to sanitize html article content, instead of the default one (which messes up some plugin params). Should help with wish4796, thanks Xavi
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@48131 b456876b-0849-0410-b77d-98878d47e9d5

tiki-edit_article.php
@@ -387,9 +387,9 @@ if (isset($_REQUEST['preview']) or !empty($errors)) {
 	$smarty->assign('edit_data', 'y');
 
 	if (isset($_REQUEST['allowhtml']) && $_REQUEST['allowhtml'] == 'on') {
-		$body = $_REQUEST['body'];
+		$body = $jitRequest->body->purifier();
 
-		$heading = $_REQUEST['heading'];
+		$heading = $jitRequest->heading->purifier();
 	} else {
 		$body = strip_tags($_REQUEST['body'], '<a><pre><p><img><hr><b><i>');
 
@@ -447,9 +447,9 @@ if (isset($_REQUEST['save']) && empty($errors)) {
 	}
 
 	if (isset($_REQUEST['allowhtml']) && $_REQUEST['allowhtml'] == 'on' || $_SESSION['wysiwyg'] == 'y') {
-		$body = $_REQUEST['body'];
+		$body = $jitRequest->body->purifier();
 
-		$heading = $_REQUEST['heading'];
+		$heading = $jitRequest->heading->purifier();
 	} else {
 		$body = strip_tags($_REQUEST['body'], '<a><pre><p><img><hr><b><i>');
 

commit 1d354fabf4a20f4702e35ee0a8ea8f8cfdcd8155

[FIX] Fixes issue wish5156 (html plugin in articles) caused by revision r48131 (thanks jonny for the hint)
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@50124 b456876b-0849-0410-b77d-98878d47e9d5

tiki-edit_article.php
@@ -400,9 +400,33 @@ if (isset($_REQUEST['preview']) or !empty($errors)) {
 	$smarty->assign('edit_data', 'y');
 
 	if (isset($_REQUEST['allowhtml']) && $_REQUEST['allowhtml'] == 'on') {
-		$body = $jitRequest->body->purifier();
+		$body = $_REQUEST['body'];
+		$parserlib = TikiLib::lib('parser');
+		$noparsed = array();
+		$parserlib->plugins_remove($body, $noparsed);
 
-		$heading = $jitRequest->heading->purifier();
+		$body = TikiFilter::get('xss')->filter($body);
+
+		$parserlib->isEditMode = true;
+		$parserlib->plugins_replace($body, $noparsed, true);
+		$parserlib->isEditMode = false;
+
+		$heading = $_REQUEST['heading'];
+		$noparsed = array();
+		$parserlib->plugins_remove($heading, $noparsed);
+
+		$heading = TikiFilter::get('xss')->filter($heading);
+
+		$parserlib->isEditMode = true;
+		$parserlib->plugins_replace($heading, $noparsed, true);
+		$parserlib->isEditMode = false;
+
+		//html is stored encoded in wysiwyg
+		if (isset($jitRequest['wysiwyg']) && $jitRequest['wysiwyg'] == 'y') {
+			$body = html_entity_decode($body, ENT_QUOTES, 'UTF-8');
+			$heading = html_entity_decode($heading, ENT_QUOTES, 'UTF-8');
+		}
+
 	} else {
 		$body = strip_tags($_REQUEST['body'], '<a><pre><p><img><hr><b><i>');
 
@@ -460,9 +484,33 @@ if (isset($_REQUEST['save']) && empty($errors)) {
 	}
 
 	if (isset($_REQUEST['allowhtml']) && $_REQUEST['allowhtml'] == 'on' || $_SESSION['wysiwyg'] == 'y') {
-		$body = $jitRequest->body->purifier();
+		$body = $_REQUEST['body'];
+		$parserlib = TikiLib::lib('parser');
+		$noparsed = array();
+		$parserlib->plugins_remove($body, $noparsed);
 
-		$heading = $jitRequest->heading->purifier();
+		$body = TikiFilter::get('xss')->filter($body);
+
+		$parserlib->isEditMode = true;
+		$parserlib->plugins_replace($body, $noparsed, true);
+		$parserlib->isEditMode = false;
+
+		$heading = $_REQUEST['heading'];
+		$noparsed = array();
+		$parserlib->plugins_remove($heading, $noparsed);
+
+		$heading = TikiFilter::get('xss')->filter($heading);
+
+		$parserlib->isEditMode = true;
+		$parserlib->plugins_replace($heading, $noparsed, true);
+		$parserlib->isEditMode = false;
+
+		//html is stored encoded in wysiwyg
+		if (isset($jitRequest['wysiwyg']) && $jitRequest['wysiwyg'] == 'y') {
+			$body = html_entity_decode($body, ENT_QUOTES, 'UTF-8');
+			$heading = html_entity_decode($heading, ENT_QUOTES, 'UTF-8');
+		}
+
 	} else {
 		$body = strip_tags($_REQUEST['body'], '<a><pre><p><img><hr><b><i>');
 