01d967bba3b47fb3867452d66582df49123623fb
fmg-sf <fmg-sf@users.sourceforge.net>
2014-04-03 01:14:16 +0000

    [FIX] two hierarchy levels to be stepped towards Tiki main page
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@50640 b456876b-0849-0410-b77d-98878d47e9d5

--- db/cert/index.php
+++ db/cert/index.php
@@ -8,8 +8,8 @@
  * @copyright (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
  * @licence Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
  */
-// $Id: index.php 44694 2013-01-30 20:55:37Z marclaporte $
+// old ID 44694, timestamp removed $
 
-// This redirects to the sites root to prevent directory browsing
-header("location: ../tiki-index.php");
+// This redirects to the site's root to prevent directory browsing
+header("location: ../../tiki-index.php");
 die;
