4c38ae8a08e016c78d014755559670bed0b7dd8d
nkoth <nkoth@users.sourceforge.net>
2014-04-24 19:35:49 +0000

    [KIL] Metrics Dashboard which was never used/too complicated experimental feature
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@50981 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki.sql
+++ db/tiki.sql
@@ -3305,39 +3305,6 @@ CREATE TABLE `tiki_auth_tokens` (
     KEY `tiki_auth_tokens_token` (`token`)
 ) ENGINE=MyISAM;
 
-DROP TABLE IF EXISTS `metrics_assigned`;
-CREATE TABLE `metrics_assigned` (
-    `assigned_id` int(11) NOT NULL AUTO_INCREMENT,
-    `metric_id` int(11) NOT NULL,
-    `tab_id` int(11) NOT NULL,
-    PRIMARY KEY (`assigned_id`),
-    KEY `metric_id` (`metric_id`),
-    KEY `tab_id` (`tab_id`)
-) ENGINE=MyISAM;
-
-DROP TABLE IF EXISTS `metrics_metric`;
-CREATE TABLE `metrics_metric` (
-    `metric_id` int(11) NOT NULL AUTO_INCREMENT,
-    `metric_name` varchar(255) NOT NULL,
-    `metric_range` varchar(1) NOT NULL DEFAULT '+' COMMENT 'values: + (daily), @ (monthly&weekly), - (weekly)',
-    `metric_datatype` varchar(1) NOT NULL DEFAULT 'i' COMMENT 'values: i(nteger), %(percentage), f(loat), L(ist)',
-    `metric_lastupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `metric_query` text,
-    `metric_dsn` VARCHAR(200) NOT NULL DEFAULT 'local',
-    PRIMARY KEY (`metric_id`),
-    UNIQUE KEY `metric_name` (`metric_name`)
-) ENGINE=MyISAM;
-
-DROP TABLE IF EXISTS `metrics_tab`;
-CREATE TABLE `metrics_tab` (
-    `tab_id` int(11) NOT NULL AUTO_INCREMENT,
-    `tab_name` varchar(255) NOT NULL,
-    `tab_order` int(11) NOT NULL DEFAULT '0',
-    `tab_content` longtext NOT NULL,
-    PRIMARY KEY (`tab_id`),
-    UNIQUE KEY `tab_name` (`tab_name`)
-) ENGINE=MyISAM;
-
 DROP TABLE IF EXISTS `tiki_file_backlinks`;
 CREATE TABLE `tiki_file_backlinks` (
        `fileId` int(14) NOT NULL,
