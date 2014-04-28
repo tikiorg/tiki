6be7e27110aabb8ab164ed3d2c98da62236d87cc
jonnybradley <jonnybradley@users.sourceforge.net>
2014-04-28 18:51:27 +0000

    [FIX] installer: More on changes following from r50989 for the database collation fix script. Now using SHOW TABLES and fetchAll, also re-fetch local.php to get real database name (thanks to Jyhem, LPH and all for the input)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@51037 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/20120429_fix_collation_tiki.php
+++ installer/schema/20120429_fix_collation_tiki.php
@@ -16,9 +16,11 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 function upgrade_20120429_fix_collation_tiki($installer)
 {
 	global $dbs_tiki;
+	require(TikiInit::getCredentialsFile());
 	$installer->query("ALTER DATABASE `" . $dbs_tiki . "` CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci'");
-	$results= $installer->query('SHOW TABLES') ;
-	while ( $table = $results->fetch_row() ) {
-		$installer->query("ALTER TABLE ".$table[0]." convert to character set DEFAULT COLLATE DEFAULT");
+	unset($dbs_tiki);
+	$results = $installer->fetchAll('SHOW TABLES');
+	foreach ( $results as $table ) {
+		$installer->query('ALTER TABLE ' . reset($table) . ' convert to character set DEFAULT COLLATE DEFAULT');
 	}
 }
