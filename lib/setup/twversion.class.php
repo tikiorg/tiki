2de7a681d3998dc0042e2f5af1ffb08a05f130cd
xavidp <xavidp@users.sourceforge.net>
2014-05-02 16:05:30 +0000

    [REL] Preparing 12.1 release
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@51082 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '12.1svn';	// needs to have no spaces for releases
+		$this->version 	= '12.1';	// needs to have no spaces for releases
 		$this->star	= 'Altair';
 		$this->releases	= array();
 
@@ -205,6 +205,7 @@ class TWVersion
 				'12.0',
 				'12.1alpha',
 				'12.1beta',
+				'12.1',
 			);
 	}
 
