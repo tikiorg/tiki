0648e75f43d85c40def482bb6e265bc599b43654
lindonb <lindonb@users.sourceforge.net>
2014-05-05 04:15:49 +0000

    [NEW] tablesorter: add tablesorter.less file (let me know if this is the wrong location for this file) and import into darkroom.less file; delete legacy style sheet. To do: add to other bootstrapified themes, which will certainly result in further fixes. Tablesorter in bootstrap default is also a little messed up now (not sure how to incorporate this less file into the default theme?).
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@51150 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup.php
+++ tiki-setup.php
@@ -499,7 +499,6 @@ if ($prefs['mobile_feature'] === 'y' && $prefs['mobile_mode'] === 'y') {
 		$headerlib->add_jsfile('vendor/jquery/plugins/media/jquery.media.js');
 	}
 	if ( $prefs['feature_jquery_tablesorter'] == 'y' ) {
-		$headerlib->add_cssfile('lib/jquery_tiki/tablesorter/style.css');
 		if ( $prefs['tiki_minify_javascript'] === 'y' ) {
 			//tablesorter has bad syntax in the non-min file, however the min file seems to work fine when double minned :)
 			$headerlib->add_jsfile('vendor/jquery/plugins/tablesorter/js/jquery.tablesorter.min.js');
