ff522f2c3990867dbcbc614ce86a5893d69d1961
lrrcenter <lrrcenter@users.sourceforge.net>
2014-05-13 13:39:22 +0000

    [bp/r51266][FIX] Mail Queue in Multitiki installations. Add mail-queue:send command (to be called from console.php).
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@51267 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/core/Tiki/Command/MailQueueSendCommand.php
@@ -0,0 +1,72 @@
+<?php
+// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: MailInPollCommand.php 50599 2014-03-31 21:22:59Z lphuberdeau $
+
+namespace Tiki\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+error_reporting(E_ALL);
+use TikiLib;
+
+class MailQueueSendCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('mail-queue:send')
+            ->setDescription('Send the messages stored in the Mail Queue');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+      require_once ("lib/mail/maillib.php");
+
+      tiki_mail_setup();
+      echo ("Mail queue processor starting...\n");
+
+      $messages = \TikiDb::get()->fetchAll("SELECT messageId, message FROM `tiki_mail_queue`");
+
+      foreach ( $messages as $message ) {
+
+          echo("Sending message ".$message["messageId"]."...");
+          $mail = unserialize($message["message"]);
+
+          if ($mail) {
+            try {
+                $mail->send();
+                $title = 'mail';
+            } catch (Zend_Mail_Exception $e) {
+                $title = 'mail error';
+            }
+
+            if ($title == 'mail error' || $prefs['log_mail'] == 'y') {
+            	foreach ($recipients as $u) {
+            		$logslib->add_log($title, $u . '/' . $mail->getSubject());
+            	}
+            }
+
+            if ($title == 'mail error') {
+            	$query = "UPDATE `tiki_mail_queue` SET attempts = attempts + 1 WHERE messageId = ?";
+            	echo ("Failed.\n");
+            	print_r($mailer->errors);
+            	echo ("\n");
+            } else {
+            	$query = "DELETE FROM `tiki_mail_queue` WHERE messageId = ?";
+            	echo ("Sent.\n");
+            }
+
+            \TikiDb::get()->query($query, array($message["messageId"]));
+          } else {
+              echo ("ERROR: Unable to unserialize the mailer object\n");
+          }
+      }
+      echo ("Mail queue processed...\n");
+    }
+}
