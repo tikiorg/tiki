8af7d7bc711c06c3101c458f0fcc4c63e364b857
robertplummer <robertplummer@users.sourceforge.net>
2014-05-18 18:49:57 +0000

    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@51374 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wikiLingo_tiki/WikiPluginBridge.php
+++ lib/wikiLingo_tiki/WikiPluginBridge.php
@@ -10,13 +10,6 @@ class WikiPluginBridge extends Base
 		$this->allowLines = true;
 	}
 
-	/**
-	 * @param Plugin $plugin
-	 * @param string $body
-	 * @param \WikiLingo\Renderer $renderer
-	 * @param \WikiLingo\Parser $parser
-	 * @return mixed|string
-	 */
 	public function render(Plugin &$plugin, &$body, &$renderer, &$parser)
 	{
 		$name = strtolower($plugin->type);
@@ -25,14 +18,11 @@ class WikiPluginBridge extends Base
 			require_once($fileLocation);
 			$fn = "wikiplugin_" . $name;
 
-			if ($plugin->parsed->type === 'Plugin') {
-				$body = $parser->syntaxBetween($plugin->parsed->arguments[0]->loc, $plugin->parsed->stateEnd->loc);
-			}
-
 			//$arguments = $this->argumentsParser->parse($plugin->parsed->arguments[0]->text);
 
 			$output = $fn($body, $plugin->parametersRaw);
-			$output = TikiLib::lib("parser")->parse_data($output, array('is_html' => true));
+
+			//$output = TikiLib::lib("parser")->parse_data($output);
 
 			return $output;
 		}
