68897530c7d8e6eeee2bedc8332080702685dff1
luciash <luciash@users.sourceforge.net>
2014-05-19 17:17:18 +0000

    [REL] Preparing 12.2 release
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@51386 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '12.2svn';	// needs to have no spaces for releases
+		$this->version 	= '12.2';	// needs to have no spaces for releases
 		$this->star	= 'Altair';
 		$this->releases	= array();
 
@@ -207,6 +207,7 @@ class TWVersion
 				'12.1alpha',
 				'12.1beta',
 				'12.1',
+				'12.2'
 			);
 	}
 
