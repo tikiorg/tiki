dbb469cf5f09742d0589346373c032ba201da2f7
nkoth <nkoth@users.sourceforge.net>
2014-07-17 23:38:24 +0000

    [FIX better way (fixes https://dev.tiki.org/bug5110) to fix double slashes in constructed urls
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/13.x@51994 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.query.php
+++ lib/smarty_tiki/function.query.php
@@ -192,6 +192,7 @@ function smarty_function_query($params, $smarty)
 			}
 		}
 
+		$php_self = "/" . ltrim($php_self, "/");
 		switch ( $params['_type'] ) {
 			case 'absolute_uri':
 				$ret = $base_host.$php_self.( $ret == '' ? '' : '?'.$ret );
