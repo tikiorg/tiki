7323086a190af53ac979728f2293eb3adf2a0fdb
jonnybradley <jonnybradley@users.sourceforge.net>
2014-07-19 14:28:03 +0000

    [bp/r52024][FIX] ajax: Strip get params from ajax referrers as new ones will be added from the params (leading to double question mark issues etc) [bp/r52025][FIX] ajax: Split assignment and test to make things clearer (thanks nelsonko)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@52027 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/function.query.php
+++ lib/smarty_tiki/function.query.php
@@ -178,6 +178,10 @@ function smarty_function_query($params, $smarty)
 				$php_self = str_replace('route.php', $inclusion, $php_self);
 			} else if (basename($php_self) === 'tiki-ajax_services.php' && !empty($_SERVER['HTTP_REFERER'])) {
 				$php_self = str_replace('tiki-ajax_services.php', basename($_SERVER['HTTP_REFERER']), $php_self);
+				$pos = strpos($php_self, '?');
+				if ($pos !== false) {
+					$php_self = substr($php_self, 0, $pos);
+				}
 			}
 		}
 
