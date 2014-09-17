48e5f6d1132554a7c3040bae274a013b297ac66d
arildb <arildb@users.sourceforge.net>
2014-09-17 18:13:19 +0000

    [FIX] Add missing tables to innodb conversion script
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/13.x@52528 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki_convert_myisam_to_innodb.sql
+++ db/tiki_convert_myisam_to_innodb.sql
@@ -269,6 +269,8 @@ ALTER TABLE `tiki_user_mailin_struct` ENGINE=InnoDB;
 ALTER TABLE `tiki_activity_stream` ENGINE=InnoDB;
 ALTER TABLE `tiki_activity_stream_mapping` ENGINE=InnoDB;
 ALTER TABLE `tiki_activity_stream_rules` ENGINE=InnoDB;
+ALTER TABLE `tiki_user_monitors` ENGINE=InnoDB;
+ALTER TABLE `tiki_output` ENGINE=InnoDB;
 ALTER TABLE `tiki_goals` ENGINE=InnoDB;
 ALTER TABLE `tiki_goal_events` ENGINE=InnoDB;
 
