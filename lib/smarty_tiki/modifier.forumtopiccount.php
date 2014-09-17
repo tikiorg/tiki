186c8aa49498dae7fa1d2ef4ff71b1863bdb58ca
nkoth <nkoth@users.sourceforge.net>
2014-09-17 21:24:44 +0000

    [ADD] Useful modifier to get count of topics in a forum
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@52529 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/smarty_tiki/modifier.forumtopiccount.php
@@ -0,0 +1,10 @@
+<?php
+
+if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
+	header("location: index.php");
+	exit;
+}
+
+function smarty_modifier_forumtopiccount($forumId) {
+	return TikiLib::lib('comments')->count_forum_topics($forumId);
+}
