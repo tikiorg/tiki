13f560549010a034d4013bbefdfb0cd7333f5851
xavidp <xavidp@users.sourceforge.net>
2014-11-10 13:53:19 +0000

    [FIX]Missing parts to have consistent naming in installs and upgrades, and with the most agreeed wording. Option Panels is set with a space in purpose, in order to have it shown in the first position under "Configuration" section of the menu.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@53044 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/20141106_menu_configuration_panel_tiki.sql
+++ installer/schema/20141106_menu_configuration_panel_tiki.sql
@@ -1,1 +1,3 @@
-update tiki_menu_options set name='Panels' where name='Admin Home';
+# xavidp. Option Panels is set with a space in purpose, in order to have it shown in the first position under "Configuration"
+update tiki_menu_options set name=' Panels' where name='Admin Home';
+update tiki_menu_options set name='Configuration' where name='Admin';
