cb38f9d0e2d814db456bc9ddfedcfb5fd335b076
jyhem <jyhem@users.sourceforge.net>
2014-11-14 16:33:35 +0000

    [REL]Closing release 12.3
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@53103 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '12.3';	// needs to have no spaces for releases
+		$this->version 	= '12.4svn';	// needs to have no spaces for releases
 		$this->star	= 'Altair';
 		$this->releases	= array();
 
