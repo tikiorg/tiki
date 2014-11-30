850e3109ed0721ab3a9eff6403c6610848a7b294
eromneg <eromneg@users.sourceforge.net>
2014-11-30 20:34:29 +0000

    [FIX] [bp53221] broken index logic due to a floating number being divided by another floating number - needed to round before making an integer
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/13.x@53222 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/graph-engine/abstract.gridbased.php
+++ lib/graph-engine/abstract.gridbased.php
@@ -652,7 +652,7 @@ class StaticGridScale extends GridScale // {{{1
 
 	function _getValue( $pos ) // {{{2
 	{
-		$index = intval($pos / $this->width);
+		$index = intval(round($pos / $this->width));
 		if ( isset( $this->labels[ $index ] ) )
 			return $this->labels[ $index ];
 		else
