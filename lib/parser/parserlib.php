357cc1ef7d6ee02d3ba70d9c30690c0d3c4f1565
nkoth <nkoth@users.sourceforge.net>
2014-12-11 17:00:34 +0000

    [FIX] Cleanup and set empty value in event of no value jsut in case
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@53294 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/parser/parserlib.php
+++ lib/parser/parserlib.php
@@ -2251,10 +2251,10 @@ if ( \$('#$id') ) {
 						if ( isset($_GET[$name]) ) {
 							$value = $_GET[$name];
 						} else {
-								
+							$value = '';
 							include_once('lib/wiki-plugins/wikiplugin_showpref.php');	
 							if ($prefs['wikiplugin_showpref'] == 'y' && $showpref = wikiplugin_showpref('', array('pref' => $name))) {
-								$value = $showpref; //TODO after mani setups the $pref for the whitelist, check the whitelist before running the plugin.
+								$value = $showpref;
 							}
 						}
 						break;
