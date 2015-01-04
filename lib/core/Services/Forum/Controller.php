273dbfd026f20f1bbd9ae5c4009d28768d6baca4
lindonb <lindonb@users.sourceforge.net>
2015-01-04 01:14:42 +0000

    [FIX] forum: use new alert service rather than separate function for forums; replace deprecated check_ticket with check_authenticity for CSRF security; conformed merge_topic and move_topic functions to updated logic
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@53394 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Services/Forum/Controller.php
+++ lib/core/Services/Forum/Controller.php
@@ -23,8 +23,7 @@ class Services_Forum_Controller
 	 */
 	function action_lock_topic($input)
 	{
-		$ret = $this->lockUnlock($input, 'lock');
-		return ['FORWARD' => $ret];
+		return $this->lockUnlock($input, 'lock');
 	}
 
 	/**
@@ -34,8 +33,7 @@ class Services_Forum_Controller
 	 */
 	function action_unlock_topic($input)
 	{
-		$ret = $this->lockUnlock($input, 'unlock');
-		return ['FORWARD' => $ret];
+		return $this->lockUnlock($input, 'unlock');
 	}
 
 	/**
@@ -46,27 +44,77 @@ class Services_Forum_Controller
 	 */
 	function action_merge_topic($input)
 	{
-		if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['toId'])) {
-			$ret = $this->setVars($input, 'merge', 2);
-			if (!empty($ret['selectedTopics'])) {
-				$commentslib = TikiLib::lib('comments');
-				check_ticket('view-forum');
-				foreach ($ret['selectedTopics'] as $id => $topic) {
-					if ($id !== $ret['toId']) {
-						$commentslib->set_parent($id, $ret['toId']);
-					}
+		$access = TikiLib::lib('access');
+		$check = $access->check_authenticity(null, false);
+		if (!empty($check['ticket'])) {
+			parse_str($input->offsetGet('form'), $form);
+			//check number of topics on first pass
+			if (count($form['forumtopic']) > 0) {
+				$items = $this->getTopicTitles($form['forumtopic']);
+				$toList = json_decode($form['comments_coms'], true);
+				return [
+					'action' => 'merge_topic',
+					'title' => tra('Merge selected topics with another topic'),
+					'items' => $items,
+					'ticket' => $check['ticket'],
+					'toList' => $toList,
+					'modal' => '1',
+				];
+			} else {
+				//oops if no topics were selected
+				return [
+					'FORWARD' => [
+						'controller' => 'utilities',
+						'action' => 'alert',
+						'type' => 'warning',
+						'title' => tra('Topic merge feedback'),
+						'heading' => tra('Oops'),
+						'msg' => tra('No topics were selected. Please select the topics you wish to merge before clicking the merge button.'),
+						'modal' => '1'
+					]
+				];
+			}
+		} elseif ($check === true && $_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['toId'])) {
+			$items = json_decode($input->offsetGet('items'), true);
+			$toList = json_decode($input->offsetGet('toList'), true);
+			$toId = $input->toId->int();
+			$commentslib = TikiLib::lib('comments');
+			foreach ($items as $id => $topic) {
+				if ($id !== $toId) {
+					$commentslib->set_parent($id, $toId);
 				}
 			}
-			$ret['title'] = tr('Topic merge feedback');
-			$ret['action'] = 'success';
-			unset($ret['toId']);
+			$toName = $toList[$toId];
+			if (count($items) == 1) {
+				$msg = tr('The following topic has been merged with the %0%1%2 topic:', '<em>', $toName, '</em>');
+			} else {
+				$msg = tr('The following topics have been merged with the %0%1%2 topic:', '<em>', $toName, '</em>');
+			}
 			return [
-				'FORWARD' => $ret
+				'FORWARD' => [
+					'controller' => 'utilities',
+					'action' => 'alert',
+					'type' => 'feedback',
+					'title' => tra('Topic merge feedback'),
+					'heading' => tra('Success'),
+					'items' => $items,
+					'msg' => $msg,
+					'timeoutMsg' => tra('This popup will automatically close in 5 seconds.'),
+					'modal' => '1'
+				]
 			];
-		} else {
-			$ret = $this->setVars($input, 'merge');
-			$ret['title'] = tr('Merge selected topics with another topic');
-			return $ret;
+		} elseif ($check === false) {
+			return [
+				'FORWARD' => [
+					'controller' => 'utilities',
+					'action' => 'alert',
+					'type' => 'error',
+					'title' => tra('Topic merge feedback'),
+					'heading' => tra('Error'),
+					'msg' => tra('Sea Surfing (CSRF) detected. Operation blocked.'),
+					'modal' => '1'
+				]
+			];
 		}
 	}
 
@@ -78,30 +126,82 @@ class Services_Forum_Controller
 	 */
 	function action_move_topic($input)
 	{
-		if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['toId'])) {
-			$ret = $this->setVars($input, 'move', 2);
-			if (!empty($ret['selectedTopics'])) {
-				$commentslib = TikiLib::lib('comments');
-				foreach ($ret['selectedTopics'] as $id => $topic) {
-					check_ticket('view-forum');
-					// To move a topic you just have to change the object
-					$obj = 'forum:' . $ret['toId'];
-					$commentslib->set_comment_object($id, $obj);
-					// update the stats for the source and destination forums
-					$commentslib->forum_prune($ret['forumId']);
-					$commentslib->forum_prune($ret['toId']);
-				}
+		$access = TikiLib::lib('access');
+		$check = $access->check_authenticity(null, false);
+		if (!empty($check['ticket'])) {
+			parse_str($input->offsetGet('form'), $form);
+			//check number of topics on first pass
+			if (count($form['forumtopic']) > 0) {
+				$items = $this->getTopicTitles($form['forumtopic']);
+				$toList = json_decode($form['all_forums'], true);
+				return [
+					'action' => 'move_topic',
+					'title' => tra('Move selected topics to another forum'),
+					'items' => $items,
+					'ticket' => $check['ticket'],
+					'toList' => $toList,
+					'forumName' => $toList[$form['forumId']],
+					'modal' => '1',
+				];
+			} else {
+				//oops if no topics were selected
+				return [
+					'FORWARD' => [
+						'controller' => 'utilities',
+						'action' => 'alert',
+						'type' => 'warning',
+						'title' => tra('Topic move feedback'),
+						'heading' => tra('Oops'),
+						'msg' => tra('No topics were selected. Please select the topics you wish to move before clicking the move button.'),
+						'modal' => '1'
+					]
+				];
 			}
-			$ret['title'] = tr('Topic move feedback');
-			$ret['action'] = 'success';
-			unset($ret['toId'], $ret['forumId']);
+		} elseif ($check === true && $_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['toId'])) {
+			$items = json_decode($input->offsetGet('items'), true);
+			$toList = json_decode($input->offsetGet('toList'), true);
+			$toId = $input->toId->int();
+			$forumId = $input->forumId->int();
+			$commentslib = TikiLib::lib('comments');
+			foreach ($items as $id => $topic) {
+				// To move a topic you just have to change the object
+				$obj = 'forum:' . $toId;
+				$commentslib->set_comment_object($id, $obj);
+				// update the stats for the source and destination forums
+				$commentslib->forum_prune($forumId);
+				$commentslib->forum_prune($toId);
+			}
+			$toName = $toList[$toId];
+			if (count($items) == 1) {
+				$msg = tr('The following topic has been moved to the %0%1%2 forum:', '<em>', $toName, '</em>');
+			} else {
+				$msg = tr('The following topics have been moved to the %0%1%2 forum:', '<em>', $toName, '</em>');
+			}
 			return [
-				'FORWARD' => $ret
+				'FORWARD' => [
+					'controller' => 'utilities',
+					'action' => 'alert',
+					'type' => 'feedback',
+					'title' => tra('Topic move feedback'),
+					'heading' => tra('Success'),
+					'items' => $items,
+					'msg' => $msg,
+					'timeoutMsg' => tra('This popup will automatically close in 5 seconds.'),
+					'modal' => '1'
+				]
 			];
-		} else {
-			$ret = $this->setVars($input, 'move');
-			$ret['title'] = tr('Move selected topics to another forum');
-			return $ret;
+		} elseif ($check === false) {
+			return [
+				'FORWARD' => [
+					'controller' => 'utilities',
+					'action' => 'alert',
+					'type' => 'error',
+					'title' => tra('Topic move feedback'),
+					'heading' => tra('Error'),
+					'msg' => tra('Sea Surfing (CSRF) detected. Operation blocked.'),
+					'modal' => '1'
+				]
+			];
 		}
 	}
 
@@ -144,7 +244,7 @@ class Services_Forum_Controller
 						'type' => 'warning',
 						'title' => tra('Topic delete feedback'),
 						'heading' => tra('Oops'),
-						'msg' => tra('No topics were selected. Please select the topics you wish to delete and then click the delete button.'),
+						'msg' => tra('No topics were selected. Please select the topics you wish to delete before clicking the delete button.'),
 						'modal' => '1'
 					]
 				];
@@ -192,26 +292,12 @@ class Services_Forum_Controller
 	}
 
 	/**
-	 * Used by action functions to give success feedback
-	 * @param $input
-	 * @return array
+	 * Utility to get topic names
+	 *
+	 * @param $topicIds
+	 * @return mixed
+	 * @throws Exception
 	 */
-	function action_success($input)
-	{
-		$ret = [
-			'title' => $input->title->striptags(),
-			'action' => 'success',
-			'toName' => $input->toName->striptags(),
-			'selectedTopics' => $input->selectedTopics->striptags(),
-			'type' => $input->type->word(),
-			'modal' => '1'
-		];
-		if ($ret['type'] === 'move') {
-			$ret['forumName'] = $input->forumName->striptags();
-		}
-		return $ret;
-	}
-
 	private function getTopicTitles($topicIds)
 	{
 		$commentslib = TikiLib::lib('comments');
@@ -220,61 +306,6 @@ class Services_Forum_Controller
 			$ret[(int) $id] = $info['title'];
 		}
 		return $ret;
-	}
-
-	/**
-	 * Utility to set return variables common to the functions in this class
-	 * @param $input
-	 * @param $type
-	 * @param int $i
-	 * @return array
-	 * @throws Exception
-	 */
-	private function setVars($input, $type, $i = 1)
-	{
-		$ret = [];
-		if ($type === 'move') {
-			$toListLabel = 'all_forums';
-		} elseif ($type === 'merge') {
-			$toListLabel = 'comments_coms';
-		}
-		if ($i === 1) {
-			parse_str($input->form->none(), $form);
-			$ret['forumId'] = $form['forumId'];
-			if (!in_array($type, ['lock', 'unlock', 'delete'])) {
-				$ret['toList'] = json_decode($form[$toListLabel], true);
-			}
-			if ($type === 'move') {
-				$ret['forumName'] = $ret['toList'][$form['forumId']];
-			}
-			$commentslib = TikiLib::lib('comments');
-			foreach ($form['forumtopic'] as $id) {
-				$info = $commentslib->get_comment($id);
-				$ret['selectedTopics'][(int) $id] = $info['title'];
-			}
-			$ret['type'] = $type;
-			return $ret;
-		} else {
-			if ($type === 'delete_topic') {
-				$index = 'items';
-			} else {
-				$index = 'forumtopic';
-			}
-			$ret['selectedTopics'] = json_decode($input->$index->striptags(), true);
-			$ret['type'] = $type;
-			if ($type === 'move' || $type === 'delete') {
-				$ret['forumId'] = $input->forumId->int();
-			}
-			if ($type !== 'delete_topic') {
-				$toList = json_decode($input->toList->striptags(), true);
-				if ($type === 'move') {
-					$ret['forumName'] = $toList[$ret['forumId']];
-				}
-				$ret['toId'] = $input->toId->int();
-				$ret['toName'] = $toList[$ret['toId']];
-			}
-			return $ret;
-		}
 	}
 
 	/**
@@ -287,18 +318,47 @@ class Services_Forum_Controller
 	private function lockUnlock($input, $type)
 	{
 		$fn = $type . '_comment';
-		$ret = $this->setVars($input, $type);
-		if (!empty($ret['selectedTopics'])) {
+		parse_str($input->form->none(), $form);
+		if (count($form['forumtopic']) > 0) {
 			$commentslib = TikiLib::lib('comments');
 			check_ticket('view-forum');
-			foreach ($ret['selectedTopics'] as $id => $topic) {
+			$items = $this->getTopicTitles($form['forumtopic']);
+			foreach ($items as $id => $topic) {
 				$commentslib->$fn($id);
 			}
+			if (count($items) == 1) {
+				$msg = tr('The following topic has been %0:', tra($type . 'ed'));
+			} else {
+				$msg = tr('The following topics have been %0:', tra($type . 'ed'));
+			}
+			return [
+				'FORWARD' => [
+					'controller' => 'utilities',
+					'action' => 'alert',
+					'type' => 'feedback',
+					'title' => tr('Topic %0 feedback', $type),
+					'heading' => tra('Success'),
+					'items' => $items,
+					'msg' => $msg,
+					'timeoutMsg' => tra('This popup will automatically close in 5 seconds.'),
+					'modal' => '1'
+				]
+			];
+		} else {
+			//oops if no topics were selected
+			return [
+				'FORWARD' => [
+					'controller' => 'utilities',
+					'action' => 'alert',
+					'type' => 'warning',
+					'title' => tr('Topic %0 feedback', tra($type)),
+					'heading' => tra('Oops'),
+					'msg' => tr('No topics were selected. Please select the topics you wish to %0 before clicking the %1 button.',
+						tra($type), tra($type)),
+					'modal' => '1'
+				]
+			];
 		}
-		$ret['title'] = tr('Topic %0 feedback', $type);
-		$ret['action'] = 'success';
-		$ret['modal'] = '1';
-		return $ret;
 	}
 
 }
--- templates/forum/move_topic.tpl
+++ templates/forum/move_topic.tpl
@@ -3,61 +3,52 @@
 	{title}{$title|escape}{/title}
 {/block}
 {block name="content"}
-	{if $selectedTopics|count > 0}
-		<form id="move_topic" method="post" action="{service controller=forum action=move_topic}">
-			<fieldset>
-				<div class="form-group">
-					<label for="movefrom" class="control-label">
-						{if $selectedTopics|count > 1}
-							{tr}Move these topics:{/tr}
-						{else}
-							{tr}Move this topic:{/tr}
-						{/if}
-					</label><br>
-					<div id="movefrom">
-						<ul>
-							{foreach from=$selectedTopics key=id item=name}
-								<li>{$name|escape}</li>
-							{/foreach}
-						</ul><br>
-					</div>
-					<label for="toId" class="control-label">
-						{tr _0=$forumName _1="<em>" _2="</em>"}
-							From the %1%0%2 forum to this forum:
-						{/tr}
-					</label><br><br>
-					<div class="col-lg-7">
-						<select class="form-control" name="toId">
-							{foreach from=$toList key=id item=name}
-								{if $id ne $forumId}
-									<option value="{$id|escape}">
-										{$name|escape}
-									</option>
-								{/if}
-							{/foreach}
-						</select>
-					</div>
+	<form id="move_topic" method="post" action="{service controller=forum action=move_topic}">
+		<fieldset>
+			<div class="form-group">
+				<label for="movefrom" class="control-label">
+					{if $items|count > 1}
+						{tr}Move these topics:{/tr}
+					{else}
+						{tr}Move this topic:{/tr}
+					{/if}
+				</label><br>
+				<div id="movefrom">
+					<ul>
+						{foreach from=$items key=id item=name}
+							<li>{$name|escape}</li>
+						{/foreach}
+					</ul><br>
 				</div>
-					<div class="submit">
-						<button type='submit' id="success" name="success" class="btn btn-primary success">
-							{tr}Move{/tr}
-						</button>
-						{$encodedTopics = json_encode($selectedTopics)}
-						<input type='hidden' name='forumtopic' value="{$encodedTopics|escape}">
-						{$encodedForums = json_encode($toList)}
-						<input type='hidden' name='toList' value="{$encodedForums|escape}">
-						<input type='hidden' name='forumId' value="{$forumId|escape}">
-					</div>
-			</fieldset>
-		</form>
-	{else}
-		<div class="alert alert-warning">
-			<h4>{tr}Oops!{/tr}</h4>
-			<p>
-				{tr}
-					No topics have been selected. Please check the topics you wish to move before clicking the move button.
-				{/tr}
-			</p>
-		</div>
-	{/if}
+				<label for="toId" class="control-label">
+					{tr _0=$forumName _1="<em>" _2="</em>"}
+						From the %1%0%2 forum to this forum:
+					{/tr}
+				</label><br><br>
+				<div class="col-lg-7">
+					<select class="form-control" name="toId">
+						{foreach from=$toList key=id item=name}
+							{if $id ne $forumId}
+								<option value="{$id|escape}">
+									{$name|escape}
+								</option>
+							{/if}
+						{/foreach}
+					</select>
+				</div>
+			</div>
+				<div class="submit">
+					<button type='submit' id="success" name="success" class="btn btn-primary success">
+						{tr}Move{/tr}
+					</button>
+					{$encodedItems = json_encode($items)}
+					<input type='hidden' name='items' value="{$encodedItems|escape}">
+					{$encodedForums = json_encode($toList)}
+					<input type='hidden' name='toList' value="{$encodedForums|escape}">
+					<input type='hidden' name='forumId' value="{$forumId|escape}">
+					<input type='hidden' name='ticket' value="{$ticket}">
+					<input type='hidden' name='daconfirm' value="y">
+				</div>
+		</fieldset>
+	</form>
 {/block}
