8377555c0fcf0d771a15ff4fa40ca9af9c8d017c
lindonb <lindonb@users.sourceforge.net>
2015-01-18 22:32:14 +0000

    [ENH] tablesorter: extend tablesorter to tiki-admin_forums.php.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@53551 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-admin_forums.php
+++ tiki-admin_forums.php
@@ -248,6 +248,9 @@ if (isset($_REQUEST["find"])) {
 }
 $smarty->assign('find', $find);
 $smarty->assign_by_ref('sort_mode', $sort_mode);
+if (isset($_REQUEST['numrows'])) {
+	$maxRecords = $_REQUEST['numrows'];
+}
 $channels = $commentslib->list_forums($offset, $maxRecords, $sort_mode, $find);
 $max = count($channels["data"]);
 for ($i = 0; $i < $max; $i++) {
@@ -262,6 +265,42 @@ for ($i = 0; $i < $max; $i++) {
 		$channels["data"][$i]["individual"] = 'n';
 	}
 }
+
+//add tablesorter sorting and filtering
+$tsOn = Table_Check::isEnabled(true);
+$smarty->assign('tsOn', $tsOn);
+$tsAjax = Table_Check::isAjaxCall();
+$smarty->assign('tsAjax', $tsAjax);
+static $iid = 0;
+++$iid;
+$ts_tableid = 'adminforums' . $iid;
+$smarty->assign('ts_tableid', $ts_tableid);
+if ($tsOn) {
+	$ts_countid = $ts_tableid . '-count';
+	$ts_offsetid = $ts_tableid . '-offset';
+	$smarty->assign('ts_countid', $ts_countid);
+	$smarty->assign('ts_offsetid', $ts_offsetid);
+}
+//initialize tablesorter
+if ($tsOn && !$tsAjax) {
+	//set tablesorter code
+	Table_Factory::build(
+		'TikiAdminForums',
+		array(
+			'id' => $ts_tableid,
+			'total' => $channels['cant'],
+			'ajax' => array(
+				'servercount' => array(
+					'id' => $ts_countid,
+				),
+				'serveroffset' => array(
+					'id' => $ts_offsetid,
+				),
+			),
+		)
+	);
+}
+
 $smarty->assign_by_ref('channels', $channels["data"]);
 $smarty->assign_by_ref('cant', $channels["cant"]);
 $cat_type = 'forum';
@@ -420,5 +459,9 @@ ask_ticket('admin-forums');
 // disallow robots to index page:
 $smarty->assign('metatag_robots', 'NOINDEX, NOFOLLOW');
 // Display the template
-$smarty->assign('mid', 'tiki-admin_forums.tpl');
-$smarty->display("tiki.tpl");
+if ($tsAjax) {
+	$smarty->display('tiki-admin_forums.tpl');
+} else {
+	$smarty->assign('mid', 'tiki-admin_forums.tpl');
+	$smarty->display("tiki.tpl");
+}
