865dea58fd3fe4be773bf98ee1cc2657658ef9bb
walper <walper@users.sourceforge.net>
2015-01-25 10:03:37 +0000

    [DOC] Started to comment parts of the code involved in the permission system since this is quite complex. Some minor refractoring.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@53635 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Perms/Check/Direct.php
+++ lib/core/Perms/Check/Direct.php
@@ -7,11 +7,25 @@
 
 class Perms_Check_Direct implements Perms_Check
 {
+	
+	/*
+	 * Check permission for a specific list of groups 
+	 * @param Perms_Resolver $resolver 
+	 * @param array $context - not used
+	 * @param string $name - name of the permission to check
+	 * @param array $groups - list of groups to check permission against
+	 * @return boolean $hasPermission- true|false  
+	 */
 	function check( Perms_Resolver $resolver, array $context, $name, array $groups ) 
 	{
 		return $resolver->check($name, $groups);
 	}
 
+	/*
+	 * Get the applicable groups.  
+	 * @params Perms_Resolver $resolver - not used
+	 * @return array $applicableGroups - List of groups  
+	 */
 	function applicableGroups( Perms_Resolver $resolver ) 
 	{
 		return $resolver->applicableGroups();
