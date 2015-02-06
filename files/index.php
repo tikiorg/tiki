f7b36b4d7b8d42a5861db05c642ed7727564eef1
jyhem <jyhem@users.sourceforge.net>
2015-02-06 00:50:16 +0000

    Copyright bumped to 2015 Happy new year (again)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@53804 b456876b-0849-0410-b77d-98878d47e9d5

--- files/index.php
+++ files/index.php
@@ -5,7 +5,7 @@
  * @ignore 
  * @package TikiWiki
  * @subpackage files
- * @copyright (c) Copyright 2002-2014 by authors of the Tiki Wiki CMS Groupware Project
+ * @copyright (c) Copyright 2002-2015 by authors of the Tiki Wiki CMS Groupware Project
  * @licence Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
  */
 // $Id$
