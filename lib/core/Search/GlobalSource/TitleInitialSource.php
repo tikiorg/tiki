fd8d2f0e100bd8cf2c6de595e6f5ad545b0de6c8
lphuberdeau <lphuberdeau@users.sourceforge.net>
2015-02-18 17:41:34 +0000

    [MOD] Adding a title_firstword field and facet
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@53986 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Search/GlobalSource/TitleInitialSource.php
+++ lib/core/Search/GlobalSource/TitleInitialSource.php
@@ -1,5 +1,5 @@
 <?php
-// (c) Copyright 2002-2015 by authors of the Tiki Wiki CMS Groupware Project
+// (c) Copyright 2002-2014 by authors of the Tiki Wiki CMS Groupware Project
 //
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -26,12 +26,14 @@ class Search_GlobalSource_TitleInitialSource implements Search_GlobalSource_Inte
 		return array(
 			Search_Query_Facet_Term::fromField('title_initial')
 				->setLabel(tr('Letter')),
+			Search_Query_Facet_Term::fromField('title_firstword')
+				->setLabel(tr('First Word')),
 		);
 	}
 
 	function getProvidedFields()
 	{
-		return array('title_initial');
+		return array('title_initial', 'title_firstword');
 	}
 
 	function getGlobalFields()
@@ -50,8 +52,11 @@ class Search_GlobalSource_TitleInitialSource implements Search_GlobalSource_Inte
 		$first = $substr($title, 0, 1);
 		$first = TikiLib::take_away_accent($first);
 		$letter = $strtoupper($first);
+		
+		$firstword = preg_replace('/^([^:\s]+).*$/u', '$1', $title);
 		return array(
 			'title_initial' => $typeFactory->identifier($letter),
+			'title_firstword' => $typeFactory->identifier($firstword),
 		);
 	}
 
