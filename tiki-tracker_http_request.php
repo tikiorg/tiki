817100b72d79f50d2b1618093e9568afdd540174
walper <walper@users.sourceforge.net>
2015-03-02 17:23:10 +0000

    [FIX] Fix trackerfield Dynamic-Item-List. IMPORTANT: This fix saves the itemId of the referenced Item, not the native value! Thus it will break existing usecases. Improvements/tested combinations: One can assign multiple DIL-Fields to one selection field (before it was just one). Filterfield Combinations: category/category, itemlink/itemlink, itemlink/textfield (other equal fieldtypes should work as well). Category can only be used with category for security. Displayfields (to make the selection from) : category, itemlist, itemlink, textfield (other simple fields should work).
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@54193 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-tracker_http_request.php
+++ tiki-tracker_http_request.php
@@ -10,6 +10,15 @@
 
 // TODO - refactor to ajax-services then KILME
 
+// If you make changes here check also Tracker/Field/DynamicList.php
+/**
+ * Handler for (XHR) requests from the trackerfield type dynamic itemslist. This handler does not save or update anything.
+ * It only updates the listfield when the $filterFieldValueHere changes, which happens if it is for example as selection change on a linkfield or category field.
+ * 
+ * @see lib/core/Tracker/Field/DynamicList.php for furher details.
+ */
+
+
 require_once ('tiki-setup.php');
 $trklib = TikiLib::lib('trk');
 if ($prefs['feature_categories'] == 'y') {
@@ -18,90 +27,149 @@ if ($prefs['feature_categories'] == 'y') {
 if ($prefs['feature_trackers'] != 'y') {
 	die;
 }
-$arrayTrackerId = explode(',', $_GET["trackerIdList"]);
-$arrayMandatory = explode(',', $_GET["mandatory"]);
-if (isset($_GET['selected'])) $arraySelected = explode(',', utf8_encode(rawurldecode($_GET["selected"])));
-$arrayFieldlist = explode(',', $_GET["fieldlist"]);
-$arrayFilterfield = explode(',', $_GET["filterfield"]);
-$arrayStatus = explode(',', $_GET["status"]);
-$arrayItem = explode(',', $_GET['item']);
-$sort_mode = 'f_' . $arrayFieldlist[0] . '_asc';
+$access = TikiLib::lib('access');
+
+$filterFieldIdHere =  isset($_GET["filterFieldIdHere"]) ? $_GET["filterFieldIdHere"] : null;
+$trackerIdThere =  isset($_GET["trackerIdThere"]) ? $_GET["trackerIdThere"] : null;
+$filterFieldValueHere =  isset($_GET["filterFieldValueHere"]) ? $_GET["filterFieldValueHere"] : null;
+$filterFieldIdThere = isset($_GET["filterFieldIdThere"]) ? $_GET["filterFieldIdThere"] : null;
+$listFieldIdThere = isset($_GET["listFieldIdThere"]) ? $_GET["listFieldIdThere"] : null;
+$statusThere = isset($_GET["statusThere"]) ? $_GET["statusThere"] : null;
+// needed when multiple fields are bound to the same selection i.e $filterFieldValueHere
+$insertId = isset($_GET["insertId"]) ? $_GET["insertId"] : null;
+// needed when the default should be passed back to the frontend
+$originalValue = isset($_GET["originalValue"]) ? $_GET["originalValue"] : null;
+
 header('Cache-Control: no-cache');
 header('content-type: application/x-javascript');
-Perms::bulk(array( 'type' => 'tracker' ), 'object', $arrayTrackerId);
 
 $json_return = array();
+// prepare response, we need to pass back some request data:
+//  $insertId: their could be more than one dynamic list field bound to the same $filterFieldValueHere. Need to know Dynamic Item List field we refer to.
+// $originalValue: to select the orginalValue (the value currently used for selecting the element), on changes of $filterFieldValueHere  
+$json_return['request'] = array(
+	'insertId' => $insertId,
+	'originalValue' => $originalValue
+);
+$json_return['response'] = array();
 
-for ($index = 0, $count_arrayTrackerId = count($arrayTrackerId); $index < $count_arrayTrackerId; $index++) {
-	$tikilib->get_perm_object($arrayTrackerId[$index], 'tracker');
-
-	if (!isset($_GET['selected'])) {
-		$selected = '';
-		$filtervalue = utf8_encode(rawurldecode($_GET["filtervalue"]));
-	} elseif (isset($_GET["filtervalue"])) {
-		$selected = $arraySelected[$index];
-		$filtervalue = $_GET["filtervalue"];
-	}
-	if (!empty($_GET['item'])) { // we want the value of field filterfield for item 
-		$filtervalue = $trklib-> get_item_value($arrayTrackerId[$index], $arrayItem[$index], $arrayFilterfield[$index]);
-
-		if (!$filtervalue) {
-			$otherField = $trklib->get_tracker_field($arrayFilterfield[$index]);
-			if ($otherField['type'] == 'r') {		// filterFieldIdThere is itemlink, so get the filtervalue from what that links to
-				$filtervalue = $trklib-> get_item_value($otherField['options_array'][0], $arrayItem[$index], $otherField['options_array'][1]);
-			} else if ($otherField['type'] == 'u') {
-				$exactvalue = $arrayItem[$index]; 
-			}
-		}
-	}
-
-	if ($filtervalue || !empty($exactvalue)) {
-		$xfields = $trklib->list_tracker_fields($arrayTrackerId[$index], 0, -1, 'name_asc', '');
-		foreach ($xfields["data"] as $idfi => $val) {
-			if ($xfields["data"][$idfi]["fieldId"] == $arrayFieldlist[$index]) {
-				$fid = $xfields["data"][$idfi]["fieldId"];
-				$dfid = $idfi;
-				break;
-			}
-		}
-		$listfields = array();
-		$listfields[$fid]['fieldId'] = $fid;
-		$listfields[$fid]['type'] = $xfields["data"][$dfid]["type"];
-		$listfields[$fid]['name'] = $xfields["data"][$dfid]["name"];
-		$listfields[$fid]['options'] = $xfields["data"][$dfid]["options"];
-		$listfields[$fid]['options_array'] = explode(',', $xfields["data"][$dfid]["options"]);
-		$listfields[$fid]['isMain'] = $xfields["data"][$dfid]["isMain"];
-		$listfields[$fid]['isTblVisible'] = $xfields["data"][$dfid]["isTblVisible"];
-		$listfields[$fid]['isHidden'] = $xfields["data"][$dfid]["isHidden"];
-		$listfields[$fid]['isSearchable'] = $xfields["data"][$dfid]["isSearchable"];
-
-		if ($filtervalue) {
-			$items = $trklib->list_items($arrayTrackerId[$index], 0, -1, $sort_mode, $listfields, $arrayFilterfield[$index], $filtervalue, $arrayStatus[$index]);
-		} elseif (!empty($exactvalue)) {
-			$items = $trklib->list_items($arrayTrackerId[$index], 0, -1, $sort_mode, $listfields, $arrayFilterfield[$index], $filtervalue, $arrayStatus[$index], '', $exactvalue);
-		}
-
-		if ($arrayMandatory[$index] != 'y') {
-			$json_return[] = array('', '');
-		}
-
-		foreach ($items['data'] as $item) {
-			$field = $item['field_values'][0];
-			if ($field['type'] === 'e' && !empty($field['list'])) {		// for category fields get the label not the value
-																		// possibly required for other field types after 11.0?
-				$label = $trklib->get_field_handler($field, $item)->renderOutput();
-				$label = str_replace('<br/>', ',', $label);				// categories can be many so replace html
-				$json_return[] = array($field['value'], $label);
-			} else if ($field['type'] == 'r') {
-				$label = $trklib->get_field_handler($field, $item)->renderOutput(array('list_mode' => 'csv'));
-				$json_return[] = array($field['value'], $label);
-			} else {
-				$json_return[] = array($field['value'], $field['value']);
-			}
-		}
-	}
+// if we do not have something to compare with we return empty result
+if (empty($filterFieldValueHere)) {
+	$access->output_serialized($json_return);
+	return;
 }
 
-$access = TikiLib::lib('access');
-$access->output_serialized($json_return);
+// start pre processing
+$filterFieldHere = $trklib->get_tracker_field($filterFieldIdHere);
+$filterFieldThere = $trklib->get_tracker_field($filterFieldIdThere);
+// set defaults - normalize $filterValueHere to $finalFilterValueHere, $filterFieldIdThere to $finalFilterFieldIdThere
+$finalFilterFieldIdThere = $filterFieldIdThere;
+$finalFilterValueHere = $filterFieldValueHere;
 
+// check which combination of filterFieldTypeHere and filterFieldTypeThere we have and how to deal with it.
+// case 'xyz' - xyz tested means: this combinination has been tested and works for both, trackerlistview and item templateview.
+switch ($filterFieldHere['type']) {
+	case 'e': // category tested
+		// only allow category with category. disallow any other category combination.
+		if ($filterFieldThere['type'] != 'e') {
+			$access->output_serialized($json_return);
+			return;
+		}
+	break;
+	
+	case 'r': // r = itemlink - disallow itemlink/category, allow itemlink/itemlink, itemlink/simplefield types like text
+		switch ($filterFieldThere['type']) {
+			case 'r': // r = itemlink tested 
+			break;
+			
+			case 't': // textfield tested
+			default:
+				$handler = $trklib->get_field_handler($filterFieldHere);
+				$optTrackerId = $handler->getOption('trackerId');
+				$optFieldId = $handler->getOption('fieldId');
+				$finalFilterValueHere = $trklib->get_item_value($optTrackerId, $filterFieldValueHere, $optFieldId);
+			break;
+		}
+	break;
+
+	default:
+		// if both field types are unknown but match, we assume that it could work
+		if ($filterFieldHere['type'] != $filterFieldThere['type']) {
+			$access->output_serialized($json_return);
+			return;
+		}
+	break;
+}
+
+// start main processing - $trackerIdThere is not used at all in get_items_list()
+$remoteItemIds = $trklib->get_items_list($trackerIdThere, $finalFilterFieldIdThere, $finalFilterValueHere, $statusThere);
+$listFieldThere = $trklib->get_tracker_field($listFieldIdThere);
+// special handling for itemList field. We would get always the same values on each iteration so we do it only one time.
+$itemListFirstRun = true;
+foreach ($remoteItemIds as $remoteItemId) {
+
+	$itemInfo = $trklib->get_tracker_item($remoteItemId);
+
+	// @TODO although it should be checked by the fieldhandler, verify that permissions on itemId level are respected. i.e restricted by a category field etc.
+	// however: something like this does not work: $permObject = $tikilib->get_perm_object($remoteItemId, 'trackeritem');
+	$hasPermission = true;
+	if (!$hasPermission) {
+		continue;
+	}
+	
+	$listFieldThere = array_merge($listFieldThere, array('value' => $itemInfo[$listFieldIdThere]));
+	$handler = $trklib->get_field_handler($listFieldThere, $itemInfo);
+	// do not inherit showlinks settings from remote items.
+	$context = array('showlinks' => 'n');
+	
+	// permissions are ok, now get the values depending on the fieldtype of $listFieldIdThere
+	switch ($listFieldThere['type']) {
+
+		case 'l': // itemlist tested
+			// itemlink can have multiple matches and we would the same matches on each iteration.
+			// so we only process one iteration and take the fields as selections out of it.
+			if (!$itemListFirstRun) {
+				break;
+			}
+
+			$valueFields = $handler->getFieldData();
+			if (is_array($valueFields)) {
+				// return each item of that list - requires match in DynamicList.php renderInnerOutput()
+				// note: we save the itemId of the item selected out of the list, not the value. Allows to keep the links consistant on changing values.
+				foreach ( $valueFields['items'] as $valueField => $labelField) {
+					$json_return['response'][] = array($valueField, $labelField);
+				}
+			}
+			$itemListFirstRun = false;
+		break;
+
+		case 'r': // itemlink tested
+			$valueField = $handler->getFieldData();
+			$labelField = $handler->renderOutput($context);
+			$json_return['response'][] = array($valueField['value'], $labelField);
+		break;
+			
+		case 'e': // category tested
+			// array selected_categories etc.
+			$valueField = $handler->getFieldData();
+			// for some reason, need to apply the values back, oterwise renderOutput does not return a value - bug?
+			$listFieldThere = array_merge($listFieldThere, $valueField);
+			$handler = $trklib->get_field_handler($listFieldThere, $itemInfo);
+			$labelField = $handler->renderOutput($context);
+			// we return all categories per itemId, without html, comma separated
+			$labelField = str_replace('<br/>', ', ', $labelField);
+			$json_return['response'][] = array($remoteItemId, $labelField);
+		break;
+		
+		// other fieldtypes
+		case 't': // textfield tested
+		default:		
+			$labelField = $handler->renderOutput($context);
+			$json_return['response'][] = array($remoteItemId, $labelField);
+		break;
+	} // switch
+
+} // foreach
+	
+$access->output_serialized($json_return);
+return;
