tikiorg/tiki

commit b748cadcb9895f3214d6e7fd4f9fa4979844f18f

[FIX] Should check perms to show recordings when room not created
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@49040 b456876b-0849-0410-b77d-98878d47e9d5

lib/wiki-plugins/wikiplugin_bigbluebutton.php
@@ -96,7 +96,11 @@ function wikiplugin_bigbluebutton( $data, $params )
 
 		if ( ! $bigbluebuttonlib->roomExists($meeting) ) {
 			if ( ! isset($_POST['bbb']) || $_POST['bbb'] != $meeting || ! $perms->bigbluebutton_create ) {
-				$smarty->assign('bbb_recordings', $bigbluebuttonlib->getRecordings($meeting));
+				if ($perms->bigbluebutton_view_rec && $params['recording']) {
+					$smarty->assign('bbb_recordings', $bigbluebuttonlib->getRecordings($meeting));
+				} else {
+					$smarty->assign('bbb_recordings', null);
+				}
 				return $smarty->fetch('wiki-plugins/wikiplugin_bigbluebutton_create.tpl');
 			}
 		}

commit 22f30d20288071efb8d96a537610a6cf082936e3

[FIX]Fix regressions introduced in r49040 and r47865 setting a requirement to have bbb plugin set to record new meetings in order to allow displaying previous recordings. Without this fix, the versatile behavior for bbb 0.8.x is prevented (allow a group of people to use the bbb system and watch a log of meetings from the admins, but only record meetings when admins set recordings to be stored for their official meetings, etc). Unclear yet how this might have to be adapted for bbb 0.9.x (which is unreleased as stable yet, anyway)
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@54326 b456876b-0849-0410-b77d-98878d47e9d5

lib/wiki-plugins/wikiplugin_bigbluebutton.php
@@ -95,7 +95,7 @@ function wikiplugin_bigbluebutton( $data, $params )
 
 		if ( ! $bigbluebuttonlib->roomExists($meeting) ) {
 			if ( ! isset($_POST['bbb']) || $_POST['bbb'] != $meeting || ! $perms->bigbluebutton_create ) {
-				if ($perms->bigbluebutton_view_rec && $params['recording']) {
+				if ($perms->bigbluebutton_view_rec) {
 					$smarty->assign('bbb_recordings', $bigbluebuttonlib->getRecordings($meeting));
 				} else {
 					$smarty->assign('bbb_recordings', null);
@@ -104,7 +104,7 @@ function wikiplugin_bigbluebutton( $data, $params )
 			}
 		}
 
-		if ($perms->bigbluebutton_view_rec && $params['recording']) {
+		if ($perms->bigbluebutton_view_rec) {
 			$smarty->assign('bbb_recordings', $bigbluebuttonlib->getRecordings($meeting));
 		} else {
 			$smarty->assign('bbb_recordings', null);