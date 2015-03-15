85ccb74cf29f0da90f9c0d9bc0f5bf3c9562f781
lindonb <lindonb@users.sourceforge.net>
2015-03-15 18:56:21 +0000

    [FIX] Partial reversal of r54400 - could cause other sefurl problems - better fix is under way (thanks Jonny)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@54420 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/servicelib.php
+++ lib/servicelib.php
@@ -46,7 +46,7 @@ class ServiceLib
 	{
 		global $prefs;
 
-		if ($prefs['feature_sefurl'] == 'y' || $params['modal'] == '1') {
+		if ($prefs['feature_sefurl'] == 'y') {
 			$url = "tiki-{$params['controller']}";
 
 			if (isset($params['action'])) {
