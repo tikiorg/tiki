ff549a41d9d9ebd03c10d92e5c1d8e24cf11b972
xavidp <xavidp@users.sourceforge.net>
2015-03-24 08:48:53 +0000

    [bp/r54716][FIX]Exclude param was not working. There is some warning still, but at least it works as expected for the end user, and param exclude is taken into account and its contents excluded from the list of results shown. Feel free to improve anyone with higer skills than me.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/13.x@54717 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_backlinks.php
+++ lib/wiki-plugins/wikiplugin_backlinks.php
@@ -37,6 +37,7 @@ class WikiPluginBackLinks extends PluginsLib
 	function run ($data, $params)
 	{
 		$wikilib = TikiLib::lib('wiki');
+		$exclude = $params['exclude'];
 		$params = $this->getParams($params, true);
 		$aInfoPreset = array_keys($this->aInfoPresetNames);
 		extract($params, EXTR_SKIP);
