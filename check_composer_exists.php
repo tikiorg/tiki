80c6411818db968398ad85717a9cde0fc9788eee
jyhem <jyhem@users.sourceforge.net>
2015-04-06 22:18:16 +0000

    [FIX] Do not display important text in white on white
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@54977 b456876b-0849-0410-b77d-98878d47e9d5

--- check_composer_exists.php
+++ check_composer_exists.php
@@ -65,7 +65,7 @@ function createPage($title, $content)
 			<div class="middle_outer">
 				<div name="middle" class="fixedwidth">
 					<div id="tiki-top" class="clearfix">
-						<h1 style="font-size: 30px; line-height: 30px; color: #fff; text-shadow: 3px 2px 0 #781437; margin: 8px 0 0 10px; padding: 0;">
+						<h1 style="font-size: 30px; line-height: 30px; text-shadow: 3px 2px 0 #ccccff; margin: 8px 0 0 10px; padding: 0;">
 							$title
 						</h1>
 					</div>
