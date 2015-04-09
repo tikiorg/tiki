a92c74a464b362c3794cc4d42a71c68a6cee5030
patrick-proulx <patrick-proulx@users.sourceforge.net>
2015-04-09 17:19:31 +0000

    [ENH] Added special permission check to trackeritem to block.permission smarty function
    block.permission only checked standard permissions and didn't check the tracker permissions for users to be able to modify/delete their own tracker items
    
    
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@55019 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/block.permission.php
+++ lib/smarty_tiki/block.permission.php
@@ -22,6 +22,32 @@ function smarty_block_permission($params, $content, $smarty, &$repeat)
 {
 	if ( $repeat ) return;
 
+	// Removing and Modifying a tracker item require a special permissions check
+	if ( $params['type'] == 'trackeritem' ) {
+		$removePerms = ['remove_tracker_items','remove_tracker_items_pending','remove_tracker_items_closed'];
+		$modifyPerms = ['modify_tracker_items','modify_tracker_items_pending','modify_tracker_items_closed'];
+
+		$trklib = TikiLib::lib('trk');
+		$itemInfo = $trklib->get_tracker_item($params['object']);
+
+		if (!$itemInfo){
+			return ""; //invalid tracker item.
+		}
+
+		$itemObject = Tracker_Item::fromInfo($itemInfo);
+
+		if ( in_array($params['name'],$removePerms) ){
+			if ($itemObject->canRemove()) {
+				return $content;
+			}
+		} elseif ( in_array($params['name'], $modifyPerms) ) {
+			if ( $itemObject->canModify() ) {
+				return $content;
+			}
+		}
+	}
+
+	//Standard permissions check
 	$context = array();
 
 	if ( isset( $params['type'], $params['object'] ) ) {
