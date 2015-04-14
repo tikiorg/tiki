72a1bdaf134660ca65649d7d651a4485461bec72
nkoth <nkoth@users.sourceforge.net>
2015-04-14 12:36:57 +0000

    [FIX] Avoid notices
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@55073 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Tiki/Profile.php
+++ lib/core/Tiki/Profile.php
@@ -399,7 +399,7 @@ class Tiki_Profile
 				if ( is_numeric($key) )
 					$old[] = $value;
 				else
-					$old[$key] = $this->mergeData($old[$key], $value);
+					$old[$key] = $this->mergeData(isset($old[$key]) ? $old[$key] : null, $value);	
 			}
 
 			return $old;
