7b1a4293ea27ac8e78449f42916291fca58d77a7
eromneg <eromneg@users.sourceforge.net>
2015-04-23 17:57:51 +0000

    [FIX] [bp 55068, 55152, 55182] restore Webmail handler which was 'lost' when the profile handler code was restructured and additional facilities added
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@55196 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Tiki/Profile/Installer.php
+++ lib/core/Tiki/Profile/Installer.php
@@ -134,6 +134,7 @@ class Tiki_Profile_Installer
 		'calendar' => 'Tiki_Profile_InstallHandler_Calendar',
 		'extwiki' => 'Tiki_Profile_InstallHandler_ExtWiki',
 		'webmail_account' => 'Tiki_Profile_InstallHandler_WebmailAccount',
+		'webmail' => 'Tiki_Profile_InstallHandler_Webmail',
 		'sheet' => 'Tiki_Profile_InstallHandler_Sheet',
 		'rating_config' => 'Tiki_Profile_InstallHandler_RatingConfig',
 		'rating_config_set' => 'Tiki_Profile_InstallHandler_RatingConfigSet',
