734a5e45d3bb69109191529565e5c63cc7ec4473
eromneg <eromneg@users.sourceforge.net>
2015-04-23 16:39:18 +0000

    [ENH] [bp 54960, 54961, 54962 & 54994] extend and tidy up CATORPHANS plugin and associated .tpl to extend the object types it will handle with an associated update to categlib.php
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@55189 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_catorphans.php
+++ lib/wiki-plugins/wikiplugin_catorphans.php
@@ -1,5 +1,5 @@
 <?php
-// (c) Copyright 2002-2013 by authors of the Tiki Wiki CMS Groupware Project
+// (c) Copyright 2002-2015 by authors of the Tiki Wiki CMS Groupware Project
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -17,11 +17,16 @@ function wikiplugin_catorphans_info()
 			'objects' => array(
 				'required' => false,
 				'name' => tra('Object'),
-				'description' => tra('Currently, only works with wiki pages (set to wiki (Wiki Pages) by default)'),
+				'description' => tra('Currently, only works with wiki pages, file galleries, articles, trackers, blogs, calendars, and forums (set to wiki (Wiki Pages) by default)'),
 				'default' => 'wiki',
 				'options' => array(
-					array('text' => '', 'value' => ''), 
 					array('text' => tra('Wiki Pages'), 'value' => 'wiki'),
+					array('text' => tra('File Galleries'), 'value' => 'file gallery'),
+					array('text' => tra('Articles'), 'value' => 'article'),
+					array('text' => tra('Trackers'), 'value' => 'tracker'),
+					array('text' => tra('Blogs'), 'value' => 'blog'),
+					array('text' => tra('Calendars'), 'value' => 'calendar'),
+					array('text' => tra('Forums'), 'value' => 'forum'),
 				) 
 			),
 			'max' => array(
@@ -42,59 +47,47 @@ function wikiplugin_catorphans_info()
 
 function wikiplugin_catorphans($data, $params)
 {
-	global $dbTiki, $smarty, $tikilib, $prefs, $access;
+	global $prefs;
+	$access = TikiLib::lib('access');
 	$access->check_feature('feature_categories');
-	global $categlib; require_once ('lib/categories/categlib.php');
+	$smarty = TikiLib::lib('smarty');
+	$tikilib = TikiLib::lib('tiki');
+	$categlib = TikiLib::lib('categ');
 
 	$default = array('offset'=>0, 'max'=>$prefs['maxRecords'], 'objects'=>'wiki');
 	$params = array_merge($default, $params);
 	extract($params, EXTR_SKIP);
+	// check required objects parameter
+	if ($params['objects'] !== 'wiki' && $params['objects'] !== 'file gallery' && $params['objects'] !== 'article' && $params['objects'] !== 'tracker' && $params['objects'] !== 'blog' && $params['objects'] !== 'calendar' && $params['objects'] !== 'forum') {
+		return ("<span class='error'>Wrong objects parameter - only wiki, file gallery, article, tracker, blog, calendar, and forum allowed at present</span>");
+	}
 
-	// array for converting long type names (as in database) to short names (as used in plugin)
-	$typetokens = array(
-		"article" => "article",
-		"blog" => "blog",
-		"faq" => "faq",
-		"file gallery" => "fgal",
-		"image gallery" => "igal",
-		"newsletter" => "newsletter",
-		"poll" => "poll",
-		"quiz" => "quiz",
-		"survey" => "survey",
-		"tracker" => "tracker",
-		"wiki page" => "wiki"
-	);
-
-	// TODO: move this array to a lib
-	// array for converting long type names to translatable headers (same strings as in application menu)
-	$typetitles = array(
-		"article" => "Articles",
-		"blog" => "Blogs",
-		"directory" => "Directory",
-		"faq" => "FAQs",
-		"file gallery" => "File Galleries",
-		"forum" => "Forums",
-		"image gallery" => "Image Gals",
-		"newsletter" => "Newsletters",
-		"poll" => "Polls",
-		"quiz" => "Quizzes",
-		"survey" => "Surveys",
-		"tracker" => "Trackers",
-		"wiki page" => "Wiki"
-	);
 	if (!empty($_REQUEST['offset'])) {
 		$offset = $_REQUEST['offset'];
 	}
 
-	// currently only supports display of wiki pages
+	// assign the various smarty variables and get the data for the individual object types
+	$smarty->assign('objecttype', $objects);
 	if ($objects == 'wiki') {
-		$listpages = $tikilib->list_pages($offset, $max, 'pageName_asc', '', '', true, true, false, false, array('noCateg' => true));
-		$smarty->assign_by_ref('orphans', $listpages['data']);
-		$smarty->assign('pagination', array('cant'=>$listpages['cant'], 'step'=>$max, 'offset'=>$offset));
-		$out = '~np~' . $smarty->fetch('wiki-plugins/wikiplugin_catorphans.tpl') . '~/np~';
-		$smarty->assign('pagination', null);
-		return $out;
+		$listobjects = $categlib->get_catorphan_object_type($offset, $max, 'wiki page','pages','page_Id');
+	} elseif ($objects == 'file gallery') {
+		$listobjects = $categlib->get_catorphan_object_type($offset, $max, 'file gallery','file_galleries','galleryId');
+	} elseif ($objects == 'article') {
+		$listobjects = $categlib->get_catorphan_object_type($offset, $max, 'article','articles','articleId');		
+	} elseif ($objects == 'tracker') {
+		$listobjects = $categlib->get_catorphan_object_type($offset, $max, 'tracker','trackers','trackerId');
+	} elseif ($objects == 'blog') {
+		$listobjects = $categlib->get_catorphan_object_type($offset, $max, 'blog','blogs','blogId');
+	} elseif ($objects == 'calendar') {
+		$listobjects = $categlib->get_catorphan_object_type($offset, $max, 'calendar','calendars','calendarId');
+	} elseif ($objects == 'forum') {
+		$listobjects = $categlib->get_catorphan_object_type($offset, $max, 'forum','forums','forumId');
 	}
-	return '';
+	$smarty->assign_by_ref('orphans', $listobjects['data']);
+	$smarty->assign('pagination', array('cant'=>$listobjects['cant'], 'step'=>$max, 'offset'=>$offset));
+	$smarty->assign('totalcount', $listobjects['countall']);
+	$out = '~np~' . $smarty->fetch('wiki-plugins/wikiplugin_catorphans.tpl') . '~/np~';	
+	$smarty->assign('pagination', null);
+	return $out;
 
 }
