b817f47457f2e6520ab6684fd0c5663513fc34d8
eromneg <eromneg@users.sourceforge.net>
2015-05-12 17:29:42 +0000

    [ENH] add a a new count_users_consolidated function to give the consolidated number of users in a set of Groups
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@55416 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/userslib.php
+++ lib/userslib.php
@@ -6878,6 +6878,15 @@ class UsersLib extends TikiLib
 
 		return $rv[$group];
 	}
+	
+	function count_users_consolidated($groups)
+	{
+		$groupset = implode("','",$groups);
+		$query = "select userId from `users_usergroups` where `groupName` in ('".$groupset."')";
+		$result = $this->fetchAll($query, array());
+		$resultcons = array_unique(array_column($result, 'userId'));
+		return count($resultcons);
+	}	
 
 	function related_users($user, $max = 10, $type = 'wiki')
 	{
