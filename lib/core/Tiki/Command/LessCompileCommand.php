379c3f259899f789d33e7674802cbebb2192dbb8
jonnybradley <jonnybradley@users.sourceforge.net>
2015-06-09 16:16:29 +0000

    [FIX] less: More on gettting "php console.php less:compile" to run under Windows - use the real lessc, file not the unix-only symlink (seems less.php fails later anyway, but this gets us one step closer! Thanks chibaguy)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@55680 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Tiki/Command/LessCompileCommand.php
+++ lib/core/Tiki/Command/LessCompileCommand.php
@@ -77,7 +77,7 @@ class LessCompileCommand  extends Command
                     if (file_exists($css_file && filemtime($css_file) >= filemtime($less_file))) {
                         continue;
                     }
-					$command = "php vendor/bin/lessc $less_file $css_file";
+					$command = "php vendor/oyejorge/less.php/bin/lessc $less_file $css_file";
 					$output->writeln($command);
 					$result = shell_exec($command);
                     $result = str_replace(array("\r", "\n"), '', $result);
