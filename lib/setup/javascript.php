ca3db3b4df6c9d8d4e78c14c820d8c7fc1c2f929
jonnybradley <jonnybradley@users.sourceforge.net>
2015-09-04 12:18:26 +0000

    [FIX] javascript: Don't reload the page if the old runs_before_js_detect cookie is present to avoid endless reload loops in some cases
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@56140 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/javascript.php
+++ lib/setup/javascript.php
@@ -27,10 +27,9 @@ if ($prefs['disableJavascript'] == 'y' ) {
 	$prefs['javascript_enabled'] = 'y';
 	setCookieSection('javascript_enabled_detect', '', '', time() - 3600);	// remove the test cookie
 } else {
-	if (isset($_COOKIE['runs_before_js_detect'])) {	// pre-tiki 14 method detected, delete both tests and reload
+	if (isset($_COOKIE['runs_before_js_detect'])) {	// pre-tiki 14 method detected, delete both test cookies (reloading here caused redirect a loop in some case)
 		setcookie('runs_before_js_detect', '', time() - 3600);
 		setcookie('javascript_enabled_detect', '', time() - 3600);
-		$access->redirect();
 	}
 	$prefs['javascript_enabled'] = '';
 }
