07f09271730e5f68b3596bdb3d7e7f4603d7d71f
xavidp <xavidp@users.sourceforge.net>
2015-09-16 12:53:22 +0000

    [NEW]Two new simple functions to compared whether a number is less than (or more than) another one. Thanks jonnyb and nkoth for feedback and hints.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@56195 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/core/Math/Formula/Function/MoreThan.php
@@ -0,0 +1,28 @@
+<?php
+// (c) Copyright 2002-2015 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+class Math_Formula_Function_MoreThan extends Math_Formula_Function
+{
+	function evaluate( $element )
+	{
+
+		if (count($element) > 2) {
+			$this->error(tr('Too many arguments on more than.'));
+		}
+
+		$reference = $this->evaluateChild($element[0]);
+
+		$mynumber = $this->evaluateChild($element[1]);
+			if ($mynumber > $reference) {
+				return false;
+			}
+
+
+		return true;
+	}
+}
+
