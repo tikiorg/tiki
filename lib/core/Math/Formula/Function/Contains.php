ac4a385623248811520f4a9e6604c94aff0ad004
xavidp <xavidp@users.sourceforge.net>
2015-09-29 11:37:10 +0000

    [bp/r56184][ENH] Add Contains function for activity stream rules [bp/r56354][FIX]Add svn property keywords Id so that svn info is updated inside the php file
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@56355 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/core/Math/Formula/Function/Contains.php
@@ -0,0 +1,22 @@
+<?php
+// (c) Copyright 2002-2015 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+class Math_Formula_Function_Contains extends Math_Formula_Function
+{
+    function evaluate( $element )
+    {
+        $reference = $this->evaluateChild($element[0]);
+        $pattern = $element[1];
+
+        if (preg_match("|" . preg_quote($pattern) . "|", $reference)) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
