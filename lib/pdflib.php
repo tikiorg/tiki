19785bba2d5754bf4a08b5c39f45dd13c9334cbd
jonnybradley <jonnybradley@users.sourceforge.net>
2015-10-07 11:14:29 +0000

    [bp/r56421][ENH] pdf: Add support for Weasyprint - see http://weasyprint.org for more info (thanks marclaporte)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@56429 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/pdflib.php
+++ lib/pdflib.php
@@ -23,8 +23,14 @@ class PdfGenerator
 
 		if ( $prefs['print_pdf_from_url'] == 'webkit' ) {
 			$path = $prefs['print_pdf_webkit_path'];
-			if ( ! empty($path) && is_executable($path) ) {
+			if (!empty($path) && is_executable($path)) {
 				$this->mode = 'webkit';
+				$this->location = $path;
+			}
+		} else if ($prefs['print_pdf_from_url'] == 'weasyprint') {
+			$path = $prefs['print_pdf_weasyprint_path'];
+			if (!empty($path) && is_executable($path)) {
+				$this->mode = 'weasyprint';
 				$this->location = $path;
 			}
 		} elseif ( $prefs['print_pdf_from_url'] == 'webservice' ) {
@@ -97,6 +103,45 @@ class PdfGenerator
 		$quotedCommand = '"'.$this->location.'"';
 		
 		`$quotedCommand -q $arg $quotedFilename`;
+
+		// Read the out file
+		$pdf = file_get_contents($filename);
+
+		// Delete the outfile
+		unlink($filename);
+
+		return $pdf;
+	}
+
+   /**
+     * @param $url
+     * @return mixed
+     */
+    private function weasyprint( $url )
+	{
+		// Make sure shell_exec is available
+		if (!function_exists('shell_exec')) {
+			die(tra('Required function shell_exec is not enabled.'));
+		}
+
+		// escapeshellarg will replace all % characters with spaces on Windows
+		// So, decode the URL before sending it to the commandline
+		$urlDecoded = urldecode($url);
+		$arg = escapeshellarg($urlDecoded);
+
+		// Write a temporary file, instead of using stdout
+		// There seemed to be encoding issues when using stdout (on Windows 7 64 bit).
+
+		// Use temp/public. It is cleaned up during a cache clean, in case some files are left
+		$filename = 'temp/public/out'.rand().'.pdf';
+
+		// Run shell_exec command to generate out file
+		// NOTE: this requires write permissions
+		$quotedFilename = '"'.$filename.'"';
+		$quotedCommand = '"'.$this->location.'"';
+
+		// redirect STDERR to null with 2>/dev/null becasue it outputs plenty of irrelevant warnings (hopefully nothing critical)
+		`$quotedCommand $arg $quotedFilename 2>/dev/null`;
 
 		// Read the out file
 		$pdf = file_get_contents($filename);
