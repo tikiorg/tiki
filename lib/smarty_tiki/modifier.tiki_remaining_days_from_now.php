ff506bd975bbac235536eaab37e5d7a1c97d0c25
jonnybradley <jonnybradley@users.sourceforge.net>
2016-01-06 18:54:18 +0000

    [bp/r56913][FIX] tiki_remaining_days_from_now: If the target time is less than 24 hours ahead show "Today" instead of just "-" [bp/r56914][MOD] tiki_remaining_days_from_now: Remove misleading comment about "human" format (coming soon) and unused pref var
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@57121 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/modifier.tiki_remaining_days_from_now.php
+++ lib/smarty_tiki/modifier.tiki_remaining_days_from_now.php
@@ -11,18 +11,28 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 	exit;
 }
 
+/**
+ * @param int $time future time to calculate number of day to go
+ * @param string $format PHP date format for the displayed date set in $time.
+ *
+ * @return string
+ */
 function smarty_modifier_tiki_remaining_days_from_now($time, $format)
 {
-	global $prefs, $tikilib;
+	global $tikilib;
 
-	$iNbDayBetween = round(( $time - $tikilib->now ) / ( 60 * 60 * 24 ));
-	if ( $iNbDayBetween > 0 ) {
-		return sprintf(
-			( $iNbDayBetween > 1 ? tra('in %s days, the %s') : tra('in %s day, the %s') ),
-			'<b>' . $iNbDayBetween . '</b>',
+	$iNbDayBetween = ($time - $tikilib->now) / (60 * 60 * 24);
+
+	if ($iNbDayBetween > 1) {
+		$result = sprintf(
+			($iNbDayBetween > 1 ? tra('in %s days, the %s') : tra('in %s day, the %s')),
+			'<b>' . round($iNbDayBetween) . '</b>',
 			$tikilib->date_format($format, $time)
 		);
+	} else if ($iNbDayBetween > 0) {
+		$result = tra('Today');
 	} else {
-		return '-';
+		$result = '-';
 	}
+	return $result;
 }
