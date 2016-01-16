14d934a713c836fedd2480221fc970f1a49f5d94
xavidp <xavidp@users.sourceforge.net>
2016-01-16 15:32:37 +0000

    [FIX]Fix fatal errors due to single quotes surrounding strings to be translated, and single quotes also inside the string in English itself!
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@57204 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/prefs/user.php
+++ lib/prefs/user.php
@@ -13,24 +13,24 @@ function prefs_user_list($partial = false)
 	$fieldFormat = '{title} ({tracker_name})';
 	return array(
 		'user_show_realnames' => array(
-			'name' => tra('Show user\'s real name instead of username (log-in name), when possible'),
-			'description' => tra('Show user\'s real name instead of username (log-in name), when possible'),
+			'name' => tra("Show user\'s real name instead of username (log-in name), when possible"),
+			'description' => tra("Show user\'s real name instead of username (log-in name), when possible"),
 			'help' => 'User+Preferences',
 			'type' => 'flag',
 			'default' => 'n',
 		),
 		'user_unique_email' => array(
-			'name' => tra('User e-mails must be unique'),
-			'description' => tra('User e-mails must be unique'),
+			'name' => tra("User e-mails must be unique"),
+			'description' => tra("User e-mails must be unique"),
 			'help' => 'User+Preferences',
 			'type' => 'flag',
 			'default' => 'n',
 		),
 		'user_tracker_infos' => array(
-			'name' => tra('Display user tracker information on the user information page'),
-			'description' => tra('Display user tracker information on the user information page'),
+			'name' => tra("Display user tracker information on the user information page"),
+			'description' => tra("Display user tracker information on the user information page"),
 			'help' => 'User+Tracker',
-			'hint' => tra('Input the user tracker ID then field IDs to be shown, all separated by commas. Example: 1,1,2,3,4 (user tracker ID 1 followed by field IDs 1-4)'),
+			'hint' => tra("Input the user tracker ID then field IDs to be shown, all separated by commas. Example: 1,1,2,3,4 (user tracker ID 1 followed by field IDs 1-4)"),
 			'type' => 'text',
 			'size' => '50',
 			'dependencies' => array(
@@ -40,25 +40,25 @@ function prefs_user_list($partial = false)
 			'profile_reference' => 'prefs_user_tracker_references',
 		),
 		'user_assigned_modules' => array(
-			'name' => tra('Users can configure modules'),
+			'name' => tra("Users can configure modules"),
 			'help' => 'Users+Configure+Modules',
 			'type' => 'flag',
 			'default' => 'n',
 		),	
 		'user_flip_modules' => array(
-			'name' => tra('Users can open and close the modules'),
+			'name' => tra("Users can open and close the modules"),
 			'help' => 'Users+Shade+Modules',
 			'type' => 'list',
-			'description' => tra('Allows users to open and close modules using the icon in the module header.'),
+			'description' => tra("Allows users to open and close modules using the icon in the module header."),
 			'options' => array(
-				'y' => tra('Always'),
-				'module' => tra('Module decides'),
-				'n' => tra('Never'),
+				'y' => tra("Always"),
+				'module' => tra("Module decides"),
+				'n' => tra("Never"),
 			),
 			'default' => 'module',
 		),
 		'user_store_file_gallery_picture' => array(
-			'name' => tra('Store full-size copy of profile picture in file gallery'),
+			'name' => tra("Store full-size copy of profile picture in file gallery"),
 			'help' => 'User+Preferences',
 			'keywords' => 'avatar',
 			'type' => 'flag',
@@ -66,21 +66,21 @@ function prefs_user_list($partial = false)
 			'dependencies' => ['user_picture_gallery_id',],
 		),
 		'user_small_avatar_size' => array(
-			'name' => tra('Size of the small avatar stored for users.'),
+			'name' => tra("Size of the small avatar stored for users."),
 			'help' => 'User+Preferences',
 			'type' => 'text',
 			'filter' => 'digits',
 			'default' => '45',
 		),
 		'user_small_avatar_square_crop' => array(
-			'name' => tra('Sets whether the image avatar thumbnail should be cropped to a square'),
+			'name' => tra("Sets whether the image avatar thumbnail should be cropped to a square"),
 			'help' => 'User+Preferences',
 			'type' => 'flag',
 			'default' => 'n',
 		),
 		'user_picture_gallery_id' => array(
-			'name' => tra('File gallery in which to store full-size profile picture'),
-			'description' => tra('Enter the gallery ID here. Please create a dedicated gallery that is admin-only for security, or make sure gallery permissions are set so that only admins can edit.'),
+			'name' => tra("File gallery in which to store full-size profile picture"),
+			'description' => tra("Enter the gallery ID here. Please create a dedicated gallery that is admin-only for security, or make sure gallery permissions are set so that only admins can edit."),
 			'help' => 'User+Preferences',
 			'keywords' => 'avatar',
 			'type' => 'text',
@@ -91,8 +91,8 @@ function prefs_user_list($partial = false)
 			'dependencies' => ['feature_file_galleries',],
 		),
 		'user_default_picture_id' => array(
-			'name' => tra('File ID of default profile picture'),
-			'deacription' => tra('File ID of image to use in file gallery as the profile picture if user has no profile picture in file galleries'),
+			'name' => tra("File ID of default profile picture"),
+			'deacription' => tra("File ID of image to use in file gallery as the profile picture if user has no profile picture in file galleries"),
 			'keywords' => 'avatar',
 			'help' => 'User+Preferences',
 			'type' => 'text',
@@ -103,8 +103,8 @@ function prefs_user_list($partial = false)
 			'profile_reference' => 'file',
 		),
 		'user_who_viewed_my_stuff' => array(
-			'name' => tra('Display who has viewed "my items" on the user information page'),
-			'description' => tra('This requires activation of tracking of views for various items in the action log'),
+			'name' => tra("Display who has viewed \"my items\" on the user information page"),
+			'description' => tra("This requires activation of tracking of views for various items in the action log"),
 			'type' => 'flag',
 			'dependencies' => array(
 				'feature_actionlog',
@@ -112,16 +112,16 @@ function prefs_user_list($partial = false)
 			'default' => 'n',
 		),
 		'user_who_viewed_my_stuff_days' => array(
-			'name' => tra('Number of days to consider in displaying "who viewed my items"'),
-			'description' => tra('Number of days before the current day to consider when displayng "who viewed my items"'),
+			'name' => tra("Number of days to consider in displaying \"who viewed my items\""),
+			'description' => tra("Number of days before the current day to consider when displayng \"who viewed my items\""),
 			'type' => 'text',
 			'filter' => 'digits',
 			'size' => '4',
 			'default' => 90,
 		),
 		'user_who_viewed_my_stuff_show_others' => array(
-			'name' => tra('Show to others "who viewed my items" on the user information page'),
-			'description' => tra('Show to others "who viewed my items" on the user information page. Admins can always see this information.'),
+			'name' => tra("Show to others \"who viewed my items\" on the user information page"),
+			'description' => tra("Show to others \"who viewed my items\" on the user information page. Admins can always see this information."),
 			'type' => 'flag',
 			'dependencies' => array(
 				'user_who_viewed_my_stuff',
@@ -129,15 +129,15 @@ function prefs_user_list($partial = false)
 			'default' => 'n',
 		),
 		'user_list_order' => array(
-			'name' => tra('Sort Order'),
+			'name' => tra("Sort Order"),
 			'type' => 'list',
 			'options' => $partial ? array() : UserListOrder(),
 			'default' => 'score_desc',
 		),
 		'user_register_prettytracker' => array(
-			'name' => tra('Use pretty trackers for registration form'),
+			'name' => tra("Use pretty trackers for registration form"),
 			'help' => 'User+Tracker',
-			'description' => tra('Use pretty trackers for registration form'),
+			'description' => tra("Use pretty trackers for registration form"),
 			'type' => 'flag',
 			'dependencies' => array(
 				'userTracker',
@@ -145,8 +145,8 @@ function prefs_user_list($partial = false)
 			'default' => 'n',
 		),
 		'user_register_prettytracker_tpl' => array(
-			'name' => tra('Registration pretty tracker template'),
-			'description' => tra('Use wiki page name or template file with .tpl extension'),
+			'name' => tra("Registration pretty tracker template"),
+			'description' => tra("Use wiki page name or template file with .tpl extension"),
 			'type' => 'text',
 			'size' => '20',
 			'dependencies' => array(
@@ -156,9 +156,9 @@ function prefs_user_list($partial = false)
 			'profile_reference' => 'wiki_page',
 		),
 		'user_register_prettytracker_output' => array(
-			'name' => tra('Output the registration results'),
+			'name' => tra("Output the registration results"),
 			'help' => 'User+Tracker',
-			'description' => tra('Use a wiki page as template to output the registration results to'),
+			'description' => tra("Use a wiki page as template to output the registration results to"),
 			'type' => 'flag',
 			'default' => 'n',
 			'dependencies' => array(
@@ -167,8 +167,8 @@ function prefs_user_list($partial = false)
 			'profile_reference' => 'wiki_page',
 		),
 		'user_register_prettytracker_outputwiki' => array(
-			'name' => tra('Output registration pretty tracker template'),
-			'description' => tra('Wiki page only'),
+			'name' => tra("Output registration pretty tracker template"),
+			'description' => tra("Wiki page only"),
 			'type' => 'text',
 			'size' => '20',
 			'default' => '',
@@ -178,8 +178,8 @@ function prefs_user_list($partial = false)
 			'profile_reference' => 'wiki_page',
 		),
 		'user_register_prettytracker_outputtowiki' => array(
-			'name' => tra('Page name field ID'),
-			'description' => tra('User tracker's field ID whose value is used as output page name'),
+			'name' => tra("Page name field ID"),
+			'description' => tra("User tracker's field ID whose value is used as output page name"),
 			'type' => 'text',
 			'size' => '20',
 			'default' => '',
@@ -190,8 +190,8 @@ function prefs_user_list($partial = false)
 			'format' => $fieldFormat,
 		),
 		'user_trackersync_trackers' => array(
-			'name' => tra('User tracker IDs to sync prefs from'),
-			'description' => tra('Enter the IDs separated by commas of trackers to sync user prefs from'),
+			'name' => tra("User tracker IDs to sync prefs from"),
+			'description' => tra("Enter the IDs separated by commas of trackers to sync user prefs from"),
 			'type' => 'text',
 			'size' => '10',
 			'dependencies' => array(
@@ -202,8 +202,8 @@ function prefs_user_list($partial = false)
 			'profile_reference' => 'tracker',
 		),
 		'user_trackersync_realname' => array(
-			'name' => tra('Tracker field IDs to sync the "real name" pref from'),
-			'description' => tra('Enter the IDs separated by commas in priority of being chosen, each item can concatenate multiple fields using +, e.g. 2+3,4'),
+			'name' => tra("Tracker field IDs to sync the "real name" pref from"),
+			'description' => tra("Enter the IDs separated by commas in priority of being chosen, each item can concatenate multiple fields using +, e.g. 2+3,4"),
 			'type' => 'text',
 			'size' => '10',
 			'dependencies' => array(
@@ -215,8 +215,8 @@ function prefs_user_list($partial = false)
 			'format' => $fieldFormat,
 		),
 		'user_trackersync_geo' => array(
-			'name' => tra('Synchronize long/lat/zoom to location field'),
-			'description' => tra('Synchronize user geolocation prefs to main location field'),
+			'name' => tra("Synchronize long/lat/zoom to location field"),
+			'description' => tra("Synchronize user geolocation prefs to main location field"),
 			'type' => 'flag',
 			'dependencies' => array(
 				'userTracker',
@@ -225,7 +225,7 @@ function prefs_user_list($partial = false)
 			'default' => 'n',
 		),
 		'user_trackersync_lang' => array(
-			'name' => tra('Change user system language when changing user tracker item language'),
+			'name' => tra("Change user system language when changing user tracker item language"),
 			'type' => 'flag',
 			'dependencies' => array(
 				'userTracker',
@@ -234,7 +234,7 @@ function prefs_user_list($partial = false)
 			'default' => 'n',
 		),
 		'user_tracker_auto_assign_item_field' => array(
-			'name' => tra('Assign a user tracker item when registering if email equals this field'),
+			'name' => tra("Assign a user tracker item when registering if email equals this field"),
 			'type' => 'text',
 			'filter' => 'digits',
 			'dependencies' => array(
@@ -245,67 +245,67 @@ function prefs_user_list($partial = false)
 			'format' => $fieldFormat,
 		),
 		'user_selector_threshold' => array(
-			'name' => tra('Maximum number of users to show in drop-down lists'),
-			'description' => tra('Prevents out-of-memory and performance issues when the user list is very large, by using a jQuery autocomplete text input box.'),
+			'name' => tra("Maximum number of users to show in drop-down lists"),
+			'description' => tra("Prevents out-of-memory and performance issues when the user list is very large, by using a jQuery autocomplete text input box."),
 			'type' => 'text',
 			'size' => '5',
 			'dependencies' => array('feature_jquery_autocomplete'),
 			'default' => 50,
 		),
 		'user_selector_realnames_tracker' => array(
-			'name' => tra('Show user\'s real name instead of log-in name in the autocomplete selector in trackers'),
-			'description' => tra('Use user\'s real name instead of log-in name in the autocomplete selector in trackers'),
+			'name' => tra("Show user\'s real name instead of log-in name in the autocomplete selector in trackers"),
+			'description' => tra("Use user\'s real name instead of log-in name in the autocomplete selector in trackers"),
 			'type' => 'flag',
 			'dependencies' => array('feature_jquery_autocomplete', 'user_show_realnames', 'feature_trackers'),
 			'default' => 'n',
 		),
 		'user_selector_realnames_messu' => array(
-			'name' => tra('Show user\'s real name instead of log-in name in the autocomplete selector in the messaging feature'),
-			'description' => tra('Use user\'s real name instead of log-in name in the autocomplete selector in the messaging feature'),
+			'name' => tra("Show user\'s real name instead of log-in name in the autocomplete selector in the messaging feature"),
+			'description' => tra("Use user\'s real name instead of log-in name in the autocomplete selector in the messaging feature"),
 			'type' => 'flag',
 			'dependencies' => array('feature_jquery_autocomplete', 'user_show_realnames', 'feature_messages'),
 			'default' => 'n',
 		),
 		'user_favorites' => array(
-			'name' => tra('User Favorites'),
-			'description' => tra('Allows users to flag content as their favorite.'),
+			'name' => tra("User Favorites"),
+			'description' => tra("Allows users to flag content as their favorite."),
 			'type' => 'flag',
 			'default' => 'n',
 		),
 		'user_likes' => array(
-			'name' => tra('User Likes'),
-			'description' => tra('Allows for users to "like" content.'),
+			'name' => tra("User Likes"),
+			'description' => tra("Allows for users to \"like\" content."),
 			'type' => 'flag',
 			'default' => 'n',
 		),
 		'user_must_choose_group' => array(
-			'name' => tra('Users must choose a group at registration'),
-			'description' => tra('Users cannot register without choosing one of the groups indicated above.'),
+			'name' => tra("Users must choose a group at registration"),
+			'description' => tra("Users cannot register without choosing one of the groups indicated above."),
 			'type' => 'flag',
 			'default' => 'n',
 		),
 		'user_in_search_result' => array(
-			'name' => tr('Users available in search results'),
-			'description' => tr('Users available within search results. Content related to the user will be included in the index.'),
+			'name' => tr("Users available in search results"),
+			'description' => tr("Users available within search results. Content related to the user will be included in the index."),
 			'type' => 'list',
 			'dependencies' => array('feature_search'),
 			'options' => array(
-				'none' => tr('None'),
-				'all' => tr('All'),
-				'public' => tr('Public'),
+				'none' => tr("None"),
+				'all' => tr("All"),
+				'public' => tr("Public"),
 			),
 			'default' => 'none',
 		),
 		'user_use_gravatar' => array(
-			'name' => tr('Use Gravatar for user profile pictures'),
-			'description' => tr('Always request the Gravatar image for the user profile picture.'),
-			'hint' => tr('See [http://gravatar.com/|Gravatar].'),
+			'name' => tr("Use Gravatar for user profile pictures"),
+			'description' => tr("Always request the Gravatar image for the user profile picture."),
+			'hint' => tr("See [http://gravatar.com/|Gravatar]."),
 			'type' => 'flag',
 			'default' => 'n',
 		),
 		'user_multilike_config' => array(
-			'name' => tr('Configuration for multilike'),
-			'description' => tr('Separate configurations by a blank line. E.g. relation_prefix=tiki.multilike values=1,3,5 labels=Good,Great,Excellent)'),
+			'name' => tr("Configuration for multilike"),
+			'description' => tr("Separate configurations by a blank line. E.g. relation_prefix=tiki.multilike values=1,3,5 labels=Good,Great,Excellent)"),
 			'type' => 'textarea',
 			'size' => 5,
 			'default' => ''
@@ -325,17 +325,17 @@ function UserListOrder()
 	$options = array();
 
 	if ($prefs['feature_community_list_score'] == 'y') {
-		$options['score_asc'] = tra('Score ascending');
-		$options['score_desc'] = tra('Score descending');
+		$options['score_asc'] = tra("Score ascending");
+		$options['score_desc'] = tra("Score descending");
 	}
 	
 	if ($prefs['feature_community_list_name'] == 'y') {
-		$options['pref:realname_asc'] = tra('Name ascending');
-		$options['pref:realname_desc'] = tra('Name descending');
+		$options['pref:realname_asc'] = tra("Name ascending");
+		$options['pref:realname_desc'] = tra("Name descending");
 	}
 
-	$options['login_asc'] = tra('Login ascending');
-	$options['login_desc'] = tra('Login descending');
+	$options['login_asc'] = tra("Login ascending");
+	$options['login_desc'] = tra("Login descending");
 
 	return $options;
 }
