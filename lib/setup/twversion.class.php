abd28ea91435c1ef0af0f13e624676b5bde28103
nkoth <nkoth@users.sourceforge.net>
2016-02-02 17:09:26 +0000

    [REL] Closing Tiki 12.6 release
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@57434 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '12.6';	// needs to have no spaces for releases
+		$this->version 	= '12.7svn';	// needs to have no spaces for releases
 		$this->star	= 'Altair';
 		$this->releases	= array();
 
