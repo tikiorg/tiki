7a712b8d0eaaa8a7ad4e8d9930952462105fe71d
patrick-proulx <patrick-proulx@users.sourceforge.net>
2016-02-09 19:41:53 +0000

    [ENH] Made the addon wikiplugin into a smarty block, for better functionality when used in smarty. Also added the ability to store the returned addon value in a smarty variable rather than displaying
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@57490 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_addon.php
+++ lib/wiki-plugins/wikiplugin_addon.php
@@ -39,30 +39,8 @@ function wikiplugin_addon_info()
 
 function wikiplugin_addon($data, $params)
 {
-	if (empty($params['package']) || empty($params['view'])) {
-		return tra("Please specify the name of the package and the view.");
-	}
+	global $smarty;
+	$smarty->loadPlugin('smarty_block_addonview');
 
-	$parts = explode('/', $params['package']);
-	$path = TIKI_PATH . '/addons/' . $parts[0] . '_' . $parts[1] . '/views/' . $params['view'] . '.php';
-
-	if (!file_exists($path)) {
-		return tra("Error: Unable to locate view file for the package.");
-	}
-
-	require_once($path);
-
-	$functionname = "tikiaddon\\" . $parts[0] . "\\" . $parts[1] . "\\" . $params['view'];
-
-	if (!function_exists($functionname)) {
-		return tra("Error: Unable to locate function name for the view.");
-	}
-
-	$prefname = 'ta_' . $parts[0] . '_' . $parts[1] . '_on';
-	$folder = $parts[0] . '_' . $parts[1];
-	if (!isset($GLOBALS['prefs'][$prefname]) || $GLOBALS['prefs'][$prefname] != 'y') {
-		return tra('Addon is not activated: ') . $folder;
-	}
-
-	return "~np~" . $functionname($data, $params) . "~/np~";
+	return "~np~" . smarty_block_addonview($params, $data, $smarty) . "~/np~";
 }
