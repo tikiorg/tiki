a587f5b55da0939cc0c0eb5aa2e2943dbd98100a
jonnybradley <jonnybradley@users.sourceforge.net>
2016-02-15 17:04:46 +0000

    [FIX] structures: Use pageSlug instead of pageName when using the new slug manager SEFURLs (wiki_url_scheme)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@57524 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/structures/structlib.php
+++ lib/structures/structlib.php
@@ -977,7 +977,7 @@ class StructLib extends TikiLib
 		if ( empty( $this->displayLanguageOrder ) ) {
 			$query = 'select `page_ref_id` ';
 			$query .= 'from `tiki_structures` ts, `tiki_pages` tp ';
-			$query .= 'where ts.`page_id`=tp.`page_id` and `pageName`=?';
+			$query .= 'where ts.`page_id`=tp.`page_id` and (tp.`pageName`=? OR tp.`pageSlug`=?)';
 		} else {
 			$query = "
 				SELECT DISTINCT
@@ -988,10 +988,10 @@ class StructLib extends TikiLib
 					LEFT JOIN tiki_translated_objects b ON a.traId = b.traId AND b.type = 'wiki page'
 					LEFT JOIN tiki_pages tp ON ts.page_id = tp.page_id OR b.objId = tp.page_id
 				WHERE
-					pageName = ?";
+					tp.`pageName`=? OR tp.`pageSlug=?";
 		}
 
-		$result = $this->query($query, array($pageName));
+		$result = $this->query($query, array($pageName,$pageName));
 		while ($res = $result->fetchRow()) {
 			$next_page = $this->s_get_structure_info($res['page_ref_id']);
 			//Add each structure head only once
