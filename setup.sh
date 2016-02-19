798758fc8c1b8d0c4c2a725e1f3a1371700a9401
jonnybradley <jonnybradley@users.sourceforge.net>
2016-02-19 14:15:58 +0000

    [FIX] maps: Remove left over maps dir formerly used by mapserver feature
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@57546 b456876b-0849-0410-b77d-98878d47e9d5

--- setup.sh
+++ setup.sh
@@ -269,8 +269,8 @@ check_distribution
 # part 3 - default and writable subdirs
 # -------------------------------------
 
-DIR_LIST_DEFAULT="addons admin db doc dump files img installer lang lib maps modules permissioncheck temp templates templates_c tests themes tiki_tests vendor vendor_extra whelp"
-DIR_LIST_WRITABLE="db dump img/wiki img/wiki_up img/trackers modules/cache temp temp/cache temp/public templates_c templates themes maps whelp mods files tiki_tests/tests temp/unified-index"
+DIR_LIST_DEFAULT="addons admin db doc dump files img installer lang lib modules permissioncheck temp templates templates_c tests themes tiki_tests vendor vendor_extra whelp"
+DIR_LIST_WRITABLE="db dump img/wiki img/wiki_up img/trackers modules/cache temp temp/cache temp/public templates_c templates themes whelp mods files tiki_tests/tests temp/unified-index"
 DIRS=${DIR_LIST_WRITABLE}
 
 # part 4 - several functions
