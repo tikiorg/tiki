1baea11caf98b9226e86fd82381eb56003d307b2
chibaguy <chibaguy@users.sourceforge.net>
2016-03-01 06:55:38 +0000

    [FIX] Use plural form consistently for feature name.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@57697 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/prefs/feature.php
+++ lib/prefs/feature.php
@@ -330,7 +330,7 @@ function prefs_feature_list($partial = false)
 			'tags' => array('basic'),
 		),
 		'feature_faqs' => array(
-			'name' => tra('FAQ'),
+			'name' => tra('FAQs'),
 			'description' => tra('Frequently asked questions and answers'),
 			'warning' => tra('There has been discussion about this feature being retired, so keep in mind that normal wiki pages can be used to create and display FAQs.'),
 			'help' => 'FAQ',
