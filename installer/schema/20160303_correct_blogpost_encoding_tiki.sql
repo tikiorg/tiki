f9ae8e92237496c4685a355566f5cff4ea11580b
jyhem <jyhem@users.sourceforge.net>
2016-03-04 14:03:33 +0000

    [FIX] Partially rolls back r57755. This only worked for non-wysiwyg blog posts. A better solution is needed.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@57756 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/20160303_correct_blogpost_encoding_tiki.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `tiki_blog_posts` SET `data` = REPLACE(`data`, '&gt;', '>');
-UPDATE `tiki_blog_posts` SET `data` = REPLACE(`data`, '&lt;', '<');
-UPDATE `tiki_blog_posts` SET `data` = REPLACE(`data`, '&amp;', '&');
