af041ca4fd4c9cc1ffa3753abe1b443aa122ea83
jyhem <jyhem@users.sourceforge.net>
2016-03-17 19:24:45 +0000

    Copyright bumped to 2016 Happy new year (again)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@57941 b456876b-0849-0410-b77d-98878d47e9d5

--- remote.php
+++ remote.php
@@ -3,7 +3,7 @@
  * Used by Tiki's InterTiki feature
  *
  * @package Tiki
- * @copyright (c) Copyright 2002-2015 by authors of the Tiki Wiki CMS Groupware Project. All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * @copyright (c) Copyright 2002-2016 by authors of the Tiki Wiki CMS Groupware Project. All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * @licence LGPL-2.1. See license.txt for details.
  */
 // $Id$
