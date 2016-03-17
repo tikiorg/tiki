333c59cd3ced588d78ab66d8cb6559c0c6dda9c9
jyhem <jyhem@users.sourceforge.net>
2016-03-17 19:29:02 +0000

    Copyright bumped to 2016 Happy new year (again)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@57947 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/parser/parserlib.php
+++ lib/parser/parserlib.php
@@ -1,5 +1,5 @@
 <?php
-// (c) Copyright 2002-2015 by authors of the Tiki Wiki CMS Groupware Project
+// (c) Copyright 2002-2016 by authors of the Tiki Wiki CMS Groupware Project
 //
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -15,7 +15,7 @@
  * @package		Tiki
  * @subpackage		Parser
  * @author		Robert Plummer
- * @copyright (c) Copyright 2002-2015 by authors of the Tiki Wiki CMS Groupware Project
+ * @copyright (c) Copyright 2002-2016 by authors of the Tiki Wiki CMS Groupware Project
  * 			See copyright.txt for details and a complete list of authors.
  * @licence Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
  * @version		SVN $Rev$
