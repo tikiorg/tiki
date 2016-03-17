4abcff93e506b5b8980af1be8515884f162aa88d
jyhem <jyhem@users.sourceforge.net>
2016-03-17 19:20:19 +0000

    Copyright bumped to 2016 Happy new year
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@57937 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-user_unified_details.php
+++ tiki-user_unified_details.php
@@ -2,7 +2,7 @@
 /**
  * @package tikiwiki
  */
-// (c) Copyright 2002-2015 by authors of the Tiki Wiki CMS Groupware Project
+// (c) Copyright 2002-2016 by authors of the Tiki Wiki CMS Groupware Project
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
