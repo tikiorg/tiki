7206b2150c665c7e38ef7a51f8bae5d666543bdf
jonnybradley <jonnybradley@users.sourceforge.net>
2016-04-02 10:46:42 +0000

    [MOD] connection: Don't redirect to the installer if the database is set up but unavailable - display an html page instead as discussed (thanks Geoff)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@58156 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki-db.php
+++ db/tiki-db.php
@@ -212,7 +212,7 @@ if (! $db && ! defined('TIKI_IN_INSTALLER')) {
 	if(!empty($dbfail_url)) {
 		header('location: '.$dbfail_url);
 	} else {
-		header('location: tiki-install.php');
+		header('location: ' . $tikiroot . 'tiki-db_connection_error.html');
 	}
 	exit;
 } elseif ($db) {
