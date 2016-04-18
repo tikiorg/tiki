e87696b84bdd168070359ba442e03be4426c62b7
jonnybradley <jonnybradley@users.sourceforge.net>
2016-04-18 18:41:33 +0000

    [bp/r58364][FIX] typo (testing is good)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@58365 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-listpages.php
+++ tiki-listpages.php
@@ -348,7 +348,7 @@ if (!empty($multiprint_pages)) {
 	}
 
 	$smarty->assign('initial', $initial);
-	if (isset($_REQUEST['exact_match']) && $_REQUEST['exact_match']) == 'y' ) {
+	if (isset($_REQUEST['exact_match']) && $_REQUEST['exact_match'] == 'y' ) {
 		$exact_match = true;
 		$smarty->assign('exact_match', 'y');
 	} else {
