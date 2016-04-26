d9124ba3115168a88475fcd4baefdb6f9d09edcf
xavidp <xavidp@users.sourceforge.net>
2016-04-26 13:44:26 +0000

    [FIX] WYSIWYCA: if you have the permission to manage structures (aside of the perm to edit wiki pages), you should be shown the link to do so when viewing the pages in the structure you can manage.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@58460 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki/renderlib.php
+++ lib/wiki/renderlib.php
@@ -141,7 +141,9 @@ class WikiRenderer
 			}
 		}
 		if ($tikilib->user_has_perm_on_object($this->user, $navigation_info['home']['pageName'], 'wiki page', 'tiki_p_edit', 'tiki_p_edit_structures'))
-			$this->smartyassign('struct_editable', 'y');
+			$this->smartyassign('struct_editable', 'a'); # a stands for both perms: tiki_p_edit and tiki_p_edit_structures
+		else if ($tikilib->user_has_perm_on_object($this->user, $navigation_info['home']['pageName'], 'wiki page', 'tiki_p_edit_structures'))
+			$this->smartyassign('struct_editable', 'y'); # y stands for only tiki_p_edit_structures perm (but not tiki_p_edit)
 		else
 			$this->smartyassign('struct_editable', 'n');
 		// To show position
