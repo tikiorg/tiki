1b372ed9b2da0f8d122d82ae23da76ab17848632
jonnybradley <jonnybradley@users.sourceforge.net>
2016-05-18 18:12:55 +0000

    [MRG] Automatic merge, branches/15.x 58619 to 58626
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@58627 b456876b-0849-0410-b77d-98878d47e9d5

--- admin/include_video.php
+++ admin/include_video.php
@@ -26,6 +26,7 @@ if ($prefs['feature_kaltura'] === 'y') {
 		$kcwDefault = $prefs['kaltura_kcwUIConf'];
 		if (empty($kcwDefault) || !empty($_REQUEST['kcw_rebuild'])) {
 			$kcwDefault = $kalturaadminlib->updateStandardTikiKcw();
+			$cachelib->invalidate($kalturaadminlib::CONFIGURATION_LIST);
 		}
 		if ($kcwDefault) {
 			$kcwText = "<div class='adminoptionbox'>KCW Configuration ID: $kcwDefault (automatically configured)</div>";
