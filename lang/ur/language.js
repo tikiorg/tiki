4c32ea87d669a9d69e5914666f7c9905f77d9cac
amnabilal <amnabilal@users.sourceforge.net>
2016-05-19 08:48:51 +0000

    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@58632 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lang/ur/language.js
@@ -0,0 +1,28 @@
+﻿/* 
+ * Javascript translation definition
+ * Example to help translation of some English strings in some js files
+ * 
+ * $Id: language.js 30343 2010-10-27 12:50:51Z xavidp $
+ */
+
+lang = {
+	"Very Secure" : "بہت محفوظ",
+	"Secure" : "محفوظ",
+	"Very Strong" : "بہت مضبوط",
+	"Strong" : "مضبوط",
+	"Average" : "اوسط",
+	"Weak" : "کمزور",
+	"Very Weak" : "بہت کمزور",
+	"Strength" : "طاقت",
+	"Valid User Name" : "درست نام",
+	"Passwords match" : "پاس ورڈ میچ",
+	"Valid Email" : "درست ای میل",
+	"Close" : "بند کریں",
+	"Submit" : "جمع کرائیں",
+	"Insert" : "داخل کریں",
+	"Replace" : "بدل دیں",
+	"Match" : "میچ",
+	"Do not match" : "ایک ساتھ نیہں ملتے",
+	"Advanced options" : "دیگر اپشن",
+	"Pick a file." : "فائل چنیں",
+};
