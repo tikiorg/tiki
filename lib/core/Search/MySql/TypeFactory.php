a0655bfdc23b94933a38d8f87cb7e0096a742c9c
jonnybradley <jonnybradley@users.sourceforge.net>
2016-05-25 12:56:34 +0000

    [FIX] search: When using the msql engine and mysql 5.7 (or 5.6 in NO_ZERO_DATE mode) then 0000-00-00 00:00:00 being used as an "empty" date was causing multiple fatal errors, and this fix seems to prevent most of them. Should address wish5936 at least partially, will continue testing (thanks Xavi)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@58695 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Search/MySql/TypeFactory.php
+++ lib/core/Search/MySql/TypeFactory.php
@@ -22,7 +22,8 @@ class Search_MySql_TypeFactory implements Search_Type_Factory_Interface
 		if (is_numeric($value)) {
 			return new Search_Type_Timestamp(gmdate('Y-m-d H:i:s', $value));
 		} else {
-			return new Search_Type_Timestamp('0000-00-00 00:00:00');
+			// if mysql sql_mode is set to NO_ZERO_IN_DATE or NO_ZERO_DATE then'0000-00-00 00:00:00' produces errors
+			return null;
 		}
 
 	}
