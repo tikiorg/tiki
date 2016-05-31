tikiorg/tiki

commit 43640261fce4807a20b96805b3a1148877ad9c8b

[ENH] #3603: Trackerlist records can only be clicked at the values
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@56310 b456876b-0849-0410-b77d-98878d47e9d5

templates/wiki-plugins/wikiplugin_trackerlist.tpl
@@ -1,5 +1,11 @@
 {* $Id$ *}
 {strip}
+	{capture assign=tdinstyle}overflow:hidden{/capture}
+	{capture assign=tdstyle}style="{$tdinstyle}"{/capture}
+	{capture assign=tdastyle}style="margin:-10em;padding:10em;display:block"{/capture}
+	{capture assign=txturl}{if isset($showlinks) && $showlinks=='y'}{$url}{/if}{/capture}
+	{capture assign=rowurl}{if isset($showlinks) && $showlinks=='r'}{$url}{/if}{/capture}
+	{capture assign=showlinks}{if isset($showlinks) && $showlinks=='r'}n{/if}{/capture}
 	{if $showtitle eq 'y'}<div class="pagetitle">{$tracker_info.name}</div>{/if}
 	{if $showdesc eq 'y'}
 		<div class="wikitext">
@@ -139,7 +145,7 @@ the section loop so that the vars are not replaced by nested pretty tracker exec
 							<td class="numeric" style="padding-right:2px">
 								{foreach from=$computedFields[$ix.fieldId] item=computedField name=computedField}
 									{if $computedField.operator eq 'avg'}{tr}Average{/tr}{else}{tr}Total{/tr}{/if}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-									{trackeroutput field=$computedField item=$items[user] list_mode=$list_mode url=$url}<br/>
+									{trackeroutput field=$computedField item=$items[user] list_mode=$list_mode url=$txturl|replacei:'#itemId':$items[user].itemId}<br/>
 								{/foreach}
 							</td>
 						{else}
@@ -223,7 +229,7 @@ the section loop so that the vars are not replaced by nested pretty tracker exec
 
 						{foreach from=$items[user].field_values item=f}
 							{if in_array($f.fieldId, $popupfields)}
-								{capture name=popupl}{trackeroutput field=$f item=$items[user] url=$url editable=in_array($f.fieldId, $editableFields)}{/capture}
+								{capture name=popupl}{trackeroutput field=$f item=$items[user] url=$txturl|replacei:'#itemId':$items[user].itemId editable=in_array($f.fieldId, $editableFields)}{/capture}
 								{if !empty($smarty.capture.popupl)}
 									<tr>{if count($popupfields) > 1}<th class="{cycle advance=false}">{$f.name}</th>{/if}<td>{$smarty.capture.popupl}</td></tr>
 								{/if}
@@ -261,32 +267,33 @@ the section loop so that the vars are not replaced by nested pretty tracker exec
 					or $field.isHidden eq 'p' or $perms.tiki_p_admin_trackers eq 'y') and $field.type ne 'x' and $field.type ne 'h'
 					and in_array($field.fieldId, $listfields) and ($field.type ne 'p' or $field.options_array[0] ne 'password')
 					and (empty($field.visibleBy) or in_array($default_group, $field.visibleBy) or $perms.tiki_p_admin_trackers eq 'y')}
-		<td class={if $field.type eq 'n' or $field.type eq 'q' or $field.type eq 'b'}"numeric"{else}"auto"{/if} {if $field.type eq 'b'} style="padding-right:5px"{/if}>
+		<td class={if $field.type eq 'n' or $field.type eq 'q' or $field.type eq 'b'}"numeric"{else}"auto"{/if} {if $field.type eq 'b'} style="padding-right:5px;{$tdinstyle}"{else}{$tdstyle}{/if}>
+			{if isset($rowurl)}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}
 					{if $field.isHidden eq 'c' and $fieldr and $perms.tiki_p_admin_trackers ne 'y'}
 					{elseif isset($perms)}
-						{trackeroutput item=$items[user] field=$field list_mode=$list_mode showlinks=$showlinks showpopup=$showpopup popupfields=$popupfields url=$url editable=in_array($field.fieldId, $editableFields)
+						{trackeroutput item=$items[user] field=$field list_mode=$list_mode showlinks=$showlinks showpopup=$showpopup popupfields=$popupfields url=$txturl editable=in_array($field.fieldId, $editableFields)
 								tiki_p_view_trackers=$perms.tiki_p_view_trackers tiki_p_modify_tracker_items=$perms.tiki_p_modify_tracker_items tiki_p_modify_tracker_items_pending=$perms.tiki_p_modify_tracker_items_pending
 								tiki_p_modify_tracker_items_closed=$perms.tiki_p_modify_tracker_items_closed tiki_p_comment_tracker_items=$perms.tiki_p_comment_tracker_items reloff=$itemoff}
 					{else}
-						{trackeroutput item=$items[user] field=$field list_mode=$list_mode reloff=$itemoff showlinks=$showlinks showpopup=$showpopup popupfields=$popupfields url=$url editable=in_array($field.fieldId, $editableFields)}
+						{trackeroutput item=$items[user] field=$field list_mode=$list_mode reloff=$itemoff showlinks=$showlinks showpopup=$showpopup popupfields=$popupfields url=$txturl editable=in_array($field.fieldId, $editableFields)}
 					{/if}
+			{if isset($rowurl)}&nbsp;</a>{/if}
 		</td>
 				{/if}
 			{/foreach}
 {* ------------------------------------ *}
 
 			{if $showcreated eq 'y'}
-		<td>{if $tracker_info.showCreatedFormat}{$items[user].created|tiki_date_format:$tracker_info.showCreatedFormat}{else}{$items[user].created|tiki_short_datetime}{/if}</td>
+		<td {$tdstyle}>{if isset($rowurl)}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{if $tracker_info.showCreatedFormat}{$items[user].created|tiki_date_format:$tracker_info.showCreatedFormat}{else}{$items[user].created|tiki_short_datetime}{/if}{if isset($rowurl)}</a>{/if}</td>
 			{/if}
 			{if $showlastmodif eq 'y'}
-		<td>{if $tracker_info.showLastModifFormat}{$items[user].lastModif|tiki_date_format:$tracker_info.showLastModifFormat}{else}{$items[user].lastModif|tiki_short_datetime}{/if}</td>
+		<td {$tdstyle}>{if isset($rowurl)}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{if $tracker_info.showLastModifFormat}{$items[user].lastModif|tiki_date_format:$tracker_info.showLastModifFormat}{else}{$items[user].lastModif|tiki_short_datetime}{/if}{if isset($rowurl)}</a>{/if}</td>
 			{/if}
 			{if $showlastmodifby eq 'y'}
-		<td>{$items[user].lastModifBy}</td>
+		<td {$tdstyle}>{if isset($rowurl)}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{$items[user].lastModifBy}{if isset($rowurl)}</a>{/if}</td>
 			{/if}
 			{if $tracker_info.useComments eq 'y' and ($tracker_info.showComments eq 'y' or $tracker_info.showLastComment eq 'y') and $perms.tiki_p_tracker_view_comments ne 'n'}
-		<td style="text-align:center;">{if $tracker_info.showComments eq 'y'}{$items[user].comments}{/if}{if $tracker_info.showComments eq 'y' and $tracker_info.showLastComment eq 'y'}<br>{/if}{if $tracker_info.showLastComment eq 'y' and !empty($items[user].lastComment)}{$items[user].lastComment.userName|escape}-{$items[user].lastComment.commentDate|tiki_short_date}{/if}</td>
-
+		<td style="text-align:center;{$tdinstyle}">{if isset($rowurl)}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{if $tracker_info.showComments eq 'y'}{$items[user].comments}{/if}{if $tracker_info.showComments eq 'y' and $tracker_info.showLastComment eq 'y'}<br>{/if}{if $tracker_info.showLastComment eq 'y' and !empty($items[user].lastComment)}{$items[user].lastComment.userName|escape}-{$items[user].lastComment.commentDate|tiki_short_date}{/if}{if isset($rowurl)}</a>{/if}</td>
 			{/if}
 			{if $tracker_info.useAttachments eq 'y' and $tracker_info.showAttachments eq 'y'}
 		<td style="text-align:center;"><a href="tiki-view_tracker_item.php?trackerId={$listTrackerId}&amp;itemId={$items[user].itemId}&amp;show=att"

lib/smarty_tiki/modifier.replacei.php
@@ -1,0 +1,25 @@
+<?php
+// (c) Copyright 2002-2015 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: modifier.replacei.php 0000 2015-09-22 22:42:50Z minger0 $
+
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * Smarty replacei modifier plugin
+ *
+ * Type:     modifier<br>
+ * Name:     replacei<br>
+ * Purpose:  Returns a case insensitive replaced string.
+ *           Same arguments as PHP str_ireplace function.
+ */
+function smarty_modifier_replacei($string, $find, $replacement)
+{
+	return str_ireplace($find, $replacement, $string);
+}

lib/wiki-plugins/wikiplugin_trackerlist.php
@@ -82,7 +82,8 @@ function wikiplugin_trackerlist_info()
 				 'options' => array(
 					 array('text' => '', 'value' => ''),
 					 array('text' => tra('Yes'), 'value' => 'y'),
-					 array('text' => tra('No'), 'value' => 'n')
+					 array('text' => tra('No'), 'value' => 'n'),
+					 array('text' => tra('Row'), 'value' => 'r')
 				 )
 			 ),
 			 'showdesc' => array(

commit 1ac4ee29a5b8f4551edd1809760dfd14a0c953a7

[FIX] trackerlist: Fix for regressions caused by r56310 (should have been done totally differently imho, so this is a quick patch). Change tests for $rowurl as it is always set (by code from the same commit) which was causing pointless broken links on all fields and preventing inline-editing from working (testing is good ;) Hopefully addresses wishes 5922 and 5935, thanks Xavi
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@58745 b456876b-0849-0410-b77d-98878d47e9d5

templates/wiki-plugins/wikiplugin_trackerlist.tpl
@@ -298,7 +298,7 @@ the section loop so that the vars are not replaced by nested pretty tracker exec
 					and in_array($field.fieldId, $listfields) and ($field.type ne 'p' or $field.options_array[0] ne 'password')
 					and (empty($field.visibleBy) or in_array($default_group, $field.visibleBy) or $perms.tiki_p_admin_trackers eq 'y')}
 		<td class={if $field.type eq 'n' or $field.type eq 'q' or $field.type eq 'b'}"numeric"{else}"auto"{/if} {if $field.type eq 'b'} style="padding-right:5px;{$tdinstyle}"{else}{$tdstyle}{/if}>
-			{if isset($rowurl)}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}
+			{if $rowurl}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}
 					{if $field.isHidden eq 'c' and $fieldr and $perms.tiki_p_admin_trackers ne 'y'}
 					{elseif isset($perms)}
 						{trackeroutput item=$items[user] field=$field list_mode=$list_mode showlinks=$showlinks showpopup=$showpopup popupfields=$popupfields url=$txturl editable=in_array($field.fieldId, $editableFields)
@@ -307,23 +307,23 @@ the section loop so that the vars are not replaced by nested pretty tracker exec
 					{else}
 						{trackeroutput item=$items[user] field=$field list_mode=$list_mode reloff=$itemoff showlinks=$showlinks showpopup=$showpopup popupfields=$popupfields url=$txturl editable=in_array($field.fieldId, $editableFields)}
 					{/if}
-			{if isset($rowurl)}&nbsp;</a>{/if}
+			{if $rowurl}&nbsp;</a>{/if}
 		</td>
 				{/if}
 			{/foreach}
 {* ------------------------------------ *}
 
 			{if $showcreated eq 'y'}
-		<td {$tdstyle}>{if isset($rowurl)}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{if $tracker_info.showCreatedFormat}{$items[user].created|tiki_date_format:$tracker_info.showCreatedFormat}{else}{$items[user].created|tiki_short_datetime}{/if}{if isset($rowurl)}</a>{/if}</td>
+		<td {$tdstyle}>{if $rowurl}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{if $tracker_info.showCreatedFormat}{$items[user].created|tiki_date_format:$tracker_info.showCreatedFormat}{else}{$items[user].created|tiki_short_datetime}{/if}{if $rowurl}</a>{/if}</td>
 			{/if}
 			{if $showlastmodif eq 'y'}
-		<td {$tdstyle}>{if isset($rowurl)}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{if $tracker_info.showLastModifFormat}{$items[user].lastModif|tiki_date_format:$tracker_info.showLastModifFormat}{else}{$items[user].lastModif|tiki_short_datetime}{/if}{if isset($rowurl)}</a>{/if}</td>
+		<td {$tdstyle}>{if $rowurl}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{if $tracker_info.showLastModifFormat}{$items[user].lastModif|tiki_date_format:$tracker_info.showLastModifFormat}{else}{$items[user].lastModif|tiki_short_datetime}{/if}{if $rowurl}</a>{/if}</td>
 			{/if}
 			{if $showlastmodifby eq 'y'}
-		<td {$tdstyle}>{if isset($rowurl)}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{$items[user].lastModifBy}{if isset($rowurl)}</a>{/if}</td>
+		<td {$tdstyle}>{if $rowurl}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{$items[user].lastModifBy}{if $rowurl}</a>{/if}</td>
 			{/if}
 			{if $tracker_info.useComments eq 'y' and ($tracker_info.showComments eq 'y' or $tracker_info.showLastComment eq 'y') and $perms.tiki_p_tracker_view_comments ne 'n'}
-		<td style="text-align:center;{$tdinstyle}">{if isset($rowurl)}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{if $tracker_info.showComments eq 'y'}{$items[user].comments}{/if}{if $tracker_info.showComments eq 'y' and $tracker_info.showLastComment eq 'y'}<br>{/if}{if $tracker_info.showLastComment eq 'y' and !empty($items[user].lastComment)}{$items[user].lastComment.userName|escape}-{$items[user].lastComment.commentDate|tiki_short_date}{/if}{if isset($rowurl)}</a>{/if}</td>
+		<td style="text-align:center;{$tdinstyle}">{if $rowurl}<a href="{$rowurl|replacei:'#itemId':$items[user].itemId}" {$tdastyle}>{/if}{if $tracker_info.showComments eq 'y'}{$items[user].comments}{/if}{if $tracker_info.showComments eq 'y' and $tracker_info.showLastComment eq 'y'}<br>{/if}{if $tracker_info.showLastComment eq 'y' and !empty($items[user].lastComment)}{$items[user].lastComment.userName|escape}-{$items[user].lastComment.commentDate|tiki_short_date}{/if}{if $rowurl}</a>{/if}</td>
 			{/if}
 			{if $tracker_info.useAttachments eq 'y' and $tracker_info.showAttachments eq 'y'}
 		<td style="text-align:center;"><a href="tiki-view_tracker_item.php?trackerId={$listTrackerId}&amp;itemId={$items[user].itemId}&amp;show=att"