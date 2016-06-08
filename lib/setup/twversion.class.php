85a04ddf6fcdbe6526f032d5e1b2cff138a07df3
luciash <luciash@users.sourceforge.net>
2016-06-08 15:53:01 +0000

    [REL]  Preparing 14.3 release
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/14.x@58845 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '14.3svn';	// needs to have no spaces for releases
+		$this->version 	= '14.3';	// needs to have no spaces for releases
 		$this->star	= 'Peony';
 		$this->releases	= array();
 
