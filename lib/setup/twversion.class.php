87f200ff86fdf6999d54d56d9234fda607de5900
jonnybradley <jonnybradley@users.sourceforge.net>
2016-06-08 16:46:18 +0000

    [REL] Closing 12.7 release
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@58863 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '12.7';	// needs to have no spaces for releases
+		$this->version 	= '12.8svn';	// needs to have no spaces for releases
 		$this->star	= 'Altair';
 		$this->releases	= array();
 
