d1905a91973b61aacb8799cc06babde0b29e2b8a
amnabilal <amnabilal@users.sourceforge.net>
2016-07-01 14:00:25 +0000

    [Fix]Added extra check, passed current active session id to tiki-download_file.php to check if user is coming from valid active session before pdf generator is allowed to access images.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@59029 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/pdflib.php
+++ lib/pdflib.php
@@ -86,9 +86,9 @@ class PdfGenerator
 
 		$url = $base_url . $file . '?' . http_build_query($params, '', '&');
         $session_params = session_get_cookie_params();
-		echo str_replace("tiki-download_file.php?","tiki-download_file.php?genPDF=1&sid=".session_id()."&",$pdata);
+		//echo str_replace("tiki-download_file.php?","tiki-download_file.php?genPDF=1&sid=".session_id()."&",$pdata);
 		//echo $session_params['path'];
-	//	return $this->{$this->mode}( $url,str_replace("tiki-download_file.php?","tiki-download_file.php?genPDF=1&sid=".session_id(),$pdata));
+		return $this->{$this->mode}( $url,str_replace("tiki-download_file.php?","tiki-download_file.php?genPDF=1&sid=".session_id()."&",$pdata));
 	}
 
     /**
