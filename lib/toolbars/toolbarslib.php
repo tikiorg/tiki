tikiorg/tiki

commit 7d9fd04274b7543b734c1d77d74a1997a0862c94

[FIX] wysiwyg: When there are more than one ckeditors on the page (e.g. blogs with excerpts) then use the right area_id to insert the images in.
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@59048 b456876b-0849-0410-b77d-98878d47e9d5

lib/toolbars/toolbarslib.php
@@ -1544,18 +1544,19 @@ class ToolbarFileGallery extends Toolbar
 					);
 				};'
 			);
-			return 'openElFinderDialog(
+			return 'var area_id = editor.name;
+				openElFinderDialog(
 				this,
 				{
 					defaultGalleryId: ' . (empty($prefs['home_file_gallery']) ? $prefs['fgal_root_id'] : $prefs['home_file_gallery']) . ',
 					deepGallerySearch: true,
 					getFileCallback: function(file,elfinder) {
-							window.handleFinderInsertAt(file,elfinder,\''.$areaId.'\');
+							window.handleFinderInsertAt(file,elfinder,area_id);
 						},
 					eventOrigin:this,
 					uploadCallback: function (data) {
 							if (data.data.added.length === 1 && confirm(tr(\'Do you want to use this file in your page?\'))) {
-								window.handleFinderInsertAt(data.data.added[0],window.elFinder,\''.$areaId.'\');
+								window.handleFinderInsertAt(data.data.added[0],window.elFinder,area_id);
 							}
 						}
 				}

commit 4cefdbcd1b7607fec17ea19412c5a17e613c4ad3

[FIX] editor: Fix regression for wiki editors' upload image button introduced in r59048 - sorry, had forgotten this code it used by both types of editor. Thanks Xavi, hopefully fixes wish6017 (once i backport it to 15.x)
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@59064 b456876b-0849-0410-b77d-98878d47e9d5

lib/toolbars/toolbarslib.php
@@ -1544,8 +1544,9 @@ class ToolbarFileGallery extends Toolbar
 					);
 				};'
 			);
-			return 'var area_id = editor.name;
-				openElFinderDialog(
+			return '
+			var area_id = (typeof editor === \'undefined\' ?  \'' . $areaId . '\' : editor.name);
+			openElFinderDialog(
 				this,
 				{
 					defaultGalleryId: ' . (empty($prefs['home_file_gallery']) ? $prefs['fgal_root_id'] : $prefs['home_file_gallery']) . ',