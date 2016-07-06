tikiorg/tiki

commit 761cb7aaf27248dfffc2aa7ea3a33f415ebfa0e5

[bp/r59051 and r59052][FIX] wiki up: moving multiple wiki up files to file galleries failed after the first move - not sure how this ever worked.
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@59054 b456876b-0849-0410-b77d-98878d47e9d5

lib/filegals/filegallib.php
@@ -13,6 +13,9 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 
 class FileGalLib extends TikiLib
 {
+
+	private $wikiupMoved = [];
+
 	function isPodCastGallery($galleryId, $gal_info=null)
 	{
 		if (empty($gal_info))
@@ -4014,6 +4017,7 @@ class FileGalLib extends TikiLib
 				$this->moveWikiUpToFgal($page, $fgalId, $errors, $feedbacks);
 			}
 		}
+		$this->wikiupMoved = [];
 	}
 	function moveWikiUpToFgal($page_info, $fgalId, &$errors, &$feedbacks)
 	{
@@ -4033,20 +4037,30 @@ class FileGalLib extends TikiLib
 				$arguments = $argumentParser->parse($match->getArguments());
 				$newArgs = array();
 				foreach ($arguments as $key=>$val) {
-					if ($key == 'src') {
-						if (false === $data = @file_get_contents($val)) {
-							$errors[] = tra('Cannot open this file:').' '.$val.' '.tra('Page:').' '.$page_info['pageName'];
-							continue;
-						}
-						$name = preg_replace('|.*/([^/]*)|', '$1', $val);
-						$fileId = $this->insert_file($fgalId, $name, 'Used in '.$page_info['pageName'], $name, $data, strlen($data), $mimelib->from_path($name, $val), $user, '', 'wiki_up conversion');
-						if (empty($fileId)) {
-							$errors[] = tra('Cannot upload this file').' '.$val.' '.tra('Page:').' '.$page_info['pageName'];
-							continue;
+					if ($key === 'src' && strpos($val, 'img/wiki_up') !== false) {
+						//first time the wiki_up file is found
+						if (!isset($this->wikiupMoved[$val])) {
+							if (false === $data = @file_get_contents($val)) {
+								$errors[] = tra('Cannot open this file:').' '.$val.' '.tra('Page:').' '.$page_info['pageName'];
+								continue;
+							}
+							$name = preg_replace('|.*/([^/]*)|', '$1', $val);
+							$fileId = $this->insert_file($fgalId, $name, 'Used in '.$page_info['pageName'], $name, $data, strlen($data), $mimelib->from_path($name, $val), $user, '', 'wiki_up conversion');
+							if (empty($fileId)) {
+								$errors[] = tra('Cannot upload this file').' '.$val.' '.tra('Page:').' '.$page_info['pageName'];
+								continue;
+							} else {
+								$files[] = $val;
+								$modif = true;
+								$newArgs[] = 'fileId="'.$fileId.'"';
+								//save wiki_up file name and fileId pair in case there are more instances using this file
+								$this->wikiupMoved[$val] = $fileId;
+							}
+						//wiki_up file was already moved to file galleries
 						} else {
 							$files[] = $val;
 							$modif = true;
-							$newArgs[] = 'fileId="'.$fileId.'"';
+							$newArgs[] = 'fileId="' . $this->wikiupMoved[$val] . '"';
 						}
 					} else
 						$newArgs[] = "$key=\"$val\"";
@@ -4058,8 +4072,11 @@ class FileGalLib extends TikiLib
 		}
 		if (!empty($files)) {
 			$tikilib->update_page($page_info['pageName'], $matches->getText(), 'wiki_up conversion', $user, $tikilib->get_ip_address());
+			$files = array_unique($files);
 			foreach ($files as $file) {
-				unlink($file);
+				if (file_exists($file)) {
+					unlink($file);
+				}
 			}
 			$feedbacks[] = $page_info['pageName'];
 		}

commit 2953f78b156278d36aa31b485a6b3fea2646b556

[REL] PHP 5.3 syntax is required for 12.x (regression from r59054)
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@59106 b456876b-0849-0410-b77d-98878d47e9d5

lib/filegals/filegallib.php
@@ -14,7 +14,7 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 class FileGalLib extends TikiLib
 {
 
-	private $wikiupMoved = [];
+	private $wikiupMoved = array();
 
 	function isPodCastGallery($galleryId, $gal_info=null)
 	{
@@ -4017,7 +4017,7 @@ class FileGalLib extends TikiLib
 				$this->moveWikiUpToFgal($page, $fgalId, $errors, $feedbacks);
 			}
 		}
-		$this->wikiupMoved = [];
+		$this->wikiupMoved = array();
 	}
 	function moveWikiUpToFgal($page_info, $fgalId, &$errors, &$feedbacks)
 	{