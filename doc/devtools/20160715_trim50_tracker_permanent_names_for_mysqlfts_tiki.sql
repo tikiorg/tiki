4378ea5e41f69040d39845fb71adafacac436faa
xavidp <xavidp@users.sourceforge.net>
2016-07-15 09:17:41 +0000

    [bp/r59197] [ENH]Allow trimming PermName on upgrades to 50 characters, which seems to be the maximum allowed by MySQL Full Text Search as Unified Search Index. Using same arbitrary convention as added in the code here: https://sourceforge.net/p/tikiwiki/code/59180 . This upgrade script is not placed under the installer/schema folder by default since it might potentially break some installations upon upgrade if they were using such long permanent names for real. Therefore, we leave this upgrade script here under doc/devtools/ by default, so that each admin can move it to installer/schema in their own install at their own risk. Thanks marclaporte
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@59198 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ doc/devtools/20160715_trim50_tracker_permanent_names_for_mysqlfts_tiki.sql
@@ -0,0 +1,22 @@
+-- Ensure that PermName is no longer than 50 characters, which seems to be the maximum allowed
+-- by MySQL Full Text Search as Unified Search Index. We could allow longer permanent names when other
+-- search index engines are the ones being used, but this will probably only delay the problem until the admin
+-- wants to change the search engine for some reason (some constrains in Lucene or Elastic Search,
+-- as experience demonstrated in some production sites in real use cases over long periods of time).
+-- And to increase chances to avoid conflict when long names only differ in the end of the long string,
+-- where some meaningful info resides, we'll get the first 40 chars, 1 underscore and the last 9 chars.
+--
+--  Using same arbitrary convention as added in the code here: https://sourceforge.net/p/tikiwiki/code/59180
+--
+-- This upgrade script is not placed under the installer/schema folder by default since it might potentially
+-- break some installations upon upgrade if they were using such long permanent names for real.
+-- Therefore, we leave this upgrade script here under doc/devtools/ by default, so that each admin can
+-- move it to installer/schema in their own install at their own risk.
+UPDATE `tiki_tracker_fields`
+SET permName = REPLACE(permName,
+                       permName,
+                       (SELECT CONCAT( LEFT(permName, 40), '_', RIGHT(permName, 9)))
+                      )
+WHERE char_length(permName) > 50
+
+
