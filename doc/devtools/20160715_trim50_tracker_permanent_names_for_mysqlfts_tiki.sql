8aa9c63436b45235b8af48f76d9fc5ea5ff92677
xavidp <xavidp@users.sourceforge.net>
2016-07-15 09:50:28 +0000

    [bp/r59200][FIX]Update maximum length allowed for permNames to 36 instead of 50, since tiki will internally prepend 14 extra characters to them (tracker_field_). [bp/r59201]Update maximum length allowed for permNames to 36 instead of 50, since tiki will internally prepend 14 extra characters to them (tracker_field_).
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@59203 b456876b-0849-0410-b77d-98878d47e9d5

--- doc/devtools/20160715_trim50_tracker_permanent_names_for_mysqlfts_tiki.sql
+++ doc/devtools/20160715_trim50_tracker_permanent_names_for_mysqlfts_tiki.sql
@@ -1,12 +1,13 @@
--- Ensure that PermName is no longer than 50 characters, which seems to be the maximum allowed
--- by MySQL Full Text Search as Unified Search Index. We could allow longer permanent names when other
+-- Ensure that PermName is no longer than 36 characters, since the maximum allowed
+-- by MySQL Full Text Search as Unified Search Index is 50, and trackers will internally prepend
+-- "tracker_field_", which are another 14 characters (36+14=50). We could allow longer permanent names when other
 -- search index engines are the ones being used, but this will probably only delay the problem until the admin
 -- wants to change the search engine for some reason (some constrains in Lucene or Elastic Search,
 -- as experience demonstrated in some production sites in real use cases over long periods of time).
 -- And to increase chances to avoid conflict when long names only differ in the end of the long string,
--- where some meaningful info resides, we'll get the first 40 chars, 1 underscore and the last 9 chars.
+-- where some meaningful info resides, we'll get the first 26 chars, 1 underscore and the last 9 chars.
 --
---  Using same arbitrary convention as added in the code here: https://sourceforge.net/p/tikiwiki/code/59180
+--  Using same arbitrary convention as added in the code here: https://sourceforge.net/p/tikiwiki/code/59200
 --
 -- This upgrade script is not placed under the installer/schema folder by default since it might potentially
 -- break some installations upon upgrade if they were using such long permanent names for real.
@@ -15,8 +16,8 @@
 UPDATE `tiki_tracker_fields`
 SET permName = REPLACE(permName,
                        permName,
-                       (SELECT CONCAT( LEFT(permName, 40), '_', RIGHT(permName, 9)))
+                       (SELECT CONCAT( LEFT(permName, 26), '_', RIGHT(permName, 9)))
                       )
-WHERE char_length(permName) > 50
+WHERE char_length(permName) > 36
 
 
