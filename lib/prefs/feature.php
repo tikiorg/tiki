04829321d8c4f2a5779c41a0f78d8d3c31c405d9
yonixxx <yonixxx@users.sourceforge.net>
2016-07-19 13:04:07 +0000

    [FIX] Display the page name for a wiki page is enable by default. It make sense the option to disable it is visible with basic (default) admin preferences.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@59219 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/prefs/feature.php
+++ lib/prefs/feature.php
@@ -501,6 +501,7 @@ function prefs_feature_list($partial = false)
 			'description' => tra('Display the page name at the top of each page as page title. If not enabled, the page content should contain an h1 heading to function as the page title, or the page description can be used as the title.'),
 			'type' => 'flag',
 			'default' => 'y',
+			'tags' => array('basic'),
 		),
 		'feature_wiki_pageid' => array(
 			'name' => tra('Display page ID'),
