c6dad6179cc5947cf5c19dedf2f7501bc53db6de
jonnybradley <jonnybradley@users.sourceforge.net>
2016-07-27 10:26:11 +0000

    [bp/r59263][DOC] missing var type [bp/r59282][DOC] missing var type
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@59283 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/init/tra.php
+++ lib/init/tra.php
@@ -14,7 +14,7 @@ $interactive_collected_strings = array();
 
 /**
  * needs a description
- * @param $content
+ * @param string $content
  * @return mixed|string
  */function tr($content)
 {
@@ -24,7 +24,7 @@ $interactive_collected_strings = array();
 
 /**
  * translate an English string
- * @param        $content English string
+ * @param string $content English string
  * @param string $lg      language - if not specify = global current language
  * @param bool   $unused
  * @param array  $args
