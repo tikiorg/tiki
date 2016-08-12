7de1435d100896da12163bfabba2c78f84d78ffc
kroky6 <kroky6@users.sourceforge.net>
2016-08-12 10:37:04 +0000

    [FIX] update tablesorter auto dropdown filter options to match upstream settings more closely and not copy internals
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@59431 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Table/Code/WidgetOptionsFilter.php
+++ lib/core/Table/Code/WidgetOptionsFilter.php
@@ -44,8 +44,8 @@ class Table_Code_WidgetOptionsFilter extends Table_Code_WidgetOptions
 			//filter_functions and filter_formatter
 			if (parent::$filtercol) {
 				$ffunc = '';
+				$fsource = '';
 				$fform = '';
-				$custom_filter_columns = array();
 				foreach (parent::$s['columns'] as $col => $info) {
 					$info = !empty($info['filter']) ? $info['filter'] : [];
 					$colpointer =  parent::$usecolselector ? (string) '\'' . $col . '\'' : (int) $col;
@@ -68,7 +68,7 @@ class Table_Code_WidgetOptionsFilter extends Table_Code_WidgetOptions
 										$ffunc[] = $colpointer . ' : ' . $options;
 									}
 								} elseif (!parent::$ajax) {
-									if( array_key_exists('empty', $info) ) {
+									if( array_key_exists('empty', $info) )
 										$ffunc[] = $colpointer . ' : {
 					\'(empty)\': function( e, n, f, i, $r, c, data ) {
 						if( typeof e === "Object" && typeof n === "Object" && typeof f === "undefined" ) {
@@ -84,11 +84,16 @@ class Table_Code_WidgetOptionsFilter extends Table_Code_WidgetOptions
 								data.filter === data.exact;
 					}
 				}';
-										$custom_filter_columns[$col] = $info['empty'];
-									} else {
+									else
 										$ffunc[] = $colpointer . ' : true';
-										$custom_filter_columns[$col] = false;
-									}
+									$fsource[] = $colpointer . ' : function( table, column, onlyAvail ) {'
+										. $this->nt4 . 'var array = $.tablesorter.filter.getOptions(table, column, onlyAvail);'
+										. $this->nt4 . 'array = array.join(\',\').split(/\s*,\s*/);'
+										. ( array_key_exists('empty', $info) ?
+											$this->nt4 . 'array.push({value: \'(empty)\', text: ' . json_encode($info['empty']) . '});'
+											: '' )
+										. $this->nt4 . 'return array;'
+									. $this->nt3 . '}';
 								}
 								break;
 							case 'range' :
@@ -114,71 +119,12 @@ class Table_Code_WidgetOptionsFilter extends Table_Code_WidgetOptions
 				if (is_array($ffunc)) {
 					$wof[] = $this->iterate($ffunc, 'filter_functions : {', $this->nt3 . '}', $this->nt4, '');
 				}
+				if (is_array($fsource)) {
+					$wof[] = $this->iterate($fsource, 'filter_selectSource : {', $this->nt3 . '}', $this->nt4, '');
+				}
 				if (is_array($fform)) {
 					$wof[] = $this->iterate($fform, 'filter_formatter : {', $this->nt3 . '}', $this->nt4, '');
 				}
-				if( $custom_filter_columns ) {
-					$wof[] = 'filter_matchType : { \'input\': \'exact\', \'select\': \'match\' }';
-					$wof[] = 'filter_selectSource : function( table, column, onlyAvail ) {
-				table = $( table )[0];
-				var rowIndex, tbodyIndex, len, row, cell, cache, indx, child, childLen,
-					c = table.config,
-					wo = c.widgetOptions,
-					arry = [];
-				var emptyFilters = ' . json_encode($custom_filter_columns) . ';
-				if( emptyFilters[column] )
-					arry[ arry.length ] = {value: "(empty)", text: emptyFilters[column]};
-				for ( tbodyIndex = 0; tbodyIndex < c.$tbodies.length; tbodyIndex++ ) {
-					cache = c.cache[tbodyIndex];
-					len = c.cache[tbodyIndex].normalized.length;
-					// loop through the rows
-					for ( rowIndex = 0; rowIndex < len; rowIndex++ ) {
-						// get cached row from cache.row ( old ) or row data object
-						// ( new; last item in normalized array )
-						row = cache.row ?
-							cache.row[ rowIndex ] :
-							cache.normalized[ rowIndex ][ c.columns ].$row[0];
-						// check if has class filtered
-						if ( onlyAvail && row.className.match( wo.filter_filteredRow ) ) {
-							continue;
-						}
-						// get non-normalized cell content
-						if ( wo.filter_useParsedData ||
-							c.parsers[column].parsed ||
-							c.$headerIndexed[column].hasClass( \'filter-parsed\' ) ) {
-							arry[ arry.length ] = \'\' + cache.normalized[ rowIndex ][ column ];
-							// child row parsed data
-							if ( wo.filter_childRows && wo.filter_childByColumn ) {
-								childLen = cache.normalized[ rowIndex ][ c.columns ].$row.length - 1;
-								for ( indx = 0; indx < childLen; indx++ ) {
-									arry[ arry.length ] = \'\' + cache.normalized[ rowIndex ][ c.columns ].child[ indx ][ column ];
-								}
-							}
-						} else {
-							// get raw cached data instead of content directly from the cells
-							// and parse by new line to add rows as separate values
-							cell = cache.normalized[ rowIndex ][ c.columns ].$row.children().eq( column );
-							if( cell.text().match(/[\r\n]/) )
-								arry = arry.concat(cell.text().split(/[\r\n]/));
-							else
-								arry = arry.concat(cell.html().split(/\s*<br\s*\/?>\s*/i));
-							// child row unparsed data
-							if ( wo.filter_childRows && wo.filter_childByColumn ) {
-								childLen = cache.normalized[ rowIndex ][ c.columns ].$row.length;
-								for ( indx = 1; indx < childLen; indx++ ) {
-									cell = cache.normalized[ rowIndex ][ c.columns ].$row.eq( indx ).children().eq( column );
-									if( cell.text().match(/[\r\n]/) )
-										arry = arry.concat(cell.text().split(/[\r\n]/));
-									else
-										arry = arry.concat(cell.html().split(/\s*<br\s*\/?>\s*/i));
-								}
-							}
-						}
-					}
-				}
-				return arry;
-			}';
-									}
 			}
 			if (count($wof) > 0) {
 				return $wof;
