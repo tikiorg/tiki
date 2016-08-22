9d7cdb0514237006d6aa562daefc9e4c74715806
jonnybradley <jonnybradley@users.sourceforge.net>
2016-08-22 17:57:59 +0000

    [bp/r59488][FIX] mail-queue: Check the object retrieved from the database is a Zend mail message, i.e. if there was old data from pre Tiki 15 using ZF1 (thanks marclaporte)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@59489 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Tiki/Command/MailQueueSendCommand.php
+++ lib/core/Tiki/Command/MailQueueSendCommand.php
@@ -39,7 +39,7 @@ class MailQueueSendCommand extends Command
           $output->writeln('Sending message '.$message['messageId'].'...');
           $mail = unserialize($message['message']);
 
-          if ($mail) {
+          if ($mail && get_class($mail) === 'Zend\Mail\Message') {
             try {
                 tiki_send_email($mail);
                 $title = 'mail';
@@ -69,7 +69,7 @@ class MailQueueSendCommand extends Command
 
             \TikiDb::get()->query($query, array($message['messageId']));
           } else {
-              $output->writeln('ERROR: Unable to unserialize the mailer object.');
+              $output->writeln('ERROR: Unable to unserialize the mail object id:' . $messages['messageId']);
           }
       }
       $output->writeln('Mail queue processed...');
