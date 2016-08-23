2ff082465cf61a91e54f2864c82df5a7a86d0ada
jonnybradley <jonnybradley@users.sourceforge.net>
2016-08-23 17:07:57 +0000

    [FIX] search: When the default operator is set to AND, any searches including words in the stopword list (pref) were failing, so filter them out when the phrase is being parsed.  Working for elastic and lucene, mysql would be complicated... (thanks marclaporte)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@59503 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Search/Expr/Parser.php
+++ lib/core/Search/Expr/Parser.php
@@ -18,7 +18,9 @@ class Search_Expr_Parser
 			if (in_array(strtoupper($part), $this->special)) {
 				$tokens[] = strtoupper($part);
 			} elseif (strpos($part, ' ') === false) {
-				$tokens[] = new Search_Expr_Token($part);
+				if ( ! $this->isAStopWord($part)) {
+					$tokens[] = new Search_Expr_Token($part);
+				}
 			} else {
 				$tokens[] = new Search_Expr_ExplicitPhrase($part);
 			}
@@ -182,6 +184,26 @@ class Search_Expr_Parser
 		}
 
 		return $out;
+	}
+
+	/**
+	 * when using AND as the default operator queries including stopped words fail
+	 * so remove them here for the relevant engines (elastic and lucene only so far)
+	 *
+	 * @param string $word
+	 * @return boolean
+	 */
+	private function isAStopWord($word)
+	{
+		global $prefs;
+
+		if ($prefs['unified_lucene_default_operator'] == 0 || $prefs['unified_engine'] === 'mysql') {
+			return false;
+		}
+
+		$stopwords = $prefs['unified_stopwords'];
+
+		return in_array($word, $stopwords);
 	}
 }
 
