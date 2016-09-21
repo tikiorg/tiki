8d3b26a72835173471a03fe09d0df883f4355984
jonnybradley <jonnybradley@users.sourceforge.net>
2016-09-21 18:05:48 +0000

    [FIX] PHP minimum version 5.6
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@59767 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup.php
+++ tiki-setup.php
@@ -20,13 +20,13 @@ if (strpos($_SERVER['SCRIPT_NAME'], basename(__FILE__)) !== false) {
 	header('location: index.php');
 	exit;
 }
-if (version_compare(PHP_VERSION, '5.5.0', '<')){
+if (version_compare(PHP_VERSION, '5.6.0', '<')){
 	if(php_sapi_name() != 'cli') {					// if not running a command line version of php, show requirements
 		header('location: tiki-install.php');
 		exit;
 	}
 	// This is command-line. No 'location' command make sense here. Let admins access what works and deal with the rest.
-	echo "Warning: Tiki13 and above expects PHP 5.5.0 and above. You are running " . phpversion() . " at your own risk\n";
+	echo "Warning: Tiki16 and above expects PHP 5.6.0 and above. You are running " . phpversion() . " at your own risk\n";
 }
 
 // Be sure that the user is not already defined by PHP on hosts that still have the php.ini config "register_globals = On"
