01c0182c7d1eacdf92299d814dd86da574c620e3
kroky6 <kroky6@users.sourceforge.net>
2016-09-28 09:56:45 +0000

    [FIX] use UTC timezone when using search formatter datetime as all index datetime strings are stored in UTC
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@59828 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Search/Formatter/ValueFormatter/Datetime.php
+++ lib/core/Search/Formatter/ValueFormatter/Datetime.php
@@ -24,9 +24,16 @@ class Search_Formatter_ValueFormatter_Datetime extends Search_Formatter_ValueFor
 		}
 
 		$tikilib = TikiLib::lib('tiki');
+
+		// indexed datetime value is always UTC, so use correct timezone when converting back to timestamp
+		$old_tz = date_default_timezone_get();
+		date_default_timezone_set('UTC');
+		$time = strtotime($value);
+		date_default_timezone_set($old_tz);
+
 		if (is_numeric($value)) {	// expects a unix timestamp but might be getting the default value
 			return $tikilib->date_format($this->format, $value);
-		} elseif (false !== $time = strtotime($value)) {
+		} elseif (false !== $time) {
 			return $tikilib->date_format($this->format, $time);
 		} else {
 			return $value;
