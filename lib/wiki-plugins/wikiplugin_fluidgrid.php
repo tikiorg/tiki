e6b28863004daf2aeb8a43900a33fbd084307050
pjollans <pjollans@users.sourceforge.net>
2016-10-23 08:24:25 +0000

    - Show an error message if there are more than 12 columns - New column size logic for better compatibility with the split plugin.   If the sum of the column sizes is greater than we, then they are interpreted as approximate weighting values.   For example, 800|400 would be equivalent to 8|4. Probably 900|400 would also result in 8|4. - joincols parameter implemented - New parameter displaysize can be used to select the bootstrap option small, medium, large or extra large,   equivalent to the abbreviations sm, md, lg and xl in the class names, e.g. col-sm-6, or col-md-6
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@60031 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_fluidgrid.php
+++ lib/wiki-plugins/wikiplugin_fluidgrid.php
@@ -7,63 +7,77 @@
 
 function wikiplugin_fluidgrid_info()
 {
-	return array(
-		'name' => tra('Fluid Grid'),
-		'documentation' => 'PluginFluidGrid',
-		'description' => tra('Arrange content into rows and columns using the bootstrap fluid grid'),
-		'prefs' => array( 'wikiplugin_fluidgrid' ),
-		'body' => tra('Text'),
-		'filter' => 'wikicontent',
-		'iconname' => 'table',
-		'introduced' => 1,
-		'tags' => array( 'basic' ),
-		'params' => array(
-			'joincols' => array(
-				'required' => false,
-				'name' => tra('Join Columns'),
-				'description' => tra('Generate the colspan attribute if columns are missing'),
-				'since' => '1',
-				'filter' => 'alpha',
-				'default' => 'y',
-				'options' => array(
-					array('text' => '', 'value' => ''), 
-					array('text' => tra('Yes'), 'value' => 'y'), 
-					array('text' => tra('No'), 'value' => 'n')
-				)
-			),
-			'colsize' => array(
-				'required' => false,
-				'name' => tra('Column Sizes'),
-				'description' => tra('Specify all column widths in units which add up to 12'),
-				'since' => '1',
-				'seprator' => '|',
-				'filter' => 'text',
-				'default' => '',
-			),
-			'first' => array(
-				'required' => false,
-				'name' => tra('First'),
-				'description' => tra('Cells specified are ordered first left to right across rows (default) or top to
-					bottom down columns'),
-				'since' => '1',
-				'filter' => 'alpha',
-				'default' => 'line',
-				'options' => array(
-					array('text' => '', 'value' => ''), 
-					array('text' => tra('Column'), 'value' => 'col'), 
-					array('text' => tra('Line'), 'value' => 'line')
-				)
-			),
-			'customclass' => array(
-				'required' => false,
-				'name' => tra('Custom Class'),
-				'description' => tra('Add a class to customize the design'),
-				'since' => '3.0',
-				'filter' => 'text',
-				'default' => '',
-			),
-		),
-	);
+    return array(
+        'name' => tra('Fluid Grid'),
+        'documentation' => 'PluginFluidGrid',
+        'description' => tra('Arrange content into rows and columns using the bootstrap fluid grid'),
+        'prefs' => array( 'wikiplugin_fluidgrid' ),
+        'body' => tra('Text'),
+        'filter' => 'wikicontent',
+        'iconname' => 'table',
+        'introduced' => 1,
+        'tags' => array( 'basic' ),
+        'params' => array(
+            'joincols' => array(
+                'required' => false,
+                'name' => tra('Join Columns'),
+                'description' => tra('Merge empty cells into the cell to their left'),
+                'since' => '1',
+                'filter' => 'alpha',
+                'default' => 'y',
+                'options' => array(
+                    array('text' => '', 'value' => ''), 
+                    array('text' => tra('Yes'), 'value' => 'y'), 
+                    array('text' => tra('No'), 'value' => 'n')
+                )
+            ),
+            'devicesize' => array(
+                'required' => false,
+                'name' => tra('Device size'),
+                'description' => tra('Specify the device size below which the cells will be stacked vertically'),
+                'since' => '1',
+                'filter' => 'alpha',
+                'default' => 'sm',
+                'options' => array(
+                    array('text' => '',                 'value' => ''), 
+                    array('text' => tra('Small'),       'value' => 'sm'), 
+                    array('text' => tra('Medium'),      'value' => 'md'), 
+                    array('text' => tra('Large'),       'value' => 'lg'), 
+                    array('text' => tra('Extra Large'), 'value' => 'xl')
+                )
+            ),
+            'colsize' => array(
+                'required' => false,
+                'name' => tra('Column Sizes'),
+                'description' => tra('Specify all column widths in units which add up to 12'),
+                'since' => '1',
+                'seprator' => '|',
+                'filter' => 'text',
+                'default' => '',
+            ),
+            'first' => array(
+                'required' => false,
+                'name' => tra('First'),
+                'description' => tra('Cells specified are ordered first left to right across rows (default) or top to bottom down columns'),
+                'since' => '1',
+                'filter' => 'alpha',
+                'default' => 'line',
+                'options' => array(
+                    array('text' => '', 'value' => ''), 
+                    array('text' => tra('Column'), 'value' => 'col'), 
+                    array('text' => tra('Line'), 'value' => 'line')
+                )
+            ),
+            'customclass' => array(
+                'required' => false,
+                'name' => tra('Custom Class'),
+                'description' => tra('Add a class to customize the design'),
+                'since' => '3.0',
+                'filter' => 'text',
+                'default' => '',
+            ),
+        ),
+    );
 }
 
 //
@@ -72,10 +86,10 @@ function wikiplugin_fluidgrid_info()
 // 
 function wikiplugin_fluidgrid_rollback($data, $hashes)
 {
-	foreach ($hashes as $hash=>$match) {
-		$data = str_replace($hash, $match, $data);
-	}
-	return $data;
+    foreach ($hashes as $hash=>$match) {
+        $data = str_replace($hash, $match, $data);
+    }
+    return $data;
 }
 
 /*
@@ -84,18 +98,18 @@ function wikiplugin_fluidgrid_rollback($data, $hashes)
  *       start of new line ('\n' character - e.g. lists and headers)... such
  *       user lines must stay with the same layout when applying
  *       this plugin to render them properly after...
- *		$data = the preparsed data (plugin, code, np.... already parsed)
- *		$pos is the position in the object where the non-parsed data begins
+ *      $data = the preparsed data (plugin, code, np.... already parsed)
+ *      $pos is the position in the object where the non-parsed data begins
  */
 function wikiplugin_fluidgrid($data, $params, $pos)
 {
-	//
+    //
     // The following function uses a regular expression in the form
-	// "/pattern/ismU"
+    // "/pattern/ismU"
     // where / is used as a delimiter and ismU are pattern modifiers
     // i = case insensitive
     // s = dot matches anything (including new line)
-	// m = multiline 
+    // m = multiline 
     // U = Ungreedy 
     //
     // The regular expression matches a list of specific plugins. The following
@@ -109,44 +123,50 @@ function wikiplugin_fluidgrid($data, $params, $pos)
     // e.g. {SPIIT()}...{SPLIT()}...{SPIIT}...{SPLIT}
     //
     global $tikilib, $tiki_p_admin_wiki, $tiki_p_admin, $section;
-	global $replacement;
-	preg_match_all('/{(FLUIDGRID|SPLIT|CODE|HTML|FADE|JQ|JS|MOUSEOVER|VERSIONS).+{\1}/ismU', $data, $matches);
-	$hashes = array();
-	foreach ($matches[0] as $match) {
-		if (empty($match)) continue;
-		$hash = md5($match);
-		$hashes[$hash] = $match;
-		$data = str_replace($match, $hash, $data);
-	}
+    global $replacement;
+    preg_match_all('/{(FLUIDGRID|SPLIT|CODE|HTML|FADE|JQ|JS|MOUSEOVER|VERSIONS).+{\1}/ismU', $data, $matches);
+    $hashes = array();
+    foreach ($matches[0] as $match) {
+        if (empty($match)) continue;
+        $hash = md5($match);
+        $hashes[$hash] = $match;
+        $data = str_replace($match, $hash, $data);
+    }
 
-	// Remove first <ENTER> if exists...
-	// it may be here if present after {FLUIDGRID()} in original text
-	if (substr($data, 0, 2) == "\r\n")
-		$data2 = substr($data, 2);
-	else
-		$data2 = $data;
-	
-	extract($params, EXTR_SKIP);
-	$joincols  = (!isset($joincols)  || $joincols  == 'y' || $joincols  == 1 ? true : false);
-	// Split data by rows and cells
+    // Remove first <ENTER> if exists...
+    // it may be here if present after {FLUIDGRID()} in original text
+    if (substr($data, 0, 2) == "\r\n")
+        $data2 = substr($data, 2);
+    else
+        $data2 = $data;
+    
+    extract($params, EXTR_SKIP);
+    $joincols  = (!isset($joincols)  || $joincols  == 'y' || $joincols  == 1 ? true : false);
+    
+    // Check the device size parameter which must be one of 'sm', 'md', 'lg' or 'xl'
+    if ( !isset($devicesize) || !( ( $devicesize == 'sm' ) || ( $devicesize == 'md' ) || ( $devicesize == 'lg' ) || ( $devicesize == 'xl' ) ) )
+    {
+      $devicesize = 'sm' ;     
+    }
+    
+    // Split data by rows and cells
+    $sections = preg_split("/@@@+/", $data2);
+    $rows = array();
+    $maxcols = 0;
+    foreach ($sections as $i) {
+        // split by --- but not by ----
+        //  $rows[] = preg_split("/([^\-]---[^\-]|^---[^\-]|[^\-]---$|^---$)+/", $i);
+        //  not to eat the character close to - and to split on --- and not ----
+        $rows[] = preg_split("/(?<!-)---(?!-)/", $i);
+        $maxcols = max($maxcols, count(end($rows)));
+    }
 
-	$sections = preg_split("/@@@+/", $data2);
-	$rows = array();
-	$maxcols = 0;
-	foreach ($sections as $i) {
-		// split by --- but not by ----
-		//	$rows[] = preg_split("/([^\-]---[^\-]|^---[^\-]|[^\-]---$|^---$)+/", $i);
-		//	not to eat the character close to - and to split on --- and not ----
-		$rows[] = preg_split("/(?<!-)---(?!-)/", $i);
-		$maxcols = max($maxcols, count(end($rows)));
-	}
-
-	// Are there any sections present?
-	// Do not touch anything if not... don't even generate <table>
-	if (count($rows) <= 1 && count($rows[0]) <= 1)
-	   return wikiplugin_fluidgrid_rollback($data, $hashes);
-
-	//
+    // Are there any sections present?
+    // Do not touch anything if not... don't even generate <table>
+    if (count($rows) <= 1 && count($rows[0]) <= 1)
+       return wikiplugin_fluidgrid_rollback($data, $hashes);
+       
+    //
     // The "first" parameter indicates whether the content is listed in columns
     // or in rows (aka. lines).
     //
@@ -160,7 +180,7 @@ function wikiplugin_fluidgrid($data, $params, $pos)
     // flipping the matrices before generating the table. This is probably
     // not very efficient, but keeps the code fairly readable. 
     //
-	if ( isset($first) && $first == 'col' ) 
+    if ( isset($first) && $first == 'col' ) 
     {
       $cols    = array() ;
       $maxrows = count($rows) ;
@@ -184,81 +204,202 @@ function wikiplugin_fluidgrid($data, $params, $pos)
       $maxcols = $maxrows ;
     }
 
+    // The bootstrap fluid grid can handle a maximum of 12 colums.
+    // Check this AFTER flipping the axis for column mode.
+    if ( $maxcols > 12 )
+      return ( "<b>Fluid Grid can have a maximum of 12 columns</b><br/>") ;
+      
     // Handle the column widths.
-    // The colsize parameter can be used to specify column widths. If used
-    // then the values should add up to 12, e.g. 6|2|2|2
-    
+    //
+    // There are several cases:
+    // (1) Colsize is not present
+    //     - Share out the space as evenly as possible
+    //
+    // (2) Colsize is present
+    //     It specifies the size of all columns
+    //     The total size is <= 12
+    //     - Assign exactly the specified sizes
+    //  
+    // (3) Colsize is present
+    //     It does not specifiy the size of all columns
+    //     The total size plus the number of unsized columns is <= 12
+    //     - Assign the specified sizes and share out the remaining
+    //       units among the unsized columns
+    //  
+    // (4) Colsize is present
+    //     It specifies the size of all columns
+    //     The total size is > 12
+    //     - Use the size as an approximate weighting
+    //  
+    // (5) Colsize is present
+    //     It does not specifiy the size of all columns
+    //     The total size is > 12
+    //     - Use the size as an approximate weighting
+    //  
+
+    // We will store the final widths in this array
+    $w_array = array() ;
+
     if ( isset($colsize) ) 
-        $tdsize = explode("|", $colsize);
-    else    
-        $tdsize = array() ;
-        
-    // Total size of columns with specified size
-    $tdtotal  = 0 ;
-    
-    // Count of columns without specified size
-    $tdnosize = 0 ;
-    
-    for ( $i=0 ; $i<$maxcols ; $i++ ) 
     {
-      // I want something like 6||2 to behave in a reasonable manner
-      // (although I'm not sure it is really useful) so check for 
-      // empty strings.
-      if ( isset ( $tdsize[$i] ) && ( trim ( $tdsize[$i] ) != '' ) ) 
+      // colsize is specified
+      // Count the total size and the number of unsized columns
+      $tdsize   = explode("|", $colsize);
+      $tdtotal  = 0 ;
+      $tdnosize = 0 ;
+      $s_array  = array() ;
+
+      for ( $i=0 ; $i<$maxcols ; $i++ ) 
       {
-        $tdsize[$i] = trim ( $tdsize[$i] ) ;
-        $tdtotal    += $tdsize[$i] ;
-      }
-      else
-      {
-        $tdsize[$i] = 0 ;
-        $tdnosize++ ;
-      }
-    }
-    
-    // Share the remaining space out among the unsized columns
-    $remaining = 12 - $tdtotal ;
-    $share     = ceil ( $remaining / $tdnosize ) ;
-    
-    for ( $i=0 ; $i<$maxcols ; $i++ ) 
-    {
-      if ( $tdsize[$i] == 0 ) 
-      {
-        if ( $remaining > $share )
+        if ( isset ( $tdsize[$i] ) && ( trim ( $tdsize[$i] ) != '' ) ) 
         {
-          $tdsize[$i] = $share ;
-          $remaining -= $share ;
+          $w = trim ( $tdsize[$i] ) ;
+          if ( $w < 1 )
+          {
+            // treat 0 as unsized
+            $s_array[$i] = 0 ;
+            $tdnosize++ ;
+          }  
+          else
+          {
+            $s_array[$i] =  $w ;
+            $tdtotal     += $w ;
+          }  
         }
         else
         {
-          $tdsize[$i] = $remaining ;
-          $remaining  = 0 ;
-        }   
+          $s_array[$i] = 0 ;
+          $tdnosize++ ;
+        }
+      }
+      
+      if ( ( $tdtotal + $tdnosize ) <= 12 )
+      {
+        // Use the values as specified.
+        // Share the remaining space out among the unsized columns
+        $remaining = 12 - $tdtotal ;
+        $share     = ceil ( $remaining / $tdnosize ) ;
+        
+        for ( $i=0 ; $i<$maxcols ; $i++ ) 
+        {
+          if ( $s_array[$i] == 0 ) 
+          {
+            $w_array[$i] = ceil ( $remaining / $tdnosize ) ;
+            $remaining   -= $w_array[$i] ;
+            $tdnosize-- ;
+          }
+          else
+          {
+            $w_array[$i] = $s_array[$i] ; 
+          }   
+        }
+      }
+      else
+      {
+        // Use the values as approximate weightings
+        // Start by assigning every column a width of 1
+        for ( $i=0 ; $i<$maxcols ; $i++ ) 
+        {
+          $w_array[$i] = 1 ; 
+        }
+        
+        // Now share out the rest
+        $i = 0 ;
+        $j = $maxcols ; 
+        $h = 0 ;
+        while ( $j < 12 )
+        {
+          // Increment the width if it is underweight
+          if ( ( $w_array[$i] / 12 ) < ( $s_array[$i] / $tdtotal ) )
+          {
+            $w_array[$i]++ ;
+            $j++ ; 
+          }
+          
+          // Increment column number and wraparound
+          $i++ ;
+          if ( $i >= $maxcols )
+          {
+            // Wraparound
+            $i = 0 ;
+            
+            // $j must increase in each pass through the columns.
+            if ( $h < $j )
+            {
+              // Store the current position
+              $h = $j ;
+            }
+            else
+            {
+              // We get here if the alogrithm fails.
+              // This should be dead code, but that's better then an endless loop.   
+              break ;
+            }
+          }    
+        }
       }
     }
-    
-	//$result = "<div class='container-fluid" . ( !empty($customclass) ? " $customclass" : "") . "'>" ;
-	$result = "<div" . ( !empty($customclass) ? " class='$customclass'" : "") . ">" ;
-
-	foreach ($rows as $r) 
+    else
     {
-	  $result .= "<div class='row'>" ;
-      
-      $idx = 0 ;
-      foreach ($r as $i) 
+      // colsize is not specified
+      // Share out the 12 units
+      $remaining = 12  ;
+      for ( $i=0 ; $i<$maxcols ; $i++ ) 
       {
-        // Insert "\n" at data begin (so start-of-line-sensitive syntaxes will be parsed OK)
-        $result .= "<div class='col-sm-" . $tdsize[$idx] . "'>\n" . trim($i) . "</div>" ;
+        // Share among the remaining columns.
+        // Round up as long as there is a remainder.
+        // Eventually it will be an integer.
+        $w_array[$i] =  ceil ( $remaining / ($maxcols-$i) ) ;
+        $remaining   -= $w_array[$i] ; 
+      }
+    }
 
-        $idx++;
+    //$result = "<div class='container-fluid" . ( !empty($customclass) ? " $customclass" : "") . "'>" ;
+    $result = "<div" . ( !empty($customclass) ? " class='$customclass'" : "") . ">" ;
+
+    foreach ($rows as $r) 
+    {
+      // Start of the row
+      $result .= "<div class='row'>" ;
+
+      $j=0 ; 
+      while ( $j < $maxcols )
+      {
+        // Get the column width
+        $w = $w_array[$j] ;
+        
+        // Get the content
+        $c = ( isset($r[$j]) ) ? trim($r[$j]) : "" ;
+        
+        if ( $joincols )
+        {
+          // Check for empty columns to the right
+          for ( $k = $j+1 ; $k < $maxcols ; $k++ )
+          {
+            if ( isset($r[$k]) && ( trim ($r[$k]) != '' ) )
+              break ;
+            else
+            {
+              // Grab the space from the next column and skip it
+              $j = $k ;  
+              $w += $w_array[$j] ;
+            }       
+          }  
+        }
+        
+        // Generate some output
+        $result .= "<div class='col-" . $devicesize . "-" . $w . "'>\n" . $c . "</div>" ;
+
+        // Increment the column number (because we are using while, not for)
+        $j++ ;            
       }
       
+      // End of the row
       $result .= "</div>";
-	}
+    }
 
-	// Close HTML table (no \n at end!)
-	$result .= "</div>";
+    // Close HTML table (no \n at end!)
+    $result .= "</div>";
 
-	return wikiplugin_fluidgrid_rollback($result, $hashes);
+    return wikiplugin_fluidgrid_rollback($result, $hashes);
 }
-
