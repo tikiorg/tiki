4e0f321862ca4ada943c4a72ca78f99c079e1e1d
drsassafras <drsassafras@users.sourceforge.net>
2016-11-06 03:57:06 +0000

    [FIX] Resolving a bug I introduced the prevents admins from verifying accounts.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@60169 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/userslib.php
+++ lib/userslib.php
@@ -1570,9 +1570,9 @@ class UsersLib extends TikiLib
 		$user = $res['login'];
 
 		// check for account flags
-			if ($res['waiting'] == 'u'){				// if account is in validation mode.
+			if ($res['waiting'] == 'u' || 'a'){				// if account is in validation mode.
 
-				if ($pass == $res['valid']) 			// if user sucessfully provies code from email
+				if ($pass == $res['valid']) 			// if user successfully provides code from email
 					return array(USER_VALID, $user);
 				else
 					return array(ACCOUNT_WAITING_USER, $user);  // if code validation fails, (or user tries to log in before verifying)
@@ -1582,7 +1582,7 @@ class UsersLib extends TikiLib
 				return array(ACCOUNT_DISABLED, $user);
 
 			if ($validate_phase)									 
-				return array(USER_PREVIOUSLY_VALIDATED, $user);		// if email verifycation code is used an a validated account, deny.
+				return array(USER_PREVIOUSLY_VALIDATED, $user);		// if email verification code is used an a validated account, deny.
 
 
 		// next verify the password with every hashes methods
