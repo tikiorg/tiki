b8aaa3c60e613d7d53bcd18b75655a94ecd8f486
drsassafras <drsassafras@users.sourceforge.net>
2016-11-06 16:36:54 +0000

    [ENH] Update installer to new login code.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@60175 b456876b-0849-0410-b77d-98878d47e9d5

--- db/tiki.sql
+++ db/tiki.sql
@@ -2742,7 +2742,7 @@ CREATE TABLE `users_users` (
 ) ENGINE=MyISAM AUTO_INCREMENT=1 ;
 
 -- Administrator account
-INSERT INTO users_users(email,login,password,hash,created,registrationDate) VALUES ('','admin','admin',md5('adminadmin'),UNIX_TIMESTAMP(),UNIX_TIMESTAMP());
+INSERT INTO users_users(email,login,hash,created,registrationDate) VALUES ('','admin','$2y$10$nzMJ64PLyjKqgKvqSvO/S.n8jtgAiRzmNMYPLq/TQVLfYIFa0xqkG',UNIX_TIMESTAMP(),UNIX_TIMESTAMP());
 INSERT INTO tiki_user_preferences (user,`prefName`,value) VALUES ('admin','realName','System Administrator');
 INSERT INTO users_usergroups (`userId`, `groupName`) VALUES(1,'Admins');
 INSERT INTO users_grouppermissions (`groupName`, `permName`) VALUES ('Admins','tiki_p_admin');
