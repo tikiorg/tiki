2b45988d83517998b50fd77578cb1950a1b31a33
kroky6 <kroky6@users.sourceforge.net>
2016-11-11 07:09:09 +0000

    [bp/60239] [FIX] prevent duplication of trackeritem search index queue updates but use SORT_REGULAR as unique comparison since string comparisons left only one object in the list of arrays
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@60240 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Search/Indexer.php
+++ lib/core/Search/Indexer.php
@@ -80,7 +80,7 @@ class Search_Indexer
 	public function update(array $objectList)
 	{
 
-		foreach (array_unique($objectList) as $object) {
+		foreach (array_unique($objectList, SORT_REGULAR) as $object) {
 			$this->searchIndex->invalidateMultiple(array($object));
 			$this->addDocument($object['object_type'], $object['object_id']);
 		}
