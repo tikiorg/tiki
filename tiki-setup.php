0859daff67ffe5af77d0999a4ad394bd7c5055f0
yonixxx <yonixxx@users.sourceforge.net>
2016-12-12 09:13:58 +0000

    [bp/r59001] [Fix] Missing language prefs for re captcha.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@60595 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-setup.php
+++ tiki-setup.php
@@ -247,7 +247,11 @@ if ($prefs['feature_wysiwyg'] == 'y') {
 if ($prefs['feature_antibot'] == 'y' && empty($user)) {
 	if ($prefs['recaptcha_enabled'] === 'y') {
 		if ($prefs['recaptcha_version'] == '2') {
-			$headerlib->add_jsfile_cdn("$url_scheme://www.google.com/recaptcha/api.js");
+			if (!empty($prefs['language'])) {
+				$headerlib->add_jsfile_cdn("$url_scheme://www.google.com/recaptcha/api.js?hl=" . $prefs['language']);
+			} else {
+				$headerlib->add_jsfile_cdn("$url_scheme://www.google.com/recaptcha/api.js");
+			}
 		} else {
 			$headerlib->add_jsfile_cdn("$url_scheme://www.google.com/recaptcha/api/js/recaptcha_ajax.js");
 		}
