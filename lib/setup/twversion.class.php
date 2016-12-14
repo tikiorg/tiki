39d11c6c41018c7ad70055826ddfeece584498fa
jonnybradley <jonnybradley@users.sourceforge.net>
2016-12-14 16:04:55 +0000

    [REL] preparing 15.3 release
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@60643 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '15.3svn';	// needs to have no spaces for releases
+		$this->version 	= '15.3';	// needs to have no spaces for releases
 		$this->star	= 'Situla';
 		$this->releases	= array();
 
@@ -240,6 +240,7 @@ class TWVersion
 				'15.0',
 				'15.1',
 				'15.2',
+				'15.3',
 			);
 	}
 
