d16550a768e5d19106ef84293f74595ed1ced84e
drsassafras <drsassafras@users.sourceforge.net>
2017-01-09 10:14:17 +0000

    [REF] removed comments used for testing
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@60816 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/theme.php
+++ lib/setup/theme.php
@@ -153,14 +153,7 @@ if ($prefs['site_favicon_disable'] !== 'y') {
     $favicon_path = $themelib->get_theme_path($prefs['theme'], $prefs['theme_option'], 'browserconfig.xml', 'favicons/');
     if (!$favicon_path) $favicon_path = $themelib->get_theme_path('base_files', '', 'browserconfig.xml', 'favicons/');
     if ($favicon_path) $headerlib->add_meta('msapplication-config', $favicon_path);
-    /*
-    {if $favicon_touch == true}<link rel="apple-touch-icon" sizes="180x180" href="img/favicons/apple-touch-icon.png">{/if}
-	{if $favicon_32 == true}<link rel="icon" type="image/png" href="img/favicons/favicon-32x32.png" sizes="32x32">{/if}
-	{if $favicon_16 == true}<link rel="icon" type="image/png" href="img/favicons/favicon-16x16.png" sizes="16x16">{/if}
-	{if $favicon_json == true}<link rel="manifest" href="img/favicons/manifest.json">{/if}
-	{if $favicon_pinned == true}<link rel="mask-icon" href="img/favicons/safari-pinned-tab.svg" color="#5bbad5">{/if}
-	{if $favicon_xml == true}<meta name="msapplication-config" content="img/favicons/browserconfig.xml">{/if}
-}*/
+
 }
 
 //8) produce $iconset to be used for generating icons
