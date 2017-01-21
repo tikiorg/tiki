d3ebe2ea2416c6868e04641b884a3b1baf4798b8
jonnybradley <jonnybradley@users.sourceforge.net>
2017-01-21 15:16:07 +0000

    [REL] Preparing release 12.11 and removing previous secdb file
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/12.x@60988 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/twversion.class.php
+++ lib/setup/twversion.class.php
@@ -27,7 +27,7 @@ class TWVersion
 		$this->branch 	= 'stable';
 
 		// Set everything else, including defaults.
-		$this->version 	= '12.11svn';	// needs to have no spaces for releases
+		$this->version 	= '12.11';	// needs to have no spaces for releases
 		$this->star	= 'Altair';
 		$this->releases	= array();
 
@@ -217,6 +217,7 @@ class TWVersion
 				'12.8',
 				'12.9',
 				'12.10',
+				'12.11',
 			);
 	}
 
