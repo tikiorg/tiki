cd12617b112254d17bd7f64648ef16f6ba7cc89f
drsassafras <drsassafras@users.sourceforge.net>
2017-01-30 06:25:32 +0000

    [ENH][FIX] Moved wiki dumps to Tools page; (part 2) adding index.php files to new directories.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@61131 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ storage/index.php
@@ -0,0 +1,10 @@
+<?php
+// (c) Copyright 2002-2017 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: index.php 57961 2016-03-17 20:01:56Z jonnybradley $
+
+// This redirects to the sites root to prevent directory browsing
+header("location: ../tiki-index.php");
+die;
