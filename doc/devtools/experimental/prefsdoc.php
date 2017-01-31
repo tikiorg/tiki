6b18a874b604982cab0d80126f736802040b89d9
drsassafras <drsassafras@users.sourceforge.net>
2017-01-31 14:52:35 +0000

    [ENH][REF] Pref Documentation Generation - Error Checking, escaping, bounds checking.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@61160 b456876b-0849-0410-b77d-98878d47e9d5

--- doc/devtools/experimental/prefsdoc.php
+++ doc/devtools/experimental/prefsdoc.php
@@ -13,13 +13,14 @@ if (php_sapi_name() !== 'cli'){
 	die;
 }
 
-
 $baseDir = realpath(__DIR__ .'/../../../').'/';
+
 require_once ($baseDir.'tiki-setup.php');
 
 /**
- * This file is used for automatically generating prefrence documentaion
- * for use on doc.tiki.org. It takes the currently installed prefrences
+ *
+ * Used for automatically generating preference documentation
+ * for use on doc.tiki.org. It takes the currently installed preferences
  * and generates files in wiki-syntax to be pasted into doc.tiki.org
  *
  * Running this will overwrite any previously generated pref doc files
@@ -27,66 +28,181 @@ require_once ($baseDir.'tiki-setup.php');
  *
  * limitations
  *
- * Wiki Syntax is not supported in documentation, but it is on doc.tiki.org
- * Currently there is no escaping of wiki syntax when files are generated.
+ * Currently a single file is generated for each preference page, however the
+ * documentation on doc.tiki.org is split into tabs.
  *
- * Currently a single file is generated for each prefrence page, however the
- * documentaion on doc.tiki.org is split into tabs.
+ *
+ * Class PrefsDoc
  *
  */
 
-@mkdir ($baseDir.'storage/prefsdoc');			// create subdir for housing generated files, if it does not exist
-$PrefVars = array();
-$fileCount=0;
+class PrefsDoc extends TWVersion{
 
-$docFiles = scandir($baseDir.'lib/prefs'); // grab all the files that house prefs
-foreach ($docFiles as $fileName) {
-	if ($fileName !== 'index.php' && substr($fileName, -4) === '.php') {  // filter out any file thats not a pref file
-		require ($baseDir.'lib/prefs/'.$fileName);
-		$callVar = 'prefs_'.substr($fileName,0,-4).'_list';
-		$PrefVars = array_merge($PrefVars,$callVar());						// create one big var with all the pref info
+	public $baseDir;
+	private $PrefVars;
+	public $fileCount;
+	public $prefCount;
+	public $prefDefault;
+	public $prefDescription;
+	public $prefName;
+	public $docTable;
+
+	public function getPrefs(){
+
+		$prefs = array();
+		$docFiles = scandir($this->baseDir.'lib/prefs'); // grab all the files that house prefs
+		foreach ($docFiles as $fileName) {
+			if ($fileName !== 'index.php' && substr($fileName, -4) === '.php') {  // filter out any file thats not a pref file
+				require ($this->baseDir.'lib/prefs/'.$fileName);
+				$callVar = 'prefs_'.substr($fileName,0,-4).'_list';
+				$prefs = array_merge($prefs,$callVar());			// create one big var with all the pref info
+
+			}
+		}
+		$this->PrefVars = $prefs;
+	}
+
+
+
+	/**
+	 *
+	 * This generates a list of prefs in the order that they appear on the admin panel.
+	 *
+	 * @param string $fileName Name of file to scan
+	 *
+	 * @return array  array of pref names
+	 *
+	 */
+	public function getAdminUIPrefs($fileName){
+			$file = file_get_contents($this->baseDir . 'templates/admin/' . $fileName);
+			$file = str_replace(array('\'', '"'), '', $file);
+			preg_match_all('{preference name=(\w*)(.*)}', $file, $out, PREG_PATTERN_ORDER);  // return array of every pref name in file
+			return $out[1];
+	}
+
+
+	/**
+	 *
+	 * This sets an vars of an indivigual prefrence
+	 *
+	 * @param $param
+	 */
+	public function setParams($param){
+
+		// set default
+		if ($this->PrefVars[$param]['default'] == 'n') {
+			$this->prefDefault = 'Disabled';
+		} else if ($this->PrefVars[$param]['default'] == 'y') {
+			$this->prefDefault = 'Enabled';			// Change default codes to human readable format
+		} else if (is_array($this->PrefVars[$param]['default'])){
+			$this->prefDefault = implode(', ',$this->PrefVars[$param]['default']);
+		}else{
+			$this->prefDefault = $this->PrefVars[$param]['default'];
+		}
+		if (strlen($this->prefDefault) > 30){
+			$this->prefDefault = substr($this->prefDefault,0,27).'...';
+		}
+
+		$this->prefDefault = $this->wikiConvert($this->prefDefault,true);
+
+		// set name
+		if ($this->PrefVars[$param]['help']) {
+			$this->prefName = '((' . $this->PrefVars[$param]['help'] . '|' . $this->PrefVars[$param]['name'] . '))';
+		}else {
+			$this->prefName = $this->PrefVars[$param]['name'];
+		}
+		$this->prefName = $this->wikiConvert($this->prefName);
+
+		// set description
+		$this->prefDescription = $this->PrefVars[$param]['description'];
+		if ($this->PrefVars[$param]['hint'])
+			$this->prefDescription .= " ''Hint: ".$this->PrefVars[$param]['hint']."''";
+		foreach ($this->PrefVars[$param]['tags'] as $tag)
+			if ($tag === 'experimental')
+				$this->prefDescription .= ' (experimental)';
+		$this->prefDescription = $this->wikiConvert($this->prefDescription);
+	}
+
+
+	/**
+	 * Preps a string from tiki prefs for insertion into tiki-syntax land
+	 *
+	 * @param $string string to be parsed
+	 *
+	 * @param $escape bool if $string should be enclosed in tiki no-parse tags
+	 *
+	 *
+	 * @return string parsed string sutable for wiki insertion
+	 *
+	 */
+	private function wikiConvert($string, $escape = false){
+		$escapedString = '';
+		if ($string) {
+			if ($escape) {
+				$escapedString = ' ~np~';
+			}
+			$escapedString .= str_replace("\n", ' ', $string);
+			$escapedString = trim($escapedString);
+			if ($escape) {
+				$escapedString .= '~/np~';
+			}
+		}
+
+		return $escapedString;
+
+	}
+
+	/**
+	 *
+	 * Writes a generated pref wiki formatted file to disk
+	 *
+	 * @param string $fileName  The file name to write to disk.
+	 *
+	 */
+	public function writeFile($fileName){
+
+		$begining = '~hc~ -- Table generated with prefsdoc.php for tiki '.$this->getVersion()." -- ~/hc~\n";
+		$begining .= '{FANCYTABLE(head="Option | Description | Default" sortable="n")}'."\n";
+
+		$end = "{FANCYTABLE}\n/////\n";
+
+		$docName = substr(substr($fileName, 8), 0, -4) . ".txt";				// Name of file to be written
+
+		if (!is_dir($this->baseDir.'storage/prefsdoc')) {
+			if (!mkdir($this->baseDir . 'storage/prefsdoc')) ;
+			{            // create subdir for housing generated files, if it does not exist
+				die("Cant create storage/prefsdoc directory\n");
+			}
+		}if (is_file($this->baseDir . 'storage/prefsdoc/' . $docName)) {
+			if (!unlink($this->baseDir . 'storage/prefsdoc/' . $docName)){
+				die("Cant overwrite existing $docName\n");
+				}
+			}
+
+		if (!file_put_contents($this->baseDir . 'storage/prefsdoc/' . $docName, $begining.$this->docTable.$end))		// write one file for each pref page on control panel
+			die("Unable to write $docName\n");
 	}
 }
-$prefCount=0;
 
-$TWV = new TWVersion();
+$Doc = new PrefsDoc();
+$Doc->baseDir = $baseDir;
+$Doc->fileCount = 0;
+$Doc->prefCount = 0;
+$Doc->getPrefs();
 
 $docFiles = scandir($baseDir.'templates/admin'); // grab all the files that house prefs
 foreach ($docFiles as $fileName) {
-	if (substr($fileName, 0,8) === 'include_') {  // filter out any file thats not a pref file
-		$file = file_get_contents($baseDir.'templates/admin/' . $fileName);
-		$file = str_replace(array('\'', '"'), '', $file);
-		preg_match_all('{preference name=(\w*)(.*)}', $file, $out, PREG_PATTERN_ORDER);  // return array of every pref name in file
-		$writeFile = '~hc~ -- Table generated with prefsdoc.php for tiki '.$TWV->getVersion()." -- ~/hc~\n";
-		$writeFile .= '{FANCYTABLE(head="Option | Description | Default" sortable="n")}'."\n";
-		foreach ($out[1] as $param) {
-			if ($PrefVars[$param]['default'] == 'n') {
-				$PrefVars[$param]['default'] = 'Disabled';
-			} else if ($PrefVars[$param]['default'] == 'y') {
-				$PrefVars[$param]['default'] = 'Enabled';			// Change default codes to human readable format
-			} else if (is_array($PrefVars[$param]['default'])){
-				$PrefVars[$param]['default'] = implode(', ',$PrefVars[$param]['default']);
-			}
-			$writeFile .= $PrefVars[$param]['name'] . '~|~';
-			$writeFile .= $PrefVars[$param]['description'];
-			if ($PrefVars[$param]['hint'])
-				$writeFile .= " ''Hint: ".$PrefVars[$param]['hint']."''";
-			if ($PrefVars[$param]['help'])
-				$writeFile .= ' (('.$PrefVars[$param]['help'].'|Read More))';
-			foreach ($PrefVars[$param]['tags'] as $tag)
-				if ($tag === 'experimental')
-					$writeFile .= ' (experimental)';
-			$writeFile .= '~|~';
-			$writeFile .= (string)$PrefVars[$param]['default'] . "\n";
-			$prefCount++;
+	if (substr($fileName, 0, 8) === 'include_') {  // filter out any file thats not a pref file
+		$FilePrefs = $Doc->getAdminUIPrefs($fileName);
+		$Doc->fileCount ++;
+		$Doc->docTable = '';
+		foreach ($FilePrefs as $param) {
+			$Doc->setParams($param);
+			$Doc->prefCount++;
+			$Doc->docTable .= $Doc->prefName . '~|~' . $Doc->prefDescription . '~|~' . $Doc->prefDefault . "\n";
 		}
-		$writeFile .= "{FANCYTABLE}\n/////\n";
-		$fileCount++;
-		$docName = substr(substr($fileName,8),0,-4).".txt";				// Name of file to be written
-		@unlink($baseDir.'storage/prefsdoc/'.$docName);
-		file_put_contents($baseDir.'storage/prefsdoc/'.$docName, $writeFile);		// write one file for each pref page on control panel
+		$Doc->writeFile($fileName);
 	}
 }
 
-
-echo "\033[33m".$fileCount. ' pref files written to storage/prefsdoc/ with a total of '.$prefCount." prefs.\033[0m\n";
+echo "\033[33m".$Doc->fileCount. ' pref files written to storage/prefsdoc/ with a total of '.$Doc->prefCount." prefs.\033[0m\n";
