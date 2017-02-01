1f13b3f57e8bc58d6d2d1180dc5ba55dae51425b
chealer <chealer@users.sourceforge.net>
2017-02-01 01:11:17 +0000

    [REF] Reformat
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@61167 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/admin/adminlib.php
+++ lib/admin/adminlib.php
@@ -7,8 +7,8 @@
 
 //this script may only be included - so its better to die if called directly.
 if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
-  header("location: index.php");
-  exit;
+	header("location: index.php");
+	exit;
 }
 
 /**
@@ -17,27 +17,27 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 class AdminLib extends TikiLib
 {
 
-    /**
-     * @param $offset
-     * @param $maxRecords
-     * @param $sort_mode
-     * @param $find
-     * @return array
-     */
-    function list_dsn($offset, $maxRecords, $sort_mode, $find)
+	/**
+	 * @param $offset
+	 * @param $maxRecords
+	 * @param $sort_mode
+	 * @param $find
+	 * @return array
+	 */
+	function list_dsn($offset, $maxRecords, $sort_mode, $find)
 	{
 
-		$bindvars=array();
+		$bindvars = array();
 		if ($find) {
 			$findesc = '%' . $find . '%';
 
 			$mid = " where (`dsn` like ?)";
-			$bindvars[]=$findesc;
+			$bindvars[] = $findesc;
 		} else {
 			$mid = "";
 		}
 
-		$query = "select * from `tiki_dsn` $mid order by ".$this->convertSortMode($sort_mode);
+		$query = "select * from `tiki_dsn` $mid order by " . $this->convertSortMode($sort_mode);
 		$query_cant = "select count(*) from `tiki_dsn` $mid";
 		$result = $this->query($query, $bindvars, $maxRecords, $offset);
 		$cant = $this->getOne($query_cant, $bindvars);
@@ -53,22 +53,22 @@ class AdminLib extends TikiLib
 		return $retval;
 	}
 
-    /**
-     * @param $dsnId
-     * @param $dsn
-     * @param $name
-     * @return bool
-     */
-    function replace_dsn($dsnId, $dsn, $name)
+	/**
+	 * @param $dsnId
+	 * @param $dsn
+	 * @param $name
+	 * @return bool
+	 */
+	function replace_dsn($dsnId, $dsn, $name)
 	{
 		// Check the name
 		if ($dsnId) {
 			$query = "update `tiki_dsn` set `name`=?,`dsn`=? where `dsnId`=?";
-			$bindvars=array($name,$dsn, $dsnId);
+			$bindvars = array($name, $dsn, $dsnId);
 			$result = $this->query($query, $bindvars);
 		} else {
 			$query = "delete from `tiki_dsn`where `name`=? and `dsn`=?";
-			$bindvars=array($name,$dsn);
+			$bindvars = array($name, $dsn);
 			$result = $this->query($query, $bindvars);
 			$query = "insert into `tiki_dsn`(`name`,`dsn`)
                 		values(?,?)";
@@ -78,11 +78,11 @@ class AdminLib extends TikiLib
 		return true;
 	}
 
-    /**
-     * @param $dsnId
-     * @return bool
-     */
-    function remove_dsn($dsnId)
+	/**
+	 * @param $dsnId
+	 * @return bool
+	 */
+	function remove_dsn($dsnId)
 	{
 		$info = $this->get_dsn($dsnId);
 
@@ -91,11 +91,11 @@ class AdminLib extends TikiLib
 		return true;
 	}
 
-    /**
-     * @param $dsnId
-     * @return bool
-     */
-    function get_dsn($dsnId)
+	/**
+	 * @param $dsnId
+	 * @return bool
+	 */
+	function get_dsn($dsnId)
 	{
 		$query = "select * from `tiki_dsn` where `dsnId`=?";
 
@@ -108,11 +108,11 @@ class AdminLib extends TikiLib
 		return $res;
 	}
 
-    /**
-     * @param $dsnName
-     * @return bool
-     */
-    function get_dsn_from_name($dsnName)
+	/**
+	 * @param $dsnName
+	 * @return bool
+	 */
+	function get_dsn_from_name($dsnName)
 	{
 		$query = "select * from `tiki_dsn` where `name`=?";
 
@@ -125,26 +125,26 @@ class AdminLib extends TikiLib
 		return $res;
 	}
 
-    /**
-     * @param $offset
-     * @param $maxRecords
-     * @param $sort_mode
-     * @param $find
-     * @return array
-     */
-    function list_extwiki($offset, $maxRecords, $sort_mode, $find)
+	/**
+	 * @param $offset
+	 * @param $maxRecords
+	 * @param $sort_mode
+	 * @param $find
+	 * @return array
+	 */
+	function list_extwiki($offset, $maxRecords, $sort_mode, $find)
 	{
-		$bindvars=array();
+		$bindvars = array();
 		if ($find) {
 			$findesc = '%' . $find . '%';
 
 			$mid = " where (`extwiki` like ? )";
-			$bindvars[]=$findesc;
+			$bindvars[] = $findesc;
 		} else {
 			$mid = "";
 		}
 
-		$query = "select * from `tiki_extwiki` $mid order by ".$this->convertSortMode($sort_mode);
+		$query = "select * from `tiki_extwiki` $mid order by " . $this->convertSortMode($sort_mode);
 		$query_cant = "select count(*) from `tiki_extwiki` $mid";
 		$result = $this->fetchAll($query, $bindvars, $maxRecords, $offset);
 		$cant = $this->getOne($query_cant, $bindvars);
@@ -156,13 +156,13 @@ class AdminLib extends TikiLib
 		return $retval;
 	}
 
-    /**
-     * @param $extwikiId
-     * @param $extwiki
-     * @param $name
-     * @return bool
-     */
-    function replace_extwiki($extwikiId, $extwiki, $name, $indexName = '', $groups = [])
+	/**
+	 * @param $extwikiId
+	 * @param $extwiki
+	 * @param $name
+	 * @return bool
+	 */
+	function replace_extwiki($extwikiId, $extwiki, $name, $indexName = '', $groups = [])
 	{
 		$table = $this->table('tiki_extwiki');
 		$data = [
@@ -178,11 +178,11 @@ class AdminLib extends TikiLib
 		return true;
 	}
 
-    /**
-     * @param $extwikiId
-     * @return bool
-     */
-    function remove_extwiki($extwikiId)
+	/**
+	 * @param $extwikiId
+	 * @return bool
+	 */
+	function remove_extwiki($extwikiId)
 	{
 		$info = $this->get_extwiki($extwikiId);
 
@@ -191,16 +191,16 @@ class AdminLib extends TikiLib
 		return true;
 	}
 
-    /**
-     * @param $extwikiId
-     * @return bool
-     */
-    function get_extwiki($extwikiId)
+	/**
+	 * @param $extwikiId
+	 * @return bool
+	 */
+	function get_extwiki($extwikiId)
 	{
 		$table = $this->table('tiki_extwiki');
 		$row = $table->fetchFullRow(['extwikiId' => $extwikiId]);
-		
-		if (! empty($row['groups'])) {
+
+		if (!empty($row['groups'])) {
 			$row['groups'] = json_decode($row['groups']);
 		}
 		return $row;
@@ -217,7 +217,7 @@ class AdminLib extends TikiLib
 		while ($res = $result->fetchRow()) {
 			preg_match_all("/\{(picture |img )([^\}]+)\}/ixs", $res['data'], $pics); // to be fixed: pick also the picture into ~np~
 
-			foreach (array_unique($pics[2])as $pic) {
+			foreach (array_unique($pics[2]) as $pic) {
 				if (preg_match("/(src|file)=\"([^\"]+)\"/xis", $pic, $matches))
 					$pictures[] = $matches[2];
 				if (preg_match("/(src|file)=&quot;([^&]+)&quot;/xis", $pic, $matches))
@@ -231,7 +231,7 @@ class AdminLib extends TikiLib
 
 		$path = "img/wiki_up";
 		if ($tikidomain) {
-			$path.= "/$tikidomain";
+			$path .= "/$tikidomain";
 		}
 		$h = opendir($path);
 
@@ -326,11 +326,11 @@ class AdminLib extends TikiLib
 		}
 	}
 
-    /**
-     * @param $tag
-     * @return mixed
-     */
-    function tag_exists($tag)
+	/**
+	 * @param $tag
+	 * @return mixed
+	 */
+	function tag_exists($tag)
 	{
 		$query = "select distinct `tagName` from `tiki_tags` where `tagName` = ?";
 
@@ -338,11 +338,11 @@ class AdminLib extends TikiLib
 		return $result->numRows($result);
 	}
 
-    /**
-     * @param $tagname
-     * @return bool
-     */
-    function remove_tag($tagname)
+	/**
+	 * @param $tagname
+	 * @return bool
+	 */
+	function remove_tag($tagname)
 	{
 		$query = "delete from `tiki_tags` where `tagName`=?";
 		$this->query($query, array($tagname));
@@ -351,10 +351,10 @@ class AdminLib extends TikiLib
 		return true;
 	}
 
-    /**
-     * @return array
-     */
-    function get_tags()
+	/**
+	 * @return array
+	 */
+	function get_tags()
 	{
 		$query = "select distinct `tagName` from `tiki_tags`";
 
@@ -370,12 +370,12 @@ class AdminLib extends TikiLib
 
 	// This function can be used to store the set of actual pages in the "tags"
 	// table preserving the state of the wiki under a tag name.
-    /**
-     * @param $tagname
-     * @return bool
+	/**
+	 * @param $tagname
+	 * @return bool
 	 * @see dump()
-     */
-    function create_tag($tagname)
+	 */
+	function create_tag($tagname)
 	{
 		$query = "select * from `tiki_pages`";
 		$result = $this->query($query, array());
@@ -387,7 +387,7 @@ class AdminLib extends TikiLib
 			$query = "delete from `tiki_tags`where `tagName`=? and `pageName`=?";
 			$this->query($query, array($tagname, $pageName), -1, -1, false);
 			$query = "insert into `tiki_tags`(`tagName`,`pageName`,`hits`,`data`,`lastModif`,`comment`,`version`,`user`,`ip`,`flag`,`description`)" .
-               " values(?,?,?,?,?,?,?,?,?,?,?)";
+				" values(?,?,?,?,?,?,?,?,?,?,?)";
 			$this->query(
 				$query,
 				array(
@@ -413,11 +413,11 @@ class AdminLib extends TikiLib
 
 	// This funcion recovers the state of the wiki using a tagName from the
 	// tags table
-    /**
-     * @param $tagname
-     * @return bool
-     */
-    function restore_tag($tagname)
+	/**
+	 * @param $tagname
+	 * @return bool
+	 */
+	function restore_tag($tagname)
 	{
 
 		$query = "update `tiki_pages` set `cache_timestamp`=0";
@@ -428,8 +428,8 @@ class AdminLib extends TikiLib
 		while ($res = $result->fetchRow()) {
 
 			$query = "update `tiki_pages`" .
-								" set `hits`=?,`data`=?,`lastModif`=?,`comment`=?,`version`=`version`+1,`user`=?,`ip`=?,`flag`=?,`description`=?" .
-								"  where `pageName`=?";
+				" set `hits`=?,`data`=?,`lastModif`=?,`comment`=?,`version`=`version`+1,`user`=?,`ip`=?,`flag`=?,`description`=?" .
+				"  where `pageName`=?";
 
 			$this->query(
 				$query,
@@ -486,23 +486,23 @@ class AdminLib extends TikiLib
 			//preg_match_all("/tiki-index.php\?page=([^ ]+)/",$dat,$cosas);
 			//print_r($cosas);
 			$data = "<html><head><title>" .
-							$res["pageName"] .
-							"</title><link rel='StyleSheet' href='styles/" .
-							$prefs['style']  .
-							"' type='text/css'></head><body><a class='wiki' href='" .
-							$prefs['wikiHomePage'] .
-							".html'>home</a><br /><h1>" .
-							$res["pageName"] .
-							"</h1><div class='wikitext'>" .
-							$dat .
-							'</div></body></html>';
+				$res["pageName"] .
+				"</title><link rel='StyleSheet' href='styles/" .
+				$prefs['style'] .
+				"' type='text/css'></head><body><a class='wiki' href='" .
+				$prefs['wikiHomePage'] .
+				".html'>home</a><br /><h1>" .
+				$res["pageName"] .
+				"</h1><div class='wikitext'>" .
+				$dat .
+				'</div></body></html>';
 			$tar->addData($pageName, $data, $res["lastModif"]);
 		}
 
 		$tar->toTar("$dump_path/dump_wiki.tar", FALSE);
 		unset ($tar);
 		$logslib = TikiLib::lib('logs');
-		$logslib->add_log('dump', 'wiki file dump created: '.$filename);
+		$logslib->add_log('dump', 'wiki file dump created: ' . $filename);
 	}
 
 	public function getOpcodeCacheStatus()
@@ -518,9 +518,9 @@ class AdminLib extends TikiLib
 			'warning_xcache_blocked' => false,
 		);
 
-		if ( function_exists('apc_sma_info') && ini_get('apc.enabled') ) {
+		if (function_exists('apc_sma_info') && ini_get('apc.enabled')) {
 
-			if ( $_REQUEST['apc_clear']) {
+			if ($_REQUEST['apc_clear']) {
 				check_ticket('admin-inc-performance');
 				apc_clear_cache();
 				apc_clear_cache('user');
@@ -532,7 +532,7 @@ class AdminLib extends TikiLib
 
 			$cache = apc_cache_info(null, true);
 			$hit_total = $cache['num_hits'] + $cache['num_misses'];
-			if (!$hit_total) {	// cheat for chart after cache clear
+			if (!$hit_total) {    // cheat for chart after cache clear
 				$hit_total = 1;
 				$cache['num_misses'] = 1;
 			}
@@ -540,7 +540,7 @@ class AdminLib extends TikiLib
 			$opcode_stats = array(
 				'opcode_cache' => 'APC',
 				'stat_flag' => 'apc.stat',
-				'memory_used' => ( $mem_total - $sma['avail_mem'] ) / $mem_total,
+				'memory_used' => ($mem_total - $sma['avail_mem']) / $mem_total,
 				'memory_avail' => $sma['avail_mem'] / $mem_total,
 				'memory_total' => $mem_total,
 				'hit_hit' => $cache['num_hits'] / $hit_total,
@@ -548,8 +548,8 @@ class AdminLib extends TikiLib
 				'hit_total' => $hit_total,
 				'type' => 'apc',
 			);
-		} elseif ( function_exists('xcache_info') && ( ini_get('xcache.cacher') == '1' || ini_get('xcache.cacher') == 'On' ) ) {
-			if ( ini_get('xcache.admin.enable_auth') == '1' || ini_get('xcache.admin.enable_auth') == 'On' ) {
+		} elseif (function_exists('xcache_info') && (ini_get('xcache.cacher') == '1' || ini_get('xcache.cacher') == 'On')) {
+			if (ini_get('xcache.admin.enable_auth') == '1' || ini_get('xcache.admin.enable_auth') == 'On') {
 				$opcode_stats['warning_xcache_blocked'] = true;
 			} else {
 				$opcode_stats = array(
@@ -582,7 +582,7 @@ class AdminLib extends TikiLib
 			}
 			$opcode_stats['opcode_cache'] = 'XCache';
 
-		} elseif ( function_exists('wincache_ocache_fileinfo') && ( ini_get('wincache.ocenabled') == '1') ) {
+		} elseif (function_exists('wincache_ocache_fileinfo') && (ini_get('wincache.ocenabled') == '1')) {
 			$opcode_stats = array(
 				'opcode_cache' => 'WinCache',
 				'stat_flag' => 'wincache.ocenabled',
@@ -593,7 +593,7 @@ class AdminLib extends TikiLib
 				'hit_miss' => 0,
 				'hit_total' => 0,
 				'type' => 'wincache',
-				);
+			);
 
 			$info = wincache_ocache_fileinfo();
 			$opcode_stats['hit_hit'] = $info['total_hit_count'];
@@ -609,7 +609,7 @@ class AdminLib extends TikiLib
 			$opcode_stats['memory_avail'] /= $opcode_stats['memory_total'];
 			$opcode_stats['hit_hit'] /= $opcode_stats['hit_total'];
 			$opcode_stats['hit_miss'] /= $opcode_stats['hit_total'];
-		} elseif ( function_exists('opcache_get_status') && ini_get('opcache.enable') == '1' ) {
+		} elseif (function_exists('opcache_get_status') && ini_get('opcache.enable') == '1') {
 			$opcode_stats['opcode_cache'] = 'OpCache';
 			$status = opcache_get_status();
 
@@ -633,7 +633,7 @@ class AdminLib extends TikiLib
 		$opcode_stats['hit_hit'] = round($opcode_stats['hit_hit'], 2);
 		$opcode_stats['hit_miss'] = round($opcode_stats['hit_miss'], 2);
 
-		if ( isset($opcode_stats['hit_total']) ) {
+		if (isset($opcode_stats['hit_total'])) {
 			$opcode_stats = array_merge(
 				$opcode_stats,
 				array(
@@ -643,19 +643,19 @@ class AdminLib extends TikiLib
 			);
 		}
 
-		if ( isset($opcode_stats['memory_total']) ) {
+		if (isset($opcode_stats['memory_total'])) {
 			$opcode_stats = array_merge(
 				$opcode_stats,
 				array(
 					'warning_starve' => $opcode_stats['memory_avail'] < 0.2,
-					'warning_low' => $opcode_stats['memory_total'] < 60*1024*1024,
+					'warning_low' => $opcode_stats['memory_total'] < 60 * 1024 * 1024,
 				)
 			);
 		}
 
 		$stat_flag = $opcode_stats['stat_flag'];
-		if ( $stat_flag ) {
-			$opcode_stats['warning_check'] = (bool) ini_get($stat_flag);
+		if ($stat_flag) {
+			$opcode_stats['warning_check'] = (bool)ini_get($stat_flag);
 		}
 
 		return $opcode_stats;
