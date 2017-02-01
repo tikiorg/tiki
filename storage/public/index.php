d263b89eb19302f7859dc5e7128ac391a7051ac1
chealer <chealer@users.sourceforge.net>
2017-02-01 03:48:03 +0000

    [REF] Remove Id keyword line
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@61177 b456876b-0849-0410-b77d-98878d47e9d5

--- storage/public/index.php
+++ storage/public/index.php
@@ -3,7 +3,6 @@
 // 
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: index.php 57961 2016-03-17 20:01:56Z jonnybradley $
 
 // This redirects to the sites root to prevent directory browsing
 header("location: ../../tiki-index.php");
