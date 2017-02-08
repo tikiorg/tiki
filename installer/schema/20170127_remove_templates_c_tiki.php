ef06e2de3e0c00bdcfb75c4f949bd565fdb20e40
drsassafras <drsassafras@users.sourceforge.net>
2017-02-08 10:17:50 +0000

    [FIX] Remove old templates_c direcotry if it still exists
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@61247 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ installer/schema/20170127_remove_templates_c_tiki.php
@@ -0,0 +1,36 @@
+<?php
+// (c) Copyright 2002-2016 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: 20150508_perspectives_style_to_theme_tiki.php 57954 2016-03-17 19:34:29Z jyhem $
+
+if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
+	header("location: index.php");
+	exit;
+}
+
+/**
+ * @param $installer
+ */
+function upgrade_20170127_remove_templates_c_tiki($installer)
+{
+
+	$dirname = 'templates_c';
+	if (is_dir($dirname))
+		$dir_handle = opendir($dirname);
+	if (!$dir_handle)
+		return false;
+	while($file = readdir($dir_handle)) {
+		if ($file != "." && $file != "..") {
+			if (!is_dir($dirname."/".$file))
+				unlink($dirname."/".$file);
+			else
+				delete_directory($dirname.'/'.$file);
+		}
+	}
+	closedir($dir_handle);
+	rmdir($dirname);
+
+}
+
