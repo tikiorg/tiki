5fb6bc9761a7e3ce55af7d447f21dac77716b059
kroky6 <kroky6@users.sourceforge.net>
2017-02-14 13:05:31 +0000

    [MOD] remove 'no' from the default unified stop words list as it is causing common Checkbox/Radio button tracker fields with yes/no values to stop working with Elasticsearch index type
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@61295 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/prefs/unified.php
+++ lib/prefs/unified.php
@@ -271,7 +271,7 @@ function prefs_unified_list()
 			'name' => tr('Stop Word List'),
 			'description' => tr('Words excluded from the search index as they can be too frequent and cause noise.'),
 			'type' => 'text',
-			'default' => ["a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "no", "not", "of", "on", "or", "s", "such", "t", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with"],
+			'default' => ["a", "an", "and", "are", "as", "at", "be", "but", "by", "for", "if", "in", "into", "is", "it", "not", "of", "on", "or", "s", "such", "t", "that", "the", "their", "then", "there", "these", "they", "this", "to", "was", "will", "with"],
 			'separator' => ',',
 			'hint' => tr('MySQL full-text search has its own list of stop words configured in the server.'),
 		),
