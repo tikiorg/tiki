a9cbd68be6aa8cbdb6beba5998bb7283c60fbb6f
kroky6 <kroky6@users.sourceforge.net>
2017-02-27 22:18:01 +0000

    [bp/r61417] [ENH] plotly renderers highlighing user's values use additional text label on hover to help user distinguish tracker items
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/16.x@61418 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_pivottable.php
+++ lib/wiki-plugins/wikiplugin_pivottable.php
@@ -366,36 +366,6 @@ function wikiplugin_pivottable($data, $params)
 		}
 		$pivotData[] = $row;
 	}
-
-	$highlight = array();
-	if( !empty($params['highlightMine']) && $params['highlightMine'] === 'y' ) {
-		$ownerField = $definition->getField($definition->getUserField());
-		if( $ownerField ) {
-			foreach( $pivotData as $item ) {
-				$itemUsers = TikiLib::lib('trk')->parse_user_field(@$item[$ownerField['name']]);
-				if( in_array($user, $itemUsers) ) {
-					$highlight[] = $item;
-				}
-			}
-		}
-	}
-	if( !empty($params['highlightGroup']) && $params['highlightGroup'] === 'y' ) {
-		$groupField = null;
-		foreach( $fields as $field ) {
-			if( $field['type'] == 'g' ) {
-				$groupField = $field;
-				break;
-			}
-		}
-		if( $groupField ) {
-			$myGroups = TikiLib::lib('tiki')->get_user_groups($user);
-			foreach( $pivotData as $item ) {
-				if( in_array(@$item[$groupField['name']], $myGroups) ) {
-					$highlight[] = $item;
-				}
-			}
-		}
-	}
 	
 	//translating permName to field name for columns and rows
 	$cols = array();
@@ -485,6 +455,36 @@ function wikiplugin_pivottable($data, $params)
 		}
 	} else {
 		$params['aggregateDetails'] = array();
+	}
+
+	$highlight = array();
+	if( !empty($params['highlightMine']) && $params['highlightMine'] === 'y' ) {
+		$ownerField = $definition->getField($definition->getUserField());
+		if( $ownerField ) {
+			foreach( $pivotData as $item ) {
+				$itemUsers = TikiLib::lib('trk')->parse_user_field(@$item[$ownerField['name']]);
+				if( in_array($user, $itemUsers) ) {
+					$highlight[] = $item;
+				}
+			}
+		}
+	}
+	if( !empty($params['highlightGroup']) && $params['highlightGroup'] === 'y' ) {
+		$groupField = null;
+		foreach( $fields as $field ) {
+			if( $field['type'] == 'g' ) {
+				$groupField = $field;
+				break;
+			}
+		}
+		if( $groupField ) {
+			$myGroups = TikiLib::lib('tiki')->get_user_groups($user);
+			foreach( $pivotData as $item ) {
+				if( in_array(@$item[$groupField['name']], $myGroups) ) {
+					$highlight[] = $item;
+				}
+			}
+		}
 	}
 	
 	//checking if user can see edit button
