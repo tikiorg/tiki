a366f3d25028b2ced213b3a94847730a839785c0
jonnybradley <jonnybradley@users.sourceforge.net>
2017-03-02 10:39:33 +0000

    [rb/r61450] [rb/r61395,61393,61389] garbled formatting restored Seems that in r61450 other changes were unintentionally also rolled back, so i think all commits between approximately r61395 and r61450 need to be checked...
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@61454 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/ointegratelib.php
+++ lib/ointegratelib.php
@@ -82,6 +82,7 @@ class OIntegrate
 			}
 			break;
 		case 'index':
+		case 'mindex':
 			if ($to == 'index') {
 				return new OIntegrate_Converter_Indexer;
 			} elseif ( $to == 'html' ) {
@@ -92,21 +93,25 @@ class OIntegrate
 
 	} // }}}
 
-    /**
-     * @param $url
-     * @param null $postBody
-     * @return OIntegrate_Response
-     */
-    function performRequest( $url, $postBody = null ) // {{{
+	/**
+	 * @param string $url
+	 * @param string $postBody url or json encoded post parameters
+	 * @param bool $clearCache
+	 * @return OIntegrate_Response
+	 */
+    function performRequest( $url, $postBody = null, $clearCache = false ) // {{{
 	{
 		$cachelib = TikiLib::lib('cache');
 		$tikilib = TikiLib::lib('tiki');
 
-		if ( $cache = $cachelib->getSerialized($url.$postBody)) {
-			if ( time() < $cache['expires'] )
-				return $cache['data'];
+		$cacheKey = $url . $postBody;
 
-			$cachelib->invalidate($url.$postBody);
+		if ( $cache = $cachelib->getSerialized($cacheKey)) {
+			if (time() < $cache['expires'] && ! $clearCache) {
+				return $cache['data'];
+			}
+
+			$cachelib->invalidate($cacheKey);
 		}
 
 		$client = $tikilib->get_http_client($url);
@@ -181,14 +186,14 @@ class OIntegrate
 			$expiry = time() + $maxage;
 
 			$cachelib->cacheItem(
-				$url,
+				$cacheKey,
 				serialize(array('expires' => $expiry, 'data' => $response))
 			);
 		// Unless service specifies not to cache result, apply a default cache
-		} elseif ( $nocache !== null && $prefs['webservice_consume_defaultcache'] > 0 ) {
+		} elseif (empty($nocache) && $prefs['webservice_consume_defaultcache'] > 0 ) {
 			$expiry = time() + $prefs['webservice_consume_defaultcache'];
 
-			$cachelib->cacheItem($url, serialize(array('expires' => $expiry, 'data' => $response)));
+			$cachelib->cacheItem($cacheKey, serialize(array('expires' => $expiry, 'data' => $response)));
 		}
 
 		return $response;
@@ -552,7 +557,18 @@ class OIntegrate_Converter_Indexer implements OIntegrate_Converter
 
 				$source = new Search_ContentSource_WebserviceSource();
 				$factory = new Search_Type_Factory_Direct();
-				$data = $source->getDocument($_REQUEST['nt_name'], $factory);
+
+				if ($_REQUEST['nt_output'] === 'mindex') {
+					$documents = $source->getDocuments();
+					$data = [];
+					foreach ($documents as $document) {
+						if (strpos($document, $_REQUEST['nt_name']) === 0) {
+							$data[$document] = $source->getDocument($document, $factory);
+						}
+					}
+				} else {
+					$data = $source->getDocument($_REQUEST['nt_name'], $factory);
+				}
 
 				$output = '<h3>' . tr('Parsed Data') . '</h3>';
 				$output .= '<pre style="max-height: 40em; overflow: auto; white-space: pre-wrap">';
