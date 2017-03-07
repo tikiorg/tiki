fecc8f7169867961020fd09a4556c77ca2c11027
kroky6 <kroky6@users.sourceforge.net>
2017-03-07 13:29:55 +0000

    [FIX] Userslib->get_members to work for empty group filter too
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@61548 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/userslib.php
+++ lib/userslib.php
@@ -2141,6 +2141,8 @@ class UsersLib extends TikiLib
 	{
 		if( !is_array($group) ) {
 			$group = array($group);
+		} elseif( count($group) == 0 ) {
+			return array();
 		}
 		$users = $this->fetchAll('SELECT ug.groupName, u.login FROM `users_usergroups` ug INNER JOIN `users_users` u ON u.userId = ug.userId WHERE ug.groupName IN ('
 			.implode(',', array_fill(0, count($group), '?')).')', $group);
