1be20de13882fb0c93b537d6a33a7a9fad4bedb8
jonnybradley <jonnybradley@users.sourceforge.net>
2017-03-13 14:58:28 +0000

    [MOD] plugins: Move plugin edit form processing to a serverside AJAX service. TODO Switch over remaining usages of tiki-wikiplugin_edit.php to service plugin replace and remove that and tiki*js.php files Probably further tidying up and fixes to do too, please advice of any breakages (thanks marcalporte)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@61664 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Services/Edit/PluginController.php
+++ lib/core/Services/Edit/PluginController.php
@@ -17,7 +17,7 @@ class Services_Edit_PluginController
 
 	function __construct()
 	{
-		$this->parserlib = TikiLib::lib('parser');
+		$this->pluginList = [];
 	}
 
 	function setUp()
@@ -27,7 +27,12 @@ class Services_Edit_PluginController
 		$this->pluginList = TikiLib::lib('wiki')->list_plugins(true);
 	}
 
-
+	/**
+	 * List all or some of theplugins for the textarea control panel
+	 *
+	 * @param JitFilter $input
+	 * @return array
+	 */
 	function action_list($input)
 	{
 		$filter = $input->filter->text();
@@ -41,7 +46,7 @@ class Services_Edit_PluginController
 		}
 		$results = TikiLib::lib('prefs')->getMatchingPreferences($query);
 
-		foreach($results as $result) {
+		foreach ($results as $result) {
 			if (strpos($result, 'wikiplugin_') === 0) {
 				$key = strtoupper(substr($result, 11));
 				$arr = array_filter($this->pluginList, function ($plugin) use ($key) {
@@ -70,6 +75,214 @@ class Services_Edit_PluginController
 			'title' => $title,
 			'pref_filters' => TikiLib::lib('prefs')->getFilters(),
 		);
+	}
+
+	/**
+	 * Display plugin edit form or process saving changes
+	 *
+	 * @param JitFilter $input
+	 * @return array
+	 * @throws Services_Exception_BadRequest
+	 * @throws Services_Exception_Denied
+	 */
+	function action_edit($input)
+	{
+		global $prefs;
+
+		$parserlib = TikiLib::lib('parser');
+
+		$area_id = $input->area_id->alnumdash();
+		$type = $input->type->word();
+		$index = $input->index->int();
+		$page = $input->page->pagename();
+		$pluginArgs = $input->pluginArgs->array();
+		$bodyContent = $input->bodyContent->wikicontent();
+		$edit_icon = $input->edit_icon->text();
+		$selectedMod = $input->selectedMod->text();
+
+		$tikilib = TikiLib::lib('tiki');
+		$pageInfo = $tikilib->get_page_info($page);
+		if (! $pageInfo) {
+			// in edit mode
+		} else {
+			$perms = $tikilib->get_perm_object($page, 'wiki page', $pageInfo, false);
+			if ($perms['tiki_p_edit'] !== 'y') {
+				throw new Services_Exception_Denied(tr('You do not have permission to edit "%0"', $page));
+			}
+		}
+
+
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+			$this->action_replace($input);
+
+			return [
+				'redirect' => TikiLib::lib('wiki')->sefurl($page),
+			];
+
+		} else {        // render the form
+
+			$info = $parserlib->plugin_info($type);
+			$info['advancedParams'] = [];
+			$validationRules = [];
+
+			foreach ($info['params'] as $key => $param) {
+				if ($prefs['feature_jquery_validation'] === 'y') {
+					// $("#insertItemForm4").validate({rules: { ins_11: { required: true}, ins_13: { remote: { url: "validate-ajax.php", type: "post", data: { validator: "distinct", parameter: "trackerId=4&fieldId=13&itemId=0", message: "", input: function() { return $("#ins_13").val(); } } } }, ins_18: { required: true, remote: { url: "validate-ajax.php", type: "post", data: { validator: "distinct", parameter: "trackerId=4&fieldId=18&itemId=0", message: "this is not distinct!", input: function() { return $("#ins_18").val(); } } } }}, messages: { ins_11: { required: "This field is required" }, ins_18: { required: "this is not distinct!" }},
+					if ($param['required']) {
+						if (empty($param['parent'])) {
+							$validationRules["params[$key]"] = ['required' => true];
+						} else {
+							$validationRules["params[$key]"] = ['required_in_group' => [
+								1,
+								'.group-' . $param['parent']['name'],
+								'other',
+							]];
+						}
+					}
+				}
+				if (! empty($param['advanced']) && ! isset($pluginArgs[$key]) && empty($param['parent'])) {
+					$info['advancedParams'][$key] = $param;
+					unset($info['params'][$key]);
+				}
+			}
+
+			if ($validationRules) {
+				$rules = json_encode(['rules' => $validationRules]);
+				TikiLib::lib('header')->add_jq_onready('$("#plugin_params > form").validate(' . $rules . ');');
+			}
+
+			if ($type === 'module' && isset($pluginArgs['module'])) {
+				if ($selectedMod) {
+					$pluginArgs['module'] = $selectedMod;
+				}
+				$file = 'modules/mod-func-' . $pluginArgs['module'] . '.php';
+				if (file_exists($file)) {
+					include_once($file);
+					$info_func = "module_{$pluginArgs['module']}_info";
+					if (function_exists($info_func)) {
+						$moduleInfo = $info_func();
+						if (isset($info['params']['max'])) {
+							$max = $info['params']['max'];
+							unset($info['params']['max']);	// move "max" to last
+						}
+						foreach($moduleInfo['params'] as $key => $value) {
+							$info['params'][$key] = $value;
+						}
+						if (! empty($max)) {
+							$info['params']['max'] = $max;
+						}
+						// replace the module plugin description with the one from the select module
+						$info['params']['module']['description'] = $moduleInfo['description'];
+					}
+
+				}
+			}
+
+			return [
+				// pass back the input parameters
+				'area_id' => $area_id,
+				'type' => $type,
+				'index' => $index,
+				'pageName' => $page,
+				'pluginArgs' => $pluginArgs,
+				'pluginArgsJSON' => json_encode($pluginArgs),
+				'bodyContent' => $bodyContent,
+				'edit_icon' => $edit_icon,
+				'selectedMod' => $selectedMod,
+
+				'info' => $info,
+				'title' => $type,
+			];
+		}
+	}
+
+	/**
+	 * Replace plugin in wiki content
+	 * Migrated from tiki-wikiplugin_edit.php
+	 *
+	 * @param JitFilter $input
+	 * @return array
+	 * @throws Services_Exception
+	 * @throws Services_Exception_BadRequest
+	 * @throws Services_Exception_Denied
+	 */
+	function action_replace($input)
+	{
+		global $user;
+
+		$tikilib = TikiLib::lib('tiki');
+		$parserlib = TikiLib::lib('parser');
+
+		$page = $input->page->pagename();
+		$type = $input->type->word();
+		$message = $input->message->text();
+		$content = $input->content->wikicontent();
+		$index = $input->index->int();
+		$params = $input->params->array();
+
+		$referer = $_SERVER['HTTP_REFERER'];
+
+		if (! $page || ! $type || ! $referer || $_SERVER['REQUEST_METHOD'] !== 'POST') {
+			throw new Services_Exception(tr('Missing parameters'));
+		}
+
+		$plugin = strtolower($type);
+		$meta = $parserlib->plugin_info($plugin);
+
+		if (! $page || ! $type || ! $referer) {
+			throw new Services_Exception(tr('Plugin "%0" not found', $plugin));
+		}
+
+		if (! $message) {
+			$message = tr('%0 Plugin modified by editor.', $plugin);
+		}
+
+		$info = $tikilib->get_page_info($page);
+		if (! $info) {
+			throw new Services_Exception_BadRequest(tr('Page "%0" not found', $page));
+		}
+
+		$perms = $tikilib->get_perm_object($page, 'wiki page', $info, false);
+		if ($perms['tiki_p_edit'] !== 'y') {
+			throw new Services_Exception_Denied(tr('You do not have permission to edit "%0"', $page));
+		}
+
+		$current = $info['data'];
+
+		$matches = WikiParser_PluginMatcher::match($current);
+		$count = 0;
+		foreach ($matches as $match) {
+			if ($match->getName() !== $plugin) {
+				continue;
+			}
+
+			++$count;
+
+			if ($index === $count) {
+				// by using content of "~same~", it will not replace the body that is there
+				$content = ($content == "~same~" ? $match->getBody() : $content);
+
+				if (! $params) {
+					$params = $match->getArguments();
+				}
+
+				$match->replaceWithPlugin($plugin, $params, $content);
+
+				$tikilib->update_page(
+					$page,
+					$matches->getText(),
+					$message,
+					$user,
+					$tikilib->get_ip_address()
+				);
+
+				break;
+			}
+		}
+
+		return [];
+
 	}
 
 }
---
+++ templates/plugin/edit.tpl
@@ -0,0 +1,116 @@
+{extends 'layout_view.tpl'}
+
+{block name="title"}
+	<h3>{$title}</h3>
+{/block}
+
+{block name="content"}
+	{function plugin_edit_row}{* needs to be in the same block it seems? *}
+		{if $param.area}{$inputId=$param.area|escape}{else}{$inputId="param_{$name|escape}_input"}{/if}
+		<div  class="col-sm-3">
+			<label for="{$inputId}">{$param.name|escape}</label>
+			{if not empty($param.type)}
+				{$onclick = "openFgalsWindow('{$prefs.home_file_gallery|sefurl:'file gallery':true}filegals_manager={$param.area|escape}&id=1', true);return false;"}
+				{if $param.type eq 'image'}
+					<br>{icon name='image' title='{tr}Select image{/tr}' onclick=$onclick class='btn btn-xs btn-default'}
+				{elseif $param.type eq 'fileId'}
+					<br>{icon name='file' title='{tr}Pick a file{/tr}' onclick=$onclick class='btn btn-xs btn-default'}
+				{elseif $param.type eq 'kaltura'}
+					{jq}
+$("#picker_{{$name|escape}}").parent().click(function () {
+	$(this).serviceDialog({
+		title: tr("Upload or record media"),
+		width: 710,
+		height: 450,
+		hideButtons: true,
+		success: function (data) {
+			if (data.entries) {
+				input.value = data.entries[0];
+			}
+		}
+	});
+	return false;
+});
+					{/jq}
+					<br>{icon name='video' title='{tr}Upload or record media{/tr}' href={service controller='kaltura' action='upload'} id='picker_'|cat:$name|escape class='btn btn-xs btn-default'}
+				{/if}
+			{/if}
+		</div>
+		<div class="col-sm-9">
+			{if (not empty($param.parent.name))}
+				{$groupClass = " group-`$param.parent.name`"}
+			{else}
+				{$groupClass = ''}
+			{/if}
+			{if empty($param.options)}
+				<input value="{if isset($pluginArgs[$name])}{$pluginArgs[$name]}{/if}" class="form-control{$groupClass}" id="{$inputId}" type="text" name="params[{$name|escape}]">
+				{if not empty($param.filter)}
+					{if $param.filter eq "pagename"}
+						{jq}$({{$inputId}}).tiki("autocomplete", "pagename");{/jq}
+					{elseif $param.filter eq "groupname"}
+						{jq}$({{$inputId}}).tiki("autocomplete", "groupname", {multiple: true, multipleSeparator: "|"});{/jq}
+					{elseif $param.filter eq "username"}
+						{jq}$({{$inputId}}).tiki("autocomplete", "username", {multiple: true, multipleSeparator: "|"});{/jq}
+					{elseif $param.filter eq "date"}
+						{jq}$({{$inputId}}).tiki("datepicker");{/jq}
+					{/if}
+				{/if}
+
+			{else}
+				<select class="form-control{$groupClass}" type="text" name="params[{$name|escape}]" id="{$inputId}">
+					{foreach $param.options as $option}
+						<option value="{$option.value|escape}" {if isset($pluginArgs[$name]) and $pluginArgs[$name] eq $option.value} selected="selected"{/if}>
+							{$option.text|escape}
+						</option>
+					{/foreach}
+				</select>
+			{/if}
+			<div class="description">{$param.description}</div>
+		</div>
+	{/function}
+	<div class="form-horizontal" id="plugin_params">
+		<form action="{service controller='plugin' action='edit'}" method="post">
+			{foreach $info.params as $name => $param}
+				<div class="form-group{if $param.advanced} advanced{/if}" id="param_{$name|escape}">
+					{plugin_edit_row param=$param name=$name info=$info pluginArgs=$pluginArgs}
+				</div>
+			{/foreach}
+			{if not empty($info.advancedParams)}
+				{button _text='Advanced' _onclick="$('.form-group.advanced.default').toggle('fast'); return false;" _class='btn btn-xs margin-bottom-md'}
+				{foreach $info.advancedParams as $name => $param}
+					<div class="form-group advanced default" style="display: none;">
+						{plugin_edit_row param=$param name=$name info=$info pluginArgs=$pluginArgs}
+					</div>
+				{/foreach}
+			{/if}
+
+			<div class="form-group"{if empty($info.body)} style="display:none"{/if}>
+				<label for="content" class="col-sm-3">{tr}Body{/tr}</label>
+				<div class="col-sm-9">
+					<textarea name="content" id="content" class="form-control">{$bodyContent|escape}</textarea>
+					<div class="description">{$info.body}</div>
+				</div>
+			</div>
+
+			<div class="submit">
+				<input type="hidden" name="page" value="{$pageName}">
+				<input type="hidden" name="type" value="{$type}">
+				<input type="hidden" name="index" value="{$index}">
+				<input type="hidden" name="args" value="{$pluginArgsJSON}">
+				<input type="submit" class="btn btn-primary" value="{tr}Save{/tr}">
+			</div>
+
+			{if $type eq 'module'}
+				{jq}$("#param_module_input").change(function () {
+					var selectMod = $(this).val();
+					$.closeModal();
+					$(document).one("hidden.bs.modal", function () {
+						popupPluginForm("{{$area_id}}","{{$type}}", {{$index}}, "{{$pageName}}", {{$pluginArgsJSON}}, "{{$bodyContent}}", "{{$edit_icon}}", selectMod);
+					});
+				});{/jq}
+			{/if}
+
+		</form>
+	</div>
+{/block}
+
