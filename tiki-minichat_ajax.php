6aacee262473c5b94e4ce5ec53b16e78e6e409f2
luciash <luciash@users.sourceforge.net>
2017-03-14 09:59:40 +0000

    [bp/r61601][FIX] minichat: some Bootstrapification of the module, added spaces for better readability, fixed broken paths to smileys (thanks marclaporte) <- no broken path in branch 15.x actually -- luci
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@61678 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-minichat_ajax.php
+++ tiki-minichat_ajax.php
@@ -133,7 +133,7 @@ foreach ($chans as $chan) {
                     $t = date("H:i", $row['ts']);
                 }
             }
-            $msgtotal = "<span class='minichat_ts'>[$t]</span><span class='minichat_nick'>&lt;" . ($row['nick'] === null ? '' : $row['nick']) . "&gt;</span><span class='minichat_msg'>" . htmlentities($row['msg'], ENT_QUOTES, 'UTF-8') . "</span><br>" . $msgtotal;
+            $msgtotal = "<span class='minichat_ts'>[$t]</span>&nbsp;<span class='minichat_nick'>&lt;" . ($row['nick'] === null ? '' : $row['nick']) . "&gt;</span> <span class='minichat_msg'>" . htmlentities($row['msg'], ENT_QUOTES, 'UTF-8') . "</span><br>" . $msgtotal;
 		}
 		$msgtotal = str_replace(":-D", "<img border='0' src='img/smiles/icon_biggrin.gif' width='15' height='15'>", $msgtotal);
 		$msgtotal = str_replace(":D", "<img border='0' src='img/smiles/icon_biggrin.gif' width='15' height='15'>", $msgtotal);
