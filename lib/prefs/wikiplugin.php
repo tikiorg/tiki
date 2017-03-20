d4ea4e00896e2e4fc40850d82cc87c703489a841
pom2ter <pom2ter@users.sourceforge.net>
2017-03-20 23:50:47 +0000

    [FIX] fixed typo in the list plugin pref description
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@61792 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/prefs/wikiplugin.php
+++ lib/prefs/wikiplugin.php
@@ -156,7 +156,7 @@ function prefs_wikiplugin_list($partial = false)
 	// temporary pref for developpment of the list plugin GUI
 	$prefs['wikiplugin_list_gui'] = [
 		'name' => tr('GUI for the list plugin'),
-		'description' => tr('EExperimental GUI for the list plugin in popup plugin edit forms.'),
+		'description' => tr('Experimental GUI for the list plugin in popup plugin edit forms.'),
 		'tags' => ['experimental'],
 		'default' => 'n',
 		'dependencies' => ['wikiplugin_list'],
