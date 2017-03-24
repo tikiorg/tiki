fe7422e4fa15a111e91096037a86e6846643a24b
kroky6 <kroky6@users.sourceforge.net>
2017-03-24 13:59:27 +0000

    [bp/r61842] [bp/r61843] [ENH] use available pivottable locale based on tiki site language and add missing translate function to template strings [ENH] use smaller plotly dist package in pivottable plugin which has all the currently used plot types but avoid the overhead of pulling the full js file
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/16.x@61844 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_pivottable.php
+++ lib/wiki-plugins/wikiplugin_pivottable.php
@@ -188,8 +188,16 @@ function wikiplugin_pivottable($data, $params)
 	$headerlib = TikiLib::lib('header');
 	$headerlib->add_cssfile('vendor/nicolaskruchten/pivottable/dist/pivot.css');
 	$headerlib->add_jsfile('vendor/nicolaskruchten/pivottable/dist/pivot.js', true);
-	$headerlib->add_jsfile('vendor/plotly/plotly.js/dist/plotly.min.js', true);
+	$headerlib->add_jsfile('vendor/plotly/plotly.js/dist/plotly-cartesian.min.js', true);
 	$headerlib->add_jsfile('lib/jquery_tiki/wikiplugin-pivottable.js', true);
+
+	$lang = substr($prefs['site_language'], 0, 2);
+	if( file_exists('vendor/nicolaskruchten/pivottable/dist/pivot.'.$lang.'.js') ) {
+		$headerlib->add_jsfile('vendor/nicolaskruchten/pivottable/dist/pivot.'.$lang.'.js', true);
+	}
+
+	$smarty = TikiLib::lib('smarty');
+	$smarty->assign('lang', $lang);
 	
 	//checking data type
 	if( empty($params['data']) || !is_array($params['data']) ) {
@@ -420,7 +428,6 @@ function wikiplugin_pivottable($data, $params)
 		}
 	}
 
-	$smarty = TikiLib::lib('smarty');
 	$smarty->loadPlugin('smarty_function_object_link');
 
 	if (!isset($params['aggregateDetails'])) {
