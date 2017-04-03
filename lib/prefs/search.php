83f1dc39035a78dd49cd9303efe1c08069bd3acd
marclaporte <marclaporte@users.sourceforge.net>
2017-04-03 08:24:02 +0000

    fix a typo. Issue not present in 17.x so not a backport
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/15.x@62047 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/prefs/search.php
+++ lib/prefs/search.php
@@ -81,7 +81,7 @@ function prefs_search_list()
 		),
 		'search_facet_default_amount' => array(
 			'name' => tra('Facet result count'),
-			'description' => tra('DDefault number of facet results to obtain.'),
+			'description' => tra('Default number of facet results to obtain.'),
 			'type' => 'text',
 			'size' => 8,
 			'filter' => 'digits',
