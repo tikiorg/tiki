52bf7f5aae952dc5100d70f5b81fef49d64a6dee
jyhem <jyhem@users.sourceforge.net>
2017-04-08 16:09:25 +0000

    [NEW] Add option for actually keeping current user and group
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@62136 b456876b-0849-0410-b77d-98878d47e9d5

--- setup.sh
+++ setup.sh
@@ -162,6 +162,7 @@ usage: $0 [<switches>] ${POSSIBLE_COMMANDS}
 -v virtuals  list of virtuals (for multitiki, example: "www1 www2")
 -p php       alternate PHP command (default: php)
 -n           not prompt for user and group, assume current
+-k           don't guess user and group from context, keep same user and group as web root
 -d off|on    disable|enable debugging mode (override script default)
 -q           quiet (workaround to silence composer, e.g. in cron scripts)
 
@@ -188,14 +189,15 @@ OPT_PHPCLI=
 OPT_USE_CURRENT_USER_GROUP=
 OPT_QUIET=
 
-while getopts "hu:g:v:p:nd:q" OPTION; do
+while getopts "hu:g:v:nkd:q" OPTION; do
 	case $OPTION in
 		h) usage ; exit 0 ;;
 		u) OPT_AUSER=$OPTARG ;;
 		g) OPT_AGROUP=$OPTARG ;;
 		v) OPT_VIRTUALS=$OPTARG ;;
 		p) OPT_PHPCLI=$OPTARG ;;
-		n) OPT_USE_CURRENT_USER_GROUP=1 ;;
+		n) OPT_USE_CURRENT_USER_GROUP=1 ;; # Actually guess from context for historical reasons
+		k) OPT_GUESS_USER_GROUP_FROM_ROOT=1 ;; # Overrides -n user and group values
 		d) set_debug ;;
 		q) OPT_QUIET="-q" ;;
 		?) usage ; exit 1 ;;
@@ -278,7 +280,16 @@ else
 fi
 }
 
-check_distribution
+check_webroot() {
+	AUSER=`stat -c "%U" .`
+	AGROUP=`stat -c "%G" .`
+}
+
+if [ -z "${OPT_GUESS_USER_GROUP_FROM_ROOT}" ]; then
+	check_distribution
+else
+	check_webroot
+fi
 
 # part 3 - default and writable subdirs
 # -------------------------------------
