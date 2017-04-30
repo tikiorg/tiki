064c92f1bfef1fa6d23a489cbf9f8c8811067a42
lindonb <lindonb@users.sourceforge.net>
2017-04-30 15:39:31 +0000

    set svn keyword Id
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/17.x@62389 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/TikiFilter/Lang.php
+++ lib/core/TikiFilter/Lang.php
@@ -1,9 +1,9 @@
 <?php
-// (c) Copyright 2002-2016 by authors of the Tiki Wiki CMS Groupware Project
+// (c) Copyright 2002-2017 by authors of the Tiki Wiki CMS Groupware Project
 //
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: RawUnsafe.php 57968 2016-03-17 20:06:57Z jonnybradley $
+// $Id$
 
 /**
  * Class TikiFilter_Lang
