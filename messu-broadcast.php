7112ca3d3f78a42c53bb32e8b330aa548cc87a1c
lindonb <lindonb@users.sourceforge.net>
2017-05-08 01:54:17 +0000

    [REF] Arrange code more logically to better place feedback; use Feedback class for errors; sort users for feedback.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@62494 b456876b-0849-0410-b77d-98878d47e9d5

--- messu-broadcast.php
+++ messu-broadcast.php
@@ -58,80 +58,78 @@ if ((isset($_POST['send']) || isset($_POST['preview'])) && $access->ticketMatch(
 	// Validation:
 	// must have a subject or body non-empty (or both)
 	if (empty($_POST['subject']) && empty($_POST['body'])) {
-		$smarty->assign('message', tra('ERROR: Either the subject or body must be non-empty'));
-		$smarty->display("tiki.tpl");
-		die;
-	}
-	// Remove invalid users from the to, cc and bcc fields
-	if (isset($_POST['groupbr'])) {
-		if ($_POST['groupbr'] == 'all' && $tiki_p_broadcast_all == 'y') {
-			$a_all_users = $userlib->get_users(0, -1, 'login_desc', '');
-			$all_users = array();
-			foreach ($a_all_users['data'] as $a_user) {
-				$all_users[] = $a_user['user'];
+		Feedback::error(tra('The message must have either a subject or a body.'));
+	} else {
+		// Remove invalid users from the to, cc and bcc fields
+		if (isset($_POST['groupbr'])) {
+			if ($_POST['groupbr'] == 'all' && $tiki_p_broadcast_all == 'y') {
+				$a_all_users = $userlib->get_users(0, -1, 'login_desc', '');
+				$all_users = array();
+				foreach ($a_all_users['data'] as $a_user) {
+					$all_users[] = $a_user['user'];
+				}
+			} elseif (in_array($_POST['groupbr'], $groups)) {
+				$all_users = $userlib->get_group_users($_POST['groupbr']);
+			} else {
+				$access->display_error('', tra("You do not have permission to use this feature").": ". $permission, '403', false);
 			}
-		} elseif (in_array($_POST['groupbr'], $groups)) {
-			$all_users = $userlib->get_group_users($_POST['groupbr']);
-		} else {
-			$access->display_error('', tra("You do not have permission to use this feature").": ". $permission, '403', false);
+			$smarty->assign('groupbr', $_POST['groupbr']);
 		}
-		$smarty->assign('groupbr', $_POST['groupbr']);
-	}
 
-	$users = array();
-	foreach ($all_users as $a_user) {
-		if (!empty($a_user)) {
-			if ($userlib->user_exists($a_user)) {
-				if (!$userlib->user_has_permission($a_user, 'tiki_p_messages')) {
-					$message .= sprintf(tra('User %s does not have the permission'), htmlspecialchars($a_user)). "<br />" ;
-				} elseif ($tikilib->get_user_preference($a_user, 'allowMsgs', 'y') == 'y') {
-					$users[] = $a_user;
+		$users = array();
+		asort($all_users);
+		foreach ($all_users as $a_user) {
+			if (!empty($a_user)) {
+				if ($userlib->user_exists($a_user)) {
+					if (!$userlib->user_has_permission($a_user, 'tiki_p_messages')) {
+						$message .= sprintf(tra('User %s does not have the permission'), htmlspecialchars($a_user)). "<br />" ;
+					} elseif ($tikilib->get_user_preference($a_user, 'allowMsgs', 'y') == 'y') {
+						$users[] = $a_user;
+					} else {
+						$message.= sprintf(tra("User %s does not want to receive messages"), htmlspecialchars($a_user)). "<br />" ;
+					}
 				} else {
-					$message.= sprintf(tra("User %s does not want to receive messages"), htmlspecialchars($a_user)). "<br />" ;
+					$message.= tra("Invalid user") . "$a_user<br />";
+				}
+			}
+		}
+		$users = array_unique($users);
+		// Validation: either to, cc or bcc must have a valid user
+		if (count($users) > 0) {
+			$users_formatted = array();
+			foreach ($users as $rawuser) {
+				$users_formatted[] = htmlspecialchars($rawuser);
+			}
+			if (isset($_POST['send'])) {
+				$message .= tra('The message has been sent to:').' ';
+			} else {
+				$message .= tra('The message will be sent to:').' ';
+			}
+			$message .= implode(',', $users_formatted) . "<br />";
+			if (isset($_POST['send'])) {
+				$smarty->assign('sent', 1);
+				// Insert the message in the inboxes of each user
+				foreach ($users as $a_user) {
+					$messulib->post_message($a_user, $user, $a_user, '', $_POST['subject'], $_POST['body'], $_POST['priority']);
+					// if this is a reply flag the original messages replied to
+					if ($_POST['replyto_hash'] <> '') {
+						$messulib->mark_replied($a_user, $_POST['replyto_hash']);
+					}
+				}
+				// Insert a copy of the message in the sent box of the sender
+				$messulib->save_sent_message($user, $user, $_POST['groupbr'], null, $_POST['subject'], $_POST['body'], $_POST['priority'], $_POST['replyto_hash']);
+				$smarty->assign('message', $message);
+				if ($prefs['feature_actionlog'] == 'y') {
+					$logslib->add_action('Posted', '', 'message', 'add=' . strlen($_POST['body']));
 				}
 			} else {
-				$message.= tra("Invalid user") . "$a_user<br />";
+				$smarty->assign('preview', 1);
 			}
-		}
-	}
-	$users = array_unique($users);
-	// Validation: either to, cc or bcc must have a valid user
-	if (count($users) > 0) {
-		$users_formatted = array();
-		foreach ($users as $rawuser)
-			$users_formatted[] = htmlspecialchars($rawuser);
-		if (isset($_POST['send'])) {
-			$message .= tra('The message has been sent to:').' ';
 		} else {
-			$message .= tra('The message will be sent to:').' ';
+			Feedback::error(tra('No valid users to send the message to.'));
 		}
-		$message .= implode(',', $users_formatted) . "<br />";
-	} else {
-		$message .= tra('ERROR: No valid users to send the message');
-		$smarty->assign('message', $message);
-		$smarty->display("tiki.tpl");
-		die;
 	}
-	if (isset($_POST['send'])) {
-		$smarty->assign('sent', 1);
-		// Insert the message in the inboxes of each user
-		foreach ($users as $a_user) {
-			$messulib->post_message($a_user, $user, $a_user, '', $_POST['subject'], $_POST['body'], $_POST['priority']);
-			// if this is a reply flag the original messages replied to
-			if ($_POST['replyto_hash'] <> '') {
-				$messulib->mark_replied($a_user, $_POST['replyto_hash']);
-			}
-		}
-		// Insert a copy of the message in the sent box of the sender
-		$messulib->save_sent_message($user, $user, $_POST['groupbr'], null, $_POST['subject'], $_POST['body'], $_POST['priority'], $_POST['replyto_hash']);
-		$smarty->assign('message', $message);
-		if ($prefs['feature_actionlog'] == 'y') {
-			$logslib->add_action('Posted', '', 'message', 'add=' . strlen($_POST['body']));
-		}
-	} else {
-		$smarty->assign('message', $message);
-		$smarty->assign('preview', 1);
-	}
+	$smarty->assign('message', $message);
 }
 include_once ('tiki-section_options.php');
 include_once ('tiki-mytiki_shared.php');
