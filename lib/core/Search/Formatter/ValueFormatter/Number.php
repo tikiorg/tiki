a7b78c18899989cdff6f6f75c19b5603620a7859
kroky6 <kroky6@users.sourceforge.net>
2017-05-10 08:10:56 +0000

    [ENH] add number_format value formatter to be used in plugin list and related search results
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@62521 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/core/Search/Formatter/ValueFormatter/Number.php
@@ -0,0 +1,37 @@
+<?php
+// (c) Copyright 2002-2016 by authors of the Tiki Wiki CMS Groupware Project
+// 
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id$
+
+class Search_Formatter_ValueFormatter_Number extends Search_Formatter_ValueFormatter_Abstract
+{
+	private $decimals = null;
+	private $dec_point = null;
+	private $thousands_sep = null;
+
+	function __construct($arguments)
+	{
+		if (isset($arguments['decimals'])) {
+			$this->decimals = $arguments['decimals'];
+		}
+		if (isset($arguments['dec_point'])) {
+			$this->dec_point = $arguments['dec_point'];
+		}
+		if (isset($arguments['thousands_sep'])) {
+			$this->thousands_sep = $arguments['thousands_sep'];
+		}
+	}
+
+	function render($name, $value, array $entry)
+	{
+		if ($this->dec_point && $this->thousands_sep) {
+			return number_format($value, $this->decimals, $this->dec_point, $this->thousands_sep);
+		} elseif ($this->decimals) {
+			return number_format($value, $this->decimals);
+		} else {
+			return number_format($value);
+		}
+	}
+}
