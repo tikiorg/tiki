9a9b2437375404ba401bfdee8800c795201ddf62
jonnybradley <jonnybradley@users.sourceforge.net>
2017-05-10 11:10:58 +0000

    [FIX] trackers: Respect csv output on tracker dates
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/17.x@62522 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/Tracker/Field/DateTime.php
+++ lib/core/Tracker/Field/DateTime.php
@@ -112,6 +112,10 @@ class Tracker_Field_DateTime extends Tracker_Field_Abstract implements Tracker_F
 		$value = $this->getConfiguration('value');
 
 		if ($value) {
+			if ($context['list_mode'] == 'csv') {
+				return $tikilib->get_short_datetime($value);
+			}
+
 			if ($prefs['jquery_timeago'] === 'y' && $this->getOption('useTimeAgo')) {
 				TikiLib::lib('header')->add_jq_onready('$("time.timeago").timeago();');
 				return '<time class="timeago" datetime="' . TikiLib::date_format('c', $value, false, 5, false) .  '">' . $tikilib->get_short_datetime($value) . '</time>';
@@ -120,13 +124,9 @@ class Tracker_Field_DateTime extends Tracker_Field_Abstract implements Tracker_F
 			if ($this->getOption('datetime') == 'd') {
 				return $date;
 			}
-			
+
 			if ($this->getOption('datetime') == 't') {
 				return $tikilib->get_short_time($value);
-			}
-
-			if ($context['list_mode'] == 'csv') {
-				return $tikilib->get_short_datetime($value);
 			}
 
 			$current = $tikilib->get_short_date($tikilib->now);
