b85b5fc8f588acccca93c4b4226272409d84fa1b
luciash <luciash@users.sourceforge.net>
2017-05-12 15:05:22 +0000

    better to use str_replace here and keep the double quotes (in case they are used in user real name)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/17.x@62563 b456876b-0849-0410-b77d-98878d47e9d5

--- tiki-minichat_ajax.php
+++ tiki-minichat_ajax.php
@@ -136,7 +136,7 @@ foreach ($chans as $chan) {
                 }
             }
 	//TODO: improve matching and replace with better smileys + use global lib
-            $msgtotal = "<span class='minichat_ts'>[$t]</span>&nbsp;<span class='minichat_nick'>&lt;" . ($row['nick'] == '' ? "<em>" . tra('Anonymous') . "</em>" : strtr(smarty_modifier_userlink($row['user']), "\"", "'")) . "&gt;</span> <span class='minichat_msg'>" . htmlentities($row['msg'], ENT_QUOTES, 'UTF-8') . "</span><br>" . $msgtotal;
+            $msgtotal = "<span class='minichat_ts'>[$t]</span>&nbsp;<span class='minichat_nick'>&lt;" . ($row['nick'] == '' ? "<em>" . tra('Anonymous') . "</em>" : str_replace('"', '\"', smarty_modifier_userlink($row['user']))) . "&gt;</span> <span class='minichat_msg'>" . htmlentities($row['msg'], ENT_QUOTES, 'UTF-8') . "</span><br>" . $msgtotal;
 		}
 		$msgtotal = str_replace(":-D", "<img src='img/smiles/icon_biggrin.gif' width='15' height='15'>", $msgtotal);
 		$msgtotal = str_replace(":D", "<img src='img/smiles/icon_biggrin.gif' width='15' height='15'>", $msgtotal);
