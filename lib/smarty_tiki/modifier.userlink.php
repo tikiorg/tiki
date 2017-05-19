2f8b03690394385222986c7f52416329a6036b3a
jyhem <jyhem@users.sourceforge.net>
2017-05-19 17:33:09 +0000

    [FIX] Make it obvious which comments were entered by anonymous
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/17.x@62661 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/modifier.userlink.php
+++ lib/smarty_tiki/modifier.userlink.php
@@ -41,6 +41,7 @@ function smarty_modifier_userlink($other_user, $class='userlink', $idletime='not
 		if (count($other_user) > 1) {
 			$other_user = array_map(
 				function ($username) use ($class, $idletime, $popup) {
+					$username = TikiLib::lib('user')->distinguish_anonymous_users($username);
 					return smarty_modifier_userlink($username, $class, $idletime, '', 0, $popup);
 				},
 				$other_user
@@ -49,8 +50,10 @@ function smarty_modifier_userlink($other_user, $class='userlink', $idletime='not
 			$last = array_pop($other_user);
 			return tr('%0 and %1', implode(', ', $other_user), $last);
 		} else {
-			$other_user = reset($other_user);
+			$other_user = TikiLib::lib('user')->distinguish_anonymous_users(reset($other_user));
 		}
+	} else {
+		$other_user = TikiLib::lib('user')->distinguish_anonymous_users($other_user);
 	}
 	if (!$fullname) {
 		$fullname = TikiLib::lib('user')->clean_user($other_user);
@@ -68,3 +71,4 @@ function smarty_modifier_userlink($other_user, $class='userlink', $idletime='not
 
 	return TikiLib::lib('user')->build_userinfo_tag($other_user, $fullname, $class, $popup);
 }
+
