3b68f99b831cf128de7efd37e5d64753e872acd4
chealer <chealer@users.sourceforge.net>
2017-06-06 19:45:47 +0000

    [REF] Fix documentation [REF] Deprecate "feedback" identifier [REF] Clarify
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@62918 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/smarty_tiki/block.remarksbox.php
+++ lib/smarty_tiki/block.remarksbox.php
@@ -10,18 +10,24 @@
  * @package Smarty
  * @subpackage plugins
  *
- * \brief Smarty {remarksbox}{/remarksbox} block handler (tip (default), comment, note or warning)
+ * \brief Smarty message (tip/note (default), confirmation, warning or error) box
  *
- * To make a module it is enough to place smth like following
- * into corresponding mod-name.tpl file:
+ * To make a box, a template may contain something like this:
  * \code
- *  {remarksbox type="tip|comment|note|warning|errors" title="Remark title" highlight="y|n" icon="id"}
- *    <!-- module Smarty/HTML/Text here -->
+ *  {remarksbox title="Welcome to the control panels"}
+ *    This page is a kind of <a href="http://en.wikipedia.org/wiki/Map">map</a> to all control panels.
+ *    In each control panel, this large map will be replaced by a miniature version.
  *  {/remarksbox}
  * \endcode
  *
  * \params
- *  - type		    "tip|comment|note|warning|errors|feedback|confirm default=tip
+ *  - type
+ *  	String determining the appearance (style/icon) by categorizing as one of 4 types of messages.
+ *  	Each type has at least 1 identifier. By increasing importance:
+ * 			success|confirm|feedback: Confirmation message. The "feedback" identifier is deprecated (use "success" or "confirm").
+ *	 		tip|comment|note|info: Informative message, such as a tip (default)
+ * 			warning: Warning message
+ * 			errors|error|danger: Error message (or extremely important warning?)
  *  - title		    Text as a label. Leave out for no label (or icon)
  *  - highlight	    "y|n" default=n
  *  - icon		    Override default icons. See function.icon.php for more info
@@ -69,23 +75,26 @@ function smarty_block_remarksbox($params, $content, $smarty, &$repeat)
 	}
 
 	switch ($params['type']) {
-		case 'warning':
-			$class = 'alert-warning';
-			$icon = 'warning';
-			break;
 		case 'error':
 		case 'errors':
 		case 'danger':
 			$class = 'alert-danger';
 			$icon = 'error';
 			break;
+		case 'warning':
+			$class = 'alert-warning';
+			$icon = 'warning';
+			break;
 		case 'success':
 		case 'confirm':
-		case 'feedback':
+		case 'feedback': // Deprecated
 			$class = 'alert-success';
 			$icon = 'success';
 			break;
+		case 'comment':
 		case 'info':
+		case 'note':
+		case 'tip':
 		default:
 			$class = 'alert-info';
 			$icon = 'information';
