578438551c321fbe91cf5bfb1d1f4944b54d8adf
chibaguy <chibaguy@users.sourceforge.net>
2017-06-26 12:40:07 +0000

    [FIX] Removed extra double-quote mark in description.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/17.x@63081 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/prefs/activity.php
+++ lib/prefs/activity.php
@@ -57,7 +57,7 @@ function prefs_activity_list()
 		),
 		'activity_custom_events' => array(
 			'name' => tr('Custom activities'),
-			'description' => tr('"Allows the defining of custom behaviors in addition to internal events.'),
+			'description' => tr('Allows the defining of custom behaviors in addition to internal events.'),
 			'help' => 'Activity+Stream',
 			'type' => 'flag',
 			'default' => 'n',
