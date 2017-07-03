a4ffbde7b9896da3907864091e2f1eb7fcb42b97
chealer <chealer@users.sourceforge.net>
2017-07-03 18:28:03 +0000

    [REF] Optimize
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@63156 b456876b-0849-0410-b77d-98878d47e9d5

--- categorize.php
+++ categorize.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @package tikiwiki
+ * Update object categories
  */
 // (c) Copyright 2002-2016 by authors of the Tiki Wiki CMS Groupware Project
 // 
@@ -13,21 +14,15 @@ if (basename($_SERVER['SCRIPT_NAME']) === basename(__FILE__)) {
 }
 
 require_once('tiki-setup.php');
-$smarty = TikiLib::lib('smarty');
 
 global $prefs;
-
-$catobjperms = Perms::get(array( 'type' => $cat_type, 'object' => $cat_objid ));
-if ($prefs['feature_categories'] == 'y' && $catobjperms->modify_object_categories ) {
-	$categlib = TikiLib::lib('categ');
-
+if ($prefs['feature_categories'] == 'y' && Perms::get(array( 'type' => $cat_type, 'object' => $cat_objid ))->modify_object_categories ) {
 	if (isset($_REQUEST['import']) and isset($_REQUEST['categories'])) {
 		$_REQUEST["cat_categories"] = explode(',', $_REQUEST['categories']);
 		$_REQUEST["cat_categorize"] = 'on';
-	}
-
-	if ( !isset($_REQUEST["cat_categorize"]) || $_REQUEST["cat_categorize"] != 'on' ) {
+	} elseif ( !isset($_REQUEST["cat_categorize"]) || $_REQUEST["cat_categorize"] != 'on' ) {
 		$_REQUEST['cat_categories'] = NULL;
 	}
+	$categlib = TikiLib::lib('categ');
 	$categlib->update_object_categories(isset($_REQUEST['cat_categories'])?$_REQUEST['cat_categories']:'', $cat_objid, $cat_type, $cat_desc, $cat_name, $cat_href, $_REQUEST['cat_managed']);
 }
