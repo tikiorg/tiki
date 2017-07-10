1838cb0e972010dbe922eea338302eab518126e9
drsassafras <drsassafras@users.sourceforge.net>
2017-07-10 12:48:35 +0000

    [FIX] Fix for tiki-wizard_admin.php?url=index.php (2 of 2)
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/16.x@63224 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ lib/core/TikiFilter/RelativeURL.php
@@ -0,0 +1,42 @@
+<?php
+// (c) Copyright 2002-2017 by authors of the Tiki Wiki CMS Groupware Project
+//
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// $Id: RelativeUrl.php 63209 2017-07-09 04:51:59Z drsassafras $
+
+/**
+ * Class TikiFilter_RelativeURL
+ *
+ * Filters for valid relative URL's, and strips any tags.
+ */
+class TikiFilter_RelativeURL implements Zend\Filter\FilterInterface
+{
+	/**
+	 *
+	 * @param string $input		Absolute or relative URL.
+	 * @return string			Absolute URL components stripped out.
+	 */
+
+
+	function filter($input)
+	{
+
+		$filter = new Zend\Filter\StripTags();
+		$url =  $filter->filter($input);
+
+		$url = Zend\Uri\UriFactory::factory($url);
+
+		$query = $url->getQuery();
+		$fragment = $url->getFragment();
+		$url = $url->getPath();
+
+		if ($query)
+			$url .= '?'.$query;
+		if ($fragment)
+			$url .= '#'.$fragment;
+
+		return $url;
+
+	}
+}
