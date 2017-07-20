e192826d26e40ba7b1b3cb774472a3e98b33b56b
chealer <chealer@users.sourceforge.net>
2017-07-20 15:03:08 +0000

    [REF] get_extension(): Declare static [REF] handle_physical_exceptions(), from_file_extension(): Make static
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@63293 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/mime/mimelib.php
+++ lib/mime/mimelib.php
@@ -61,23 +61,23 @@ class MimeLib
      * @param $content
      * @return string
      */
-    private function handle_physical_exceptions($type, $filename, $content = '')
+    private static function handle_physical_exceptions($type, $filename, $content = '')
 	{
 		global $prefs;
 
 		if ($type === 'application/zip' || $type === 'application/octet-stream' || $type === 'application/vnd.ms-office') {
-			return $this->from_file_extension($filename);
-		} else if ($type === 'text/html' && $this->get_extension($filename) == 'svg') {
+			return self::from_file_extension($filename);
+		} else if ($type === 'text/html' && self::get_extension($filename) == 'svg') {
 			$type = 'image/svg+xml';
 		} else if ($type === 'text/html' && $prefs['vimeo_upload'] === 'y' && is_numeric($filename)) {
 			if (strpos($content, 'vimeo.com') !== false) {
 				$type = 'video/vimeo';
 			}
 		} else {
-			$extension = $this->get_extension($filename);
+			$extension = self::get_extension($filename);
 
 			if (in_array($extension, array("xlsx", "xltx", "potx", "ppsx", "pptx", "sldx", "docx", "dotx", "xlam", "xlsb"))) {
-				return $this->from_file_extension($filename);
+				return self::from_file_extension($filename);
 			}
 		}
 
@@ -88,7 +88,7 @@ class MimeLib
      * @param $filename
      * @return string
      */
-    private function get_extension($filename)
+    private static function get_extension($filename)
 	{
 		$ext = pathinfo($filename);
 		return isset($ext['extension']) ? $ext['extension'] : '';
@@ -98,12 +98,12 @@ class MimeLib
      * @param $filename
      * @return string
      */
-    private function from_file_extension($filename)
+    private static function from_file_extension($filename)
 	{
 		global $mimetypes; include_once('lib/mime/mimetypes.php');
 
 		if (isset($mimetypes)) {
-			$ext = $this->get_extension($filename);
+			$ext = self::get_extension($filename);
 			$mimetype = isset($mimetypes[$ext]) ? $mimetypes[$ext] : '';
 
 			if (!empty($mimetype)) {
