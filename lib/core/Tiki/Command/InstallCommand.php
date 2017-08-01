tikiorg/tiki

commit 2b074c64fb7f9c46d19bbc194c3e2042effb801c

[REF] Installer: Rename and group properties failures and success
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@63433 b456876b-0849-0410-b77d-98878d47e9d5

lib/core/Tiki/Command/InstallCommand.php
@@ -39,8 +39,8 @@ class InstallCommand extends Command
 			$output->writeln('Installation completed.');
 			$output->writeln('<info>Queries executed successfully: ' . count($installer->success) . '</info>');
 
-			if ( count($installer->failures) ) {
-				foreach ( $installer->failures as $key => $error ) {
+			if ( count($installer->queries['failed']) ) {
+				foreach ( $installer->queries['failed'] as $key => $error ) {
 					list( $query, $message, $patch ) = $error;
 
 					$output->writeln("<error>Error $key in $patch\n\t$query\n\t$message</error>");

lib/test/bootstrap.php
@@ -79,7 +79,7 @@ if (!$installer->tableExists('tiki_preferences')) {
 } else if ($installer->requiresUpdate()) {
 	echo "Updating Tiki database...\n";
 	$installer->update();
-	if ( count($installer->failures) ) {
+	if ( count($installer->queries['failed']) ) {
 		foreach ( $installer->failures as $key => $error ) {
 			list( $query, $message, $patch ) = $error;
 

installer/installlib.php
@@ -25,9 +25,8 @@ class Installer extends TikiDb_Bridge
 	public $installed = array();
 	public $executed = array();
 
-	public $success = array();
-	public $failures = array();
-	
+	public $queries = array('successful' => [], 'failed' => []);
+
 	public $useInnoDB = false;
 
     /**
@@ -299,10 +298,10 @@ class Installer extends TikiDb_Bridge
 		$result = $this->queryError($query, $error, $values);
 
 		if ( $result && empty($error) ) {
-			$this->success[] = $query;
+			$this->queries['successful'][] = $query;
 			return $result;
 		} else {
-			$this->failures[] = array($query, $error, substr(basename($patch), 0, -4));
+			$this->queries['failed'][] = array($query, $error, substr(basename($patch), 0, -4));
 			return false;
 		}
 	} // }}}
@@ -332,7 +331,7 @@ class Installer extends TikiDb_Bridge
 		
 		if ( empty($installed) ) {
 			// Erase initial error
-			$this->failures = array();
+			$this->queries['failed'] = array();
 		}
 
 		$this->patches = array_diff($this->patches, $installed);

lib/core/Tiki/Command/UpdateCommand.php
@@ -49,10 +49,10 @@ class UpdateCommand extends Command
 				}
 			}
 
-			$output->writeln('<info>Queries executed successfully: ' . count($installer->success) . '</info>');
+			$output->writeln('<info>Queries executed successfully: ' . count($installer->queries['successful']) . '</info>');
 
-			if ( count($installer->failures) ) {
-				foreach ( $installer->failures as $key => $error ) {
+			if ( count($installer->queries['failed']) ) {
+				foreach ( $installer->queries['failed'] as $key => $error ) {
 					list( $query, $message, $patch ) = $error;
 
 					$output->writeln("<error>Error $key in $patch\n\t$query\n\t$message</error>");

commit 0db7a4733e83eb0c59620c532bd3611d50bd142d

[FIX] Regressions from r63433 Oops
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@63434 b456876b-0849-0410-b77d-98878d47e9d5

lib/core/Tiki/Command/InstallCommand.php
@@ -37,7 +37,7 @@ class InstallCommand extends Command
 		if (! $installed || $force) {
 			$installer->cleanInstall();
 			$output->writeln('Installation completed.');
-			$output->writeln('<info>Queries executed successfully: ' . count($installer->success) . '</info>');
+			$output->writeln('<info>Queries executed successfully: ' . count($installer->queries['successful']) . '</info>');
 
 			if ( count($installer->queries['failed']) ) {
 				foreach ( $installer->queries['failed'] as $key => $error ) {

templates/tiki-install.tpl
@@ -512,7 +512,7 @@
 							{tr}You can now log in into Tiki as user <strong>admin</strong> and start configuring the application.{/tr}
 						</p>
 					{/remarksbox}
-					{if $installer->success|@count gt 0}
+					{if $installer->queries.successful|@count gt 0}
 						<p>{icon name="ok"}
 						<span style="font-weight:bold">
 						{if isset($smarty.post.update)}
@@ -521,12 +521,12 @@
 							{tr}Installation operations executed successfully:{/tr}
 						{/if}
 						</span>
-						{$installer->success|@count} {tr}SQL queries.{/tr}</p>
+						{$installer->queries.successful|@count} {tr}SQL queries.{/tr}</p>
 					{else}
 						<p>{icon name="ok"} <span style="font-weight: bold">{tr}Database was left unchanged.{/tr}</span></p>
 					{/if}
 					<form action="tiki-install.php" method="post">
-						{if $installer->failures|@count > 0}
+						{if $installer->queries.failed|@count > 0}
 							<script type='text/javascript'><!--//--><![CDATA[//><!--
 										{literal}
 										function sql_failed() {
@@ -535,12 +535,12 @@
 										{/literal}
 							//--><!]]></script>
 
-							<p>{icon name="error"} <strong>{tr}Operations failed:{/tr}</strong> {$installer->failures|@count} {tr}SQL queries.{/tr}
+							<p>{icon name="error"} <strong>{tr}Operations failed:{/tr}</strong> {$installer->queries.failed|@count} {tr}SQL queries.{/tr}
 							<a href="javascript:sql_failed()">{tr}Display details.{/tr}</a>
 							<div id="sql_failed_log" style="display:none">
 								<p>{tr}During an upgrade, it is normal to have SQL failures resulting with <strong>Table already exists</strong> messages.{/tr}</p>
 								{assign var='patch' value=''}
-								{foreach from=$installer->failures item=item}
+								{foreach from=$installer->queries.failed item=item}
 									{if $patch ne $item[2]}
 										{if $patch ne ''}
 											</textarea>

lib/test/bootstrap.php
@@ -80,7 +80,7 @@ if (!$installer->tableExists('tiki_preferences')) {
 	echo "Updating Tiki database...\n";
 	$installer->update();
 	if ( count($installer->queries['failed']) ) {
-		foreach ( $installer->failures as $key => $error ) {
+		foreach ( $installer->queries['failed'] as $key => $error ) {
 			list( $query, $message, $patch ) = $error;
 
 			echo "Error $key in $patch\n\t$query\n\t$message\n\n";