tikiorg/tiki

commit 9ec55133de86399068d56264dca7eb40b7702106

[ENH] Make sure the Named Object references are validated on export and when applying profiles
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@63400 b456876b-0849-0410-b77d-98878d47e9d5

lib/core/Tiki/Profile.php
@@ -277,6 +277,28 @@ class Tiki_Profile
 		return false;
 	} // }}}
 
+	/**
+	 * Validates if the value supplied can be considered a valid reference
+	 *
+	 * @param string $value The value to test
+	 * @param bool $simpleCheck if you want to check only the name of the reference without the rest of the reference
+	 *
+	 * @return bool
+	 */
+	public static function isValidReference($value, $simpleCheck = false)
+	{
+		if ($simpleCheck){
+			$value = '$simple_test:'.$value.'$';
+		}
+
+		if ( preg_match(self::SHORT_PATTERN, $value, $parts) ){
+			return true;
+		} elseif ( preg_match_all(self::LONG_PATTERN, $value, $parts, PREG_SET_ORDER) ) {
+			return true;
+		}
+		return false;
+	}
+
 	private function __construct() // {{{
 	{
 	} // }}}
@@ -904,5 +926,21 @@ class Tiki_Profile
 			return $domain;
 		}
 	} // }}}
+
+	/**
+	 * Validate that the values of the Named Objects that will be used as references are valid references
+	 *
+	 * @return bool
+	 */
+	public function validateNamedObjectsReferences()
+	{
+		$namedObjects = $this->getNamedObjects();
+		foreach ($namedObjects as $namedObject) {
+			if (!self::isValidReference($namedObject['object'], true)){
+				return false;
+			}
+		}
+		return true;
+	}
 }
 

lib/core/Tiki/Command/ProfileExport/Tracker.php
@@ -32,6 +32,12 @@ class Tracker extends ObjectWriter
 	{
 		$trackerId = $input->getArgument('tracker');
 
+		$ref = $input->getOption('reference');
+		if ($ref && !\Tiki_Profile::isValidReference($ref, true)) {
+			$output->writeln('<error>The value provided for the parameter reference do not have the right format: '.$ref.'</error>');
+			return;
+		}
+
 		$writer = $this->getProfileWriter($input);
 
 		$result = \Tiki_Profile_InstallHandler_Tracker::export($writer, $trackerId);

lib/core/Tiki/Command/ProfileInstallCommand.php
@@ -52,6 +52,11 @@ class ProfileInstallCommand extends Command
 			return;
 		}
 
+		if (!$profile->validateNamedObjectsReferences()){ // sanity check on the Named Objects references
+			$output->writeln('<error>Some of the named object references in the profile are invalid</error>');
+			return;
+		}
+
 		$tikilib = \TikiLib::lib('tiki');
 
 		$installer = new \Tiki_Profile_Installer;

commit 41834f3430382b052c7a93b13b29cbd304ad428a

[FIX] Fix wrong string concatenation when do simple reference check introduced in r63400
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@63471 b456876b-0849-0410-b77d-98878d47e9d5

lib/core/Tiki/Profile.php
@@ -288,7 +288,7 @@ class Tiki_Profile
 	public static function isValidReference($value, $simpleCheck = false)
 	{
 		if ($simpleCheck){
-			$value = '$simple_test:'.$value.'$';
+			$value = '$simple_test:'.$value;
 		}
 
 		if ( preg_match(self::SHORT_PATTERN, $value, $parts) ){