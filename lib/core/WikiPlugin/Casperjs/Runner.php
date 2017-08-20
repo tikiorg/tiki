665db6a22acaf7a7a62fbe62e41f16051d0a12ce
rjsmelo <rjsmelo@users.sourceforge.net>
2017-08-20 23:16:05 +0000

    [FIX] use $casperInstance instead of hardcode "casper" as instance name
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/17.x@63607 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/core/WikiPlugin/Casperjs/Runner.php
+++ lib/core/WikiPlugin/Casperjs/Runner.php
@@ -110,7 +110,7 @@ EOT;
 /* ********************* Tiki Bridge PostFix ********************* */
 {$casperInstance}.run(function () {
     tikiBridge.done({$casperInstance});
-    casper.exit();
+    {$casperInstance}.exit();
 });
 
 EOT;
