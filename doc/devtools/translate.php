d736b0480dc102c782a3099a96d8ef04d5fd6c17
drsassafras <drsassafras@users.sourceforge.net>
2017-08-21 13:02:53 +0000

    [FIX] - english update command - fixed an bug causing strings to be missed, better documentation
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/17.x@63610 b456876b-0849-0410-b77d-98878d47e9d5

--- doc/devtools/translate.php
+++ doc/devtools/translate.php
@@ -4,7 +4,7 @@
 //
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
-// $Id: svnup.php 62620 2017-05-16 18:05:56Z drsassafras $
+// $Id$
 
 namespace Tiki\Command;
 
@@ -24,13 +24,13 @@ require_once $tikiBase . '/vendor_bundled/vendor/autoload.php';
 
 
 /**
- * Add a singleton command "svnup" using the Symfony console component for this script
+ * Add a singleton command "englishupdate" using the Symfony console component for this script
  *
- * Class SvnUpCommand
+ * Class EnglishUpdateCommand
  * @package Tiki\Command
  */
 
-class SvnUpCommand extends Command
+class EnglishUpdateCommand extends Command
 {
 
 	protected function configure()
@@ -38,17 +38,17 @@ class SvnUpCommand extends Command
 		$this
 			->setName('englishupdate')
 			->setDescription("Update translation files with updates made to English strings. Will compare working copy by default.")
-			->addOption(
+	/*		->addOption(
 				'email',
 				'e',
 				InputOption::VALUE_REQUIRED,
-				'Email address to send a message if untranslated strings are found. Strings sill not be updated if this option is selected.'
-			)
+				'Email address to send a message if untranslated strings are found. Strings will not be updated if this option is selected.'
+			) */
 			->addOption(
 				'revision',
 				'r',
 				InputOption::VALUE_REQUIRED,
-				'Revision numbers my be selected eg. 63000:63010, or simply 63000 to update strings from 63000 onward.'
+				'Revision numbers may be selected eg. 63000:63010, or simply 63000 to update strings from 63000 onward.'
 			)
 			->addOption(
 				'lag',
@@ -63,7 +63,14 @@ class SvnUpCommand extends Command
 	{
 		$tikiBase = realpath(dirname(__FILE__) . '/../..');
 
-		echo $tikiBase;
+		$output->writeln('*******************************************************');
+		$output->writeln('*                     Limitations                     *');
+		$output->writeln('* Does not check for multiple uses of changed strings *');
+		$output->writeln('* so dont orphan reused strings.                      *');
+		$output->writeln('* Does not handle string forking so dont use when 2   *');
+		$output->writeln('* identical strings are changed non-identically.      *');
+		$output->writeln('*******************************************************');
+		$output->writeln('');
 
 		$rev = '';
 
@@ -102,7 +109,7 @@ class SvnUpCommand extends Command
 		$progress->setMessage('Getting Diffs');
 		$progress->start();
 
-		$raw = shell_exec("svn diff $rev | egrep  \"^([+-]\s|@)\" 2>&1");
+		$raw = shell_exec("svn diff $rev 2>&1");
 
 		$progress->setMessage('Finding Updated Strings');
 		$progress->advance();
@@ -110,31 +117,46 @@ class SvnUpCommand extends Command
 		// strip any empty translation strings now to avoid complexities later
 		$raw = preg_replace('/tra?\(["\'](\s*?)[\'"]\)/m','',$raw);
 
-		// add missing end-mark to positively match next regex
-		$raw .= "\n@@";
-
 		$output->writeln($raw, OutputInterface::VERBOSITY_DEBUG);
-
 
 		/**
 		 * @var $pairedMatches array any changes that took away and added lines.
 		 */
-		preg_match_all('/@@$([\w\W]*?)^@@/m',$raw,$diffs);
+		// place in an array changes that have multiple lines changes
+		preg_match_all('/(\n[-+]\s.*){2,}/m',$raw,$diffs);
 
 		unset ($raw);
 
+		$pairs = array();
+		foreach ($diffs[0] as $diff){
+
+			//now trim it down so its a - then + pair
+			if (preg_match('/^-[\s\S]*^\+.*/m',$diff,$pair)) {
+
+				// now extract a equally paired sets
+				$count = min(preg_match_all('/^-/m', $pair[0]), preg_match_all('/^\+/m', $pair[0]));
+				if ($count) {
+					preg_match('/(?>\n-.*){'.$count.'}(?>\n\+.*){'.$count.'}/',"\n".$pair[0],$equilPair);
+					$pairs[] = $equilPair[0];
+				}
+			}
+		}
+
+		unset ($diff);
 		$count = 0;
 		$pairedMatches = array();
-		foreach ($diffs[1] as $diff){
-			if (preg_match_all('/^-(.*)/m',$diff,$negativeMatch)) {
-				if (preg_match_all('/^\+(.*)/m',$diff,$positiveMatch)){
+
+		foreach ($pairs as $pair){
+			if (preg_match_all('/^-(.*)/m',$pair,$negativeMatch)) {
+				if (preg_match_all('/^\+(.*)/m',$pair,$positiveMatch)){
 					$pairedMatches[$count]['-']= implode('', $negativeMatch[1]);
 					$pairedMatches[$count]['+']= implode('', $positiveMatch[1]);
 					$count++;
 				}
 			}
 		}
-		unset ($diffs);
+
+		unset ($pairs);
 
 		/**
 		 * @var $pairedStrings array sets of translation strings found.
@@ -175,6 +197,11 @@ class SvnUpCommand extends Command
 		}
 		unset ($pairedStrings);
 
+		/*
+
+		Need to fix. it currently tells if a tr string as been updated, but it really needs to check if its been updated & not updated in the language.php files.
+
+
 		// send email if that option is selected, then die
 		if ($input->getOption('email')) {
 			mail($input->getOption('email'), $overCount . ' Undated Translation Strings Found', wordwrap(var_export($updateStrings,true), 70, "\r\n"));
@@ -183,6 +210,7 @@ class SvnUpCommand extends Command
 			echo "\n";
 			return false;
 		}
+*/
 
 		$progress->setMessage("Updating $overCount translation strings");
 		$progress->advance();
@@ -220,7 +248,7 @@ class SvnUpCommand extends Command
 
 // create the application and new console
 $console = new Application;
-$console->add(new SvnUpCommand);
+$console->add(new EnglishUpdateCommand);
 $console->setDefaultCommand('englishupdate');
 $console->run();
 
