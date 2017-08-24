92c65374a8f983f4efa64def5ffd9ac30d796885
drsassafras <drsassafras@users.sourceforge.net>
2017-08-24 03:10:30 +0000

    [ENH] Update English Strings - Now skips duplicate replacement and search strings, significantly better verbiage, implemented 'audit' mode to view out of sync strings, implemented 'email' mode for out of sync strings
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/branches/17.x@63637 b456876b-0849-0410-b77d-98878d47e9d5

--- doc/devtools/translate.php
+++ doc/devtools/translate.php
@@ -40,12 +40,6 @@ class EnglishUpdateCommand extends Command
 		$this
 			->setName('englishupdate')
 			->setDescription("Update translation files with updates made to English strings. Will compare working copy by default.")
-			/*		->addOption(
-						'email',
-						'e',
-						InputOption::VALUE_REQUIRED,
-						'Email address to send a message if untranslated strings are found. Strings will not be updated if this option is selected.'
-					) */
 			->addOption(
 				'revision',
 				'r',
@@ -57,6 +51,18 @@ class EnglishUpdateCommand extends Command
 				'l',
 				InputOption::VALUE_REQUIRED,
 				'Search through previous commits by X number of days, for updated translation strings. Working copy will be ignored.'
+			)
+			->addOption(
+				'audit',
+				'a',
+				InputOption::VALUE_NONE,
+				'Reports any translation strings that have been broken. Will not change repository. '
+			)
+			->addOption(
+				'email',
+				'e',
+				InputOption::VALUE_REQUIRED,
+				'Email address to send a message to if untranslated strings are found. Must be used in conjunction with "audit".'
 			);
 	}
 
@@ -67,6 +73,17 @@ class EnglishUpdateCommand extends Command
 	private $stringCount = 0;
 
 
+	/**
+	 * The number of identical original & changed pairs found.
+	 * @var int
+	 */
+	private $duplicates = 0;
+
+	/**
+	 * An array of all the language directories in Tiki
+	 * @var array
+	 */
+	private $languages;
 	/**
 	 *
 	 * Seperates svn diff output into changes made in PHP and TPL files
@@ -238,6 +255,29 @@ class EnglishUpdateCommand extends Command
 		return $updateStrings;
 	}
 
+	/**
+	 * Takes a paired list of original and replacement strings and checks if they are identical
+	 *
+	 * @param $content array paired string, that has previously been processed by filterStrings()
+	 *
+	 * @return array return an array of paired strings with duplicate entries omitted
+	 */
+
+	private function removeIdentical($content){
+
+		$filtered = array();
+		foreach ($content as $array) {
+			if(!in_array($array, $filtered)){
+				$filtered[] = $array;
+			}
+		}
+		$this->duplicates = $this->stringCount - count($filtered);
+		$this->stringCount -= $this->duplicates;
+
+		return $filtered;
+
+	}
+
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
@@ -253,7 +293,15 @@ class EnglishUpdateCommand extends Command
 		$output->writeln('');
 
 		$rev = '';
+		// check that email is being used in audit mode
+		if ($input->getOption('email') && !$input->getOption('audit')){
+			$help = new HelpCommand();
+			$help->setCommand($this);
+			$help->run($input, $output);
 
+			return $output->writeln(' --email, only available when running in --audit mode.');
+
+		}
 		// check that the --lag option is valid, and complain if its not.
 		if ($input->getOption('lag')) {
 			if ($input->getOption('lag') < 0 || !is_numeric($input->getOption('lag'))) {
@@ -262,7 +310,6 @@ class EnglishUpdateCommand extends Command
 				$help->run($input, $output);
 
 				return $output->writeln('Invalid option for --lag, must be a positive integer.');
-
 			}
 			// current time minus number of days specified through lag
 			$rev = date('{"Y-m-d H:i"}', time() - $input->getOption('lag') * 60 * 60 * 24);
@@ -271,22 +318,28 @@ class EnglishUpdateCommand extends Command
 			$rev = '-r' . $input->getOption('revision');
 		}
 
-		// die gracefully if shell_exec is not enabled;
-		if (!is_callable('shell_exec')) {
-			$output->writeln('<error>Translation string update Failed. Could not execute shell_exec()</error>');
+		$this->languages = glob($tikiBase . '/lang/*', GLOB_ONLYDIR);
 
-			return false;
-		}
-
-		$progress = new ProgressBar($output, 3);
+		$progress = new ProgressBar($output, count($this->languages) + 7);
 		if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE)
 			$progress->setOverwrite(false);
 		$progress->setFormatDefinition('custom', ' %current%/%max% [%bar%] -- %message%');
 		$progress->setFormat('custom');
 
+		$progress->setMessage('Checking System');
+		$progress->start();
+
+
+		// die gracefully if shell_exec is not enabled;
+		if (!is_callable('shell_exec')) {
+			$progress->setMessage('<error>Translation string update Failed. Could not execute shell_exec()</error>');
+			$progress->finish();
+
+			return false;
+		}
 
 		$progress->setMessage('Getting String Changes');
-		$progress->start();
+		$progress->advance();
 
 		$raw = shell_exec("svn diff $rev 2>&1");
 
@@ -300,85 +353,139 @@ class EnglishUpdateCommand extends Command
 
 		$diffs = $this->separatePhpTpl($raw);
 
-		$output->writeln(var_export($diffs,true),OutputInterface::VERBOSITY_DEBUG);
+		$output->writeln(var_export($diffs, true), OutputInterface::VERBOSITY_DEBUG);
 
 		$diffs['php'] = $this->pairMatches($diffs['php']);
 		$diffs['tpl'] = $this->pairMatches($diffs['tpl']);
 
-		$output->writeln(var_export($diffs,true),OutputInterface::VERBOSITY_DEBUG);
+		$progress->setMessage('Found ' . count($diffs['php']) . ' PHP and ' . count($diffs['tpl']) . ' TPL changes');
+		$progress->advance();
+
+		$output->writeln(var_export($diffs, true), OutputInterface::VERBOSITY_DEBUG);
 
 		$diffs['php'] = $this->pairStrings($diffs['php'], 'php');
 		$diffs['tpl'] = $this->pairStrings($diffs['tpl'], 'tpl');
 		$diffs = array_merge($diffs['php'], $diffs['tpl']);
 
-		$output->writeln(var_export($diffs,true),OutputInterface::VERBOSITY_DEBUG);
+		$progress->setMessage('Found ' . count($diffs) . ' String pairs');
+		$progress->advance();
+
+		$output->writeln(var_export($diffs, true), OutputInterface::VERBOSITY_DEBUG);
 
 		$diffs = $this->filterStrings($diffs);
-
-		if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERY_VERBOSE) {
-			$output->writeln("\n\n<info>Strings Being Updated</info>\n");
-			foreach ($diffs as $diff) {
-				$output->writeln($diff['-']);
-				$output->writeln($diff['+'] . "\n");
-			}
-		}
-
-
-		/*
-
-		Need to fix. it currently tells if a tr string as been changed, but it really needs to check if its been changed & not updated in the language.php files.
-
-
-		// send email if that option is selected, then die
-		if ($input->getOption('email')) {
-			mail($input->getOption('email'), $this->stringCount . ' Undated Translation Strings Found', wordwrap(var_export($updateStrings,true), 70, "\r\n"));
-			$progress->setMessage("$this->stringCount translation strings available for update. Email Sent. Strings not updated.");
-			$progress->finish();
-			echo "\n";
-			return false;
-		}
-*/
 
 		$progress->setMessage("Found $this->stringCount translation strings");
 		$progress->advance();
 
-		$directories = glob($tikiBase . '/lang/*', GLOB_ONLYDIR);
+		$diffs = $this->removeIdentical($diffs);
+
+		$progress->setMessage('Found ' . $this->duplicates . ' duplicate translation strings');
+		$progress->advance();
+
+		if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERY_VERBOSE) {
+			$output->writeln("\n\n<info>Strings Being Updated</info>\n");
+			foreach ($diffs as $diff) {
+				$output->writeln('* ' . $diff['-']);
+				$output->writeln('* ' . $diff['+'] . "\n");
+			}
+		}
+
+		/**
+		 * Tokens indicating that the replacement sting was found and replaced in the language file
+		 * @ver array
+		 */
+		$string = array();
+
+		/**
+		 * Tokens indicating that the replacement string was already present in the language file, so was skipped
+		 * @var array
+		 */
+		$skipped = array();
+
+		/**
+		 * Tokens indicating what language files have had changes made to them
+		 * @var array
+		 */
+		$lang = array();
 
 		// update the language files with the new strings
-		$string = array();
-		$lang = array();
-		if ($this->stringCount)
-			foreach ($directories as $directory) {
+
+		if ($this->stringCount) {
+			foreach ($this->languages as $directory) {
 				$langNow = substr($directory, strrpos($directory, "/") + 1);
 				if (is_writable($directory . '/language.php')) {
 					$file = file_get_contents($directory . '/language.php');
 					foreach ($diffs as $key => $entry) {
-						if (preg_match('/.*?"' . preg_quote($entry['-'], '/') . '[' . implode('', Language::punctuations) . ']?".*/',$file,$match)){
-							// if the string is in the language file, then replace it with an exact copy and a 'updated' copy on the next line
-							$replace = preg_replace('/"' . preg_quote($entry['-'], '/') . '[' . implode('', Language::punctuations) . ']?"/', '"' . $entry['+'] . '"', $match[0], 2);
-							$file = str_replace($match[0], $match[0] . "\n".$replace,$file);
+						// if the original string is in the language file
+						if (preg_match('/.*?"' . preg_quote($entry['-'], '/') . '[' . implode('', Language::punctuations) . ']?".*/', $file, $match)) {
+							// if the replacement string does not already exist
+							if (!strpos($file, '"' . $entry['+'] . '"')) {
+								// then replace the original string with an exact copy and a 'updated' copy on the next line
+								$replace = preg_replace('/"' . preg_quote($entry['-'], '/') . '[' . implode('', Language::punctuations) . ']?"/', '"' . $entry['+'] . '"', $match[0], 2);
+								$file = str_replace($match[0], $match[0] . "\n" . $replace, $file);
 
-							// keep track of overall numbers
+								// keep track of overall numbers
 								$string[$key] = true;
 								$lang[$langNow] = true;
+							} else {
+								$skipped[$key] = true;
 							}
-						file_put_contents($directory . '/language.php', $file);
+						}
 					}
+					if (isset($lang[$langNow])) {
+						$progress->setMessage($langNow . "\tStrings to update");
+						$progress->advance();
+						if (!$input->getOption('audit'))
+							file_put_contents($directory . '/language.php', $file);
+					} else {
+						$progress->setMessage($langNow . "\tNo changes to make");
+						$progress->advance();
+					}
+				} else {
+					$progress->setMessage($langNow . "\tSkipping <info>language.php not writable</info>");
+					$progress->advance();
 				}
 			}
-		$progress->setMessage('Updated ' . count($string) . ' of ' . $this->stringCount . ' strings in ' . count($lang) . ' of ' . count($directories) . ' language files.');
+		}
+		$skippedMessage = '';
+		if ($this->duplicates)
+			$skippedMessage = ' Skipped ' . $this->duplicates . ' duplicate strings.';
+
+		if ($input->getOption('audit')) {
+			$updateMessage = 'Out of Sync';
+		}else{
+			$updateMessage = 'Updated';
+		}
+		$progress->setMessage(count($string) . " of $this->stringCount strings $updateMessage in " . count($lang) . ' of ' . count($this->languages) . ' language files.' . $skippedMessage);
 		$progress->finish();
 
-		if (count($string) < $this->stringCount) {
-			$output-> writeln("\n\n<info>These strings were not updated</info>\n");
-			foreach ($diffs as $key => $entry) {
-				if (!isset($string[$key]))
-					$output->writeln('* '.$entry['-']);
+		if ($input->getOption('audit')) {
+			if (count($string)) {
+				$syncMessage= "\n";
+				$output->writeln("\n\n<info>Updated Strings not found in Language Files</info>");
+				foreach ($diffs as $key => $entry) {
+					if (isset($string[$key]))
+						$syncMessage.='* ' . $entry['-']."\n";
+				}
+				$output->writeln($syncMessage);
+				if ($input->getOption('email')) {
+					mail($input->getOption('email'), 'Updated Strings not found in Language Files', wordwrap($tikiBase."\n".$syncMessage, 70, "\r\n"));
+				}
+			} else {
+				$output->writeln("\n\n<info>English and Translations are in Sync</info>\n");
 			}
+			// if were not in audit mode
+		} else {
+			if (count($string) < $this->stringCount) {
+				$output->writeln("\n\n<info>Original Strings not Found in Language Files</info>");
+				foreach ($diffs as $key => $entry) {
+					if (!isset($string[$key]) && !isset($skipped[$key]))
+						$output->writeln('* ' . $entry['-']);
+				}
+			}
+			$output->writeln("\n\nOptionally run php get_strings.php to remove any unused translation strings.");
+			$output->writeln("Verify before committing.\n");
 		}
-		$output->writeln("\n\nOptionally run php get_strings.php to remove any unused translations.");
-		$output->writeln("Verify before committing.\n");
-
 		return true;
 	}
 }
@@ -388,5 +495,3 @@ $console = new Application;
 $console->add(new EnglishUpdateCommand);
 $console->setDefaultCommand('englishupdate');
 $console->run();
-
-
