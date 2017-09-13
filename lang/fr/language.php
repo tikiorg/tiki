9d7794fe7f8a1fa6549d40c1c67cc4f54f409c17
chealer <chealer@users.sourceforge.net>
2017-09-13 13:39:19 +0000

    [FIX] "Ces valeurs seront enregistrés" -> "Ces valeurs seront enregistrées"
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@63825 b456876b-0849-0410-b77d-98878d47e9d5

--- lang/fr/language.php
+++ lang/fr/language.php
@@ -11591,7 +11591,7 @@ $lang = array(
 // "Site identity" => "Site identity",
 // "Themes" => "Themes",
 // "Go to the <a href=\"tiki-admin.php?page=look\" class=\"alert-link\">Look & Feel</a> section for additional site customization preferences" => "Go to the <a href=\"tiki-admin.php?page=look\" class=\"alert-link\">Look & Feel</a> section for additional site customization preferences",
-"These values will be stored in plain text in the database" => "Ces valeurs seront enregistrés en texte clair dans la base de données",
+"These values will be stored in plain text in the database" => "Ces valeurs seront enregistrées en texte clair dans la base de données",
 "Email to send a test mail" => "Courriel pour l'envoi d'un test",
 // "Logging and reporting" => "Logging and reporting",
 // "Web Cron" => "Web Cron",
