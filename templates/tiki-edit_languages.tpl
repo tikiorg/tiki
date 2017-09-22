tikiorg/tiki

commit 46faf612689521f7956cb446ac80b27702a70c5f

[ENH] use find.tpl template for searches on tiki-edit_languages.php [ENH] use can set number of translations to show on tiki-edit_languages.php
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@30013 b456876b-0849-0410-b77d-98878d47e9d5

templates/tiki-edit_languages.tpl
@@ -11,6 +11,12 @@
 {tabset}
 	{tab name="{tr}Edit languages{/tr}"}
 		<form action="tiki-edit_languages.php" id="select_action" method="post">
+			{if isset($find)}
+				<input type="hidden" name="find" value="{$find}" />
+			{/if}
+			{if isset($maxRecords)}
+				<input type="hidden" name="maxRecords" value="{$maxRecords}" />
+			{/if}
 			<div class="adminoptionbox">
 				<label for="edit_language">{tr}Select the language to edit{/tr}:</label>
 				<select id="edit_language" class="translation_action"name="edit_language">
@@ -65,12 +71,9 @@
 					<h4>{if $action eq 'edit_tran_sw'}{tr}Edit translations:{/tr}{else}{tr}Translate recorded:{/tr}{/if}</h4>
 					<table class="formcolor" id="edit_translations">
 						<tr>
-							<td align="left" colspan=4>
-								<input name="search" value="{$search|escape}" size=10 />
-								<input type="submit" name="tran_search_sm" value="{tr}Search{/tr}" />
-								{if $action eq 'edit_rec_sw'}
-									<tr><td align="center"><input type="submit" name="tran_reset" value="{tr}Delete all{/tr}" /></td></tr>
-								{/if}
+							<td align="center" colspan=3>
+								{include file='find.tpl' find_show_num_rows='y'}
+								<hr />
 							</td>
 						</tr>
 						{foreach from=$translations name=translations key=source item=translation}
@@ -91,10 +94,14 @@
 							<tr>
 								<td colspan="3"><hr /></td>
 							</tr>
+						{foreachelse}
+							<tr><td align="center"><h4>{tr}No records found.{/tr}</h4></td></tr>
 						{/foreach}
 					</table>
 					<input type="hidden" name="offset" value="{$offset|escape}" />
-					<input type="submit" name="translate_all" value="{tr}Translate all{/tr}" />
+					{if !empty($translations)}
+						<input type="submit" name="translate_all" value="{tr}Translate all{/tr}" />
+					{/if}
 					{pagination_links cant=$total step=$maxRecords offset=$offset}{/pagination_links}
 				</div>
 			{/if}

tiki-edit_languages.php
@@ -22,10 +22,6 @@ if (!isset($_SESSION['interactive_translation_mode']))
 else
 	$smarty->assign('interactive_translation_mode',$_SESSION['interactive_translation_mode']);
 
-if (isset($_REQUEST["imp_language"])) {
-	$imp_language = preg_replace('/\.\./','',$_REQUEST['imp_language']);
-}
-
 // Get available languages
 $languages = $tikilib->list_languages();
 $smarty->assign_by_ref('languages', $languages);
@@ -81,8 +77,15 @@ if (isset($_REQUEST["action"])) {
 
 if ($action == "edit_rec_sw" || $action == "edit_tran_sw") {
 	check_ticket('edit-languages');
+	
+	$offset = isset($_REQUEST["offset"]) ? $_REQUEST['offset'] : 0;
+	$smarty->assign('offset', $offset);
+	
+	$maxRecords = (isset($_REQUEST['maxRecords']) && $_REQUEST['maxRecords'] > 0) ? $_REQUEST['maxRecords'] : $prefs['maxRecords'];
+	$smarty->assign('maxRecords', $maxRecords);
+	
 	//check if user has translated something
-	for ($i = 0; $i < $prefs['maxRecords']; $i++) {
+	for ($i = 0; $i < $maxRecords; $i++) {
 		// Handle edits in translate recorded
 		if (isset($_REQUEST["edit_tran_$i"]) || isset($_REQUEST['translate_all'])) {
 			// Handle edits in edit translations
@@ -109,16 +112,12 @@ if ($action == "edit_rec_sw" || $action == "edit_tran_sw") {
 		${"lang_$edit_language"}[ $row['source'] ] = $row['tran'];
 	}
 
-	$offset = isset($_REQUEST["offset"]) ? $_REQUEST['offset'] : 0;
-	$smarty->assign('offset', $offset);
-	$smarty->assign('maxRecords', $prefs['maxRecords']);
-
 	//Handle searches
-	$search = '';
+	$find = '';
 
-	if (isset($_REQUEST['search']) && strlen($_REQUEST['search']) > 0) {
-		$search = $_REQUEST['search'];
-		$smarty->assign('search', $search);
+	if (isset($_REQUEST['find']) && strlen($_REQUEST['find']) > 0) {
+		$find = $_REQUEST['find'];
+		$smarty->assign('find', $find);
 	}
 
 	$sort_mode = "source_asc";
@@ -126,14 +125,14 @@ if ($action == "edit_rec_sw" || $action == "edit_tran_sw") {
 	$data = array();
 
 	if ($action == "edit_rec_sw") {
-		$data = $language->getRecordedUntranslated($sort_mode, $maxRecords, $offset, $search);
+		$data = $language->getRecordedUntranslated($sort_mode, $maxRecords, $offset, $find);
 	} elseif ($action == "edit_tran_sw") {
 		if (isset($_REQUEST['only_db_translations'])) {
 			// display only database stored translations
-			$data = $language->getDbTranslations($sort_mode, $maxRecords, $offset, $search);
+			$data = $language->getDbTranslations($sort_mode, $maxRecords, $offset, $find);
 		} else {
 			// display all available translations (db + custom.php + language.php)
-			$data = $language->getAllTranslations($maxRecords, $offset, $search);
+			$data = $language->getAllTranslations($maxRecords, $offset, $find);
 		}
 	}
 	$smarty->assign_by_ref('translations', $data['translations']);

commit 3b49a76b3c684c2f45af2237e0e63f273dd2ac2e

[FIX] Broken HTML From r30013
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@63983 b456876b-0849-0410-b77d-98878d47e9d5

templates/tiki-edit_languages.tpl
@@ -118,14 +118,12 @@
 					{tr}Untranslated strings{/tr}
 				{/if}
 			</div>
+			{* Forms cannot embed forms. <div class="col-md-8">
+				{include file='find.tpl' find_show_num_rows='y'}
+			</div>*}
 			<div class="panel-body">
 				<table class="table table-condensed table-hover" id="edit_translations">
 					<thead>
-						<tr>
-							<div class="col-md-8">
-								{include file='find.tpl' find_show_num_rows='y'}
-							</div>
-						</tr>					
 						<tr>
 							<th>
 								{tr}Original string{/tr}
@@ -136,10 +134,10 @@
 							<th>
 								{tr}Translation{/tr}
 							</th>
-							<th></th>								
+							<th></th>
 						</tr>
 					</thead>
-					<tbody>					
+					<tbody>
 						{foreach from=$translations name=translations item=item}
 							<tr>
 								<td class="col-md-3">