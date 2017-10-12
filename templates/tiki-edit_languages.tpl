tikiorg/tiki

commit 16fc245d76c613dc96fd0550701d89085deb0699

Support distinguishing database translations which are specific to the Tiki instance from general translations which can be contributed to the Tiki community This removes the LanguageTranslations::updateTrans() bypass for unchanged strings, which causes tiki_language.changed to be set to 1, but there seems to be no way tiki_language.changed could take a value other than 1 anyway. [REF] Replace _getDbTranslationsEscaped() with PHPEscape(), to be used with _getDbTranslations()
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@64028 b456876b-0849-0410-b77d-98878d47e9d5

templates/tiki-edit_languages.tpl
@@ -152,14 +152,26 @@
 									<textarea id="tran_{$smarty.foreach.translations.index}" name="tran_{$smarty.foreach.translations.index}" class="form-control" rows="2">{$item.tran|escape}</textarea>
 								</td>
 								<td class="col-md-3 text-center">
-									<button type="submit" class="btn btn-primary btn-sm tips" name="edit_tran_{$smarty.foreach.translations.index}" title=":{tr}Save translation in the database{/tr}">
-										{tr}Translate{/tr}
-									</button>
-									{if $action eq 'edit_tran_sw' && isset($item.changed)}
-										<button type="submit" class="btn btn-warning btn-sm tips" name="del_tran_{$smarty.foreach.translations.index}" title=":{tr}Delete translation from the database{/tr}">
-											{tr}Delete{/tr}
-										</button>
+									{if $prefs.lang_control_contribution eq 'y' and isset($item.id)}{* Only translations in the database have an id. *}
+									<label for="scope_{$smarty.foreach.translations.index}">
+										Contribute :
+									</label>
+									<select name="scope_{$smarty.foreach.translations.index}" id="scope_{$smarty.foreach.translations.index}">
+										<option {if ! isset($item.general)}selected {/if}value="">Undecided</option>
+										<option {if $item.general === true}selected {/if}value="general">Yes</option>
+										<option {if $item.general === false}selected {/if}value="local">No</option>
+									</select>
 									{/if}
+									<div>
+										<button type="submit" class="btn btn-primary btn-sm tips" name="edit_tran_{$smarty.foreach.translations.index}" title=":{tr}Save translation in the database{/tr}">
+											{tr}Translate{/tr}
+										</button>
+										{if $action eq 'edit_tran_sw' && isset($item.changed)}
+											<button type="submit" class="btn btn-warning btn-sm tips" name="del_tran_{$smarty.foreach.translations.index}" title=":{tr}Delete translation from the database{/tr}">
+												{tr}Delete{/tr}
+											</button>
+										{/if}
+									</div>
 									{assign var=itemIndex value=$smarty.foreach.translations.index}
 									{if isset($item.originalTranslation)}
 										{button _flip_id="diff_$itemIndex" _flip_hide_text="n" _text="{tr}Compare{/tr}" _title=":{tr}Compare the original translation with the database translation{/tr}" _class="btn btn-default btn-sm tips"}
@@ -187,6 +199,11 @@
 						{/foreach}
 					</tbody>
 				</table>
+				{jq}
+					jQuery('select[name^="scope_"]').tooltip(
+						{title: tr("For translations specific to this Tiki instance, select No. If this translation can be contributed to the Tiki community, select Yes.")}
+						);
+				{/jq}
 				<div class="text-center">
 					{pagination_links cant=$total step=$maxRecords offset=$offset _ajax='n'}{strip}
 					tiki-edit_languages.php?edit_language={$edit_language}&action={$action}&maxRecords={$maxRecords}&only_db_translations={$only_db_translations}&only_db_untranslated={$only_db_untranslated}{if isset($find)}&find={$find}{/if}

db/tiki.sql
@@ -1244,6 +1244,7 @@ CREATE TABLE `tiki_language` (
   `lang` char(16) NOT NULL default '',
   `tran` text,
   `changed` bool,
+  `general` bool DEFAULT NULL COMMENT 'true if this translation is general and can be contributed to the Tiki community, false if it is specific to this instance',
   `userId` int(8),
   `lastModif` int(14) NULL,
   PRIMARY KEY (`id`)

installer/schema/20170925_add_general_to_language_tiki.sql
@@ -1,0 +1,1 @@
+ALTER TABLE `tiki_language` ADD `general` bool DEFAULT NULL COMMENT 'true if this translation is general and can be contributed to the Tiki community, false if it is specific to this instance' AFTER `changed`;

lib/core/Services/Language/Controller.php
@@ -108,7 +108,8 @@ class Services_Language_Controller
 			$export_language = new LanguageTranslations($language);
 			//write to language.php
 			try {
-				$stats = $export_language->writeLanguageFile();
+				global $prefs;
+				$stats = $export_language->writeLanguageFile($prefs['lang_control_contribution'] == 'y' && ! $input->offsetExists('all'));
 			} catch (Exception $e) { //TODO: this is messy
 				$smarty->assign('msg', $e->getMessage());
 				$smarty->display('error.tpl');

lib/language/LanguageTranslations.php
@@ -76,11 +76,29 @@ class LanguageTranslations extends TikiDb_Bridge
 	 *
 	 * @param string $originalStr the original string
 	 * @param string $translatedStr the translated string
+	 * @param array $optionalParameters Associative array of extra parameters:
+	 * 	bool|null 'general': true if the translation can be contributed to Tiki, false if it is specific to this instance
 	 * @return null
 	 */
-	public function updateTrans($originalStr, $translatedStr)
+	public function updateTrans($originalStr, $translatedStr, $optionalParameters = [])
 	{
 		global ${"lang_$this->lang"}, $user, $tikilib;
+		
+		foreach ($optionalParameters as $name => $value) {
+			switch ($name) {
+				case 'general':
+					if (! is_null($value) && ! is_bool($value)) {
+						throw new InvalidArgumentException('general should be a boolean.');
+					}
+					$general = $value;
+					$generalDefinedByCaller = true;
+					break;
+				default:
+					throw UnexpectedValueException;
+			}
+		}
+		/** @var boolean $generalDefinedByCaller true if the $general variable's value was defined by the caller, to distinguish the caller passing null from the caller not passing anything */
+		$generalDefinedByCaller = isset($generalDefinedByCaller);
 
 		// only the user name is globally available? not the user_id?
 		$userId = $tikilib->get_user_id($user);
@@ -95,24 +113,30 @@ class LanguageTranslations extends TikiDb_Bridge
 			return;
 		}
 
-		// don't change anything in the database if the translation hasn't been changed
-		if (isset(${"lang_$this->lang"}[$originalStr]) && ${"lang_$this->lang"}[$originalStr] == $translatedStr) {
-			return;
-		}
-
 		$query = 'select * from `tiki_language` where `lang`=? and binary `source` = ?';
 		$result = $this->query($query, array($this->lang, $originalStr));
 
 		if (!$result->numRows()) {
-			$query = 'insert into `tiki_language` (`source`, `lang`, `tran`, `changed`, `userId`, `lastModif`) values (?,?,?,?,?,?)';
-			$result = $this->query($query, array($originalStr, $this->lang, $translatedStr, 1, $userId, $tikilib->now));
+			$query = 'insert into `tiki_language` (`source`, `lang`, `tran`, `changed`, `general`, `userId`, `lastModif`) values (?, ?, ?, ?, ?, ?, ?)';
+			$result = $this->query($query, array($originalStr, $this->lang, $translatedStr, 1, $general, $userId, $tikilib->now));
 		} else {
 			if (strlen($translatedStr) == 0) {
 				$query = 'delete from `tiki_language` where binary `source`=? and `lang`=?';
 				$result = $this->query($query, array($originalStr, $this->lang));
 			} else {
-				$query = 'update `tiki_language` set `tran`=?, `changed`=?, `userId`=?, `lastModif`=? where binary `source`=? and `lang`=?';
-				$result = $this->query($query, array($translatedStr, 1, $userId, $tikilib->now, $originalStr, $this->lang));
+				$query = 'update `tiki_language` set `tran`=?, `changed`=?, `userId`=?, `lastModif`=?';
+				if ($generalDefinedByCaller) {
+					$query .= ', `general`=?';
+				}
+				$query .= ' where binary `source`=? and `lang`=?';
+				
+				$boundVariables = array($translatedStr, 1, $userId, $tikilib->now);
+				if ($generalDefinedByCaller) {
+					array_push($boundVariables, $general);
+				}
+				array_push($boundVariables, $originalStr, $this->lang);
+				
+				$result = $this->query($query, $boundVariables);
 			}
 		}
 
@@ -126,16 +150,23 @@ class LanguageTranslations extends TikiDb_Bridge
 	 * language.php file and remove the translations
 	 * from the database
 	 *
+	 * @param bool $generalOnly true if only strings to contribute upstream should be considered, false for all
 	 * @return array number of modified strings (key 'modif') and new
 	 * strings (key 'new') or null if not possible to write to file
 	 */
-	public function writeLanguageFile()
+	public function writeLanguageFile($generalOnly = false)
 	{
 		set_time_limit(0);
 
 		if (is_writable($this->filePath)) {
 			$langFile = file($this->filePath);
-			$dbTrans = $this->_getDbTranslationsEscaped();
+			if ($generalOnly) {
+				$translationsFilter = 'AND `general`=TRUE';
+			} else {
+				$translationsFilter = '';
+			}
+			$dbTrans = $this->_getDbTranslations('source_asc', -1, 0, false, $translationsFilter);
+			array_walk($dbTrans, array('LanguageTranslations', 'PHPEscape'));
 			$stats = array('modif' => 0, 'new' => 0);
 
 			// add new strings to the language.php
@@ -189,7 +220,7 @@ class LanguageTranslations extends TikiDb_Bridge
 			}
 
 			fclose($f);
-			$this->deleteTranslations();
+			$this->deleteTranslations($generalOnly);
 
 			return $stats;
 		} else {
@@ -207,11 +238,11 @@ class LanguageTranslations extends TikiDb_Bridge
 	 * @param int $maxRecords
 	 * @param int $offset
 	 * @param bool $originalTranslations if true include for each database translation the original translation from language.php
-	 * @param string $searchQuery if set limit the results to
+	 * @param string $searchQuery SQL query fragment to limit the results
 	 * @return array
 	 */
 	protected function _getDbTranslations($sort_mode = 'source_asc', $maxRecords = -1,
-					$offset = 0, $originalTranslations = false, $searchQuery = null)
+					$offset = 0, $originalTranslations = false, $searchQuery = '')
 	{
 		if ($originalTranslations) {
 			// load $lang with all translations excluding database translations to compare changes
@@ -241,7 +272,9 @@ class LanguageTranslations extends TikiDb_Bridge
 				$res['originalTranslation'] = $lang[$res['source']]['tran'];
 				$res['diff'] = $this->_diff($res['originalTranslation'], $res['tran']);
 			}
-
+			if (isset($res['general'])) {
+				$res['general'] = (bool) $res['general'];
+			}
 			$translations[$res['source']] = $res;
 		}
 
@@ -261,31 +294,28 @@ class LanguageTranslations extends TikiDb_Bridge
 	}
 
 	/**
-	 * Return all the custom translations in the database with
-	 * special characters escaped
-	 *
-	 * @return array
+	 * Escapes strings for usage in a double-quoted PHP string
+	 * @param string $value Initial value is an array defining a translation, set to escaped target string
+	 * @param string $key Initial value ignored, set to escaped original string  
+	 * @return void
 	 */
-	protected function _getDbTranslationsEscaped()
+	protected static function PHPEscape(&$value, &$key)
 	{
-		require_once('lib/language/Language.php');
-
-		$trans = $this->_getDbTranslations();
-		$escapedTrans = array();
-
-		foreach ($trans as $item) {
-			$escapedTrans[Language::addPhpSlashes($item['source'])] = Language::addPhpSlashes($item['tran']);
-		}
-
-		return $escapedTrans;
+		$key = Language::addPhpSlashes($value['source']);
+		$value = Language::addPhpSlashes($value['tran']);
 	}
 
 	/**
-	 * Delete all the translations from the current language
+	 * Delete translations from the current language
+	* @param bool $generalOnly true if only translations to contribute upstream should be deleted, false for all
 	 */
-	public function deleteTranslations()
+	public function deleteTranslations($generalOnly = false)
 	{
-		$this->query('DELETE FROM `tiki_language` WHERE `lang`=?', array($this->lang));
+		$query = 'DELETE FROM `tiki_language` WHERE `lang`=?';
+		if ($generalOnly) {
+			$query .= ' AND `general` = 1';
+		}
+		$this->query($query, array($this->lang));
 	}
 
 	/**
@@ -308,8 +338,8 @@ class LanguageTranslations extends TikiDb_Bridge
 	 */
 	public function createCustomFile()
 	{
-		$strings = $this->_getDbTranslationsEscaped();
-
+		$strings = $this->_getDbTranslations();
+		array_walk($strings, 'self::PHPEscape');
 		$data = "<?php\n\$lang=";
 		$data .= $this->_removeSpaces(var_export($strings, true));
 		$data .= ";\n?>\n";

lib/prefs/lang.php
@@ -16,6 +16,13 @@ function prefs_lang_list()
 			'default' => 'n',
 			'hint' => tr('Edit, export and import languages'),
 		),
+		'lang_control_contribution' => array(
+			'name' => tra('Manage contribution of translated strings'),
+			'description' => tra('Offers to set whether each translated string should be contributed to the Tiki community or kept locally'),
+			'type' => 'flag',
+			'default' => 'n',
+			'tags' => ['advanced'],
+		),
 		'lang_machine_translate_implementation' => array(
 			'name' => tr('Machine translation implementation'),
 			'description' => tr('Select between alternate impementations for machine translation. Depending on the implementation, different API keys may be required.'),

templates/admin/include_i18n.tpl
@@ -69,6 +69,9 @@
 			{preference name=wikiplugin_translated}
 		</div>
 		{preference name=lang_use_db}
+		<div class="adminoptionboxchild" id="lang_use_db_childcontainer">
+			{preference name=lang_control_contribution}
+		</div>
 		{preference name=record_untranslated}
 		{preference name=feature_machine_translation}
 		<div class="adminoptionboxchild" id="feature_machine_translation_childcontainer">

templates/language/write_to_language_php.tpl
@@ -20,6 +20,11 @@
 					<label>
 						{tr}Translations in the database{/tr}: <span class="badge">{$db_translation_count}</span>
 					</label>
+					{if $prefs.lang_control_contribution eq 'y'}
+						<label>
+							<input type="checkbox" name="all"> {tr}Merge all translations, whether or not they are marked as general (for contribution){/tr}
+						</label>
+					{/if}
 				</div>
 				<div class="form-group">
 					<label>

commit 05ac7b96943cefd94582ea7d2d01864d00230ed2

[FIX] Allow setting scope of new database translations Fixes uncaught exception introduced in r64028. Oops, thanks Ricardo Melo
git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@64243 b456876b-0849-0410-b77d-98878d47e9d5

templates/tiki-edit_languages.tpl
@@ -152,15 +152,17 @@
 									<textarea id="tran_{$smarty.foreach.translations.index}" name="tran_{$smarty.foreach.translations.index}" class="form-control" rows="2">{$item.tran|escape}</textarea>
 								</td>
 								<td class="col-md-3 text-center">
-									{if $prefs.lang_control_contribution eq 'y' and isset($item.id)}{* Only translations in the database have an id. *}
-									<label for="scope_{$smarty.foreach.translations.index}">
-										{tr}Contribute:{/tr}
-									</label>
-									<select name="scope_{$smarty.foreach.translations.index}" id="scope_{$smarty.foreach.translations.index}">
-										<option {if ! isset($item.general)}selected {/if}value="">{tr}Undecided{/tr}</option>
-										<option {if $item.general === true}selected {/if}value="general">{tr}Yes{/tr}</option>
-										<option {if $item.general === false}selected {/if}value="local">{tr}No{/tr}</option>
-									</select>
+									{if $prefs.lang_control_contribution eq 'y'}
+										<label for="scope_{$smarty.foreach.translations.index}"
+											{if ! isset($item.id)}style="display: none"{/if}{* Only translations in the database have an id. *}
+											>
+											{tr}Contribute:{/tr}
+											<select name="scope_{$smarty.foreach.translations.index}" id="scope_{$smarty.foreach.translations.index}">
+												<option {if ! isset($item.general)}selected {/if}value="">{tr}Undecided{/tr}</option>
+												<option {if $item.general === true}selected {/if}value="general">{tr}Yes{/tr}</option>
+												<option {if $item.general === false}selected {/if}value="local">{tr}No{/tr}</option>
+											</select>
+										</label>
 									{/if}
 									<div>
 										<button type="submit" class="btn btn-primary btn-sm tips" name="edit_tran_{$smarty.foreach.translations.index}" title=":{tr}Save translation in the database{/tr}">
@@ -203,6 +205,11 @@
 					jQuery('select[name^="scope_"]').tooltip(
 						{title: "{tr}For translations specific to this Tiki instance, select No. If this translation can be contributed to the Tiki community, select Yes.{/tr}"}
 						);
+					
+					// Allow setting scope of database translations
+					jQuery('textarea[name^="tran_"]').change(function() {
+							jQuery(this).closest('tr').find("label[for^='scope_']").show();
+						});
 				{/jq}
 				<div class="text-center">
 					{pagination_links cant=$total step=$maxRecords offset=$offset _ajax='n'}{strip}