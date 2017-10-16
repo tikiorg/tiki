7e4b29571becb26316509e6676634a92192deb8f
chealer <chealer@users.sourceforge.net>
2017-10-16 17:52:17 +0000

    [rb/r62691][FIX] sefurl(): URL special characters escaping This actually re-breaks some links to pages from tiki-listpages.php, but that's because the filters break the page parameter's processing. This restores the proper encoding (which works with tiki-index_raw).
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@64289 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki/wikilib.php
+++ lib/wiki/wikilib.php
@@ -1282,7 +1282,7 @@ class WikiLib extends TikiLib
 
 		$pages = TikiDb::get()->table('tiki_pages');
 		$page = $pages->fetchOne('pageSlug', ['pageName' => $page]) ?: $page;
-		$href = "$script_name?page=" . urlencode($page);
+		$href = "$script_name?page=" . $page;
 
 		if (isset($prefs['feature_wiki_use_date_links']) && $prefs['feature_wiki_use_date_links'] == 'y') {
 			if (isset($_REQUEST['date'])) {
