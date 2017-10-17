f16508c75fda337c58ac38c30f795f5fa2e9a56a
rjsmelo <rjsmelo@users.sourceforge.net>
2017-10-17 00:35:49 +0000

    [REF] Fix notice with $dir_handle not being defined, improve code formatting
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@64294 b456876b-0849-0410-b77d-98878d47e9d5

--- installer/schema/20170127_remove_templates_c_tiki.php
+++ installer/schema/20170127_remove_templates_c_tiki.php
@@ -12,25 +12,27 @@ if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
 
 /**
  * @param $installer
+ * @return void|bool
  */
 function upgrade_20170127_remove_templates_c_tiki($installer)
 {
+	$dir_handle = false;
 	$dirname = 'templates_c';
 	if (is_dir($dirname)) {
 		$dir_handle = opendir($dirname);
 	}
-	if (!$dir_handle) {
+	if (! $dir_handle) {
 		return false;
 	}
-	while($file = readdir($dir_handle)) {
+	while ($file = readdir($dir_handle)) {
 		if ($file != "." && $file != "..") {
-			if (!is_dir($dirname."/".$file))
-				unlink($dirname."/".$file);
-			else
-				delete_directory($dirname.'/'.$file);
+			if (! is_dir($dirname . "/" . $file)) {
+				unlink($dirname . "/" . $file);
+			} else {
+				delete_directory($dirname . '/' . $file);
+			}
 		}
 	}
 	closedir($dir_handle);
 	rmdir($dirname);
 }
-
