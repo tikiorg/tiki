feecea098ea8e0ad38d4b573bfbd37e7a770717e
chealer <chealer@users.sourceforge.net>
2017-11-20 15:35:05 +0000

    type parameter description: [FIX] Redundancy "e.g." + "etc." [FIX] Remove "through ViewerJS" parenthesis The format(s) it applies to are unclear, and it is misleading as ViewerJS is optional.
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@64643 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_mediaplayer.php
+++ lib/wiki-plugins/wikiplugin_mediaplayer.php
@@ -76,7 +76,7 @@ function wikiplugin_mediaplayer_info()
 			'type' => [
 				'required' => false,
 				'name' => tra('File type'),
-				'description' => tr('File type for source URL, e.g. %0mp4%1, %0pdf%1, %0odp%1 (through ViewerJS), etc. Specify one of the supported file types when
+				'description' => tr('File type for source URL, e.g. %0mp4%1, %0pdf%1 or %0odp%1. Specify one of the supported file types when
 					the URL of the file is missing the file extension. This is the case for File Gallery files which
 					have a URL such as %0tiki-download_file.php?fileId=4&display%1 or %0display4%1 if you have Clean URLs
 					enabled.', '<code>', '</code>'),
