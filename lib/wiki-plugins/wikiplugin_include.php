3ac5f98e989b6074d11e9e07c4080e92c131f2f4
lfagundes <lfagundes@users.sourceforge.net>
2017-11-21 20:14:06 +0000

    "Read more..." link to included wiki page when only a part of content is included with Plugin Include
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@64661 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/wiki-plugins/wikiplugin_include.php
+++ lib/wiki-plugins/wikiplugin_include.php
@@ -187,6 +187,11 @@ function wikiplugin_include($dataIn, $params)
 	$text = $parserlib->parse_data($text, $options);
 	$parserlib->setOptions($old_options);
 
+	// append a "Read more" link at end of text if only a portion of page is being included
+	if (isset($start) || isset($stop)) {
+		$text .= '<p><a href="tiki-index.php?page='.urlencode($page).'">'.tra("Read more...").'</a><p>';
+	}
+
 	// append an edit button if page_edit_icon does not equal 'n'
 	if ($page_edit_icon != 'n') {
 		if (isset($perms) && $perms['tiki_p_edit'] === 'y' && strpos($_SERVER['PHP_SELF'], 'tiki-send_newsletters.php') === false) {
