d00f7c7cbf643de2e59b6b71eff869196582211a
lfagundes <lfagundes@users.sourceforge.net>
2017-11-21 18:54:49 +0000

    migration for Plugin Include relations now include trackers, articles and blogs
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@64657 b456876b-0849-0410-b77d-98878d47e9d5

---
+++ installer/schema/20171121_create_plugin_include_relations.php
@@ -0,0 +1,140 @@
+<?php
+if (strpos($_SERVER["SCRIPT_NAME"], basename(__FILE__)) !== false) {
+	header("location: index.php");
+	exit;
+}
+
+/**
+ * Create object relations of type 'tiki.wiki.include', by parsing all
+ * wiki pages and comments and check for Plugin Include calls.
+ *
+ * These are used to keep consistency when renaming an included page and to
+ * warn user when an edition can affect other pages.
+ *
+ * @param Installer $installer
+ */
+function upgrade_20171114_create_plugin_include_relations($installer)
+{
+	global $prefs;
+	$prefs['wikiplugin_maximum_passes'] = 500;
+
+    /**
+     * Wiki pages
+     */
+	$tiki_pages = $installer->table('tiki_pages');
+	$pages = $tiki_pages->fetchAll();
+
+	foreach ($pages as $page) {
+        insert_include_relations($installer, 'wiki page', $page['pageName'], $page['data']);
+	}
+
+    /**
+     * Comments and forum posts
+     */
+	$table = $installer->table('tiki_comments');
+	foreach ($table->fetchAll() as $comment) {
+		if ($comment['objectType'] == 'forum') {
+			$type = 'forum post';
+		} else {
+			$type = $comment['objectType'] . ' comment';
+		}
+        insert_include_relations($installer, $type, $comment['threadId'], $comment['data']);
+	}
+
+    /**
+     * Blog posts
+     */
+	$table = $installer->table('tiki_blog_posts');
+
+	foreach ($table->fetchAll() as $item) {
+        insert_include_relations($installer, 'post', $item['postId'], $item['data']);
+	}
+
+    /**
+     * Articles
+     */
+	$table = $installer->table('tiki_articles');
+
+	foreach ($table->fetchAll() as $item) {
+        $data = $item['heading'] . "\n" . $item['body'];
+        insert_include_relations($installer, 'article', $item['articleId'], $data);
+	}
+
+    /**
+     * Calendar events
+     */
+	$table = $installer->table('tiki_calendar_items');
+
+	foreach ($table->fetchAll() as $item) {
+        insert_include_relations($installer, 'calendar event', $item['calitemId'], $item['description']);
+	}
+
+    /**
+     * Trackers
+     */
+	$table = $installer->table('tiki_trackers');
+
+	foreach ($table->fetchAll() as $item) {
+        if ($item['descriptionIsParsed'] == 'y') {
+            insert_include_relations($installer, 'tracker', $item['trackerId'], $item['description']);
+        }
+	}
+
+    /**
+     * Tracker fields
+     */
+	$table = $installer->table('tiki_tracker_fields');
+
+	foreach ($table->fetchAll() as $item) {
+        if ($item['descriptionIsParsed'] == 'y') {
+            insert_include_relations($installer, 'trackerfield', $item['fieldId'], $item['description']);
+        }
+	}
+
+    
+    /**
+     * Tracker item fields
+     */
+	$trackerFields = $installer->table('tiki_tracker_fields');
+    $itemFields = $installer->table('tiki_tracker_item_fields');
+	foreach ($trackerFields->fetchAll(['fieldId'], ['type' => 'a']) as $field) {
+        $fieldId = $field['fieldId'];
+        foreach($itemFields->fetchAll([], ['fieldId' => (int)$fieldId]) as $itemField) {
+            $objectId = sprintf("%d:%d", (int)$itemField['itemId'], $fieldId);
+            insert_include_relations($installer, 'trackeritemfield', $objectId, $itemField['value']);
+        }
+	}
+}
+
+
+function insert_include_relations($installer, $type, $objectId, $data) {
+    $matches = WikiParser_PluginMatcher::match($data);
+    $argParser = new WikiParser_PluginArgumentParser();
+    foreach ($matches as $match) {
+        if ($match->getName() == 'include') {
+            $params = $argParser->parse($match->getArguments());
+            $existing = $installer->table('tiki_object_relations')->fetchCount([
+                'relation' => 'tiki.wiki.include',
+                'source_type' => $type,
+                'source_itemId' => $objectId,
+                'target_type' => 'wiki page',
+                'target_itemId' => $params['page'],
+            ]);
+            if (!$existing) {
+                $installer->query(
+                    'INSERT INTO `tiki_object_relations` (`relation`, `source_type`, `source_itemId`, `target_type`, `target_itemId`) VALUES(?, ?, ?, ?, ?)',
+                    [
+                        'tiki.wiki.include',
+                        $type,
+                        $objectId,
+                        'wiki page',
+                        $params['page'],
+                    ]
+                );
+            }
+        }
+    }
+}
+
+
+
