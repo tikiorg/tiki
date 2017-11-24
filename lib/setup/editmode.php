29ecad7148928cd20037772020f526642396b6d1
chealer <chealer@users.sourceforge.net>
2017-11-24 16:28:40 +0000

    [REF] Fix/clarify comment
    git-svn-id: https://svn.code.sf.net/p/tikiwiki/code/trunk@64713 b456876b-0849-0410-b77d-98878d47e9d5

--- lib/setup/editmode.php
+++ lib/setup/editmode.php
@@ -45,8 +45,8 @@ if ($_SESSION['wysiwyg'] == 'y') {
 	if ($prefs['wysiwyg_htmltowiki'] !== 'y' && ! isset($info['is_html'])) { // new pages in wysiwyg mode
 		$is_html = true;
 	}
-	if ($is_html && $prefs['feature_wiki_allowhtml'] !== 'y') {
-		$prefs['feature_wiki_allowhtml'] = 'y';		// is page is html temporarily allow html even if pref says no
+	if ($is_html) {
+		$prefs['feature_wiki_allowhtml'] = 'y';		// FIXME: hack. If page is HTML, temporarily allow html even if pref disables it.
 	}
 } elseif ($prefs['feature_wiki_allowhtml'] == 'y' and ($tiki_p_admin == 'y' or $tiki_p_use_HTML == 'y')) {
 	if (isset($_REQUEST['preview']) || isset($jitRequest['edit'])) {
